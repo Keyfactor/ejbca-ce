8d38050242e5550730fa25a7f6c01e8e61a5b850
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2001-11-21 10:11:00 +0000

    *** empty log message ***
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@17 37aeb1de-b14b-0410-9a0e-82b28170d228

--- runtest.sh
+++ runtest.sh
@@ -2,15 +2,15 @@
 
 cd src/java
 echo Testing ra
-#java -cp .:../../lib/junit.jar:../../lib/jnp-client.jar:../../lib/jboss-client.jar:../../lib/jboss-jaas.jar:../../lib/log4j.jar:../../lib/bcprov.jar se.anatom.ejbca.ra.junit.TestRunner
+java -cp .:../../lib/junit.jar:../../lib/jnp-client.jar:../../lib/jboss-client.jar:../../lib/jboss-jaas.jar:../../lib/log4j.jar:../../lib/bcprov.jar se.anatom.ejbca.ra.junit.TestRunner
 echo Testing ca.auth
-#java -cp .:../../lib/junit.jar:../../lib/jnp-client.jar:../../lib/jboss-client.jar:../../lib/jboss-jaas.jar:../../lib/log4j.jar:../../lib/bcprov.jar se.anatom.ejbca.ca.auth.junit.TestRunner
+java -cp .:../../lib/junit.jar:../../lib/jnp-client.jar:../../lib/jboss-client.jar:../../lib/jboss-jaas.jar:../../lib/log4j.jar:../../lib/bcprov.jar se.anatom.ejbca.ca.auth.junit.TestRunner
 echo Testing ca.store
-#java -cp .:../../lib/junit.jar:../../lib/jnp-client.jar:../../lib/jboss-client.jar:../../lib/jboss-jaas.jar:../../lib/log4j.jar:../../lib/bcprov.jar se.anatom.ejbca.ca.store.junit.TestRunner
+java -cp .:../../lib/junit.jar:../../lib/jnp-client.jar:../../lib/jboss-client.jar:../../lib/jboss-jaas.jar:../../lib/log4j.jar:../../lib/bcprov.jar se.anatom.ejbca.ca.store.junit.TestRunner
 echo Testing ca.sign
 java -cp .:../../lib/junit.jar:../../lib/jnp-client.jar:../../lib/jboss-client.jar:../../lib/jboss-jaas.jar:../../lib/log4j.jar:../../lib/bcprov.jar se.anatom.ejbca.ca.sign.junit.TestRunner
 echo Testing ca.crl
-#java -cp .:../../lib/junit.jar:../../lib/jnp-client.jar:../../lib/jboss-client.jar:../../lib/jboss-jaas.jar:../../lib/log4j.jar:../../lib/bcprov.jar se.anatom.ejbca.ca.crl.junit.TestRunner
+java -cp .:../../lib/junit.jar:../../lib/jnp-client.jar:../../lib/jboss-client.jar:../../lib/jboss-jaas.jar:../../lib/log4j.jar:../../lib/bcprov.jar se.anatom.ejbca.ca.crl.junit.TestRunner
 echo Testing batch
-#java -cp .:../../lib/junit.jar:../../lib/jnp-client.jar:../../lib/jboss-client.jar:../../lib/jboss-jaas.jar:../../lib/log4j.jar:../../lib/bcprov.jar se.anatom.ejbca.batch.junit.TestRunner
+java -cp .:../../lib/junit.jar:../../lib/jnp-client.jar:../../lib/jboss-client.jar:../../lib/jboss-jaas.jar:../../lib/log4j.jar:../../lib/bcprov.jar se.anatom.ejbca.batch.junit.TestRunner
 cd ../..
