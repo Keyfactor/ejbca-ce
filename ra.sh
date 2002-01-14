1e1d59b36f065803f9daaccf182992e0b8b97da2
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2002-01-14 14:19:25 +0000

    Fixed scripts to allow blanks in DNs
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@122 37aeb1de-b14b-0410-9a0e-82b28170d228

--- ra.sh
+++ ra.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-
+IFS=
 java -cp ./:./admin.jar:./lib/jnp-client.jar:./lib/jboss-client.jar:./lib/jboss-jaas.jar:./lib/log4j.jar:./lib/jce-jdk13-111.jar se.anatom.ejbca.admin.ra $1 $2 $3 $4 $5 $6
 
