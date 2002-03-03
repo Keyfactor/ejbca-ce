3b3f22302f58315ddbd1a5ea7e525512320c150c
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2002-03-03 16:01:43 +0000

    Restructured database HOWTOs
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@186 37aeb1de-b14b-0410-9a0e-82b28170d228

--- doc/tohtml.sh
+++ doc/tohtml.sh
@@ -4,6 +4,6 @@ txt2html README > README.html
 txt2html SECURITY > SECURITY.html
 txt2html --nolink FAQ > FAQ.html
 txt2html --nolink HOWTO-LDAP.txt > HOWTO-LDAP.html
-txt2html --nolink HOWTO-mysql.txt > HOWTO-mysql.html
-txt2html --nolink HOWTO-postgresql.txt > HOWTO-postgresql.html
-
+#txt2html --nolink HOWTO-mysql.txt > HOWTO-mysql.html
+#txt2html --nolink HOWTO-postgresql.txt > HOWTO-postgresql.html
+txt2html --nolink HOWTO-database.txt > HOWTO-database.html
