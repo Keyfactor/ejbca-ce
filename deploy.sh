e601b5f8709e02cabca02b09e5a1d66a830d47e4
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2002-03-07 15:09:26 +0000

    *** empty log message ***
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@198 37aeb1de-b14b-0410-9a0e-82b28170d228

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,14 @@
 
 if [ -f $JBOSS_HOME ]
 then
-	echo JBOSS_HOME must be set to deploy automgically.
-	exit
+        echo JBOSS_HOME must be set to deploy automagically.
+        exit
 fi
 
 cp dist/*.war $JBOSS_HOME/deploy
 cp dist/*.jar $JBOSS_HOME/deploy
 
-echo jce-jdk13-111.jar and ldap.jar  must be placed in jboss/lib/ext.
+cp lib/jce-jdk13-111.jar $JBOSS_HOME/lib/ext
+cp lib/ldap.jar $JBOSS_HOME/lib/ext
+
+#echo jce-jdk13-111.jar and ldap.jar  must be placed in jboss/lib/ext.
