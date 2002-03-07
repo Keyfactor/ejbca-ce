fbbe4d804d628aad0e562ab01a39f5bcc0580da6
karlkoenig <USER@primekey.com>
2002-03-07 13:55:51 +0000

    Fix problem 525526
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@196 37aeb1de-b14b-0410-9a0e-82b28170d228

--- deploy.cmd
+++ deploy.cmd
@@ -1,14 +1,30 @@
 @echo off
+rem ----
+rem $Id: deploy.cmd,v 1.5 2002-03-07 13:55:51 karlkoenig Exp $
+rem
+rem Deploy script for EJBCA
+rem
+rem Copies all files to their respective location. Also checks
+rem that the dependant files are properly installed.
+rem
+rem ----
 
+rem Check for proper settings of environment variables
 if "%JBOSS_HOME%" == ""  goto error
-goto deploy
-:error 
-echo JBOSS_HOME must be set to deploy automgically.
-goto end
+
+rem Check for dependencies
+if exist %JBOSS_HOME%\lib\ext\jce-jdk13-111.jar goto deploy
+
+:install
+xcopy lib\jce-jdk13-111.jar %JBOSS_HOME%\lib\ext /Q /Y
+xcopy lib\ldap.jar %JBOSS_HOME%\lib\ext /Q /Y
 
 :deploy
-copy dist\*.war %JBOSS_HOME%\deploy
-copy dist\*.jar %JBOSS_HOME%\deploy
+xcopy dist\*.war %JBOSS_HOME%\deploy /Q /Y
+xcopy dist\*.jar %JBOSS_HOME%\deploy /Q /Y
+goto end
+
+:error 
+echo JBOSS_HOME must be set
 
 :end
-echo jce-jdk13-111.jar and ldap.jar must be copied to jboss/lib/ext.
