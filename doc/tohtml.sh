0d563b95baa2798083eaf9bfc09b00f5870b1a57
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2002-04-02 07:10:19 +0000

    *** empty log message ***
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@249 37aeb1de-b14b-0410-9a0e-82b28170d228

--- doc/tohtml.sh
+++ doc/tohtml.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+echo building README.html
 txt2html README > README.html
+echo building SECURITY.html
 txt2html SECURITY > SECURITY.html
+echo building FAQ.html
 txt2html --nolink FAQ > FAQ.html
+echo building HOWTO-LDAP.html
 txt2html --nolink HOWTO-LDAP.txt > HOWTO-LDAP.html
+echo building HOWTO-database.html
 #txt2html --nolink HOWTO-mysql.txt > HOWTO-mysql.html
 #txt2html --nolink HOWTO-postgresql.txt > HOWTO-postgresql.html
 txt2html --nolink HOWTO-database.txt > HOWTO-database.html
