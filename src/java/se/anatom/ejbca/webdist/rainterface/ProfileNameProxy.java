00c35420ae3836bd51e7f2cbd70b2caab194f977
herrvendil <USER@primekey.com>
2002-07-28 23:41:35 +0000

    Added 'Advanced mode' in list user page and 'Previously added users' in add user page, in the ra web gui.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@427 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ src/java/se/anatom/ejbca/webdist/rainterface/ProfileNameProxy.java
@@ -0,0 +1,61 @@
+/*
+ * ProfileNameProxy.java
+ *
+ * Created on den 23 juli 2002, 17:49
+ */
+
+package se.anatom.ejbca.webdist.rainterface;
+import javax.ejb.CreateException;
+import javax.ejb.FinderException;
+import java.rmi.RemoteException;
+import javax.naming.*;
+import java.util.HashMap;
+
+import se.anatom.ejbca.ra.raadmin.IRaAdminSessionRemote;
+import se.anatom.ejbca.ra.raadmin.IRaAdminSessionHome;
+/**
+ * A class used to improve performance by proxying profileid to profilename mappings by minimizing the number of needed lockups over rmi.
+ * 
+ * @author  TomSelleck
+ */
+public class ProfileNameProxy {
+    
+    /** Creates a new instance of ProfileNameProxy */
+    public ProfileNameProxy()  throws   RemoteException, NamingException, FinderException, CreateException {
+              // Get the RaAdminSession instance.
+      InitialContext jndicontext = new InitialContext();
+      Object obj1 = jndicontext.lookup("RaAdminSession");
+      IRaAdminSessionHome raadminsessionhome = (IRaAdminSessionHome) javax.rmi.PortableRemoteObject.narrow(jndicontext.lookup("RaAdminSession"), 
+                                                                                 IRaAdminSessionHome.class);
+      raadminsession = raadminsessionhome.create();  
+      
+      profilenamestore = new HashMap(); 
+        
+    }
+    
+    /**
+     * Method that first tries to find profilename in local hashmap and if it doesn't exists looks it up over RMI.
+     *
+     * @param profileid the profile id number to look up.
+     * @return the profilename or null if no profilename is relatied to the given id
+     */
+    public String getProfileName(int profileid) throws RemoteException {
+      String returnval = null;  
+      // Check if name is in hashmap
+      returnval = (String) profilenamestore.get(new Integer(profileid));
+      
+      if(returnval==null){
+        // Retreive profilename over RMI
+        returnval = raadminsession.getProfileName(profileid);
+        if(returnval != null)
+          profilenamestore.put(new Integer(profileid),returnval);
+      }    
+       
+      return returnval;
+    }
+    
+    // Private fields
+    private HashMap profilenamestore;
+    private IRaAdminSessionRemote raadminsession;
+
+}
