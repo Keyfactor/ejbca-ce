ec47fda73680cb6648ed2305512fb2071d52005e
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2002-10-08 20:10:55 +0000

    more debug
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@548 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/se/anatom/ejbca/protocol/ScepServlet.java
+++ src/java/se/anatom/ejbca/protocol/ScepServlet.java
@@ -37,7 +37,7 @@ import se.anatom.ejbca.util.Base64;
 * 6. sign the reply data (PKCS#7) from the previous step
 * 7. output the result as a der encoded block on stdout
 * -----
-* @version  $Id: ScepServlet.java,v 1.3 2002-09-21 16:12:54 anatom Exp $
+* @version  $Id: ScepServlet.java,v 1.4 2002-10-08 20:10:55 anatom Exp $
 */
 public class ScepServlet extends HttpServlet {
 
@@ -86,6 +86,7 @@ public class ScepServlet extends HttpServlet {
 			administrator = new Admin(Admin.TYPE_PUBLIC_WEB_USER, request.getRemoteAddr());
 			cat.debug("Got request '" + operation + "'");
 			debug.print("<h3>Operation: " + operation + "</h3>");
+            cat.debug("Message: "+message);
             ISignSessionRemote ss = home.create(administrator);
 			if (operation.equals("PKIOperation")) {
                 byte[] scepmsg = Base64.decode(message.getBytes());
