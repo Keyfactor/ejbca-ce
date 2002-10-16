8415a02acd29b644145134765c7d6addb593b5c2
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2002-10-16 13:48:20 +0000

    *** empty log message ***
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@562 37aeb1de-b14b-0410-9a0e-82b28170d228

--- runtest.cmd
+++ runtest.cmd
@@ -5,10 +5,10 @@ rem Check for proper settings of environment variables
 if "%JBOSS_HOME%" == ""  goto error
 
 rem JBoss
-set TEST_CP=.;%JBOSS_HOME%\client\jnp-client.jar;%JBOSS_HOME%\client\jboss-client.jar;%JBOSS_HOME%\client\jboss-j2ee.jar;%JBOSS_HOME%\client\jbosssx-client.jar;%JBOSS_HOME%\client\jboss-common-client.jar;..\..\lib\junit.jar;..\..\lib\log4j-1.2.jar;..\..\lib\jce-jdk13-116.jar
+set TEST_CP=.;%JBOSS_HOME%\client\jnp-client.jar;%JBOSS_HOME%\client\jboss-client.jar;%JBOSS_HOME%\client\jboss-j2ee.jar;%JBOSS_HOME%\client\jbosssx-client.jar;%JBOSS_HOME%\client\jboss-common-client.jar;..\..\lib\junit.jar;..\..\lib\log4j-1.2.jar;..\..\lib\jce-jdk13-116.jar;..\..\lib\bcmail-jdk13-116.jar
 
 rem Weblogic
-rem set TEST_CP=.;..\..\lib\weblogic.jar;..\..\lib\junit.jar;..\..\lib\log4j-1.2.jar;..\..\lib\jce-jdk13-116.jar
+rem set TEST_CP=.;..\..\lib\weblogic.jar;..\..\lib\junit.jar;..\..\lib\log4j-1.2.jar;..\..\lib\jce-jdk13-116.jar;..\..\lib\bcmail-jdk13-116.jar
 
 
 echo Testing utils
