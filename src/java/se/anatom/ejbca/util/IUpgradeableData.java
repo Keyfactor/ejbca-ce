6e6a82696fdf737e9a277c2a85d615abcbb06e8c
herrvendil <USER@primekey.com>
2002-10-24 20:27:50 +0000

    Lots of changes
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@568 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ src/java/se/anatom/ejbca/util/IUpgradeableData.java
@@ -0,0 +1,40 @@
+package se.anatom.ejbca.util;
+
+/**
+ *  IUpgradableData is an interface intended to be used by classed saved to database as BLOB.
+ *  Every such class should put all it's data in one of the Collection data structures and it
+ *  will only be the collection saved to the database. This is to avoid serialization problems
+ *  when upgrading the class.
+ *
+ *  
+ *
+ * @version $Id: IUpgradeableData.java,v 1.1 2002-10-24 20:27:50 herrvendil Exp $
+ */
+
+
+import java.util.Collection;
+
+public interface IUpgradeableData {
+    
+    /** Should return a constant containing the latest available version of the class. */
+    public abstract float getLatestVersion();
+    
+    /** Function returning the current version of the class data. */
+    public abstract float getVersion();
+    
+    /** Function sending the data to be saved to the database. */
+    public abstract Object saveData();   
+    
+    /** 
+     * Function loading saved data into to data structure.
+     */
+    public abstract void loadData(Object data);
+    
+    /**
+     * Function that should handle the update of the data in the class so it's up to date with the latest version.
+     */
+    public abstract void upgrade();
+    
+}
+    
+ 
---
+++ src/java/se/anatom/ejbca/util/UpgradeableDataHashMap.java
@@ -0,0 +1,53 @@
+package se.anatom.ejbca.util;
+
+/**
+ *  UpgradeableDataHashMap is an class implementing the IUpgradeableData intended to be extended by classes
+ *  saving it's data to a database in BLOB form. 
+ *  
+ * @see se.anatom.ejbca.util.IUpgradeableData
+ * @version $Id: UpgradeableDataHashMap.java,v 1.1 2002-10-24 20:27:37 herrvendil Exp $
+ */
+
+
+import java.util.Collection;
+import java.util.HashMap;
+
+public abstract class UpgradeableDataHashMap implements IUpgradeableData {
+        
+    public UpgradeableDataHashMap(){
+      data = new HashMap();  
+      data.put(VERSION,new Float(getLatestVersion()));   
+    }
+    
+    /** Should return a constant containing the latest available version of the class. */
+    public abstract float getLatestVersion();
+    
+    /** Function returning the current version of the class data. */
+    public float getVersion(){
+      return ((Float) data.get(VERSION)).floatValue();   
+    }
+    
+    /** Function sending the data to be saved to the database. */
+    public Object saveData(){
+      return (Object) data;
+    }
+    
+    /** 
+     * Function loading saved data into to data structure.
+     */
+    public  void loadData(Object data){
+      this.data = (HashMap) data;    
+    }
+    
+    /**
+     * Function that should handle the update if of the data in the class so it's up to date with the latest version.
+     * An update is only done when needed.
+     */
+    public abstract void upgrade();
+    
+    protected HashMap data;
+    protected static final String VERSION = "version";
+    
+}
+    
+ 
