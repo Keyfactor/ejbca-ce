bad68a594f45437bcad3f57bb0f4cab749e20afd
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2002-11-13 12:23:39 +0000

    moved conmstants
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@598 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/se/anatom/ejbca/apply/ApplyBean.java
+++ src/java/se/anatom/ejbca/apply/ApplyBean.java
@@ -23,74 +23,74 @@ import se.anatom.ejbca.log.Admin;
 import se.anatom.ejbca.ca.store.ICertificateStoreSessionHome;
 import se.anatom.ejbca.ca.store.ICertificateStoreSessionRemote;
 import se.anatom.ejbca.ca.store.certificateprofiles.CertificateProfile;
+import se.anatom.ejbca.SecConst;
 
 /**
  * A class used as an interface between Apply jsp pages and ejbca functions.
  *
  * @author  Philip Vendil
- * @version $Id: ApplyBean.java,v 1.1 2002-11-07 10:31:08 herrvendil Exp $
+ * @version $Id: ApplyBean.java,v 1.2 2002-11-13 12:23:39 anatom Exp $
  */
 public class ApplyBean   {
 
-    
     /** Creates a new instance of CaInterfaceBean */
     public ApplyBean() {
     }
-    
+
     // Public methods
     public void initialize(HttpServletRequest request) throws  Exception{
       if(!initialized){
         if( request.getAttribute( "javax.servlet.request.X509Certificate" ) != null)
           administrator = new Admin(((X509Certificate[]) request.getAttribute( "javax.servlet.request.X509Certificate" ))[0]);
         else
-           administrator = new Admin(Admin.TYPE_PUBLIC_WEB_USER, request.getRemoteAddr()); 
+           administrator = new Admin(Admin.TYPE_PUBLIC_WEB_USER, request.getRemoteAddr());
         InitialContext jndicontext = new InitialContext();
         Object obj1 = jndicontext.lookup("UserAdminSession");
-        useradminhome = (IUserAdminSessionHome) javax.rmi.PortableRemoteObject.narrow(obj1, IUserAdminSessionHome.class);  
+        useradminhome = (IUserAdminSessionHome) javax.rmi.PortableRemoteObject.narrow(obj1, IUserAdminSessionHome.class);
         obj1 = jndicontext.lookup("CertificateStoreSession");
-        certificatesessionhome = (ICertificateStoreSessionHome) javax.rmi.PortableRemoteObject.narrow(obj1, ICertificateStoreSessionHome.class);        
-        initialized =true; 
-      }    
+        certificatesessionhome = (ICertificateStoreSessionHome) javax.rmi.PortableRemoteObject.narrow(obj1, ICertificateStoreSessionHome.class);
+        initialized =true;
+      }
     }
-    
+
     /** Method that returns a users tokentype defined in SecConst, if 0 is returned user couldn't be found i database. */
     public int getTokenType(String username) throws Exception{
-      int returnval =0;  
-      UserAdminData userdata = null;      
-      IUserAdminSessionRemote useradminsession = useradminhome.create(administrator); 
+      int returnval =0;
+      UserAdminData userdata = null;
+      IUserAdminSessionRemote useradminsession = useradminhome.create(administrator);
       try{
         userdata = useradminsession.findUser(username);
       }catch(FinderException fe){}
       if(userdata != null)
         returnval = userdata.getTokenType();
-      
+
       return returnval;
     }
 
-    /** Method that returns a bitlengths available for the user. Returns null if user couldn't be found in database.*/   
+    /** Method that returns a bitlengths available for the user. Returns null if user couldn't be found in database.*/
     public int[] availableBitLengths(String username) throws Exception {
-      int[] returnval = null;  
-      UserAdminData userdata = null;        
-      IUserAdminSessionRemote useradminsession = useradminhome.create(administrator);  
-      try{      
+      int[] returnval = null;
+      UserAdminData userdata = null;
+      IUserAdminSessionRemote useradminsession = useradminhome.create(administrator);
+      try{
         userdata = useradminsession.findUser(username);
-      }catch(FinderException fe){}      
+      }catch(FinderException fe){}
       if(userdata != null){
-        ICertificateStoreSessionRemote certstoresession = certificatesessionhome.create(administrator);           
-        int certprofile = userdata.getCertificateProfileId();        
-        if(certprofile !=ICertificateStoreSessionRemote.NO_CERTIFICATEPROFILE){
-          returnval =  certstoresession.getCertificateProfile(certprofile).getAvailableBitLengths(); 
-        }        
-      }  
+        ICertificateStoreSessionRemote certstoresession = certificatesessionhome.create(administrator);
+        int certprofile = userdata.getCertificateProfileId();
+        if(certprofile !=SecConst.PROFILE_NO_CERTIFICATEPROFILE){
+          returnval =  certstoresession.getCertificateProfile(certprofile).getAvailableBitLengths();
+        }
+      }
       return returnval;
     }
-    
-  
+
+
     // Private methods
 
     // Private fields
     private IUserAdminSessionHome             useradminhome;
     private ICertificateStoreSessionHome      certificatesessionhome;
     private boolean                           initialized;
-    private Admin                             administrator;    
+    private Admin                             administrator;
 }
