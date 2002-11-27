86b8659f9a83233c3e244169ef4c80201237f006
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2002-11-27 08:08:18 +0000

    fixed usage message
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@607 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/se/anatom/ejbca/admin/CaMakeReqCommand.java
+++ src/java/se/anatom/ejbca/admin/CaMakeReqCommand.java
@@ -13,7 +13,7 @@ import se.anatom.ejbca.util.KeyTools;
 
 /** Generates keys and creates a keystore (PKCS12) to be used by the CA.
  *
- * @version $Id: CaMakeReqCommand.java,v 1.2 2002-09-16 15:21:28 anatom Exp $
+ * @version $Id: CaMakeReqCommand.java,v 1.3 2002-11-27 08:08:18 anatom Exp $
  */
 public class CaMakeReqCommand extends BaseCaAdminCommand {
 
@@ -23,42 +23,42 @@ public class CaMakeReqCommand extends BaseCaAdminCommand {
     }
 
     public void execute() throws IllegalAdminCommandException, ErrorAdminCommandException {
+        if (args.length < 7) {
+            String msg = "Usage: CA makereq <DN> <keysize> <rootca-certificate> <request-file> <keystore-file> <storepassword>";
+            msg += "\nGenerates a certification request for a subCA for sending to a RootCA.";
+            throw new IllegalAdminCommandException(msg);
+        }
+        String dn = args[1];
+        int keysize = Integer.parseInt(args[2]);
+        String rootfile = args[3];
+        String reqfile = args[4];
+        String ksfile = args[5];
+        String storepwd = args[6];
+
+        System.out.println("Generating cert request (and keystore):");
+        System.out.println("DN: "+dn);
+        System.out.println("Keysize: "+keysize);
+        System.out.println("RootCA cert file: "+rootfile);
+        System.out.println("Storing CertificationRequest in: "+reqfile);
+        System.out.println("Storing KeyStore in: "+ksfile);
+        System.out.println("Protected with storepassword: "+storepwd);
+
         try {
-            if (args.length < 7) {
-                String msg = "Usage: CA makereq <DN> <keysize> <rootca-certificate> <request-file> <keystore-file> <storepassword>";
-                msg += "Generates a certification request for a subCA for sending to a RootCA.";
-                throw new IllegalAdminCommandException(msg);
-            }
-            String dn = args[1];
-            int keysize = Integer.parseInt(args[2]);
-            String rootfile = args[3];
-            String reqfile = args[4];
-            String ksfile = args[5];
-            String storepwd = args[6];
-            
-            System.out.println("Generating cert request (and keystore):");
-            System.out.println("DN: "+dn);
-            System.out.println("Keysize: "+keysize);
-            System.out.println("RootCA cert file: "+rootfile);
-            System.out.println("Storing CertificationRequest in: "+reqfile);
-            System.out.println("Storing KeyStore in: "+ksfile);
-            System.out.println("Protected with storepassword: "+storepwd);
-            
             // Read in RootCA certificate
             X509Certificate rootcert = CertTools.getCertfromByteArray(FileTools.readFiletoBuffer(rootfile));
-            
+
             // Generate keys
             System.out.println("Generating keys, please wait...");
             KeyPair rsaKeys = KeyTools.genKeys(keysize);
             // Create selfsigned cert...
             X509Certificate selfcert = CertTools.genSelfCert(dn, 365, null, rsaKeys.getPrivate(), rsaKeys.getPublic(), true);
-            
+
             // Create certificate request
             makeCertRequest(dn, rsaKeys, reqfile);
-            
+
             // Create keyStore
             KeyStore ks = KeyTools.createP12("privateKey", rsaKeys.getPrivate(), selfcert, rootcert);
-            
+
             FileOutputStream os = new FileOutputStream(ksfile);
             ks.store(os, storepwd.toCharArray());
             System.out.println("Keystore '"+ksfile+"' generated succefully.");
@@ -66,5 +66,5 @@ public class CaMakeReqCommand extends BaseCaAdminCommand {
             throw new ErrorAdminCommandException(e);
         }
     } // execute
-    
+
 }
