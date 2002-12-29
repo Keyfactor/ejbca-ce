38fbf194f2ba1c488a0a7e0c7b4dd664a17ae49b
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2002-12-29 16:00:22 +0000

    fixed comments...
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@648 37aeb1de-b14b-0410-9a0e-82b28170d228

--- runtest.sh
+++ runtest.sh
@@ -16,21 +16,21 @@ TEST_CP=.:$JBOSS_HOME/client/jnp-client.jar:$JBOSS_HOME/client/jboss-j2ee.jar:$J
 #TEST_CP=.:../../lib/weblogic.jar:../../lib/junit.jar:../../lib/log4j-1.2.jar:../../lib/jce-jdk13-117.jar:../../lib/bcmail-jdk13-117.jar
 
 echo Testing utils
-#java -cp $TEST_CP se.anatom.ejbca.util.junit.TestRunner
+java -cp $TEST_CP se.anatom.ejbca.util.junit.TestRunner
 echo Testing messages
-java -cp $TEST_CP se.anatom.ejbca.protocol.junit.TestRunner
+#java -cp $TEST_CP se.anatom.ejbca.protocol.junit.TestRunner
 echo Testing ra
-#java -cp $TEST_CP se.anatom.ejbca.ra.junit.TestRunner
+java -cp $TEST_CP se.anatom.ejbca.ra.junit.TestRunner
 echo Testing ca.auth
-#java -cp $TEST_CP se.anatom.ejbca.ca.auth.junit.TestRunner
+java -cp $TEST_CP se.anatom.ejbca.ca.auth.junit.TestRunner
 echo Testing ca.store
-#java -cp $TEST_CP se.anatom.ejbca.ca.store.junit.TestRunner
+java -cp $TEST_CP se.anatom.ejbca.ca.store.junit.TestRunner
 echo Testing ca.sign
-#java -cp $TEST_CP se.anatom.ejbca.ca.sign.junit.TestRunner
+java -cp $TEST_CP se.anatom.ejbca.ca.sign.junit.TestRunner
 echo Testing ca.crl
-#java -cp $TEST_CP se.anatom.ejbca.ca.crl.junit.TestRunner
+java -cp $TEST_CP se.anatom.ejbca.ca.crl.junit.TestRunner
 echo Testing batch
-#java -cp $TEST_CP se.anatom.ejbca.batch.junit.TestRunner
+java -cp $TEST_CP se.anatom.ejbca.batch.junit.TestRunner
 
 cd ../..
 
