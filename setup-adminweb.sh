86f7655c1cd13f5e5755b2e0c0620d9f5be4536b
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2003-01-17 12:12:56 +0000

    made to work
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@678 37aeb1de-b14b-0410-9a0e-82b28170d228

--- setup-adminweb.sh
+++ setup-adminweb.sh
@@ -6,40 +6,39 @@ IFS=
 
 if [ -f $1 ]
 then
-	goto error
+    echo "Usage: setup-adminweb <DN Tomcat Server Cert> <Tomcat keystore passwd> <java cacert keystore passwd>"
+    exit
 fi
 if [ -f $2 ]
 then
-	goto error
+    echo "Usage: setup-adminweb <DN Tomcat Server Cert> <Tomcat keystore passwd> <java cacert keystore passwd>"
+    exit
 fi
 
-ra.sh adduser tomcat $2 $1 null null 1 3
+./ra.sh adduser tomcat $2 \"$1\" null null 1 3
 
-ra.sh adduser walter foo123 "CN=walter" null null 65 2
+./ra.sh adduser walter foo123 "CN=walter" null null 65 2
 
-ra.sh setclearpwd tomcat $2
+./ra.sh setclearpwd tomcat $2
 
-ra.sh setclearpwd walter foo123
+./ra.sh setclearpwd walter foo123
 
-batch.sh
+./batch.sh
 
 cp p12/tomcat.jks $JBOSS_HOME/.keystore
 
-ca.sh getrootcert tmp/rootca.der
+./ca.sh getrootcert tmp/rootca.der
 
-keytool -import -trustcacerts -file tmp\rootca.der -keystore $JAVA_HOME/jre/lib/security/cacerts -storepass $3
+#This command must be run as root
+echo run this command as root:
+echo keytool -import -trustcacerts -file tmp/rootca.der -keystore $JAVA_HOME/jre/lib/security/cacerts -storepass $3
 
 rm tmp/rootca.der
 
 export CP=.:./admin.jar:./lib/regexp1_0_0.jar
 
-java -cp $CP se.anatom.ejbca.util.TomcatServiceXMLPasswordReplace src/adminweb/WEB-INF/tomcat4-service.xml tmp/tomcat4-service.xml %2
+java -cp $CP se.anatom.ejbca.util.TomcatServiceXMLPasswordReplace src/adminweb/WEB-INF/tomcat4-service.xml tmp/tomcat4-service.xml $2
 
 cp tmp/tomcat4-service.xml $JBOSS_HOME/server/default/deploy/tomcat4-service.xml
 
 rm tmp/tomcat4-service.xml
-
-goto end
-:error
-echo "Usage: setup-adminweb <DN Tomcat Server Cert> <Tomcat keystore passwd> <java cacert keystore passwd>"
-:end
