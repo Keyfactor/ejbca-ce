907f15b02a0ba5cfc8cb9da079246151ba84a22d
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2003-02-15 13:30:46 +0000

    removed wrong throws clause
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@757 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/se/anatom/ejbca/keyrecovery/IKeyRecoverySessionLocal.java
+++ src/java/se/anatom/ejbca/keyrecovery/IKeyRecoverySessionLocal.java
@@ -9,7 +9,7 @@ import se.anatom.ejbca.log.Admin;
 
 /** Local interface for EJB, unforturnately this must be a copy of the remote interface except that RemoteException is not thrown, see IKeyRecoverySessionRemote for docs.
  *
- * @version $Id: IKeyRecoverySessionLocal.java,v 1.1 2003-02-12 13:21:30 herrvendil Exp $
+ * @version $Id: IKeyRecoverySessionLocal.java,v 1.2 2003-02-15 13:30:46 anatom Exp $
  * @see java.se.anatom.ejbca.keyrecovery.IKeyRecoverySessionRemote
  */
 
@@ -18,65 +18,65 @@ public interface IKeyRecoverySessionLocal extends javax.ejb.EJBLocalObject
 {
     /**
      * @see java.se.anatom.ejbca.keyrecovery.IKeyRecoverySessionRemote
-     */        
-    
-    public boolean addKeyRecoveryData(Admin admin, X509Certificate certificate, String username, KeyPair keypair);   
-    
+     */
+
+    public boolean addKeyRecoveryData(Admin admin, X509Certificate certificate, String username, KeyPair keypair);
+
     /**
      * @see java.se.anatom.ejbca.keyrecovery.IKeyRecoverySessionRemote
-     */     
-    
-    public boolean changeKeyRecoveryData(Admin admin,  X509Certificate certificate, 
-                                         boolean markedasrecoverable, KeyPair keypair);    
-    
+     */
+
+    public boolean changeKeyRecoveryData(Admin admin,  X509Certificate certificate,
+                                         boolean markedasrecoverable, KeyPair keypair);
+
     /**
      * @see java.se.anatom.ejbca.keyrecovery.IKeyRecoverySessionRemote
-     */     
-    
+     */
+
     public void removeKeyRecoveryData(Admin admin, X509Certificate certificate);
-    
+
     /**
      * @see java.se.anatom.ejbca.keyrecovery.IKeyRecoverySessionRemote
-     */     
-    
-    public void removeAllKeyRecoveryData(Admin admin, String username);    
-      
+     */
+
+    public void removeAllKeyRecoveryData(Admin admin, String username);
+
     /**
      * @see java.se.anatom.ejbca.keyrecovery.IKeyRecoverySessionRemote
-     */     
-    
+     */
+
     public KeyRecoveryData keyRecovery(Admin admin, String username);
-    
+
     /**
      * @see java.se.anatom.ejbca.keyrecovery.IKeyRecoverySessionRemote
-     */     
-    
-    public boolean markNewestAsRecoverable(Admin admin, String username);    
-    
+     */
+
+    public boolean markNewestAsRecoverable(Admin admin, String username);
+
     /**
      * @see java.se.anatom.ejbca.keyrecovery.IKeyRecoverySessionRemote
-     */     
-    
+     */
+
     public boolean markAsRecoverable(Admin admin, X509Certificate certificate);
-    
+
     /**
      * @see java.se.anatom.ejbca.keyrecovery.IKeyRecoverySessionRemote
-     */     
-    
+     */
+
     public void unmarkUser(Admin admin, String username);
-    
+
     /**
      * @see java.se.anatom.ejbca.keyrecovery.IKeyRecoverySessionRemote
-     */     
-    
+     */
+
     public boolean isUserMarked(Admin admin, String username);
-    
+
     /**
      * @see java.se.anatom.ejbca.keyrecovery.IKeyRecoverySessionRemote
-     */     
-    
-    public boolean existsKeys(Admin admin, X509Certificate certificate) throws RemoteException;    
-    
+     */
+
+    public boolean existsKeys(Admin admin, X509Certificate certificate);
+
 
 }
 
