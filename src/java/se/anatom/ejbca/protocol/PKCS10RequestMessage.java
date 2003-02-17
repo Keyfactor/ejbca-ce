e75e735a7d2a247e20ff61613c4b5aea583847e3
scop <USER@primekey.com>
2003-02-17 11:39:22 +0000

    Added a PKCS10CertificationRequest based constructor, ensure that the PKCS10 is initialized in getCertificationRequest().
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@762 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/se/anatom/ejbca/protocol/PKCS10RequestMessage.java
+++ src/java/se/anatom/ejbca/protocol/PKCS10RequestMessage.java
@@ -1,6 +1,7 @@
 package se.anatom.ejbca.protocol;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.Serializable;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
@@ -16,32 +17,50 @@ import org.bouncycastle.jce.PKCS10CertificationRequest;
 /**
  * Class to handle PKCS10 request messages sent to the CA.
  *
- * @version $Id: PKCS10RequestMessage.java,v 1.9 2003-02-12 11:23:18 scop Exp $
+ * @version $Id: PKCS10RequestMessage.java,v 1.10 2003-02-17 11:39:22 scop Exp $
  */
 public class PKCS10RequestMessage implements IRequestMessage, Serializable {
 
     private static Logger log = Logger.getLogger(PKCS10RequestMessage.class);
 
-    /** Raw form of the PKCS10 message
+    /**
+     * Raw form of the PKCS10 message
      */
     private byte[] msg;
-    /** The pkcs10 request message
+
+    /**
+     * The pkcs10 request message
      */
     private transient PKCS10CertificationRequest pkcs10 = null;
 
-    /** Constucts a new PKCS10 message handler object.
-     * @param msg The DER encoded PKCS10 request.
+    /**
+     * Constructs a new PKCS#10 message handler object.
+     * @param msg The DER encoded PKCS#10 request.
      * @throws IOException if the request can not be parsed.
      */
     public PKCS10RequestMessage(byte[] msg) {
-        log.debug(">PKCS10RequestMessage");
+        log.debug(">PKCS10RequestMessage(byte[])");
         this.msg = msg;
         init();
-        log.debug("<PKCS10RequestMessage");
+        log.debug("<PKCS10RequestMessage(byte[])");
     }
+
+    /**
+     * Constructs a new PKCS#10 message handler object.
+     * @param p10 the PKCS#10 request
+     */
+    public PKCS10RequestMessage(PKCS10CertificationRequest p10)
+    {
+        log.debug(">PKCS10RequestMessage(PKCS10CertificationRequest)");
+        msg = p10.getEncoded();
+        pkcs10 = p10;
+        log.debug("<PKCS10RequestMessage(PKCS10CertificationRequest)");
+    }
+
     private void init() {
         pkcs10 = new PKCS10CertificationRequest(msg);
     }
+
     public PublicKey getRequestPublicKey() throws InvalidKeyException, NoSuchAlgorithmException, NoSuchProviderException {
         try {
             if (pkcs10 == null)
@@ -51,10 +70,15 @@ public class PKCS10RequestMessage implements IRequestMessage, Serializable {
             return null;
         }
         return pkcs10.getPublicKey();
-      }
+    }
 
+    /**
+     * Gets the underlying BC <code>PKCS10CertificationRequest</code> object.
+     * @return the request object
+     */
     public PKCS10CertificationRequest getCertificationRequest()
     {
+        if (pkcs10 == null) init();
         return pkcs10;
     }
 
