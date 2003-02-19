aa1d10f8c23b644d384831e4401a3d1579a3439a
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2003-02-19 10:20:39 +0000

    fixed primary key
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@764 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/se/anatom/ejbca/keyrecovery/KeyRecoveryDataBean.java
+++ src/java/se/anatom/ejbca/keyrecovery/KeyRecoveryDataBean.java
@@ -22,7 +22,7 @@ import se.anatom.ejbca.util.CertTools;
  *  keypair
  * </pre>
  *
- * @version $Id: KeyRecoveryDataBean.java,v 1.1 2003-02-12 13:21:30 herrvendil Exp $
+ * @version $Id: KeyRecoveryDataBean.java,v 1.2 2003-02-19 10:20:39 anatom Exp $
  **/
 
 public abstract class KeyRecoveryDataBean implements javax.ejb.EntityBean {
@@ -32,27 +32,27 @@ public abstract class KeyRecoveryDataBean implements javax.ejb.EntityBean {
     private static Category log = Category.getInstance(KeyRecoveryDataBean.class.getName() );
 
     protected EntityContext  ctx;
-    public abstract KeyRecoveryDataPK getPK();
-    public abstract void setPK(KeyRecoveryDataPK pk);
- 
-    public abstract String getCertSN();   
-    public abstract void setCertSN(String certificatesn);   
- 
-    public abstract String getIssuerDN();   
-    public abstract void setIssuerDN(String issuerdn);   
-    
+    public abstract int getPK();
+    public abstract void setPK(int pK);
+
+    public abstract String getCertSN();
+    public abstract void setCertSN(String certificatesn);
+
+    public abstract String getIssuerDN();
+    public abstract void setIssuerDN(String issuerdn);
+
     public abstract String getUsername();
-    public abstract void setUsername(String username);                       
-        
-    public abstract boolean getMarkedAsRecoverable();   
+    public abstract void setUsername(String username);
+
+    public abstract boolean getMarkedAsRecoverable();
     public abstract void setMarkedAsRecoverable(boolean markedasrecoverable);
-    
-    public abstract KeyPair getKeyPair(); 
+
+    public abstract KeyPair getKeyPair();
     public abstract void setKeyPair(KeyPair keypair);
-    
+
     public BigInteger getCertificateSN(){ return new BigInteger(getCertSN(),16);  }
-    public void setCertificateSN(BigInteger certificatesn){ setCertSN(certificatesn.toString(16)); }      
-   
+    public void setCertificateSN(BigInteger certificatesn){ setCertSN(certificatesn.toString(16)); }
+
     //
     // Fields required by Container
     //
@@ -67,13 +67,13 @@ public abstract class KeyRecoveryDataBean implements javax.ejb.EntityBean {
 
     public KeyRecoveryDataPK ejbCreate(BigInteger certificatesn, String issuerdn, String username, KeyPair keypair) throws CreateException {
         KeyRecoveryDataPK pk = new KeyRecoveryDataPK(certificatesn,issuerdn);
-        setPK(pk);
+        setPK(pk.pK);
         setCertificateSN(certificatesn);
         setIssuerDN(issuerdn);
         setUsername(username);
         setMarkedAsRecoverable(false);
-        setKeyPair(keypair);        
-        
+        setKeyPair(keypair);
+
         log.debug("Created Key Recoverydata for user "+ username );
         return pk;
     }
