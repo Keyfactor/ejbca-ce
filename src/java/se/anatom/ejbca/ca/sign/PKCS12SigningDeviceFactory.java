28d2bcf21b13a951681f9f922ef23325708752a8
koen_serry <USER@primekey.com>
2003-02-28 00:42:59 +0000

    implements SigningdeviceFactory
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@824 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/se/anatom/ejbca/ca/sign/PKCS12SigningDeviceFactory.java
+++ src/java/se/anatom/ejbca/ca/sign/PKCS12SigningDeviceFactory.java
@@ -4,13 +4,10 @@ import java.util.Properties;
 
 /** Factory for PKCS12 signing device.
  *
- * @version $Id: PKCS12SigningDeviceFactory.java,v 1.4 2002-08-20 12:17:11 anatom Exp $
+ * @version $Id: PKCS12SigningDeviceFactory.java,v 1.5 2003-02-28 00:42:59 koen_serry Exp $
  */
 
-public class PKCS12SigningDeviceFactory {
-
-    public PKCS12SigningDeviceFactory() {
-    }
+public class PKCS12SigningDeviceFactory implements ISigningDeviceFactory {
 
    /** Creates (if needed) the signing device and returns the object.
     * @param prop Arguments needed for the eventual creation of the object
