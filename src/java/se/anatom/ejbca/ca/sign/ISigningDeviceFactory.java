45147a5391726405f6e0acd345f98bf30c3cdb33
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2003-02-28 08:53:42 +0000

    added id
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@826 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/se/anatom/ejbca/ca/sign/ISigningDeviceFactory.java
+++ src/java/se/anatom/ejbca/ca/sign/ISigningDeviceFactory.java
@@ -2,7 +2,12 @@ package se.anatom.ejbca.ca.sign;
 
 import java.util.Properties;
 
+/**
+ * Interface for a factory for ISigningDevices.
+ *
+ * @version $Id$
+ */
 public interface ISigningDeviceFactory
 {
- public ISigningDevice makeInstance(Properties prop) throws Exception;
+    public ISigningDevice makeInstance(Properties prop) throws Exception;
 }
