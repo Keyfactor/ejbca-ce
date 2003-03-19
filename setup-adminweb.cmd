16c9c17fb2afd0853d0d34b22fabba3cc8a62cc8
herrvendil <USER@primekey.com>
2003-03-19 08:59:35 +0000

    *** empty log message ***
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@904 37aeb1de-b14b-0410-9a0e-82b28170d228

--- setup-adminweb.cmd
+++ setup-adminweb.cmd
@@ -9,17 +9,17 @@ if %4() == () goto error
 
 call ra adduser tomcat %2 %1 null null 1 3
 
-rem call ra adduser superadmin %3 "CN=SuperAdmin" null null 65 2
+call ra adduser superadmin %3 "CN=SuperAdmin" null null 65 2
 
-rem call ra setclearpwd tomcat %2
+call ra setclearpwd tomcat %2
 
-rem call ra setclearpwd superadmin %3
+call ra setclearpwd superadmin %3
 
-rem call batch
+call batch
 
-rem copy p12\tomcat.jks %JBOSS_HOME%\.keystore
+copy p12\tomcat.jks %JBOSS_HOME%\.keystore
 
-rem call ca getrootcert tmp\rootca.der
+call ca getrootcert tmp\rootca.der
 
 keytool -alias EJBCA-CA -delete -keystore %JAVA_HOME%\jre\lib\security\cacerts -storepass %4
 keytool -alias EJBCA-CA -import -trustcacerts -file tmp\rootca.der -keystore %JAVA_HOME%\jre\lib\security\cacerts -storepass %4
@@ -31,11 +31,11 @@ set CP=.;.\admin.jar;.\lib\regexp1_0_0.jar
 set TOMCAT_XML=tomcat41-service.xml
 if exist "%JBOSS_HOME%\server\default\deploy\tomcat4-service.xml" set TOMCAT_XML=tomcat4-service.xml
 
-rem java -cp %CP% se.anatom.ejbca.util.TomcatServiceXMLPasswordReplace src\adminweb\WEB-INF\%TOMCAT_XML% tmp\%TOMCAT_XML% %2
+java -cp %CP% se.anatom.ejbca.util.TomcatServiceXMLPasswordReplace src\adminweb\WEB-INF\%TOMCAT_XML% tmp\%TOMCAT_XML% %2
 
-rem copy tmp\%TOMCAT_XML% %JBOSS_HOME%\server\default\deploy\%TOMCAT_XML%
+copy tmp\%TOMCAT_XML% %JBOSS_HOME%\server\default\deploy\%TOMCAT_XML%
 
-rem del tmp\%TOMCAT_XML%
+del tmp\%TOMCAT_XML%
 
 goto end
 :error
