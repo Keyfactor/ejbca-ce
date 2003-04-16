09ccd5a147a13a7387b855b8fed63d63dac84e2c
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2003-04-16 08:45:34 +0000

    support for jboss 3.2
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@948 37aeb1de-b14b-0410-9a0e-82b28170d228

--- ra.cmd
+++ ra.cmd
@@ -4,7 +4,7 @@ rem Check for proper settings of environment variables
 if "%JBOSS_HOME%" == ""  goto error
 
 rem JBoss
-set CP=.;.\admin.jar;%JBOSS_HOME%\client\jnp-client.jar;%JBOSS_HOME%\client\jboss-client.jar;%JBOSS_HOME%\client\jboss-j2ee.jar;%JBOSS_HOME%\client\jbosssx-client.jar;%JBOSS_HOME%\client\jboss-common-client.jar;.\lib\junit.jar;.\lib\log4j-1.2.7.jar;.\lib\bcprov-jdk14-119.jar;..\..\lib\bcmail-jdk14-119.jar
+set CP=.;.\admin.jar;%JBOSS_HOME%\client\jnp-client.jar;%JBOSS_HOME%\client\jbossall-client.jar;%JBOSS_HOME%\client\jboss-client.jar;%JBOSS_HOME%\client\jboss-j2ee.jar;%JBOSS_HOME%\client\jbosssx-client.jar;%JBOSS_HOME%\client\jboss-common-client.jar;.\lib\junit.jar;.\lib\log4j-1.2.7.jar;.\lib\bcprov-jdk14-119.jar;..\..\lib\bcmail-jdk14-119.jar
 
 rem Weblogic
 rem set CP=.;.\admin.jar;.\lib\weblogic.jar;.\lib\junit.jar;.\lib\log4j-1.2.7.jar;.\lib\bcprov-jdk14-119.jar;..\..\lib\bcmail-jdk14-119.jar
