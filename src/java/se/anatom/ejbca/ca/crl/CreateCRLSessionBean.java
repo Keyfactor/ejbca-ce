5afcd1bc05738a8c295aa16acaa83d571520157b
herrvendil <USER@primekey.com>
2003-09-03 15:47:16 +0000

    EJBCA 3.0
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@1100 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/se/anatom/ejbca/ca/crl/CreateCRLSessionBean.java
+++ src/java/se/anatom/ejbca/ca/crl/CreateCRLSessionBean.java
@@ -9,133 +9,127 @@ import javax.ejb.*;
 
 import se.anatom.ejbca.BaseSessionBean;
 import se.anatom.ejbca.IJobRunnerSession;
-import se.anatom.ejbca.ca.crl.RevokedCertInfo;
-import se.anatom.ejbca.ca.sign.ISignSessionLocal;
-import se.anatom.ejbca.ca.sign.ISignSessionLocalHome;
+import se.anatom.ejbca.ca.caadmin.ICAAdminSessionLocal;
+import se.anatom.ejbca.ca.caadmin.ICAAdminSessionLocalHome;
+import se.anatom.ejbca.ca.caadmin.CAInfo;
+import se.anatom.ejbca.ca.store.ICertificateStoreSessionLocalHome;
+import se.anatom.ejbca.ca.store.ICertificateStoreSessionLocal;
+import se.anatom.ejbca.ca.store.CertificateDataLocalHome;
 import se.anatom.ejbca.ca.store.CertificateData;
 import se.anatom.ejbca.ca.store.CertificateDataLocal;
 import se.anatom.ejbca.ca.store.CertificateDataLocalHome;
 import se.anatom.ejbca.ca.store.CertificateDataPK;
-import se.anatom.ejbca.ca.store.ICertificateStoreSessionLocal;
-import se.anatom.ejbca.ca.store.ICertificateStoreSessionLocalHome;
+import se.anatom.ejbca.ca.crl.RevokedCertInfo;
+import se.anatom.ejbca.ca.sign.ISignSessionLocalHome;
+import se.anatom.ejbca.ca.sign.ISignSessionLocal;
 import se.anatom.ejbca.log.Admin;
-import se.anatom.ejbca.log.ILogSessionHome;
-import se.anatom.ejbca.log.ILogSessionRemote;
+import se.anatom.ejbca.log.ILogSessionLocal;
+import se.anatom.ejbca.log.ILogSessionLocalHome;
 import se.anatom.ejbca.log.LogEntry;
 
 
 /**
- * Generates a new CRL by looking in the database for revoked certificates and generating a CRL.
+ * Generates a new CRL by looking in the database for revoked certificates and
+ * generating a CRL.
  *
- * @version $Id: CreateCRLSessionBean.java,v 1.14 2003-07-24 08:43:30 anatom Exp $
+ * @version $Id: CreateCRLSessionBean.java,v 1.15 2003-09-03 15:47:16 herrvendil Exp $
  */
 public class CreateCRLSessionBean extends BaseSessionBean implements IJobRunnerSession {
-    private Long crlperiod;
+    
 
-    /** The home interface of Certificate store */
+    /** The local home interface of Certificate store */
     private ICertificateStoreSessionLocalHome storeHome = null;
 
-    /** The home interface of Certificate entity bean */
+    /** The local home interface of Certificate entity bean */
     private CertificateDataLocalHome certHome = null;
 
-    /** The home interface of the signing session */
+    /** The local home interface of the signing session */
     private ISignSessionLocalHome signHome = null;
 
-    /** The remote interface of the log session bean */
-    private ILogSessionRemote logsession;
-
-    /**
-     * Default create for SessionBean without any creation Arguments.
-     *
+    /** The local home interface of the caadmin session */
+    private ICAAdminSessionLocalHome caadminHome = null;    
+    
+    /** The local interface of the log session bean */
+    private ILogSessionLocal logsession;        
+    
+   
+    
+    /** Default create for SessionBean without any creation Arguments.
      * @throws CreateException if bean instance can't be created
      */
-    public void ejbCreate() throws CreateException {
+    public void ejbCreate () throws CreateException {
         debug(">ejbCreate()");
-
-        // Get env variables and read in nessecary data
-        crlperiod = (Long) lookup("java:comp/env/CRLPeriod", java.lang.Long.class);
-        debug("crlperiod:" + crlperiod);
-        storeHome = (ICertificateStoreSessionLocalHome) lookup(
-                "java:comp/env/ejb/CertificateStoreSessionLocal");
-        certHome = (CertificateDataLocalHome) lookup("java:comp/env/ejb/CertificateDataLocal");
-        signHome = (ISignSessionLocalHome) lookup("java:comp/env/ejb/SignSessionLocal");
-
-        try {
-            ILogSessionHome logsessionhome = (ILogSessionHome) lookup("java:comp/env/ejb/LogSession",
-                    ILogSessionHome.class);
-            logsession = logsessionhome.create();
-        } catch (Exception e) {
-            throw new EJBException(e);
-        }
-
+        caadminHome = (ICAAdminSessionLocalHome)lookup("java:comp/env/ejb/CAAdminSessionLocal");
+        storeHome = (ICertificateStoreSessionLocalHome)lookup("java:comp/env/ejb/CertificateStoreSessionLocal");
+        certHome = (CertificateDataLocalHome)lookup("java:comp/env/ejb/CertificateDataLocal");
+        signHome = (ISignSessionLocalHome)lookup("java:comp/env/ejb/SignSessionLocal");
+        
+        try{
+          ILogSessionLocalHome logsessionhome = (ILogSessionLocalHome) lookup("java:comp/env/ejb/LogSessionLocal",ILogSessionLocalHome.class);       
+          logsession = logsessionhome.create();
+        }catch(Exception e){
+          throw new EJBException(e);   
+        }  
+        
         debug("<ejbCreate()");
     }
 
-    /**
-     * Generates a new CRL by looking in the database for revoked certificates and generating a
-     * CRL.
-     *
-     * @param admin administrator performing the task
-     *
-     * @throws EJBException om ett kommunikations eller systemfel intr?ffar.
-     */
-    public void run(Admin admin) throws RemoteException {
+	/**
+	 * Generates a new CRL by looking in the database for revoked certificates and generating a
+	 * CRL.
+	 *
+	 * @param admin administrator performing the task
+	 *
+	 * @throws EJBException om ett kommunikations eller systemfel intr?ffar.
+	 */
+    public void run(Admin admin, String issuerdn) throws RemoteException {
         debug(">run()");
-
-        try {
+        int caid = issuerdn.hashCode();
+        try {       
+            ICAAdminSessionLocal caadmin = caadminHome.create();
             ICertificateStoreSessionLocal store = storeHome.create();
-
+            
+            CAInfo cainfo = caadmin.getCAInfo(admin, issuerdn.hashCode());
+            int crlperiod = cainfo.getCRLPeriod();
             // Find all revoked certificates
-            Collection revcerts = store.listRevokedCertificates(admin);
-            debug("Found " + revcerts.size() + " revoked certificates.");
+            Collection revcerts = store.listRevokedCertificates(admin, issuerdn);
+            debug("Found "+revcerts.size()+" revoked certificates.");
 
             // Go through them and create a CRL, at the same time archive expired certificates
             Date now = new Date();
-
             // crlperiod is hours = crlperiod*60*60*1000 milliseconds
-            now.setTime(now.getTime() - (crlperiod.longValue() * 60 * 60 * 1000));
-
+            now.setTime(now.getTime() - (crlperiod * 60 * 60 * 1000));
             Vector certs = new Vector();
             Iterator iter = revcerts.iterator();
-
             while (iter.hasNext()) {
-                CertificateDataPK pk = new CertificateDataPK((String) iter.next());
+                CertificateDataPK pk = new CertificateDataPK((String)iter.next());
                 CertificateDataLocal data = certHome.findByPrimaryKey(pk);
-
                 // We want to include certificates that was revoked after the last CRL was issued, but before this one
                 // so the revoked certs are included in ONE CRL at least.
-                if ((data.getStatus() == CertificateData.CERT_REVOKED) &&
-                        (data.getExpireDate() < now.getTime())) {
-                    data.setStatus(CertificateData.CERT_ARCHIVED);
-                } else {
-                    if (data.getRevocationDate() == -1) {
+                if ( (data.getStatus() == CertificateData.CERT_REVOKED) &&
+                    (data.getExpireDate() < now.getTime()) )
+                {
+                        data.setStatus(CertificateData.CERT_ARCHIVED);
+                } else
+                {
+                    if (data.getRevocationDate() == -1)
                         data.setRevocationDate((new Date()).getTime());
-                    }
-
-                    RevokedCertInfo certinfo = new RevokedCertInfo(new BigInteger(
-                                data.getSerialNumber()), new Date(data.getRevocationDate()),
-                            data.getRevocationReason());
+                    RevokedCertInfo certinfo = new RevokedCertInfo(new BigInteger(data.getSerialNumber()),new Date(data.getRevocationDate()), data.getRevocationReason());
                     certs.add(certinfo);
                 }
             }
-
             ISignSessionLocal sign = signHome.create();
-            X509CRL crl = sign.createCRL(admin, certs);
+            X509CRL crl = (X509CRL) sign.createCRL(admin, caid, certs);
 
             //FileOutputStream fos = new FileOutputStream("srvtestcrl.der");
             //fos.write(crl.getEncoded());
             //fos.close();
-        } catch (Exception e) {
-            try {
-                logsession.log(admin, LogEntry.MODULE_CA, new java.util.Date(), null, null,
-                    LogEntry.EVENT_ERROR_CREATECRL, "");
-            } catch (RemoteException re) {
-                throw new EJBException(re);
-            }
-
+        } catch (Exception e) {            
+            logsession.log(admin, caid, LogEntry.MODULE_CA, new java.util.Date(),null, null, LogEntry.EVENT_ERROR_CREATECRL,"");                
             throw new EJBException(e);
         }
-
         debug("<run()");
     }
 }
+
+
--- src/java/se/anatom/ejbca/ca/crl/RevokedCertInfo.java
+++ src/java/se/anatom/ejbca/ca/crl/RevokedCertInfo.java
@@ -3,37 +3,37 @@ package se.anatom.ejbca.ca.crl;
 import java.math.BigInteger;
 import java.util.Date;
 
-
 /**
- * Holds information about a revoked certificate. The information kept here is the information that
- * goes into a CRLEntry.
+ * Holds information about a revoked certificate. The information kept here is the
+ * information that goes into a CRLEntry.
  *
- * @version $Id: RevokedCertInfo.java,v 1.4 2003-07-24 08:43:30 anatom Exp $
- */
+ * @version $Id: RevokedCertInfo.java,v 1.5 2003-09-03 15:47:16 herrvendil Exp $
+ **/
 public class RevokedCertInfo extends java.lang.Object implements java.io.Serializable {
+    
     /** Constants defining different revokation reasons. */
-    public static final int REVOKATION_REASON_UNSPECIFIED = 0;
-    public static final int REVOKATION_REASON_KEYCOMPROMISE = 1;
-    public static final int REVOKATION_REASON_CACOMPROMISE = 2;
-    public static final int REVOKATION_REASON_AFFILIATIONCHANGED = 3;
-    public static final int REVOKATION_REASON_SUPERSEDED = 4;
-    public static final int REVOKATION_REASON_CESSATIONOFOPERATION = 5;
-    public static final int REVOKATION_REASON_CERTIFICATEHOLD = 6;
-    public static final int REVOKATION_REASON_REMOVEFROMCRL = 8;
-    public static final int REVOKATION_REASON_PRIVILEGESWITHDRAWN = 9;
-    public static final int REVOKATION_REASON_AACOMPROMISE = 10;
-    private BigInteger userCertificate;
-    private Date revocationDate;
-    private int reason;
+    public final static int REVOKATION_REASON_UNSPECIFIED          = 0;
+    public final static int REVOKATION_REASON_KEYCOMPROMISE        = 1;
+    public final static int REVOKATION_REASON_CACOMPROMISE         = 2;
+    public final static int REVOKATION_REASON_AFFILIATIONCHANGED   = 3;
+    public final static int REVOKATION_REASON_SUPERSEDED           = 4;
+    public final static int REVOKATION_REASON_CESSATIONOFOPERATION = 5;
+    public final static int REVOKATION_REASON_CERTIFICATEHOLD      = 6;      
+    public final static int REVOKATION_REASON_REMOVEFROMCRL        = 8;
+    public final static int REVOKATION_REASON_PRIVILEGESWITHDRAWN  = 9;
+    public final static int REVOKATION_REASON_AACOMPROMISE         = 10;
+    
+
+    private BigInteger  userCertificate;
+    private Date        revocationDate;
+    private int         reason;
 
     /**
      * Constuctor filling in the whole object.
      *
-     * @param serno certificate serial number
-     * @param date date of revocation
-     * @param reason revocation reason
-     */
-    public RevokedCertInfo(BigInteger serno, Date date, int reason) {
+     **/
+    public RevokedCertInfo(BigInteger serno, Date date, int reason)
+    {
         this.userCertificate = serno;
         this.revocationDate = date;
         this.reason = reason;
@@ -41,39 +41,32 @@ public class RevokedCertInfo extends java.lang.Object implements java.io.Seriali
 
     /**
      * Certificate serial number
-     *
-     * @return certificate serial number
-     */
+     **/
     public BigInteger getUserCertificate() {
         return this.userCertificate;
     }
 
     /**
      * Certificate serial number
-     *
-     * @param serno certificate serial number
-     */
-    public void setUserCertificate(BigInteger serno) {
+     **/
+    public void setUserCertificate( BigInteger serno ) {
         this.userCertificate = serno;
     }
 
     /**
      * Date when the certificate was revoked.
-     *
-     * @return revocation date
-     */
+     **/
     public Date getRevocationDate() {
         return this.revocationDate;
     }
 
     /**
      * Date when the certificate was revoked.
-     *
-     * @param date revocation date
-     */
-    public void setRevocationDate(Date date) {
+     **/
+    public void setRevocationDate( Date date ) {
         this.revocationDate = date;
     }
+
 
     /**
      * The reason the certificate was revoked.
@@ -91,28 +84,19 @@ public class RevokedCertInfo extends java.lang.Object implements java.io.Seriali
      *    aACompromise(10)
      * }
      * </pre>
-     *
-     * @return revocation reason
-     */
+     **/
     public int getReason() {
         return this.reason;
     }
 
     /**
      * The reason the certificate was revoked.
-     *
-     * @param reason revocation reason
-     */
-    public void setReason(int reason) {
+     **/
+    public void setReason( int reason ) {
         this.reason = reason;
     }
 
-    /**
-     * overrides standard method
-     *
-     * @return string representation
-     */
     public String toString() {
-        return (this.userCertificate == null) ? "null" : this.userCertificate.toString();
+        return this.userCertificate == null ? "null" : this.userCertificate.toString();
     }
 }
