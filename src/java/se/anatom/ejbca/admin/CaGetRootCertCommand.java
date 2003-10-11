b27979910df8c02b7cdcb15e63c575d856ccff40
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2003-10-11 14:38:41 +0000

    getting in DER should really do that
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@1211 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/se/anatom/ejbca/admin/CaGetRootCertCommand.java
+++ src/java/se/anatom/ejbca/admin/CaGetRootCertCommand.java
@@ -9,7 +9,7 @@ import se.anatom.ejbca.util.CertTools;
 /**
  * Export root CA certificate.
  *
- * @version $Id: CaGetRootCertCommand.java,v 1.6 2003-09-07 09:50:22 anatom Exp $
+ * @version $Id: CaGetRootCertCommand.java,v 1.7 2003-10-11 14:38:41 anatom Exp $
  */
 public class CaGetRootCertCommand extends BaseCaAdminCommand {
     /**
@@ -30,7 +30,7 @@ public class CaGetRootCertCommand extends BaseCaAdminCommand {
     public void execute() throws IllegalAdminCommandException, ErrorAdminCommandException {
         if (args.length < 3) {
             String msg = "Save root CA certificate (PEM- or DER-format) to file.\n";
-            msg += "Usage: CA rootcert <caname> <filename> <-der>";
+            msg += "Usage: CA getrootcert <caname> <filename> <-der>";
             throw new IllegalAdminCommandException(msg);
         }
 
@@ -39,7 +39,7 @@ public class CaGetRootCertCommand extends BaseCaAdminCommand {
         boolean pem = true;
         if (args.length > 3) {
             if (("-cer").equals(args[3])) {
-                pem = true;
+                pem = false;
             }
         }
         
