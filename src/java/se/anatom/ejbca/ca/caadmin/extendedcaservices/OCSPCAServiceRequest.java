37ce900fdbe562f01fbb5fa56353f507cd7d54a2
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2003-12-27 10:56:07 +0000

    comment
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@1330 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/se/anatom/ejbca/ca/caadmin/extendedcaservices/OCSPCAServiceRequest.java
+++ src/java/se/anatom/ejbca/ca/caadmin/extendedcaservices/OCSPCAServiceRequest.java
@@ -7,11 +7,14 @@ import java.io.Serializable;
 /**
  * Class used when requesting OCSP related services from a CA.  
  *
- * @version $Id: OCSPCAServiceRequest.java,v 1.1 2003-11-02 15:51:37 herrvendil Exp $
+ * @version $Id: OCSPCAServiceRequest.java,v 1.2 2003-12-27 10:56:07 anatom Exp $
  */
 public class OCSPCAServiceRequest extends ExtendedCAServiceRequest implements Serializable {    
     
-                   
+    /** Cunstructor for OCSPCAServiceRequest
+     * 
+     * @param requesttype not used, 0
+     */                   
     public OCSPCAServiceRequest(int requesttype){       
     }    
             
