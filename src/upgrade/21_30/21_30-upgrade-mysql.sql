7c3a0624fa4764cb1d71b0fb9cd65bb87528e7cd
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2004-01-13 15:41:12 +0000

    *** empty log message ***
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@1411 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ src/upgrade/21_30/21_30-upgrade-mysql.sql
@@ -0,0 +1,69 @@
+
+ALTER TABLE accessrulesdata
+    ADD rule int(11) NOT NULL DEFAULT '0' AFTER accessRule,
+    ADD isRecursive tinyint(4) NOT NULL DEFAULT '0' AFTER rule,
+    DROP resource,
+    MODIFY accessRule varchar(250) binary NULL DEFAULT NULL,
+    MODIFY `AdminGroupData_accessRules` int(11) NULL DEFAULT NULL;
+#
+#  Fieldformats of
+#    accessrulesdata.accessRule changed from longblob NULL DEFAULT NULL to varchar(250) binary NULL DEFAULT NULL.
+#    accessrulesdata.AdminGroupData_accessRules changed from varchar(250) binary NULL DEFAULT NULL to int(11) NULL DEFAULT NULL.
+#  Possibly data modifications needed!
+#
+
+ALTER TABLE adminentitydata
+    MODIFY matchWith int(11) NOT NULL DEFAULT '0',
+    MODIFY matchType int(11) NOT NULL DEFAULT '0',
+    MODIFY `AdminGroupData_adminEntities` int(11) NULL DEFAULT NULL;
+#
+#  Fieldformats of
+#    adminentitydata.matchWith changed from int(11) NULL DEFAULT NULL to int(11) NOT NULL DEFAULT '0'.
+#    adminentitydata.matchType changed from int(11) NULL DEFAULT NULL to int(11) NOT NULL DEFAULT '0'.
+#    adminentitydata.AdminGroupData_adminEntities changed from varchar(250) binary NULL DEFAULT NULL to int(11) NULL DEFAULT NULL.
+#  Possibly data modifications needed!
+#
+
+ALTER TABLE admingroupdata
+    ADD pK int(11) NOT NULL DEFAULT '0' FIRST,
+    ADD cAId int(11) NOT NULL DEFAULT '0' AFTER adminGroupName,
+    MODIFY adminGroupName varchar(250) binary NULL DEFAULT NULL,
+    DROP PRIMARY KEY,
+    ADD PRIMARY KEY (pK);
+#
+#  Fieldformat of
+#    admingroupdata.adminGroupName changed from varchar(250) binary NOT NULL DEFAULT '' to varchar(250) binary NULL DEFAULT NULL.
+#  Possibly data modifications needed!
+#
+
+DROP TABLE availableaccessrulesdata;
+
+ALTER TABLE hardtokendata
+    ADD significantIssuerDN varchar(250) binary NULL DEFAULT NULL AFTER tokenType;
+
+ALTER TABLE hardtokenissuerdata
+    ADD adminGroupId int(11) NOT NULL DEFAULT '0' AFTER alias,
+    DROP certificateSN,
+    DROP certIssuerDN;
+
+ALTER TABLE keyrecoverydata
+    DROP pK,
+    MODIFY certSN varchar(250) binary NOT NULL DEFAULT '',
+    MODIFY issuerDN varchar(250) binary NOT NULL DEFAULT '',
+    DROP PRIMARY KEY,
+    ADD PRIMARY KEY (certSN, issuerDN);
+#
+#  Fieldformats of
+#    keyrecoverydata.certSN changed from varchar(250) binary NULL DEFAULT NULL to varchar(250) binary NOT NULL DEFAULT ''.
+#    keyrecoverydata.issuerDN changed from varchar(250) binary NULL DEFAULT NULL to varchar(250) binary NOT NULL DEFAULT ''.
+#  Possibly data modifications needed!
+#
+
+ALTER TABLE logentrydata
+    ADD caId int(11) NOT NULL DEFAULT '0' AFTER adminData;
+
+ALTER TABLE userdata
+    ADD cAId int(11) NOT NULL DEFAULT '0' AFTER subjectDN,
+    ADD extendedInformationData longblob NULL DEFAULT NULL AFTER keyStorePassword;
+
+
