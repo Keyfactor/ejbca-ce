ec2c259a2a06e8d073bfbdaac7be559d5f0de041
herrvendil <USER@primekey.com>
2004-03-17 05:25:00 +0000

    Fixed better error checking
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@1541 37aeb1de-b14b-0410-9a0e-82b28170d228

--- install.cmd
+++ install.cmd
@@ -7,11 +7,7 @@ rem Check for proper settings of environment variables
 if "%JBOSS_HOME%" == ""  goto jbosserror
 if "%JAVA_HOME%" == ""  goto javaerror
 
-
-rem check for java in path
-
-java -version
-if NOT ERRORLEVEL 0 goto javanotfound
+set PATH=%PATH%;%JAVA_HOME%\bin
 
 
 set CP=.;.\admin.jar;.\lib\ldap.jar;%JBOSS_HOME%\client\jnp-client.jar;%JBOSS_HOME%\client\jboss-j2ee.jar;%JBOSS_HOME%\client\jbossall-client.jar;%JBOSS_HOME%\client\jboss-client.jar;%JBOSS_HOME%\client\jbosssx-client.jar;%JBOSS_HOME%\client\jboss-common-client.jar
@@ -20,18 +16,14 @@ java -cp %CP% se.anatom.ejbca.admin.Install install windows en ejbca jboss
 
 if NOT ERRORLEVEL 0 goto end
 
-%JAVA_HOME%\bin\keytool -alias EJBCA-CA -delete -keystore %JAVA_HOME%\jre\lib\security\cacerts -storepass %JAVACACERTPASSWD%
-%JAVA_HOME%\bin\keytool -alias EJBCA-CA -import -trustcacerts -file tmp\rootca.der -keystore %JAVA_HOME%\jre\lib\security\cacerts -storepass %JAVACACERTPASSWD%
+keytool -alias EJBCA-CA -delete -keystore %JAVA_HOME%\jre\lib\security\cacerts -storepass %JAVACACERTPASSWD%
+keytool -alias EJBCA-CA -import -trustcacerts -file tmp\rootca.der -keystore %JAVA_HOME%\jre\lib\security\cacerts -storepass %JAVACACERTPASSWD%
 
 del tmp\rootca.der
 
 java -cp %CP% se.anatom.ejbca.admin.Install displayendmessage windows en ejbca jboss
 
 goto end
-
-
-:javanotfound
-echo cannot find java jvm. Please add it to your path.
 
 :jbosserror
 echo JBOSS_HOME must be set in order to install sucessfully.
