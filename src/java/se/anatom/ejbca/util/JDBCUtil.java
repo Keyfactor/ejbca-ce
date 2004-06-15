558a6f6db16868f818f4a5f8af44e476c8240762
sbailliez <USER@primekey.com>
2004-06-15 16:33:55 +0000

    Add some more utility methods
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@1770 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/se/anatom/ejbca/util/JDBCUtil.java
+++ src/java/se/anatom/ejbca/util/JDBCUtil.java
@@ -10,47 +10,116 @@
  *  See terms of license at gnu.org.                                     *
  *                                                                       *
  *************************************************************************/
- 
+
 package se.anatom.ejbca.util;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+
+import org.apache.log4j.Logger;
 
 
 /**
- * @version $Id: JDBCUtil.java,v 1.3 2004-04-16 07:38:59 anatom Exp $
+ * Utility methods for jdbc calls.
+ * <p/>
+ * close() methods do not close quietly but log errors (exceptions) as warning. Closing
+ * quietly could mask potential errors that could have no impact depending on GC
+ * stress conditions, or simply exhaust database resources, so it is better to have some
+ * warning than nothing at all.
+ *
+ * @version $Id: JDBCUtil.java,v 1.4 2004-06-15 16:33:55 sbailliez Exp $
  */
 public class JDBCUtil {
 
+    private static final Logger LOG = Logger.getLogger(JDBCUtil.class);
+
+    /**
+     * Return the requested datasource name. It assumes that the dsName returns a reference (java.lang.String)
+     * to the real datasource instance, otherwise you're likely to get a ClassCastException.
+     *
+     * @param dsName the name of the requested datasource
+     * @return the requested datasource
+     * @throws ServiceLocatorException if there is an error locating the datasource
+     */
+    public static DataSource getDataSource(String dsName)
+            throws ServiceLocatorException {
+        String dataSourceName = ServiceLocator.getInstance().getString(dsName);
+        return (DataSource) ServiceLocator.getInstance().getDataSource(dataSourceName);
+    }
+
+    /**
+     * return a requested database connection
+     *
+     * @param dsName the name of the datasource
+     * @return a database connection
+     * @throws ServiceLocatorException if it cannot get the datasource connection
+     * @see #getDataSource(java.lang.String)
+     */
+    public static Connection getDBConnection(String dsName) throws ServiceLocatorException {
+        try {
+            return getDataSource(dsName).getConnection();
+        } catch (SQLException e) {
+            throw new ServiceLocatorException("Error while getting db connection", e);
+        }
+    }
+
+    /**
+     * Close a db connection logging closing errors as warning.
+     *
+     * @param con the connection to close (can be null)
+     */
     public static void close(Connection con) {
-        try {
-            if (con != null) {
-                con.close(); 
+        if (con != null)
+            try {
+                con.close();
+            } catch (SQLException e) {
+                LOG.warn("Could not close connection", e);
             }
-        } catch (Exception ex) {
-            // ignore
-        }
     }
 
-    public static void close(PreparedStatement ps) {
-        try {
-            if (ps != null) {
-                ps.close(); 
-            }
-        } catch (Exception ex) {
-            // ignore
-        }
-    }
-
+    /**
+     * Close a resultset logging closing errors as warning.
+     *
+     * @param rs the resultset to close (can be null)
+     */
     public static void close(ResultSet rs) {
-        try {
-            if (rs != null) {
-                rs.close(); 
+        if (rs != null)
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                LOG.warn("Could not close ResultSet", e);
             }
-        } catch (Exception ex) {
-            // ignore
-        }
     }
-    
+
+    /**
+     * Close a prepared statement logging closing errors as warning.
+     *
+     * @param ps the prepared statement to close (can be null)
+     */
+    public static void close(PreparedStatement ps) {
+        if (ps != null)
+            try {
+                ps.close();
+            } catch (SQLException e) {
+                LOG.warn("Could not close PreparedStatement", e);
+            }
+    }
+
+    /**
+     * Close a connection, prepared statement and result set, logging potential
+     * closing errors as warning.
+     *
+     * @param con the connection to close (can be null)
+     * @param ps  the prepared statement to close (can be null)
+     * @param rs  the result set to close (can be null)
+     */
+    public static void close(Connection con, PreparedStatement ps, ResultSet rs) {
+        close(con);
+        close(ps);
+        close(rs);
+    }
+
 }
