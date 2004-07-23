25ded41a3609e1822a3bd12c8c3ff1444693d6b5
sbailliez <USER@primekey.com>
2004-07-23 12:28:10 +0000

    XDoclet fixes. I'm getting rid of the base property bean which is only used here and gives me too much trouble with xdoclet
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@1859 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/se/anatom/ejbca/hardtoken/HardTokenPropertyEntityBean.java
+++ src/java/se/anatom/ejbca/hardtoken/HardTokenPropertyEntityBean.java
@@ -13,8 +13,9 @@
 
 package se.anatom.ejbca.hardtoken;
 
-import se.anatom.ejbca.BasePropertyEntityBean;
+import se.anatom.ejbca.BaseEntityBean;
 
+import javax.ejb.CreateException;
 
 
 /**
@@ -40,22 +41,20 @@ import se.anatom.ejbca.BasePropertyEntityBean;
  * @ejb.permission role-name="InternalUser"
  *
  * @ejb.pk
- *   class="se.anatom.ejbca.PropertyEntityPK"
+ *   class="se.anatom.ejbca.hardtoken.HardTokenPropertyPK"
  *   extends="java.lang.Object"
  *
  * @ejb.home
- *   generate="local"
  *   local-extends="javax.ejb.EJBLocalHome"
- *   local-class="se.anatom.ejbca.BasePropertyDataLocalHome"
+ *   local-class="se.anatom.ejbca.hardtoken.HardTokenPropertyLocalHome"
  *
  * @ejb.interface
- *   generate="local"
  *   local-extends="javax.ejb.EJBLocalObject"
- *   local-class="se.anatom.ejbca.BasePropertyDataLocal"
+ *   local-class="se.anatom.ejbca.hardtoken.HardTokenPropertyLocal"
  *
  * @ejb.finder
  *   description="findByProperty"
- *   signature="se.anatom.ejbca.BasePropertyDataLocal findByProperty(java.lang.String id, java.lang.String property)"
+ *   signature="se.anatom.ejbca.hardtoken.HardTokenPropertyLocal findByProperty(java.lang.String id, java.lang.String property)"
  *   query="SELECT DISTINCT OBJECT(a) from HardTokenPropertyEntityBean a WHERE a.id =?1 AND a.property=?2"
  *
  * @ejb.finder
@@ -63,8 +62,60 @@ import se.anatom.ejbca.BasePropertyEntityBean;
  *   signature="Collection findIdsByPropertyAndValue(java.lang.String property, java.lang.String value)"
  *   query="SELECT DISTINCT OBJECT(a) from HardTokenPropertyEntityBean a WHERE a.property =?1 AND a.value=?2"
  */
-public abstract class HardTokenPropertyEntityBean extends BasePropertyEntityBean {
+public abstract class HardTokenPropertyEntityBean extends BaseEntityBean {
 
   public static final String PROPERTY_COPYOF = "copyof=";
 
+
+    /**
+     * @ejb.persistence
+     * @ejb.pk-field
+     * @ejb.interface-method
+     */
+	public abstract String getId();
+
+    /**
+     * @ejb.persistence
+     */
+	public abstract void setId(String id);
+
+    /**
+     * @ejb.persistence
+     * @ejb.pk-field
+     * @ejb.interface-method
+     */
+	public abstract String getProperty();
+
+    /**
+     * @ejb.persistence
+     */
+	public abstract void setProperty(String property);
+
+    /**
+     * @ejb.persistence
+     * @ejb.interface-method
+     */
+	public abstract String getValue();
+
+    /**
+     * @ejb.persistence
+     * @ejb.interface-method
+     */
+	public abstract void setValue(String value);
+
+	/**
+	 * Entity Bean holding data of a raadmin profile.
+     * @ejb.create-method
+     */
+	public HardTokenPropertyPK ejbCreate(String id, String property, String value)
+	       throws CreateException {
+		setId(id);
+		setProperty(property);
+		setValue(value);
+        return null;
+	}
+
+	public void ejbPostCreate(String id, String property, String value) {
+		// Do nothing. Required.
+	}
 }
