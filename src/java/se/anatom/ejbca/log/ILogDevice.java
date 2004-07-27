fe79c462bc490ad63ebcb8a6280ecf4ec4562fb4
sbailliez <USER@primekey.com>
2004-07-27 13:56:27 +0000

    Cleanup
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@1882 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/se/anatom/ejbca/log/ILogDevice.java
+++ src/java/se/anatom/ejbca/log/ILogDevice.java
@@ -15,17 +15,22 @@ package se.anatom.ejbca.log;
 
 import java.security.cert.X509Certificate;
 import java.util.Date;
+import java.io.Serializable;
 
 /**
  * Interface used by EJBCA external log devices such as Log4j.
- *
- *
  */
-public interface ILogDevice extends java.io.Serializable {
+public interface ILogDevice extends Serializable {
 
     /**
      * Function used by EJBCA to log information.
      *
+     * @see #log(Admin, int, int, Date, String, X509Certificate, int, String, Exception)
+     */
+    public void log(Admin admininfo, int caid, int module, Date time, String username, X509Certificate certificate, int event, String comment);
+
+    /**
+     * Log information.
      * @param admininfo contains information about the administrator performing the event.
      * @param caid the id of the catch (connected to the event.
      * @param module indicates the module using the bean.
@@ -34,14 +39,7 @@ public interface ILogDevice extends java.io.Serializable {
      * @param certificate the certificate involved in the event or null if no certificate is involved.
      * @param event id of the event, should be one of the se.anatom.ejbca.log.LogEntry.EVENT_ constants.
      * @param comment comment of the event.
-     */
-    public void log(Admin admininfo, int caid, int module, Date time, String username, X509Certificate certificate, int event, String comment);
-
-    /**
-     * Overloaded function that also logs an exception
-     * See function above for more documentation.
-     *
-     * @param exception the exception that has occured
+     * @param exception the exception that has occurred (can be null)
      */
     public void log(Admin admininfo, int caid, int module, Date time, String username, X509Certificate certificate, int event, String comment, Exception exception);
 
