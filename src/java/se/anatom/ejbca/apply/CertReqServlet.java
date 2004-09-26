336a9238105ecd3af12bbad15a15abcbc03445d9
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2004-09-26 08:42:18 +0000

    Fixed error message when enrolling with un-allowed keysize from browser.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@2030 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/se/anatom/ejbca/apply/CertReqServlet.java
+++ src/java/se/anatom/ejbca/apply/CertReqServlet.java
@@ -79,7 +79,7 @@ import se.anatom.ejbca.util.KeyTools;
  * </p>
  *
  * @author Original code by Lars Silv?n
- * @version $Id: CertReqServlet.java,v 1.47 2004-08-08 11:11:11 anatom Exp $
+ * @version $Id: CertReqServlet.java,v 1.48 2004-09-26 08:40:56 anatom Exp $
  */
 public class CertReqServlet extends HttpServlet {
     private static Logger log = Logger.getLogger(CertReqServlet.class);
@@ -240,12 +240,10 @@ public class CertReqServlet extends HttpServlet {
             debug.printMessage(
                 "To generate a certificate a valid username and password must be entered.");
             debug.printDebugInfo();
-
             return;
         } catch (AuthStatusException ase) {
             log.debug("Wrong user status!");
             debug.printMessage("Wrong user status!");
-
             if (usekeyrecovery) {
                 debug.printMessage(
                     "To generate a certificate for a user the user must have status new, failed or inprocess.");
@@ -253,9 +251,7 @@ public class CertReqServlet extends HttpServlet {
                 debug.printMessage(
                     "To generate a certificate for a user the user must have status new, failed or inprocess.");
             }
-
             debug.printDebugInfo();
-
             return;
         } catch (AuthLoginException ale) {
             log.debug("Wrong password for user!");
@@ -263,41 +259,40 @@ public class CertReqServlet extends HttpServlet {
             debug.printMessage(
                 "To generate a certificate a valid username and password must be entered.");
             debug.printDebugInfo();
-
             return;
         } catch (SignRequestException re) {
             log.debug("Invalid request!");
             debug.printMessage("Invalid request!");
             debug.printMessage("Please supply a correct request.");
             debug.printDebugInfo();
-
             return;
         } catch (SignRequestSignatureException se) {
             log.debug("Invalid signature on certificate request!");
             debug.printMessage("Invalid signature on certificate request!");
             debug.printMessage("Please supply a correctly signed request.");
             debug.printDebugInfo();
-
             return;
         } catch (java.lang.ArrayIndexOutOfBoundsException ae) {
             log.debug("Empty or invalid request received.");
             debug.printMessage("Empty or invalid request!");
             debug.printMessage("Please supply a correct request.");
             debug.printDebugInfo();
-
+            return;
+        } catch (se.anatom.ejbca.ca.exception.IllegalKeyException e) {
+            log.debug("Illegal Key received: "+e.getMessage());
+            debug.printMessage("Invalid Key in request: "+e.getMessage());
+            debug.printMessage("Please supply a correct request.");
+            debug.printDebugInfo();
             return;
         } catch (Exception e) {
             log.debug(e);
             debug.print("<h3>parameter name and values: </h3>");
-
             Enumeration paramNames = request.getParameterNames();
-
             while (paramNames.hasMoreElements()) {
                 String name = paramNames.nextElement().toString();
                 String parameter = request.getParameter(name);
                 debug.print("<h4>" + name + ":</h4>" + parameter + "<br>");
             }
-
             debug.takeCareOfException(e);
             debug.printDebugInfo();
         }
