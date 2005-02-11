718369474ba07b3117ae939940d41949848b00ae
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2005-02-11 13:32:16 +0000

    removed code duplication
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@2188 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/se/anatom/ejbca/admin/BaseCommand.java
+++ src/java/se/anatom/ejbca/admin/BaseCommand.java
@@ -15,6 +15,8 @@ package se.anatom.ejbca.admin;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
+import java.rmi.RemoteException;
 import java.security.KeyFactory;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
@@ -26,12 +28,22 @@ import java.security.cert.X509Certificate;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 
+import javax.ejb.CreateException;
 import javax.naming.*;
 
 import org.apache.log4j.Logger;
 
 import se.anatom.ejbca.ca.caadmin.ICAAdminSessionHome;
+import se.anatom.ejbca.ca.caadmin.ICAAdminSessionRemote;
+import se.anatom.ejbca.ca.publisher.IPublisherSessionHome;
+import se.anatom.ejbca.ca.publisher.IPublisherSessionRemote;
+import se.anatom.ejbca.ca.store.ICertificateStoreSessionHome;
+import se.anatom.ejbca.ca.store.ICertificateStoreSessionRemote;
 import se.anatom.ejbca.log.Admin;
+import se.anatom.ejbca.ra.IUserAdminSessionHome;
+import se.anatom.ejbca.ra.IUserAdminSessionRemote;
+import se.anatom.ejbca.ra.raadmin.IRaAdminSessionHome;
+import se.anatom.ejbca.ra.raadmin.IRaAdminSessionRemote;
 import se.anatom.ejbca.util.Base64;
 import se.anatom.ejbca.util.CertTools;
 import se.anatom.ejbca.util.KeyTools;
@@ -40,46 +52,180 @@ import se.anatom.ejbca.util.InitialContextBuilder;
 /**
  * Base for Commands, contains useful functions
  *
- * @version $Id: BaseCommand.java,v 1.7 2005-02-11 13:12:18 anatom Exp $
+ * @version $Id: BaseCommand.java,v 1.8 2005-02-11 13:32:16 anatom Exp $
  */
 public abstract class BaseCommand {
     /** Log4j instance for Base */
-    private static Logger baseLog = Logger.getLogger(BaseCommand.class);
-
+    private static Logger baseLog = Logger.getLogger(BaseAdminCommand.class);
     /** Log4j instance for actual class */
     private Logger log;
 
-    /** Cached initial context to save JNDI lookups */
+    /** UserAdminSession handle, not static since different object should go to different session beans concurrently */
+    private IUserAdminSessionRemote cacheAdmin = null;
+    /** Handle to AdminSessionHome */
+    private static IUserAdminSessionHome cacheHome = null;
+    /** RaAdminSession handle, not static since different object should go to different session beans concurrently */
+    private IRaAdminSessionRemote raadminsession = null;
+    /** Handle to RaAdminSessionHome */
+    private static IRaAdminSessionHome raadminHomesession = null;    
+    /** CAAdminSession handle, not static since different object should go to different session beans concurrently */
+    private ICAAdminSessionRemote caadminsession = null;
+    /** Handle to CertificateStoreSessionRemote, not static... */
+    private ICertificateStoreSessionRemote certstoresession = null;
+    /** Handle to PublisherSessionRemote, not static... */
+    private IPublisherSessionRemote publishersession = null;
+    
     protected Admin administrator = null;
+    
+    /** Where print output of commands */
+    private PrintStream outStream = System.out;
+
+    /** holder of argument array */
+    protected String[] args = null;
 
     /**
-     * Creates a new instance of the class
+     * Creates a new default instance of the class
      *
      */
     public BaseCommand() {
-        log = Logger.getLogger(this.getClass());
-        administrator = new Admin(Admin.TYPE_CACOMMANDLINE_USER);
+        init(null, Admin.TYPE_CACOMMANDLINE_USER, System.out);
     }
+
+    /**
+     * Initialize a new instance of BaseCommand
+     *
+     * @param args command line arguments
+     * @param adminType type of admin Admin.TYPE_RACOMMANDLINE_USER, or Admin.TYPE_CACOMMANDLINE_USER
+     * @param outStream stream where commands write its output
+     */
+    protected void init(String[] args, int adminType, PrintStream outStream) {
+        log = Logger.getLogger(this.getClass());
+        this.args = args;
+        if( outStream != null ) {
+          this.outStream = outStream;
+        }
+        administrator = new Admin(adminType);
+    }
+
+    /**
+     * Gets InitialContext
+     *
+     * @return InitialContext
+     */
+    protected InitialContext getInitialContext() throws NamingException {
+        baseLog.debug(">getInitialContext()");
+
+        try {
+            InitialContext cacheCtx = InitialContextBuilder.getInstance().getInitialContext();
+            baseLog.debug("<getInitialContext()");
+            return cacheCtx;
+        } catch (NamingException e) {
+            baseLog.error("Can't get InitialContext", e);
+            throw e;
+        }
+    } // getInitialContext
+
+    /** Gets CA admin session
+     *@return ICAAdminSessionRemote
+     */
+    protected ICAAdminSessionRemote getCAAdminSessionRemote() throws Exception{
+        if(caadminsession == null){
+          Context ctx = getInitialContext();
+          ICAAdminSessionHome home = (ICAAdminSessionHome) javax.rmi.PortableRemoteObject.narrow(ctx.lookup("CAAdminSession"), ICAAdminSessionHome.class );            
+          caadminsession = home.create();          
+        } 
+        return caadminsession;
+     } // getCAAdminSessionRemote
+
+    /** Gets certificate store session
+     *@return ICertificateStoreSessionRemote
+     */
+    protected ICertificateStoreSessionRemote getCertificateStoreSession() throws Exception{
+        if(certstoresession == null){
+          Context ctx = getInitialContext();
+          ICertificateStoreSessionHome home = (ICertificateStoreSessionHome) javax.rmi.PortableRemoteObject.narrow(ctx.lookup("CertificateStoreSession"), ICertificateStoreSessionHome.class );            
+          certstoresession = home.create();          
+        } 
+        return certstoresession;
+     } // getCertificateStoreSession
+    
+    /** Gets publisher session
+     *@return ICertificateStoreSessionRemote
+     */
+    protected IPublisherSessionRemote getPublisherSession() throws Exception{
+        if(publishersession == null){
+          Context ctx = getInitialContext();
+          IPublisherSessionHome home = (IPublisherSessionHome) javax.rmi.PortableRemoteObject.narrow(ctx.lookup("PublisherSession"), IPublisherSessionHome.class );            
+          publishersession = home.create();          
+        } 
+        return publishersession;
+     } // getPublisherSession
+    /** Gets user admin session
+     *@return InitialContext
+     */
+    protected IUserAdminSessionRemote getAdminSession()
+        throws CreateException, NamingException, RemoteException {
+        debug(">getAdminSession()");
+        try {
+            if (cacheAdmin == null) {
+                if (cacheHome == null) {
+                    Context jndiContext = getInitialContext();
+                    Object obj1 = jndiContext.lookup("UserAdminSession");
+                    cacheHome = (IUserAdminSessionHome) javax.rmi.PortableRemoteObject.narrow(obj1,
+                            IUserAdminSessionHome.class);
+                }
+                cacheAdmin = cacheHome.create();
+            }
+            debug("<getAdminSession()");
+            return cacheAdmin;
+        } catch (NamingException e) {
+            error("Can't get Admin session", e);
+            throw e;
+        }
+    } // getAdminSession
+    
+    /** Gets ra admin session
+     *@return InitialContext
+     */
+    protected IRaAdminSessionRemote getRaAdminSession() throws CreateException, NamingException, RemoteException {
+        debug(">getRaAdminSession()");
+        administrator = new Admin(Admin.TYPE_RACOMMANDLINE_USER);
+        try {
+            if( raadminsession == null ) {
+                if (raadminHomesession == null) {
+                    Context jndiContext = getInitialContext();
+                    Object obj1 = jndiContext.lookup("RaAdminSession");
+                    raadminHomesession = (IRaAdminSessionHome) javax.rmi.PortableRemoteObject.narrow(obj1, IRaAdminSessionHome.class);
+                }
+                raadminsession = raadminHomesession.create();
+            }
+            debug("<getRaAdminSession()");
+            return  raadminsession;
+        } catch (NamingException e ) {
+            error("Can't get RaAdmin session", e);
+            throw e;
+        }
+    } // getRaAdminSession    
 
     /**
      * Method checking if the application server is running.
      * 
      * @return true if app server is running.
      */
-	protected boolean appServerRunning() {
-		// Check that the application server is running by getting a home interface for user admin session
-		try {
-	        Context ctx = getInitialContext();
-			ICAAdminSessionHome home = (ICAAdminSessionHome) javax.rmi.PortableRemoteObject.narrow(ctx.lookup("CAAdminSession"),ICAAdminSessionHome.class);
+    protected boolean appServerRunning() {
+        // Check that the application server is running by getting a home interface for user admin session
+        try {
+            Context ctx = getInitialContext();
+            ICAAdminSessionHome home = (ICAAdminSessionHome) javax.rmi.PortableRemoteObject.narrow(ctx.lookup("CAAdminSession"),ICAAdminSessionHome.class);
             home.getClass(); // avoid PMD warning :)
-			return true;
-		} catch (Exception e) {
-			error("Appserver not running: ", e);
+            return true;
+        } catch (Exception e) {
+            error("Appserver not running: ", e);
             return false;
         }
-	}
+    }
 
-    /** Priavet key with length 1024 bits */
+    /** Private key with length 1024 bits */
     static byte[] keys1024bit = Base64.decode(
     ("MIICeAIBADANBgkqhkiG9w0BAQEFAASCAmIwggJeAgEAAoGBAKA5rNhYbPuVcArT"
     +"mkthfrW2tX1Z7SkCD01sDYrkiwOcodFmS1cSyz8eHM51iwHA7CW0WFvfUjomBT5y"
@@ -109,6 +255,7 @@ public abstract class BaseCommand {
     +"9w0BAQUFAAOBgQA1cB6wWzC2rUKBjFAzfkLvDUS3vEMy7ntYMqqQd6+5s1LHCoPw"
     +"eaR42kMWCxAbdSRgv5ATM0JU3Q9jWbLO54FkJDzq+vw2TaX+Y5T+UL1V0o4TPKxp"
     +"nKuay+xl5aoUcVEs3h3uJDjcpgMAtyusMEyv4d+RFYvWJWFzRTKDueyanw==").getBytes());
+
     /**
      * Method checking if strong crypto is installed (extra package from java.sun.com)
      * 
@@ -127,23 +274,6 @@ public abstract class BaseCommand {
         // If we didn't throw an exception, we were succesful
         return true;
     }
-    
-    /**
-     * Gets InitialContext
-     *
-     * @return InitialContext
-     */
-    protected InitialContext getInitialContext() throws NamingException {
-        baseLog.debug(">getInitialContext()");
-        try {
-            InitialContext cacheCtx = InitialContextBuilder.getInstance().getInitialContext();
-            baseLog.debug("<getInitialContext()");
-            return cacheCtx;
-        } catch (NamingException e) {
-            baseLog.error("Can't get InitialContext", e);
-            throw e;
-        }
-    } // getInitialContext
 
     /**
      * Logs a message with priority DEBUG
@@ -201,4 +331,26 @@ public abstract class BaseCommand {
     public void error(String msg, Throwable t) {
         log.error(msg, t);
     }
+
+
+    /**
+     * Return the PrintStream used to print output of commands
+     *
+     */
+    public PrintStream getOutputStream() {
+        return outStream;
+    }
+
+    /**
+     * Set the PrintStream used to print output of commands
+     *
+     * @param outStream stream where commands write its output
+     */
+    public void setOutputStream(PrintStream outStream) {
+    if( outStream == null )
+        this.outStream = System.out;
+    else
+        this.outStream = outStream;
+    }   
+
 } //BaseCommand
