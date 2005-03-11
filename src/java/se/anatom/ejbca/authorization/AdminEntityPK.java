83ca47b71012b761562824a17861cf6feddc6566
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2005-03-11 11:25:00 +0000

    docs
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@2251 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/se/anatom/ejbca/authorization/AdminEntityPK.java
+++ src/java/se/anatom/ejbca/authorization/AdminEntityPK.java
@@ -15,7 +15,14 @@
 package se.anatom.ejbca.authorization;
 
 /**
- * @version $Id: AdminEntityPK.java,v 1.6 2005-03-10 13:36:06 anatom Exp $
+ * The current pk in AdminEntityData and AccessRulesData is a mix of integer pk and 
+ * constraints and actually works fine. 
+ * It's used like a primitive int primary key in the db, but embeds logic for 
+ * enforcing constraints, which would otherwise have to be programatically added to the beans.
+ * If needed it can easily be replaced with an int pk and programatic logic to handle 
+ * constraints. From the database view the pk is just an int.
+ * 
+ * @version $Id: AdminEntityPK.java,v 1.7 2005-03-11 11:25:00 anatom Exp $
  */
 public final class AdminEntityPK implements java.io.Serializable {
 
