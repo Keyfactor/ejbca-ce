46e9a3c83702c1a6a367314da054b0413cad89ca
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2005-05-17 13:52:00 +0000

    rollback of latest change
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@2407 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/se/anatom/ejbca/ca/caadmin/X509CA.java
+++ src/java/se/anatom/ejbca/ca/caadmin/X509CA.java
@@ -105,7 +105,7 @@ import se.anatom.ejbca.util.StringTools;
  * X509CA is a implementation of a CA and holds data specific for Certificate and CRL generation 
  * according to the X509 standard. 
  *
- * @version $Id: X509CA.java,v 1.39 2005-05-17 11:04:40 herrvendil Exp $
+ * @version $Id: X509CA.java,v 1.40 2005-05-17 13:52:00 anatom Exp $
  */
 public class X509CA extends CA implements Serializable {
 
@@ -226,7 +226,7 @@ public class X509CA extends CA implements Serializable {
             CMSSignedDataGenerator gen = new CMSSignedDataGenerator();
             gen.addSigner(getCAToken().getPrivateKey(SecConst.CAKEYPURPOSE_CERTSIGN), (X509Certificate)getCACertificate(), CMSSignedDataGenerator.DIGEST_SHA1);
             gen.addCertificatesAndCRLs(certs);
-            CMSSignedData s = gen.generate(msg, true, "BC");
+            CMSSignedData s = gen.generate(msg, true, getCAToken().getProvider());
             return s.getEncoded();
         } catch (CATokenOfflineException e) {
         	this.setStatus(SecConst.CA_OFFLINE);
