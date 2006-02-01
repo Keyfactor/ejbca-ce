777f702aac4cee00974000af8ff16d8994e66304
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2006-02-01 14:21:48 +0000

    ECA-212: upgrade from EJBCA 3.1.x to 3.2
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@2826 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/upgrade/31_32/31_32-upgrade-oracle.sql
+++ src/upgrade/31_32/31_32-upgrade-oracle.sql
@@ -1,3 +1,4 @@
 
 # Format of column changed, no information was stored here earlier though
-alter table UserData modify extendedInformationData LONG;
+alter table UserData drop column extendedInformationData;
+alter table UserData add extendedInformationData LONG;
