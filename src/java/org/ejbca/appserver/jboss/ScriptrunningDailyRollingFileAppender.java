810427634c2811eac21604f59177114f4e28b1c2
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2006-08-14 08:50:56 +0000

    ECA-290: possibility to run script for signing when logs are rolled over
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@3348 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/appserver/jboss/ScriptrunningDailyRollingFileAppender.java
+++ src/java/org/ejbca/appserver/jboss/ScriptrunningDailyRollingFileAppender.java
@@ -1,35 +1,16 @@
 package org.ejbca.appserver.jboss;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.math.BigInteger;
-import java.security.MessageDigest;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
-import java.util.Random;
 import java.util.TimeZone;
 
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpStatus;
-import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.log4j.FileAppender;
 import org.apache.log4j.Layout;
 import org.apache.log4j.helpers.LogLog;
 import org.apache.log4j.spi.LoggingEvent;
-import org.bouncycastle.tsp.TSPAlgorithms;
-import org.bouncycastle.tsp.TSPException;
-import org.bouncycastle.tsp.TSPValidationException;
-import org.bouncycastle.tsp.TimeStampRequest;
-import org.bouncycastle.tsp.TimeStampRequestGenerator;
-import org.bouncycastle.tsp.TimeStampResponse;
-import org.bouncycastle.util.encoders.Base64;
-import org.ejbca.util.CertTools;
-import org.ejbca.util.FileTools;
 
 /**
  * A shameless copy of DailyRollingFileAppender from log4j and merged (also shamelessly)
@@ -38,11 +19,21 @@ import org.ejbca.util.FileTools;
  * This was the only way I could find to implement the desired functionality.
  * 
  * @author tomas
- * @version $Id: SigningDailyRollingFileAppender.java,v 1.2 2006-08-14 08:46:16 anatom Exp $
+ * @version $Id: ScriptrunningDailyRollingFileAppender.java,v 1.1 2006-08-14 08:46:19 anatom Exp $
  */
-public class SigningDailyRollingFileAppender extends FileAppender {
+public class ScriptrunningDailyRollingFileAppender extends FileAppender {
 
-	private Thread signerThread;
+	// The code assumes that the following constants are in a increasing
+	// sequence.
+	static final int TOP_OF_TROUBLE=-1;
+	static final int TOP_OF_MINUTE = 0;
+	static final int TOP_OF_HOUR   = 1;
+	static final int HALF_DAY      = 2;
+	static final int TOP_OF_DAY    = 3;
+	static final int TOP_OF_WEEK   = 4;
+	static final int TOP_OF_MONTH  = 5;
+
+	private Thread scriptThread;
 
 	/**
 	     The date pattern. By default, the pattern is set to
@@ -50,12 +41,9 @@ public class SigningDailyRollingFileAppender extends FileAppender {
 	 */
 	private String datePattern = "'.'yyyy-MM-dd";
 
-	/** The method use to create a signature */
-	private String signMethod;
+	/** The script to run after rotating log */
+	private String script;
 
-	/** The URL to a TSA server used to create time stamps for rolled over log files */
-	private String tsaUrl;
-	
 	/**
 	     The log file will be renamed to the value of the
 	     scheduledFilename variable when the next interval is entered. For
@@ -78,7 +66,7 @@ public class SigningDailyRollingFileAppender extends FileAppender {
 
 	RollingCalendar rc = new RollingCalendar();
 
-	int checkPeriod = RollingCalendar.TOP_OF_TROUBLE;
+	int checkPeriod = TOP_OF_TROUBLE;
 
 	// The gmtTimeZone is used only in computeCheckPeriod() method.
 	static final TimeZone gmtTimeZone = TimeZone.getTimeZone("GMT");
@@ -86,7 +74,7 @@ public class SigningDailyRollingFileAppender extends FileAppender {
 
 	/**
 	     The default constructor does nothing. */
-	public SigningDailyRollingFileAppender() {
+	public ScriptrunningDailyRollingFileAppender() {
 	}
 
 	/**
@@ -95,7 +83,7 @@ public class SigningDailyRollingFileAppender extends FileAppender {
 	    become the ouput destination for this appender.
 
 	 */
-	public SigningDailyRollingFileAppender (Layout layout, String filename,
+	public ScriptrunningDailyRollingFileAppender (Layout layout, String filename,
 			String datePattern) throws IOException {
 		super(layout, filename, true);
 		this.datePattern = datePattern;
@@ -125,19 +113,6 @@ public class SigningDailyRollingFileAppender extends FileAppender {
 		return datePattern;
 	}
 	
-	public void setSignMethod(String method) {
-		signMethod = method;
-	}
-	public String getSignMethod() {
-		return signMethod;
-	}
-	public void setTsaUrl(String url) {
-		tsaUrl = url;
-	}
-	public String getTsaUrl() {
-		return tsaUrl;
-	}
-
 	public void activateOptions() {
 		super.activateOptions();
 		if(datePattern != null && fileName != null) {
@@ -152,38 +127,33 @@ public class SigningDailyRollingFileAppender extends FileAppender {
 		} else {
 			LogLog.error("Either File or DatePattern options are not set for appender ["+name+"].");
 		}
-		if (signMethod != null) {
-			if (tsaUrl == null) {
-				LogLog.error("TsaUrl option is not set for appender ["+name+"].");				
-			}
-		} else {
-			LogLog.error("SignMethod option is not set for appender ["+name+"].");			
+		if (script == null) {
+		    LogLog.error("Script option is not set for appender ["+name+"].");				
 		}
-		CertTools.installBCProvider();
 	}
 
 	void printPeriodicity(int type) {
 		switch(type) {
-		case RollingCalendar.TOP_OF_MINUTE:
+		case TOP_OF_MINUTE:
 			LogLog.debug("Appender ["+name+"] to be rolled every minute.");
 			break;
-		case RollingCalendar.TOP_OF_HOUR:
+		case TOP_OF_HOUR:
 			LogLog.debug("Appender ["+name
 					+"] to be rolled on top of every hour.");
 			break;
-		case RollingCalendar.HALF_DAY:
+		case HALF_DAY:
 			LogLog.debug("Appender ["+name
 					+"] to be rolled at midday and midnight.");
 			break;
-		case RollingCalendar.TOP_OF_DAY:
+		case TOP_OF_DAY:
 			LogLog.debug("Appender ["+name
 					+"] to be rolled at midnight.");
 			break;
-		case RollingCalendar.TOP_OF_WEEK:
+		case TOP_OF_WEEK:
 			LogLog.debug("Appender ["+name
 					+"] to be rolled at start of week.");
 			break;
-		case RollingCalendar.TOP_OF_MONTH:
+		case TOP_OF_MONTH:
 			LogLog.debug("Appender ["+name
 					+"] to be rolled at start of every month.");
 			break;
@@ -207,7 +177,7 @@ public class SigningDailyRollingFileAppender extends FileAppender {
 		// set sate to 1970-01-01 00:00:00 GMT
 		Date epoch = new Date(0);
 		if(datePattern != null) {
-			for(int i = RollingCalendar.TOP_OF_MINUTE; i <= RollingCalendar.TOP_OF_MONTH; i++) {
+			for(int i = TOP_OF_MINUTE; i <= TOP_OF_MONTH; i++) {
 				SimpleDateFormat simpleDateFormat = new SimpleDateFormat(datePattern);
 				simpleDateFormat.setTimeZone(gmtTimeZone); // do all date formatting in GMT
 				String r0 = simpleDateFormat.format(epoch);
@@ -220,7 +190,7 @@ public class SigningDailyRollingFileAppender extends FileAppender {
 				}
 			}
 		}
-		return RollingCalendar.TOP_OF_TROUBLE; // Deliberately head for trouble...
+		return TOP_OF_TROUBLE; // Deliberately head for trouble...
 	}
 
 	/**
@@ -266,19 +236,15 @@ public class SigningDailyRollingFileAppender extends FileAppender {
 		catch(IOException e) {
 			errorHandler.error("setFile("+fileName+", false) call failed.");
 		}
-		if (signMethod.equalsIgnoreCase("tsa")) {
-			if (tsaUrl != null) {
-				// Now do the actual signing
+		if ( (script != null) && (script.length() > 0) ) {
+				// Now call the script
 				// Check first if an old instance of the thread is blocking
-				if ( (signerThread != null) && signerThread.isAlive() ) {
+				if ( (scriptThread != null) && scriptThread.isAlive() ) {
 					System.out.println("Stopping old hanging signerthread");
-					signerThread.interrupt();
+                    scriptThread.interrupt();
 				}
-				signerThread = new Thread(new SignerThread(tsaUrl, scheduledFilename, scheduledFilename+".tsp"));
-				signerThread.start();							
-			} else {
-				System.out.println("No TsaUrl set, can not sign logs!");
-			}
+                scriptThread = new Thread(new ScriptThread(script, scheduledFilename));
+                scriptThread.start();							
 		}
 
 		scheduledFilename = datedFilename;
@@ -307,96 +273,45 @@ public class SigningDailyRollingFileAppender extends FileAppender {
 		super.subAppend(event);
 	}
 
+    public String getScript() {
+        return script;
+    }
+
+    public void setScript(String script) {
+        this.script = script;
+    }
+
 }
 
-class SignerThread implements Runnable {
-	private String urlstr;
+class ScriptThread implements Runnable {
+	private String script;
 	private String infile;
-	private String outfile;
-	public SignerThread(String urlstr, String infile, String outfile) {
-		this.urlstr = urlstr;
+	public ScriptThread(String script, String infile) {
+		this.script = script;
 		this.infile = infile;
-		this.outfile = outfile;
 	}
 	public void run() {
 		
 		try {
-			boolean base64 = true;
-			TimeStampRequestGenerator timeStampRequestGenerator = new TimeStampRequestGenerator();
-
-			Random rand = new Random();
-			int nonce = rand.nextInt();
-			byte[] digestBytes = new byte[20];
-			if (infile != null) {
-				digestBytes = FileTools.readFiletoBuffer(infile);
-			}
-			MessageDigest dig = MessageDigest.getInstance(TSPAlgorithms.SHA1, "BC");
-			dig.update(digestBytes);
-			byte[] digest = dig.digest();
-			TimeStampRequest timeStampRequest = timeStampRequestGenerator.generate(TSPAlgorithms.SHA1, digest, BigInteger.valueOf(nonce));
-
-			// create a singular HttpClient object
-			HttpClient client = new HttpClient();
-
-			//establish a connection within 5 seconds
-			client.setConnectionTimeout(5000);
-			//client.getHttpConnectionManager().getParams().setConnectionTimeout(5000);			
-			PostMethod method = new PostMethod(urlstr);
-			method.setParameter("http.socket.timeout", "5000");
-			method.setRequestHeader("Content-Type", "application/timestamp-query");
-			method.setRequestBody(new ByteArrayInputStream(timeStampRequest.getEncoded()));
-			//method.setRequestEntity(new InputStreamRequestEntity(new ByteArrayInputStream(timeStampRequest.getEncoded())));
-			//method.setContentChunked(true);
-			InputStream input = null;
-			ByteArrayOutputStream baos = null;
-			byte[] replyBytes = null;
-			try {
-				client.executeMethod(method);
-				if (method.getStatusCode() == HttpStatus.SC_OK) {
-					replyBytes = method.getResponseBody();
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			} finally {
-				method.releaseConnection();
-				if (input != null) input.close();
-				if (baos != null) baos.close();
-			}   
-
-			if ( (outfile != null) && (replyBytes != null) ) {
-				// Store request
-				byte[] outBytes;
-				if (base64) {
-					outBytes=Base64.encode(replyBytes);
-				} else {
-					outBytes = replyBytes;
-				}
-				FileOutputStream fos = null;
-				try {
-					fos = new FileOutputStream(outfile);
-					fos.write(outBytes);					
-				} finally {
-					if (fos != null) fos.close();
-				}
-			}
-
-			if (replyBytes != null) {
-				try {
-					TimeStampResponse timeStampResponse = new TimeStampResponse(replyBytes);
-					timeStampResponse.validate(timeStampRequest);
-				} catch (TSPValidationException e) {
-					LogLog.error("TimeStampResponse validation failed.", e);
-					e.printStackTrace();
-				} catch (TSPException e) {
-					LogLog.error("TimeStampResponse failed.", e);
-					e.printStackTrace();
-				}			
-			} else {
-				LogLog.error("No reply bytes received, is TSA down?");
-				System.out.println("SigningDailyRollingFileAppender: No reply bytes received, is TSA down?");
-			}			
+		    Runtime rt =Runtime.getRuntime();
+		    String s = script +" "+infile;
+            rt.exec(s);
+            /*
+            Process p = rt.exec(s);
+            try {
+                int e = p.exitValue(); 
+                if (e != 0) {
+                    LogLog.error("Script did not terminate with 0 return value.");                    
+                    System.out.println("Script did not terminate with 0 return value.");                    
+                }
+            } catch (IllegalThreadStateException e) {
+                LogLog.error("Script did not terminate within the timeout, 60 seconds.");
+                System.out.println("Script did not terminate within the timeout, 60 seconds.");
+                p.destroy();
+            }
+            */
 		} catch (Exception e) {
-			LogLog.error("Exception caught while signing log: ", e);
+			LogLog.error("Exception caught while running script: ", e);
 			e.printStackTrace();
 		} 
 		
