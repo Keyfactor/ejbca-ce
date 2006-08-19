4e34758f25830ff0ed25bfd725b94aca54d94598
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2006-08-19 16:12:33 +0000

    ECA-189: we need one more transaction to be completely sure
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@3378 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/core/ejb/log/LogConfigurationDataBean.java
+++ src/java/org/ejbca/core/ejb/log/LogConfigurationDataBean.java
@@ -31,7 +31,7 @@ import org.ejbca.core.model.log.LogEntry;
  * logentryrownumber is the number of the last row number in the log entry database.
  * </pre>
  *
- * @version $Id: LogConfigurationDataBean.java,v 1.4 2006-08-06 14:20:58 anatom Exp $
+ * @version $Id: LogConfigurationDataBean.java,v 1.5 2006-08-19 16:12:33 anatom Exp $
  *
  * @ejb.bean
  *   description="This enterprise bean entity represents a Log Entry with accompanying data"
@@ -143,7 +143,7 @@ public abstract class LogConfigurationDataBean extends BaseEntityBean {
      * DOCUMENT ME!
      *
      * @ejb.interface-method view-type="local"
-     * @ejb.transaction type="Required"
+     * @ejb.transaction type="RequiresNew"
      */
     public Integer getAndIncrementRowCount() {
         int returnval = getLogEntryRowNumber();
