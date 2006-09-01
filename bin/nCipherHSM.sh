a3a9a4e2a1b7df488a13f67ebecfb02292cc6ab3
primelars <USER@primekey.com>
2006-09-01 10:49:45 +0000

    uses real build
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@3414 37aeb1de-b14b-0410-9a0e-82b28170d228

--- bin/nCipherHSM.sh
+++ bin/nCipherHSM.sh
@@ -7,7 +7,7 @@ args+="$@"
 JARS=/opt/nfast/java/classes
 cp=$JARS/rsaprivenc.jar:$JARS/nfjava.jar:$JARS/kmjava.jar:$JARS/kmcsp.jar:$JARS/jutils.jar
 cp+=:$EJBCA_HOME/lib/bcprov-jdk15.jar
-cp+=:$EJBCA_HOME/out/classes
-#cp+=:$EJBCA_HOME/tmp/bin/classes
+#cp+=:$EJBCA_HOME/out/classes
+cp+=:$EJBCA_HOME/tmp/bin/classes
 
 "$JAVA_HOME/bin/java" -cp $cp org.ejbca.ui.cli.HSMKeyTool $args
