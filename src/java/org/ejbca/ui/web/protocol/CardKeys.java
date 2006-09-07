efbbe4083d98eee5ccad94faa71e39e5524c18d9
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2006-09-07 10:00:08 +0000

    added correct headers
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@3432 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/ui/web/protocol/CardKeys.java
+++ src/java/org/ejbca/ui/web/protocol/CardKeys.java
@@ -1,3 +1,15 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.ejbca.ui.web.protocol;
 
 import java.security.PrivateKey;
@@ -5,7 +17,7 @@ import java.security.interfaces.RSAPublicKey;
 
 /**
  * @author lars
- *
+ * @version $Id: CardKeys.java,v 1.5 2006-09-07 10:00:08 anatom Exp $
  */
 public interface CardKeys {
 
