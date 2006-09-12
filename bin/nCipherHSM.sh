09273de4ace6cf6a1d41ac7c7ecf245d752b2724
primelars <USER@primekey.com>
2006-09-12 10:22:37 +0000

    see http://jira.primekey.se/browse/ECA-321 jutils.jar needed when gnerating keys
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@3437 37aeb1de-b14b-0410-9a0e-82b28170d228

--- bin/nCipherHSM.sh
+++ bin/nCipherHSM.sh
@@ -5,7 +5,7 @@ shift
 args+="com.ncipher.provider.km.nCipherKM nCipher.sworld "
 args+="$@"
 JARS=/opt/nfast/java/classes
-cp=$JARS/rsaprivenc.jar:$JARS/nfjava.jar:$JARS/kmjava.jar:$JARS/kmcsp.jar
+cp=$JARS/rsaprivenc.jar:$JARS/nfjava.jar:$JARS/kmjava.jar:$JARS/kmcsp.jar:$JARS/jutils.jar
 cp+=:$EJBCA_HOME/lib/bcprov-jdk15.jar
 #cp+=:$EJBCA_HOME/out/classes
 cp+=:$EJBCA_HOME/tmp/bin/classes
