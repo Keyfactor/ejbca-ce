54e67108a18c591c0f8c7a3bc32e6fc070e423cb
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2006-10-20 18:46:03 +0000

    ECA-99: possibility to protect response messages with PBE
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@3629 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/core/protocol/cmp/CmpMessageHelper.java
+++ src/java/org/ejbca/core/protocol/cmp/CmpMessageHelper.java
@@ -17,6 +17,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.security.InvalidKeyException;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.PrivateKey;
@@ -26,6 +27,10 @@ import java.security.cert.CertificateEncodingException;
 import java.security.cert.X509Certificate;
 import java.util.Date;
 import java.util.Random;
+
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 import org.apache.log4j.Logger;
 import org.bouncycastle.asn1.ASN1InputStream;
@@ -49,14 +54,16 @@ import org.ejbca.core.protocol.ResponseStatus;
 import org.ejbca.util.Base64;
 import org.ejbca.util.CertTools;
 
+import com.novosec.pkix.asn1.cmp.PKIBody;
 import com.novosec.pkix.asn1.cmp.PKIHeader;
 import com.novosec.pkix.asn1.cmp.PKIMessage;
+import com.novosec.pkix.asn1.crmf.PBMParameter;
 
 /**
  * Helper class to create different standard parts of CMP messages
  * 
  * @author tomas
- * @version $Id: CmpMessageHelper.java,v 1.4 2006-09-25 12:54:59 anatom Exp $
+ * @version $Id: CmpMessageHelper.java,v 1.5 2006-10-20 18:46:03 anatom Exp $
  */
 public class CmpMessageHelper {
 	private static Logger log = Logger.getLogger(CmpMessageHelper.class);
@@ -81,6 +88,7 @@ public class CmpMessageHelper {
 	}
 
     public static byte[] signPKIMessage(PKIMessage myPKIMessage, X509Certificate signCert, PrivateKey signKey, String digestAlg, String provider) throws InvalidKeyException, NoSuchProviderException, NoSuchAlgorithmException, SecurityException, SignatureException, IOException, CertificateEncodingException {
+		log.debug(">signPKIMessage()");
 		X509CertificateStructure signStruct = X509CertificateStructure.getInstance(new ASN1InputStream(new ByteArrayInputStream(signCert.getEncoded())).readObject());
 		CmpMessageHelper.buildCertBasedPKIProtection( myPKIMessage, signStruct, signKey, digestAlg, provider);
 
@@ -88,6 +96,7 @@ public class CmpMessageHelper {
 		DEROutputStream mout = new DEROutputStream( baos );
 		mout.writeObject( myPKIMessage );
 		mout.close();
+		log.debug("<signPKIMessage()");
 		return baos.toByteArray();
     }
     
@@ -111,6 +120,72 @@ public class CmpMessageHelper {
 		pKIMessage.setProtection( new DERBitString(sig.sign()) );
 		pKIMessage.addExtraCert( cert );
 	}
+	
+	public static byte[] protectPKIMessageWithPBE(PKIMessage msg, String keyId, String raSecret, String digestAlgId, String macAlgId, String protAlgId) throws NoSuchAlgorithmException, NoSuchProviderException, InvalidKeyException, IOException {
+		log.debug(">protectPKIMessageWithPBE()");
+		// Create the PasswordBased protection of the message
+		PKIHeader head = msg.getHeader();
+		head.setSenderKID(new DEROctetString(keyId.getBytes()));
+		// SHA1
+		//AlgorithmIdentifier owfAlg = new AlgorithmIdentifier("1.3.14.3.2.26");
+		AlgorithmIdentifier owfAlg = new AlgorithmIdentifier(digestAlgId);
+		// 1024 iterations
+		int iterationCount = 1024;
+		DERInteger iteration = new DERInteger(iterationCount);
+		// HMAC/SHA1
+		//AlgorithmIdentifier macAlg = new AlgorithmIdentifier("1.2.840.113549.2.7");
+		AlgorithmIdentifier macAlg = new AlgorithmIdentifier(macAlgId);
+		// We need some random bytes for the nonce
+		byte[] saltbytes = createSenderNonce();
+		DEROctetString derSalt = new DEROctetString(saltbytes);
+		
+		// Create the new protected return message
+		//String objectId = "1.2.840.113533.7.66.13";
+		String objectId = protAlgId;
+		PBMParameter pp = new PBMParameter(derSalt, owfAlg, iteration, macAlg);
+		AlgorithmIdentifier pAlg = new AlgorithmIdentifier(new DERObjectIdentifier(objectId), pp);
+		head.setProtectionAlg(pAlg);
+		PKIBody body = msg.getBody();
+		PKIMessage ret = new PKIMessage(head, body);
+
+		// Calculate the protection bits
+		byte[] rasecret = raSecret.getBytes();
+		byte[] basekey = new byte[rasecret.length + saltbytes.length];
+		for (int i = 0; i < rasecret.length; i++) {
+			basekey[i] = rasecret[i];
+		}
+		for (int i = 0; i < saltbytes.length; i++) {
+			basekey[rasecret.length+i] = saltbytes[i];
+		}
+		// Construct the base key according to rfc4210, section 5.1.3.1
+		MessageDigest dig = MessageDigest.getInstance(owfAlg.getObjectId().getId(), "BC");
+		for (int i = 0; i < iterationCount; i++) {
+			basekey = dig.digest(basekey);
+			dig.reset();
+		}
+		// Do the mac
+		String macOid = macAlg.getObjectId().getId();
+		byte[] protectedBytes = ret.getProtectedBytes();
+		Mac mac = Mac.getInstance(macOid, "BC");
+		SecretKey key = new SecretKeySpec(basekey, macOid);
+		mac.init(key);
+		mac.reset();
+		mac.update(protectedBytes, 0, protectedBytes.length);
+		byte[] out = mac.doFinal();
+		DERBitString bs = new DERBitString(out);
+
+		// Finally store the protection bytes in the msg
+		ret.setProtection(bs);
+		
+		// Return response as byte array 
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		DEROutputStream mout = new DEROutputStream( baos );
+		mout.writeObject( ret );
+		mout.close();
+		log.debug("<protectPKIMessageWithPBE()");
+		return baos.toByteArray();
+	}
+
 
 	/** Creates a 16 bytes random sender nonce
 	 * 
