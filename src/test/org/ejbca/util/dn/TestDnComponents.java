047873e39fe9f9d81b901882e5ed5ef3cfa5c746
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2006-12-04 12:09:27 +0000

    update with new numbers
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@3862 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/test/org/ejbca/util/dn/TestDnComponents.java
+++ src/test/org/ejbca/util/dn/TestDnComponents.java
@@ -22,7 +22,7 @@ import org.ejbca.util.CertTools;
 /**
  * Tests the StringTools class .
  *
- * @version $Id: TestDnComponents.java,v 1.1 2006-11-18 13:37:28 anatom Exp $
+ * @version $Id: TestDnComponents.java,v 1.2 2006-12-04 12:09:10 anatom Exp $
  */
 public class TestDnComponents extends TestCase {
     private static Logger log = Logger.getLogger(TestDnComponents.class);
@@ -53,31 +53,32 @@ public class TestDnComponents extends TestCase {
      */
     public void test01CheckObjects() throws Exception {
         String[] s = DnComponents.getDnObjects();
-        assertEquals(20, s.length);
+        assertEquals(21, s.length);
         assertEquals("unstructuredaddress",s[0]);
         assertEquals("unstructuredname",s[1]);
-        assertEquals("uid",s[5]);
-        assertEquals("cn",s[6]);
-        assertEquals("t",s[13]);
-        assertEquals("c",s[19]);
+        assertEquals("dn",s[5]);
+        assertEquals("uid",s[6]);
+        assertEquals("cn",s[7]);
+        assertEquals("t",s[14]);
+        assertEquals("c",s[20]);
 
         String[] s1 = DnComponents.getDnObjectsReverse();
-        assertEquals(20, s1.length);
-        assertEquals("unstructuredaddress",s1[19]);
-        assertEquals("unstructuredname",s1[18]);
+        assertEquals(21, s1.length);
+        assertEquals("unstructuredaddress",s1[20]);
+        assertEquals("unstructuredname",s1[19]);
         assertEquals("uid",s1[14]);
         assertEquals("cn",s1[13]);
         assertEquals("t",s1[6]);
         assertEquals("c",s1[0]);
 
         String[] s2 = DnComponents.getDnObjects();
-        assertEquals(20, s2.length);
+        assertEquals(21, s2.length);
         assertEquals("unstructuredaddress",s2[0]);
         assertEquals("unstructuredname",s2[1]);
-        assertEquals("uid",s2[5]);
-        assertEquals("cn",s2[6]);
-        assertEquals("t",s2[13]);
-        assertEquals("c",s2[19]);
+        assertEquals("uid",s2[6]);
+        assertEquals("cn",s2[7]);
+        assertEquals("t",s2[14]);
+        assertEquals("c",s2[20]);
 
     }
     public void test02() {
