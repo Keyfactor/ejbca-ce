490a5ef814454cda8d9ce39cfbfe901b6d7b552d
primelars <USER@primekey.com>
2006-12-15 15:09:35 +0000

    encrypt decrypt test added
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@3926 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/ui/cli/HSMKeyTool.java
+++ src/java/org/ejbca/ui/cli/HSMKeyTool.java
@@ -36,52 +36,21 @@ import java.security.UnrecoverableKeyException;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.security.interfaces.RSAKey;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.crypto.Cipher;
 import javax.security.auth.x500.X500Principal;
 
 import org.bouncycastle.x509.X509V3CertificateGenerator;
 
-class Test {
-    final private String sigAlgName = "SHA1withRSA";
-    final private byte signInput[] = "Lillan gick på vägen ut.".getBytes();
-    final private String alias;
-    final private KeyPair keyPair;
-    final private String providerName;
-    public Test(String a, KeyPair kp, String pn) {
-        alias = a;
-        keyPair = kp;
-        providerName = pn;
-    }
-    public void doIt(int i) throws Exception {
-        final byte signBA[]; {
-            Signature signature = Signature.getInstance(sigAlgName, providerName);
-            signature.initSign( keyPair.getPrivate() );
-            signature.update( signInput );
-            signBA = signature.sign();
-        }
-        {
-            Signature signature = Signature.getInstance(sigAlgName);
-            signature.initVerify(keyPair.getPublic());
-            signature.update(signInput);
-            boolean result = signature.verify(signBA);
-            System.out.println("Signature test of key "+alias+
-                               ": signature length " + signBA.length +
-                               "; test nr " + i +
-                               "; first byte " + Integer.toHexString(0xff&signBA[0]) +
-                               "; verifying " + result);
-        }
-        System.gc();
-        System.runFinalization();
-    }
-}
-
 /**
  * @author lars
- * @version $Id: HSMKeyTool.java,v 1.11 2006-12-03 15:20:16 primelars Exp $
+ * @version $Id: HSMKeyTool.java,v 1.12 2006-12-15 15:09:35 primelars Exp $
  *
  */
 public class HSMKeyTool {
@@ -302,6 +271,72 @@ public class HSMKeyTool {
             }
         }
         return (Test[])testSet.toArray(new Test[0]);
+    }
+    private static class Test {
+        final private String alias;
+        final private KeyPair keyPair;
+        final private String providerName;
+        public Test(String a, KeyPair kp, String pn) {
+            alias = a;
+            keyPair = kp;
+            providerName = pn;
+        }
+        public void doIt(int i) throws Exception {
+            signTest(i);
+            cryptTest(i);
+        }
+        private void cryptTest(int i) throws Exception {
+            final String testS = "   01 0123456789   02 0123456789   03 0123456789   04 0123456789   05 0123456789   06 0123456789   07 0123456789   08 0123456789   09 0123456789   10 0123456789   11 0123456789   12 0123456789   13 0123456789   14 0123456789   15 0123456789   16 0123456789   17 0123456789   18 0123456789   19 0123456789   20 0123456789   21 0123456789   22 0123456789   23 0123456789   24 0123456789   25 0123456789   26 0123456789   27 0123456789   28 0123456789   29 0123456789   30 0123456789   31 0123456789   32 0123456789   33 0123456789   34 0123456789   35 0123456789   36 0123456789   37 0123456789";
+            final int modulusLength = ((RSAKey)keyPair.getPublic()).getModulus().bitLength();
+            final int byteLength = (modulusLength+7)/8-11;
+            final byte original[] = testS.substring(0, byteLength).getBytes();
+            final String pkcs1Padding="RSA/ECB/PKCS1Padding";
+//            final String noPadding="RSA/ECB/NoPadding";
+            final byte encoded[]; {
+                final Cipher cipher = Cipher.getInstance(pkcs1Padding);
+                System.out.print("encryption provider: "+cipher.getProvider());
+                cipher.init(Cipher.ENCRYPT_MODE, keyPair.getPublic());
+                encoded = cipher.doFinal(original);
+            }
+            final byte decoded[]; {
+                final Cipher cipher = Cipher.getInstance(pkcs1Padding, providerName);
+                System.out.print("; decryption provider: "+cipher.getProvider());
+                cipher.init(Cipher.DECRYPT_MODE, keyPair.getPrivate());
+                decoded = cipher.doFinal(encoded);
+            }
+            final boolean isSame = Arrays.equals(original, decoded);
+            System.out.print("; modulus length: "+modulusLength+"; byte length "+byteLength);
+            if (isSame)
+                System.out.println(". The docoded byte string is equal to the original!");
+            else {
+                System.out.println("The original and the decoded byte array differs!");
+                System.out.println("Original: \""+new String(original)+'\"');
+                System.out.println("Decoded: \""+new String(decoded)+'\"');
+            }
+        }
+        private void signTest(int i) throws Exception {
+            final String sigAlgName = "SHA1withRSA";
+            final byte signInput[] = "Lillan gick på vägen ut.".getBytes();
+            final byte signBA[]; {
+                Signature signature = Signature.getInstance(sigAlgName, providerName);
+                signature.initSign( keyPair.getPrivate() );
+                signature.update( signInput );
+                signBA = signature.sign();
+            }
+            {
+                Signature signature = Signature.getInstance(sigAlgName);
+                signature.initVerify(keyPair.getPublic());
+                signature.update(signInput);
+                boolean result = signature.verify(signBA);
+                System.out.println("Signature test of key "+alias+
+                                   ": signature length " + signBA.length +
+                                   "; test nr " + i +
+                                   "; first byte " + Integer.toHexString(0xff&signBA[0]) +
+                                   "; verifying " + result);
+            }
+            System.gc();
+            System.runFinalization();
+        }
     }
     private static void test(final String providerClassName,
                              final String keyStoreType,
