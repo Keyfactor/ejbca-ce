ae835ae689b97fafc36c862321da04ae12e63b90
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2006-12-22 10:39:11 +0000

    ECA-393: Make number of returned log rows configurable
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@3960 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/core/model/log/LogConstants.java
+++ src/java/org/ejbca/core/model/log/LogConstants.java
@@ -1,13 +1,13 @@
 package org.ejbca.core.model.log;
 
 /**
- * @version $Id: LogConstants.java,v 1.2 2006-12-20 08:33:32 anatom Exp $
+ * @version $Id: LogConstants.java,v 1.3 2006-12-22 10:39:11 anatom Exp $
  */
 public interface LogConstants {
 
 	/** Constant limiting thenumber of rows returned when querying logfiles to be viewed by in the admin-GUI
 	 */
-    public static final int MAXIMUM_QUERY_ROWCOUNT = 1000;
+    public static final int MAXIMUM_QUERY_ROWCOUNT = Integer.parseInt("@log.maxqueryrowcount@");
 
     /**
      * Constant containing caid that couldn't be determined in any other way. Log events can only be viewed.
