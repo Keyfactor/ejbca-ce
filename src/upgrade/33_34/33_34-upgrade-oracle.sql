d6b90b2d9909d88b4a0fe11166ac66d12ad67727
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2007-01-12 10:15:00 +0000

    ECA-402: oracle and weblogic issues
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@4048 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/upgrade/33_34/33_34-upgrade-oracle.sql
+++ src/upgrade/33_34/33_34-upgrade-oracle.sql
@@ -1,3 +1,3 @@
 
 # Format of column changed, no information was stored here earlier though
-alter table CAData add updateTime NUMBER(19) NOT NULL default 0;
+alter table CAData add updateTime NUMBER(19) default 0;
