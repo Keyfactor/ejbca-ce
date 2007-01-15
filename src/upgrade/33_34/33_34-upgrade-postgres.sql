d1122265788737186fc0ec055311f7e6812cf982
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2007-01-15 13:15:59 +0000

    ECA-403: update sql with better one from Bruno Bonfils, works with all versions of postgresql.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@4057 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/upgrade/33_34/33_34-upgrade-postgres.sql
+++ src/upgrade/33_34/33_34-upgrade-postgres.sql
@@ -1,3 +1,6 @@
 
 # Format of column changed, no information was stored here earlier though
-alter table CAData add updateTime INT8 NOT NULL DEFAULT 0;
+alter table CAData add updateTime INT8;
+update cadata set updateTime = 0;
+alter table cadata alter column updateTime set not null;
+alter table cadata alter column updateTime set default 0;
