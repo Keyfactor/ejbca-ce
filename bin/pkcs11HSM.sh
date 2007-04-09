0f11e40db35b8e9f89cbb0280240b2be937620d6
primelars <USER@primekey.com>
2007-04-09 17:27:15 +0000

    see https://jira.primekey.se/browse/ECA-470 Environment variable for pkcs#11 lib
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@4319 37aeb1de-b14b-0410-9a0e-82b28170d228

--- bin/pkcs11HSM.sh
+++ bin/pkcs11HSM.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 
-#
-# Bruno Bonfils, <asyd@asyd.net>
-# January 2007
 # 
-# Create a key via a netHSM device # 
+# Create a key via a PKCS#11 device # 
 # Example:
 #
 
 if [ -z $EJBCA_HOME ]; then
-        echo "Fatal error: EJBCA_HOME is not set"
-        exit 1
+	echo "Fatal error: EJBCA_HOME is not set"
+	exit 1
 fi
 
 if [ -z $JAVA_HOME ]; then
-        echo "Fatal error: JAVA_HOME is not set"
+	echo "Fatal error: JAVA_HOME is not set"
+fi
+
+if [ -z $JAVA_PKCS11_LIB ]; then
+	echo "Fatal error: JAVA_PKCS11_LIB is not set"
+	exit 1
 fi
 
 CLASSES=$EJBCA_HOME/lib/bcprov-jdk15.jar
 CLASSES=$CLASSES:$EJBCA_HOME/lib/bcmail-jdk15.jar
-#CLASSES=$CLASSES:$EJBCA_HOME/tmp/bin/classes
+CLASSES=$CLASSES:$EJBCA_HOME/tmp/bin/classes
 # use this instead if you want build from eclipse
-CLASSES=$CLASSES:$EJBCA_HOME/out/classes
-
+#CLASSES=$CLASSES:$EJBCA_HOME/out/classes
 
 # Prepare arguments
 args="`basename $0` $1"
 shift
-args="$args /opt/ETcpsdk/lib/linux-i386/libcryptoki.so null pkcs11 $@"
+args="$args $JAVA_PKCS11_LIB null pkcs11 $@"
 
 # Finally run java
 #set -x
