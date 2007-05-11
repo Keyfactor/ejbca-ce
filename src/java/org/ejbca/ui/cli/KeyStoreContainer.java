accc38e19efd3ca23984e2baa585cd9d56f6cd29
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2007-05-11 07:49:55 +0000

    add ejbca headers
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@4458 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/ui/cli/KeyStoreContainer.java
+++ src/java/org/ejbca/ui/cli/KeyStoreContainer.java
@@ -1,6 +1,15 @@
-/**
- * 
- */
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.ejbca.ui.cli;
 
 import java.io.BufferedInputStream;
@@ -55,6 +64,11 @@ import sun.security.pkcs11.SunPKCS11;
 
 import com.sun.security.auth.callback.TextCallbackHandler;
 
+/**
+ * 
+ * @version $Id: KeyStoreContainer.java,v 1.17 2007-05-11 07:49:55 anatom Exp $
+ *
+ */
 public abstract class KeyStoreContainer {
     protected final KeyStore keyStore;
     private final String providerName;
--- src/java/org/ejbca/ui/cli/KeyStoreContainerTest.java
+++ src/java/org/ejbca/ui/cli/KeyStoreContainerTest.java
@@ -1,6 +1,15 @@
-/**
- * 
- */
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.ejbca.ui.cli;
 
 import java.io.BufferedReader;
@@ -16,6 +25,11 @@ import java.util.Set;
 
 import javax.crypto.Cipher;
 
+/**
+ * 
+ * @version $Id: KeyStoreContainerTest.java,v 1.6 2007-05-11 07:49:55 anatom Exp $
+ *
+ */
 class KeyStoreContainerTest {
     final private String alias;
     final private KeyPair keyPair;
