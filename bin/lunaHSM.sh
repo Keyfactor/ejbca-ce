7029ff7cd2919af0a4c4b816e9b64c6352f34eba
jbagnert <USER@primekey.com>
2007-11-05 14:28:48 +0000

    ECA-642: Added a check in lunaHSM.sh if EJBCA_HOME is set. If it is not set it outputs a warning and exits.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@4892 37aeb1de-b14b-0410-9a0e-82b28170d228

--- bin/lunaHSM.sh
+++ bin/lunaHSM.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
+if [ -z $EJBCA_HOME ]; then
+        echo "Fatal error: EJBCA_HOME is not set"
+        exit 1
+fi
+
+if [ -z $JAVA_HOME ]; then
+        echo "Fatal error: JAVA_HOME is not set"
+fi
+
 "$JAVA_HOME/bin/java" -cp $EJBCA_HOME/lib/LunaJCASP.jar:$EJBCA_HOME/tmp/bin/classes org.ejbca.ui.cli.LunaKeyTool "$@"
