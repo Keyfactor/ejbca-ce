8afe693c34ebc74659da533db29c075dcd546e34
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2007-12-06 11:01:54 +0000

    ECA-633: comment for oracle index
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@4993 37aeb1de-b14b-0410-9a0e-82b28170d228

--- doc/howto/create-index-ejbca3.sql
+++ doc/howto/create-index-ejbca3.sql
@@ -10,6 +10,7 @@ create index userdata_idx1 on UserData (caid,endEntityprofileId);
 create index historydata_idx1 on CertReqHistoryData (username);
 create index protect_idx1 on TableProtectData (dbKey,dbType);
 create index protectedlogdata_idx1 on ProtectedLogData (nodeGUID, counter);
+-- Oracle does not like the b64Protection(10) notification, use simply b64Protection instead
 create index protectedlogdata_idx2 on ProtectedLogData (nodeGUID, eventTime, b64Protection(10));
 create index protectedlogexportdata_idx2 on ProtectedLogExportData (exportStartTime);
 
