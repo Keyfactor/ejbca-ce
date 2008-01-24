74f81e7bfd9dc4043552b56f174eb87d6c1dc28c
Johan Eklund <johan.eklund@primekey.com>
2008-01-24 17:45:02 +0000

    ECA-693: Added JBoss-specific row-locking to LogConfigurationData to avoid Duplicate key exceptions during concurrent logging.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@5163 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/core/ejb/log/LogConfigurationDataBean.java
+++ src/java/org/ejbca/core/ejb/log/LogConfigurationDataBean.java
@@ -31,7 +31,7 @@ import org.ejbca.core.model.log.LogConstants;
  * logentryrownumber is the number of the last row number in the log entry database.
  * </pre>
  *
- * @version $Id: LogConfigurationDataBean.java,v 1.10 2007-12-04 14:21:41 jeklund Exp $
+ * @version $Id: LogConfigurationDataBean.java,v 1.11 2008-01-24 17:45:02 jeklund Exp $
  *
  * @ejb.bean
  *   description="This enterprise bean entity represents a Log Entry with accompanying data"
@@ -66,6 +66,9 @@ import org.ejbca.core.model.log.LogConstants;
  *
  * @jonas.jdbc-mapping
  *   jndi-name="${datasource.jndi-name}"
+ *   
+ * @jboss.persistence
+ *   row-locking="true" 
  */
 public abstract class LogConfigurationDataBean extends BaseEntityBean {
 
@@ -92,7 +95,6 @@ public abstract class LogConfigurationDataBean extends BaseEntityBean {
 
     /**
      * @ejb.persistence column-name="logEntryRowNumber"
-     * @jboss.persistence row-locking="true" 
      */
     public abstract int getLogEntryRowNumber();
 
