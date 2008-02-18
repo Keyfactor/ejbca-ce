3bdbb4cb82081ae3a2cf1e379d6b3817cf53a531
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2008-02-18 14:46:59 +0000

    make it possible to use without setting EJBCA_HOME and JAVA_HOME
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@5256 37aeb1de-b14b-0410-9a0e-82b28170d228

--- bin/pkcs11HSM.sh
+++ bin/pkcs11HSM.sh
@@ -5,13 +5,26 @@
 # Example:
 #
 
-if [ -z $EJBCA_HOME ]; then
-	echo "Fatal error: EJBCA_HOME is not set"
-	exit 1
+if [ -z "$EJBCA_HOME" ] ; then
+	EJBCA_FILE="$0" 
+	EJBCA_HOME=`echo $(dirname $(dirname $EJBCA_FILE))`
 fi
 
-if [ -z $JAVA_HOME ]; then
-	echo "Fatal error: JAVA_HOME is not set"
+#if [ -z $EJBCA_HOME ]; then
+#	echo "Fatal error: EJBCA_HOME is not set"
+#	exit 1
+#fi
+
+JAVACMD=`which java`
+# Check that JAVA_HOME is set
+if [ ! -n "$JAVA_HOME" ]; then
+    if [ ! -n "$JAVACMD" ]
+    then
+        echo "You must set JAVA_HOME before running the EJBCA cli."
+        exit 1
+    fi
+else
+    JAVACMD=$JAVA_HOME/bin/java
 fi
 
 CLASSES=$EJBCA_HOME/lib/bcprov-jdk15.jar
@@ -41,4 +54,4 @@ args="$args null pkcs11 $@"
 
 # Finally run java
 #set -x
-$JAVA_HOME/bin/java -cp $CLASSES org.ejbca.ui.cli.HSMKeyTool $args
+$JAVACMD -cp $CLASSES org.ejbca.ui.cli.HSMKeyTool $args
