f52332360d67a3a4074a7742379e91059a116671
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2008-02-25 19:42:15 +0000

    automate performance test
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@5284 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/test/se/anatom/ejbca/ca/sign/TestSignLotsOfCerts.java
+++ src/test/se/anatom/ejbca/ca/sign/TestSignLotsOfCerts.java
@@ -13,11 +13,18 @@
 
 package se.anatom.ejbca.ca.sign;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.rmi.RemoteException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.PublicKey;
 import java.security.cert.X509Certificate;
 import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
 
 import javax.ejb.DuplicateKeyException;
 import javax.naming.Context;
@@ -26,27 +33,43 @@ import javax.naming.NamingException;
 import junit.framework.TestCase;
 
 import org.apache.log4j.Logger;
+import org.ejbca.core.ejb.authorization.IAuthorizationSessionHome;
+import org.ejbca.core.ejb.authorization.IAuthorizationSessionRemote;
+import org.ejbca.core.ejb.ca.caadmin.ICAAdminSessionHome;
+import org.ejbca.core.ejb.ca.caadmin.ICAAdminSessionRemote;
 import org.ejbca.core.ejb.ca.sign.ISignSessionHome;
 import org.ejbca.core.ejb.ca.sign.ISignSessionRemote;
 import org.ejbca.core.ejb.ra.IUserAdminSessionHome;
 import org.ejbca.core.ejb.ra.IUserAdminSessionRemote;
 import org.ejbca.core.model.SecConst;
+import org.ejbca.core.model.ca.caadmin.CAExistsException;
+import org.ejbca.core.model.ca.caadmin.CAInfo;
+import org.ejbca.core.model.ca.caadmin.X509CAInfo;
+import org.ejbca.core.model.ca.caadmin.extendedcaservices.ExtendedCAServiceInfo;
+import org.ejbca.core.model.ca.caadmin.extendedcaservices.OCSPCAServiceInfo;
+import org.ejbca.core.model.ca.caadmin.extendedcaservices.XKMSCAServiceInfo;
+import org.ejbca.core.model.ca.catoken.CATokenConstants;
+import org.ejbca.core.model.ca.catoken.CATokenInfo;
+import org.ejbca.core.model.ca.catoken.SoftCATokenInfo;
 import org.ejbca.core.model.log.Admin;
 import org.ejbca.core.model.ra.UserDataConstants;
 import org.ejbca.util.CertTools;
 
 
-/** This is a manual test that requires some manual set up and configuration.
- * -caid should a CA that has "finish user" set to off
- * -the users performancefoono1-10 should not exist in the database
+/** This is a performance test:
+ * - 10 threads generates 1000 certificates each with 1024 bit public key
+ * - CA uses 2048 bit signature key
+ * - total time for certificate generation is counted to get number of certificates generated per second
+ * 
  * 
  *
- * @version $Id: TestSignLotsOfCerts.java,v 1.7 2006-08-12 09:49:53 herrvendil Exp $
+ * @version $Id: TestSignLotsOfCerts.java,v 1.8 2008-02-25 19:42:15 anatom Exp $
  */
 public class TestSignLotsOfCerts extends TestCase {
     private static Logger log = Logger.getLogger(TestSignLotsOfCerts.class);
     public static Context ctx;
     private static IUserAdminSessionRemote usersession;
+    private static ICAAdminSessionRemote cacheAdmin;
     public static KeyPair keys;
     private static int caid = 0;
     public Admin admin;
@@ -68,13 +91,21 @@ public class TestSignLotsOfCerts extends TestCase {
 
         admin = new Admin(Admin.TYPE_BATCHCOMMANDLINE_USER);
 
-        caid = -1688117755; // EDIT THIS
+        caid = "CN=TESTPERF1".hashCode();
 
         ctx = getInitialContext();
         Object obj = ctx.lookup("UserAdminSession");
         IUserAdminSessionHome userhome = (IUserAdminSessionHome) javax.rmi.PortableRemoteObject.narrow(obj, IUserAdminSessionHome.class);
         usersession = userhome.create();
         
+        if (cacheAdmin == null) {
+        	Context jndiContext = getInitialContext();
+        	Object obj1 = jndiContext.lookup("CAAdminSession");
+        	ICAAdminSessionHome home = (ICAAdminSessionHome) javax.rmi.PortableRemoteObject.narrow(obj1, ICAAdminSessionHome.class);
+        	cacheAdmin = home.create();
+        }
+
+
         keys = genKeys();
 
         log.debug("<setUp()");
@@ -128,10 +159,113 @@ public class TestSignLotsOfCerts extends TestCase {
         }
 
     }
+    private void deleteUser(String post) throws Exception {
+        // Make user that we know...
+        boolean userExists = false;
+        try {
+            usersession.deleteUser(admin, "performancefoo"+post);
+            log.debug("deleted user: performancefoo"+post);
+        } catch (RemoteException re) {
+            if (re.detail instanceof DuplicateKeyException) {
+                userExists = true;
+            }
+        }
+    }
+    
+    public void test00AddRSACA() throws Exception {
+        boolean ret = false;
+        try {
+
+            Context context = getInitialContext();
+            IAuthorizationSessionHome authorizationsessionhome = (IAuthorizationSessionHome) javax.rmi.PortableRemoteObject.narrow(context.lookup("AuthorizationSession"), IAuthorizationSessionHome.class);
+            IAuthorizationSessionRemote authorizationsession = authorizationsessionhome.create();
+            authorizationsession.initialize(admin, "CN=TESTPERF1".hashCode());
+
+            SoftCATokenInfo catokeninfo = new SoftCATokenInfo();
+            catokeninfo.setSignKeySpec("2048");
+            catokeninfo.setEncKeySpec("2048");
+            catokeninfo.setSignKeyAlgorithm(SoftCATokenInfo.KEYALGORITHM_RSA);
+            catokeninfo.setEncKeyAlgorithm(SoftCATokenInfo.KEYALGORITHM_RSA);
+            catokeninfo.setSignatureAlgorithm(CATokenInfo.SIGALG_SHA1_WITH_RSA);
+            catokeninfo.setEncryptionAlgorithm(CATokenInfo.SIGALG_SHA1_WITH_RSA);
+            // Create and active OSCP CA Service.
+            ArrayList extendedcaservices = new ArrayList();
+            extendedcaservices.add(new OCSPCAServiceInfo(ExtendedCAServiceInfo.STATUS_ACTIVE,
+                    "CN=OCSPSignerCertificate, " + "CN=TESTPERF1",
+                    "",
+                    "1024",
+                    CATokenConstants.KEYALGORITHM_RSA));
+            extendedcaservices.add(new XKMSCAServiceInfo(ExtendedCAServiceInfo.STATUS_INACTIVE,
+                    "CN=XKMSCertificate, " + "CN=TESTPERF1",
+                    "",
+                    "1024",
+                    CATokenConstants.KEYALGORITHM_RSA));
+
+
+            X509CAInfo cainfo = new X509CAInfo("CN=TESTPERF1",
+                    "TESTPERF1", SecConst.CA_ACTIVE, new Date(),
+                    "", SecConst.CERTPROFILE_FIXED_ROOTCA,
+                    3650,
+                    null, // Expiretime
+                    CAInfo.CATYPE_X509,
+                    CAInfo.SELFSIGNED,
+                    (Collection) null,
+                    catokeninfo,
+                    "JUnit RSA CA",
+                    -1, null,
+                    null, // PolicyId
+                    24, // CRLPeriod
+                    0, // CRLIssueInterval
+                    10, // CRLOverlapTime
+                    10, // Delta CRL period
+                    new ArrayList(),
+                    true, // Authority Key Identifier
+                    false, // Authority Key Identifier Critical
+                    true, // CRL Number
+                    false, // CRL Number Critical
+                    null, // defaultcrldistpoint 
+                    null, // defaultcrlissuer 
+                    null, // defaultocsplocator
+                    null, // defaultfreshestcrl
+                    false, // Finish User
+                    extendedcaservices,
+                    false, // use default utf8 settings
+                    new ArrayList(), // Approvals Settings
+                    1, // Number of Req approvals
+                    false, // Use UTF8 subject DN by default
+            		true, // Use LDAP DN order by default
+            		false, // Use CRL Distribution Point on CRL
+            		false  // CRL Distribution Point on CRL critical
+            		);
+
+            cacheAdmin.createCA(admin, cainfo);
+
+
+            CAInfo info = cacheAdmin.getCAInfo(admin, "TESTPERF1");
+
+            X509Certificate cert = (X509Certificate) info.getCertificateChain().iterator().next();
+            assertTrue("Error in created ca certificate", cert.getSubjectDN().toString().equals("CN=TESTPERF1"));
+            assertTrue("Creating CA failed", info.getSubjectDN().equals("CN=TESTPERF1"));
+            PublicKey pk = cert.getPublicKey();
+            if (pk instanceof RSAPublicKey) {
+            	RSAPublicKey rsapk = (RSAPublicKey) pk;
+				assertEquals(rsapk.getAlgorithm(), "RSA");
+			} else {
+				assertTrue("Public key is not EC", false);
+			}
+            assertTrue("CA is not valid for the specified duration.",cert.getNotAfter().after(new Date(new Date().getTime()+10*364*24*60*60*1000L)) && cert.getNotAfter().before(new Date(new Date().getTime()+10*366*24*60*60*1000L)));
+            ret = true;
+        } catch (CAExistsException pee) {
+            log.info("CA exists.");
+        }
+
+        assertTrue("Creating RSA CA failed", ret);
+    }
+
     /**
      * creates new user
      *
-     * @throws Exception if en error occurs...
+     * @throws Exception if an error occurs...
      */
     public void test01CreateNewUser() throws Exception {
         log.debug(">test01CreateNewUser()");
@@ -199,14 +333,42 @@ public class TestSignLotsOfCerts extends TestCase {
         no10.join();
 		long after = System.currentTimeMillis();
 		long diff = after - before;
-        System.out.println("All threads finished. Total time: "+diff);
+        System.out.println("All threads finished. Total time: "+diff+" ms");
+        int noOfGeneratedCerts = (10 * SignTester.NO_CERTS);
+        System.out.println("Generated "+noOfGeneratedCerts+" certificates in total.");
+        BigDecimal d = new BigDecimal(diff).divide(new BigDecimal(1000));
+        BigDecimal noCerts = new BigDecimal(noOfGeneratedCerts).divide(d, 2, RoundingMode.UP);
+        System.out.println("Performance is "+ noCerts.intValue() +" certs/sec.");
         //FileOutputStream fos = new FileOutputStream("testcert.crt");
         //fos.write(cert.getEncoded());
         //fos.close();
         log.debug("<test03SignLotsOfCerts()");
     }
     
+    public void test99CleanUp() throws Exception {
+        boolean ret = false;
+        try {
+            cacheAdmin.removeCA(admin, "CN=TESTPERF1".hashCode());
+            ret = true;
+        } catch (Exception pee) {
+        }
+        assertTrue("Removing TESTPERF1 CA failed", ret);
+        deleteUser("no1");
+        deleteUser("no2");
+        deleteUser("no3");
+        deleteUser("no4");
+        deleteUser("no5");
+        deleteUser("no6");
+        deleteUser("no7");
+        deleteUser("no8");
+        deleteUser("no9");
+        deleteUser("no10");
+    }
+
     private class SignTester implements Runnable {
+    	
+    	public static final int NO_CERTS=1000;
+    	
     	public void run() {
             try {
                 Object obj = ctx.lookup("RSASignSession");
@@ -214,19 +376,19 @@ public class TestSignLotsOfCerts extends TestCase {
                 ISignSessionRemote rsaremote = rsahome.create();
                 String user = "performancefoo"+Thread.currentThread().getName();
 				long before = System.currentTimeMillis();
-				for (int i = 0; i<1000;i++) {
+				for (int i = 0; i<NO_CERTS;i++) {
 			        // user that we know exists...
 				    X509Certificate cert = (X509Certificate) rsaremote.createCertificate(admin, user, "foo123", keys.getPublic());
-				    assertNotNull("Misslyckades skapa cert", cert);
+				    assertNotNull("Failed to create certificate", cert);
 				    if ((i % 100) == 0) {
 				    	long mellantid = System.currentTimeMillis() - before;
-				    	System.out.println(Thread.currentThread().getName()+" har skapat "+i+", tid="+mellantid);
+				    	System.out.println(Thread.currentThread().getName()+" has generated "+i+", time="+mellantid);
 				    	
 				    }
 				}
 				long after = System.currentTimeMillis();
 				long diff = after - before;
-				System.out.println("Tidsåtgång ("+Thread.currentThread().getName()+"): "+diff);
+				System.out.println("Time used ("+Thread.currentThread().getName()+"): "+diff);
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
