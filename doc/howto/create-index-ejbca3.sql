30d51629ad914edcfe4e8c9b295a2061bff1b989
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2008-03-27 18:01:39 +0000

    a couple of new indexes for large installations
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@5443 37aeb1de-b14b-0410-9a0e-82b28170d228

--- doc/howto/create-index-ejbca3.sql
+++ doc/howto/create-index-ejbca3.sql
@@ -1,17 +1,19 @@
 create index logentry_idx1 on LogEntryData (time);
-create index logentry_idx2 on LogEntryData (caid);
+create index logentry_idx2 on LogEntryData (caId);
 create index logentry_idx3 on LogEntryData (event);
 create index logentry_idx4 on LogEntryData (username);
 create index crldata_idx1 on CRLData (issuerDN,cRLNumber);
 create index certificatedata_idx1 on CertificateData (issuerDN,serialNumber);
 create index certificatedata_idx2 on CertificateData (username);
 create index certificatedata_idx3 on CertificateData (status,issuerDN);
-create index userdata_idx1 on UserData (caid,endEntityprofileId);
+create index certificatedata_idx4 ON CertificateData(subjectDN); 
+create index userdata_idx1 on UserData (cAId,endEntityProfileId);
 create index historydata_idx1 on CertReqHistoryData (username);
+create index historydata_idx2 on CertReqHistoryData (issuerDN,serialNumber);
 create index protect_idx1 on TableProtectData (dbKey,dbType);
 create index protectedlogdata_idx1 on ProtectedLogData (nodeGUID, counter);
 -- Oracle does not like the b64Protection(1) notification, use simply b64Protection instead
+-- create index protectedlogdata_idx2 on ProtectedLogData (nodeGUID, eventTime, b64Protection);
 create index protectedlogdata_idx2 on ProtectedLogData (nodeGUID, eventTime, b64Protection(1));
-create index protectedlogdata_idx3 on ProtectedLogData (username, caid, module);
-create index protectedlogexportdata_idx2 on ProtectedLogExportData (exportStartTime);
-
+create index protectedlogdata_idx3 on ProtectedLogData (username, caId, module);
+create index protectedlogexportdata_idx1 on ProtectedLogExportData (exportStartTime);
