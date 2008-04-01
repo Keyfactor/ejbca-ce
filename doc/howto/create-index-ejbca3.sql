6ee0c24edab06be7fc15f352edd475c7dd5e300c
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2008-04-01 20:37:58 +0000

    more indexes and improved docs for indexes
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@5481 37aeb1de-b14b-0410-9a0e-82b28170d228

--- doc/howto/create-index-ejbca3.sql
+++ doc/howto/create-index-ejbca3.sql
@@ -1,18 +1,36 @@
+-- Selecting log entries when viewing logs:
 create index logentry_idx1 on LogEntryData (time);
 create index logentry_idx2 on LogEntryData (caId);
 create index logentry_idx3 on LogEntryData (event);
 create index logentry_idx4 on LogEntryData (username);
-create index crldata_idx1 on CRLData (issuerDN,cRLNumber);
-create index certificatedata_idx1 on CertificateData (issuerDN,serialNumber);
+
+-- Indexes on CRLData:	
+-- unique to ensure that no two CRLs with the same CRLnumber from the same issuer is created
+create unique index crldata_idx1 on CRLData (issuerDN,cRLNumber);
+create index crldata_idx2 on CRLData (issuerDN,deltaCRLIndicator);
+-- On EJBCA 3.5: create index crldata_idx2 on CRLData (issuerDN);
+
+-- Indexes on CertificateData:
+-- unique to increase security the no two certificate with the same issuer and serial number can be issued
+create unique index certificatedata_idx1 on CertificateData (issuerDN,serialNumber);
 create index certificatedata_idx2 on CertificateData (username);
 create index certificatedata_idx3 on CertificateData (status,issuerDN);
 create index certificatedata_idx4 ON CertificateData(subjectDN); 
+
+-- Indexes on UserData:
 create index userdata_idx1 on UserData (cAId,endEntityProfileId);
 create index userdata_idx2 on UserData (cAId,timeCreated);
 create index userdata_idx3 on UserData (cAId,timeModified);
+
+-- Indexes on CertReqHistoryData: for viewing history
 create index historydata_idx1 on CertReqHistoryData (username);
 create index historydata_idx2 on CertReqHistoryData (issuerDN,serialNumber);
+
+-- Indexes on TableProtectData: searches when verifying, usually this is disabled
 create index protect_idx1 on TableProtectData (dbKey,dbType);
+
+-- Indexes on ProtectedLogData;
+-- ProtectedLogData does only exist in EJBCA 3.6 and later
 create index protectedlogdata_idx1 on ProtectedLogData (nodeGUID, counter);
 -- Oracle does not like the b64Protection(1) notification, use simply b64Protection instead
 -- create index protectedlogdata_idx2 on ProtectedLogData (nodeGUID, eventTime, b64Protection);
