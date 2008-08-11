6bb49f36c2b55bec47d46ec73d9a9d0e7514883e
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2008-08-11 07:18:29 +0000

    add JUNit tests for EjbcaHelathCheck
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@5988 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ src/test/org/ejbca/ui/web/pub/cluster/WebEjbcaHealthCheckTest.java
@@ -0,0 +1,95 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
+package org.ejbca.ui.web.pub.cluster;
+
+import java.net.URL;
+
+import junit.framework.TestCase;
+
+import org.apache.log4j.Logger;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebConnection;
+import com.gargoylesoftware.htmlunit.WebRequestSettings;
+import com.gargoylesoftware.htmlunit.WebResponse;
+
+
+/**
+ *
+ * @version $Id$
+ */
+public class WebEjbcaHealthCheckTest extends TestCase {
+    private static final Logger log = Logger.getLogger(WebEjbcaHealthCheckTest.class);
+
+    private static final String httpReqPath = "http://localhost:8080/ejbca/publicweb/healthcheck/ejbcahealth";
+
+    /**
+     * Creates a new TestSignSession object.
+     *
+     * @param name name
+     */
+    public WebEjbcaHealthCheckTest(String name) {
+        super(name);
+    }
+
+    protected void setUp() throws Exception {
+    }
+
+    protected void tearDown() throws Exception {
+    }
+
+    
+
+    /**
+     * Creates a number of threads that bombards the health check servlet 1000 times each
+     */
+    public void test01EjbcaHealthHttp() throws Exception {
+        log.debug(">test01EjbcaHealthHttp()");
+
+        // Make a quick test first that it works at all before starting all threads
+        final WebClient webClient = new WebClient();
+        WebRequestSettings settings = new WebRequestSettings(new URL(httpReqPath));
+        WebConnection con = webClient.getWebConnection();
+        WebResponse resp = con.getResponse(settings);
+        assertEquals( "Response code", 200, resp.getStatusCode() );
+        assertEquals("ALLOK", resp.getContentAsString());
+
+		long before = System.currentTimeMillis();
+        Thread no1 = new Thread(new WebEjbcaHealthRunner(httpReqPath),"no1");
+        Thread no2 = new Thread(new WebEjbcaHealthRunner(httpReqPath),"no2");
+        Thread no3 = new Thread(new WebEjbcaHealthRunner(httpReqPath),"no3");
+        Thread no4 = new Thread(new WebEjbcaHealthRunner(httpReqPath),"no4");
+        Thread no5 = new Thread(new WebEjbcaHealthRunner(httpReqPath),"no5");
+        no1.start();
+        System.out.println("Started no1");
+        no2.start();
+        System.out.println("Started no2");
+        no3.start();
+        System.out.println("Started no3");
+        no4.start();
+        System.out.println("Started no4");
+        no5.start();
+        System.out.println("Started no5");
+        no1.join();
+        no2.join();
+        no3.join();
+        no4.join();
+        no5.join();
+		long after = System.currentTimeMillis();
+		long diff = after - before;
+        System.out.println("All threads finished. Total time: "+diff+" ms");
+        log.debug("<test01EjbcaHealthHttp()");
+    }
+    
+}
---
+++ src/test/org/ejbca/ui/web/pub/cluster/WebEjbcaHealthRunner.java
@@ -0,0 +1,53 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.ui.web.pub.cluster;
+
+import java.net.URL;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebConnection;
+import com.gargoylesoftware.htmlunit.WebRequestSettings;
+import com.gargoylesoftware.htmlunit.WebResponse;
+
+public class WebEjbcaHealthRunner implements Runnable {
+
+	public static final int NO_TESTS=100;
+	
+    private String httpReqPath = "http://localhost:8080/ejbca/publicweb/healthcheck/ejbcahealth";
+
+	public WebEjbcaHealthRunner(String reqPath) {
+		httpReqPath = reqPath;
+	}
+	
+	public void run() {
+		try {
+			long before = System.currentTimeMillis();
+			final WebClient webClient = new WebClient();
+			WebRequestSettings settings = new WebRequestSettings(new URL(httpReqPath));
+			WebConnection con = webClient.getWebConnection();
+			for (int i = 0; i<NO_TESTS;i++) {
+				WebResponse resp = con.getResponse(settings);
+				int ret = resp.getStatusCode();
+				if (ret != 200) {
+					throw new Exception("Status code is "+ret);
+				}
+			}
+			long after = System.currentTimeMillis();
+			long diff = after - before;
+			System.out.println("Time used ("+Thread.currentThread().getName()+"): "+diff);			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
