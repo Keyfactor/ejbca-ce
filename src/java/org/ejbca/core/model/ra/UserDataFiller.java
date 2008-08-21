9b06d1ef922e0f4f2ee8f96fb3cc49cdfbd60bf3
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2008-08-21 16:22:49 +0000

    add EJBCA license header and set svn:keyword Id
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@6112 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/core/model/ra/UserDataFiller.java
+++ src/java/org/ejbca/core/model/ra/UserDataFiller.java
@@ -1,8 +1,15 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.ejbca.core.model.ra;
 
 import java.util.ArrayList;
