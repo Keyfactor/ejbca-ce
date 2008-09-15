e29a3de5154b5e20348af2a8a6515fa671834a3b
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2008-09-15 09:10:28 +0000

    prepare for 3.8.0 with release notes etc.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@6314 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/upgrade/37_38/37_38-upgrade-oracle.sql
+++ src/upgrade/37_38/37_38-upgrade-oracle.sql
@@ -1,5 +1,1 @@
-
-# Format of column changed, no information was stored here earlier though
-alter table CRLData add deltaCRLIndicator NUMBER(10) default -1;
-
 ALTER TABLE AdminEntityData ADD cAId NUMBER(10) DEFAULT 0;
