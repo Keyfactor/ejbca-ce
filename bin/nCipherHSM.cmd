4e99df43eb1076624e94fce75e8bd273b7d3dae3
Johan Eklund <johan.eklund@primekey.com>
2008-10-16 20:52:32 +0000

    Added missing dependency on log4j that is present in the bash-version of the script.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@6474 37aeb1de-b14b-0410-9a0e-82b28170d228

--- bin/nCipherHSM.cmd
+++ bin/nCipherHSM.cmd
@@ -28,6 +28,7 @@ set CLASSES=%EJBCA_HOME%\lib\bcprov-jdk15.jar
 set CLASSES=%CLASSES%;%EJBCA_HOME%\lib\bcmail-jdk15.jar
 set CLASSES=%CLASSES%;%EJBCA_HOME%\lib\cert-cvc.jar
 set CLASSES=%CLASSES%;%EJBCA_HOME%\lib\jline-0.9.94.jar
+set CLASSES=%CLASSES%:%EJBCA_HOME%\lib\log4j.jar
 set CLASSES=%CLASSES%;%EJBCA_HOME%\tmp\bin\classes
 rem use this instead if you want build from eclipse
 rem CLASSES=$CLASSES:$EJBCA_HOME/out/classes
