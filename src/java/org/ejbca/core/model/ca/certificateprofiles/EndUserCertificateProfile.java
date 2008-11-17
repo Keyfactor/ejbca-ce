5968340f08d12702f075ae5950b3d58601fca890
dcarella <USER@primekey.com>
2008-11-17 11:03:51 +0000

    ECA-1021: Fix the default ENDUSER Certificate Profile
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@6594 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/core/model/ca/certificateprofiles/EndUserCertificateProfile.java
+++ src/java/org/ejbca/core/model/ca/certificateprofiles/EndUserCertificateProfile.java
@@ -47,7 +47,6 @@ public class EndUserCertificateProfile extends CertificateProfile{
 
       setUseExtendedKeyUsage(true);
       ArrayList eku = new ArrayList();
-      eku.add(new Integer(SERVERAUTH));		/* dcarella: temporary, to fix the server certificate problem */
       eku.add(new Integer(CLIENTAUTH));
       eku.add(new Integer(EMAILPROTECTION));
       setExtendedKeyUsage(eku);
