6960fa0915220a09f7e87aecd59be321ebd79619
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2009-01-28 15:17:22 +0000

    ECA-1006: document MySQL UTF-8 index restrictions with example.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@6905 37aeb1de-b14b-0410-9a0e-82b28170d228

--- doc/howto/create-index-ejbca3.sql
+++ doc/howto/create-index-ejbca3.sql
@@ -1,3 +1,8 @@
+-- Note!
+-- If using UTF-8 character enoding in MySQL index length is limited to 1000 bytes, and UTF-8 characters take up 3 bytes.
+-- Some index rown needs to be changed because of this like:
+-- create index protect_idx1 on TableProtectData (dbKey(150),dbType(150)); 
+ 
 -- Selecting log entries when viewing logs:
 create index logentry_idx1 on LogEntryData (time);
 create index logentry_idx2 on LogEntryData (caId);
