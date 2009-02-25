6a410903640e3c5df6475de33e44593695ededa8
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2009-02-25 12:40:07 +0000

    ECA-1116: org.ejbca.core.protocol.ws.client.ejbcawsracli can not be brought in by regular cli classes.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@7034 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/ui/cli/EjbcaWsRaCli.java
+++ src/java/org/ejbca/ui/cli/EjbcaWsRaCli.java
@@ -31,8 +31,9 @@ public class EjbcaWsRaCli extends ClientToolBox {
         for ( int i=1; i<args.length; i++)  { // remove first argument
             lArgs.add(args[i]);
         }
-        try {// the ejbcawsracli can not be compiled when building EJBCA.
-            Class.forName(org.ejbca.core.protocol.ws.client.ejbcawsracli.class.getName()).getMethod("main", new Class<?>[]{String[].class}).invoke(null, new Object[]{lArgs.toArray(new String[]{})});
+        try {
+        	// the ejbcawsracli can not be compiled when building EJBCA.
+            Class.forName("org.ejbca.core.protocol.ws.client.ejbcawsracli").getMethod("main", new Class<?>[]{String[].class}).invoke(null, new Object[]{lArgs.toArray(new String[]{})});
         } catch (Exception e) {
             e.printStackTrace(System.err);
         }
