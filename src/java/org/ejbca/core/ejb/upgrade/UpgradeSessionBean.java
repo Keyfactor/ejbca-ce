cfc05b42e16cd0204317c1b19191606b4aba4207
Johan Eklund <johan.eklund@primekey.com>
2009-04-07 08:37:56 +0000

    ECA-1173: Dropped upgrade support for EJBCA 3.1.x.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@7248 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/core/ejb/upgrade/UpgradeSessionBean.java
+++ src/java/org/ejbca/core/ejb/upgrade/UpgradeSessionBean.java
@@ -13,7 +13,6 @@
 
 package org.ejbca.core.ejb.upgrade;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -39,15 +38,10 @@ import org.ejbca.core.ejb.authorization.AdminGroupDataLocal;
 import org.ejbca.core.ejb.authorization.AdminGroupDataLocalHome;
 import org.ejbca.core.ejb.ca.caadmin.ICAAdminSessionLocal;
 import org.ejbca.core.ejb.ca.caadmin.ICAAdminSessionLocalHome;
-import org.ejbca.core.ejb.log.LogConfigurationDataLocal;
-import org.ejbca.core.ejb.log.LogConfigurationDataLocalHome;
 import org.ejbca.core.model.authorization.AdminGroup;
 import org.ejbca.core.model.log.Admin;
 import org.ejbca.util.JDBCUtil;
 import org.ejbca.util.SqlExecutor;
-
-import se.anatom.ejbca.log.OldLogConfigurationDataLocal;
-import se.anatom.ejbca.log.OldLogConfigurationDataLocalHome;
 
 /** The upgrade session bean is used to upgrade the database between ejbca releases.
  *
@@ -124,9 +118,7 @@ public class UpgradeSessionBean extends BaseSessionBean {
     /** Var holding JNDI name of datasource */
     private String dataSource = "";
 
-    private  OldLogConfigurationDataLocalHome oldLogHome = null;
-    private  LogConfigurationDataLocalHome logHome = null;
-    /** The local interface of the ca admin session bean */
+    /** The local interface of the CA Admin session bean */
     private ICAAdminSessionLocal caadminsession = null;
     private Admin administrator = null;
     
@@ -137,9 +129,6 @@ public class UpgradeSessionBean extends BaseSessionBean {
      */
     public void ejbCreate() throws CreateException {
         dataSource = getLocator().getString(JNDINames.DATASOURCE);
-        logHome = (LogConfigurationDataLocalHome)ServiceLocator.getInstance().getLocalHome(LogConfigurationDataLocalHome.COMP_NAME);
-        oldLogHome = (OldLogConfigurationDataLocalHome)ServiceLocator.getInstance().getLocalHome(OldLogConfigurationDataLocalHome.COMP_NAME);
-        
     }
 
     /** 
@@ -228,29 +217,12 @@ public class UpgradeSessionBean extends BaseSessionBean {
 
         // Upgrade database change between ejbca 3.1.x and 3.2.x if needed
         if (oldVersion <= 301) {
-            if (!preCheck()) {
-            	info("preCheck failed, no upgrade performed.");
-                return false;
-            }
-            if (!migrateDatabase31(dbtype)) {
-            	return false;
-            }
-            if (!upgradeHardTokenClassPath()) {
-            	return false;
-            }
-            if (!upgradeUserDataVO()) {
-                return false;
-            }
-            ArrayList datas = logConfStep1();
-            // If we got some datas, we have something to upgrade, otherwise we don't
-            if (datas != null) {
-                if (!logConfStep2(datas)) {
-                    return false;
-                }
-                if (!logConfStep3(datas)) {
-                    return false;
-                }
-            }
+        	error("Upgrade from EJBCA 3.1.x is no longer supported in EJBCA 3.9.x and later.");
+        	return false;
+        }
+        if (!preCheck()) {
+         	info("preCheck failed, no upgrade performed.");
+            return false;
         }
         // Upgrade database change between ejbca 3.3.x and 3.4.x if needed
         if (oldVersion <= 303) {
@@ -306,17 +278,6 @@ public class UpgradeSessionBean extends BaseSessionBean {
         return true;
 	}
 
-    /** 
-     * @ejb.interface-method
-     * @jboss.method-attributes transaction-timeout="3600"
-     * 
-     */
-	public boolean migrateDatabase31(String dbtype) {
-		error("(this is not an error) Starting upgrade from ejbca 3.1.x to ejbca 3.2.x");
-		boolean ret = migradeDatabase("/31_32/31_32-upgrade-"+dbtype+".sql");
-        error("(this is not an error) Finished migrating database.");
-        return ret;
-	}
     /** 
      * @ejb.interface-method
      * @jboss.method-attributes transaction-timeout="3600"
@@ -416,186 +377,4 @@ public class UpgradeSessionBean extends BaseSessionBean {
         error("(this is not an error) Finished migrating database.");
         return ret;
 	}
-
-	/** 
-     * @ejb.interface-method
-     * @jboss.method-attributes transaction-timeout="3600"
-     * 
-     */
-	public boolean upgradeHardTokenClassPath() {
-		try {
-			ICAAdminSessionLocal casession = getCaAdminSession(); 
-	        Collection caids = casession.getAvailableCAs(administrator);
-	        Iterator iter = caids.iterator();
-	        while (iter.hasNext()) {
-	            int caid = ((Integer) iter.next()).intValue();
-	            casession.upgradeFromOldCAHSMKeyStore(administrator, caid);
-	        }			
-		} catch (Exception e) {
-			error("Error upgrading hard token class path: ", e);
-			return false;
-		}
-        return true;
-	}
-
-	/** 
-     * @ejb.interface-method
-     * @jboss.method-attributes transaction-timeout="3600"
-     * 
-     */
-    public ArrayList logConfStep1() {
-        ArrayList datas = new ArrayList();
-        try {
-            Collection oldColl = oldLogHome.findAll();
-            Iterator it = oldColl.iterator();
-            while (it.hasNext()) {
-                OldLogConfigurationDataLocal odata = (OldLogConfigurationDataLocal)it.next();
-                LogConfData d = new LogConfData();
-                d.id = odata.getId();
-                d.data = odata.getLogConfiguration();
-                d.row = odata.getLogEntryRowNumber();
-                datas.add(d);
-            }
-            error("(this is not an error) read "+datas.size()+" old LogConfigurationData.");
-        } catch (Exception e) {
-            error("Error reading old LogConfigurationData: ", e);
-            return null;
-        }
-        return datas;
-    }
-
-    /** 
-     * @ejb.interface-method
-     * @jboss.method-attributes transaction-timeout="3600"
-     * 
-     */
-    public boolean logConfStep2(ArrayList datas) {
-        try {
-            Iterator it2 = datas.iterator();
-            while (it2.hasNext()) {
-                LogConfData d = (LogConfData) it2.next();
-                OldLogConfigurationDataLocal l = oldLogHome.findByPrimaryKey(d.id);
-                oldLogHome.remove(l.getPrimaryKey());
-            }
-            error("(this is not an error) deleted old LogConfigurationData.");
-        } catch (Exception e) {
-            error("Failed to delete old LogConfigurationData");
-            return false;
-        }
-        return true;
-    }
-    /** 
-     * @ejb.interface-method
-     * @jboss.method-attributes transaction-timeout="3600"
-     * 
-     */
-    public boolean logConfStep3(ArrayList datas) {
-        try {
-            // Start creating the new ones
-            Iterator it2 = datas.iterator();
-            error("(this is not an error) Upgrading "+datas.size()+" LogConfigurationData.");
-            while (it2.hasNext()) {
-                LogConfData d = (LogConfData)it2.next();
-                se.anatom.ejbca.log.LogConfiguration logConf = d.data;
-                org.ejbca.core.model.log.LogConfiguration newLog = new org.ejbca.core.model.log.LogConfiguration(
-                logConf.useLogDB(), logConf.useExternalLogDevices(), logConf.getConfigurationData());
-                logHome.create(d.id, newLog);
-                LogConfigurationDataLocal dl = logHome.findByPrimaryKey(d.id);
-                dl.setLogEntryRowNumber(d.row);
-            }
-            error("(this is not an error) Upgraded LogConfigurationData successfully.");
-        } catch (Exception e) {
-            error("Error upgrading LogConfigurationData: ", e);  
-            return false;
-        } 
-        return true;
-    }
-    
-    private boolean upgradeUserDataVO() {
-        PreparedStatement ps1 = null;
-        PreparedStatement ps2 = null;
-        Connection con = null;
-        int count = 0;
-        try {
-            con = JDBCUtil.getDBConnection(JNDINames.DATASOURCE);
-            ps1 = con.prepareStatement("select fingerprint,userDataVO from CertReqHistoryData");
-            ps2 = con.prepareStatement("update CertReqHistoryData set userDataVO=? where fingerprint=?");
-            ResultSet rs = ps1.executeQuery();
-            se.anatom.ejbca.common.UserDataVO oldud = null;
-            while (rs.next()) {
-                boolean goon = true;
-                String fp = rs.getString(1);
-                String ud = rs.getString(2);
-                try {
-                    java.beans.XMLDecoder decoder = null;
-                    try {
-                      decoder = new java.beans.XMLDecoder(new java.io.ByteArrayInputStream(ud.getBytes("UTF8")));
-                      oldud  = (se.anatom.ejbca.common.UserDataVO)decoder.readObject();                          
-                      decoder.close();
-                    } catch (UnsupportedEncodingException e) {
-                        goon = false;
-                    }
-                } catch (Exception e) {
-                    error("Can not decode old UserDataVO for fingerprint "+fp+": ", e);
-                    goon = false;
-                }
-                if (goon) {
-                    org.ejbca.core.model.ra.UserDataVO newud = createNewUserDataVO(oldud);
-                    ByteArrayOutputStream baos = null; 
-                    try {
-                        baos = new java.io.ByteArrayOutputStream();
-                        java.beans.XMLEncoder encoder = new java.beans.XMLEncoder(baos);
-                        encoder.writeObject(newud);
-                        encoder.close();
-                        String newudstr = baos.toString("UTF8");
-                        ps2.setString(1, newudstr);
-                        ps2.setString(2, fp);
-                        ps2.executeUpdate();                                            
-                        count ++;
-                    } catch (Exception e) {
-                        error("Can not create new UserDataVO for fingerprint "+fp+": ", e);
-                    } finally {
-                        try {
-                            if (baos != null) {
-                            	baos.close();                            
-                            }
-                        } catch (Exception e) {} 
-                    }
-                }
-                if ( (count % 1000) == 0) {
-                    error("(this is not an error) migrated "+count+" UserDataVO");
-                }
-            }
-            //con.commit();
-        } catch (SQLException sqle) {
-            error("Error updating CertReqHistoryData: ", sqle);
-            return false;
-        } finally {
-            JDBCUtil.close(ps1);
-            JDBCUtil.close(ps2);
-            JDBCUtil.close(con);
-        }
-        error("(this is not an error) migrated "+count+" UserDataVO");
-        return true;
-    }
-    
-    private org.ejbca.core.model.ra.UserDataVO createNewUserDataVO(se.anatom.ejbca.common.UserDataVO old) {
-        org.ejbca.core.model.ra.UserDataVO ret = new org.ejbca.core.model.ra.UserDataVO(
-                old.getUsername(), old.getDN(), old.getCAId(),
-                old.getSubjectAltName(),old.getEmail(),old.getStatus(),
-                old.getType(),old.getEndEntityProfileId(),old.getCertificateProfileId(),
-                old.getTimeCreated(),old.getTimeModified(),old.getTokenType(),
-                old.getHardTokenIssuerId(),old.getExtendedinformation()
-                );
-        return ret;
-    }
-    /**
-     * Enum type to hold old logconfigurationdatalocal
-     *
-     */
-    private class LogConfData {
-        public Integer id;
-        public se.anatom.ejbca.log.LogConfiguration data;
-        public int row;
-    }
-} // UpgradeSessionBean
+}
--- src/upgrade/31_32/31_32-upgrade-mysql.sql
+++
@@ -1,3 +0,0 @@
-
-# Format of column changed, no information was stored here earlier though
-alter table UserData modify extendedInformationData LONGTEXT;
