e4c03fce324c381cad3d5ea246028e42205da9ac
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2009-04-07 13:52:12 +0000

    ECA-1169: for ws cli, separate bcprov and other libs from the "endorsed" libs like in clienttoolBox. this fixes issue with JCE can not be verified using PKCS12req.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@7251 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/jaxws/cli/cvcwscli.sh
+++ src/jaxws/cli/cvcwscli.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Djava.endorsed.dirs=lib -cp ejbcawscli.jar org.ejbca.core.protocol.ws.client.cvcwscli "$@"
+java -Djava.endorsed.dirs=lib/endorsed -cp ejbcawscli.jar org.ejbca.core.protocol.ws.client.cvcwscli "$@"
