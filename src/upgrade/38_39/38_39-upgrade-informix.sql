033b672aaa93e9bfd7804f2de01a9bc19c531400
Johan Eklund <johan.eklund@primekey.com>
2009-05-04 22:09:14 +0000

    ECA-1198: Use more general SQL for upgrade.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@7370 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/upgrade/38_39/38_39-upgrade-informix.sql
+++ src/upgrade/38_39/38_39-upgrade-informix.sql
@@ -1,5 +1,5 @@
 ALTER TABLE CertificateData ADD tag VARCHAR(255,0) DEFAULT NULL;
 update UserData set certificateProfileId=9 where username='tomcat' and certificateProfileId=1;
 ALTER TABLE CertificateData ADD certificateProfileId INTEGER DEFAULT NULL;
-UPDATE CertificateData, UserData SET CertificateData.certificateProfileId=UserData.certificateProfileId WHERE CertificateData.username=UserData.username;
+UPDATE CertificateData SET certificateProfileId=(SELECT certificateProfileId FROM UserData WHERE CertificateData.username=UserData.username);
 ALTER TABLE CertificateData ADD updateTime NUMERIC(18,0) NOT NULL DEFAULT 0;
--- src/upgrade/38_39/38_39-upgrade-mysql.sql
+++ src/upgrade/38_39/38_39-upgrade-mysql.sql
@@ -1,5 +1,5 @@
 ALTER TABLE CertificateData ADD tag VARCHAR(250) BINARY DEFAULT NULL;
 update UserData set certificateProfileId=9 where username='tomcat' and certificateProfileId=1;
 ALTER TABLE CertificateData ADD certificateProfileId int(11) DEFAULT NULL;
-UPDATE CertificateData, UserData SET CertificateData.certificateProfileId=UserData.certificateProfileId WHERE CertificateData.username=UserData.username;
+UPDATE CertificateData SET certificateProfileId=(SELECT certificateProfileId FROM UserData WHERE CertificateData.username=UserData.username);
 ALTER TABLE CertificateData ADD updateTime bigint(20) NOT NULL DEFAULT 0;
