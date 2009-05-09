7a7e743bbcd1ec36c6764811decf1227251459c6
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2009-05-09 09:37:18 +0000

    ECA-1198: new upgrade SQL for certificateProfileId in CertifcateData, be more conservative with sql where I can't test...
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@7449 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/upgrade/38_39/38_39-upgrade-hsqldb.sql
+++ src/upgrade/38_39/38_39-upgrade-hsqldb.sql
@@ -1,5 +1,6 @@
-ALTER TABLE CertificateData ADD COLUMN tag VARCHAR(256) DEFAULT NULL;
 update UserData set certificateProfileId=9 where username='tomcat' and certificateProfileId=1;
+ALTER TABLE CertificateData ADD COLUMN tag VARCHAR(256) DEFAULT NULL;
 ALTER TABLE CertificateData ADD COLUMN certificateProfileId INTEGER DEFAULT 0;
-update CertificateData c, UserData u set c.certificateProfileId=u.certificateProfileId where c.username=u.username;
 ALTER TABLE CertificateData ADD COLUMN updateTime BIGINT DEFAULT 0 NOT NULL;
+UPDATE CertificateData SET certificateProfileId=(SELECT certificateProfileId FROM UserData WHERE CertificateData.username=UserData.username);
+UPDATE CertificateData SET certificateProfileId=0 where certificateProfileId is null; 
--- src/upgrade/38_39/38_39-upgrade-postgres.sql
+++ src/upgrade/38_39/38_39-upgrade-postgres.sql
@@ -1,5 +1,6 @@
-ALTER TABLE CertificateData ADD tag TEXT DEFAULT NULL;
 update UserData set certificateProfileId=9 where username='tomcat' and certificateProfileId=1;
+ALTER TABLE CertificateData ADD tag TEXT DEFAULT NULL;
 ALTER TABLE CertificateData ADD certificateProfileId INT4 DEFAULT 0;
-update CertificateData c, UserData u set c.certificateProfileId=u.certificateProfileId where c.username=u.username;
 ALTER TABLE CertificateData ADD updateTime INT8 NOT NULL DEFAULT 0;
+UPDATE CertificateData SET certificateProfileId=(SELECT certificateProfileId FROM UserData WHERE CertificateData.username=UserData.username);
+UPDATE CertificateData SET certificateProfileId=0 where certificateProfileId is null;
