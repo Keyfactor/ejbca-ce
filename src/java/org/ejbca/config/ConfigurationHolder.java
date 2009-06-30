7e2212b41ee496562230cd57b4a87a1f6fce8683
primelars <USER@primekey.com>
2009-06-30 13:08:07 +0000

    ECA-1299: configuration moved jaxws.properties
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@7764 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/config/ConfigurationHolder.java
+++ src/java/org/ejbca/config/ConfigurationHolder.java
@@ -50,7 +50,7 @@ public class ConfigurationHolder {
 	private ConfigurationHolder() {}
 	
 	/** ejbca.properties must be first in this file, because CONFIGALLOWEXTERNAL is defined in there. */
-	public static final String[] CONFIG_FILES = {"ejbca.properties", "web.properties", "cmp.properties", "externalra-caservice.properties", "ocsp.properties", "extendedkeyusage.properties"};
+	public static final String[] CONFIG_FILES = {"ejbca.properties", "web.properties", "cmp.properties", "externalra-caservice.properties", "ocsp.properties", "extendedkeyusage.properties", "jaxws.properties"};
 
 	/** Configuration property that enables dynamic reading of properties from the file system. This is not allowed by default for security reasons. */
 	public static final String CONFIGALLOWEXTERNAL = "allow.external-dynamic.configuration";
