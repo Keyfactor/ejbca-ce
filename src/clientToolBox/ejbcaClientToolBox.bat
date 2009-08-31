77b749a36eb9f9aff81147f5108e92a66d704a08
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2009-08-31 07:25:22 +0000

    ECA-1379: use shift operator to be able to read 10 parameters from input
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@7945 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/clientToolBox/ejbcaClientToolBox.bat
+++ src/clientToolBox/ejbcaClientToolBox.bat
@@ -2,4 +2,20 @@ rem Starting ctb using a Windows .BAT file
 rem change line below to suit your setup
 set TOOLBOX_HOME=".\clientToolBox-dist"
 set CLASSES=%TOOLBOX_HOME%\clientToolBox.jar
-java -Djava.endorsed.dirs=%TOOLBOX_HOME%/endorse;%TOOLBOX_HOME%/lib -cp %CLASSES% org.ejbca.ui.cli.ClientToolBox %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+rem Fixup arguments, we have to do this since windows normally only 
+rem supports %1-%9 as command line arguments
+set a=%1
+set b=%2
+set c=%3
+set d=%4
+set e=%5
+set f=%6
+set g=%7
+set h=%8
+set i=%9
+shift
+set j=%9
+rem echo %a% %b% %c% %d% %e% %f% %g% %h% %i% %j%
+
+java -Djava.endorsed.dirs=%TOOLBOX_HOME%/endorse;%TOOLBOX_HOME%/lib -cp %CLASSES% org.ejbca.ui.cli.ClientToolBox %a% %b% %c% %d% %e% %f% %g% %h% %i% %j%
