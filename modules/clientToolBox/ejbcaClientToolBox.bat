4d5386a7d27cdb9357b2c607d2744dd4b5125c51
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2009-10-12 15:32:17 +0000

    ECA-1431: fox ejbcaClientToolBox.bat, fix from Adam Lorentzon.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@8104 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/clientToolBox/ejbcaClientToolBox.bat
+++ modules/clientToolBox/ejbcaClientToolBox.bat
@@ -1,7 +1,9 @@
+@echo off
 rem Starting ctb using a Windows .BAT file
-rem change line below to suit your setup
-set TOOLBOX_HOME=".\clientToolBox-dist"
-set CLASSES=%TOOLBOX_HOME%\clientToolBox.jar
+set TOOLBOX_HOME=%~dp0
+set CLASSES=%TOOLBOX_HOME%clientToolBox.jar
+SETLOCAL ENABLEDELAYEDEXPANSION
+FOR %%J IN ("%TOOLBOX_HOME%lib\*.jar") DO set CLASSES=!CLASSES!;%%J
 
 rem Fixup arguments, we have to do this since windows normally only 
 rem supports %1-%9 as command line arguments
@@ -18,4 +20,4 @@ shift
 set j=%9
 rem echo %a% %b% %c% %d% %e% %f% %g% %h% %i% %j%
 
-java -Djava.endorsed.dirs=%TOOLBOX_HOME%/endorse;%TOOLBOX_HOME%/lib -cp %CLASSES% org.ejbca.ui.cli.ClientToolBox %a% %b% %c% %d% %e% %f% %g% %h% %i% %j%
+java -Djava.endorsed.dirs=%TOOLBOX_HOME%endorsed -cp %CLASSES% org.ejbca.ui.cli.ClientToolBox %a% %b% %c% %d% %e% %f% %g% %h% %i% %j%
