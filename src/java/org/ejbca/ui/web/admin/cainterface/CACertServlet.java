f179591128d05e7768063164d83a0de276e41d60
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2010-01-04 14:10:29 +0000

    ECA-1495: correct the ejb-ref link to CreateCRLSession
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@8498 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/ui/web/admin/cainterface/CACertServlet.java
+++ src/java/org/ejbca/ui/web/admin/cainterface/CACertServlet.java
@@ -193,6 +193,13 @@ import org.ejbca.util.CertTools;
  *  home="org.ejbca.core.ejb.ca.publisher.IPublisherQueueSessionLocalHome"
  *  local="org.ejbca.core.ejb.ca.publisher.IPublisherQueueSessionLocal"
  *  
+ * @web.ejb-local-ref
+ *  name="ejb/CreateCRLSessionLocal"
+ *  type="Session"
+ *  link="CreateCRLSession"
+ *  home="org.ejbca.core.ejb.ca.crl.ICreateCRLSessionLocalHome"
+ *  local="org.ejbca.core.ejb.ca.crl.ICreateCRLSessionLocal"
+ *  
  * @web.resource-ref
  *  name="${datasource.jndi-name-prefix}${datasource.jndi-name}"
  *  type="javax.sql.DataSource"
