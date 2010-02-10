228f9e77fbe3f8220cdaa1fa97a8a163d69aaf1f
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2010-02-10 13:13:56 +0000

    organize imports
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@8641 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/core/ejb/ra/LocalCertificateRequestSessionBean.java
+++ src/java/org/ejbca/core/ejb/ra/LocalCertificateRequestSessionBean.java
@@ -30,9 +30,6 @@ import java.security.spec.KeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Enumeration;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.xml.ws.WebServiceContext;
-import javax.xml.ws.handler.MessageContext;
 import javax.ejb.CreateException;
 import javax.ejb.EJBException;
 import javax.ejb.FinderException;
@@ -43,7 +40,6 @@ import org.bouncycastle.asn1.ASN1Sequence;
 import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
 import org.bouncycastle.jce.netscape.NetscapeCertRequest;
 import org.ejbca.core.EjbcaException;
-import org.ejbca.core.ErrorCode;
 import org.ejbca.core.ejb.BaseSessionBean;
 import org.ejbca.core.ejb.ServiceLocatorException;
 import org.ejbca.core.ejb.authorization.IAuthorizationSessionLocal;
