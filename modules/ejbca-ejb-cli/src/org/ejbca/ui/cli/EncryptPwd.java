78471346f3def35f6307a32bc9e17b2bbc5a2040
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2010-03-16 09:13:21 +0000

    ECA-1599: don't echo passwords when "encrypting" them
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@8748 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb-cli/src/org/ejbca/ui/cli/EncryptPwd.java
+++ modules/ejbca-ejb-cli/src/org/ejbca/ui/cli/EncryptPwd.java
@@ -13,10 +13,8 @@
  
 package org.ejbca.ui.cli;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-import org.ejbca.util.CertTools;
+import org.ejbca.ui.cli.util.ConsolePasswordReader;
+import org.ejbca.util.CryptoProviderTools;
 import org.ejbca.util.StringTools;
 
 /**
@@ -34,11 +32,10 @@ public class EncryptPwd extends BaseCommand {
     	try {
     		getLogger().info("Please note that this encryption does not provide absolute security, it uses a build in key for encryption to keep the password from at least accidentaly beeing known.");
     		getLogger().info("Enter word to encrypt: ");
-    		InputStreamReader isr = new InputStreamReader ( System.in );
-    		BufferedReader br = new BufferedReader ( isr );
-    		String s = br.readLine();
-    		CertTools.installBCProvider();
-    		getLogger().info("Encrypting pwd '"+s+"'");
+    		ConsolePasswordReader r = new ConsolePasswordReader();
+    		String s = String.valueOf(r.readPassword());
+    		CryptoProviderTools.installBCProvider();
+    		getLogger().info("Encrypting pwd...");
             String enc = StringTools.pbeEncryptStringWithSha256Aes192(s);
             getLogger().info(enc);
     	} catch (Exception e) {
