c7e053d2cf7f8053ea0b16b5a436d9e1ed7a6cc3
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2010-03-17 10:09:10 +0000

    improve privileges script with more limitations and complete revoke, resulting sql script now applies fine.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@8756 37aeb1de-b14b-0410-9a0e-82b28170d228

--- doc/howto/mysql-privileges.sh
+++ doc/howto/mysql-privileges.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+# This script will create an SQL file that you can inspect and apply to your database.
+# apply with 'mysql -u root -p < mysql-database-privileges.sql'
+
+# root user used to create this script
+SQLROOTUSER=root
+# The user that we should revoke and grant privileges from
 SQLUSER=ejbca
 SQLHOST=localhost
+# The database that SQLUSER will have access to
 SQLDATABASE=ejbca
+# The output file where resulting the sql script will be written 
 SQLFILE=mysql-database-privileges.sql
 
-echo "Accessing database \"$SQLDATABASE\" as user \"$SQLUSER\"."
-COMMAND=`echo "show tables" | mysql -u $SQLUSER -p $SQLDATABASE | grep -v Tables_in_ejbca`
-echo "revoke ALL on $SQLDATABASE.* from '$SQLUSER'@'$SQLHOST';" > $SQLFILE
-for table in $COMMAND; do echo "revoke ALL on $SQLDATABASE.$table from '$SQLUSER'@'$SQLHOST';"; done >> $SQLFILE
-for table in $COMMAND; do echo "grant ALL on $SQLDATABASE.$table to '$SQLUSER'@'$SQLHOST';"; done >> $SQLFILE
+echo "Accessing database \"$SQLDATABASE\" as user \"$SQLROOTUSER\"."
+COMMAND=`echo "show tables" | mysql -u $SQLROOTUSER -p $SQLDATABASE | grep -v Tables_in_ejbca`
+echo "revoke ALL PRIVILEGES, GRANT OPTION from '$SQLUSER'@'$SQLHOST';" > $SQLFILE
+for table in $COMMAND; do echo "grant SELECT,INSERT,UPDATE,DELETE,INDEX on $SQLDATABASE.$table to '$SQLUSER'@'$SQLHOST';"; done >> $SQLFILE
 for table in $COMMAND; do echo "revoke ALL on $SQLDATABASE.$table from '$SQLUSER'@'$SQLHOST';" | grep -i ProtectedLog | grep -iv ProtectedLogExportData; done >> $SQLFILE
-for table in $COMMAND; do echo "grant SELECT,INSERT,CREATE on $SQLDATABASE.$table to '$SQLUSER'@'$SQLHOST';" | grep -i ProtectedLog | grep -iv ProtectedLogExportData; done >> $SQLFILE
+for table in $COMMAND; do echo "grant SELECT,INSERT on $SQLDATABASE.$table to '$SQLUSER'@'$SQLHOST';" | grep -i ProtectedLog | grep -iv ProtectedLogExportData; done >> $SQLFILE
 for table in $COMMAND; do echo "revoke ALL on $SQLDATABASE.$table from '$SQLUSER'@'$SQLHOST';" | grep -i LogEntryData; done >> $SQLFILE
-for table in $COMMAND; do echo "grant SELECT,INSERT,CREATE on $SQLDATABASE.$table to '$SQLUSER'@'$SQLHOST';" | grep -i LogEntryData; done >> $SQLFILE
+for table in $COMMAND; do echo "grant SELECT,INSERT on $SQLDATABASE.$table to '$SQLUSER'@'$SQLHOST';" | grep -i LogEntryData; done >> $SQLFILE
