3759a968f9abdcb4d65bab7a6b973f867bbebdfa
aveen4711 <USER@primekey.com>
2010-06-15 07:04:43 +0000

    ECA-1693: Adding javadoc
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@9202 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/core/ejb/ra/userdatasource/CustomFieldException.java
+++ src/java/org/ejbca/core/ejb/ra/userdatasource/CustomFieldException.java
@@ -1,5 +1,24 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
 package org.ejbca.core.ejb.ra.userdatasource;
 
+/**
+ * Exception is cast when values of an end entity field does not match a specific rule in the function FieldValidator.validate()
+ *  
+ * @author aveen
+ *
+ */
 public class CustomFieldException extends Exception {
 
 	private static final long serialVersionUID = -4270699717178908309L;
