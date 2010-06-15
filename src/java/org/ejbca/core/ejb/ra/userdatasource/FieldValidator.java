0fbb451939c4b61b23cb8adeeafafd32373c2350
aveen4711 <USER@primekey.com>
2010-06-15 07:04:43 +0000

    ECA-1693: Adding javadoc
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@9202 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/core/ejb/ra/userdatasource/FieldValidator.java
+++ src/java/org/ejbca/core/ejb/ra/userdatasource/FieldValidator.java
@@ -1,3 +1,16 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
 package org.ejbca.core.ejb.ra.userdatasource;
 
 import org.ejbca.core.model.ra.UserDataVO;
@@ -13,7 +26,15 @@ import org.ejbca.util.dn.DnComponents;
 public class FieldValidator {
 	
 	public FieldValidator(){}
-	
+
+	/**
+	 * The rules and regulations of the contents of End Entity fields should be implemented here.
+	 * 
+	 * @param userdata object containing all the possible fields of an end entity
+	 * @param profileid of the end entity profile
+	 * @param profilename of the end entity profile
+	 * @throws CustomFieldException when the value of a field does not match a specific rule.
+	 */
 	public static void validate(UserDataVO userdata, int profileid, String profilename) throws CustomFieldException{}
 
 }
