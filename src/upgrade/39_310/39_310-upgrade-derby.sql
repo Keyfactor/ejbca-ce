66a503dd55b8186c7702c9f6f4e3540778563f1c
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2010-06-16 13:36:11 +0000

    add docs about upgrading older versions of derby
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@9234 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/upgrade/39_310/39_310-upgrade-derby.sql
+++ src/upgrade/39_310/39_310-upgrade-derby.sql
@@ -1,3 +1,6 @@
+-- If you are using an older version of Derby, such as 10.2, you need to drop and re-create this table. See doc/howto/create-tables-ejbca3-derby.sql for drop/create statements.
+-- this is because Derby 10.2 does not support rename column or drop column
 RENAME COLUMN LogEntryData.comment TO logComment;
+-- If you are using an older version of Derby, such as 10.2, you need to drop and re-create this table. See doc/howto/create-tables-ejbca3-derby.sql for drop/create statements.
 ALTER TABLE TableProtectData DROP COLUMN keyRef;
 ALTER TABLE CertificateData ADD COLUMN subjectKeyId VARCHAR(256) WITH DEFAULT NULL;
