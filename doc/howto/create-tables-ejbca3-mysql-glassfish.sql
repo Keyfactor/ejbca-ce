169cab6c31e087df7bda0d96fa6635621b64f10f
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2010-07-04 18:00:40 +0000

    ECA-1729: sql and schema files for MySQL for Glassfish
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@9382 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ doc/howto/create-tables-ejbca3-mysql-glassfish.sql
@@ -0,0 +1,350 @@
+--
+-- these definitions should work for ejbca 3.8.x, mysql 4.x or 5.x.
+--
+
+drop table if exists accessrulesdata;
+create table accessrulesdata (
+    pk int(11) not null default '0',
+    accessrule varchar(250) binary null default null,
+    rule int(11) not null default '0',
+    isrecursive tinyint(4) not null default '0',
+    `admingroupdata_accessrules` int(11) null default null,
+    primary key (pk)
+);
+
+drop table if exists adminentitydata;
+create table adminentitydata (
+    pk int(11) not null default '0',
+    matchwith int(11) not null default '0',
+    matchtype int(11) not null default '0',
+    matchvalue varchar(250) binary null default null,
+    `admingroupdata_adminentities` int(11) null default null,
+    caid int(11) not null default '0',
+    primary key (pk)
+);
+
+drop table if exists admingroupdata;
+create table admingroupdata (
+    pk int(11) not null default '0',
+    admingroupname varchar(250) binary null default null,
+    caid int(11) not null default '0',
+    primary key (pk)
+);
+
+drop table if exists adminpreferencesdata;
+create table adminpreferencesdata (
+    id varchar(250) binary not null default '',
+    data longblob null default null,
+    primary key (id)
+);
+
+drop table if exists approvaldata;
+create table approvaldata (
+    id int(11) not null default '0',
+    approvalid int(11) not null default '0',
+    approvaltype int(11) not null default '0',
+    endentityprofileid int(11) not null default '0',
+    caid int(11) not null default '0',
+    reqadmincertissuerdn varchar(250) binary null default null,
+    reqadmincertsn varchar(250) binary null default null,
+    status int(11) not null default '0',
+    approvaldata longtext null default null,
+    requestdata longtext null default null,    
+    requestdate bigint(20) not null default '0',
+    expiredate bigint(20) not null default '0',    
+    remainingapprovals int(11) not null default '0',
+    primary key (id)
+);
+
+drop table if exists authorizationtreeupdatedata;
+create table authorizationtreeupdatedata (
+    pk int(11) not null default '0',
+    authorizationtreeupdatenumber int(11) not null default '0',
+    primary key (pk)
+);
+
+drop table if exists cadata;
+create table cadata (
+    caid int(11) not null default '0',
+    name varchar(250) binary null default null,
+    subjectdn varchar(250) binary null default null,
+    status int(11) not null default '0',
+    expiretime bigint(20) not null default '0',
+    updatetime bigint(20) not null default '0',
+    data text null default null,
+    primary key (caid)
+);
+
+drop table if exists crldata;
+create table crldata (
+    fingerprint varchar(250) binary not null default '',
+    crlnumber int(11) not null default '0',
+    issuerdn varchar(250) binary null default null,
+    cafingerprint varchar(250) binary null default null,
+    thisupdate bigint(20) not null default '0',
+    nextupdate bigint(20) not null default '0',
+    deltacrlindicator int(11) not null default '-1',
+    base64crl longtext null default null,
+    primary key (fingerprint)
+);
+
+drop table if exists certreqhistorydata;
+create table certreqhistorydata (
+    fingerprint varchar(250) binary not null default '',
+    issuerdn varchar(250) binary null default null,
+    serialnumber varchar(250) binary null default null,
+    `timestamp` bigint(20) not null default '0',
+    userdatavo longtext null default null,
+    username varchar(250) binary null default null,
+    primary key (fingerprint)
+);
+
+drop table if exists certificatedata;
+create table certificatedata (
+    fingerprint varchar(250) binary not null default '',
+    issuerdn varchar(250) binary null default null,
+    subjectdn varchar(250) binary null default null,
+    cafingerprint varchar(250) binary null default null,
+    status int(11) not null default '0',
+    type int(11) not null default '0',
+    serialnumber varchar(250) binary null default null,
+    expiredate bigint(20) not null default '0',
+    revocationdate bigint(20) not null default '0',
+    revocationreason int(11) not null default '0',
+    base64cert text null default null,
+    username varchar(250) binary null default null,
+    tag varchar(250) binary null default null,
+    certificateprofileid int(11) null default '0',
+    updatetime bigint(20) not null default '0',
+    subjectkeyid varchar(250) binary null default null,
+    primary key (fingerprint)
+);
+
+drop table if exists certificateprofiledata;
+create table certificateprofiledata (
+    id int(11) not null default '0',
+    certificateprofilename varchar(250) binary null default null,
+    data longblob null default null,
+    primary key (id)
+);
+
+drop table if exists endentityprofiledata;
+create table endentityprofiledata (
+    id int(11) not null default '0',
+    profilename varchar(250) binary null default null,
+    data longblob null default null,
+    primary key (id)
+);
+
+drop table if exists globalconfigurationdata;
+create table globalconfigurationdata (
+    configurationid varchar(250) binary not null default '',
+    data longblob null default null,
+    primary key (configurationid)
+);
+
+drop table if exists hardtokencertificatemap;
+create table hardtokencertificatemap (
+    certificatefingerprint varchar(250) binary not null default '',
+    tokensn varchar(250) binary null default null,
+    primary key (certificatefingerprint)
+);
+
+drop table if exists hardtokendata;
+create table hardtokendata (
+    tokensn varchar(250) binary not null default '',
+    username varchar(250) binary null default null,
+    ctime bigint(20) not null default '0',
+    mtime bigint(20) not null default '0',
+    tokentype int(11) not null default '0',
+    significantissuerdn varchar(250) binary null default null,
+    data longblob null default null,
+    primary key (tokensn)
+);
+
+drop table if exists hardtokenissuerdata;
+create table hardtokenissuerdata (
+    id int(11) not null default '0',
+    alias varchar(250) binary null default null,
+    admingroupid int(11) not null default '0',
+    data longblob null default null,
+    primary key (id)
+);
+
+drop table if exists hardtokenprofiledata;
+create table hardtokenprofiledata (
+    id int(11) not null default '0',
+    name varchar(250) binary null default null,
+    updatecounter int(11) not null default '0',
+    data longtext null default null,
+    primary key (id)
+);
+
+drop table if exists hardtokenpropertydata;
+create table hardtokenpropertydata (
+    id varchar(250) binary not null default '',
+    property varchar(250) binary not null default '',
+    value varchar(250) binary null default null,
+    primary key (id, property)
+);
+
+drop table if exists keyrecoverydata;
+create table keyrecoverydata (
+    certsn varchar(250) binary not null default '',
+    issuerdn varchar(250) binary not null default '',
+    username varchar(250) binary null default null,
+    markedasrecoverable tinyint(4) not null default '0',
+    keydata text null default null,
+    primary key (certsn, issuerdn)
+);
+
+drop table if exists logconfigurationdata;
+create table logconfigurationdata (
+    id int(11) not null default '0',
+    logconfiguration longblob null default null,
+    logentryrownumber int(11) not null default '0',
+    primary key (id)
+);
+
+drop table if exists logentrydata;
+create table logentrydata (
+    id int(11) not null default '0',
+    admintype int(11) not null default '0',
+    admindata varchar(250) binary null default null,
+    caid int(11) not null default '0',
+    module int(11) not null default '0',
+    `time` bigint(20) not null default '0',
+    username varchar(250) binary null default null,
+    certificatesnr varchar(250) binary null default null,
+    event int(11) not null default '0',
+    logcomment varchar(250) binary null default null,
+    primary key (id)
+);
+
+drop table if exists protectedlogdata;
+create table protectedlogdata (
+	pk varchar(250) binary not null default '',
+	admintype int(11) not null default '0',
+	admindata varchar(250) binary null default null,
+    caid int(11) not null default '0',
+    module int(11) not null default '0',
+    eventtime bigint(20) not null default '0',
+    username varchar(250) binary null default null,
+    certificateserialnumber varchar(250) binary null default null,
+    certificateissuerdn varchar(250) binary null default null,
+    eventid int(11) not null default '0',
+    eventcomment text null default null,
+    nodeguid int(11) not null default '0',
+    counter bigint(20) not null default '0',
+    nodeip varchar(250) binary null default null,
+    b64linkedineventidentifiers text null default null,
+    b64linkedineventshash varchar(250) binary null default null,
+    currenthashalgorithm varchar(250) binary null default null,
+    protectionkeyidentifier int(11) not null default '0',
+    protectionkeyalgorithm varchar(250) binary null default null,
+    b64protection text null default null,
+    primary key (pk)
+);
+
+drop table if exists protectedlogexportdata;
+create table protectedlogexportdata (
+	pk varchar(250) binary not null default '',
+    timeofexport bigint(20) not null default '0',
+    exportendtime bigint(20) not null default '0',
+    exportstarttime bigint(20) not null default '0',
+    b64logdatahash varchar(250) binary null default null,
+    b64previosexporthash varchar(250) binary null default null,
+    currenthashalgorithm varchar(250) binary null default null,
+    b64signaturecertificate text null default null,
+    deleted tinyint(4) not null default '0',
+    b64signature text null default null,
+	primary key (pk)
+);
+
+drop table if exists protectedlogtokendata;
+create table protectedlogtokendata (
+	pk varchar(250) binary not null default '',
+    tokenidentifier int(11) not null default '0',
+    tokentype int(11) not null default '0',
+    b64tokencertificate text null default null,
+    tokenreference text null default null,
+    primary key (pk)
+);
+
+drop table if exists publisherdata;
+create table publisherdata (
+    id int(11) not null default '0',
+    name varchar(250) binary null default null,
+    updatecounter int(11) not null default '0',
+    data text null default null,
+    primary key (id)
+);
+
+drop table if exists publisherqueuedata;
+create table publisherqueuedata (
+    pk varchar(250) binary not null default '',
+    timecreated bigint(20) not null default '0',
+    lastupdate bigint(20) not null default '0',
+    publishstatus int(11) not null default '0',
+    trycounter int(11) not null default '0',
+    publishtype int(11) not null default '0',
+    fingerprint varchar(250) binary null default null,
+    publisherid int(11) not null default '0',
+    volatiledata text null default null,
+    primary key (pk)
+);
+
+drop table if exists servicedata;
+create table servicedata (
+    id int(11) not null default '0',
+    name varchar(250) binary null default null,
+    data text null default null,
+    primary key (id)
+);
+
+drop table if exists tableprotectdata;
+create table tableprotectdata (
+    id varchar(250) binary not null default '',
+    version int(11) not null default '0',
+    hashversion int(11) not null default '0',
+    protectionalg varchar(250) binary null default null,
+    hash varchar(250) binary null default null,
+    signature varchar(250) binary null default null,
+    time bigint(20) not null default '0',
+    dbkey varchar(250) binary null default null,
+    dbtype varchar(250) binary null default null,
+    keytype varchar(250) binary null default null,
+    primary key (id)
+);
+
+drop table if exists userdata;
+create table userdata (
+    username varchar(250) binary not null default '',
+    subjectdn varchar(250) binary null default null,
+    caid int(11) not null default '0',
+    subjectaltname varchar(250) binary null default null,
+    subjectemail varchar(250) binary null default null,
+    status int(11) not null default '0',
+    type int(11) not null default '0',
+    clearpassword varchar(250) binary null default null,
+    passwordhash varchar(250) binary null default null,
+    timecreated bigint(20) not null default '0',
+    timemodified bigint(20) not null default '0',
+    endentityprofileid int(11) not null default '0',
+    certificateprofileid int(11) not null default '0',
+    tokentype int(11) not null default '0',
+    hardtokenissuerid int(11) not null default '0',
+    extendedinformationdata longtext null default null,
+    keystorepassword varchar(250) binary null default null,
+    cardnumber varchar(19) binary null default null,
+    primary key (username)
+);
+
+drop table if exists userdatasourcedata;
+create table userdatasourcedata (
+    id int(11) not null default '0',
+    name varchar(250) binary null default null,
+    updatecounter int(11) not null default '0',
+    data text null default null,
+    primary key (id)
+);
