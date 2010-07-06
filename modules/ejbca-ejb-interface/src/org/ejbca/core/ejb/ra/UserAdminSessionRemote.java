e692cf03d14668077644a1ef2393c9aec4096a57
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2010-07-06 17:35:45 +0000

    update javadoc
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@9403 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb-interface/src/org/ejbca/core/ejb/ra/UserAdminSessionRemote.java
+++ modules/ejbca-ejb-interface/src/org/ejbca/core/ejb/ra/UserAdminSessionRemote.java
@@ -311,9 +311,9 @@ public interface UserAdminSessionRemote {
             throws org.ejbca.core.model.authorization.AuthorizationDeniedException, javax.ejb.FinderException, java.rmi.RemoteException;
 
     /**
-     * Decreases (the optional) request counter by 1, until it reaches 0.
-     * Returns the new value. If the value is already 0, -1 is returned, but the
-     * -1 is not stored in the database.
+     * Decreases (the optional) request counter by 1, until it reaches 0. Returns the new value. If the value is already 0, -1 is returned, but the 
+     * -1 is not stored in the database. 
+     * Also sets status of user to generated once the request counter reaches zero.
      * 
      * @param username
      *            the unique username.
@@ -321,6 +321,7 @@ public interface UserAdminSessionRemote {
      *            the new status, from 'UserData'.
      * @throws WaitingForApprovalException
      * @throws ApprovalException
+     * @throws FinderException if user does not exist 
      */
     public int decRequestCounter(org.ejbca.core.model.log.Admin admin, java.lang.String username)
             throws org.ejbca.core.model.authorization.AuthorizationDeniedException, javax.ejb.FinderException, org.ejbca.core.model.approval.ApprovalException,
