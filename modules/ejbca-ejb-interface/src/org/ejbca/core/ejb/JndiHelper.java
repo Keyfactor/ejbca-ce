642a094e1d3430b3a27d2529cf54743f5f5b09a3
Johan Eklund <johan.eklund@primekey.com>
2010-07-22 11:48:22 +0000

    ECA-1721: Added EJB 2.1 interfaces as a static library to allow compile of the all SSBs during migration.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@9486 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb-interface/src/org/ejbca/core/ejb/JndiHelper.java
+++ modules/ejbca-ejb-interface/src/org/ejbca/core/ejb/JndiHelper.java
@@ -13,6 +13,10 @@
 
 package org.ejbca.core.ejb;
 
+import javax.naming.Context;
+import javax.naming.NamingException;
+
+import org.apache.log4j.Logger;
 
 /**
  * The sole purpose of this class is to standardize mapping in JNDI of our Stateless Session Beans.
@@ -22,8 +26,34 @@ package org.ejbca.core.ejb;
  * 
  * @version $Id$
  */
-public abstract class JndiConstants {
-	
+public abstract class JndiHelper {
+
 	public static final String APP_JNDI_PREFIX = "ejbca/";
-	
+
+	private static final Logger log = Logger.getLogger(JndiHelper.class);
+
+	/**
+	 * Helper method to get a reference to a Remote SSB interface.
+	 * 
+	 * Example usage: CAAdminSessionRemote caadminsession = JndiHelper.getRemoteSession(new javax.naming.InitialContext(), CAAdminSessionRemote.class);
+	 * 
+	 * @param <T>
+	 * @param context
+	 * @param remoteInterface
+	 * @return
+	 */
+	public static <T> T getRemoteSession(Context context, Class<T> remoteInterface) {
+		String jndiName = APP_JNDI_PREFIX + remoteInterface.getSimpleName();
+		try {
+			return (T) context.lookup(jndiName);
+		} catch (ClassCastException e) {
+			log.error("JNDI object " + jndiName + " is not if type " +
+					remoteInterface.getName());
+		} catch (NamingException e) {
+			log.error("", e);
+		}
+		return null;
+	}
+
+
 }
