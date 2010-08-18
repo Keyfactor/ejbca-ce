7d7ac89c49653d076cd61ee831ec43db4998d5c0
aveen4711 <USER@primekey.com>
2010-08-18 07:21:06 +0000

    ECA-1791: Logging the certificate SubjectDN in OldLogDevice when an administrator logs in. If the admin certificate is not in the database, the certificate serialnumber and IssuerDN (and subjectDN in the 'Administrator Logged In' event) are displayed instead of "certificate not known to database".
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@9662 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/admin-gui/src/org/ejbca/ui/web/admin/configuration/EjbcaWebBean.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/configuration/EjbcaWebBean.java
@@ -175,7 +175,7 @@ public class EjbcaWebBean implements Serializable {
     		userdn = CertTools.getSubjectDN(certificates[0]);
     		log.debug("Verifying authorization of '"+userdn);    		
     		userAdminSession.checkIfCertificateBelongToUser(administrator, CertTools.getSerialNumber(certificates[0]), CertTools.getIssuerDN(certificates[0]));        
-    		logSession.log(administrator, certificates[0], LogConstants.MODULE_ADMINWEB,  new java.util.Date(),null, certificates[0], LogConstants.EVENT_INFO_ADMINISTRATORLOGGEDIN,"");
+    		logSession.log(administrator, certificates[0], LogConstants.MODULE_ADMINWEB,  new java.util.Date(),null, null, LogConstants.EVENT_INFO_ADMINISTRATORLOGGEDIN,"");
     	}
 
     	try {
