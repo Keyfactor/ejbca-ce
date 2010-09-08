601264ef1c836f8f80762ad810fb8e40c67515e3
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2010-09-08 09:10:21 +0000

    ECA-1720: comment out database upgrades only specific to websphere
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@9817 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/upgrade/310_40/310_40-upgrade-postgres.sql
+++ src/upgrade/310_40/310_40-upgrade-postgres.sql
@@ -1,23 +1,24 @@
 
 -- BOOLEANs on WebSphere were created as INT2 instead of "BOOLEAN". Hibernate will map this as BOOLEAN so we probably need to update this. 
 --  We cannot cast INT2 to BOOLEAN so we have to work around it..
-ALTER TABLE AccessRulesData ADD tmp BOOLEAN DEFAULT FALSE NOT NULL;
-UPDATE AccessRulesData SET tmp=TRUE WHERE isRecursive=1;
-ALTER TABLE AccessRulesData DROP isRecursive;
-ALTER TABLE AccessRulesData ADD isRecursive BOOLEAN DEFAULT FALSE NOT NULL;
-UPDATE AccessRulesData SET isRecursive=tmp;
-ALTER TABLE AccessRulesData DROP tmp;
+-- NOTE! This is only for WebSphere, not other application servers.
+--ALTER TABLE AccessRulesData ADD tmp BOOLEAN DEFAULT FALSE NOT NULL;
+--UPDATE AccessRulesData SET tmp=TRUE WHERE isRecursive=1;
+--ALTER TABLE AccessRulesData DROP isRecursive;
+--ALTER TABLE AccessRulesData ADD isRecursive BOOLEAN DEFAULT FALSE NOT NULL;
+--UPDATE AccessRulesData SET isRecursive=tmp;
+--ALTER TABLE AccessRulesData DROP tmp;
 
-ALTER TABLE KeyRecoveryData ADD tmp BOOLEAN DEFAULT FALSE NOT NULL;
-UPDATE KeyRecoveryData SET tmp=TRUE WHERE markedAsRecoverable=1;
-ALTER TABLE KeyRecoveryData DROP markedAsRecoverable;
-ALTER TABLE KeyRecoveryData ADD markedAsRecoverable BOOLEAN DEFAULT FALSE NOT NULL;
-UPDATE KeyRecoveryData SET markedAsRecoverable=tmp;
-ALTER TABLE KeyRecoveryData DROP tmp;
+--ALTER TABLE KeyRecoveryData ADD tmp BOOLEAN DEFAULT FALSE NOT NULL;
+--UPDATE KeyRecoveryData SET tmp=TRUE WHERE markedAsRecoverable=1;
+--ALTER TABLE KeyRecoveryData DROP markedAsRecoverable;
+--ALTER TABLE KeyRecoveryData ADD markedAsRecoverable BOOLEAN DEFAULT FALSE NOT NULL;
+--UPDATE KeyRecoveryData SET markedAsRecoverable=tmp;
+--ALTER TABLE KeyRecoveryData DROP tmp;
 
-ALTER TABLE ProtectedLogExportData ADD tmp BOOLEAN DEFAULT FALSE NOT NULL;
-UPDATE ProtectedLogExportData SET tmp=TRUE WHERE deleted=1;
-ALTER TABLE ProtectedLogExportData DROP deleted;
-ALTER TABLE ProtectedLogExportData ADD deleted BOOLEAN DEFAULT FALSE NOT NULL;
-UPDATE ProtectedLogExportData SET deleted=tmp;
-ALTER TABLE ProtectedLogExportData DROP tmp;
+--ALTER TABLE ProtectedLogExportData ADD tmp BOOLEAN DEFAULT FALSE NOT NULL;
+--UPDATE ProtectedLogExportData SET tmp=TRUE WHERE deleted=1;
+--ALTER TABLE ProtectedLogExportData DROP deleted;
+--ALTER TABLE ProtectedLogExportData ADD deleted BOOLEAN DEFAULT FALSE NOT NULL;
+--UPDATE ProtectedLogExportData SET deleted=tmp;
+--ALTER TABLE ProtectedLogExportData DROP tmp;
