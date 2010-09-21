13ccce5b71546f0fca9dce7345e3f374f6afdc68
Johan Eklund <johan.eklund@primekey.com>
2010-09-21 16:10:48 +0000

    ECA-1840: Changed CMP TCP service implementation from JBoss specific service to a WAR packaged service initialized by a dedicated Servlet.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@9957 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-cmp-tcp/src/org/ejbca/ui/web/protocol/CmpTcpInitServlet.java
+++ modules/ejbca-cmp-tcp/src/org/ejbca/ui/web/protocol/CmpTcpInitServlet.java
@@ -10,12 +10,35 @@
  *  See terms of license at gnu.org.                                     *
  *                                                                       *
  *************************************************************************/
- 
-package org.ejbca.appserver.jboss;
 
+package org.ejbca.ui.web.protocol;
 
-/** A Jboss service for CMP operations
- * 
- * version $Id$
-*/
-public interface CmpTcpServiceMBean extends org.jboss.system.ServiceMBean { }
+import java.net.UnknownHostException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+import org.apache.log4j.Logger;
+import org.ejbca.ui.tcp.CmpTcpServer;
+
+/**
+ * Servlet with the sole purpose of starting the CMP TCP server. 
+ *
+ * @version $Id$
+ */
+public class CmpTcpInitServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	private static final Logger log = Logger.getLogger(CmpTcpInitServlet.class);
+
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		log.info("Starting CMP TCP Service..");
+		try {
+			CmpTcpServer.start();
+		} catch (UnknownHostException e) {
+			throw new ServletException(e);
+		}
+	}
+}
