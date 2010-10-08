3ae9d711e23660dc8feabab2372ee56f83a9bbb6
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2010-10-08 13:41:18 +0000

    ECA-1784: add reorg table after adding columns to db2
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@10163 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/upgrade/310_311/310_311-post-upgrade-db2.sql
+++ src/upgrade/310_311/310_311-post-upgrade-db2.sql
@@ -1,3 +1,4 @@
 -- Add rowVersion column to all tables
 -- PublisherQueueData was a late add-on so we need to check if the column was created during appserver start-up
 ALTER TABLE PublisherQueueData ADD COLUMN rowVersion INTEGER WITH DEFAULT 0;
+CALL SYSPROC.ADMIN_CMD('REORG TABLE PublisherQueueData');
