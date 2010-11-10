221f1d82984f0ecddb9cd8f3c25cd010915d56aa
aveen4711 <USER@primekey.com>
2010-11-10 10:43:49 +0000

    ECA-1844: Adding a function to flush caches across a cluster from admin GUI
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@10407 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/publicweb-gui/src/org/ejbca/ui/web/pub/ClearCacheServlet.java
@@ -0,0 +1,138 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+ 
+package org.ejbca.ui.web.pub;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Iterator;
+import java.util.Set;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.cesecore.core.ejb.ca.store.CertificateProfileSessionLocal;
+import org.cesecore.core.ejb.log.LogSessionLocal;
+import org.cesecore.core.ejb.ra.raadmin.EndEntityProfileSessionLocal;
+import org.ejbca.core.ejb.authorization.AuthorizationSessionLocal;
+import org.ejbca.core.ejb.ca.caadmin.CAAdminSessionLocal;
+import org.ejbca.core.ejb.ca.caadmin.CaSessionLocal;
+import org.ejbca.core.ejb.ra.raadmin.RaAdminSessionLocal;
+import org.ejbca.core.model.log.Admin;
+import org.ejbca.core.model.ra.raadmin.GlobalConfiguration;
+
+/**
+ * Servlet used to clear all caches (Global Configuration Cache, End Entity Profile Cache, 
+ * Certificate Profile Cache, Log Configuration Cache, Authorization Cache and CA Cache).
+ *
+ * 
+ * @author aveen Ismail
+ * 
+ * @version $Id$
+ */
+public class ClearCacheServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -8563174167843989458L;
+	private static final Logger log = Logger.getLogger(ClearCacheServlet.class);
+	
+	@EJB
+	private RaAdminSessionLocal raadminsession;
+	@EJB
+	private EndEntityProfileSessionLocal endentitysession;
+	@EJB
+	private CertificateProfileSessionLocal certificateprofilesession;
+	@EJB
+	private AuthorizationSessionLocal authorizationsession;
+	@EJB
+	private LogSessionLocal logsession;
+	@EJB
+	private CaSessionLocal casession;
+	
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+  
+    public void doPost(HttpServletRequest req, HttpServletResponse res)	throws IOException, ServletException {
+    	doGet(req,res);
+    }
+
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+        throws IOException, ServletException {
+        log.trace(">doGet()");
+        
+        if (StringUtils.equals(req.getParameter("command"), "clearcaches")) {
+
+            if(!acceptedHost(req.getRemoteHost())) {
+        		if (log.isDebugEnabled()) {
+        			log.debug("Clear cache request denied from host "+req.getRemoteHost());
+        		}
+        		res.sendError(HttpServletResponse.SC_BAD_REQUEST, "The remote host "+req.getRemoteHost()+" is unknown");
+        	} else {
+       
+        		raadminsession.flushGlobalConfigurationCache();
+        		if(log.isDebugEnabled()){
+        			log.debug("Global Configuration cache cleared");
+        		}
+        			
+        		endentitysession.flushProfileCache();
+        		if(log.isDebugEnabled()) {
+        			log.debug("RA Profile cache cleared");
+        		}
+        		
+        		certificateprofilesession.flushProfileCache();
+        		if(log.isDebugEnabled()) {
+        			log.debug("Cert Profile cache cleared");
+        		}
+        	       			
+        		authorizationsession.flushAuthorizationRuleCache();
+        		if(log.isDebugEnabled()) {
+        			log.debug("Authorization Rule cache cleared");
+        		}
+			
+        		logsession.flushConfigurationCache();
+        		if(log.isDebugEnabled()) {
+        			log.debug("Log Configuration cache cleared");
+        		}
+        	
+        		casession.flushCACache();
+        		if(log.isDebugEnabled()) {
+        			log.debug("CA cache cleared");
+        		}
+        	}
+        }
+        log.trace("<doGet()");
+    }
+
+    private boolean acceptedHost(String remotehost) {
+    	
+		GlobalConfiguration gc = raadminsession.getCachedGlobalConfiguration(new Admin(Admin.TYPE_INTERNALUSER));
+    	Set nodes = gc.getNodesInCluster();
+		Iterator itr = nodes.iterator();
+		String nodename = null;
+		while(itr.hasNext()){
+			nodename = (String) itr.next();
+			try {
+				if(StringUtils.equals(remotehost, InetAddress.getByName(nodename).getHostAddress()))	return true;
+			} catch (UnknownHostException e) {}
+		}
+		return false;
+    }
+}
