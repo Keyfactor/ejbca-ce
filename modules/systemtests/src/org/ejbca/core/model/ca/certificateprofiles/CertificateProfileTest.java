f2b03d0c990edf4f56950d3b184bb0e5eba69329
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2010-12-12 14:20:31 +0000

    ECA-1966: return cloned copies of certificate and end entity profiles so the cache contents can not be modified outside of the session bean
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@10822 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/systemtests/src/org/ejbca/core/model/ca/certificateprofiles/CertificateProfileTest.java
+++ modules/systemtests/src/org/ejbca/core/model/ca/certificateprofiles/CertificateProfileTest.java
@@ -14,6 +14,7 @@
 package org.ejbca.core.model.ca.certificateprofiles;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import junit.framework.TestCase;
@@ -468,5 +469,26 @@ public class CertificateProfileTest extends TestCase {
         certificateProfileSession.removeCertificateProfile(admin, "TESTCPCACHE1");
 
     } // test12CertificateProfileCache
+
+    public void test13Clone() throws Exception {
+        CertificateProfile profile = new CertificateProfile();
+        CertificateProfile clone = (CertificateProfile)profile.clone();
+        HashMap profmap = (HashMap)profile.saveData();
+        HashMap clonemap = (HashMap)clone.saveData();
+        assertEquals(profmap.size(), clonemap.size());
+        clonemap.put("FOO", "BAR");
+        assertEquals(profmap.size()+1, clonemap.size());
+        profmap.put("FOO", "BAR");
+        assertEquals(profmap.size(), clonemap.size());
+        profmap.put("FOO", "FAR");
+        String profstr = (String)profmap.get("FOO");
+        String clonestr = (String)clonemap.get("FOO");
+        assertEquals("FAR", profstr);
+        assertEquals("BAR", clonestr);
+        CertificateProfile clone2 = (CertificateProfile)clone.clone();
+        HashMap clonemap2 = (HashMap)clone2.saveData();
+        assertEquals(clonemap2.size(), profmap.size());
+        log.trace("<test08FieldIds()");
+    }
 
 }
