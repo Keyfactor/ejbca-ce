57b3349a6f7396caa1477ee038d5280c5386801d
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2010-12-14 10:09:29 +0000

    ECA-1854: make global configuration cache static final by introducing a new simple class holding the volatile cache variable
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@10842 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ src/java/org/ejbca/core/ejb/ra/raadmin/GlobalConfigurationCache.java
@@ -0,0 +1,61 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.core.ejb.ra.raadmin;
+
+import org.ejbca.config.EjbcaConfiguration;
+import org.ejbca.core.model.ra.raadmin.GlobalConfiguration;
+
+/**
+ * Class Holding cache variable for global configuration. Needed because EJB spec does not allow volatile, non-final 
+ * fields in session beans.
+ * This is a trivial cache, too trivial, it needs manual handling of setting the cache variable, this class does not keep track on if
+ * the cache variable is null or not, the using class must ensure that it does not try to use a null value. 
+ * Only the method "needsUpdate will return true of the cache variable is null. 
+ * 
+ * @version $Id$
+ */
+public final class GlobalConfigurationCache {
+
+    /**
+     * Cache variable containing the global configuration. This cache may be
+     * unsynchronized between multiple instances of EJBCA, but is common to all
+     * threads in the same VM. Set volatile to make it thread friendly.
+     */
+    private volatile GlobalConfiguration globalconfigurationCache = null;
+    /** help variable used to control that GlobalConfiguration update isn't performed to often. */
+    private volatile long lastupdatetime = -1;  
+
+	public GlobalConfigurationCache() {
+		// Do nothing
+	}
+
+	public GlobalConfiguration getGlobalconfiguration() {
+		return globalconfigurationCache;
+	}
+
+	public void setGlobalconfiguration(final GlobalConfiguration globalconfiguration) {
+		globalconfigurationCache = globalconfiguration;
+        lastupdatetime = System.currentTimeMillis();
+	}
+
+	public boolean needsUpdate() {
+        if (globalconfigurationCache != null && lastupdatetime + EjbcaConfiguration.getCacheGlobalConfigurationTime() > System.currentTimeMillis()) {
+        	return false;
+        }
+        return true;
+	}
+	
+	public void clearCache() {
+		globalconfigurationCache = null;
+	}
+}
