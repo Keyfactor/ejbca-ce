52ef7ca1cb94971ffdc230001c44fb0ac954f018
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2010-12-14 16:00:17 +0000

    ECA-1854: break out cache variable into a separate class to not have static non-final variables in the session bean class, clears warnings in ejb verifier.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@10861 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ src/java/org/ejbca/core/ejb/ca/caadmin/CaHelperCache.java
@@ -0,0 +1,44 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.core.ejb.ca.caadmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * Class Holding cache variables. Needed because EJB spec does not allow volatile, non-final 
+ * fields in session beans.
+ * This is a trivial cache, too trivial, it only holds variables actually, does nothing. All updating etc is done by the user.
+ * 
+ * @version $Id$
+ */
+public final class CaHelperCache {
+
+    /**
+     * help variable used to control that CA info update (read from database)
+     * isn't performed to often.
+     */
+    protected static volatile long lastCACacheUpdateTime = -1;
+
+    /**
+     * Caching of CA IDs with CA cert hash as ID
+     */
+    protected static volatile Map<Integer, Integer> caCertToCaId = new HashMap<Integer, Integer>();
+
+
+	private CaHelperCache() {
+		// Do nothing
+	}
+
+}
