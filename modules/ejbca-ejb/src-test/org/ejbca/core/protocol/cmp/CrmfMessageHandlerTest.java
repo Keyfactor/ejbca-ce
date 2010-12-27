fdd9876ece1131598570da5a64b21ab24168a73e
Johan Eklund <johan.eklund@primekey.com>
2010-12-27 12:43:08 +0000

    ECA-1777: Added helper class so we can mock objects without creating full-blown stubs with overrides of things that we don't want to mock. Updated mock-pattern in test.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@10980 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb/src-test/org/ejbca/core/protocol/cmp/CrmfMessageHandlerTest.java
+++ modules/ejbca-ejb/src-test/org/ejbca/core/protocol/cmp/CrmfMessageHandlerTest.java
@@ -12,65 +12,19 @@
  *************************************************************************/
 package org.ejbca.core.protocol.cmp;
 
-import java.io.UnsupportedEncodingException;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.math.BigInteger;
-import java.rmi.RemoteException;
-import java.security.PublicKey;
-import java.security.cert.Certificate;
-import java.util.Collection;
-import java.util.Date;
-import java.util.List;
-
-import javax.ejb.CreateException;
-import javax.ejb.EJBHome;
-import javax.ejb.EJBObject;
-import javax.ejb.FinderException;
-import javax.ejb.Handle;
-import javax.ejb.ObjectNotFoundException;
-import javax.ejb.RemoveException;
-
 import junit.framework.TestCase;
 
-import org.ejbca.core.EjbcaException;
 import org.ejbca.core.ejb.ca.sign.SignSessionRemote;
 import org.ejbca.core.ejb.ra.UserAdminSessionRemote;
-import org.ejbca.core.ejb.ra.UserData;
-import org.ejbca.core.model.approval.ApprovalException;
-import org.ejbca.core.model.approval.WaitingForApprovalException;
-import org.ejbca.core.model.authorization.AuthorizationDeniedException;
-import org.ejbca.core.model.ca.AuthLoginException;
-import org.ejbca.core.model.ca.AuthStatusException;
-import org.ejbca.core.model.ca.IllegalKeyException;
-import org.ejbca.core.model.ca.SignRequestException;
-import org.ejbca.core.model.ca.SignRequestSignatureException;
-import org.ejbca.core.model.ca.caadmin.CADoesntExistsException;
-import org.ejbca.core.model.ca.caadmin.extendedcaservices.ExtendedCAServiceNotActiveException;
-import org.ejbca.core.model.ca.caadmin.extendedcaservices.ExtendedCAServiceRequest;
-import org.ejbca.core.model.ca.caadmin.extendedcaservices.ExtendedCAServiceRequestException;
-import org.ejbca.core.model.ca.caadmin.extendedcaservices.ExtendedCAServiceResponse;
-import org.ejbca.core.model.ca.caadmin.extendedcaservices.IllegalExtendedCAServiceRequestException;
 import org.ejbca.core.model.log.Admin;
-import org.ejbca.core.model.ra.AlreadyRevokedException;
-import org.ejbca.core.model.ra.NotFoundException;
 import org.ejbca.core.model.ra.UserDataVO;
-import org.ejbca.core.model.ra.raadmin.UserDoesntFullfillEndEntityProfile;
-import org.ejbca.core.protocol.IRequestMessage;
-import org.ejbca.core.protocol.IResponseMessage;
-import org.ejbca.util.query.IllegalQueryException;
-import org.ejbca.util.query.Query;
+import org.ejbca.util.SimpleMock;
 
 /**
  * Unit tests for CrmfMessageHandler. 
  * 
- * FIXME: This unit test massively stubs the session
- * beans in order to be able to be run offline. These stubs should be replaced
- * by mocks in future versions of EJBCA. Note that the stubs merely produce
- * deterministic data and perform no validation.
- * 
- * TODO: How about using a Proxy instead for the relevant functions?
- * So we don't have to change this class every time we change the interface.
+ * This test verifies that the request has it's username set to the same as an existing
+ * user's with the same subject DN. 
  * 
  * @author mikek
  * @version $Id$
@@ -79,413 +33,33 @@ public class CrmfMessageHandlerTest extends TestCase {
 
     private static String USER_NAME = "foobar";
 
-    private Admin admin;
-
-    public CrmfMessageHandlerTest() {
-
-    }
-
     public CrmfMessageHandlerTest(String name) {
         super(name);
     }
 
-    public void setUp() throws Exception {
-        super.setUp();
-        admin = new Admin(Admin.TYPE_RA_USER);
-    }
-
-    public void tearDown() throws Exception {
-        super.tearDown();
-        admin = null;
-    }
-
-    public void testExtractUserNameComponent() throws RemoteException, CreateException, InstantiationException, IllegalAccessException,
-            IllegalArgumentException, InvocationTargetException, SecurityException, NoSuchMethodException, NoSuchFieldException,
-            ClassNotFoundException {
-
-        
-        
-        
+    public void testExtractUserNameComponent() {
         CrmfMessageHandler crmfMessageHandler = new CrmfMessageHandler();
-
         /*
          * Some slight reflective manipulation of crmfMessageHandler here in
          * order to get around the fact that we're not running any of the logic
          * in its usual constructor, instead using the empty default one.
          */
-
-        Field adminField = BaseCmpMessageHandler.class.getDeclaredField("admin");
-        adminField.setAccessible(true);
-        adminField.set(crmfMessageHandler, admin);
-
-        Field userSessionField = CrmfMessageHandler.class.getDeclaredField("userAdminSession");
-        userSessionField.setAccessible(true);
-        userSessionField.set(crmfMessageHandler, new UserAdminSessionRemoteStub());
-
-        Field signSessionField = CrmfMessageHandler.class.getDeclaredField("signSession");
-        signSessionField.setAccessible(true);
-        SignSessionStub signsession = new SignSessionStub();
-        signSessionField.set(crmfMessageHandler, signsession);
-
-        crmfMessageHandler.handleMessage(new CrmfRequestMessage() {
+        SimpleMock.inject(crmfMessageHandler, "admin", new Admin(Admin.TYPE_RA_USER));
+        final UserAdminSessionRemote userAdminSessionMock = new SimpleMock(UserAdminSessionRemote.class) {{
+        	map("findUserBySubjectDN", new UserDataVO() {
+				private static final long serialVersionUID = 1L;
+				public String getUsername() { return USER_NAME; };
+			});
+        }}.mock();
+        SimpleMock.inject(crmfMessageHandler, "userAdminSession", userAdminSessionMock);
+        SimpleMock.inject(crmfMessageHandler, "signSession", new SimpleMock(SignSessionRemote.class).mock());
+        final CrmfRequestMessage requestMock = new CrmfRequestMessage() {
             private static final long serialVersionUID = 1L;
-
             public String getSubjectDN() {
-                return "foo";
+                return "foo";	// Just return something that isn't null
             }
-
-        });
-
-        assertEquals("testExtractUserNameComponent did not process user name correctly", USER_NAME, signsession.getUserName());
-
+        };
+        crmfMessageHandler.handleMessage(requestMock);
+        assertEquals("crmfMessageHandler.handleMessage did not process user name correctly", USER_NAME, requestMock.getUsername());
     }
-
-    /*
-     * Below here are stubs of the SessionBeans. These should be replaced by
-     * mocks in EJBCA4. Code below this point is merely utility.
-     */
-
-    public static class SignSessionStub implements SignSessionRemote {
-
-        /**
-         * For the purpose of this test stub, this variable simply stores a test
-         * result;
-         */
-        private String userName = "";
-
-        public String getUserName() {
-            return userName;
-        }
-
-        public Certificate createCertificate(Admin admin, String username, String password, PublicKey pk) throws EjbcaException,
-                ObjectNotFoundException {
-
-            return null;
-        }
-
-        public Certificate createCertificate(Admin admin, String username, String password, PublicKey pk, int keyusage, Date notBefore, Date notAfter)
-                throws EjbcaException, ObjectNotFoundException {
-
-            return null;
-        }
-
-        public Certificate createCertificate(Admin admin, String username, String password, Certificate incert) throws EjbcaException,
-                ObjectNotFoundException {
-
-            return null;
-        }
-
-        /**
-		 * This method has been stubbed to respond to
-		 * testExtractUserNameComponent
-		 */
-		public IResponseMessage createCertificate(Admin admin, IRequestMessage req, Class responseClass, UserDataVO suppliedUserData) throws EjbcaException {
-            userName = req.getUsername();		
-		    return null;
-		}
-
-        public Certificate createCertificate(Admin admin, String username, String password, PublicKey pk, int keyusage, Date notBefore, Date notAfter, int certificateprofileid, int caid) 
-        throws EjbcaException, ObjectNotFoundException {
-        	return null;
-        }
-        
-		public byte[] createPKCS7(Admin admin, Certificate cert, boolean includeChain) throws CADoesntExistsException, SignRequestSignatureException {
-
-            return null;
-        }
-
-        public byte[] createPKCS7(Admin admin, int caId, boolean includeChain) throws CADoesntExistsException {
-
-            return null;
-        }
-
-        public IResponseMessage createRequestFailedResponse(Admin admin, IRequestMessage req, Class responseClass) throws AuthLoginException,
-                AuthStatusException, IllegalKeyException, CADoesntExistsException, SignRequestSignatureException, SignRequestException {
-
-            return null;
-        }
-
-        public IRequestMessage decryptAndVerifyRequest(Admin admin, IRequestMessage req) throws ObjectNotFoundException, AuthStatusException,
-                AuthLoginException, IllegalKeyException, CADoesntExistsException, SignRequestException, SignRequestSignatureException {
-
-            return null;
-        }
-
-        public ExtendedCAServiceResponse extendedService(Admin admin, int caid, ExtendedCAServiceRequest request)
-                throws ExtendedCAServiceRequestException, IllegalExtendedCAServiceRequestException, ExtendedCAServiceNotActiveException,
-                CADoesntExistsException {
-
-            return null;
-        }
-
-        public IResponseMessage getCRL(Admin admin, IRequestMessage req, Class responseClass) throws AuthStatusException, AuthLoginException,
-                IllegalKeyException, CADoesntExistsException, SignRequestException, SignRequestSignatureException, UnsupportedEncodingException {
-
-            return null;
-        }
-
-        public Collection getCertificateChain(Admin admin, int caid) {
-
-            return null;
-        }
-
-        public boolean isUniqueCertificateSerialNumberIndex() {
-
-            return false;
-        }
-
-        public EJBHome getEJBHome() throws RemoteException {
-
-            return null;
-        }
-
-        public Handle getHandle() throws RemoteException {
-
-            return null;
-        }
-
-        public Object getPrimaryKey() throws RemoteException {
-
-            return null;
-        }
-
-        public boolean isIdentical(EJBObject arg0) throws RemoteException {
-
-            return false;
-        }
-
-        public void remove() throws RemoteException, RemoveException {
-
-        }
-
-    }
-
-    class UserAdminSessionRemoteStub implements UserAdminSessionRemote {
-
-        public void addUser(Admin admin, String username, String password, String subjectdn, String subjectaltname, String email, boolean clearpwd,
-                int endentityprofileid, int certificateprofileid, int type, int tokentype, int hardwaretokenissuerid, int caid)
-                throws AuthorizationDeniedException, UserDoesntFullfillEndEntityProfile, WaitingForApprovalException, CADoesntExistsException,
-                EjbcaException {
-
-        }
-
-        public void addUser(Admin admin, UserDataVO userdata, boolean clearpwd) throws AuthorizationDeniedException,
-                UserDoesntFullfillEndEntityProfile, WaitingForApprovalException, CADoesntExistsException, EjbcaException {
-
-        }
-
-        public void addUserFromWS(Admin admin, UserDataVO userdata, boolean clearpwd) throws AuthorizationDeniedException,
-                UserDoesntFullfillEndEntityProfile, WaitingForApprovalException, CADoesntExistsException, EjbcaException {
-
-        }
-
-        public void changeUser(Admin admin, String username, String password, String subjectdn, String subjectaltname, String email,
-                boolean clearpwd, int endentityprofileid, int certificateprofileid, int type, int tokentype, int hardwaretokenissuerid, int status,
-                int caid) throws AuthorizationDeniedException, UserDoesntFullfillEndEntityProfile, WaitingForApprovalException,
-                CADoesntExistsException, EjbcaException {
-
-        }
-
-        public void changeUser(Admin admin, UserDataVO userdata, boolean clearpwd) throws AuthorizationDeniedException,
-                UserDoesntFullfillEndEntityProfile, WaitingForApprovalException, CADoesntExistsException, EjbcaException {
-
-        }
-
-        public void changeUser(Admin admin, UserDataVO userdata, boolean clearpwd, boolean fromWebService) throws AuthorizationDeniedException,
-                UserDoesntFullfillEndEntityProfile, WaitingForApprovalException, CADoesntExistsException, EjbcaException {
-
-        }
-
-        public boolean checkForCAId(Admin admin, int caid)  {
-
-            return false;
-        }
-
-        public boolean checkForCertificateProfileId(Admin admin, int certificateprofileid) {
-
-            return false;
-        }
-
-        public boolean checkForEndEntityProfileId(Admin admin, int endentityprofileid) {
-
-            return false;
-        }
-
-        public boolean checkForHardTokenProfileId(Admin admin, int profileid) {
-
-            return false;
-        }
-
-        public void checkIfCertificateBelongToAdmin(Admin admin, BigInteger certificatesnr, String issuerdn) throws AuthorizationDeniedException {
-
-        }
-
-        public void checkIfCertificateBelongToUser(Admin admin, BigInteger certificatesnr, String issuerdn) throws AuthorizationDeniedException {
-
-        }
-
-        public void cleanUserCertDataSN(Admin admin, String username) throws AuthorizationDeniedException, FinderException, ApprovalException,
-                WaitingForApprovalException {
-
-        }
-
-        public void decRemainingLoginAttempts(Admin admin, String username) throws AuthorizationDeniedException, FinderException {
-
-        }
-
-        public int decRequestCounter(Admin admin, String username) throws AuthorizationDeniedException, FinderException, ApprovalException,
-                WaitingForApprovalException {
-
-            return 0;
-        }
-
-        public void deleteUser(Admin admin, String username) throws AuthorizationDeniedException, NotFoundException, RemoveException {
-
-        }
-
-        public boolean existsUser(Admin admin, String username) {
-
-            return false;
-        }
-
-        public Collection findAllUsersByCaId(Admin admin, int caid) {
-
-            return null;
-        }
-
-        public Collection findAllUsersByStatus(Admin admin, int status) throws FinderException {
-
-            return null;
-        }
-
-        public List<UserDataVO> findAllBatchUsersByStatusWithLimit(int status) {
-            return null;
-        }
-
-        public Collection findAllUsersWithLimit(Admin admin) {
-
-            return null;
-        }
-
-        public UserDataVO findUser(Admin admin, String username) throws AuthorizationDeniedException {
-
-            return null;
-        }
-
-        public Collection findUserByEmail(Admin admin, String email) throws AuthorizationDeniedException {
-
-            return null;
-        }
-
-        public UserDataVO findUserBySubjectAndIssuerDN(Admin admin, String subjectdn, String issuerdn) throws AuthorizationDeniedException {
-
-            return null;
-        }
-
-        public UserDataVO findUserBySubjectDN(Admin admin, String subjectdn) throws AuthorizationDeniedException {
-
-            return new UserDataVOStub();
-        }
-
-        public Admin getAdmin(Certificate certificate) {
-
-            return null;
-        }
-
-        public boolean prepareForKeyRecovery(Admin admin, String username, int endEntityProfileId, Certificate certificate)
-                throws AuthorizationDeniedException, ApprovalException, WaitingForApprovalException {
-
-            return false;
-        }
-
-        public Collection query(Admin admin, Query query, String caauthorizationstring, String endentityprofilestring, int numberofrows)
-                throws IllegalQueryException {
-
-            return null;
-        }
-
-        public void resetRemainingLoginAttempts(Admin admin, String username) throws AuthorizationDeniedException, FinderException {
-
-        }
-
-        public void revokeAndDeleteUser(Admin admin, String username, int reason) throws AuthorizationDeniedException, ApprovalException,
-                WaitingForApprovalException, RemoveException, NotFoundException {
-
-        }
-
-        public void revokeCert(Admin admin, BigInteger certserno, String issuerdn, String username, int reason) throws AuthorizationDeniedException,
-                FinderException, ApprovalException, WaitingForApprovalException, AlreadyRevokedException {
-
-        }
-
-        public void revokeUser(Admin admin, String username, int reason) throws AuthorizationDeniedException, FinderException, ApprovalException,
-                WaitingForApprovalException, AlreadyRevokedException {
-
-        }
-
-        public void setClearTextPassword(Admin admin, String username, String password) throws UserDoesntFullfillEndEntityProfile,
-                AuthorizationDeniedException, FinderException {
-
-        }
-
-        public void setPassword(Admin admin, String username, String password) throws UserDoesntFullfillEndEntityProfile,
-                AuthorizationDeniedException, FinderException {
-
-        }
-
-        public void setUserStatus(Admin admin, String username, int status) throws AuthorizationDeniedException, FinderException, ApprovalException,
-                WaitingForApprovalException {
-
-        }
-
-        public boolean verifyPassword(Admin admin, String username, String password) throws UserDoesntFullfillEndEntityProfile,
-                AuthorizationDeniedException, FinderException {
-
-            return false;
-        }
-
-        public EJBHome getEJBHome() throws RemoteException {
-
-            return null;
-        }
-
-        public Handle getHandle() throws RemoteException {
-
-            return null;
-        }
-
-        public Object getPrimaryKey() throws RemoteException {
-
-            return null;
-        }
-
-        public boolean isIdentical(EJBObject arg0) throws RemoteException {
-
-            return false;
-        }
-
-        public void remove() throws RemoteException, RemoveException {
-
-        }
-
-        class UserDataVOStub extends UserDataVO {
-            private static final long serialVersionUID = 1L;
-
-            public String getUsername() {
-                return USER_NAME;
-            }
-        }
-
-        @Override
-        public void cleanUserCertDataSN(UserDataVO data) throws ObjectNotFoundException {
-            
-        }
-
-        @Override
-        public List<UserData> findUsers(List<Integer> caIds, long timeModified, int status) {
-            return null;
-        }
-
-    }
-
 }
---
+++ src/java/org/ejbca/util/SimpleMock.java
@@ -0,0 +1,174 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
+package org.ejbca.util;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Simple class for mocking an interface that can return pre-configured values
+ * or throw defined Exceptions.<br/>
+ * <br/>
+ * The mocked object will treat two methods with the same name, but different
+ * signatures as the same. (E.g. getInt() and getInt(String x) will be treated
+ * the same.)<br/>
+ * <br/>
+ * Example usage:<br/>
+ * <pre>
+ *  SomeInterface x = new SimpleMock(SomeInterface.class) {{
+ *  	map("getInt", 1); // Override method getInt(...)
+ *  	map("throwsException", new Exception("TestException")); // Override method throwsException(...)
+ *  }}.mock();
+ *  x.getInt("parameter 1", 2, "parameter 3");	// Will return 1
+ *  </pre>
+ *
+ *  Unmapped methods will return the following defaults:
+ *  <ul>
+ *   <li>boolean: false</li>
+ *   <li>char: '\0'</li>
+ *   <li>byte: 0</li>
+ *   <li>short: 0</li>
+ *   <li>int: 0</li>
+ *   <li>long: 0</li>
+ *   <li>float: 0.0</li>
+ *   <li>double: 0.0</li>
+ *   <li>Object: null</li>
+ *  </ul>
+ *  
+ *  @version $Id$
+ */
+public class SimpleMock {
+	
+	final Class<?> c;
+	final Map<String, Object> map = new HashMap<String, Object>();
+	final List<String> methodNames = new ArrayList<String>();
+
+	/** @param c is the interface to mock */
+	public SimpleMock(Class<?> c) {
+		if (!c.isInterface()) {
+			throw new RuntimeException(c.getName() + " is not an interface.");
+		}
+		this.c = c;
+		Class<?> thisInterface = c;
+		while (thisInterface != null) {
+			for (Method m : c.getMethods()) {
+				methodNames.add(m.getName());
+			}
+			thisInterface = c.getSuperclass();
+		}
+	}
+	
+	/**
+	 * Adds a mapping between a method-name and return value
+	 * @param methodName
+	 * @param valueOrException
+	 */
+	public void map(String methodName, Object valueOrException) {
+		if (!methodNames.contains(methodName)) {
+			throw new RuntimeException(new NoSuchMethodException(methodName));
+		}
+		map.put(methodName, valueOrException);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public <T> T mock() {
+		Class<?>[] cs = {c};
+		return (T) Proxy.newProxyInstance(c.getClassLoader(), cs, new MockInvocationHandler(map));
+	}
+
+	/**
+	 * Injects "value" into a (private) field named "fieldName" of "target" or
+	 * the first one of "target"'s super classes where "fieldName" exists.
+	 */
+    public static void inject(Object target, String fieldName, Object value) {
+		try {
+			Field field = null;
+			Class<?> targetClass = target.getClass();
+			while (targetClass != null) {
+				try {
+					field = targetClass.getDeclaredField(fieldName);
+					break;
+				} catch (NoSuchFieldException e) {
+				}
+				targetClass = targetClass.getSuperclass();
+			}
+			if (targetClass == null) {
+				throw new NoSuchFieldException(fieldName);
+			}
+	        field.setAccessible(true);
+	        field.set(target, value);
+		} catch (SecurityException e) {
+			throw new RuntimeException(e);
+		} catch (NoSuchFieldException e) {
+			throw new RuntimeException(e);
+		} catch (IllegalArgumentException e) {
+			throw new RuntimeException(e);
+		} catch (IllegalAccessException e) {
+			throw new RuntimeException(e);
+		}
+    }
+
+	private class MockInvocationHandler implements InvocationHandler {
+
+		final Map<String, Object> map;
+		
+		public MockInvocationHandler(Map<String, Object> map) {
+			this.map = map;
+		}
+		
+		@Override
+		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+			// Check if we should return a mapped value
+			final String methodName = method.getName();
+			if (map.containsKey(methodName)) {
+				final Object returnValue = map.get(methodName);
+				if (returnValue instanceof Throwable) {
+					throw (Throwable)returnValue;
+				} else {
+					return returnValue;
+				}
+			}
+			// No mapped value was configured, so return the default for this return type
+			final Class<?> returnType = method.getReturnType();
+			if (returnType.isPrimitive()) {
+				// We cannot return null if it is a primitive type.
+				final String returnTypeName = returnType.getName();
+				if ("boolean".equals(returnTypeName)) {
+					return false;
+				} else if ("char".equals(returnTypeName)) {
+					return '\0';
+				} else if ("byte".equals(returnTypeName)) {
+					return (byte)0;
+				} else if ("short".equals(returnTypeName)) {
+					return (short)0;
+				} else if ("int".equals(returnTypeName)) {
+					return (int)0;
+				} else if ("long".equals(returnTypeName)) {
+					return (long)0L;
+				} else if ("float".equals(returnTypeName)) {
+					return (float)0.0F;
+				} else if ("double".equals(returnTypeName)) {
+					return (double)0.0D;
+				}
+			}
+			return null;
+		}
+	}
+}
