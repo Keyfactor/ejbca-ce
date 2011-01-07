163725716defe3792f6aec8eaf2a8f8b10437960
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2011-01-07 11:05:54 +0000

    ECA-1976: fix PMD warnings
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@11088 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/util/Base64.java
+++ src/java/org/ejbca/util/Base64.java
@@ -23,15 +23,17 @@ import java.io.ByteArrayOutputStream;
  *
  * @version $Id$
  */
-public class Base64 {
+public final class Base64 {
+
+    private Base64 () {} // Not for instantiation
 
     /**
-     * encode the input data producong a base 64 encoded byte array with the output lines be split by '\n' (64 byte rows).
+     * encode the input data producing a base 64 encoded byte array with the output lines be split by '\n' (64 byte rows).
      *
      * @param data data to be encoded
      * @return a byte array containing the base 64 encoded data.
      */
-    public static byte[] encode(byte[] data) {
+    public static byte[] encode(final byte[] data) {
         return encode(data, true);
     }
 
@@ -42,26 +44,26 @@ public class Base64 {
      * @param splitlines whether the output lines will be split by '\n' (64 byte rows) or not.
      * @return a byte array containing the base 64 encoded data.
      */
-    public static byte[] encode(byte[] data, boolean splitlines) {
+    public static byte[] encode(final byte[] data, final boolean splitlines) {
 		byte[] bytes = org.bouncycastle.util.encoders.Base64.encode(data);
-        if (!splitlines) {
-            return bytes;
-        }
-
-        // make sure we get limited lines...
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        for (int i = 0; i < bytes.length; i += 64) {
-            if ((i + 64) < bytes.length) {
-                os.write(bytes, i, 64);
-                os.write('\n');
-            } else {
-                os.write(bytes, i, bytes.length - i);
+        if (splitlines) {
+            // make sure we get limited lines...
+            final ByteArrayOutputStream os = new ByteArrayOutputStream();
+            for (int i = 0; i < bytes.length; i += 64) {
+                if ((i + 64) < bytes.length) {
+                    os.write(bytes, i, 64);
+                    os.write('\n');
+                } else {
+                    os.write(bytes, i, bytes.length - i);
+                }
             }
+            bytes = os.toByteArray();
         }
-        return os.toByteArray();
+        return bytes;
+
     }
     
-    public static byte[] decode(byte[] bytes) {
+    public static byte[] decode(final byte[] bytes) {
         return org.bouncycastle.util.encoders.Base64.decode(bytes);
     }
 
