194028ea8b767fab6a6987ddaa73865d45ab9830
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2011-01-07 10:44:04 +0000

    trivial, add new simple JUnit test for ExtendedKeyUsageConfiguration
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@11086 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ejbca-ejb/src-test/org/ejbca/config/ExtendedKeyUsageConfTest.java
@@ -0,0 +1,57 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
+package org.ejbca.config;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.List;
+import java.util.Map;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests the ExtendedKeyUsageConfiguration class
+ * 
+ * @version $Id$
+ */
+public class ExtendedKeyUsageConfTest extends TestCase {
+	
+	public ExtendedKeyUsageConfTest(String name) {
+		super(name);
+		ConfigurationHolder.instance().clear();
+	}
+
+	public void test01GetOids() throws Exception {
+		// Create some values in a temp file and add to the configuration
+		File f = File.createTempFile("testextendedkeyusageconf", "properties");
+		f.deleteOnExit();
+		FileWriter fos = new FileWriter(f);
+		fos.write("extendedkeyusage.oid.0 = 2.5.29.37.0\nextendedkeyusage.name.0 = ANYEXTENDEDKEYUSAGE\n");
+		fos.write("extendedkeyusage.oid.1 = 1.3.6.1.5.5.7.3.21\nextendedkeyusage.name.1 = EKU_PKIX_SSHCLIENT\n");
+		fos.write("extendedkeyusage.oid.2 = 1.3.6.1.4.1.311.10.3.4.2\nextendedkeyusage.name.2 = PDF_SIGNING\n");
+		fos.close();
+		ConfigurationHolder.addConfigurationFile(f.getAbsolutePath());
+		// Now there will be some values
+		List<String> oids = ExtendedKeyUsageConfiguration.getExtendedKeyUsageOids();
+		assertEquals(3, oids.size()); 
+		Map<String, String> map = ExtendedKeyUsageConfiguration.getExtendedKeyUsageOidsAndNames();
+		assertTrue(oids.contains("2.5.29.37.0")); // ANYEXTENDEDKEYUSAGE
+		assertEquals("ANYEXTENDEDKEYUSAGE", map.get("2.5.29.37.0"));
+		assertTrue(oids.contains("1.3.6.1.5.5.7.3.21")); // EKU_PKIX_SSHCLIENT
+		assertEquals("EKU_PKIX_SSHCLIENT", map.get("1.3.6.1.5.5.7.3.21"));
+		assertTrue(oids.contains("1.3.6.1.4.1.311.10.3.4.2")); // PDF_SIGNING
+		assertEquals("PDF_SIGNING", map.get("1.3.6.1.4.1.311.10.3.4.2"));
+	}
+	
+}
