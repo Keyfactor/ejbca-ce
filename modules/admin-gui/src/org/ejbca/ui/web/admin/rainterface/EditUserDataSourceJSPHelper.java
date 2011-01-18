02bf46abbef890494e84fd9a009d2e11bc5bd4e3
dcarella <USER@primekey.com>
2011-01-18 23:20:20 +0000

    ECA-1913 Message keys refactoring
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@11224 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/admin-gui/src/org/ejbca/ui/web/admin/rainterface/EditUserDataSourceJSPHelper.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/rainterface/EditUserDataSourceJSPHelper.java
@@ -359,7 +359,7 @@ public class EditUserDataSourceJSPHelper implements java.io.Serializable {
     		String subjectaltnametext = ejbcawebbean.getText("SUBALTNAME");
     		String subjectdirattrtext = ejbcawebbean.getText("SUBDIRATTR");
     		
-    		modifyableFieldTexts.put(subjectdntext + " : " +  ejbcawebbean.getText("UID"),Integer.valueOf(DNFieldExtractor.UID));  
+    		modifyableFieldTexts.put(subjectdntext + " : " +  ejbcawebbean.getText("DN_PKIX_UID"),Integer.valueOf(DNFieldExtractor.UID));  
     		modifyableFieldTexts.put(subjectdntext + " : " +  ejbcawebbean.getText("COMMONNAME"),Integer.valueOf(DNFieldExtractor.CN));  
     		modifyableFieldTexts.put(subjectdntext + " : " +  ejbcawebbean.getText("DNSERIALNUMBER"), Integer.valueOf(DNFieldExtractor.SN)); 
     		modifyableFieldTexts.put(subjectdntext + " : " +  ejbcawebbean.getText("GIVENNAME1"),Integer.valueOf(DNFieldExtractor.GIVENNAME)); 
