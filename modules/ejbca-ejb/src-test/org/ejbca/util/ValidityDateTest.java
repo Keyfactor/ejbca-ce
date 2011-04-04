b6d0adae3673334d76d58dff325b3437d5f8c955
aveen4711 <USER@primekey.com>
2011-04-04 06:38:54 +0000

    ECA-2075: Changing the dateformat to yyyy-MM-dd HH:mm for the ValidityDate.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@11677 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb/src-test/org/ejbca/util/ValidityDateTest.java
+++ modules/ejbca-ejb/src-test/org/ejbca/util/ValidityDateTest.java
@@ -3,16 +3,26 @@ package org.ejbca.util;
 import java.util.Calendar;
 import java.util.Date;
 
+import org.apache.commons.lang.time.FastDateFormat;
+
 import junit.framework.TestCase;
 
 public class ValidityDateTest extends TestCase {
 
+	private String[] timePatterns = {"yyyy-MM-dd HH:mm"};
+	private FastDateFormat fastDateFormat = FastDateFormat.getInstance(timePatterns[0]);
+	
     public ValidityDateTest(String name) { super(name); }
 
     public void setUp() throws Exception { }
     public void tearDown() throws Exception { }
 
 	public void test01ParseStringDates() {
+		
+		Date date = new Date();
+		String dateStr = fastDateFormat.format(date);
+		assertEquals(date.getTime(), ValidityDate.encode(dateStr));
+		
 		
 		// TODO add code to test a period in the format *y *mo *d
 		
@@ -22,7 +32,7 @@ public class ValidityDateTest extends TestCase {
 		assertEquals(Long.parseLong(hexTime), ValidityDate.encode(hexTime));
 		assertNull(ValidityDate.getDateFromString(hexTime));
 
-		Date date = new Date(ValidityDate.encode(hexTime));
+		date = new Date(ValidityDate.encode(hexTime));
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(date);
 		assertNotSame(2038, calendar.get(Calendar.YEAR));
@@ -31,5 +41,14 @@ public class ValidityDateTest extends TestCase {
 		assertEquals(-1, ValidityDate.encode(hexTime));
 		assertNull(ValidityDate.getDateFromString(hexTime));
 	}
+	
+	public void test02FormatStringDates() {
+		
+		Date date = new Date();
+		String dateStr = fastDateFormat.format(date);
+		assertEquals(dateStr, ValidityDate.getString(date.getTime()));
+		
+	}
+	
 	
 }
