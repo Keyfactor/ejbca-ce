c0a3f78bc8f5fa57754e27ee07749d2599dd9162
Mike Agrenius Kushner <mike.kushner@primekey.com>
2011-07-20 15:45:35 +0000

    ECA-2229: Some basic authentication mocks
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@12169 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ejbca-ejb-interface/src/org/cesecore/mock/authentication/SimpleAuthenticationProviderRemote.java
@@ -0,0 +1,32 @@
+/*************************************************************************
+ *                                                                       *
+ *  CESeCore: CE Security Core                                           *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.cesecore.mock.authentication;
+
+import javax.ejb.Remote;
+
+import org.cesecore.authentication.tokens.AuthenticationProvider;
+
+/**
+ * This remote interface represents a trivial implementation of an AuthenticationProvider. This is merely a test resource and shouldn't be confused for real
+ * code.
+ * 
+ * TODO: Remove me when proper authentication infrastructure falls into place.
+ * 
+ * Based on cesecore version:
+ *      SimpleAuthenticationProviderRemote.java 841 2011-05-19 14:14:29Z johane
+ * 
+ * @version $Id$
+ */
+@Remote
+public interface SimpleAuthenticationProviderRemote extends AuthenticationProvider {
+}
