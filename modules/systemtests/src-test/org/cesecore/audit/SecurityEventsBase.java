fa4f2657fed85b629c4c8dfca0f27c90a46b3249
Mike Agrenius Kushner <mike.kushner@primekey.com>
2011-07-27 09:35:18 +0000

    ECA-2228: Added the final secure audit func tests
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@12187 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/systemtests/src-test/org/cesecore/audit/SecurityEventsBase.java
@@ -0,0 +1,83 @@
+/*************************************************************************
+ *                                                                       *
+ *  CESeCore: CE Security Core                                           *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/ 
+package org.cesecore.audit;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import org.cesecore.RoleUsingTestCase;
+import org.cesecore.authorization.control.StandardRules;
+import org.cesecore.authorization.rules.AccessRuleData;
+import org.cesecore.authorization.rules.AccessRuleState;
+import org.cesecore.jndi.JndiHelper;
+import org.cesecore.keys.token.CryptoToken;
+import org.cesecore.keys.token.CryptoTokenFactory;
+import org.cesecore.keys.token.SoftCryptoToken;
+import org.cesecore.roles.RoleData;
+import org.cesecore.roles.access.RoleAccessSessionRemote;
+import org.cesecore.roles.management.RoleManagementSessionRemote;
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ * 
+ * @version $Id$
+ *
+ */
+
+public abstract class SecurityEventsBase extends RoleUsingTestCase {
+
+    public static final String keyAlias = "secretkey";
+    public static final String tokenPin = "userpin";
+    public static final String keyPairAlgorithm = "1024";
+    public static final String keyPairSignAlgorithm = "SHA512withRSA";
+
+    protected RoleAccessSessionRemote roleAccessSession = JndiHelper.getRemoteSession(RoleAccessSessionRemote.class);
+    protected RoleManagementSessionRemote roleManagementSession = JndiHelper.getRemoteSession(RoleManagementSessionRemote.class);
+
+    public static final CryptoToken createTokenWithKeyPair() throws Exception {
+
+        Properties props = new Properties();
+        props.setProperty(CryptoToken.AUTOACTIVATE_PIN_PROPERTY, tokenPin);
+
+        CryptoToken token = CryptoTokenFactory.createCryptoToken(SoftCryptoToken.class.getName(), props, null, 1);
+        token.activate(tokenPin.toCharArray());
+        token.generateKeyPair(keyPairAlgorithm, keyAlias);
+
+        return token;
+    }
+
+    @Before
+    public void setUp() throws Exception{
+    	// Set up base role that can edit roles
+    	setUpAuthTokenAndRole("SecurityAuditTest");
+
+    	// Now we have a role that can edit roles, we can edit this role to include more privileges
+    	RoleData role = roleAccessSession.findRole("SecurityAuditTest");
+
+        // Add rules to the role, for the resource
+        List<AccessRuleData> accessRules = new ArrayList<AccessRuleData>();
+        accessRules.add(new AccessRuleData(role.getRoleName(), StandardRules.AUDITLOGEXPORT.resource(), AccessRuleState.RULE_ACCEPT, true));
+        accessRules.add(new AccessRuleData(role.getRoleName(), StandardRules.AUDITLOGVERIFY.resource(), AccessRuleState.RULE_ACCEPT, true));
+        accessRules.add(new AccessRuleData(role.getRoleName(), StandardRules.AUDITLOGSELECT.resource(), AccessRuleState.RULE_ACCEPT, true));
+        accessRules.add(new AccessRuleData(role.getRoleName(), StandardRules.AUDITLOGMANAGE.resource(), AccessRuleState.RULE_ACCEPT, true));
+        roleManagementSession.addAccessRulesToRole(roleMgmgToken, role, accessRules);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    	tearDownRemoveRole();
+    }
+
+}
---
+++ modules/systemtests/src-test/org/cesecore/audit/log/SecurityEventsLoggerSessionBeanTest.java
@@ -0,0 +1,207 @@
+/*************************************************************************
+ *                                                                       *
+ *  CESeCore: CE Security Core                                           *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.cesecore.audit.log;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.fail;
+import static org.cesecore.util.QueryCriteria.where;
+
+import java.io.File;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang.time.StopWatch;
+import org.apache.log4j.Logger;
+import org.cesecore.audit.AuditLogEntry;
+import org.cesecore.audit.SecurityEventsBase;
+import org.cesecore.audit.audit.AuditLogReportElem;
+import org.cesecore.audit.audit.AuditLogValidationReport;
+import org.cesecore.audit.audit.SecurityEventsAuditorSession;
+import org.cesecore.audit.audit.SecurityEventsAuditorSessionRemote;
+import org.cesecore.audit.enums.EventStatus;
+import org.cesecore.audit.enums.EventTypes;
+import org.cesecore.audit.enums.ModuleTypes;
+import org.cesecore.audit.enums.ServiceTypes;
+import org.cesecore.audit.impl.ExampleClassEventTypes;
+import org.cesecore.audit.impl.ExampleEnumEventTypes;
+import org.cesecore.jndi.JndiHelper;
+import org.cesecore.keys.token.CryptoToken;
+import org.cesecore.util.CryptoProviderTools;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Secure audit logs logger functional tests.
+ * 
+ * Based on cesecore version:
+ *      SecurityEventsLoggerSessionBeanTest.java 920 2011-07-01 11:27:04Z filiper
+ *      
+ * Tests for queued devices (not included in EJBCA) have been culled.
+ * 
+ * @version $Id$
+ * 
+ */
+public class SecurityEventsLoggerSessionBeanTest extends SecurityEventsBase {
+
+    private static final Logger log = Logger.getLogger(SecurityEventsLoggerSessionBeanTest.class);
+    private final SecurityEventsLoggerSession securityEventsLogger = JndiHelper.getRemoteSession(SecurityEventsLoggerSessionRemote.class);
+    private final SecurityEventsAuditorSession securityEventsAuditor = JndiHelper.getRemoteSession(SecurityEventsAuditorSessionRemote.class);
+    private final TxFailureLoggerOperationSessionRemote txFailure = JndiHelper.getRemoteSession(TxFailureLoggerOperationSessionRemote.class);
+
+    @BeforeClass
+    public static void setUpCryptoProvider() {
+        CryptoProviderTools.installBCProvider();
+    }
+
+    @Test
+    public void test01SecureLogWithoutAdditionalDetails() {
+        log.trace(">test01SecureLogWithoutAdditionalDetails");
+        securityEventsLogger.log(EventTypes.AUTHENTICATION, EventStatus.SUCCESS, ModuleTypes.AUTHENTICATION, ServiceTypes.CORE, "authToken");
+        log.trace("<test01SecureLogWithoutAdditionalDetails");
+    }
+
+    @Test
+    public void test02logAppCustomEventTypes() throws Exception {
+        log.trace(">test02logAppCustomEventTypes");
+        securityEventsLogger.log(ExampleEnumEventTypes.NEW_EVENT_TYPE, EventStatus.SUCCESS, ModuleTypes.AUTHENTICATION, ServiceTypes.CORE,
+                "authToken");
+        securityEventsLogger.log(ExampleClassEventTypes.NEW_EVENT_TYPE_CLASS, EventStatus.SUCCESS, ModuleTypes.AUTHENTICATION, ServiceTypes.CORE,
+                "authToken");
+        for (final String logDeviceId : securityEventsAuditor.getQuerySupportingLogDevices()) {
+            final List<? extends AuditLogEntry> lastSignedLogs = securityEventsAuditor.selectAuditLogs(
+                    roleMgmgToken,
+                    1,
+                    10,
+                    where().eq(AuditLogEntry.FIELD_EVENTTYPE, ExampleEnumEventTypes.NEW_EVENT_TYPE.toString()).or()
+                            .eq(AuditLogEntry.FIELD_EVENTTYPE, ExampleClassEventTypes.NEW_EVENT_TYPE_CLASS.toString()), logDeviceId);
+            assertEquals(2, lastSignedLogs.size());
+            for(AuditLogEntry ae: lastSignedLogs) {
+                assertTrue(ae.getEventTypeValue().equals(ExampleEnumEventTypes.NEW_EVENT_TYPE) || 
+                        ae.getEventTypeValue().equals(ExampleClassEventTypes.NEW_EVENT_TYPE_CLASS));
+            }
+        }
+
+        log.trace("<test02logAppCustomEventTypes");
+    }
+
+    @Test
+    public void test03SecurelogWithAdditionalDetails() {
+        log.trace(">test02SecurelogWithAdditionalDetails");
+        final Map<String, Object> details = new LinkedHashMap<String, Object>();
+        final Map<String, String> innerDetails = new LinkedHashMap<String, String>();
+        innerDetails.put("extra", "bar");
+        details.put("foo", innerDetails);
+        securityEventsLogger.log(EventTypes.AUTHENTICATION, EventStatus.SUCCESS, ModuleTypes.AUTHENTICATION, ServiceTypes.CORE, "authToken", "0",
+                "0", "someentityname", details);
+        log.trace("<test02SecurelogWithAdditionalDetails");
+    }
+
+    @Test
+    public void test04SecureMultipleLog() throws Exception {
+        log.trace(">test03SecureMultipleLog");
+        final int THREADS = 50;
+        final int WORKERS = 400;
+        final int TIMEOUT_MS = 30000;
+        final ThreadPoolExecutor workers = (ThreadPoolExecutor) Executors.newFixedThreadPool(THREADS);
+        final StopWatch time = new StopWatch();
+
+        time.start();
+        for (int i = 0; i < WORKERS; i++) {
+            workers.execute(new Runnable() {
+                @Override
+                public void run() {
+                    securityEventsLogger.log(EventTypes.AUTHENTICATION, EventStatus.SUCCESS, ModuleTypes.SECURITY_AUDIT, ServiceTypes.CORE,
+                            "test03SecureMultipleLogAuthToken");
+                }
+            });
+        }
+        while (workers.getCompletedTaskCount() < WORKERS && time.getTime() < TIMEOUT_MS) {
+            Thread.sleep(250);
+        }
+        time.stop();
+        final int completedTaskCount = Long.valueOf(workers.getCompletedTaskCount()).intValue();
+        log.info("securityEventsLogger.log: " + completedTaskCount + " completed in " + time.toString() + " using " + THREADS + " threads.");
+        workers.shutdown();
+
+        for (final String logDeviceId : securityEventsAuditor.getQuerySupportingLogDevices()) {
+            final AuditLogValidationReport report = securityEventsAuditor.verifyLogsIntegrity(roleMgmgToken, new Date(), logDeviceId);
+            assertNotNull(report);
+            final StringBuilder strBuilder = new StringBuilder();
+            for (final AuditLogReportElem error : report.errors()) {
+                strBuilder.append(String.format("invalid sequence: %d %d\n", error.getFirst(), error.getSecond()));
+                for (final String reason : error.getReasons()) {
+                    strBuilder.append(String.format("Reason: %s\n", reason));
+                }
+            }
+            assertTrue("validation report: " + strBuilder.toString(), (report.warnings().size() == 1 || report.warnings().size() == 0)
+                    && report.errors().size() == 0);
+        }
+        log.trace("<test03SecureMultipleLog");
+    }
+
+    @Test
+    public void test05ExportGeneratedLogs() throws Exception {
+        log.trace(">test04ExportGeneratedLogs");
+        final CryptoToken cryptoToken = createTokenWithKeyPair();
+        for (final String logDeviceId : securityEventsAuditor.getQuerySupportingLogDevices()) {
+            final String exportFilename = securityEventsAuditor.exportAuditLogs(roleMgmgToken, cryptoToken, new Date(), true, keyAlias,
+                    keyPairSignAlgorithm, logDeviceId).getExportedFile();
+            assertExportAndSignatureExists(exportFilename);
+        }
+        log.trace("<test04ExportGeneratedLogs");
+    }
+
+    @Test
+    // (expected = Exception.class)
+    public void test06TxFailure() throws Exception {
+        log.trace(">test05TxFailure");
+        try {
+            txFailure.willLaunchExceptionAfterLog();
+            fail("No exception was thrown..");
+        } catch (final Exception e) {
+            // Expected
+        }
+        for (final String logDeviceId : securityEventsAuditor.getQuerySupportingLogDevices()) {
+            final List<? extends AuditLogEntry> list = securityEventsAuditor.selectAuditLogs(roleMgmgToken, 1, 10,
+                    where().like(AuditLogEntry.FIELD_AUTHENTICATION_TOKEN, "TxFailureUser"), logDeviceId);
+            assertEquals("List size is:" + list.size(), 1, list.size());
+        }
+        log.trace("<test05TxFailure");
+    }
+
+   
+
+    @AfterClass
+    public static void setDown() {
+        CryptoProviderTools.removeBCProvider();
+    }
+
+    private void assertExportAndSignatureExists(final String exportFilename) {
+        final File exportFile = new File(exportFilename);
+        assertTrue("file does not exist, " + exportFile.getAbsolutePath(), exportFile.exists());
+        assertTrue("file length is not > 0, " + exportFile.getAbsolutePath(), exportFile.length() > 0);
+        assertTrue("file can not be deleted, " + exportFile.getAbsolutePath(), exportFile.delete());
+        final File signatureFile = new File(String.format("%s.sig", FilenameUtils.removeExtension(exportFile.getAbsolutePath())));
+        assertTrue("signatureFile does not exist, " + signatureFile.getAbsolutePath(), signatureFile.exists());
+        assertTrue("signatureFile length is not > 0, " + signatureFile.getAbsolutePath(), signatureFile.length() > 0);
+        assertTrue("signatureFile can not be deleted, " + signatureFile.getAbsolutePath(), signatureFile.delete());
+    }
+}
