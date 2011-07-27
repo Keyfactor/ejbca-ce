e791c3ccaaccc11da8ceff14380b630ec2452423
Mike Agrenius Kushner <mike.kushner@primekey.com>
2011-07-27 09:45:13 +0000

    ECA-2230: Final files from org.cesecore.authentication
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@12188 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ src/java/org/cesecore/authentication/tokens/UsernamePrincipal.java
@@ -0,0 +1,42 @@
+/*************************************************************************
+ *                                                                       *
+ *  CESeCore: CE Security Core                                           *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.cesecore.authentication.tokens;
+
+import java.security.Principal;
+
+/**
+ * The representation of a user name.  
+ * 
+ * Based on cesecore version:
+ *      UsernamePrincipal.java 948 2011-07-18 09:04:26Z mikek
+ * 
+ * @version $Id$
+ */
+public class UsernamePrincipal implements Principal {
+
+	final String username;
+	
+	public UsernamePrincipal(final String username) {
+		this.username = username;
+	}
+	
+	@Override
+	public String getName() {
+		return username;
+	}
+
+	@Override
+	public String toString() {
+		return username;
+	}
+}
---
+++ src/java/org/cesecore/authentication/tokens/X500PrincipalMatchValue.java
@@ -0,0 +1,38 @@
+/*************************************************************************
+ *                                                                       *
+ *  CESeCore: CE Security Core                                           *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.cesecore.authentication.tokens;
+
+/**
+ * These constants is also used as a priority indicator for access rules. The higher values the higher priority.
+ * 
+ * Based on cesecore version:
+ *      X500PrincipalMatchValue.java 124 2011-01-20 14:41:21Z tomas
+ * 
+ * @version $Id$
+ * 
+ */
+public enum X500PrincipalMatchValue {
+    WITH_COUNTRY(1), WITH_DOMAINCOMPONENT(2), WITH_STATE(3), WITH_LOCALE(4), WITH_ORGANIZATION(5), WITH_ORGANIZATIONUNIT(6), WITH_TITLE(7), WITH_COMMONNAME(
+            8), WITH_UID(9), WITH_DNSERIALNUMBER(10), WITH_SERIALNUMBER(11), WITH_DNEMAIL(12), WITH_RFC822NAME(13), WITH_UPN(14);
+
+    private X500PrincipalMatchValue(int numericValue) {
+        this.numericValue = numericValue;
+    }
+
+    public int getNumericValue() {
+        return numericValue;
+    }
+
+    private int numericValue;
+
+}
