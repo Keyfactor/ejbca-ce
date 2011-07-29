adc0314163ec11af877bd8405c5b1fd367ce7760
Mike Agrenius Kushner <mike.kushner@primekey.com>
2011-07-29 20:47:41 +0000

    ECA-2238: Added EJBCA event types for logging
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@12197 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ejbca-ejb-interface/src/org/ejbca/core/ejb/audit/enums/EjbcaServiceTypes.java
@@ -0,0 +1,31 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.core.ejb.audit.enums;
+
+import org.cesecore.audit.enums.ServiceType;
+
+/**
+ * @version $Id$
+ *
+ */
+public enum EjbcaServiceTypes implements ServiceType {
+    EJBCA;
+    
+    @Override
+    public boolean equals(ServiceType value) {
+        if(value == null) {
+            return false;
+        }
+        return this.toString().equals(value.toString());
+    }
+}
