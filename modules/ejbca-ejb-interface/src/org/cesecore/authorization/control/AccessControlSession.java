24789b395fa9a04d33d5ecb887634895caac2662
Mike Agrenius Kushner <mike.kushner@primekey.com>
2011-08-09 07:51:32 +0000

    ECA-2236: Added getAllRolesAuthorizedToEdit method to AccessControlSession
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@12219 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb-interface/src/org/cesecore/authorization/control/AccessControlSession.java
+++ modules/ejbca-ejb-interface/src/org/cesecore/authorization/control/AccessControlSession.java
@@ -12,15 +12,17 @@
  *************************************************************************/
 package org.cesecore.authorization.control;
 
+import java.util.Collection;
+
 import org.cesecore.authentication.tokens.AuthenticationToken;
+import org.cesecore.roles.RoleData;
 
 /**
  * Main interface for checking authorization. This interface makes use of roles and entity authentication to verify authorization.
  * 
  * See {@link https://www.cesecore.eu/mediawiki/index.php/Functional_Specifications_(ADV_FSP)#Access_Control_2}
  * 
- * Based on cesecore:
- *      AccessControlSession.java 338 2011-02-25 09:46:38Z tomas
+ * Based on cesecore: AccessControlSession.java 338 2011-02-25 09:46:38Z tomas
  * 
  * @version $Id$
  * 
@@ -31,16 +33,25 @@ public interface AccessControlSession {
      * Checks if the current user is authorized for the given resource.
      * 
      * @param resource String identifier of the resource in question.
-     * @return True if user is authorized, false if not. 
+     * @return True if user is authorized, false if not.
      */
     boolean isAuthorized(AuthenticationToken authenticationToken, String resource);
 
-    /** Helper method to clear the local access control rule cache. Normally the cache expires after configured time, 
-     * but when modifying access rules on the local node we can force cache clearing so we don't have to wait.
-     * Other nodes in a cluster will still wait until expire though. 
-     * This should be called whenever "accessTreeUpdateSession.signalForAccessTreeUpdate()" is called,
-     * it can't be called automatically from that method due to circular dependencies.
+    /**
+     * Helper method to clear the local access control rule cache. Normally the cache expires after configured time, but when modifying access rules
+     * on the local node we can force cache clearing so we don't have to wait. Other nodes in a cluster will still wait until expire though. This
+     * should be called whenever "accessTreeUpdateSession.signalForAccessTreeUpdate()" is called, it can't be called automatically from that method
+     * due to circular dependencies.
      */
     void forceCacheExpire();
+
+    /**
+     * Retrieves a list of the roles which the given subject is authorized to edit, by checking if that subject has rights to the CA's behind all access
+     * user aspects in that role, and all CA-based rules
+     * 
+     * @param authenticationToken An authentication token for the subject
+     * @return a list of roles which the subject is authorized to edit.
+     */
+    Collection<RoleData> getAllRolesAuthorizedToEdit(AuthenticationToken authenticationToken);
 
 }
