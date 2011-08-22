7da379bf918eef45909d8c0c9246637262e89ae1
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2011-08-22 12:26:53 +0000

    ECA-2171: two old legacy classes needed to support upgrades from 4.x
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@12321 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ src/java/org/ejbca/util/Base64PutHashMap.java
@@ -0,0 +1,45 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
+package org.ejbca.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.cesecore.util.StringTools;
+
+
+/** Only used for backwards compatibility with earlier versions of EJBCA
+ * @see org.cesecore.util.Base64PutHashMap
+ * 
+ * @version $Id$
+ */
+public class Base64PutHashMap extends HashMap {
+    public Base64PutHashMap() {
+        super();
+    }
+    public Base64PutHashMap(Map m) {
+        super(m);
+    }
+    public Object put(Object key, Object value) {
+        if (value == null) {
+            return super.put(key, value);
+        }
+        if (value instanceof String) {
+            String s = StringTools.putBase64String((String)value);
+            return super.put(key,s);
+        }
+        return super.put(key, value);
+    }
+    
+}
