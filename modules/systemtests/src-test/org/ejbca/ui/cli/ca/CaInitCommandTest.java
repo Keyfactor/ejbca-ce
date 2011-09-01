3773a29b3da7230059f5ab69ade55c737a658b97
Mike Agrenius Kushner <mike.kushner@primekey.com>
2011-09-01 11:44:09 +0000

    ECA-2249: Added header and removed author tag
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@12449 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/systemtests/src-test/org/ejbca/ui/cli/ca/CaInitCommandTest.java
+++ modules/systemtests/src-test/org/ejbca/ui/cli/ca/CaInitCommandTest.java
@@ -1,3 +1,16 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
 package org.ejbca.ui.cli.ca;
 
 import static org.junit.Assert.assertNotNull;
@@ -21,7 +34,6 @@ import org.junit.Test;
 /**
  * System test class for CaInitCommandTest
  * 
- * @author mikek
  * @version $Id$
  */
 public class CaInitCommandTest {
