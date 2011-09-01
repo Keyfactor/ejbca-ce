881aa6907ad468cda170c24b14f247b160cf65e7
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2011-09-01 07:11:31 +0000

    ECA-2249: adding back some files that buggy svn deleted instead of comitting...really. Don't know if it's eclipse or svn that is buggy, but it is something.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@12438 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/systemtests/src/org/cesecore/certificates/ca/CaTestSessionBean.java
@@ -0,0 +1,45 @@
+/*************************************************************************
+ *                                                                       *
+ *  CESeCore: CE Security Core                                           *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.cesecore.certificates.ca;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+
+import org.cesecore.authentication.tokens.AuthenticationToken;
+import org.cesecore.authorization.AuthorizationDeniedException;
+import org.cesecore.jndi.JndiConstants;
+
+/**
+ * Session bean used by functional tests in order to access local EJB interfaces of CaSession
+ * 
+ * @version $Id: CaTestSessionBean.java 841 2011-05-19 14:14:29Z johane $
+ */
+@Stateless(mappedName = JndiConstants.APP_JNDI_PREFIX + "CaTestSessionRemote")
+@TransactionAttribute(TransactionAttributeType.REQUIRED)
+public class CaTestSessionBean implements CaTestSessionRemote {
+
+    @EJB
+    private CaSessionLocal caSession;
+
+	@Override
+	public CA getCA(AuthenticationToken admin, int caid) throws CADoesntExistsException, AuthorizationDeniedException {
+		return caSession.getCA(admin, caid);
+	}
+
+	@Override
+	public CA getCA(AuthenticationToken admin, String name) throws CADoesntExistsException, AuthorizationDeniedException {
+		return caSession.getCA(admin, name);
+	}
+}
---
+++ modules/systemtests/src/org/cesecore/certificates/certificate/InternalCertificateStoreSessionBean.java
@@ -0,0 +1,79 @@
+/*************************************************************************
+ *                                                                       *
+ *  CESeCore: CE Security Core                                           *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.cesecore.certificates.certificate;
+
+import java.math.BigInteger;
+import java.security.cert.Certificate;
+import java.util.Collection;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.cesecore.config.CesecoreConfiguration;
+import org.cesecore.jndi.JndiConstants;
+import org.cesecore.util.CertTools;
+
+/**
+ * @version $Id: InternalCertificateStoreSessionBean.java 1046 2011-08-29 10:38:20Z tomas $
+ */
+@Stateless(mappedName = JndiConstants.APP_JNDI_PREFIX + "InternalCertificateStoreSessionRemote")
+@TransactionAttribute(TransactionAttributeType.REQUIRED)
+public class InternalCertificateStoreSessionBean implements InternalCertificateStoreSessionRemote {
+
+    @PersistenceContext(unitName = CesecoreConfiguration.PERSISTENCE_UNIT)
+    private EntityManager entityManager;
+
+    @EJB
+    CertificateStoreSessionLocal certStore;
+    
+    @Override
+    public void removeCertificate(BigInteger serno) {
+    	if (serno != null) {
+            Collection<CertificateData> coll = CertificateData.findBySerialNumber(entityManager, serno.toString());
+            for (CertificateData certificateData : coll) {
+                entityManager.remove(certificateData);
+            }    		
+    	}
+    }
+
+	@Override
+	public void removeCertificate(String fingerprint) {
+		if (fingerprint != null) {
+	        CertificateData cert = CertificateData.findByFingerprint(entityManager, fingerprint);
+	        if (cert != null) {
+	        	entityManager.remove(cert);
+	        }
+		}
+	}
+
+	@Override
+    public void removeCertificate(Certificate certificate) {
+		if (certificate != null) {
+	        CertificateData certificateData = CertificateData.findByFingerprint(entityManager, CertTools.getFingerprintAsString(certificate));
+	        if (certificateData != null) {
+		        entityManager.remove(certificateData);	        	
+	        }
+		}
+    }
+	
+	@Override
+    public List<Object[]> findExpirationInfo(Collection<String> cas, long activeNotifiedExpireDateMin, long activeNotifiedExpireDateMax, long activeExpireDateMin) {
+    	return certStore.findExpirationInfo(cas, activeNotifiedExpireDateMin, activeNotifiedExpireDateMax, activeExpireDateMin);
+    }
+
+}
