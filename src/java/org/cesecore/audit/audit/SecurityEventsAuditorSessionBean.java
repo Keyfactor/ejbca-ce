33b3f0b946f32b3993888d481fb75564bfb3609f
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2011-09-12 11:32:09 +0000

    ECA-2171: merge of CESECORE-252 to ejbca: use required transaction attribute since a transaction is needed in postgres in order to select Lobs.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@12544 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/cesecore/audit/audit/SecurityEventsAuditorSessionBean.java
+++ src/java/org/cesecore/audit/audit/SecurityEventsAuditorSessionBean.java
@@ -41,17 +41,16 @@ import org.cesecore.keys.token.CryptoToken;
 import org.cesecore.util.QueryCriteria;
 
 /**
- * This class handles secure logs auditing. This class is responsible for checking authorization and delegating a request to the right implementation.
+ * This class handles secure logs auditing. This class is responsible for checking
+ * authorization and delegating a request to the right implementation.
  * 
- * Based on cesecore: SecurityEventsAuditorSessionBean.java 936 2011-07-13 14:46:13Z johane
- * 
- * @version $Id$
+ * @version $Id: SecurityEventsAuditorSessionBean.java 960 2011-07-22 10:14:28Z filiper $
  */
 @Stateless(mappedName = JndiConstants.APP_JNDI_PREFIX + "SecurityEventsAuditorSessionRemote")
 @TransactionAttribute(TransactionAttributeType.REQUIRED)
 public class SecurityEventsAuditorSessionBean implements SecurityEventsAuditorSessionLocal, SecurityEventsAuditorSessionRemote {
-
-    private static final Logger LOG = Logger.getLogger(SecurityEventsAuditorSessionBean.class);
+	
+	private static final Logger LOG = Logger.getLogger(SecurityEventsAuditorSessionBean.class);
 
     @EJB
     private AccessControlSessionLocal accessControlSession;
@@ -59,27 +58,24 @@ public class SecurityEventsAuditorSessionBean implements SecurityEventsAuditorSe
     private IntegrityProtectedAuditorSessionLocal integrityProtectedAuditorSession;
     @EJB
     private QueuedAuditorSessionLocal queuedAuditorSession;
-
+    
     @Override
     @TransactionAttribute(TransactionAttributeType.SUPPORTS)
     public Set<String> getQuerySupportingLogDevices() {
-        return AuditDevicesConfig.getQuerySupportingDeviceIds();
+    	return AuditDevicesConfig.getQuerySupportingDeviceIds();
     }
 
     @Override
-    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
-    public List<? extends AuditLogEntry> selectAuditLogs(final AuthenticationToken token, final int startIndex, final int max,
-            final QueryCriteria criteria, final String logDeviceId) throws AuthorizationDeniedException {
+    // Requires TransactionAttributeType.REQUIRED in order to select large objects (postgres
+    public List<? extends AuditLogEntry> selectAuditLogs(final AuthenticationToken token, final int startIndex, final int max, final QueryCriteria criteria, final String logDeviceId) throws AuthorizationDeniedException {
         assertAuthorization(token, StandardRules.AUDITLOGSELECT.resource());
-        return AuditDevicesConfig.getDevice(getEjbs(), logDeviceId).selectAuditLogs(token, startIndex, max, criteria,
-                AuditDevicesConfig.getProperties(logDeviceId));
+    	return AuditDevicesConfig.getDevice(getEjbs(), logDeviceId).selectAuditLogs(token, startIndex, max, criteria, AuditDevicesConfig.getProperties(logDeviceId));
     }
 
     @Override
-    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
+    // Requires TransactionAttributeType.REQUIRED in order to select large objects (postgres
     public AuditLogExportReport exportAuditLogs(final AuthenticationToken token, final CryptoToken cryptoToken, final Date timestamp,
-            final boolean deleteAfterExport, final String keyAlias, final String algorithm, final String logDeviceId)
-            throws AuditLogExporterException, AuthorizationDeniedException {
+            final boolean deleteAfterExport, final String keyAlias, final String algorithm, final String logDeviceId) throws AuditLogExporterException, AuthorizationDeniedException {
         final HashMap<String, Object> details = new LinkedHashMap<String, Object>();
         details.put(SigningFileOutputStream.EXPORT_SIGN_KEYALIAS, keyAlias);
         details.put(SigningFileOutputStream.EXPORT_SIGN_ALG, algorithm);
@@ -87,7 +83,7 @@ public class SecurityEventsAuditorSessionBean implements SecurityEventsAuditorSe
     }
 
     @Override
-    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
+    // Requires TransactionAttributeType.REQUIRED in order to select large objects (postgres
     public AuditLogExportReport exportAuditLogs(final AuthenticationToken token, final CryptoToken cryptoToken, final Date timestamp,
             final boolean deleteAfterExport, final String keyAlias, final String algorithm, final Certificate certificate, final String logDeviceId)
             throws AuditLogExporterException, AuthorizationDeniedException {
@@ -99,61 +95,59 @@ public class SecurityEventsAuditorSessionBean implements SecurityEventsAuditorSe
     }
 
     @Override
-    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
+    // Requires TransactionAttributeType.REQUIRED in order to select large objects (postgres
     public AuditLogExportReport exportAuditLogs(final AuthenticationToken token, final CryptoToken cryptoToken, final Date timestamp,
-            final boolean deleteAfterExport, final Map<String, Object> signatureDetails, final String logDeviceId) throws AuditLogExporterException,
-            AuthorizationDeniedException {
-        // StandardRules.AUDITLOGEXPORT export implies StandardRules.AUDITLOGVERIFY.
+            final boolean deleteAfterExport, final Map<String, Object> signatureDetails, final String logDeviceId) throws AuditLogExporterException, AuthorizationDeniedException {
+    	// StandardRules.AUDITLOGEXPORT export implies StandardRules.AUDITLOGVERIFY.
         assertAuthorization(token, StandardRules.AUDITLOGEXPORT.resource());
         LOG.info("Export of audit logs from device " + logDeviceId + " requested.");
         final Class<? extends AuditExporter> exporter = AuditDevicesConfig.getExporter(logDeviceId);
         final Properties properties = AuditDevicesConfig.getProperties(logDeviceId);
-        return AuditDevicesConfig.getDevice(getEjbs(), logDeviceId).exportAuditLogs(token, cryptoToken, timestamp, deleteAfterExport,
-                signatureDetails, properties, exporter);
+    	return AuditDevicesConfig.getDevice(getEjbs(), logDeviceId).exportAuditLogs(token, cryptoToken, timestamp, deleteAfterExport, signatureDetails, properties, exporter);
     }
 
     @Override
-    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
-    public AuditLogValidationReport verifyLogsIntegrity(final AuthenticationToken token, final Date timestamp, final String logDeviceId)
-            throws AuditLogValidatorException, AuthorizationDeniedException {
-        assertAuthorization(token, StandardRules.AUDITLOGVERIFY.resource());
+    // Requires TransactionAttributeType.REQUIRED in order to select large objects (postgres
+    public AuditLogValidationReport verifyLogsIntegrity(final AuthenticationToken token, final Date timestamp, final String logDeviceId) throws AuditLogValidatorException, AuthorizationDeniedException {
+    	assertAuthorization(token, StandardRules.AUDITLOGVERIFY.resource());
         LOG.info("Validation of audit logs in device " + logDeviceId + " requested.");
-        return AuditDevicesConfig.getDevice(getEjbs(), logDeviceId).verifyLogsIntegrity(token, timestamp,
-                AuditDevicesConfig.getProperties(logDeviceId));
+    	return AuditDevicesConfig.getDevice(getEjbs(), logDeviceId).verifyLogsIntegrity(token, timestamp, AuditDevicesConfig.getProperties(logDeviceId));
     }
 
-    @Override
-    public void prepareReset() throws AuditLogResetException {
-        LogServiceState.INSTANCE.disable();
-        for (final String loggerId : AuditDevicesConfig.getAllDeviceIds()) {
-            AuditDevicesConfig.getDevice(getEjbs(), loggerId).prepareReset();
-        }
-    }
 
-    @Override
-    public void reset() throws AuditLogResetException {
+	@Override
+	public void prepareReset() throws AuditLogResetException {
+		LogServiceState.INSTANCE.disable();
         for (final String loggerId : AuditDevicesConfig.getAllDeviceIds()) {
-            AuditDevicesConfig.getDevice(getEjbs(), loggerId).reset();
+    		AuditDevicesConfig.getDevice(getEjbs(), loggerId).prepareReset();
         }
-        // should be called after the reset because if we enable log before might happen that a log call runs before reset
+	}
+
+	@Override
+	public void reset() throws AuditLogResetException {
+        for (final String loggerId : AuditDevicesConfig.getAllDeviceIds()) {
+    		AuditDevicesConfig.getDevice(getEjbs(), loggerId).reset();
+        }
+        //should be called after the reset because if we enable log before might happen that a log call runs before reset
         LogServiceState.INSTANCE.enable();
-    }
+	}
 
-    /** Assert that we are authorized to the requested resource. */
+	/** Assert that we are authorized to the requested resource. */
     private void assertAuthorization(final AuthenticationToken token, final String accessRule) throws AuthorizationDeniedException {
         if (!accessControlSession.isAuthorized(token, accessRule)) {
-            throw new AuthorizationDeniedException("not authorized to: " + token.toString());
-        }
+			throw new AuthorizationDeniedException("not authorized to: "+ token.toString());				
+		} 
     }
 
     /**
-     * Propagate the injected SSBs, since we can't use application server agnostic EJB lookup in EJB 3.0. With EJB 3.1 this should be delegated to
-     * each implementation where the implementation specific SSBs can be looked up.
+     * Propagate the injected SSBs, since we can't use application server agnostic EJB lookup in EJB 3.0.
+     * With EJB 3.1 this should be delegated to each implementation where the implementation specific SSBs
+     * can be looked up.
      */
     private Map<Class<?>, Object> getEjbs() {
-        final Map<Class<?>, Object> ejbs = new HashMap<Class<? extends Object>, Object>();
-        ejbs.put(IntegrityProtectedAuditorSessionLocal.class, integrityProtectedAuditorSession);
-        ejbs.put(QueuedAuditorSessionLocal.class, queuedAuditorSession);
-        return ejbs;
+    	final Map<Class<?>, Object> ejbs = new HashMap<Class<? extends Object>, Object>();
+    	ejbs.put(IntegrityProtectedAuditorSessionLocal.class, integrityProtectedAuditorSession);
+    	ejbs.put(QueuedAuditorSessionLocal.class, queuedAuditorSession);
+    	return ejbs;
     }
 }
