911357e21868d8264cc7d7b3fbb654da96ab9001
primelars <USER@primekey.com>
2011-09-15 13:05:48 +0000

    ECA-2269: tested with wscli but no junit yet.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@12588 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ src/java/org/ejbca/util/cert/OID.java
@@ -0,0 +1,57 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.util.cert;
+
+import java.util.StringTokenizer;
+
+import org.bouncycastle.asn1.DERObjectIdentifier;
+
+/**
+ * Utilities for to be used on an Object Identifier (OID).
+ * 
+ * @author Lars Silvén
+ * @version $Id$
+ */
+public class OID {
+	/**
+	 * Tests if the beginning of a string identifies an OID.
+	 * @param s the string to be tested.
+	 * @return true if s is a OID or if first part of s is a OID and the first character after the OID is '.' in s.
+	 */
+	@SuppressWarnings("unused")
+	public static boolean isStartingWithValidOID(String s) {
+		if ( s==null || s.length()<1 ) {
+			return false;
+		}
+		final StringTokenizer st = new StringTokenizer(s, ".");
+		String sOID = "";
+		while( st.hasMoreTokens() ) {
+			final String token = st.nextToken();
+			try {
+				Integer.parseInt(token);
+			} catch ( NumberFormatException e ) {
+				break;
+			}
+			if ( sOID.length()>0 ) {
+				sOID += ".";
+			}
+			sOID+=token;
+		}
+		try {
+			new DERObjectIdentifier(sOID);
+		} catch ( IllegalArgumentException e ) {
+			return false;
+		}
+		return true;
+	}
+}
