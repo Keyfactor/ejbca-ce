6bb86ae73ccb2fcdc8e5cb09c86222093a01fa23
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2011-09-23 10:57:31 +0000

    ECA-2171: merge of CESECORE-80 to EJBCA. Work on key extraction.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@12697 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-entity/src/org/cesecore/dbprotection/CachedCryptoToken.java
+++ modules/ejbca-entity/src/org/cesecore/dbprotection/CachedCryptoToken.java
@@ -38,49 +38,49 @@ import org.cesecore.keys.token.PrivateKeyNotExtractableException;
 /**
  * Wrapper for a CryptoToken that caches references to private and secret keys.
  * Used to speed up database integrity protection, where the key should always
- * be available.  
- * 
+ * be available.
+ *
  * @version $Id$
- * 
+ *
  */
 public class CachedCryptoToken implements CryptoToken {
-	
-	private static final long serialVersionUID = 1L;
 
-	private final CryptoToken wrappedCryptoToken;
-	private Key cachedKey = null;
-	private PrivateKey cachedPrivateKey = null;
-	private String cachedSignProviderName = null;
-	
-	public CachedCryptoToken(final CryptoToken wrappedCryptoToken) {
-		this.wrappedCryptoToken = wrappedCryptoToken;
-	}
+    private static final long serialVersionUID = 1L;
 
-	@Override
-	public void activate(char[] authenticationcode) throws CryptoTokenOfflineException, CryptoTokenAuthenticationFailedException {
-		wrappedCryptoToken.activate(authenticationcode);
-	}
+    private final CryptoToken wrappedCryptoToken;
+    private Key cachedKey = null;
+    private PrivateKey cachedPrivateKey = null;
+    private String cachedSignProviderName = null;
 
-	@Override
-	public void deactivate() {
-		wrappedCryptoToken.deactivate();
-	}
+    public CachedCryptoToken(final CryptoToken wrappedCryptoToken) {
+        this.wrappedCryptoToken = wrappedCryptoToken;
+    }
 
-	@Override
-	public void deleteEntry(char[] authenticationcode, String alias) throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException, CryptoTokenOfflineException {
-		wrappedCryptoToken.deleteEntry(authenticationcode, alias);
-	}
+    @Override
+    public void activate(char[] authenticationcode) throws CryptoTokenOfflineException, CryptoTokenAuthenticationFailedException {
+        wrappedCryptoToken.activate(authenticationcode);
+    }
 
-	@Override
-	public boolean doPermitExtractablePrivateKey() {
-		return wrappedCryptoToken.doPermitExtractablePrivateKey();
-	}
+    @Override
+    public void deactivate() {
+        wrappedCryptoToken.deactivate();
+    }
 
-	@Override
-	public byte[] extractKey(String privKeyTransform, String encryptionKeyAlias, String privateKeyAlias) throws NoSuchAlgorithmException, NoSuchPaddingException,
-			NoSuchProviderException, InvalidKeyException, IllegalBlockSizeException, CryptoTokenOfflineException, PrivateKeyNotExtractableException, InvalidAlgorithmParameterException {
-		return wrappedCryptoToken.extractKey(privKeyTransform, encryptionKeyAlias, privateKeyAlias);
-	}
+    @Override
+    public void deleteEntry(char[] authenticationcode, String alias) throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException, CryptoTokenOfflineException {
+        wrappedCryptoToken.deleteEntry(authenticationcode, alias);
+    }
+
+    @Override
+    public boolean doPermitExtractablePrivateKey() {
+        return wrappedCryptoToken.doPermitExtractablePrivateKey();
+    }
+
+    @Override
+    public byte[] extractKey(String privKeyTransform, String encryptionKeyAlias, String privateKeyAlias) throws NoSuchAlgorithmException, NoSuchPaddingException,
+            NoSuchProviderException, InvalidKeyException, IllegalBlockSizeException, CryptoTokenOfflineException, PrivateKeyNotExtractableException, InvalidAlgorithmParameterException {
+        return wrappedCryptoToken.extractKey(privKeyTransform, encryptionKeyAlias, privateKeyAlias);
+    }
 
     @Override
     public byte[] extractKey(String privKeyTransform, AlgorithmParameterSpec spec, String encryptionKeyAlias, String privateKeyAlias) throws NoSuchAlgorithmException, NoSuchPaddingException,
@@ -88,103 +88,103 @@ public class CachedCryptoToken implements CryptoToken {
         return wrappedCryptoToken.extractKey(privKeyTransform, spec, encryptionKeyAlias, privateKeyAlias);
     }
 
-	@Override
-	public void generateKey(String algorithm, int keysize, String alias) throws NoSuchAlgorithmException, NoSuchProviderException,
-			KeyStoreException, CryptoTokenOfflineException, InvalidKeyException, InvalidAlgorithmParameterException,
-			SignatureException, CertificateException, IOException, NoSuchPaddingException, IllegalBlockSizeException {
-		wrappedCryptoToken.generateKey(algorithm, keysize, alias);
-	}
-
-	@Override
-	public void generateKeyPair(String keySpec, String alias) throws NoSuchAlgorithmException, NoSuchProviderException,
-			InvalidAlgorithmParameterException, InvalidKeyException, SignatureException, KeyStoreException, CertificateException,
-			IOException, CryptoTokenOfflineException {
-		wrappedCryptoToken.generateKeyPair(keySpec, alias);
-	}
-
-	@Override
-	public void generateKeyPair(AlgorithmParameterSpec spec, String alias) throws NoSuchAlgorithmException, NoSuchProviderException,
-			InvalidAlgorithmParameterException, InvalidKeyException, SignatureException, KeyStoreException, CertificateException,
-			IOException, CryptoTokenOfflineException {
-		wrappedCryptoToken.generateKeyPair(spec, alias);
-	}
-
-	@Override
-	public String getEncProviderName() {
-		return wrappedCryptoToken.getEncProviderName();
-	}
-
-	@Override
-	public int getId() {
-		return wrappedCryptoToken.getId();
-	}
-
-	@Override
-	public Key getKey(String alias) throws CryptoTokenOfflineException {
-		if (cachedKey==null) {
-			cachedKey = wrappedCryptoToken.getKey(alias);
-		}
-		return cachedKey;
-	}
-
-	@Override
-	public PrivateKey getPrivateKey(String alias) throws CryptoTokenOfflineException {
-		if (cachedPrivateKey==null) {
-			cachedPrivateKey = wrappedCryptoToken.getPrivateKey(alias);
-		}
-		return cachedPrivateKey;
-	}
-
-	@Override
-	public Properties getProperties() {
-		return wrappedCryptoToken.getProperties();
-	}
-
-	@Override
-	public PublicKey getPublicKey(String alias) throws CryptoTokenOfflineException {
-		return wrappedCryptoToken.getPublicKey(alias);
-	}
-
-	@Override
-	public String getSignProviderName() {
-		if (cachedSignProviderName==null) {
-			cachedSignProviderName = wrappedCryptoToken.getSignProviderName();
-		}
-		return cachedSignProviderName;
-	}
-
-	@Override
-	public byte[] getTokenData() {
-		return wrappedCryptoToken.getTokenData();
-	}
-
-	@Override
-	public int getTokenStatus() {
-		return wrappedCryptoToken.getTokenStatus();
-	}
-
-	@Override
-	public void init(Properties properties, byte[] data, int id) throws Exception {
-		wrappedCryptoToken.init(properties, data, id);
-	}
-
-	@Override
-	public void reset() {
-		wrappedCryptoToken.reset();
-	}
-
-	@Override
-	public void setProperties(Properties properties) {
-		wrappedCryptoToken.setProperties(properties);
-	}
-
-	@Override
-	public void testKeyPair(PrivateKey privateKey, PublicKey publicKey) throws InvalidKeyException, NoSuchProviderException {
-		wrappedCryptoToken.testKeyPair(privateKey, publicKey);
-	}
+    @Override
+    public void generateKey(String algorithm, int keysize, String alias) throws NoSuchAlgorithmException, NoSuchProviderException,
+            KeyStoreException, CryptoTokenOfflineException, InvalidKeyException, InvalidAlgorithmParameterException,
+            SignatureException, CertificateException, IOException, NoSuchPaddingException, IllegalBlockSizeException {
+        wrappedCryptoToken.generateKey(algorithm, keysize, alias);
+    }
 
     @Override
-    public Enumeration<String> getAliases() throws KeyStoreException, CryptoTokenOfflineException {        
+    public void generateKeyPair(String keySpec, String alias) throws NoSuchAlgorithmException, NoSuchProviderException,
+            InvalidAlgorithmParameterException, InvalidKeyException, SignatureException, KeyStoreException, CertificateException,
+            IOException, CryptoTokenOfflineException {
+        wrappedCryptoToken.generateKeyPair(keySpec, alias);
+    }
+
+    @Override
+    public void generateKeyPair(AlgorithmParameterSpec spec, String alias) throws NoSuchAlgorithmException, NoSuchProviderException,
+            InvalidAlgorithmParameterException, InvalidKeyException, SignatureException, KeyStoreException, CertificateException,
+            IOException, CryptoTokenOfflineException {
+        wrappedCryptoToken.generateKeyPair(spec, alias);
+    }
+
+    @Override
+    public String getEncProviderName() {
+        return wrappedCryptoToken.getEncProviderName();
+    }
+
+    @Override
+    public int getId() {
+        return wrappedCryptoToken.getId();
+    }
+
+    @Override
+    public Key getKey(String alias) throws CryptoTokenOfflineException {
+        if (cachedKey==null) {
+            cachedKey = wrappedCryptoToken.getKey(alias);
+        }
+        return cachedKey;
+    }
+
+    @Override
+    public PrivateKey getPrivateKey(String alias) throws CryptoTokenOfflineException {
+        if (cachedPrivateKey==null) {
+            cachedPrivateKey = wrappedCryptoToken.getPrivateKey(alias);
+        }
+        return cachedPrivateKey;
+    }
+
+    @Override
+    public Properties getProperties() {
+        return wrappedCryptoToken.getProperties();
+    }
+
+    @Override
+    public PublicKey getPublicKey(String alias) throws CryptoTokenOfflineException {
+        return wrappedCryptoToken.getPublicKey(alias);
+    }
+
+    @Override
+    public String getSignProviderName() {
+        if (cachedSignProviderName==null) {
+            cachedSignProviderName = wrappedCryptoToken.getSignProviderName();
+        }
+        return cachedSignProviderName;
+    }
+
+    @Override
+    public byte[] getTokenData() {
+        return wrappedCryptoToken.getTokenData();
+    }
+
+    @Override
+    public int getTokenStatus() {
+        return wrappedCryptoToken.getTokenStatus();
+    }
+
+    @Override
+    public void init(Properties properties, byte[] data, int id) throws Exception {
+        wrappedCryptoToken.init(properties, data, id);
+    }
+
+    @Override
+    public void reset() {
+        wrappedCryptoToken.reset();
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+        wrappedCryptoToken.setProperties(properties);
+    }
+
+    @Override
+    public void testKeyPair(PrivateKey privateKey, PublicKey publicKey) throws InvalidKeyException, NoSuchProviderException {
+        wrappedCryptoToken.testKeyPair(privateKey, publicKey);
+    }
+
+    @Override
+    public Enumeration<String> getAliases() throws KeyStoreException, CryptoTokenOfflineException {
         return wrappedCryptoToken.getAliases();
     }
 }
