d9d137b9f39178e02d9c9fc814f2f812a310d4b8
Johan Eklund <johan.eklund@primekey.com>
2011-10-03 11:35:44 +0000

    NoTicket. Use proper license header.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@12791 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb/src-test/org/ejbca/util/HexTest.java
+++ modules/ejbca-ejb/src-test/org/ejbca/util/HexTest.java
@@ -1,9 +1,15 @@
-/*
- * Created on 2005-jun-11
- *
- * To change the template for this generated file go to
- * Window - Preferences - Java - Code Generation - Code and Comments
- */
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.ejbca.util;
 
 import static org.junit.Assert.assertEquals;
