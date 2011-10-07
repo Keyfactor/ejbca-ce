9d4a589efd2cf907db5d18497698459a2f6104bf
Johan Eklund <johan.eklund@primekey.com>
2011-10-07 16:15:00 +0000

    ECA-2307 Merge and extension of CESECORE-268: Reduce memory consumption when using InternalResouces.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@12864 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb/src-test/org/cesecore/internal/InternalResourcesTest.java
+++ modules/ejbca-ejb/src-test/org/cesecore/internal/InternalResourcesTest.java
@@ -33,9 +33,11 @@ public class InternalResourcesTest {
         InternalResources intres = new InternalResources(TEST_RESOURCE_LOCATION);
         String res = intres.getLocalizedMessage("raadmin.testmsg");
         assertEquals("Test ENG", res);
+        assertEquals("Test ENG", intres.getLocalizedMessageCs("raadmin.testmsg").toString());
         // This message will only exist in the secondary language file
         res = intres.getLocalizedMessage("raadmin.testmsgsv");
         assertEquals("Test SV", res);
+        assertEquals("Test SV", intres.getLocalizedMessageCs("raadmin.testmsgsv").toString());
     }
 
     @Test
@@ -43,6 +45,7 @@ public class InternalResourcesTest {
         InternalResources intres = new InternalResources(TEST_RESOURCE_LOCATION);
         String res = intres.getLocalizedMessage("raadmin.foo");
         assertEquals("raadmin.foo", res);
+        assertEquals("raadmin.foo", intres.getLocalizedMessageCs("raadmin.foo").toString());
     }
 
     @Test
@@ -50,6 +53,7 @@ public class InternalResourcesTest {
         InternalResources intres = new InternalResources(TEST_RESOURCE_LOCATION);
         String res = intres.getLocalizedMessage("raadmin.testparams", new Long(1), Integer.valueOf(3), "hi", Boolean.valueOf(true), "bye");
         assertEquals("Test 1 3 hi true bye message 1", res);
+        assertEquals("Test 1 3 hi true bye message 1", intres.getLocalizedMessageCs("raadmin.testparams", new Long(1), Integer.valueOf(3), "hi", Boolean.valueOf(true), "bye").toString());
     }
 
     @Test
@@ -57,9 +61,11 @@ public class InternalResourcesTest {
         InternalResources intres = new InternalResources(TEST_RESOURCE_LOCATION);
         String res = intres.getLocalizedMessage("raadmin.testparams", null, Integer.valueOf(3), null, Boolean.valueOf(true), "bye");
         assertEquals("Test  3  true bye message ", res);
+        assertEquals("Test  3  true bye message ", intres.getLocalizedMessageCs("raadmin.testparams", null, Integer.valueOf(3), null, Boolean.valueOf(true), "bye").toString());
 
         res = intres.getLocalizedMessage("raadmin.testparams");
         assertEquals("Test      message ", res);
+        assertEquals("Test      message ", intres.getLocalizedMessageCs("raadmin.testparams").toString());
     }
 
     @Test
@@ -67,9 +73,9 @@ public class InternalResourcesTest {
         InternalResources intres = new InternalResources(TEST_RESOURCE_LOCATION);
         String res = intres.getLocalizedMessage("raadmin.testmsgsv");
         assertEquals("Test SV", res);
+        assertEquals("Test SV", intres.getLocalizedMessageCs("raadmin.testmsgsv").toString());
         res = intres.getLocalizedMessage("raadmin.testmsgsv", "foo $bar \\haaaar");
         assertEquals("Test SV", res);
-
+        assertEquals("Test SV", intres.getLocalizedMessageCs("raadmin.testmsgsv", "foo $bar \\haaaar").toString());
     }
-
 }
--- modules/ejbca-ejb/src-test/org/ejbca/core/model/InternalEjbcaResourcesTest.java
+++ modules/ejbca-ejb/src-test/org/ejbca/core/model/InternalEjbcaResourcesTest.java
@@ -31,9 +31,11 @@ public class InternalEjbcaResourcesTest {
         InternalEjbcaResources intres = new InternalEjbcaResources(TEST_RESOURCE_PATH);
         String res = intres.getLocalizedMessage("test.testmsg");
         assertEquals("Test ENG", res);
+        assertEquals("Test ENG", intres.getLocalizedMessageCs("test.testmsg").toString());
         // This message will only exist in the secondary language file
         res = intres.getLocalizedMessage("test.testmsgsv");
         assertEquals("Test SV", res);
+        assertEquals("Test SV", intres.getLocalizedMessageCs("test.testmsgsv").toString());
     }
 
 	@Test
@@ -41,6 +43,7 @@ public class InternalEjbcaResourcesTest {
         InternalEjbcaResources intres = new InternalEjbcaResources(TEST_RESOURCE_PATH);
         String res = intres.getLocalizedMessage("test.foo");
         assertEquals("test.foo", res);
+        assertEquals("test.foo", intres.getLocalizedMessageCs("test.foo").toString());
     }
 
 	@Test
@@ -48,6 +51,7 @@ public class InternalEjbcaResourcesTest {
         InternalEjbcaResources intres = new InternalEjbcaResources(TEST_RESOURCE_PATH);
         String res = intres.getLocalizedMessage("test.testparams", new Long(1), new Integer(3), "hi", new Boolean(true), "bye");
         assertEquals("Test 1 3 hi true bye message 1", res);
+        assertEquals("Test 1 3 hi true bye message 1", intres.getLocalizedMessageCs("test.testparams", new Long(1), new Integer(3), "hi", new Boolean(true), "bye").toString());
     }
 
 	@Test
@@ -55,9 +59,11 @@ public class InternalEjbcaResourcesTest {
         InternalEjbcaResources intres = new InternalEjbcaResources(TEST_RESOURCE_PATH);
         String res = intres.getLocalizedMessage("test.testparams", null, new Integer(3), null, new Boolean(true), "bye");
         assertEquals("Test  3  true bye message ", res);
+        assertEquals("Test  3  true bye message ", intres.getLocalizedMessageCs("test.testparams", null, new Integer(3), null, new Boolean(true), "bye").toString());
 
         res = intres.getLocalizedMessage("test.testparams");
         assertEquals("Test      message ", res);
+        assertEquals("Test      message ", intres.getLocalizedMessageCs("test.testparams").toString());
     }
 
 	@Test
@@ -65,8 +71,10 @@ public class InternalEjbcaResourcesTest {
         InternalEjbcaResources intres = new InternalEjbcaResources(TEST_RESOURCE_PATH);
         String res = intres.getLocalizedMessage("test.testmsgsv");
         assertEquals("Test SV", res);
+        assertEquals("Test SV", intres.getLocalizedMessageCs("test.testmsgsv").toString());
         res = intres.getLocalizedMessage("test.testmsgsv", "foo $bar \\haaaar");
         assertEquals("Test SV", res);
+        assertEquals("Test SV", intres.getLocalizedMessageCs("test.testmsgsv", "foo $bar \\haaaar").toString());
     }
     
 	@Test
@@ -74,6 +82,7 @@ public class InternalEjbcaResourcesTest {
         InternalEjbcaResources intres = new InternalEjbcaResources(TEST_RESOURCE_PATH);
         String res = intres.getLocalizedMessage("raadmin.testparams", new Long(1), Integer.valueOf(3), "hi", new Boolean(true), "bye");
         assertEquals("Test 1 3 hi true bye message 1", res);    	
+        assertEquals("Test 1 3 hi true bye message 1", intres.getLocalizedMessageCs("raadmin.testparams", new Long(1), Integer.valueOf(3), "hi", new Boolean(true), "bye").toString());
     }
 
 }
