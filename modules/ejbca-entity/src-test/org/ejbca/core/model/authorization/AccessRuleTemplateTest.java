10893ac534737153834653c2d6042dca18b82506
Mike Agrenius Kushner <mike.kushner@primekey.com>
2011-10-19 12:36:34 +0000

    ECA-2311: Added a couple of more asserts and some documentation to the test case.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@12930 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-entity/src-test/org/ejbca/core/model/authorization/AccessRuleTemplateTest.java
+++ modules/ejbca-entity/src-test/org/ejbca/core/model/authorization/AccessRuleTemplateTest.java
@@ -12,6 +12,7 @@
  *************************************************************************/
 package org.ejbca.core.model.authorization;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.cesecore.authorization.rules.AccessRuleData;
@@ -24,11 +25,19 @@ import org.junit.Test;
  */
 public class AccessRuleTemplateTest {
 
+    /**
+     * This test checks the compareToAccessRuleData method in AccessRuleTemplate, making sure 
+     * that a rule and a template with the same states and recursive modes return true. 
+     */
     @Test
     public void TestCompareToAccessRuleData() {
         AccessRuleTemplate template = new AccessRuleTemplate("/monkey", AccessRuleState.RULE_ACCEPT, false);
         AccessRuleData rule = new AccessRuleData("ape", "/monkey", AccessRuleState.RULE_ACCEPT, false);
         assertTrue(template.compareToAccessRuleData(rule));
+        rule = new AccessRuleData("ape", "/monkey", AccessRuleState.RULE_ACCEPT, true);
+        assertFalse(template.compareToAccessRuleData(rule));
+        rule = new AccessRuleData("ape", "/monkey", AccessRuleState.RULE_DECLINE, false);
+        assertFalse(template.compareToAccessRuleData(rule));
     }
 
 }
