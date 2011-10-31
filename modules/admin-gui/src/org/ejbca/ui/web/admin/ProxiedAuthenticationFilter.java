98e4f8d72921c1fc402fce5e6b0db2718e53d01b
Johan Eklund <johan.eklund@primekey.com>
2011-10-31 12:55:01 +0000

    ECA-2332: Comment in properties file. Use debug instead of info when debug logging.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@13050 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/admin-gui/src/org/ejbca/ui/web/admin/ProxiedAuthenticationFilter.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/ProxiedAuthenticationFilter.java
@@ -68,7 +68,7 @@ public class ProxiedAuthenticationFilter implements Filter {
 				if (username != null) {
 				    final EjbLocalHelper ejb = new EjbLocalHelper();
 				    if (log.isDebugEnabled()) {
-	                    log.info("No client certificate supplied through SSL/TLS. Trying alternative certificate emulation lookup for subject '" + username + "'.");
+	                    log.debug("No client certificate supplied through SSL/TLS. Trying alternative certificate emulation lookup for subject '" + username + "'.");
 				    }
 					final Collection<Certificate> userCerts = ejb.getCertificateStoreSession().findCertificatesByUsernameAndStatus(username, CertificateConstants.CERT_ACTIVE);
 					Date latestestIssuance = null;
