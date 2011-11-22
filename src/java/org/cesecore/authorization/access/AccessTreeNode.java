53ffc79a28252868f1be43e3cee0068df98309b9
Mike Agrenius Kushner <mike.kushner@primekey.com>
2011-11-22 14:39:25 +0000

    NoTicket: Adding missing line to header.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@13201 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/cesecore/authorization/access/AccessTreeNode.java
+++ src/java/org/cesecore/authorization/access/AccessTreeNode.java
@@ -1,5 +1,6 @@
 /*************************************************************************
  *                                                                       *
+ *  CESeCore: CE Security Core                                           *
  *                                                                       *
  *  This software is free software; you can redistribute it and/or       *
  *  modify it under the terms of the GNU Lesser General Public           *
