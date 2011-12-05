76d34519758088739161b4e686b92b7fd32555c9
Mike Agrenius Kushner <mike.kushner@primekey.com>
2011-12-05 09:16:34 +0000

    NoTicket: Removing unused imports
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@13253 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb/src-test/org/cesecore/util/CertToolsTest.java
+++ modules/ejbca-ejb/src-test/org/cesecore/util/CertToolsTest.java
@@ -46,13 +46,10 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.bouncycastle.asn1.ASN1EncodableVector;
-import org.bouncycastle.asn1.ASN1InputStream;
 import org.bouncycastle.asn1.ASN1Sequence;
 import org.bouncycastle.asn1.ASN1Set;
-import org.bouncycastle.asn1.ASN1TaggedObject;
 import org.bouncycastle.asn1.DEREncodable;
 import org.bouncycastle.asn1.DERIA5String;
-import org.bouncycastle.asn1.DERObject;
 import org.bouncycastle.asn1.DERObjectIdentifier;
 import org.bouncycastle.asn1.DERSequence;
 import org.bouncycastle.asn1.DERSet;
