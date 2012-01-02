67ef4ce6efc5ecf92602d3f887281532353ac600
Mike Agrenius Kushner <mike.kushner@primekey.com>
2012-01-02 16:00:28 +0000

    NoTicket: Fixed some code that I broke earlier =)
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@13640 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/admin-gui/src/org/ejbca/ui/web/admin/configuration/EjbcaJSFLanguageResource.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/configuration/EjbcaJSFLanguageResource.java
@@ -1,29 +1,89 @@
 package org.ejbca.ui.web.admin.configuration;
 
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+import javax.ejb.EJBException;
+
 import org.ejbca.util.HTMLTools;
 
 /**
  * Class used to retrieve EJBCA language resources in JSF views
  * 
  * Implements a Map used for retrieving resources.
- * @author Philip Vendil
  * @version $Id$
  */
-public class EjbcaJSFLanguageResource {
+public class EjbcaJSFLanguageResource implements Map<String, String> {
 
 	private EjbcaWebBean ejbcawebbean;
 	
 	public EjbcaJSFLanguageResource(EjbcaWebBean ejbcawebbean){
 		this.ejbcawebbean = ejbcawebbean;
 	}
+	
+	@Override
+	public void clear() {
+		throw new EJBException("Method clear not supported");
+	}
 
+	@Override
 	public boolean containsKey(Object arg0) {
 		return ejbcawebbean.getText((String) arg0) != null;
 	}
 
-	public String get(String arg0) {
-		String str = ejbcawebbean.getText(arg0);
+	@Override
+	public boolean containsValue(Object arg0) {
+		throw new EJBException("Method containsValue not supported");
+	}
+
+	@Override
+	public Set<Entry<String, String>> entrySet() {
+		throw new EJBException("Method entrySet not supported");
+	}
+
+	@Override
+	public String get(Object arg0) {
+		String str = ejbcawebbean.getText(arg0.toString());
 		return HTMLTools.htmlunescape(str);
 	}
+
+	@Override
+	public boolean isEmpty() {
+		throw new EJBException("Method isEmpty not supported");
+	}
+
+	@Override
+	public Set<String> keySet() {
+		throw new EJBException("Method keySet not supported");
+	}
+
+	@Override
+	public String put(String arg0, String arg1) {
+		throw new EJBException("Method put not supported");
+	}
+
+	@Override
+	public String remove(Object arg0) {
+		throw new EJBException("Method remove not supported");
+	}
+
+	@Override
+	public int size() {
+		throw new EJBException("Method size not supported");
+	}
+
+	@Override
+	public Collection<String> values() {
+		throw new EJBException("Method values not supported");
+	}
+
+    @Override
+    public void putAll(Map<? extends String, ? extends String> arg0) {
+        throw new EJBException("Method putAll not supported");
+        
+    }
+
+
 
 }
