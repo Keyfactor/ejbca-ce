6c1f77a33ed21f4cacc3b85044d1948d336e7345
Mike Agrenius Kushner <mike.kushner@primekey.com>
2012-01-02 16:00:28 +0000

    NoTicket: Fixed some code that I broke earlier =)
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@13640 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/admin-gui/src/org/ejbca/ui/web/admin/configuration/EjbcaJSFImageResource.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/configuration/EjbcaJSFImageResource.java
@@ -1,32 +1,84 @@
 package org.ejbca.ui.web.admin.configuration;
 
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
 import javax.ejb.EJBException;
 
 /**
  * Class used to retrieve EJBCA image resources in JSF views
  * 
- * @author Philip Vendil
+ * Implements a Map used for retrieving resources.
  * @version $Id$
  * @see org.ejbca.ui.web.admin.configuration.EjbcaWebBean#getImagefileInfix(String)
  */
-public class EjbcaJSFImageResource {
+public class EjbcaJSFImageResource implements Map<String, String> {
 
-	private EjbcaWebBean ejbcawebbean;
-	
-	public EjbcaJSFImageResource(EjbcaWebBean ejbcawebbean){
-		this.ejbcawebbean = ejbcawebbean;
-	}
-	
-	public void clear() {
-		throw new EJBException("Method clear not supported");
-	}
+    private EjbcaWebBean ejbcawebbean;
+    
+    public EjbcaJSFImageResource(EjbcaWebBean ejbcawebbean){
+        this.ejbcawebbean = ejbcawebbean;
+    }
+    
+    @Override
+    public void clear() {
+        throw new EJBException("Method clear not supported");
+    }
 
-	public boolean containsKey(String arg0) {		
-		return ejbcawebbean.getImagefileInfix(arg0) != null;
-	}
+    @Override
+    public boolean containsKey(Object arg0) {      
+        return ejbcawebbean.getImagefileInfix((String) arg0) != null;
+    }
 
-	public String get(String arg0) {
-		return ejbcawebbean.getImagefileInfix(arg0);
-	}
+    @Override
+    public boolean containsValue(Object arg0) {
+        throw new EJBException("Method containsValue not supported");
+    }
+
+    @Override
+    public Set<Entry<String, String>> entrySet() {
+        throw new EJBException("Method entrySet not supported");
+    }
+
+    @Override
+    public String get(Object arg0) {
+        return ejbcawebbean.getImagefileInfix((String) arg0);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new EJBException("Method isEmpty not supported");
+    }
+
+    @Override
+    public Set<String> keySet() {
+        throw new EJBException("Method keySet not supported");
+    }
+
+    @Override
+    public String put(String arg0, String arg1) {
+        throw new EJBException("Method put not supported");
+    }
+
+    @Override
+    public void putAll(Map<? extends String, ? extends String> arg0) {
+        throw new EJBException("Method putAll not supported");
+    }
+
+    @Override
+    public String remove(Object arg0) {
+        throw new EJBException("Method remove not supported");
+    }
+
+    @Override
+    public int size() {
+        throw new EJBException("Method size not supported");
+    }
+
+    @Override
+    public Collection<String> values() {
+        throw new EJBException("Method values not supported");
+    }
 
 }
