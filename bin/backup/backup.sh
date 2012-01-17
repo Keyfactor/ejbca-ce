77a4ceb1a8d74c9d97330208d113304cf89bc89e
Mike Agrenius Kushner <mike.kushner@primekey.com>
2012-01-17 10:49:00 +0000

    ECA-2505: Removing outcommented lines
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@13823 37aeb1de-b14b-0410-9a0e-82b28170d228

--- bin/backup/backup.sh
+++ bin/backup/backup.sh
@@ -67,6 +67,6 @@ echo "Please input slot number. start with 'i' to indicate index in list"
 read SLOT_NUMBER
 echo "Please input key alias"
 read KEY_ALIAS
-#./ejbcaClientToolBox.sh PKCS11HSMKeyTool encrypt $SHARED_LIBRARY_NAME $SLOT_NUMBER $WORKING_DIRECTORY/backup.zip $WORKING_DIRECTORY/backup-$TIMESTAMP.backup
-#rm -f $WORKING_DIRECTORY/backup.zip
+./ejbcaClientToolBox.sh PKCS11HSMKeyTool encrypt $SHARED_LIBRARY_NAME $SLOT_NUMBER $WORKING_DIRECTORY/backup.zip $WORKING_DIRECTORY/backup-$TIMESTAMP.backup
+rm -f $WORKING_DIRECTORY/backup.zip
 cd $STARTING_DIRECTORY
