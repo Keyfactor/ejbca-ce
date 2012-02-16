8f6f28f4735f8b67f3681faa33c6557e9e6725ed
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2012-02-16 09:03:12 +0000

    ECA-2588: add run.bat to trunk as well
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@14102 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ejbca-entity/resources/run.bat
@@ -0,0 +1,14 @@
+@echo off 
+REM $Id: run.bat 14101 2012-02-16 08:51:25Z anatom $
+
+REM Memory settings can be specified using parameters like: -Xms20480m -Xmx20480m -XX:MaxPermSize=384m
+java -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -XX:+ExplicitGCInvokesConcurrent -XX:-UseGCOverheadLimit -Djava.endorsed.dirs=endorsed -jar ejbca-db-cli.jar %1 %2 %3 %4 %5 %6 %7 %8
+
+IF ERRORLEVEL 1 GOTO ERROR
+
+goto END
+
+:ERROR
+    echo If you see errors while running the CLI similar to "JDBC Driver class not found" your should copy your JDBC driver JAR to endorsed.
+
+:END
--- modules/ejbca-entity/resources/run.sh
+++ modules/ejbca-entity/resources/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Memeory settings can be specified using parameters like: -Xms20480m -Xmx20480m -XX:MaxPermSize=384m
+# Memory settings can be specified using parameters like: -Xms20480m -Xmx20480m -XX:MaxPermSize=384m
 java -server -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -XX:+ExplicitGCInvokesConcurrent -XX:-UseGCOverheadLimit -Djava.endorsed.dirs=`dirname "$0"`/endorsed -jar `dirname "$0"`/ejbca-db-cli.jar "$@"
 
 ERRORLEVEL="$?"
