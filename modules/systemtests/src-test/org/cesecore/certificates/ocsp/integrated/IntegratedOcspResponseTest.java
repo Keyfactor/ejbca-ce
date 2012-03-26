9d65fae2aeda53757ccc3f97240abecf5feeb440
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2012-03-26 11:14:52 +0000

    ECA-2500: re-make BC 1.47 changes that was over-written.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@14421 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/systemtests/src-test/org/cesecore/certificates/ocsp/integrated/IntegratedOcspResponseTest.java
+++ modules/systemtests/src-test/org/cesecore/certificates/ocsp/integrated/IntegratedOcspResponseTest.java
@@ -30,7 +30,7 @@ import java.util.List;
 
 import javax.ejb.EJBException;
 
-import org.bouncycastle.asn1.DERObjectIdentifier;
+import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 import org.bouncycastle.asn1.DEROctetString;
 import org.bouncycastle.asn1.ocsp.OCSPObjectIdentifiers;
 import org.bouncycastle.asn1.x509.X509Extension;
@@ -200,7 +200,7 @@ public class IntegratedOcspResponseTest extends CaCreatingTestCase {
         // An OCSP request
         OCSPReqGenerator gen = new OCSPReqGenerator();
         gen.addRequest(new CertificateID(CertificateID.HASH_SHA1, caCertificate, caCertificate.getSerialNumber()));
-        Hashtable<DERObjectIdentifier, X509Extension> exts = new Hashtable<DERObjectIdentifier, X509Extension>();
+        Hashtable<ASN1ObjectIdentifier, X509Extension> exts = new Hashtable<ASN1ObjectIdentifier, X509Extension>();
         X509Extension ext = new X509Extension(false, new DEROctetString("123456789".getBytes()));
         exts.put(OCSPObjectIdentifiers.id_pkix_ocsp_nonce, ext);
         gen.setRequestExtensions(new X509Extensions(exts));
@@ -234,7 +234,7 @@ public class IntegratedOcspResponseTest extends CaCreatingTestCase {
         // An OCSP request
         OCSPReqGenerator gen = new OCSPReqGenerator();
         gen.addRequest(new CertificateID(CertificateID.HASH_SHA1, caCertificate, ocspCertificate.getSerialNumber()));
-        Hashtable<DERObjectIdentifier, X509Extension> exts = new Hashtable<DERObjectIdentifier, X509Extension>();
+        Hashtable<ASN1ObjectIdentifier, X509Extension> exts = new Hashtable<ASN1ObjectIdentifier, X509Extension>();
         X509Extension ext = new X509Extension(false, new DEROctetString("123456789".getBytes()));
         exts.put(OCSPObjectIdentifiers.id_pkix_ocsp_nonce, ext);
         gen.setRequestExtensions(new X509Extensions(exts));
@@ -267,7 +267,7 @@ public class IntegratedOcspResponseTest extends CaCreatingTestCase {
         // An OCSP request
         OCSPReqGenerator gen = new OCSPReqGenerator();
         gen.addRequest(new CertificateID(CertificateID.HASH_SHA1, caCertificate, ocspCertificate.getSerialNumber()));
-        Hashtable<DERObjectIdentifier, X509Extension> exts = new Hashtable<DERObjectIdentifier, X509Extension>();
+        Hashtable<ASN1ObjectIdentifier, X509Extension> exts = new Hashtable<ASN1ObjectIdentifier, X509Extension>();
         X509Extension ext = new X509Extension(false, new DEROctetString("123456789".getBytes()));
         exts.put(OCSPObjectIdentifiers.id_pkix_ocsp_nonce, ext);
         gen.setRequestExtensions(new X509Extensions(exts));
@@ -308,7 +308,7 @@ public class IntegratedOcspResponseTest extends CaCreatingTestCase {
         // An OCSP request
         OCSPReqGenerator gen = new OCSPReqGenerator();
         gen.addRequest(new CertificateID(CertificateID.HASH_SHA1, caCertificate, ocspCertificate.getSerialNumber()));
-        Hashtable<DERObjectIdentifier, X509Extension> exts = new Hashtable<DERObjectIdentifier, X509Extension>();
+        Hashtable<ASN1ObjectIdentifier, X509Extension> exts = new Hashtable<ASN1ObjectIdentifier, X509Extension>();
         X509Extension ext = new X509Extension(false, new DEROctetString("123456789".getBytes()));
         exts.put(OCSPObjectIdentifiers.id_pkix_ocsp_nonce, ext);
         gen.setRequestExtensions(new X509Extensions(exts));
@@ -385,7 +385,7 @@ public class IntegratedOcspResponseTest extends CaCreatingTestCase {
             // An OCSP request
             OCSPReqGenerator gen = new OCSPReqGenerator();
             gen.addRequest(new CertificateID(CertificateID.HASH_SHA1, caCertificate, ocspCertificate.getSerialNumber()));
-            Hashtable<DERObjectIdentifier, X509Extension> exts = new Hashtable<DERObjectIdentifier, X509Extension>();
+            Hashtable<ASN1ObjectIdentifier, X509Extension> exts = new Hashtable<ASN1ObjectIdentifier, X509Extension>();
             X509Extension ext = new X509Extension(false, new DEROctetString("123456789".getBytes()));
             exts.put(OCSPObjectIdentifiers.id_pkix_ocsp_nonce, ext);
             gen.setRequestExtensions(new X509Extensions(exts));
@@ -469,7 +469,7 @@ public class IntegratedOcspResponseTest extends CaCreatingTestCase {
         // An OCSP request
         OCSPReqGenerator gen = new OCSPReqGenerator();
         gen.addRequest(new CertificateID(CertificateID.HASH_SHA1, ocspCertificate, ocspCertificate.getSerialNumber()));
-        Hashtable<DERObjectIdentifier, X509Extension> exts = new Hashtable<DERObjectIdentifier, X509Extension>();
+        Hashtable<ASN1ObjectIdentifier, X509Extension> exts = new Hashtable<ASN1ObjectIdentifier, X509Extension>();
         X509Extension ext = new X509Extension(false, new DEROctetString("123456789".getBytes()));
         exts.put(OCSPObjectIdentifiers.id_pkix_ocsp_nonce, ext);
         gen.setRequestExtensions(new X509Extensions(exts));
@@ -509,7 +509,7 @@ public class IntegratedOcspResponseTest extends CaCreatingTestCase {
         // An OCSP request
         OCSPReqGenerator gen = new OCSPReqGenerator();
         gen.addRequest(new CertificateID(CertificateID.HASH_SHA1, ocspCertificate, ocspCertificate.getSerialNumber()));
-        Hashtable<DERObjectIdentifier, X509Extension> exts = new Hashtable<DERObjectIdentifier, X509Extension>();
+        Hashtable<ASN1ObjectIdentifier, X509Extension> exts = new Hashtable<ASN1ObjectIdentifier, X509Extension>();
         X509Extension ext = new X509Extension(false, new DEROctetString("123456789".getBytes()));
         exts.put(OCSPObjectIdentifiers.id_pkix_ocsp_nonce, ext);
         gen.setRequestExtensions(new X509Extensions(exts));
