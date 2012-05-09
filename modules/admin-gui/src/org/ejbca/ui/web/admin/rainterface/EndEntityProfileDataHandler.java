3d22acc9f417ee150b58f06280325a825f4b3d81
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2012-05-09 10:18:56 +0000

    ECA-2673: remove comments and unused exception declaration
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@14640 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/admin-gui/src/org/ejbca/ui/web/admin/rainterface/EndEntityProfileDataHandler.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/rainterface/EndEntityProfileDataHandler.java
@@ -73,23 +73,13 @@ public class EndEntityProfileDataHandler implements java.io.Serializable {
     }    
     
       /** Method to get a reference to a end entity profile.*/ 
-    public EndEntityProfile getEndEntityProfile(int id) throws AuthorizationDeniedException{  
+    public EndEntityProfile getEndEntityProfile(int id) {  
         final EndEntityProfile profile = endEntityProfileSession.getEndEntityProfile(id); 
-        // We don't need to make any funky checks which CAs an admin is authorized to here. 
-        // The web page will only display the CAs the admin is authorized to in the GUI, and when adding/editing 
-        // an end entity the session beans will ensure that no admin can add/edit end entities without access to the proper 
-        // profiles rules and CAs.
-        //endEntityProfileSession.authorizedToProfileCas(administrator, profile);
         return profile;
     }      
           
-    public EndEntityProfile getEndEntityProfile(String profilename) throws AuthorizationDeniedException{
+    public EndEntityProfile getEndEntityProfile(String profilename) {
         final EndEntityProfile profile = endEntityProfileSession.getEndEntityProfile(profilename);
-        // We don't need to make any funky checks which CAs an admin is authorized to here. 
-        // The web page will only display the CAs the admin is authorized to in the GUI, and when adding/editing 
-        // an end entity the session beans will ensure that no admin can add/edit end entities without access to the proper 
-        // profiles rules and CAs.
-        //endEntityProfileSession.authorizedToProfileCas(administrator, profile);
         return profile;
     }
    
