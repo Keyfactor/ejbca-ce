5f2ea8edecfbc997a7442bec959477a4c391912a
Mike Agrenius Kushner <mike.kushner@primekey.com>
2012-06-25 13:01:46 +0000

    ECA-862: Reverting a couple of unnecessary changes. This ticket shouldn't affect CESeCore any more.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@15057 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/cesecore/certificates/ca/CA.java
+++ src/java/org/cesecore/certificates/ca/CA.java
@@ -122,12 +122,6 @@ public abstract class CA extends UpgradeableDataHashMap implements Serializable 
 
     private CAInfo cainfo = null;
 
-    /**
-     * Public constructor required for XML serialization.
-     */
-    public CA() {
-    }
-    
     /** Creates a new instance of CA, this constructor should be used when a new CA is created */
     public CA(CAInfo cainfo) {
         data = new LinkedHashMap<Object, Object>();
