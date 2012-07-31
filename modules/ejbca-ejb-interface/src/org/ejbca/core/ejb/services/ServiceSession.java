49d073270b887551a2d12c0fec40640fed3517d9
Mike Agrenius Kushner <mike.kushner@primekey.com>
2012-07-31 15:17:48 +0000

    NoTicket: Some code cleanup
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@15158 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb-interface/src/org/ejbca/core/ejb/services/ServiceSession.java
+++ modules/ejbca-ejb-interface/src/org/ejbca/core/ejb/services/ServiceSession.java
@@ -27,7 +27,7 @@ public interface ServiceSession {
      * Adds a Service to the database.
      * @throws ServiceExistsException if service already exists.
      */
-    public void addService(AuthenticationToken admin, String name, ServiceConfiguration serviceConfiguration) throws ServiceExistsException;
+    void addService(AuthenticationToken admin, String name, ServiceConfiguration serviceConfiguration) throws ServiceExistsException;
 
     /**
      * Adds a service to the database. Used for importing and exporting profiles
@@ -35,22 +35,22 @@ public interface ServiceSession {
      * 
      * @throws ServiceExistsException if service already exists.
      */
-    public void addService(AuthenticationToken admin, int id, String name, ServiceConfiguration serviceConfiguration) throws ServiceExistsException;
+    void addService(AuthenticationToken admin, int id, String name, ServiceConfiguration serviceConfiguration) throws ServiceExistsException;
 
     /**
      * Adds a service with the same content as the original.
      * @throws ServiceExistsException if service already exists.
      */
-    public void cloneService(AuthenticationToken admin, String oldname, String newname) throws ServiceExistsException;
+    void cloneService(AuthenticationToken admin, String oldname, String newname) throws ServiceExistsException;
 
     /** Removes a service from the database. */
-    public boolean removeService(AuthenticationToken admin, String name);
+    boolean removeService(AuthenticationToken admin, String name);
 
     /**
      * Renames a service.
      * @throws ServiceExistsException if service already exists.
      */
-    public void renameService(AuthenticationToken admin, String oldname, String newname) throws ServiceExistsException;
+     void renameService(AuthenticationToken admin, String oldname, String newname) throws ServiceExistsException;
 
     /**
      * Retrieves a Collection of id:s (Integer) to visible authorized services.
@@ -58,19 +58,19 @@ public interface ServiceSession {
      * 
      * @return Collection of id:s (Integer)
      */
-    public Collection<Integer> getAuthorizedVisibleServiceIds(AuthenticationToken admin);
+    Collection<Integer> getAuthorizedVisibleServiceIds(AuthenticationToken admin);
 
     /**
      * Retrieves a named service.
      * @returns the service configuration or null if it doesn't exist.
      */
-    public org.ejbca.core.model.services.ServiceConfiguration getService(String name);
+    ServiceConfiguration getService(String name);
 
     /**
      * Returns a service id, given it's service name
      * @return the id or 0 if the service cannot be found.
      */
-    public int getServiceId(String name);
+    int getServiceId(String name);
 
     /**
      * Activates the timer for a named service. The service must already be
@@ -79,32 +79,32 @@ public interface ServiceSession {
      * @param admin The administrator performing the action
      * @param name the name of the service for which to activate the timer
      */
-    public void activateServiceTimer(AuthenticationToken admin, java.lang.String name);
+     void activateServiceTimer(AuthenticationToken admin, java.lang.String name);
 
     /**
      * Returns a Service name given its id.
      * @return the name or null if id does not exist
      */
-    public String getServiceName(int id);
+    String getServiceName(int id);
 
  
     /** Loads and activates all the services from database that are active. */
-    public void load();
+    void load();
 
     /** Cancels all existing timers a unload. */
-    public void unload();
+    void unload();
     
     /**
      * Updates service configuration, but does not re-set the timer
      * @param noLogging if true no logging (to the database will be done
      */
-    public void changeService(AuthenticationToken admin, String name, ServiceConfiguration serviceConfiguration, boolean noLogging);
+    void changeService(AuthenticationToken admin, String name, ServiceConfiguration serviceConfiguration, boolean noLogging);
     
     /**
      * Finds a service configuration by id.
      * 
      * @returns the service configuration or null if it doesn't exist.
      */
-    public ServiceConfiguration getServiceConfiguration(AuthenticationToken admin, int id);
+    ServiceConfiguration getServiceConfiguration(AuthenticationToken admin, int id);
 
 }
