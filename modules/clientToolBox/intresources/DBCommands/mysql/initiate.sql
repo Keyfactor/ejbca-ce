0a41b8561b63334ffcf2b645d7d6bce717bddfd8
primelars <USER@primekey.com>
2012-08-14 15:26:58 +0000

    just fixing host issue in new mysql server
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@15328 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/clientToolBox/intresources/DBCommands/mysql/initiate.sql
+++ modules/clientToolBox/intresources/DBCommands/mysql/initiate.sql
@@ -4,11 +4,11 @@
 drop database if exists ${url.path};
 create database ${url.path};
 
-revoke ALL PRIVILEGES, GRANT OPTION from '${database.username}'@'%';
-DROP USER '${database.username}'@'%';
+revoke ALL PRIVILEGES, GRANT OPTION from '${database.username}'@'${url.host}';
+DROP USER '${database.username}'@'${url.host}';
 
-grant ALL on ${url.path}.* to '${database.username}'@'%' identified by '${database.password}';
+grant ALL on ${url.path}.* to '${database.username}'@'${url.host}' identified by '${database.password}';
 
 flush privileges;
-show grants for '${database.username}'@'%';
+show grants for '${database.username}'@'${url.host}';
 exit
--- modules/clientToolBox/intresources/DBCommands/mysql/tightenpermissions.sql
+++ modules/clientToolBox/intresources/DBCommands/mysql/tightenpermissions.sql
@@ -1,40 +1,40 @@
 # Version: $Id$
 
 
-revoke ALL PRIVILEGES, GRANT OPTION from '${database.username}'@'%';
-DROP USER '${database.username}'@'%';
+revoke ALL PRIVILEGES, GRANT OPTION from '${database.username}'@'${url.host}';
+DROP USER '${database.username}'@'${url.host}';
 
-grant LOCK TABLES on ${url.path}.* to '${database.username}'@'%' identified by '${database.password}';
+grant LOCK TABLES on ${url.path}.* to '${database.username}'@'${url.host}' identified by '${database.password}';
 
-grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.AccessRulesData to '${database.username}'@'%';
-grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.AdminEntityData to '${database.username}'@'%';
-grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.AdminGroupData to '${database.username}'@'%';
-grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.AdminPreferencesData to '${database.username}'@'%';
-grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.ApprovalData to '${database.username}'@'%';
-grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.AuthorizationTreeUpdateData to '${database.username}'@'%';
-grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.CAData to '${database.username}'@'%';
-grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.CRLData to '${database.username}'@'%';
-grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.CertReqHistoryData to '${database.username}'@'%';
-grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.CertificateData to '${database.username}'@'%';
-grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.CertificateProfileData to '${database.username}'@'%';
-grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.EndEntityProfileData to '${database.username}'@'%';
-grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.GlobalConfigurationData to '${database.username}'@'%';
-grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.HardTokenCertificateMap to '${database.username}'@'%';
-grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.HardTokenData to '${database.username}'@'%';
-grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.HardTokenIssuerData to '${database.username}'@'%';
-grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.HardTokenProfileData to '${database.username}'@'%';
-grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.HardTokenPropertyData to '${database.username}'@'%';
-grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.KeyRecoveryData to '${database.username}'@'%';
-grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.LogConfigurationData to '${database.username}'@'%';
-grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.ProtectedLogExportData to '${database.username}'@'%';
-grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.PublisherData to '${database.username}'@'%';
-grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.PublisherQueueData to '${database.username}'@'%';
-grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.ServiceData to '${database.username}'@'%';
-grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.UserData to '${database.username}'@'%';
-grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.UserDataSourceData to '${database.username}'@'%';
+grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.AccessRulesData to '${database.username}'@'${url.host}';
+grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.AdminEntityData to '${database.username}'@'${url.host}';
+grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.AdminGroupData to '${database.username}'@'${url.host}';
+grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.AdminPreferencesData to '${database.username}'@'${url.host}';
+grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.ApprovalData to '${database.username}'@'${url.host}';
+grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.AuthorizationTreeUpdateData to '${database.username}'@'${url.host}';
+grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.CAData to '${database.username}'@'${url.host}';
+grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.CRLData to '${database.username}'@'${url.host}';
+grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.CertReqHistoryData to '${database.username}'@'${url.host}';
+grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.CertificateData to '${database.username}'@'${url.host}';
+grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.CertificateProfileData to '${database.username}'@'${url.host}';
+grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.EndEntityProfileData to '${database.username}'@'${url.host}';
+grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.GlobalConfigurationData to '${database.username}'@'${url.host}';
+grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.HardTokenCertificateMap to '${database.username}'@'${url.host}';
+grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.HardTokenData to '${database.username}'@'${url.host}';
+grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.HardTokenIssuerData to '${database.username}'@'${url.host}';
+grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.HardTokenProfileData to '${database.username}'@'${url.host}';
+grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.HardTokenPropertyData to '${database.username}'@'${url.host}';
+grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.KeyRecoveryData to '${database.username}'@'${url.host}';
+grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.LogConfigurationData to '${database.username}'@'${url.host}';
+grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.ProtectedLogExportData to '${database.username}'@'${url.host}';
+grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.PublisherData to '${database.username}'@'${url.host}';
+grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.PublisherQueueData to '${database.username}'@'${url.host}';
+grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.ServiceData to '${database.username}'@'${url.host}';
+grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.UserData to '${database.username}'@'${url.host}';
+grant SELECT,INSERT,UPDATE,DELETE,INDEX on ${url.path}.UserDataSourceData to '${database.username}'@'${url.host}';
 
-grant SELECT,INSERT on ${url.path}.LogEntryData to '${database.username}'@'%';
+grant SELECT,INSERT on ${url.path}.LogEntryData to '${database.username}'@'${url.host}';
 
 flush privileges;
-show grants for '${database.username}'@'%';
+show grants for '${database.username}'@'${url.host}';
 exit
