79912a45ae420f4d08a569d87c8f2e0c70a071a5
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2012-09-25 12:30:22 +0000

    ECA-2727: fix bugs related to usernamemapping
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@15654 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/publicweb-gui/src/org/ejbca/ui/web/pub/RegisterReqBean.java
+++ modules/publicweb-gui/src/org/ejbca/ui/web/pub/RegisterReqBean.java
@@ -136,7 +136,7 @@ public class RegisterReqBean {
     }
     
     public String getUsernameMapping() {
-        String um = getCertTypeInfo(certType, "usernamemapping");
+        String um = EjbcaConfigurationHolder.getString("web.selfreg.certtypes."+certType+".usernamemapping");
         return um != null ? um.toLowerCase(Locale.ROOT) : null; 
     }
     
