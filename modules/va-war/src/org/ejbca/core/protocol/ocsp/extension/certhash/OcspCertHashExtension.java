2c4586624fb212d52159d1d295fc861702c7e792
Mike Agrenius Kushner <mike.kushner@primekey.com>
2012-10-17 16:35:22 +0000

    ECA-2788: Some minor cleanup
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@15755 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/va-war/src/org/ejbca/core/protocol/ocsp/extension/certhash/OcspCertHashExtension.java
+++ modules/va-war/src/org/ejbca/core/protocol/ocsp/extension/certhash/OcspCertHashExtension.java
@@ -24,7 +24,6 @@ import org.apache.log4j.Logger;
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 import org.bouncycastle.asn1.DEROctetString;
 import org.bouncycastle.asn1.isismtt.ocsp.CertHash;
-import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
 import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
 import org.bouncycastle.asn1.x509.Extension;
 import org.cesecore.certificates.ocsp.extension.OCSPExtension;
