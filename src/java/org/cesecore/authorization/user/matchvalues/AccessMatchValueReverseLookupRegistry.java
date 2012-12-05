614348233a06dccd8927e6412302f4ecf49e7db5
Mike Agrenius Kushner <mike.kushner@primekey.com>
2012-12-05 12:48:57 +0000

    ECA-2350: Should be a working solution. Not the prettiest, but it works.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@15973 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/cesecore/authorization/user/matchvalues/AccessMatchValueReverseLookupRegistry.java
+++ src/java/org/cesecore/authorization/user/matchvalues/AccessMatchValueReverseLookupRegistry.java
@@ -17,6 +17,7 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.security.InvalidParameterException;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -29,12 +30,21 @@ import java.util.concurrent.ConcurrentHashMap;
 public enum AccessMatchValueReverseLookupRegistry {
     INSTANCE;
     
-    private Map<String, Method> registry;
+    // Registry of methods used to look up database values
+    private Map<String, Method> databaseValueRegistry;
+    private Map<String, Map<String, AccessMatchValue>> nameLookupRegistry;
+    private Map<String, AccessMatchValue> defaultValues;
     
     private AccessMatchValueReverseLookupRegistry() {
-        registry = new ConcurrentHashMap<String, Method>();      
+        databaseValueRegistry = new ConcurrentHashMap<String, Method>();  
+        nameLookupRegistry = new ConcurrentHashMap<String, Map<String, AccessMatchValue>>();  
+        defaultValues = new ConcurrentHashMap<String, AccessMatchValue>();
     }
 
+    public Set<String> getAllTokenTypes() {
+        return databaseValueRegistry.keySet();
+    }
+    
     /**
      * This method registers a static reverse lookup method for enums implementing the AccessMatchValue
      * interface.  
@@ -42,25 +52,27 @@ public enum AccessMatchValueReverseLookupRegistry {
      * A method registered to this registry MUST be public, static and have a return type implementing AccessMatchvalue.
      * 
      * @param tokenType A string identifier 
-     * @param lookupMethod a method that must return an enum extending AccessMatchValue that must be public and static.
+     * @param databaseValueLookupMethod a method that must return an enum extending AccessMatchValue that must be public and static.
      */
-    public void registerLookupMethod(String tokenType, Method lookupMethod) {
+    public void registerLookupMethod(String tokenType, Method databaseValueLookupMethod, Map<String, AccessMatchValue> nameLookupMap, AccessMatchValue defaultValue ) {
         if (tokenType == null) {
             throw new InvalidParameterException("Parameter tokenType may not be null");
-        } else if (lookupMethod == null) {
+        } else if (databaseValueLookupMethod == null) {
             throw new InvalidParameterException("Parameter lookupMethod may not be null");
-        } else if (registry.get(tokenType) != null) {
+        } else if (databaseValueRegistry.get(tokenType) != null) {
             throw new InvalidMatchValueException("A lookup method linked to the token type " + tokenType + " is already registered.");
-        } else if (!Modifier.isPublic(lookupMethod.getModifiers())) {
+        } else if (!Modifier.isPublic(databaseValueLookupMethod.getModifiers())) {
             throw new InvalidMatchValueException("Lookup method was not public, hence invalid. Can not recover.");
-        } else if (!Modifier.isStatic(lookupMethod.getModifiers())) {
+        } else if (!Modifier.isStatic(databaseValueLookupMethod.getModifiers())) {
             throw new InvalidMatchValueException("Lookup method was not static, hence invalid. Can not recover.");
-        } else if (!AccessMatchValue.class.isAssignableFrom(lookupMethod.getReturnType())) {
+        } else if (!AccessMatchValue.class.isAssignableFrom(databaseValueLookupMethod.getReturnType())) {
             throw new InvalidMatchValueException("Lookup method does not return a class that implements the interface "
                     + AccessMatchValue.class.getSimpleName() + " Can not recover.");
         } else {
-            registry.put(tokenType, lookupMethod);
+            databaseValueRegistry.put(tokenType, databaseValueLookupMethod);
         }
+        this.nameLookupRegistry.put(tokenType, nameLookupMap);
+        this.defaultValues.put(tokenType, defaultValue);
     }
 
     /**
@@ -76,7 +88,7 @@ public enum AccessMatchValueReverseLookupRegistry {
         if (tokenType == null) {
             return null;
         } else {
-            Method callback = registry.get(tokenType);
+            Method callback = databaseValueRegistry.get(tokenType);
             if (callback == null) {
                 return null;
             } else {
@@ -91,6 +103,46 @@ public enum AccessMatchValueReverseLookupRegistry {
                 }
             }
         }
+    }
+    
+    /**
+     * Returns the AccessMatchValue for a given token type and value name
+     * 
+     * @param tokenType a name representing the sought token type.
+     * @param matchValueName the name of the match value
+     * @return the sought AccessMatchValue. Returns null if match value not found for the given token type.
+     */
+    public AccessMatchValue lookupMatchValueFromTokenTypeAndName(String tokenType, String matchValueName) {
+        Map<String, AccessMatchValue> valueMap = nameLookupRegistry.get(tokenType);
+        if(valueMap == null) {
+            throw new ReverseMatchValueLookupException("Token type of name " + tokenType + " not found.");
+        }
+        return valueMap.get(matchValueName);
+    }
+
+    /**
+     * @return the nameLookupMap for a given token type. Never returns null.
+     */
+    public Map<String, AccessMatchValue> getNameLookupRegistryForTokenType(String tokenType) {
+        Map<String, AccessMatchValue> valueMap = nameLookupRegistry.get(tokenType);
+        if(valueMap == null) {
+            throw new ReverseMatchValueLookupException("Token type of name " + tokenType + " not found.");
+        }
+        return valueMap;
+    }
+    
+    /**
+     * The default value for the given token type. 
+     * 
+     * @param tokenType the token type asked for 
+     * @return the default value for the given token type. May return null if such a value is registered as default.
+     */
+    public AccessMatchValue getDefaultValueForTokenType(String tokenType) {
+        AccessMatchValue result = defaultValues.get(tokenType);
+        if(!nameLookupRegistry.containsKey(tokenType)) {
+            throw new ReverseMatchValueLookupException("Token type " + tokenType + " does not exist.");
+        }
+        return result;
     }
 
 }
