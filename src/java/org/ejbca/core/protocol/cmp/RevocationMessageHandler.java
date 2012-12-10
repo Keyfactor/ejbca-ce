943172547e346d1c7c139232578e5a65370fb5f2
aveen4711 <USER@primekey.com>
2012-12-10 14:30:22 +0000

    ECA-2628: Fixed a spelling mistake.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@16002 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/core/protocol/cmp/RevocationMessageHandler.java
+++ src/java/org/ejbca/core/protocol/cmp/RevocationMessageHandler.java
@@ -168,7 +168,7 @@ public class RevocationMessageHandler extends BaseCmpMessageHandler implements I
 		    } catch(Exception e) {
                 LOG.debug("Could not parse the revocation request. Trying to parse it as novosec generated message.");
 		        rd = CmpMessageHelper.getNovosecRevDetails(rr);
-		        LOG.debug("Succeeded in pasring the novosec generated request.");
+		        LOG.debug("Succeeded in parsing the novosec generated request.");
 		    }
 		    final CertTemplate ct = rd.getCertDetails();
 		    final DERInteger serno = ct.getSerialNumber();
