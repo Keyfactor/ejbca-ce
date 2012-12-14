e9f1170208aef19725e63515d5002f3597ed811d
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2012-12-14 09:04:21 +0000

    ECA-2875: use more clever batch stuff in order to allow unlimited arguments on windows.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@16016 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/clientToolBox/resources/ejbcaClientToolBox.bat
+++ modules/clientToolBox/resources/ejbcaClientToolBox.bat
@@ -1,21 +1,15 @@
 @echo off
+SetLocal EnableDelayedExpansion 
+
 rem Starting ctb using a Windows .BAT file
 set TOOLBOX_HOME=%~dp0
 
 rem Fixup arguments, we have to do this since windows normally only 
 rem supports %1-%9 as command line arguments
-set a=%1
-set b=%2
-set c=%3
-set d=%4
-set e=%5
-set f=%6
-set g=%7
-set h=%8
-set i=%9
-shift
-set j=%9
-rem echo %a% %b% %c% %d% %e% %f% %g% %h% %i% %j%
+FOR %%A IN (%*) DO (
+    set args=!args! %%A
+) 
+rem echo %args%
 
 if exist "%TOOLBOX_HOME%clientToolBox.jar" goto exists
 	echo You have to build the ClientToolBox before running this command.
@@ -23,4 +17,4 @@ if exist "%TOOLBOX_HOME%clientToolBox.jar" goto exists
 :exists
 
 rem @echo on
-java -Djava.endorsed.dirs="%TOOLBOX_HOME%endorsed" -jar "%TOOLBOX_HOME%clientToolBox.jar" %a% %b% %c% %d% %e% %f% %g% %h% %i% %j%
+java -Djava.endorsed.dirs="%TOOLBOX_HOME%endorsed" -jar "%TOOLBOX_HOME%clientToolBox.jar" %args%
