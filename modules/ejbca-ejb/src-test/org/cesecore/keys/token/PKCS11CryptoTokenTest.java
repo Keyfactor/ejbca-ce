83147d0b9180381c3c9241e65f486f4951c46ab1
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2013-01-19 11:28:48 +0000

    ECA-2904: remove PKCS11Provider after each test otherwise it is still there and affect the next test by stopping it from installing the provider with it's tweaks. Makes tests independent of order.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@16146 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb/src-test/org/cesecore/keys/token/PKCS11CryptoTokenTest.java
+++ modules/ejbca-ejb/src-test/org/cesecore/keys/token/PKCS11CryptoTokenTest.java
@@ -22,6 +22,7 @@ import java.security.InvalidKeyException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
+import java.security.Security;
 import java.security.SignatureException;
 import java.security.cert.CertificateException;
 import java.security.spec.InvalidKeySpecException;
@@ -33,6 +34,7 @@ import javax.crypto.NoSuchPaddingException;
 
 import org.apache.log4j.Logger;
 import org.cesecore.util.CryptoProviderTools;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -57,6 +59,12 @@ public class PKCS11CryptoTokenTest extends CryptoTokenTestBase {
 
     public PKCS11CryptoTokenTest() {
         CryptoProviderTools.installBCProvider();
+    }
+
+    @After
+    public void tearDown() {
+        // Make sure we remove the provider after one test, so it is not still there affecting the next test
+        Security.removeProvider(getProvider());
     }
 
     @Test
