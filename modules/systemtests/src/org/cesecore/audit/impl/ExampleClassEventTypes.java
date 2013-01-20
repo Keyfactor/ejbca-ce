0394075375222c3ffaf3ca38f4eb357bd4ec45a4
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2013-01-20 16:58:20 +0000

    Noticket: just clean up headers for code drop.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@16154 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/systemtests/src/org/cesecore/audit/impl/ExampleClassEventTypes.java
+++ modules/systemtests/src/org/cesecore/audit/impl/ExampleClassEventTypes.java
@@ -1,3 +1,15 @@
+/*************************************************************************
+ *                                                                       *
+ *  CESeCore: CE Security Core                                           *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.cesecore.audit.impl;
 
 import org.cesecore.audit.enums.EventType;
@@ -8,7 +20,6 @@ import org.cesecore.audit.enums.EventTypeHolder;
  * @version $Id$
  *
  */
-
 public class ExampleClassEventTypes implements EventType {
     
     private static final long serialVersionUID = 1937482572926091659L;
