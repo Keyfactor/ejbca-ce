48f1e6c554fd9876fbe64ae15ba28d87028aed9a
Mike Agrenius Kushner <mike.kushner@primekey.com>
2013-01-21 10:19:04 +0000

    NoTicket: Resolved an unwanted dependency by mirroring a small util method
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@16161 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/cesecore/certificates/ocsp/HsmResponseThread.java
+++ src/java/org/cesecore/certificates/ocsp/HsmResponseThread.java
@@ -18,6 +18,7 @@ import java.security.cert.X509Certificate;
 import java.util.Date;
 import java.util.concurrent.Callable;
 
+import org.bouncycastle.cert.jcajce.JcaX509CertificateHolder;
 import org.bouncycastle.cert.ocsp.BasicOCSPResp;
 import org.bouncycastle.cert.ocsp.BasicOCSPRespBuilder;
 import org.bouncycastle.cert.ocsp.OCSPException;
@@ -25,7 +26,6 @@ import org.bouncycastle.operator.ContentSigner;
 import org.bouncycastle.operator.OperatorCreationException;
 import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 import org.cesecore.certificates.ocsp.exception.OcspFailureException;
-import org.ejbca.core.protocol.ocsp.OCSPUtil;
 
 /**
  * This internal class exists for the sole purpose of catching deadlocks in the HSM hardware.
@@ -57,11 +57,26 @@ public class HsmResponseThread implements Callable<BasicOCSPResp> {
     public BasicOCSPResp call() throws OCSPException {
         try {
             final ContentSigner signer = new JcaContentSignerBuilder(signingAlgorithm).setProvider(provider).build(signerKey);
-            return basicRes.build(signer, OCSPUtil.convertCertificateChainToCertificateHolderChain(chain), new Date());
+            return basicRes.build(signer, convertCertificateChainToCertificateHolderChain(chain), new Date());
         } catch (CertificateEncodingException e) {
             throw new OcspFailureException(e);
         } catch (OperatorCreationException e) {
             throw new OcspFailureException(e);
         }
+    }
+    
+    /**
+     * Converts a X509Certificate chain into a JcaX509CertificateHolder chain.
+     * 
+     * @param certificateChain input chain to be converted
+     * @return the result
+     * @throws CertificateEncodingException if there is a problem extracting the certificate information.
+     */
+    private static final JcaX509CertificateHolder[] convertCertificateChainToCertificateHolderChain(X509Certificate[] certificateChain) throws CertificateEncodingException {
+        final JcaX509CertificateHolder[] certificateHolderChain = new JcaX509CertificateHolder[certificateChain.length];
+        for (int i = 0; i < certificateChain.length; ++i) {
+            certificateHolderChain[i] = new JcaX509CertificateHolder(certificateChain[i]);
+        }
+        return certificateHolderChain;
     }
 }
