57f675ce24b16bd7f4573b949b67b3e7052f8467
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2013-01-21 12:12:59 +0000

    Noticket: add commented out method missing to match cesecore code
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@16166 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/systemtests/src-interface/org/cesecore/configuration/CesecoreConfigurationProxySessionRemote.java
+++ modules/systemtests/src-interface/org/cesecore/configuration/CesecoreConfigurationProxySessionRemote.java
@@ -33,4 +33,5 @@ public interface CesecoreConfigurationProxySessionRemote {
     
     String getConfigurationValue(String key);
     
+    //TrustedTimeProvider getTrustedTimeProvider() throws TrustedTimeProviderException;    
 }                           
