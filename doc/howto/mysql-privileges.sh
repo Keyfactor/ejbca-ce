49b9c0bb4c7f390ed541957bf8871a1284a9c655
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2013-03-11 15:28:37 +0000

    ECA-2492: improve script to allow override of default values with environment variables
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@16468 37aeb1de-b14b-0410-9a0e-82b28170d228

--- doc/howto/mysql-privileges.sh
+++ doc/howto/mysql-privileges.sh
@@ -2,18 +2,25 @@
 
 # This script will create an SQL file that you can inspect and apply to your database.
 # apply with 'mysql -u root -p < mysql-database-privileges.sql'
+#
+# Set environment variables to override default in the script, i.e.:
+# export SQLUSER=ejbca_user
+# export SQLHOST=database.domain.com
+# export SQLFILE=ejbca-script.sql
+# ./mysql-privileges.sh
 
 # root user used to create this script
 SQLROOTUSER=root
-# The user that we should revoke and grant privileges from
-SQLUSER=ejbca
-SQLHOST=localhost
+# The user@host that we should revoke and grant privileges from
+SQLUSER=${SQLUSER:-"ejbca"}
+SQLHOST=${SQLHOST:-"localhost"}
 # The database that SQLUSER will have access to
-SQLDATABASE=ejbca
+SQLDATABASE=${SQLDATABASE:-"ejbca"}
 # The output file where resulting the sql script will be written 
-SQLFILE=mysql-database-privileges.sql
+SQLFILE=${SQLFILE:-"mysql-database-privileges.sql"}
 
-echo "Accessing database \"$SQLDATABASE\" as user \"$SQLROOTUSER\"."
+echo "Accessing database \"$SQLDATABASE\" as user \"$SQLROOTUSER\" to read table metadata, the database will not be modified."
+echo "Granting/restricting privileges for user \"$SQLUSER@$SQLHOST\". Writing output to \"$SQLFILE\"."
 COMMAND=`echo "show tables" | mysql -u $SQLROOTUSER -p $SQLDATABASE | grep -v Tables_in_ejbca`
 echo "revoke ALL PRIVILEGES, GRANT OPTION from '$SQLUSER'@'$SQLHOST';" > $SQLFILE
 for table in $COMMAND; do echo "grant SELECT,INSERT,UPDATE,DELETE,INDEX on $SQLDATABASE.$table to '$SQLUSER'@'$SQLHOST';"; done >> $SQLFILE
