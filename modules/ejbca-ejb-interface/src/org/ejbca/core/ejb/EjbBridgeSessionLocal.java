ffdff08ee6cfe17497a419199c5e5ab4330273a3
netmackan <USER@primekey.com>
2013-04-08 13:58:10 +0000

    ECA-2367: Reverting all changes to this issue as  it turns out the current solution will not work on JBoss 7.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@16545 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb-interface/src/org/ejbca/core/ejb/EjbBridgeSessionLocal.java
+++ modules/ejbca-ejb-interface/src/org/ejbca/core/ejb/EjbBridgeSessionLocal.java
@@ -41,7 +41,6 @@ import org.ejbca.core.ejb.ca.publisher.PublisherSessionLocal;
 import org.ejbca.core.ejb.ca.revoke.RevocationSessionLocal;
 import org.ejbca.core.ejb.ca.sign.SignSessionLocal;
 import org.ejbca.core.ejb.ca.store.CertReqHistorySessionLocal;
-import org.ejbca.core.ejb.certificates.crl.EjbcaCrlCreateSessionLocal;
 import org.ejbca.core.ejb.config.GlobalConfigurationSessionLocal;
 import org.ejbca.core.ejb.hardtoken.HardTokenBatchJobSessionLocal;
 import org.ejbca.core.ejb.hardtoken.HardTokenSessionLocal;
@@ -82,7 +81,7 @@ public interface EjbBridgeSessionLocal {
 	RevocationSessionLocal getRevocationSession();
 	CmpMessageDispatcherSessionLocal getCmpMessageDispatcherSession();
 	CrlStoreSessionLocal getCrlStoreSession();
-	EjbcaCrlCreateSessionLocal getCrlCreateSession();
+	CrlCreateSessionLocal getCrlCreateSession();
     EjbcaAuditorSessionLocal getEjbcaAuditorSession();
 	EndEntityAuthenticationSessionLocal getEndEntityAuthenticationSession();
 	EndEntityAccessSessionLocal getEndEntityAccessSession();
