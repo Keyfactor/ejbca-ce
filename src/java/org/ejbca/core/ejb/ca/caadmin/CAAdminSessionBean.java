f3f004ffa6e89ff6a9914d68620a95bd88b93e97
netmackan <USER@primekey.com>
2013-04-08 11:39:33 +0000

    ECA-2367: Reverting last change done in this issue since r16540.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@16544 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/core/ejb/ca/caadmin/CAAdminSessionBean.java
+++ src/java/org/ejbca/core/ejb/ca/caadmin/CAAdminSessionBean.java
@@ -109,6 +109,7 @@ import org.cesecore.certificates.certificateprofile.CertificatePolicy;
 import org.cesecore.certificates.certificateprofile.CertificateProfile;
 import org.cesecore.certificates.certificateprofile.CertificateProfileConstants;
 import org.cesecore.certificates.certificateprofile.CertificateProfileSessionLocal;
+import org.cesecore.certificates.crl.CrlCreateSessionLocal;
 import org.cesecore.certificates.crl.CrlStoreSessionLocal;
 import org.cesecore.certificates.crl.RevokedCertInfo;
 import org.cesecore.certificates.endentity.EndEntityInformation;
@@ -141,7 +142,6 @@ import org.ejbca.core.ejb.audit.enums.EjbcaEventTypes;
 import org.ejbca.core.ejb.audit.enums.EjbcaServiceTypes;
 import org.ejbca.core.ejb.ca.publisher.PublisherSessionLocal;
 import org.ejbca.core.ejb.ca.revoke.RevocationSessionLocal;
-import org.ejbca.core.ejb.certificates.crl.EjbcaCrlCreateSessionLocal;
 import org.ejbca.core.model.InternalEjbcaResources;
 import org.ejbca.core.model.approval.ApprovalDataVO;
 import org.ejbca.core.model.approval.ApprovalException;
@@ -190,7 +190,7 @@ public class CAAdminSessionBean implements CAAdminSessionLocal, CAAdminSessionRe
     @EJB
     private RevocationSessionLocal revocationSession;
     @EJB
-    private EjbcaCrlCreateSessionLocal crlCreateSession;
+    private CrlCreateSessionLocal crlCreateSession;
     @EJB
     private PublisherSessionLocal publisherSession;
     @EJB
