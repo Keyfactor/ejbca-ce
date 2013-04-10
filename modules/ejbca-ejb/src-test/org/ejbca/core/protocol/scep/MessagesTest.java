341c79c2b040ed61bd1e5c4c3aa967b364d76a3c
Mike Agrenius Kushner <mike.kushner@primekey.com>
2013-04-10 13:01:37 +0000

    NoTicket: Removing some unused imports
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@16557 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb/src-test/org/ejbca/core/protocol/scep/MessagesTest.java
+++ modules/ejbca-ejb/src-test/org/ejbca/core/protocol/scep/MessagesTest.java
@@ -14,7 +14,6 @@
 package org.ejbca.core.protocol.scep;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
