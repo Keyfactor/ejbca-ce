8d7040052e14cca9e7ad7ab4686c0757c81ec3ab
Mike Agrenius Kushner <mike.kushner@primekey.com>
2013-04-11 06:53:35 +0000

    NoTicket: Unused import
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@16567 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb-cli/src/org/ejbca/ui/cli/batch/BatchToolProperties.java
+++ modules/ejbca-ejb-cli/src/org/ejbca/ui/cli/batch/BatchToolProperties.java
@@ -14,7 +14,6 @@ package org.ejbca.ui.cli.batch;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
