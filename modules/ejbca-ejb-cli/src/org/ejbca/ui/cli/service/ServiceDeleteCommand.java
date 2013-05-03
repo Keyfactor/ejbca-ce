0bb3598653b9de4237cba0d378fb25409981e2bc
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2013-05-03 15:14:14 +0000

    ECA-3019: CLI commands to modify services
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@16730 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ejbca-ejb-cli/src/org/ejbca/ui/cli/service/ServiceDeleteCommand.java
@@ -0,0 +1,50 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.ui.cli.service;
+
+import org.ejbca.core.ejb.services.ServiceSessionRemote;
+import org.ejbca.core.model.services.ServiceConfiguration;
+import org.ejbca.core.model.services.ServiceExistsException;
+import org.ejbca.ui.cli.ErrorAdminCommandException;
+
+/**
+ * CLI subcommand for deleting services.
+ * 
+ * @version $Id$
+ */
+public class ServiceDeleteCommand extends BaseServiceCommand {
+
+    @Override
+    public String getSubCommand() {
+        return "delete";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Deletes a service.";
+    }
+
+    @Override
+    public void execute(String[] args, int serviceId) throws ErrorAdminCommandException {
+        if (args.length < 2) {
+            getLogger().info("Description: " + getDescription());
+            getLogger().info("Usage: " + getCommand() + " <service name>");
+            return;
+        }
+        
+        final ServiceSessionRemote serviceSession = ejb.getRemoteSession(ServiceSessionRemote.class);
+        final String serviceName = serviceSession.getServiceName(serviceId);
+        serviceSession.removeService(getAdmin(), serviceName);
+        getLogger().info("Service deleted.");
+    }
+}
