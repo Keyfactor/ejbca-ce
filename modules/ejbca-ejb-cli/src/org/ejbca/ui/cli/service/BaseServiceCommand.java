628f09261e6d6df1d9c33e53c7c4707ae5642bc5
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2013-05-03 10:03:37 +0000

    ECA-3019: CLI commands to list and show info of services
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@16727 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ejbca-ejb-cli/src/org/ejbca/ui/cli/service/BaseServiceCommand.java
@@ -0,0 +1,65 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
+package org.ejbca.ui.cli.service;
+
+import org.cesecore.authentication.tokens.AuthenticationToken;
+import org.ejbca.core.ejb.services.ServiceSessionRemote;
+import org.ejbca.ui.cli.BaseCommand;
+import org.ejbca.ui.cli.CliUsernameException;
+import org.ejbca.ui.cli.ErrorAdminCommandException;
+
+/**
+ * Base for service commands, contains common functions for service operations
+ *
+ * @version $Id$
+ */
+public abstract class BaseServiceCommand extends BaseCommand {
+
+    @Override
+    public String getMainCommand() {
+        return "service";
+    }
+    
+    @Override
+    public void execute(String[] args) throws ErrorAdminCommandException {
+        try {
+            args = parseUsernameAndPasswordFromArgs(args);
+        } catch (CliUsernameException e) {
+            return;
+        }
+        
+        int serviceId = 0;
+        if (acceptsServiceName()) {
+            final ServiceSessionRemote serviceSession = ejb.getRemoteSession(ServiceSessionRemote.class);
+            serviceId = serviceSession.getServiceId(args[1]);
+            if (serviceId == 0 && failIfServiceMissing()) {
+                getLogger().info("Unknown Service: " + args[1]);
+                return;
+            }
+            
+        }
+        
+        execute(args, serviceId);
+    }
+    
+    public abstract void execute(String[] args, int serviceId) throws ErrorAdminCommandException;
+    
+    protected boolean acceptsServiceName() { return true; }
+    protected boolean failIfServiceMissing() { return true; }
+    
+    /** @return the EJB CLI admin */
+    protected AuthenticationToken getAdmin() {
+        return getAdmin(cliUserName, cliPassword);
+    }
+}
---
+++ modules/ejbca-ejb-cli/src/org/ejbca/ui/cli/service/ServiceInfoCommand.java
@@ -0,0 +1,121 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.ui.cli.service;
+
+import java.util.Map.Entry;
+import java.util.Properties;
+
+import org.apache.commons.lang.StringUtils;
+import org.ejbca.core.ejb.services.ServiceSessionRemote;
+import org.ejbca.core.model.services.ServiceConfiguration;
+import org.ejbca.ui.cli.ErrorAdminCommandException;
+
+/**
+ * CLI subcommand that shows the settings of a service.
+ * 
+ * @version $Id$
+ */
+public class ServiceInfoCommand extends BaseServiceCommand {
+
+    @Override
+    public String getSubCommand() {
+        return "info";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Show information about a service.";
+    }
+
+    @Override
+    public void execute(String[] args, int serviceId) throws ErrorAdminCommandException {
+        if (args.length != 2) {
+            getLogger().info("Description: " + getDescription());
+            getLogger().info("Usage: " + getCommand() + " <service name>");
+            return;
+        }
+        
+        final ServiceSessionRemote serviceSession = ejb.getRemoteSession(ServiceSessionRemote.class);
+        ServiceConfiguration serviceConfig = serviceSession.getServiceConfiguration(getAdmin(), serviceId);
+        
+        info("Id", serviceId);
+        info("Name", serviceSession.getServiceName(serviceId));
+        
+        getLogger().info("");
+        getLogger().info("----- Worker -----");
+        info("Worker class", serviceConfig.getWorkerClassPath());
+        showProperties(serviceConfig.getWorkerProperties());
+        
+        getLogger().info("");
+        getLogger().info("-----Interval -----");
+        info("Interval class", serviceConfig.getIntervalClassPath());
+        showProperties(serviceConfig.getIntervalProperties());
+        
+        getLogger().info("");
+        getLogger().info("----- Action -----");
+        info("Action class", serviceConfig.getActionClassPath());
+        showProperties(serviceConfig.getActionProperties());
+        
+        getLogger().info("");
+        getLogger().info("----- General Settings -----");
+        info("Active", serviceConfig.isActive());
+        info("Pin to nodes", serviceConfig.getPinToNodes());
+        info("Description", serviceConfig.getDescription());
+    }
+    
+    private void info(String name, String value) {
+        if (value == null) {
+            value = "null";
+        }
+        
+        value = value.replaceAll("\r?\n", "\n"+StringUtils.repeat(" ", 15));
+        getLogger().info(StringUtils.rightPad(name, 13)+": "+value);
+    }
+    
+    private void info(String name, int value) {
+        info(name, String.valueOf(value));
+    }
+    
+    private void info(String name, boolean value) {
+        info(name, value ? "Yes" : "No");
+    }
+    
+    private void info(String name, String[] value) {
+        if (value == null) {
+            info(name, "null");
+            return;
+        }
+        
+        info(name, StringUtils.join(value, '\n'));
+    }
+    
+    private void showProperties(Properties props) {
+        if (props == null) {
+            getLogger().info("Properties object is null.");
+            return;
+        }
+        
+        StringBuilder sb = new StringBuilder("Properties:\n");
+        for (Entry<Object,Object> entry : props.entrySet()) {
+            sb.append("  ");
+            sb.append(entry.getKey().toString());
+            sb.append(" = ");
+            sb.append(entry.getValue().toString());
+            sb.append('\n');
+        }
+        sb.deleteCharAt(sb.length()-1);
+        getLogger().info(sb.toString());
+    }
+    
+
+}
