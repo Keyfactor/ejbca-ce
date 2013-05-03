d87ab39cc451573cf98eb8b93a7166b7294a29ee
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2013-05-03 15:14:14 +0000

    ECA-3019: CLI commands to modify services
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@16730 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ejbca-ejb-cli/src/org/ejbca/ui/cli/service/ServiceCreateCommand.java
@@ -0,0 +1,62 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.ui.cli.service;
+
+import org.ejbca.core.ejb.services.ServiceSessionRemote;
+import org.ejbca.core.model.services.ServiceConfiguration;
+import org.ejbca.core.model.services.ServiceExistsException;
+import org.ejbca.ui.cli.ErrorAdminCommandException;
+
+/**
+ * CLI subcommand for creating services.
+ * 
+ * @version $Id$
+ */
+public class ServiceCreateCommand extends BaseServiceModificationCommand {
+
+    @Override
+    public String getSubCommand() {
+        return "create";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Creates a new service.";
+    }
+
+    @Override
+    public void execute(String[] args, int serviceId) throws ErrorAdminCommandException {
+        if (args.length < 2) {
+            getLogger().info("Description: " + getDescription());
+            getLogger().info("Usage: " + getCommand() + " <service name> <field or property>=<value>...");
+            getLogger().info("   or: " + getCommand() + " <service name> -listFields|-listProperties");
+            return;
+        }
+        
+        final ServiceSessionRemote serviceSession = ejb.getRemoteSession(ServiceSessionRemote.class);
+        final String serviceName = args[1].trim();
+        
+        ServiceConfiguration serviceConfig = new ServiceConfiguration();
+        if (modifyFromArgs(serviceConfig, args)) {
+            try {
+                serviceSession.addService(getAdmin(), serviceName, serviceConfig);
+                getLogger().info("Service created.");
+            } catch (ServiceExistsException e) {
+                getLogger().info("Service exists already.");
+            }
+        }
+    }
+    
+    @Override
+    protected boolean failIfServiceMissing() { return false; }
+}
