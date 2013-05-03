8ebd3a85ec95009a7cf82bcc14caa719fa3ff99f
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2013-05-03 15:14:14 +0000

    ECA-3019: CLI commands to modify services
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@16730 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ejbca-ejb-cli/src/org/ejbca/ui/cli/service/BaseServiceModificationCommand.java
@@ -0,0 +1,124 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.ui.cli.service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Properties;
+
+import org.ejbca.core.model.services.ServiceConfiguration;
+import org.ejbca.ui.cli.ErrorAdminCommandException;
+import org.ejbca.ui.cli.FieldEditor;
+
+/**
+ * Base class with methods to modify beans dynamically using FieldEditor.
+ * 
+ * @version $Id$
+ */
+public abstract class BaseServiceModificationCommand extends BaseServiceCommand {
+
+    protected boolean modifyFromArgs(ServiceConfiguration serviceConfig, String[] args) {
+        FieldEditor fieldEditor = new FieldEditor(getLogger());
+        
+        // Check help arguments
+        boolean exit = false;
+        if (Arrays.asList(args).contains("-listFields")) { 
+            fieldEditor.listSetMethods(serviceConfig);
+            exit = true;
+        }
+        if (Arrays.asList(args).contains("-listProperties")) {
+            displayPropertiesHelp(serviceConfig.getWorkerProperties());
+            displayPropertiesHelp(serviceConfig.getIntervalProperties());
+            displayPropertiesHelp(serviceConfig.getActionProperties());
+            exit = true;
+        }
+        if (exit) return false;
+        
+        
+        // Parse fields to modify
+        List<String> params = Arrays.asList(args).subList(2, args.length);
+        List<String> errors = new ArrayList<String>();
+        for (String property : params) {
+            if (property.equals("-listFields")) continue;
+            String[] arr = property.split("=", 2);
+            String field = arr[0].trim();
+            String value = arr[1].trim();
+            if (!modify(serviceConfig, fieldEditor, field, value)) {
+                errors.add(field);
+            }
+        }
+        
+        if (!errors.isEmpty()) {
+            getLogger().info("");
+            getLogger().info("ERROR: One or more names didn't exist either as a field or property:");
+            getLogger().info("");
+            for (String error : errors) {
+                getLogger().info("    "+error);
+            }
+            getLogger().info("");
+            getLogger().info("Changes were NOT saved!");
+            return false;
+        }
+        
+        return true;
+    }
+
+    private boolean modify(ServiceConfiguration serviceConfig, FieldEditor fieldEditor, String field, String value) {
+        try {
+            // Try to call the setter 
+            fieldEditor.listGetOrSet(false, false, ServiceConfiguration.class.getName(), field, value, serviceConfig);
+            getLogger().info("Updated field: "+field);
+            return true;
+        } catch (ErrorAdminCommandException e) {
+            // Check if it's in one of the Properties objects
+            Properties props;
+            
+            props = serviceConfig.getWorkerProperties();
+            if (props.containsKey(field)) {
+                props.setProperty(field, value);
+                getLogger().info("Updated worker property: "+field);
+                serviceConfig.setWorkerProperties(props);
+                return true;
+            }
+            
+            props = serviceConfig.getIntervalProperties();
+            if (props.containsKey(field)) {
+                props.setProperty(field, value);
+                getLogger().info("Updated interval property: "+field);
+                serviceConfig.setIntervalProperties(props);
+                return true;
+            }
+            
+            props = serviceConfig.getActionProperties();
+            if (props.containsKey(field)) {
+                props.setProperty(field, value);
+                getLogger().info("Updated action property: "+field);
+                serviceConfig.setActionProperties(props);
+                return true;
+            }
+            
+            getLogger().info(e.getMessage());
+            return false;
+        }
+    }
+        
+    private void displayPropertiesHelp(Properties props) {
+        for (Entry<Object,Object> prop : props.entrySet()) {
+            // We don't know the types but we can display the default values so the user can figure out.
+            getLogger().info(prop.getKey()+" (current value = "+prop.getValue()+")");
+        }
+    }
+
+}
---
+++ modules/ejbca-ejb-cli/src/org/ejbca/ui/cli/service/ServiceEditCommand.java
@@ -0,0 +1,54 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.ui.cli.service;
+
+import org.ejbca.core.ejb.services.ServiceSessionRemote;
+import org.ejbca.core.model.services.ServiceConfiguration;
+import org.ejbca.ui.cli.ErrorAdminCommandException;
+
+/**
+ * CLI subcommand for editing services.
+ * 
+ * @version $Id$
+ */
+public class ServiceEditCommand extends BaseServiceModificationCommand {
+
+    @Override
+    public String getSubCommand() {
+        return "edit";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Modifies fields and properties in a service.";
+    }
+
+    @Override
+    public void execute(String[] args, int serviceId) throws ErrorAdminCommandException {
+        if (args.length < 2) {
+            getLogger().info("Description: " + getDescription());
+            getLogger().info("Usage: " + getCommand() + " <service name> <field or property>=<value>...");
+            getLogger().info("   or: " + getCommand() + " <service name> -listFields|-listProperties");
+            return;
+        }
+        
+        final ServiceSessionRemote serviceSession = ejb.getRemoteSession(ServiceSessionRemote.class);
+        final String serviceName = serviceSession.getServiceName(serviceId);
+        ServiceConfiguration serviceConfig = serviceSession.getServiceConfiguration(getAdmin(), serviceId);
+        
+        if (modifyFromArgs(serviceConfig, args)) {
+            serviceSession.changeService(getAdmin(), serviceName, serviceConfig, false);
+            getLogger().info("Changes saved.");
+        }
+    }
+}
