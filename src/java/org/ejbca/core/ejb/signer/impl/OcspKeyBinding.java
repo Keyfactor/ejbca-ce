dbde85bcbbc27a474fcd590a630da3ad418894b5
Johan Eklund <johan.eklund@primekey.com>
2013-05-03 08:11:57 +0000

    ECA-2973: Translatable InternalKeyBinding properties. Clean up and some JavaDoc.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@16726 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/core/ejb/signer/impl/OcspKeyBinding.java
+++ src/java/org/ejbca/core/ejb/signer/impl/OcspKeyBinding.java
@@ -30,13 +30,15 @@ public class OcspKeyBinding extends InternalKeyBindingBase {
   
     private static final long serialVersionUID = 1L;
     private static final Logger log = Logger.getLogger(OcspKeyBinding.class);
+    
+    public enum ResponderIdType {
+        KEYHASH, NAME;
+    }
 
     public static final String IMPLEMENTATION_ALIAS = "OcspKeyBinding"; // This should not change, even if we rename the class in EJBCA 5.3+..
     public static final String PROPERTY_NON_EXISTING_GOOD = "nonexistingisgood";
-    public static final String PROPERTY_INCLUDE_CERT_IN_CHAIN = "includecertchain";
+    public static final String PROPERTY_INCLUDE_CERT_CHAIN = "includecertchain";
     public static final String PROPERTY_RESPONDER_ID_TYPE = "responderidtype";  // keyhash, name
-    public static final String PROPERTY_RESPONDER_ID_TYPE_VKEYHASH = "keyhash"; 
-    public static final String PROPERTY_RESPONDER_ID_TYPE_VNAME = "name";
     public static final String PROPERTY_REQUIRE_TRUSTED_SIGNATURE = "requireTrustedSignature";
     public static final String PROPERTY_UNTIL_NEXT_UPDATE = "untilNextUpdate";
     public static final String PROPERTY_MAX_AGE = "maxAge";
@@ -51,11 +53,11 @@ public class OcspKeyBinding extends InternalKeyBindingBase {
     public OcspKeyBinding() {
         super(new ArrayList<InternalKeyBindingProperty<? extends Serializable>>() {{
             add(new InternalKeyBindingProperty<Boolean>(PROPERTY_NON_EXISTING_GOOD, Boolean.FALSE));
-            add(new InternalKeyBindingProperty<Boolean>(PROPERTY_INCLUDE_CERT_IN_CHAIN, Boolean.TRUE));
-            add(new InternalKeyBindingProperty<String>(PROPERTY_RESPONDER_ID_TYPE, PROPERTY_RESPONDER_ID_TYPE_VKEYHASH, PROPERTY_RESPONDER_ID_TYPE_VKEYHASH, PROPERTY_RESPONDER_ID_TYPE_VNAME));
+            add(new InternalKeyBindingProperty<Boolean>(PROPERTY_INCLUDE_CERT_CHAIN, Boolean.TRUE));
+            add(new InternalKeyBindingProperty<String>(PROPERTY_RESPONDER_ID_TYPE, ResponderIdType.KEYHASH.name(), ResponderIdType.KEYHASH.name(), ResponderIdType.NAME.name()));
             add(new InternalKeyBindingProperty<Boolean>(PROPERTY_REQUIRE_TRUSTED_SIGNATURE, Boolean.FALSE));
             add(new InternalKeyBindingProperty<Integer>(PROPERTY_UNTIL_NEXT_UPDATE, 0));
-            add(new InternalKeyBindingProperty<Integer>(PROPERTY_MAX_AGE, 30));
+            add(new InternalKeyBindingProperty<Integer>(PROPERTY_MAX_AGE, 0));
         }});
     }
     
@@ -84,5 +86,35 @@ public class OcspKeyBinding extends InternalKeyBindingBase {
     }
     public void setNonExistingGood(boolean nonExistingGood) {
         setProperty(PROPERTY_NON_EXISTING_GOOD, Boolean.valueOf(nonExistingGood));
+    }
+    public boolean getIncludeCertCertChain() {
+        return (Boolean) getProperty(PROPERTY_INCLUDE_CERT_CHAIN).getValue();
+    }
+    public void setIncludeCertCertChain(boolean includeCertChain) {
+        setProperty(PROPERTY_INCLUDE_CERT_CHAIN, Boolean.valueOf(includeCertChain));
+    }
+    public ResponderIdType getResponderIdType() {
+        return ResponderIdType.valueOf((String) getProperty(PROPERTY_RESPONDER_ID_TYPE).getValue());
+    }
+    public void setResponderIdType(ResponderIdType responderIdType) {
+        setProperty(PROPERTY_RESPONDER_ID_TYPE, responderIdType.name());
+    }
+    public boolean getRequireTrustedSignature() {
+        return (Boolean) getProperty(PROPERTY_REQUIRE_TRUSTED_SIGNATURE).getValue();
+    }
+    public void setRequireTrustedSignature(boolean requireTrustedSignature) {
+        setProperty(PROPERTY_REQUIRE_TRUSTED_SIGNATURE, Boolean.valueOf(requireTrustedSignature));
+    }
+    public int getUntilNextUpdate() {
+        return (Integer) getProperty(PROPERTY_UNTIL_NEXT_UPDATE).getValue();
+    }
+    public void setUntilNextUpdate(int untilNextUpdate) {
+        setProperty(PROPERTY_UNTIL_NEXT_UPDATE, Integer.valueOf(untilNextUpdate));
+    }
+    public int getMaxAge() {
+        return (Integer) getProperty(PROPERTY_MAX_AGE).getValue();
+    }
+    public void setMaxAge(int maxAge) {
+        setProperty(PROPERTY_MAX_AGE, Integer.valueOf(maxAge));
     }
 }
