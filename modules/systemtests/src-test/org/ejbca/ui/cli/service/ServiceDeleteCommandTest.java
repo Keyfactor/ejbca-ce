f1fd3c8304e1d3175a8c6c36c9d62a4d63cf3fd1
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2013-05-06 15:14:55 +0000

    ECA-3019: Fix service CLI tests
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@16751 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/systemtests/src-test/org/ejbca/ui/cli/service/ServiceDeleteCommandTest.java
+++ modules/systemtests/src-test/org/ejbca/ui/cli/service/ServiceDeleteCommandTest.java
@@ -15,6 +15,9 @@ package org.ejbca.ui.cli.service;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
+import org.cesecore.authentication.tokens.AuthenticationToken;
+import org.cesecore.authentication.tokens.UsernamePrincipal;
+import org.cesecore.mock.authentication.tokens.TestAlwaysAllowLocalAuthenticationToken;
 import org.cesecore.util.CryptoProviderTools;
 import org.ejbca.core.model.services.ServiceConfiguration;
 import org.ejbca.ui.cli.ErrorAdminCommandException;
@@ -33,6 +36,7 @@ public class ServiceDeleteCommandTest extends ServiceTestCase {
     @org.junit.Rule
     public org.junit.rules.TestRule traceLogMethodsRule = new TraceLogMethodsRule();
     
+    private static final AuthenticationToken admin = new TestAlwaysAllowLocalAuthenticationToken(new UsernamePrincipal("ServiceDeleteCommandTest"));
     private ServiceDeleteCommand serviceDeleteCommand;
     private static final String SERVICE_NAME = "TestServiceCLIDelete";
     
@@ -44,15 +48,15 @@ public class ServiceDeleteCommandTest extends ServiceTestCase {
     public void setUp() throws Exception {
         CryptoProviderTools.installBCProviderIfNotAvailable();
         serviceDeleteCommand = new ServiceDeleteCommand();
-        getServiceSession().removeService(getAdmin(), SERVICE_NAME);
+        getServiceSession().removeService(admin, SERVICE_NAME);
         
         ServiceConfiguration sc = new ServiceConfiguration();
-        getServiceSession().addService(getAdmin(), SERVICE_NAME, sc);
+        getServiceSession().addService(admin, SERVICE_NAME, sc);
     }
 
     @After
     public void tearDown() throws Exception {
-        getServiceSession().removeService(getAdmin(), SERVICE_NAME);
+        getServiceSession().removeService(admin, SERVICE_NAME);
     }
     
     @Test
