007ea4e0c44f559f48ece0d48a521f352ccf50ae
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2013-05-06 12:19:58 +0000

    ECA-3006: implement basic editca CLI command.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@16746 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/systemtests/src-test/org/ejbca/ui/cli/ca/CaEditCommandTest.java
@@ -0,0 +1,71 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
+package org.ejbca.ui.cli.ca;
+
+import static org.junit.Assert.assertEquals;
+
+import org.cesecore.authentication.tokens.AuthenticationToken;
+import org.cesecore.authentication.tokens.UsernamePrincipal;
+import org.cesecore.certificates.ca.CAInfo;
+import org.cesecore.certificates.ca.CaSessionRemote;
+import org.cesecore.certificates.certificate.InternalCertificateStoreSessionRemote;
+import org.cesecore.certificates.certificateprofile.CertificateProfileSessionRemote;
+import org.cesecore.keys.token.CryptoTokenManagementSessionRemote;
+import org.cesecore.mock.authentication.tokens.TestAlwaysAllowLocalAuthenticationToken;
+import org.cesecore.util.CryptoProviderTools;
+import org.cesecore.util.EjbRemoteHelper;
+import org.ejbca.core.ejb.ca.CaTestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * System test class for CaInitCommandTest
+ * 
+ * @version $Id: CaInitCommandTest.java 16723 2013-05-02 15:34:40Z anatom $
+ */
+public class CaEditCommandTest {
+
+    private static final String CA_NAME = "1327editca2";
+    private static final String[] HAPPY_PATH_ARGS = { "editca", CA_NAME, "CRLPeriod", "2592000000"};
+
+    private CaEditCaCommand caEditCommand;
+    private AuthenticationToken admin = new TestAlwaysAllowLocalAuthenticationToken(new UsernamePrincipal("CaEditCommandTest"));
+
+    private CaSessionRemote caSession = EjbRemoteHelper.INSTANCE.getRemoteSession(CaSessionRemote.class);
+
+    @Before
+    public void setUp() throws Exception {
+        CryptoProviderTools.installBCProviderIfNotAvailable();
+        caEditCommand = new CaEditCaCommand();
+        CaTestCase.removeTestCA(CA_NAME);
+        CaTestCase.createTestCA(CA_NAME);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        CaTestCase.removeTestCA(CA_NAME);
+    }
+
+    /** Test trivial happy path for execute, i.e, create an ordinary CA. */
+    @Test
+    public void testExecuteHappyPath() throws Exception {
+        CAInfo info = caSession.getCAInfo(admin, CA_NAME);
+        assertEquals("CRLPeriod of a newly created default CA is incorrect, did default value change?", 86400000L, info.getCRLPeriod());
+        caEditCommand.execute(HAPPY_PATH_ARGS);
+        info = caSession.getCAInfo(admin, CA_NAME);
+        assertEquals("CRLPeriod of a edited CA is incorrect. Edit did ot work?", 2592000000L, info.getCRLPeriod());
+    }
+    
+}
