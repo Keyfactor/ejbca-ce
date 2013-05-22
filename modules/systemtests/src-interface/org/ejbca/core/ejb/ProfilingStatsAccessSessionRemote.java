e5d5a51dfa1f2fbef5b54187f879362e9965c467
Johan Eklund <johan.eklund@primekey.com>
2013-05-22 19:21:25 +0000

    ECA-3025: Added built in profiling capabilities that are invoked last of the system tests. Results are available as log output of the test. The test can also be invoked at any time using the regular "ant test:runone".
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@16853 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/systemtests/src-interface/org/ejbca/core/ejb/ProfilingStatsAccessSessionRemote.java
@@ -0,0 +1,31 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.core.ejb;
+
+import java.util.List;
+
+import javax.ejb.Remote;
+
+import org.ejbca.core.ejb.ProfilingStat;
+
+/**
+ * @see org.ejbca.core.ProfilingStatsAccessSessionBean
+ * 
+ * @version $Id$
+ */
+@Remote
+public interface ProfilingStatsAccessSessionRemote {
+    
+    /** @see org.ejbca.core.ejb.ProfilingStats#getEjbInvocationStats() */
+    public List<ProfilingStat> getEjbInvocationStats();
+}
