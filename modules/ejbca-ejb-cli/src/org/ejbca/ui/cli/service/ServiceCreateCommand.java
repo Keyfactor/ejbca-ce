bdb955cdd6354d96cee72c41bcd39419739f0bcc
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2013-05-23 14:24:04 +0000

    ECA-3019: Improve example & help text for "service create" command
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@16863 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb-cli/src/org/ejbca/ui/cli/service/ServiceCreateCommand.java
+++ modules/ejbca-ejb-cli/src/org/ejbca/ui/cli/service/ServiceCreateCommand.java
@@ -43,7 +43,12 @@ public class ServiceCreateCommand extends BaseServiceModificationCommand {
             getLogger().info("");
             displayListFieldsHelp();
             getLogger().info("Example usage:");
-            getLogger().info("        service create WeeklyService intervalClassPath=org.ejbca.core.model.services.intervals.PeriodicalInterval interval.periodical.unit=DAYS interval.periodical.value=7");
+            getLogger().info("        service create DailyCRLUpdate workerClassPath=org.ejbca.core.model.services.workers.CRLUpdateWorker worker.caidstocheck=1 intervalClassPath=org.ejbca.core.model.services.intervals.PeriodicalInterval interval.periodical.unit=DAYS interval.periodical.value=1 actionClassPath=org.ejbca.core.model.services.actions.NoAction active=true");
+            getLogger().info("");
+            getLogger().info("This example creates a service that updates the CRL on a daily basis.");
+            getLogger().info("The worker.caidstocheck value of 1 means check all CAs.");
+            getLogger().info("You can create services from the Admin Web and use the \"service info\" command");
+            getLogger().info("to learn how the CLI fields correspond to the fields in the Admin Web.");
             return;
         }
         
