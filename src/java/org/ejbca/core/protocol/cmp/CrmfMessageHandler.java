507307753a0861de83ec4c7ea75b1237b41a6536
aveen4711 <USER@primekey.com>
2013-05-23 07:58:47 +0000

    ECA-3028: CMP: Made changes so that even UnidHandler can extract CMP configurations with alias. A system test is added.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@16859 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/core/protocol/cmp/CrmfMessageHandler.java
+++ src/java/org/ejbca/core/protocol/cmp/CrmfMessageHandler.java
@@ -361,7 +361,7 @@ public class CrmfMessageHandler extends BaseCmpMessageHandler implements ICmpMes
 			// Create a username and password and register the new user in EJBCA
 			final UsernameGenerator gen = UsernameGenerator.getInstance(this.usernameGenParams);
 			// Don't convert this DN to an ordered EJBCA DN string with CertTools.stringToBCDNString because we don't want double escaping of some characters
-			final RequestMessage req =  this.extendedUserDataHandler!=null ? this.extendedUserDataHandler.processRequestMessage(crmfreq, certProfileName) : crmfreq;
+			final RequestMessage req =  this.extendedUserDataHandler!=null ? this.extendedUserDataHandler.processRequestMessage(crmfreq, certProfileName, CmpConfiguration.getUnidDataSource(this.confAlias)) : crmfreq;
 			final X500Name dnname = req.getRequestX500Name();
 			if (LOG.isDebugEnabled()) {
 				LOG.debug("Creating username from base dn: "+dnname.toString());
