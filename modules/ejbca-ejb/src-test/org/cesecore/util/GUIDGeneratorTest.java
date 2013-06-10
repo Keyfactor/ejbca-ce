861cdbf2c6604052b80d8f6b485b25c7e3405199
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2013-06-10 12:12:53 +0000

    ECA-3044: use 1000 instead of 10000
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@17033 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb/src-test/org/cesecore/util/GUIDGeneratorTest.java
+++ modules/ejbca-ejb/src-test/org/cesecore/util/GUIDGeneratorTest.java
@@ -33,8 +33,8 @@ public class GUIDGeneratorTest {
         String guid;
         for (int j = 1; j < 500001; j++) {
             // Ensure we don't generate all GUIDs within the same millisecond, so pause 1ms for every 10000 GUIDs generated.
-            // This should be more "real world", as we will probably not add more than 10000 records to a DB during 1 single millisecond
-            if (j % 10000 == 0) {
+            // This should be more "real world", as we will probably not add more than 1000 records to a DB during 1 single millisecond
+            if (j % 1000 == 0) {
                 //log.info("Generated "+j+" GUIDs so far, sleeping for 1ms.");
                 Thread.sleep(1);
             }
