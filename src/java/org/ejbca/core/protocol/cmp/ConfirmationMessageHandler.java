848b1a42920b0921f5c12c8b90c0ffe6a876a386
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2013-06-26 09:25:58 +0000

    Noticket: fix small spelling and improve debug log
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@17211 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/core/protocol/cmp/ConfirmationMessageHandler.java
+++ src/java/org/ejbca/core/protocol/cmp/ConfirmationMessageHandler.java
@@ -96,7 +96,7 @@ public class ConfirmationMessageHandler extends BaseCmpMessageHandler implements
 			// Creating the confirm message response
 			
 			if (LOG.isDebugEnabled()) {
-				LOG.debug("Creating a PKI confirm message response");
+				LOG.debug("Creating a PKI confirm message response, responseProtection="+responseProtection);
 			}
 			CmpConfirmResponseMessage cresp = new CmpConfirmResponseMessage();
 			cresp.setRecipientNonce(msg.getSenderNonce());
