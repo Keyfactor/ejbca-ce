e47b58298b477a1376f1a911f36b367961d1aaa2
Mike Agrenius Kushner <mike.kushner@primekey.com>
2013-07-05 08:33:06 +0000

    NoTicket: Formatting
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@17264 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/admin-gui/src/org/ejbca/ui/web/admin/rainterface/EditEndEntityBean.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/rainterface/EditEndEntityBean.java
@@ -30,98 +30,98 @@ import org.cesecore.certificates.endentity.ExtendedInformation;
  * @version $Id$
  */
 public class EditEndEntityBean {
-	private ExtendedInformation extendedInformation;
+    private ExtendedInformation extendedInformation;
 
-        /**
-         * Set the current end entity's ExtendedInformation.
-         * @param extendedInformation 
-         */
-	public void setExtendedInformation(ExtendedInformation extendedInformation) {
-		this.extendedInformation = extendedInformation;
-	}
+    /**
+     * Set the current end entity's ExtendedInformation.
+     * @param extendedInformation 
+     */
+    public void setExtendedInformation(ExtendedInformation extendedInformation) {
+        this.extendedInformation = extendedInformation;
+    }
 
-        /**
-         * Parses certificate extension data from a String of properties in Java 
-         * Properties format and store it in the extended information.
-         *
-         * @param extensionData properties to parse and store.
-         * @throws IOException 
-         */
-	@SuppressWarnings({ "rawtypes", "unchecked" })
+    /**
+     * Parses certificate extension data from a String of properties in Java 
+     * Properties format and store it in the extended information.
+     *
+     * @param extensionData properties to parse and store.
+     * @throws IOException 
+     */
+    @SuppressWarnings({ "rawtypes", "unchecked" })
     public void setExtensionData(String extensionData) {
-		Properties properties = new Properties();
-                try {
-                    properties.load(new StringReader(extensionData));
-                } catch (IOException ex) {
-                    // Should not happen as we are only reading from a String.
-                    throw new RuntimeException(ex);
+        Properties properties = new Properties();
+        try {
+            properties.load(new StringReader(extensionData));
+        } catch (IOException ex) {
+            // Should not happen as we are only reading from a String.
+            throw new RuntimeException(ex);
+        }
+
+        // Remove old extensiondata
+        Map data = (Map) extendedInformation.getData();
+        for (Object o : data.keySet()) {
+            if (o instanceof String) {
+                String key = (String) o;
+                if (key.startsWith(ExtendedInformation.EXTENSIONDATA)) {
+                    data.remove(key);
+                }
+            }
+        }
+
+        // Add new extensiondata
+        for (Object o : properties.keySet()) {
+            if (o instanceof String) {
+                String key = (String) o;
+                data.put(ExtendedInformation.EXTENSIONDATA + key, properties.getProperty(key));
+            }
+        }
+
+        // Updated ExtendedInformation to use the new data
+        extendedInformation.loadData(data);
+    }
+
+    /**
+     * @return The extension data read from the extended information and 
+     * formatted as in a Properties file.
+     */
+    public String getExtensionData() {
+        final String result;
+        if (extendedInformation == null) {
+            result = "";
+        } else {
+            @SuppressWarnings("rawtypes")
+            Map data = (Map) extendedInformation.getData();
+            Properties properties = new Properties();
+
+            for (Object o : data.keySet()) {
+                if (o instanceof String) {
+                    String key = (String) o;
+                    if (key.startsWith(ExtendedInformation.EXTENSIONDATA)) {
+                        String subKey = key.substring(ExtendedInformation.EXTENSIONDATA.length());
+                        properties.put(subKey, data.get(key));
+                    }
                 }
 
-		// Remove old extensiondata
-		Map data = (Map) extendedInformation.getData();
-		for (Object o : data.keySet()) {
-			if (o instanceof String) {
-				String key = (String) o;
-				if (key.startsWith(ExtendedInformation.EXTENSIONDATA)) {
-					data.remove(key);
-				}
-			}
-		}
+            }
 
-		// Add new extensiondata
-		for (Object o : properties.keySet()) {
-			if (o instanceof String) {
-				String key = (String) o;
-				data.put(ExtendedInformation.EXTENSIONDATA + key, properties.getProperty(key));
-			}
-		}
-                
-                // Updated ExtendedInformation to use the new data
-                extendedInformation.loadData(data);
-	}
-	
-        /**
-         * @return The extension data read from the extended information and 
-         * formatted as in a Properties file.
-         */
-	public String getExtensionData() {
-		final String result;
-		if (extendedInformation == null) {
-			result = "";
-		} else {
-			@SuppressWarnings("rawtypes")
-            Map data = (Map) extendedInformation.getData();
-			Properties properties = new Properties();
-			
-			for (Object o : data.keySet()) {
-				if (o instanceof String) {
-	                String key = (String) o;
-	                if (key.startsWith(ExtendedInformation.EXTENSIONDATA)) {
-	                    String subKey = key.substring(ExtendedInformation.EXTENSIONDATA.length());
-	                    properties.put(subKey, data.get(key));
-	                }
-				}
-				
-			}
-			
-			// Render the properties and remove the first line created by the Properties class.
-			StringWriter out = new StringWriter();
-                        try {
-                            properties.store(out, null);
-                        } catch (IOException ex) {
-                            // Should not happen as we are using a StringWriter
-                            throw new RuntimeException(ex);
-                        }
-			
-			StringBuffer buff = out.getBuffer();
-			String lineSeparator = System.getProperty("line.separator");
-			int firstLineSeparator = buff.indexOf(lineSeparator);
-			
-			result = firstLineSeparator >= 0 ? buff.substring(firstLineSeparator + lineSeparator.length()) : buff.toString();
-		}
-		return result;
-	}
-        
+            // Render the properties and remove the first line created by the Properties class.
+            StringWriter out = new StringWriter();
+            try {
+                properties.store(out, null);
+            } catch (IOException ex) {
+                // Should not happen as we are using a StringWriter
+                throw new RuntimeException(ex);
+            }
+
+            StringBuffer buff = out.getBuffer();
+            String lineSeparator = System.getProperty("line.separator");
+            int firstLineSeparator = buff.indexOf(lineSeparator);
+
+            result = firstLineSeparator >= 0 ? buff.substring(firstLineSeparator + lineSeparator.length()) : buff.toString();
+        }
+        return result;
+    }
+
     /**
      * 
      * @return A Map view of the extension data.
@@ -141,6 +141,5 @@ public class EditEndEntityBean {
         }
         return result;
     }
-	
-	
+
 }
