00ecc57ce109f534a1e67e9f07221f915328fda6
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2013-08-05 13:28:13 +0000

    ECA-1947: Remove unused import
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@17351 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/cesecore/util/LdapNameStyle.java
+++ src/java/org/cesecore/util/LdapNameStyle.java
@@ -16,7 +16,6 @@ package org.cesecore.util;
 import java.util.Hashtable;
 
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
-import org.bouncycastle.asn1.x500.RDN;
 import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.asn1.x500.X500NameStyle;
 import org.bouncycastle.asn1.x500.style.BCStyle;
