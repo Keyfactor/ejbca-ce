e83c87386e445a9fc638b40fe6d360938667822a
Mike Agrenius Kushner <mike.kushner@primekey.com>
2013-08-29 18:30:54 +0000

    ECA-3116: CryptoTokens can be dumped and imported
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@17540 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ src/java/org/cesecore/keys/token/CryptoTokenClassNotFoundException.java
@@ -0,0 +1,47 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.cesecore.keys.token;
+
+/**
+ * Thrown when trying to instantiate an unknown crypto token class.
+ * 
+ * @version $Id$
+ *
+ */
+public class CryptoTokenClassNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = -7935523503522491237L;
+    
+    public CryptoTokenClassNotFoundException() {
+        super();
+    }
+
+    public CryptoTokenClassNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public CryptoTokenClassNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CryptoTokenClassNotFoundException(String message) {
+        super(message);
+    }
+
+    public CryptoTokenClassNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+  
+
+}
