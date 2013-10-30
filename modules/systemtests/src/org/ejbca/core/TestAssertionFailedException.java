70783c5c160a45cddb2033cbd20ca16811482c6a
Mike Agrenius Kushner <mike.kushner@primekey.com>
2013-10-30 09:42:16 +0000

    NoTicket: Minor test cleanup
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@18026 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/systemtests/src/org/ejbca/core/TestAssertionFailedException.java
@@ -0,0 +1,52 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.core;
+
+/**
+ * Exception to be thrown when a test cannot run due to setup difficulties. 
+ * 
+ * @version $Id$
+ *
+ */
+public class TestAssertionFailedException extends RuntimeException {
+    private static final long serialVersionUID = -8713408428524985390L;
+
+    /**
+     * 
+     */
+    public TestAssertionFailedException() {
+    }
+
+    /**
+     * @param message
+     */
+    public TestAssertionFailedException(String message) {
+        super(message);
+    }
+
+    /**
+     * @param cause
+     */
+    public TestAssertionFailedException(Throwable cause) {
+        super(cause);
+    }
+
+    /**
+     * @param message
+     * @param cause
+     */
+    public TestAssertionFailedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
