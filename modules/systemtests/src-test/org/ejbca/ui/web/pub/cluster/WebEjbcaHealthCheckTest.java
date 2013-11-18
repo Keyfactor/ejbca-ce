feb36e9573ef85e1fad0602c3f7b473408c994bf
Mike Agrenius Kushner <mike.kushner@primekey.com>
2013-11-18 17:57:02 +0000

    ECA-3308: Issue fixed. Does not explicitly test for internal key bindings, but they exist in the test environment and any issue with them will log problems.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@18178 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/systemtests/src-test/org/ejbca/ui/web/pub/cluster/WebEjbcaHealthCheckTest.java
+++ modules/systemtests/src-test/org/ejbca/ui/web/pub/cluster/WebEjbcaHealthCheckTest.java
@@ -16,12 +16,32 @@ package org.ejbca.ui.web.pub.cluster;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.security.cert.X509Certificate;
+import java.util.Date;
+
 import org.apache.log4j.Logger;
+import org.cesecore.authentication.tokens.AuthenticationToken;
+import org.cesecore.certificates.ca.X509CA;
+import org.cesecore.certificates.certificate.CertificateConstants;
+import org.cesecore.certificates.certificate.CertificateStoreSessionRemote;
+import org.cesecore.certificates.certificate.InternalCertificateStoreSessionRemote;
+import org.cesecore.certificates.certificateprofile.CertificateProfileConstants;
+import org.cesecore.certificates.ocsp.OcspResponseGeneratorSessionRemote;
+import org.cesecore.certificates.util.AlgorithmConstants;
+import org.cesecore.keybind.InternalKeyBindingMgmtSessionRemote;
+import org.cesecore.keybind.InternalKeyBindingStatus;
+import org.cesecore.keybind.impl.OcspKeyBinding;
+import org.cesecore.keys.token.CryptoTokenManagementSessionRemote;
+import org.cesecore.keys.token.CryptoTokenTestUtils;
+import org.cesecore.mock.authentication.tokens.TestAlwaysAllowLocalAuthenticationToken;
 import org.cesecore.util.EjbRemoteHelper;
 import org.ejbca.config.WebConfiguration;
 import org.ejbca.core.ejb.config.ConfigurationSessionRemote;
+import org.ejbca.core.protocol.ocsp.OcspTestUtils;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.gargoylesoftware.htmlunit.WebClient;
@@ -33,10 +53,65 @@ import com.gargoylesoftware.htmlunit.WebResponse;
  */
 public class WebEjbcaHealthCheckTest extends WebHealthTestAbstract {
 
-	private static final Logger log = Logger.getLogger(WebEjbcaHealthCheckTest.class);
+    private static final Logger log = Logger.getLogger(WebEjbcaHealthCheckTest.class);
 
-    private ConfigurationSessionRemote configurationSession = EjbRemoteHelper.INSTANCE.getRemoteSession(ConfigurationSessionRemote.class, EjbRemoteHelper.MODULE_TEST);
+    private static final String CA_DN = "CN=WebEjbcaHealthCheckTestCA";
+    private static final String SIGNER_DN = "CN=WebEjbcaHealthCheckTestOcspSigner";
 
+    private ConfigurationSessionRemote configurationSession = EjbRemoteHelper.INSTANCE.getRemoteSession(ConfigurationSessionRemote.class,
+            EjbRemoteHelper.MODULE_TEST);
+
+    private static final String TESTCLASSNAME = WebEjbcaHealthCheckTest.class.getSimpleName();
+    private static final AuthenticationToken authenticationToken = new TestAlwaysAllowLocalAuthenticationToken(
+            WebEjbcaHealthCheckTest.class.getSimpleName());
+    private static int cryptoTokenId;
+    private static int internalKeyBindingId;
+    private static X509Certificate ocspSigningCertificate;
+    private static X509CA x509ca;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        x509ca = CryptoTokenTestUtils.createTestCA(authenticationToken, CA_DN);
+        cryptoTokenId = CryptoTokenTestUtils.createCryptoToken(authenticationToken, TESTCLASSNAME);
+        internalKeyBindingId = OcspTestUtils.createInternalKeyBinding(authenticationToken, cryptoTokenId,
+                OcspKeyBinding.IMPLEMENTATION_ALIAS, TESTCLASSNAME, "RSA2048", AlgorithmConstants.SIGALG_SHA1_WITH_RSA);  
+        ocspSigningCertificate = OcspTestUtils.createOcspSigningCertificate(authenticationToken, OcspTestUtils.OCSP_END_USER_NAME, SIGNER_DN, internalKeyBindingId, x509ca.getCAId());
+        OcspTestUtils.updateInternalKeyBindingCertificate(authenticationToken, internalKeyBindingId);
+        OcspTestUtils.setInternalKeyBindingStatus(authenticationToken, internalKeyBindingId, InternalKeyBindingStatus.ACTIVE);
+        final CertificateStoreSessionRemote certificateStoreSession = EjbRemoteHelper.INSTANCE
+                .getRemoteSession(CertificateStoreSessionRemote.class);
+        //Store the CA Certificate.
+        certificateStoreSession.storeCertificate(authenticationToken, x509ca.getCACertificate(), "foo", "1234", CertificateConstants.CERT_ACTIVE,
+                CertificateConstants.CERTTYPE_ROOTCA, CertificateProfileConstants.CERTPROFILE_FIXED_ROOTCA, "footag", new Date().getTime());
+        OcspResponseGeneratorSessionRemote ocspResponseGeneratorSession = EjbRemoteHelper.INSTANCE
+                .getRemoteSession(OcspResponseGeneratorSessionRemote.class);
+        ocspResponseGeneratorSession.reloadOcspSigningCache();
+    }
+    
+    @AfterClass
+    public static void afterClass() throws Exception {
+        InternalCertificateStoreSessionRemote internalCertificateStoreSession = EjbRemoteHelper.INSTANCE
+                .getRemoteSession(InternalCertificateStoreSessionRemote.class, EjbRemoteHelper.MODULE_TEST);       
+        try {
+            internalCertificateStoreSession.removeCertificate(ocspSigningCertificate);
+        } catch (Exception e) {
+            //Ignore any failures.
+        }       
+        try {
+            internalCertificateStoreSession.removeCertificate(x509ca.getCACertificate());
+        } catch (Exception e) {
+            //Ignore any failures.
+        }
+        InternalKeyBindingMgmtSessionRemote internalKeyBindingMgmtSession = EjbRemoteHelper.INSTANCE
+                .getRemoteSession(InternalKeyBindingMgmtSessionRemote.class);
+        CryptoTokenManagementSessionRemote cryptoTokenManagementSession = EjbRemoteHelper.INSTANCE
+                .getRemoteSession(CryptoTokenManagementSessionRemote.class);
+        AuthenticationToken authenticationToken = new TestAlwaysAllowLocalAuthenticationToken(TESTCLASSNAME);
+        internalKeyBindingMgmtSession.deleteInternalKeyBinding(authenticationToken, internalKeyBindingId);
+        cryptoTokenManagementSession.deleteCryptoToken(authenticationToken, cryptoTokenId);
+        OcspTestUtils.deleteCa(authenticationToken, x509ca);
+    }
+    
     @Before
     public void setUp() throws Exception {
         httpPort = configurationSession.getProperty(WebConfiguration.CONFIG_HTTPSERVERPUBHTTP);
