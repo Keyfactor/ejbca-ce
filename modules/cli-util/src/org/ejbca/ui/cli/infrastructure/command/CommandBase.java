edfb4b43b1dd226328ece996faadfdf7537dac34
Mike Agrenius Kushner <mike.kushner@primekey.com>
2013-12-23 16:44:38 +0000

    ECA-2690: A bit more complexity and some refactoring
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@18336 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cli-util/src/org/ejbca/ui/cli/infrastructure/command/CommandBase.java
+++ modules/cli-util/src/org/ejbca/ui/cli/infrastructure/command/CommandBase.java
@@ -15,25 +15,13 @@ package org.ejbca.ui.cli.infrastructure.command;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.security.Principal;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-import org.cesecore.authentication.tokens.AuthenticationSubject;
-import org.cesecore.authentication.tokens.AuthenticationToken;
-import org.cesecore.authentication.tokens.UsernamePrincipal;
-import org.cesecore.util.EjbRemoteHelper;
-import org.ejbca.config.EjbcaConfiguration;
-import org.ejbca.core.ejb.authentication.cli.CliAuthenticationProviderSessionRemote;
-import org.ejbca.core.ejb.authentication.cli.CliAuthenticationToken;
-import org.ejbca.core.ejb.authentication.cli.exception.CliAuthenticationFailedException;
 import org.ejbca.ui.cli.infrastructure.io.ValueFoundResponse;
 import org.ejbca.ui.cli.infrastructure.parameter.Parameter;
 import org.ejbca.ui.cli.infrastructure.parameter.ParameterHandler;
-import org.ejbca.ui.cli.infrastructure.parameter.enums.MandatoryMode;
-import org.ejbca.ui.cli.infrastructure.parameter.enums.ParameterMode;
-import org.ejbca.ui.cli.infrastructure.parameter.enums.StandaloneMode;
 
 /**
  * Base class for all commands
@@ -42,81 +30,8 @@ import org.ejbca.ui.cli.infrastructure.parameter.enums.StandaloneMode;
  *
  */
 public abstract class CommandBase implements CliCommandPlugin {
-
-    public static final String USERNAME_KEY = "-u";
-    public static final String PASSWORD_KEY = "-p";
-
-    private String password = null;
-    private String username = null;
     
-    private final ParameterHandler parameterHandler = new ParameterHandler(getMainCommand());
-
-    {
-        registerDefaultParameters();
-    }
-    
-    /**
-     * Extracts -u and -p parameters, similar to BaseCommand.parseUsernameAndPasswordFromArgs,
-     * but using a Map<String,String> instead of String[] for the arguments.
-     */
-    public void handleUserPasswordParams(Map<String, String> parameters) {
-        final String usernameFromArgs = parameters.get(USERNAME_KEY);
-        final String usernameFromConfig = EjbcaConfiguration.getCliDefaultUser();
-
-        if (usernameFromArgs != null) {
-            username = usernameFromArgs;
-        } else if (usernameFromConfig != null) {
-            username = usernameFromConfig;
-        } else {
-            username = "ejbca";
-        }
-
-        final String passwordFromArgs = parameters.get(PASSWORD_KEY);
-        final String passwordFromConfig = EjbcaConfiguration.getCliDefaultPassword();
-
-        if (passwordFromArgs != null) {
-            password = passwordFromArgs;
-        } else if (passwordFromConfig != null) {
-            password = passwordFromConfig;
-        } else {
-            password = "ejbca";
-        }
-    }
-
-    /**
-     * This utility method gets an authenticated CliAuthenticationToken from the
-     * authentication service.
-     * 
-     * Worth noting in this method is that the password is not sent as a
-     * credential, because this would imply sending it (or its hash) in
-     * cleartext over the network. Instead, it's only sent as part of a SHA1
-     * hash (as part of the CliAuthenticationToken specification). Actual check
-     * of the password's validity will formally occur at the first time that the
-     * authentication token is checked for authorization.
-     * 
-     * Note also that the CliAuthenticationToken may only be used for a single
-     * call via remote. I.e once it's passed through the network once, it's
-     * invalid for further use.
-     * 
-     * 
-     * @return a single use CliAuthenticationToken.
-     */
-    protected AuthenticationToken getAuthenticationToken() {
-        Set<Principal> principals = new HashSet<Principal>();
-        principals.add(new UsernamePrincipal(username));
-
-        AuthenticationSubject subject = new AuthenticationSubject(principals, null);
-
-        CliAuthenticationToken authenticationToken = (CliAuthenticationToken) EjbRemoteHelper.INSTANCE.getRemoteSession(
-                CliAuthenticationProviderSessionRemote.class).authenticate(subject);
-        // Set hashed value anew in order to send back
-        if (authenticationToken == null) {
-            throw new CliAuthenticationFailedException("Authentication failed. Username or password were not correct.");
-        } else {
-            authenticationToken.setSha1HashFromCleartextPassword(password);
-            return authenticationToken;
-        }
-    }
+    protected final ParameterHandler parameterHandler = new ParameterHandler(getMainCommand());
 
     protected static ValueFoundResponse getValueFoundResponse(ValueFoundResponse defaultResponse) {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
@@ -141,6 +56,16 @@ public abstract class CommandBase implements CliCommandPlugin {
         }
     }
 
+    @Override
+    public void execute(String... arguments) throws IOException {
+        Map<String, String> parameters = parameterHandler.parseParameters(this, arguments);
+        if(parameters == null) {
+            //Parameters couldn't be parsed, but this should already be handled. 
+            return;
+        }
+        execute(parameters);
+    }
+    
     // Return an empty set for commands without aliases
     @Override
     public Set<String> getMainCommandAliases() {
@@ -159,43 +84,17 @@ public abstract class CommandBase implements CliCommandPlugin {
         return new HashSet<String[]>();
     }
     
-    @Override
-    public  final void execute(String... arguments) throws IOException {
-        Map<String, String> parameters = parameterHandler.parseParameters(this, arguments);
-        if(parameters == null) {
-            //Parameters couldn't be parsed, but this should already be handled. 
-            return;
-        }
-        handleUserPasswordParams(parameters);
-        execute(parameters);
-    }
-    
     /**
      * Execute commands on a local level, sans boilerplate code
      * 
-     * @param parameters a map of the parameters. 
-     * @throws IOException  
+     * @param parameters a map of the parameters.  
      */
-    protected abstract void execute(Map<String, String> parameters) throws IOException;
+    protected abstract void execute(Map<String, String> parameters);
     
     protected void registerParameter(Parameter parameter) {
         parameterHandler.registerParameter(parameter);
     }
-    
-    /**
-     * Register parameters general for all commands.
-     * 
-     * @param parameterHandler the parameter handler to register the arguments with
-     */
-    private void registerDefaultParameters() {
-        final String usernameInstruction = "A user name, if required.";
-        final String passwordInstruction = "A password, if required.";
-        this.registerParameter(new Parameter(CommandBase.USERNAME_KEY, "Username", MandatoryMode.OPTIONAL, StandaloneMode.FORBID, ParameterMode.ARGUMENT,
-                usernameInstruction));
-        this.registerParameter(new Parameter(CommandBase.PASSWORD_KEY, "Password", MandatoryMode.OPTIONAL, StandaloneMode.FORBID, ParameterMode.PASSWORD,
-                passwordInstruction));
-    }
-
+   
     public abstract String getFullHelpText();
     
     public abstract String getImplementationName();
