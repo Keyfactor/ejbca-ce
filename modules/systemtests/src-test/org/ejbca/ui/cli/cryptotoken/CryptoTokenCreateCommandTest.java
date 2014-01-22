347ec95e18215ff5fde24bdc8b9063ed0532dcb8
Mike Agrenius Kushner <mike.kushner@primekey.com>
2014-01-22 14:48:38 +0000

    ECA-3377: Some more tests added to the mix
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@18395 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/systemtests/src-test/org/ejbca/ui/cli/cryptotoken/CryptoTokenCreateCommandTest.java
@@ -0,0 +1,65 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.ui.cli.cryptotoken;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.cesecore.authentication.tokens.AuthenticationToken;
+import org.cesecore.authorization.AuthorizationDeniedException;
+import org.cesecore.keys.token.CryptoTokenManagementSessionRemote;
+import org.cesecore.keys.token.SoftCryptoToken;
+import org.cesecore.mock.authentication.tokens.TestAlwaysAllowLocalAuthenticationToken;
+import org.cesecore.util.CryptoProviderTools;
+import org.cesecore.util.EjbRemoteHelper;
+import org.ejbca.ui.cli.ErrorAdminCommandException;
+import org.ejbca.ui.cli.ca.CaActivateCACommandTest;
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @version $Id$
+ *
+ */
+public class CryptoTokenCreateCommandTest {
+
+    private static final String CRYPTOTOKEN_NAME = CryptoTokenCreateCommandTest.class.getSimpleName();
+
+    private final CryptoTokenCreateCommand command = new CryptoTokenCreateCommand();
+
+    private final AuthenticationToken authenticationToken = new TestAlwaysAllowLocalAuthenticationToken(CaActivateCACommandTest.class.getSimpleName());
+
+    private CryptoTokenManagementSessionRemote cryptoTokenManagementSession = EjbRemoteHelper.INSTANCE
+            .getRemoteSession(CryptoTokenManagementSessionRemote.class);
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        CryptoProviderTools.installBCProviderIfNotAvailable();
+    }
+
+    @After
+    public void teardown() throws AuthorizationDeniedException {
+        Integer cryptoTokenId = cryptoTokenManagementSession.getIdFromName(CRYPTOTOKEN_NAME);
+        if (cryptoTokenId != null) {
+            cryptoTokenManagementSession.deleteCryptoToken(authenticationToken, cryptoTokenId);
+        }
+    }
+
+    @Test
+    public void testCommand() throws ErrorAdminCommandException {
+        String[] args = new String[] { "create", CRYPTOTOKEN_NAME, "foo123", "true", SoftCryptoToken.class.getSimpleName(), "true" };
+        command.execute(args);
+        assertNotNull("No crypto token was created.", cryptoTokenManagementSession.getIdFromName(CRYPTOTOKEN_NAME));
+    }
+
+}
