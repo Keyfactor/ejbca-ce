3bf00dc73314b3a1662e64a323d5ed29f2bfd6f2
Mike Agrenius Kushner <mike.kushner@primekey.com>
2014-02-05 10:45:34 +0000

    ECA-3377: Some RA tests (and misc cleanup)
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@18439 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/systemtests/src-test/org/ejbca/ui/cli/ra/RevokeCertificateCommandTest.java
@@ -0,0 +1,132 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.ui.cli.ra;
+
+import static org.junit.Assert.assertTrue;
+
+import java.security.KeyPair;
+import java.security.cert.Certificate;
+import java.util.Date;
+
+import org.cesecore.authentication.tokens.AuthenticationToken;
+import org.cesecore.authentication.tokens.UsernamePrincipal;
+import org.cesecore.certificates.ca.CaSessionRemote;
+import org.cesecore.certificates.ca.X509CA;
+import org.cesecore.certificates.certificate.CertificateCreateSessionRemote;
+import org.cesecore.certificates.certificate.CertificateStoreSessionRemote;
+import org.cesecore.certificates.certificate.InternalCertificateStoreSessionRemote;
+import org.cesecore.certificates.certificate.request.SimpleRequestMessage;
+import org.cesecore.certificates.certificate.request.X509ResponseMessage;
+import org.cesecore.certificates.certificateprofile.CertificateProfileConstants;
+import org.cesecore.certificates.endentity.EndEntityConstants;
+import org.cesecore.certificates.endentity.EndEntityInformation;
+import org.cesecore.certificates.endentity.EndEntityType;
+import org.cesecore.certificates.endentity.EndEntityTypes;
+import org.cesecore.certificates.util.AlgorithmConstants;
+import org.cesecore.keys.token.CryptoTokenManagementSessionRemote;
+import org.cesecore.keys.token.CryptoTokenTestUtils;
+import org.cesecore.keys.util.KeyTools;
+import org.cesecore.mock.authentication.tokens.TestAlwaysAllowLocalAuthenticationToken;
+import org.cesecore.util.CertTools;
+import org.cesecore.util.CryptoProviderTools;
+import org.cesecore.util.EjbRemoteHelper;
+import org.ejbca.core.ejb.ra.EndEntityAccessSessionRemote;
+import org.ejbca.core.ejb.ra.EndEntityManagementSessionRemote;
+import org.ejbca.core.model.SecConst;
+import org.ejbca.ui.cli.ErrorAdminCommandException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @version $Id$
+ *
+ */
+public class RevokeCertificateCommandTest {
+
+    private static final String TESTCLASS_NAME = KeyRecoveryNewestCommandTest.class.getSimpleName();
+    private static final String END_ENTITY_SUBJECT_DN = "C=SE, O=PrimeKey, CN=" + TESTCLASS_NAME;
+
+    private RevokeCertificateCommand command = new RevokeCertificateCommand();
+
+    private static final CaSessionRemote caSession = EjbRemoteHelper.INSTANCE.getRemoteSession(CaSessionRemote.class);
+    private final CertificateCreateSessionRemote certificateCreateSession = EjbRemoteHelper.INSTANCE
+            .getRemoteSession(CertificateCreateSessionRemote.class);
+    private final CertificateStoreSessionRemote certificateStoreSession = EjbRemoteHelper.INSTANCE
+            .getRemoteSession(CertificateStoreSessionRemote.class);
+    private static final CryptoTokenManagementSessionRemote cryptoTokenManagementSession = EjbRemoteHelper.INSTANCE
+            .getRemoteSession(CryptoTokenManagementSessionRemote.class);
+    private final EndEntityAccessSessionRemote endEntityAccessSession = EjbRemoteHelper.INSTANCE.getRemoteSession(EndEntityAccessSessionRemote.class);
+    private final EndEntityManagementSessionRemote endEntityManagementSession = EjbRemoteHelper.INSTANCE
+            .getRemoteSession(EndEntityManagementSessionRemote.class);
+
+    private final InternalCertificateStoreSessionRemote internalCertificateStoreSession = EjbRemoteHelper.INSTANCE.getRemoteSession(
+            InternalCertificateStoreSessionRemote.class, EjbRemoteHelper.MODULE_TEST);
+
+    private static final AuthenticationToken authenticationToken = new TestAlwaysAllowLocalAuthenticationToken(new UsernamePrincipal(TESTCLASS_NAME));
+
+    private static X509CA x509ca = null;
+
+    private String serialNumber;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        CryptoProviderTools.installBCProvider();
+        x509ca = CryptoTokenTestUtils.createTestCA(authenticationToken, "C=SE,CN=" + TESTCLASS_NAME);
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        if (x509ca != null) {
+            final int caCryptoTokenId = caSession.getCAInfo(authenticationToken, x509ca.getCAId()).getCAToken().getCryptoTokenId();
+            cryptoTokenManagementSession.deleteCryptoToken(authenticationToken, caCryptoTokenId);
+            caSession.removeCA(authenticationToken, x509ca.getCAId());
+        }
+    }
+
+    @Before
+    public void setup() throws Exception {
+        final EndEntityInformation userdata = new EndEntityInformation(TESTCLASS_NAME, END_ENTITY_SUBJECT_DN, x509ca.getCAId(), null, null,
+                EndEntityConstants.STATUS_NEW, new EndEntityType(EndEntityTypes.ENDUSER), SecConst.EMPTY_ENDENTITYPROFILE,
+                CertificateProfileConstants.CERTPROFILE_FIXED_ENDUSER, new Date(), new Date(), SecConst.TOKEN_SOFT_P12, 0, null);
+        userdata.setPassword("foo123");
+        endEntityManagementSession.addUser(authenticationToken, userdata, false);
+        KeyPair keys = KeyTools.genKeys("1024", AlgorithmConstants.KEYALGORITHM_RSA);
+        SimpleRequestMessage req = new SimpleRequestMessage(keys.getPublic(), userdata.getUsername(), userdata.getPassword());
+        serialNumber = CertTools.getSerialNumberAsString(certificateCreateSession.createCertificate(authenticationToken, userdata, req,
+                X509ResponseMessage.class).getCertificate());
+        if (null == endEntityAccessSession.findUser(authenticationToken, TESTCLASS_NAME)) {
+            throw new RuntimeException("Could not create end entity.");
+        }
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        if (null != endEntityAccessSession.findUser(authenticationToken, TESTCLASS_NAME)) {
+            endEntityManagementSession.deleteUser(authenticationToken, TESTCLASS_NAME);
+        }
+        for (Certificate certificate : certificateStoreSession.findCertificatesByUsername(TESTCLASS_NAME)) {
+            internalCertificateStoreSession.removeCertificate(certificate);
+        }
+    }
+
+    @Test
+    public void testRevokeCertificate() throws ErrorAdminCommandException {
+        final String args[] = new String[] { "revokecert", x509ca.getSubjectDN(), serialNumber, "0" };
+        command.execute(args);
+        assertTrue("Certificate was not revoked.",
+                certificateStoreSession.isRevoked(x509ca.getSubjectDN(), CertTools.getSerialNumberFromString(serialNumber)));
+    }
+}
