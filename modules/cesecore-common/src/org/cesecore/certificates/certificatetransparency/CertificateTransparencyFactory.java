62ba9e6ec93616bbd203441b789c445182abb2cc
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2014-02-11 08:33:16 +0000

    ECA-3355: Use enum singleton pattern in CertificateTransparencyFactory
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@18484 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-common/src/org/cesecore/certificates/certificatetransparency/CertificateTransparencyFactory.java
+++ modules/cesecore-common/src/org/cesecore/certificates/certificatetransparency/CertificateTransparencyFactory.java
@@ -17,42 +17,33 @@ package org.cesecore.certificates.certificatetransparency;
  * 
  * @version $Id$
  */
-public final class CertificateTransparencyFactory {
+public enum CertificateTransparencyFactory {
+
+    SINGLETON;
 
     private static final String IMPLEMENTATION_CLASSNAME = "org.cesecore.certificates.certificatetransparency.CertificateTransparencyImpl";
+
+
+    private final CertificateTransparency ct;
     
-    private static CertificateTransparency ct;
-    private static boolean triedToLoad;
-    
-    static {
-        // getInstance might have been called by static code before we get here 
-        if (ct == null) {
-            loadImplClass();
-        }
-    }
-    
-    // This class can't be instantiated
-    private CertificateTransparencyFactory() { }
-    
-    private static void loadImplClass() {
+    private CertificateTransparencyFactory() {
+        CertificateTransparency instance;
         try {
             // No lock is used since the CertificateTransparencyImpl class has no state
             // so it's OK with multiple instances.
-            ct = (CertificateTransparency)Class.forName(IMPLEMENTATION_CLASSNAME).newInstance();
-        } catch (Exception e) { // NOPMD must catch everything since it can be called from static-blocks and initializers
+            instance = (CertificateTransparency)Class.forName(IMPLEMENTATION_CLASSNAME).newInstance();
+        } catch (Exception e) { // NOPMD not a good idea to throw an exception in a enum constructor
+            instance = null;
         }
-        triedToLoad = true;
+        ct = instance;
     }
-
+    
     /**
      * Returns the implementation of the CertificateTransparency interface, or null if not available.
      * No exceptions can ever be thrown by this method, so it's safe to call from static-blocks and initializers.
      */
     public static CertificateTransparency getInstance() {
-        if (!triedToLoad) {
-            loadImplClass();
-        }
-        return ct;
+        return SINGLETON.ct;
     }
     
     /**
