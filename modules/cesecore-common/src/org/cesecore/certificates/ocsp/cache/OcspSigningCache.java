ee269506ec0fbb61ed2b4c808790e91f43e6c2e0
Mike Agrenius Kushner <mike.kushner@primekey.com>
2014-03-21 14:15:47 +0000

    NoTicket: Just adding a couple of log rows
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@18648 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-common/src/org/cesecore/certificates/ocsp/cache/OcspSigningCache.java
+++ modules/cesecore-common/src/org/cesecore/certificates/ocsp/cache/OcspSigningCache.java
@@ -36,9 +36,9 @@ import org.cesecore.util.CertTools;
  */
 public enum OcspSigningCache {
     INSTANCE;
-    
-    private Map<Integer,OcspSigningCacheEntry> cache = new HashMap<Integer, OcspSigningCacheEntry>();
-    private Map<Integer,OcspSigningCacheEntry> staging = new HashMap<Integer, OcspSigningCacheEntry>();
+
+    private Map<Integer, OcspSigningCacheEntry> cache = new HashMap<Integer, OcspSigningCacheEntry>();
+    private Map<Integer, OcspSigningCacheEntry> staging = new HashMap<Integer, OcspSigningCacheEntry>();
     private OcspSigningCacheEntry defaultResponderCacheEntry = null;
     private final ReentrantLock lock = new ReentrantLock(false);
     private final static Logger log = Logger.getLogger(OcspSigningCache.class);
@@ -46,18 +46,16 @@ public enum OcspSigningCache {
     public OcspSigningCacheEntry getEntry(final CertificateID certID) {
         return cache.get(getCacheIdFromCertificateID(certID));
     }
-    
+
     public OcspSigningCacheEntry getDefaultEntry() {
         return defaultResponderCacheEntry;
     }
 
-    
-    
     /** WARNING: This method potentially exports references to CAs private keys! */
     public Collection<OcspSigningCacheEntry> getEntries() {
         return cache.values();
     }
-    
+
     public void stagingStart() {
         lock.lock();
         staging = new HashMap<Integer, OcspSigningCacheEntry>();
@@ -70,35 +68,46 @@ public enum OcspSigningCache {
     public void stagingCommit() {
         OcspSigningCacheEntry defaultResponderCacheEntry = null;
         for (final OcspSigningCacheEntry entry : staging.values()) {
-            
+
             if (entry.getOcspSigningCertificate() == null) {
                 final X509Certificate signingCertificate = entry.getCaCertificateChain().get(0);
-                if(CertTools.getSubjectDN(signingCertificate).equals(OcspConfiguration.getDefaultResponderId())) {
+                if (CertTools.getSubjectDN(signingCertificate).equals(OcspConfiguration.getDefaultResponderId())) {
                     defaultResponderCacheEntry = entry;
+                    if (log.isDebugEnabled()) {
+                        log.info("Setting keybinding with ID" + entry.getOcspKeyBinding().getId() + " and DN "
+                                + OcspConfiguration.getDefaultResponderId() + " as default responder.");
+                    }
                     break;
                 }
             } else {
                 final X509Certificate signingCertificate = entry.getOcspSigningCertificate();
-                if(CertTools.getIssuerDN(signingCertificate).equals(OcspConfiguration.getDefaultResponderId())) {
+                if (CertTools.getIssuerDN(signingCertificate).equals(OcspConfiguration.getDefaultResponderId())) {
                     defaultResponderCacheEntry = entry;
+                    if (log.isDebugEnabled()) {
+                        log.info("Setting keybinding with ID" + entry.getOcspKeyBinding().getId() + " and DN "
+                                + OcspConfiguration.getDefaultResponderId() + " as default responder.");
+                    }
                     break;
                 }
             }
         }
         if (defaultResponderCacheEntry == null) {
-            log.info("Default OCSP responder with subject '" + OcspConfiguration.getDefaultResponderId() + "' was not found."+
-                    " OCSP requests for certificates issued by unknown CAs will fail with response code 2 (internal error).");
+            log.info("Default OCSP responder with subject '" + OcspConfiguration.getDefaultResponderId() + "' was not found."
+                    + " OCSP requests for certificates issued by unknown CAs will fail with response code 2 (internal error).");
             if (staging.values().size() > 0) {
                 // We could pick a responder at chance here, but it may be a feature to the user to not waste HSM signatures on Unknown responses..
-                log.info("No default OCSP responder has been configured. OCSP requests for certificates issued by unknown CAs "+
-                        "will fail with response code 2 (internal error).");
+                log.info("No default OCSP responder has been configured. OCSP requests for certificates issued by unknown CAs "
+                        + "will fail with response code 2 (internal error).");
             }
         }
         if (log.isDebugEnabled()) {
             log.debug("Committing the following to OCSP cache:");
             for (final Integer key : staging.keySet()) {
                 final OcspSigningCacheEntry entry = staging.get(key);
-                log.debug(" KeyBindingId: " + key + ", SubjectDN '" + CertTools.getSubjectDN(entry.getFullCertificateChain().get(0))+"', IssuerDN '"+CertTools.getIssuerDN(entry.getFullCertificateChain().get(0))+"', SerialNumber "+entry.getFullCertificateChain().get(0).getSerialNumber().toString()+"/"+entry.getFullCertificateChain().get(0).getSerialNumber().toString(16));
+                log.debug(" KeyBindingId: " + key + ", SubjectDN '" + CertTools.getSubjectDN(entry.getFullCertificateChain().get(0))
+                        + "', IssuerDN '" + CertTools.getIssuerDN(entry.getFullCertificateChain().get(0)) + "', SerialNumber "
+                        + entry.getFullCertificateChain().get(0).getSerialNumber().toString() + "/"
+                        + entry.getFullCertificateChain().get(0).getSerialNumber().toString(16));
                 if (entry.getOcspKeyBinding() != null) {
                     log.debug("   keyPairAlias: " + entry.getOcspKeyBinding().getKeyPairAlias());
                 }
@@ -135,16 +144,17 @@ public enum OcspSigningCache {
         // Use bitwise XOR of the hashcodes for IssuerNameHash and IssuerKeyHash to produce the integer.
         int result = new BigInteger(certID.getIssuerNameHash()).hashCode() ^ new BigInteger(certID.getIssuerKeyHash()).hashCode();
         if (log.isDebugEnabled()) {
-            log.debug("Using getIssuerNameHash " + new BigInteger(certID.getIssuerNameHash()).toString(16) + " and getIssuerKeyHash " + new BigInteger(certID.getIssuerKeyHash()).toString(16) + " to produce id " + result);
+            log.debug("Using getIssuerNameHash " + new BigInteger(certID.getIssuerNameHash()).toString(16) + " and getIssuerKeyHash "
+                    + new BigInteger(certID.getIssuerKeyHash()).toString(16) + " to produce id " + result);
         }
         return result;
     }
-    
+
     /** @return the CertificateID based on the provided certificate */
     public static CertificateID getCertificateIDFromCertificate(final X509Certificate certificate) {
         try {
             if (log.isTraceEnabled()) {
-                log.trace("Building CertificateId from certificate with subjectDN '"+CertTools.getSubjectDN(certificate)+"'.");
+                log.trace("Building CertificateId from certificate with subjectDN '" + CertTools.getSubjectDN(certificate) + "'.");
             }
             return new JcaCertificateID(SHA1DigestCalculator.buildSha1Instance(), certificate, certificate.getSerialNumber());
         } catch (OCSPException e) {
