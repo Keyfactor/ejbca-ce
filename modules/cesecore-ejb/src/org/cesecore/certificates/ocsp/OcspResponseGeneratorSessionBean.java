3fe5a253af6908aef60923df73ac720fbc70bf61
Mike Agrenius Kushner <mike.kushner@primekey.com>
2014-03-24 13:59:03 +0000

    ECA-3420: Removing unreachable catch block
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@18667 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-ejb/src/org/cesecore/certificates/ocsp/OcspResponseGeneratorSessionBean.java
+++ modules/cesecore-ejb/src/org/cesecore/certificates/ocsp/OcspResponseGeneratorSessionBean.java
@@ -1783,9 +1783,7 @@ public class OcspResponseGeneratorSessionBean implements OcspResponseGeneratorSe
                             log.warn(e.getMessage());
                         } catch (FileNotFoundException e) {
                             log.warn(e.getMessage());
-                        } catch (IOException e) {
-                            log.warn(e.getMessage());
-                        }
+                        } 
                     }
                 }
             }
