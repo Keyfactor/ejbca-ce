fa60094d7fa83b5d54c5935805a6286423d8c80e
Mike Agrenius Kushner <mike.kushner@primekey.com>
2014-05-05 10:56:16 +0000

    ECA-3537: Reverting mysterious test failure.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@18894 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/systemtests/src-test/org/ejbca/core/ejb/keyrecovery/KeyRecoveryTest.java
+++ modules/systemtests/src-test/org/ejbca/core/ejb/keyrecovery/KeyRecoveryTest.java
@@ -155,7 +155,7 @@ public class KeyRecoveryTest extends CaTestCase {
                 log.error("Exception generating keys/cert: ", e);
                 fail("Exception generating keys/cert");
             }
-            if(keyRecoverySession.addKeyRecoveryData(internalAdmin, cert1, user, keypair1)) {
+            if(!keyRecoverySession.addKeyRecoveryData(internalAdmin, cert1, user, keypair1)) {
                 throw new RuntimeException("Key recovery data already exists in database.");
             }
             assertTrue("Couldn't save key's in database", keyRecoverySession.existsKeys(cert1));
