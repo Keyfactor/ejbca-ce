30d471fc73255d638052ef56f61057b14c70c3c5
Mike Agrenius Kushner <mike.kushner@primekey.com>
2014-08-19 16:04:22 +0000

    ECA-3705: Set up a plugin interface, had peerconnector set up to use it as a test
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@19534 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ src/java/org/ejbca/core/ejb/authorization/AccessRulePlugin.java
@@ -0,0 +1,27 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.core.ejb.authorization;
+
+import java.util.List;
+
+
+/**
+ * Marker interface to allow access rules to be plugged in. 
+ * 
+ * @version $Id$
+ *
+ */
+public interface AccessRulePlugin {
+
+    List<String> getRules();
+}
