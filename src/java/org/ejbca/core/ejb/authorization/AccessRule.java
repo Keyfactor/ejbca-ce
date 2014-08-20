fd0da0f68a2d194b3068a0a8dc0f0bc5f465342c
Mike Agrenius Kushner <mike.kushner@primekey.com>
2014-08-20 16:30:40 +0000

    ECA-3671: Added authorization checks to the CRUD bean
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@19540 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ src/java/org/ejbca/core/ejb/authorization/AccessRule.java
@@ -0,0 +1,26 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.core.ejb.authorization;
+
+
+/**
+ * Common interface for all AccessRule sets
+ * 
+ * @version $Id$
+ *
+ */
+public interface AccessRule {
+
+    String getCategory();
+
+}
