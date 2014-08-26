a47b39e84a69c18e2c7b5d2c556795fbefbb74e1
aveen4711 <USER@primekey.com>
2014-08-26 13:47:21 +0000

    ECA-3718: Fixed the verification process when using trusted certificates in Internal Key Binding
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@19590 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-common/src/org/cesecore/keybind/impl/ClientX509TrustManager.java
+++ modules/cesecore-common/src/org/cesecore/keybind/impl/ClientX509TrustManager.java
@@ -17,7 +17,7 @@ import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
+import java.util.Iterator;
 
 import javax.net.ssl.X509TrustManager;
 
@@ -29,39 +29,55 @@ import org.cesecore.util.CertTools;
  */
 public class ClientX509TrustManager implements X509TrustManager {
 
-    private final Collection<Certificate> trustedCertificates = new ArrayList<Certificate>();
+    private final Collection<Collection<Certificate> > trustedCertificatesChains = new ArrayList< Collection<Certificate> >();
     
-    public ClientX509TrustManager(final List<X509Certificate> trustedCertificates) {
+    public ClientX509TrustManager(final Collection< Collection<Certificate> > trustedCertificates) {
         if (trustedCertificates!=null) {
-            for (X509Certificate current : trustedCertificates) {
-                this.trustedCertificates.add(current);
+            for(Collection<Certificate> col : trustedCertificates) {
+                this.trustedCertificatesChains.add(col);
             }
         }
     }
 
     @Override
     public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-        // TODO Improve
-        try {
-            CertTools.verify(chain[0], trustedCertificates);
-        } catch (Exception e) {
-            throw new CertificateException(e);
+        X509Certificate cert = chain[0];
+        if(!CertTools.verifyWithTrustedCertificates(cert, trustedCertificatesChains)) {
+            String subjectdn = CertTools.getSubjectDN(cert);
+            String issuerdn = CertTools.getIssuerDN(cert);
+            String sn = CertTools.getSerialNumberAsString(cert);
+            String errmsg = "Certificate with SubjectDN '" + subjectdn + "', IssuerDN '" + issuerdn + 
+                    "' and serialnumber '" + sn + "' is NOT trusted.";
+            throw new CertificateException(errmsg);
         }
     }
 
     @Override
     public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-        // TODO Improve
-        try {
-            CertTools.verify(chain[0], trustedCertificates);
-        } catch (Exception e) {
-            throw new CertificateException(e);
+        X509Certificate cert = chain[0];
+        if(!CertTools.verifyWithTrustedCertificates(cert, trustedCertificatesChains)) {
+            String subjectdn = CertTools.getSubjectDN(cert);
+            String issuerdn = CertTools.getIssuerDN(cert);
+            String sn = CertTools.getSerialNumberAsString(cert);
+            String errmsg = "Certificate with SubjectDN '" + subjectdn + "', IssuerDN '" + issuerdn + 
+                    "' and serialnumber '" + sn + "' is NOT trusted.";
+            throw new CertificateException(errmsg);
         }
     }
 
     @Override
     public X509Certificate[] getAcceptedIssuers() {
-        return trustedCertificates.toArray(new X509Certificate[0]);
+        ArrayList<X509Certificate> acceptedIssuers = new ArrayList<X509Certificate>();
+        for(Collection<Certificate> certChain : trustedCertificatesChains) {
+            Iterator<Certificate> itr = certChain.iterator();
+            X509Certificate cert = (X509Certificate) itr.next();
+            if(CertTools.isCA(cert)) {
+                acceptedIssuers.add(cert);
+            } else {
+                acceptedIssuers.add((X509Certificate) itr.next() );
+            }
+        }
+        return acceptedIssuers.toArray(new X509Certificate[0]);
     }
 
 }
