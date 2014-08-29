8b29390be0b852a6e1704976dacb2496bdd28428
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2014-08-29 15:25:49 +0000

    Noticket: remove uneeded wrappper objects (PMD warnings)
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@19648 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/core/model/ca/publisher/GeneralPurposeCustomPublisher.java
+++ src/java/org/ejbca/core/model/ca/publisher/GeneralPurposeCustomPublisher.java
@@ -100,16 +100,16 @@ public class GeneralPurposeCustomPublisher implements ICustomPublisher, CustomPu
     @Override
     public List<CustomPublisherProperty> getCustomUiPropertyList() {
         final List<CustomPublisherProperty> ret = new ArrayList<CustomPublisherProperty>();
-        ret.add(new CustomPublisherProperty(crlFailOnErrorCodePropertyName, CustomPublisherProperty.UI_BOOLEAN, Boolean.valueOf(crlFailOnErrorCode).toString()));
-        ret.add(new CustomPublisherProperty(crlFailOnStandardErrorPropertyName, CustomPublisherProperty.UI_BOOLEAN, Boolean.valueOf(crlFailOnStandardError).toString()));
+        ret.add(new CustomPublisherProperty(crlFailOnErrorCodePropertyName, CustomPublisherProperty.UI_BOOLEAN, String.valueOf(crlFailOnErrorCode)));
+        ret.add(new CustomPublisherProperty(crlFailOnStandardErrorPropertyName, CustomPublisherProperty.UI_BOOLEAN, String.valueOf(crlFailOnStandardError)));
         ret.add(new CustomPublisherProperty(crlExternalCommandPropertyName, CustomPublisherProperty.UI_TEXTINPUT, crlExternalCommandFileName));
-        ret.add(new CustomPublisherProperty(certFailOnErrorCodePropertyName, CustomPublisherProperty.UI_BOOLEAN, Boolean.valueOf(certFailOnErrorCode).toString()));
-        ret.add(new CustomPublisherProperty(certFailOnStandardErrorPropertyName, CustomPublisherProperty.UI_BOOLEAN, Boolean.valueOf(certFailOnStandardError).toString()));
+        ret.add(new CustomPublisherProperty(certFailOnErrorCodePropertyName, CustomPublisherProperty.UI_BOOLEAN, String.valueOf(certFailOnErrorCode)));
+        ret.add(new CustomPublisherProperty(certFailOnStandardErrorPropertyName, CustomPublisherProperty.UI_BOOLEAN, String.valueOf(certFailOnStandardError)));
         ret.add(new CustomPublisherProperty(certExternalCommandPropertyName, CustomPublisherProperty.UI_TEXTINPUT, certExternalCommandFileName));
-        ret.add(new CustomPublisherProperty(revokeFailOnErrorCodePropertyName, CustomPublisherProperty.UI_BOOLEAN, Boolean.valueOf(revokeFailOnErrorCode).toString()));
-        ret.add(new CustomPublisherProperty(revokeFailOnStandardErrorPropertyName, CustomPublisherProperty.UI_BOOLEAN, Boolean.valueOf(revokeFailOnStandardError).toString()));
+        ret.add(new CustomPublisherProperty(revokeFailOnErrorCodePropertyName, CustomPublisherProperty.UI_BOOLEAN, String.valueOf(revokeFailOnErrorCode)));
+        ret.add(new CustomPublisherProperty(revokeFailOnStandardErrorPropertyName, CustomPublisherProperty.UI_BOOLEAN, String.valueOf(revokeFailOnStandardError)));
         ret.add(new CustomPublisherProperty(revokeExternalCommandPropertyName, CustomPublisherProperty.UI_TEXTINPUT, revokeExternalCommandFileName));
-        ret.add(new CustomPublisherProperty(calclulateDeltaCrlLocallyPropertyName, CustomPublisherProperty.UI_BOOLEAN, Boolean.valueOf(calclulateDeltaCrlLocally).toString()));
+        ret.add(new CustomPublisherProperty(calclulateDeltaCrlLocallyPropertyName, CustomPublisherProperty.UI_BOOLEAN, String.valueOf(calclulateDeltaCrlLocally)));
         return ret;
     }
 
