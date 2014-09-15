4b2ee68d98cf5a270846ba49ecc666c1705f0019
aveen4711 <USER@primekey.com>
2014-09-15 08:36:58 +0000

    ECA-3763: Fixed javadocs
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@19739 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-ejb-interface/src/org/cesecore/keybind/InternalKeyBindingMgmtSessionLocal.java
+++ modules/cesecore-ejb-interface/src/org/cesecore/keybind/InternalKeyBindingMgmtSessionLocal.java
@@ -57,10 +57,11 @@ public interface InternalKeyBindingMgmtSessionLocal extends InternalKeyBindingMg
 
     /**
      * Returns a collection of the trusted certificates defined in the specified InternalKeyBinding along with their issuers' certificate chains.
+     * If only trusted CAs are specified, all certificates issued by the specified CAs will be trusted.
      * 
      * @param authenticationToken
      * @param internalKeyBinding
-     * @return a collection of the trusted certificates along with their issuers' certificate chains.
+     * @return a collection of the trusted certificates along with their issuers' certificate chains or null if no trusted certificates or CAs are specified
      * @throws CADoesntExistsException
      * @throws AuthorizationDeniedException
      */
