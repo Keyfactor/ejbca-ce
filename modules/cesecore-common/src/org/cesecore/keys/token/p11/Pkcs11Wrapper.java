a9cf05fc6a91a9da3d95a7999c8c2925ed5939b7
primelars <USER@primekey.com>
2014-09-24 23:00:39 +0000

    ECA-3590: Cache the slot list
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@19843 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-common/src/org/cesecore/keys/token/p11/Pkcs11Wrapper.java
+++ modules/cesecore-common/src/org/cesecore/keys/token/p11/Pkcs11Wrapper.java
@@ -19,6 +19,8 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import org.apache.log4j.Logger;
 
@@ -26,6 +28,10 @@ import org.apache.log4j.Logger;
  *
  * This class wraps sun.security.pkcs11.wrapper.PKCS11, so that we can access the native PKCS11 calls
  * directly.
+ * A slot list and token labels for each slot is cached so that C_GetSlotList() only has to be called once and
+ * so that C_GetTokenInfo() only has to be called once for each slot.
+ * 
+ * The {@link #getInstance(File)} method must be called before any PKCS#11 provider is created.
  *
  *  @version $Id$
  *
@@ -34,10 +40,13 @@ public class Pkcs11Wrapper {
     private static final Logger log = Logger.getLogger(Pkcs11Wrapper.class);
 
     private static volatile Map<String, Pkcs11Wrapper> instances = new HashMap<String, Pkcs11Wrapper>();
+    private static final Lock lock = new ReentrantLock();
     private final Method getSlotListMethod;
     private final Method getTokenInfoMethod;
     private final Field labelField;
     private final Object p11;
+    private final HashMap<Long, char[]> labelMap;
+    private final long slotList[];
 
     private Pkcs11Wrapper(final String fileName) {
         Class<? extends Object> p11Class;
@@ -125,6 +134,11 @@ public class Pkcs11Wrapper {
             log.error(msg, e);
             throw new IllegalStateException(msg, e);
         }
+        this.labelMap = new HashMap<Long, char[]>();
+        this.slotList = C_GetSlotList();
+        for( long id : this.slotList) {
+            this.labelMap.put(Long.valueOf(id), getTokenLabelLocal(id));
+        }
     }
 
     /**
@@ -133,27 +147,53 @@ public class Pkcs11Wrapper {
      * @return the instance.
      * @throws IllegalArgumentException
      */
-    public static synchronized Pkcs11Wrapper getInstance(final File file) throws IllegalArgumentException {
-        String canonicalFileName;
+    public static Pkcs11Wrapper getInstance(final File file) throws IllegalArgumentException {
+        final String canonicalFileName;
         try {
             canonicalFileName = file.getCanonicalPath();
         } catch (IOException e) {
             throw new IllegalArgumentException(file+" is not a valid filename.",e );
         }
-        final Pkcs11Wrapper storedP11 = instances.get(canonicalFileName);
-        if (storedP11 != null) {
-            return storedP11;
+        {
+            final Pkcs11Wrapper storedP11 = instances.get(canonicalFileName);
+            if (storedP11 != null) {
+                return storedP11;// if instance exist we don't have to wait for lock just grab it.
+            }
         }
-        final Pkcs11Wrapper newP11 = new Pkcs11Wrapper(canonicalFileName);
-        instances.put(canonicalFileName, newP11);
-        return newP11;
+        try {
+            lock.lock();// wait for lock; some other tread might be creating the instance right now.
+            final Pkcs11Wrapper storedP11 = instances.get(canonicalFileName);
+            if (storedP11 != null) {
+                return storedP11;// some other thread had already created the instance
+            }
+            // no other thread has created the instance and no other will since this thread is locking.
+            Pkcs11SlotLabel.doC_Initialize(file);// C_Initialize with multithreading args.
+            final Pkcs11Wrapper newP11 = new Pkcs11Wrapper(canonicalFileName);
+            instances.put(canonicalFileName, newP11);
+            return newP11;
+        } finally {
+            lock.unlock();// now other threads might get the instance.
+        }
     }
 
     /**
      * Get a list of p11 slot IDs to slots that has a token.
      * @return the list.
      */
-    public long[] C_GetSlotList() {
+    public long[] getSlotList() {
+        return this.slotList;
+    }
+
+    /**
+     * Get the token label of a specific slot ID.
+     * @param slotID the ID of the slot
+     * @return the token label, or null if no matching token was found.
+     */
+    public char[] getTokenLabel(long slotID) {
+        return this.labelMap.get(Long.valueOf(slotID));
+    }
+
+    private long[] C_GetSlotList() {
         try {
             return (long[]) this.getSlotListMethod.invoke(this.p11, new Object[] { Boolean.TRUE });
         } catch (IllegalAccessException e) {
@@ -173,12 +213,7 @@ public class Pkcs11Wrapper {
         }
     }
 
-    /**
-     * Get the token label of a specific slot ID.
-     * @param slotID the ID of the slot
-     * @return the token label, or null if no matching token was found.
-     */
-    public char[] getTokenLabel(long slotID)  {
+    private char[] getTokenLabelLocal(long slotID)  {
         final Object tokenInfo;
         try {
             tokenInfo = this.getTokenInfoMethod.invoke(this.p11, new Object[] { Long.valueOf(slotID) });
