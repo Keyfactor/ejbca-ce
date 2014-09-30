e4d4f6d74af7e7a75bbb3add0f2b455ff6827378
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2014-09-30 13:59:52 +0000

    EJBCAINTER-20: headers, revised
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@19898 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb/src-test/org/cesecore/dbprotection/ProtectedDataTest.java
+++ modules/ejbca-ejb/src-test/org/cesecore/dbprotection/ProtectedDataTest.java
@@ -1,8 +1,10 @@
 /*************************************************************************
  *                                                                       *
- *  EJBCA: Enterprise Certificate Authority                              *
+ *  EJBCA - Proprietary Modules: Enterprise Certificate Authority        *
  *                                                                       *
  *  Copyright (c), PrimeKey Solutions AB. All rights reserved.           *
+ *  The use of the Proprietary Modules are subject to specific           * 
+ *  commercial license terms.                                            *
  *                                                                       *
  *************************************************************************/
 package org.cesecore.dbprotection;
