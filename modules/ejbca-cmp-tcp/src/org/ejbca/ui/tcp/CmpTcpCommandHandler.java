4386885d672fae9000ff36780f99bbeeff318858
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2014-09-30 14:32:24 +0000

    EJBCAINTER-20: headers, revised
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@19902 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-cmp-tcp/src/org/ejbca/ui/tcp/CmpTcpCommandHandler.java
+++ modules/ejbca-cmp-tcp/src/org/ejbca/ui/tcp/CmpTcpCommandHandler.java
@@ -1,6 +1,6 @@
 /*************************************************************************
  *                                                                       *
- *  EJBCA: The OpenSource Certificate Authority                          *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
  *                                                                       *
  *  This software is free software; you can redistribute it and/or       *
  *  modify it under the terms of the GNU Lesser General Public           *
@@ -10,19 +10,6 @@
  *  See terms of license at gnu.org.                                     *
  *                                                                       *
  *************************************************************************/
-/*
- * This file is part of the QuickServer library 
- * Copyright (C) 2003-2005 QuickServer.org
- *
- * Use, modification, copying and distribution of this software is subject to
- * the terms and conditions of the GNU Lesser General Public License. 
- * You should have received a copy of the GNU LGP License along with this 
- * library; if not, you can download a copy from <http://www.quickserver.org/>.
- *
- * For questions, suggestions, bug-reports, enhancement-requests etc.
- * visit http://www.quickserver.org
- *
- */
 
 package org.ejbca.ui.tcp;
 
