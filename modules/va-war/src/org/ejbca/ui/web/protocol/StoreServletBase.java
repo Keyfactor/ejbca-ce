6c205c3e4272b4f259cb3aa663629d2cf3769688
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2014-09-30 14:32:24 +0000

    EJBCAINTER-20: headers, revised
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@19902 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/va-war/src/org/ejbca/ui/web/protocol/StoreServletBase.java
+++ modules/va-war/src/org/ejbca/ui/web/protocol/StoreServletBase.java
@@ -1,12 +1,12 @@
 /*************************************************************************
  *                                                                       *
- *  EJBCA: The OpenSource Certificate Authority                          *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
  *                                                                       *
  *  This software is free software; you can redistribute it and/or       *
  *  modify it under the terms of the GNU Lesser General Public           *
  *  License as published by the Free Software Foundation; either         *
  *  version 2.1 of the License, or any later version.                    *
- *                                     testCRLStore                                  *
+ *                                                                       *
  *  See terms of license at gnu.org.                                     *
  *                                                                       *
  *************************************************************************/
