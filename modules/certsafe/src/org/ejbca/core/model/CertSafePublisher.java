ae1a6c207459f34f93521b296e05e088da28ea9b
aveen4711 <USER@primekey.com>
2014-10-21 14:44:43 +0000

    ECA-3822: CertSafePublisher - Fixed verification of URL
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@20035 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/certsafe/src/org/ejbca/core/model/CertSafePublisher.java
+++ modules/certsafe/src/org/ejbca/core/model/CertSafePublisher.java
@@ -123,7 +123,7 @@ public class CertSafePublisher extends CustomPublisherContainer implements ICust
         certificateStoreSession = localHelper.getCertificateStoreSession();
 
         // Extract system properties
-        urlstr = properties.getProperty(certSafeUrlPropertyName);
+        urlstr = (properties.getProperty(certSafeUrlPropertyName)).trim();
         authKeyBindingName = properties.getProperty(certSafeAuthKeyBindingPropertyName);
         timeout = properties.containsKey(certSafeConnectionTimeOutPropertyName)? 
                         Integer.parseInt(properties.getProperty(certSafeConnectionTimeOutPropertyName)) : 
@@ -333,17 +333,26 @@ public class CertSafePublisher extends CustomPublisherContainer implements ICust
             throw new PublisherConnectionException(msg);
         }
         
+        URL uurl = null;
         try {
-            String protocol = getURL().getProtocol();
-            if(!protocol.equalsIgnoreCase("https")) {
-                String msg = "The URL must be a HTTPS address";
-                log.info(msg);
-                throw new PublisherConnectionException(msg);
-            }
-        } catch (MalformedURLException e1) {
+            uurl = getURL();
+        } catch (MalformedURLException e) {
             String msg = "Could not create a URL object from the value of " + certSafeUrlPropertyName + " property: " + urlstr;
             log.info(msg);
-            log.info(e1.getLocalizedMessage(), e1);
+            log.info(e);
+            throw new PublisherConnectionException(msg);
+        }
+        
+        String protocol = uurl.getProtocol();
+        if(!protocol.equalsIgnoreCase("https")) {
+            String msg = "The URL must be a HTTPS address";
+            log.info(msg);
+            throw new PublisherConnectionException(msg);
+        }
+        
+        if(isEmptyString(uurl.getHost())) {
+            String msg = "The URL is missing the hostname";
+            log.info(msg);
             throw new PublisherConnectionException(msg);
         }
     }
