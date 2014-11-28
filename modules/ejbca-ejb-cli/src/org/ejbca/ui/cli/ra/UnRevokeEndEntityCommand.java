861c1f2254c9f071f5f1da439379b95142fb0fb5
Mike Agrenius Kushner <mike.kushner@primekey.com>
2014-11-28 13:58:01 +0000

    ECA-2478: CLI behavior is now consistent, but general behavior should be modified in the future as well.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@20352 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb-cli/src/org/ejbca/ui/cli/ra/UnRevokeEndEntityCommand.java
+++ modules/ejbca-ejb-cli/src/org/ejbca/ui/cli/ra/UnRevokeEndEntityCommand.java
@@ -13,10 +13,10 @@
 
 package org.ejbca.ui.cli.ra;
 
+import java.math.BigInteger;
 import java.security.cert.Certificate;
-import java.security.cert.X509Certificate;
 import java.util.HashSet;
-import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import javax.ejb.FinderException;
@@ -25,7 +25,9 @@ import org.apache.log4j.Logger;
 import org.cesecore.authorization.AuthorizationDeniedException;
 import org.cesecore.certificates.certificate.CertificateStoreSessionRemote;
 import org.cesecore.certificates.crl.RevokedCertInfo;
+import org.cesecore.certificates.endentity.EndEntityConstants;
 import org.cesecore.certificates.endentity.EndEntityInformation;
+import org.cesecore.util.CertTools;
 import org.cesecore.util.EjbRemoteHelper;
 import org.ejbca.core.ejb.ra.EndEntityAccessSessionRemote;
 import org.ejbca.core.ejb.ra.EndEntityManagementSessionRemote;
@@ -79,53 +81,74 @@ public class UnRevokeEndEntityCommand extends BaseRaCommand {
 
         String username = parameters.get(USERNAME_KEY);
         EndEntityInformation data;
+        EndEntityAccessSessionRemote endEntityAccessSession = EjbRemoteHelper.INSTANCE.getRemoteSession(EndEntityAccessSessionRemote.class);
+        EndEntityManagementSessionRemote endEntityManagementSession = EjbRemoteHelper.INSTANCE
+                .getRemoteSession(EndEntityManagementSessionRemote.class);
         try {
-            data = EjbRemoteHelper.INSTANCE.getRemoteSession(EndEntityAccessSessionRemote.class).findUser(getAuthenticationToken(), username);
+            data = endEntityAccessSession.findUser(getAuthenticationToken(), username);
         } catch (AuthorizationDeniedException e1) {
             log.error("ERROR: Not authorized to revoke end entity.");
             return CommandResult.FUNCTIONAL_FAILURE;
         }
-        getLogger().info("Found user:");
-        getLogger().info("username=" + data.getUsername());
-        getLogger().info("dn=\"" + data.getDN() + "\"");
-        getLogger().info("Old status=" + data.getStatus());
-        // Revoke users certificates
+
+        if (data == null) {
+            log.error("ERROR: No such end entity: " + username);
+            return CommandResult.FUNCTIONAL_FAILURE;
+        }
+        if (data.getStatus() != EndEntityConstants.STATUS_REVOKED) {
+            log.error("ERROR: End entity '" + username + "' is not revoked.");
+            return CommandResult.FUNCTIONAL_FAILURE;
+        }
+        // Unrevoke user's certificates
         try {
             boolean foundCertificateOnHold = false;
             // Find all user certs
-            Iterator<Certificate> i = EjbRemoteHelper.INSTANCE.getRemoteSession(CertificateStoreSessionRemote.class)
-                    .findCertificatesByUsername(username).iterator();
-            while (i.hasNext()) {
-                X509Certificate cert = (X509Certificate) i.next();
-                if (EjbRemoteHelper.INSTANCE.getRemoteSession(CertificateStoreSessionRemote.class).getStatus(cert.getIssuerDN().toString(),
-                        cert.getSerialNumber()).revocationReason == RevokedCertInfo.REVOCATION_REASON_CERTIFICATEHOLD) {
-                    foundCertificateOnHold = true;
-                    try {
-                        EjbRemoteHelper.INSTANCE.getRemoteSession(EndEntityManagementSessionRemote.class).revokeCert(getAuthenticationToken(),
-                                cert.getSerialNumber(), cert.getIssuerDN().toString(), RevokedCertInfo.NOT_REVOKED);
-                    } catch (AlreadyRevokedException e) {
-                        getLogger().error("ERROR: The end entity was already reactivated while the request executed.");
-                    } catch (ApprovalException e) {
-                        getLogger().error("ERROR: Reactivation already requested.");
-                    } catch (WaitingForApprovalException e) {
-                        getLogger().info("ERROR: Reactivation request has been sent for approval.");
-                    } catch (FinderException e) {
-                        getLogger().error("ERROR: " + e.getMessage());
+            List<Certificate> certificates = EjbRemoteHelper.INSTANCE.getRemoteSession(CertificateStoreSessionRemote.class)
+                    .findCertificatesByUsername(username);
+            try {
+                if (!certificates.isEmpty()) {
+                    for (Certificate cert : certificates) {
+                        BigInteger serialNumber = CertTools.getSerialNumber(cert);
+                        String issuerDN = CertTools.getIssuerDN(cert);
+                        if (EjbRemoteHelper.INSTANCE.getRemoteSession(CertificateStoreSessionRemote.class).getStatus(issuerDN, serialNumber).revocationReason == RevokedCertInfo.REVOCATION_REASON_CERTIFICATEHOLD) {
+                            foundCertificateOnHold = true;
+                            try {
+                                endEntityManagementSession.revokeCert(getAuthenticationToken(), serialNumber, issuerDN.toString(),
+                                        RevokedCertInfo.NOT_REVOKED);
+                            } catch (AlreadyRevokedException e) {
+                                getLogger().error("ERROR: The end entity was already reactivated while the request executed.");
+                            } catch (ApprovalException e) {
+                                getLogger().error("ERROR: Reactivation already requested.");
+                            } catch (WaitingForApprovalException e) {
+                                getLogger().info("ERROR: Reactivation request has been sent for approval.");
+                            }
+                        }
                     }
+                    if (!foundCertificateOnHold) {
+                        getLogger().error("No certificates with status 'On hold' were found for this end entity.");
+                    } else {
+                        //Certificates were found and unrevoked. Set status to generates
+                        endEntityManagementSession.setUserStatus(getAuthenticationToken(), username, EndEntityConstants.STATUS_GENERATED);
+                        getLogger().info("Setting status of end entity '" + username + "' to GENERATED (40).");
+                    }
+                } else {
+                    //End entity was revoked, but had no certificates. Set it to new instead. 
+                    endEntityManagementSession.setUserStatus(getAuthenticationToken(), username, EndEntityConstants.STATUS_NEW);
+                    getLogger().info("No certifictaes were found for end entity '" + username + "'. Setting status to NEW (10).");
                 }
-            }
-            if (!foundCertificateOnHold) {
-                getLogger().error("No certificates with status 'On hold' were found for this end entity.");
-            } else {
-                data = EjbRemoteHelper.INSTANCE.getRemoteSession(EndEntityAccessSessionRemote.class).findUser(getAuthenticationToken(), username);
-                getLogger().info("New status=" + data.getStatus());
-                return CommandResult.SUCCESS;
+
+            } catch (ApprovalException e) {
+                getLogger().error("ERROR: Reactivation already requested.");
+            } catch (WaitingForApprovalException e) {
+                getLogger().info("ERROR: Reactivation request has been sent for approval.");
             }
         } catch (AuthorizationDeniedException e) {
             getLogger().error("Not authorized to reactivate end entity.");
             return CommandResult.AUTHORIZATION_FAILURE;
+        } catch (FinderException e) {
+            getLogger().error("ERROR: " + e.getMessage());
         }
-        return CommandResult.FUNCTIONAL_FAILURE;
+        return CommandResult.SUCCESS;
 
     }
 
