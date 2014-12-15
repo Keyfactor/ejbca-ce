0c27726ec05d87ccb7e82844c3b2deefd51dde99
Mike Agrenius Kushner <mike.kushner@primekey.com>
2014-12-15 12:32:54 +0000

    ECA-3992: Another warning, should fix failing unit test
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@20468 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-ejb/src/org/cesecore/certificates/certificate/CertificateCreateSessionBean.java
+++ modules/cesecore-ejb/src/org/cesecore/certificates/certificate/CertificateCreateSessionBean.java
@@ -227,7 +227,7 @@ public class CertificateCreateSessionBean implements CertificateCreateSessionLoc
             throw new CertificateCreateException(e);
         } catch (CRLException e) {
             throw new CertificateCreateException(e);
-        }
+        } 
 
         if (log.isTraceEnabled()) {
             log.trace("<createCertificate(IRequestMessage, CA)");
