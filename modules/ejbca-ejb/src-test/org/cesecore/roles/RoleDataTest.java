3e273b35de903825461591ec1d8a3c686210c089
Mike Agrenius Kushner <mike.kushner@primekey.com>
2015-03-03 10:12:34 +0000

    EJBCAINTER-37: Fixed a case of rules with similar names causing confusion. Added a test case and also improved test output a bit.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@20808 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb/src-test/org/cesecore/roles/RoleDataTest.java
+++ modules/ejbca-ejb/src-test/org/cesecore/roles/RoleDataTest.java
@@ -37,11 +37,14 @@ public class RoleDataTest {
         Map<Integer, AccessRuleData> accessRules = new HashMap<Integer, AccessRuleData>();
         accessRules.put(1, new AccessRuleData(roleName, "/foo", AccessRuleState.RULE_ACCEPT, true));
         accessRules.put(2, new AccessRuleData(roleName, "/foo/bar", AccessRuleState.RULE_DECLINE, true));
+        accessRules.put(3, new AccessRuleData(roleName, "/xyz", AccessRuleState.RULE_DECLINE, true));
+        accessRules.put(4, new AccessRuleData(roleName, "/xyz_abc", AccessRuleState.RULE_ACCEPT, true));
         testRole.setAccessRules(accessRules);
-        assertFalse("Was incorrectly given access to a rule.", testRole.hasAccessToRule("/"));
-        assertTrue("Should have been given access to a rule.", testRole.hasAccessToRule("/foo"));
-        assertFalse("Was incorrectly given access to a rule.",testRole.hasAccessToRule("/foo/bar"));
-        assertFalse("Was incorrectly given access to a rule.",testRole.hasAccessToRule("/foo/bar/xyz"));
+        assertFalse("Was incorrectly given access to a rule that should have state unknown", testRole.hasAccessToRule("/"));
+        assertTrue("Should have been given access to a rule which was explicitly accepted.", testRole.hasAccessToRule("/foo"));
+        assertFalse("Was incorrectly given access to a rule which was explicitly denied.",testRole.hasAccessToRule("/foo/bar"));
+        assertFalse("Was incorrectly given access to a rule which was denied in a subrule.",testRole.hasAccessToRule("/foo/bar/xyz"));
+        assertTrue("Should have been given access to a rule with a similar name as one denied", testRole.hasAccessToRule("/xyz_abc"));
     }
 
 }
