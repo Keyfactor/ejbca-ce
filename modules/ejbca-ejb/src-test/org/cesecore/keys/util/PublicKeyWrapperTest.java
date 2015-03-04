c0dfe73b380e27d65b42783ecef31328b0481d00
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2015-03-04 14:32:00 +0000

    Noticket: add simple serialization test
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@20836 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb/src-test/org/cesecore/keys/util/PublicKeyWrapperTest.java
+++ modules/ejbca-ejb/src-test/org/cesecore/keys/util/PublicKeyWrapperTest.java
@@ -14,9 +14,13 @@ package org.cesecore.keys.util;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.PublicKey;
-import java.security.spec.InvalidKeySpecException;
 
 import org.cesecore.certificates.util.AlgorithmConstants;
 import org.cesecore.util.CryptoProviderTools;
@@ -29,16 +33,34 @@ import org.junit.Test;
  */
 public class PublicKeyWrapperTest {
 
+    public static PublicKey testKey;
     @BeforeClass
-    public static void beforeClass() {
+    public static void beforeClass() throws InvalidAlgorithmParameterException {
         CryptoProviderTools.installBCProviderIfNotAvailable();
+        testKey = KeyTools.genKeys("512", AlgorithmConstants.KEYALGORITHM_RSA).getPublic();
     }
     
     @Test
-    public void testGetPublicKey() throws InvalidAlgorithmParameterException, InvalidKeySpecException {
-        PublicKey testKey = KeyTools.genKeys("512", AlgorithmConstants.KEYALGORITHM_RSA).getPublic();
+    public void testGetPublicKey() {
         PublicKeyWrapper testWrapper = new PublicKeyWrapper(testKey);
         assertEquals("Decoded PublicKey was not identical to encoded.", testKey, testWrapper.getPublicKey());
     }
-    
+
+    @Test
+    public void testPublicKeySerialization() throws IOException, ClassNotFoundException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(baos);
+        oos.writeObject(testKey);
+        oos.close();
+        byte[] bytes = baos.toByteArray();
+        baos.close();
+        ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+        ObjectInputStream ois = new ObjectInputStream(bais);
+        Object obj = ois.readObject();
+        PublicKey pk = (PublicKey)obj;
+        final String str = pk.getClass().getName();
+        assertEquals("Deserialized class should be a BC PublicKey", "org.bouncycastle.jcajce.provider.asymmetric.rsa.BCRSAPublicKey", str);
+        assertEquals("Deserialized PublicKey was not identical to encoded.", testKey, pk);
+    }
+
 }
