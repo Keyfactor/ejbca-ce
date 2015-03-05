106680caf26d4eacb3406cdbe0bf87a7d69fc266
Mike Agrenius Kushner <mike.kushner@primekey.com>
2015-03-05 14:57:14 +0000

    ECA-4064: Cleaned up documentation and added license header.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@20850 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/config/ScepConfiguration.java
+++ src/java/org/ejbca/config/ScepConfiguration.java
@@ -1,3 +1,16 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
 package org.ejbca.config;
 
 import java.io.Serializable;
@@ -22,7 +35,7 @@ import org.cesecore.configuration.ConfigurationBase;
 public class ScepConfiguration extends ConfigurationBase implements Serializable {
     
     
- // Constants: Configuration keys
+    // Constants: Configuration keys
     public static final String SCEP_PREFIX = "scep.";
     public static final String SCEP_RAMODE_OLD = "ra.createOrEditUser";
     public static final String SCEP_OPERATIONMODE = "operationmode";
