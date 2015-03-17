d9dfe6f58854157cd97dcd70099d4a4e1cab94a6
aveen4711 <USER@primekey.com>
2015-03-17 13:01:05 +0000

    ECA-4062: Changed parameter type from HashMap to List
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@20918 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/edition-specific/src-ejb/org/ejbca/core/ejb/EnterpriseEditionWSBridgeSessionBean.java
+++ modules/edition-specific/src-ejb/org/ejbca/core/ejb/EnterpriseEditionWSBridgeSessionBean.java
@@ -12,7 +12,7 @@
  *************************************************************************/
 package org.ejbca.core.ejb;
 
-import java.util.HashMap;
+import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
@@ -31,7 +31,7 @@ public class EnterpriseEditionWSBridgeSessionBean implements EnterpriseEditionWS
 
     @Override
     public void createCryptoToken(AuthenticationToken admin, String tokenName, String tokenType, String activationPin, 
-            boolean autoActivate, HashMap<Object, Object> cryptoTokenProperties) throws UnsupportedMethodException {
+            boolean autoActivate, List<String> cryptoTokenProperties) throws UnsupportedMethodException {
         throw new UnsupportedMethodException("This method can only be used in Enterprise edition.");
     }
     
@@ -43,7 +43,7 @@ public class EnterpriseEditionWSBridgeSessionBean implements EnterpriseEditionWS
 
     @Override
     public void createCA(AuthenticationToken admin, String caname, String cadn, String catype, String catokentype, String catokenpassword,
-            HashMap<Object, Object> catokenProperties, String cryptoTokenName, String cryptotokenKeyAlias, long validityInDays, String certprofile,
+            List<String> catokenProperties, String cryptoTokenName, String cryptotokenKeyAlias, long validityInDays, String certprofile,
             String signAlg, String policyId, int signedByCAId) throws UnsupportedMethodException {
         throw new UnsupportedMethodException("This method can only be used in Enterprise edition.");   
     }
