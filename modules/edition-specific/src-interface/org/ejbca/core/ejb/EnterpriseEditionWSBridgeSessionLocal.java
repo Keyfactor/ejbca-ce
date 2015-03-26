88862034b87986388d58ef4af764e35ce3c3edc9
Mike Agrenius Kushner <mike.kushner@primekey.com>
2015-03-26 12:32:20 +0000

    ECA-4092: Did some general cleanup and fixed javadoc
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@21014 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/edition-specific/src-interface/org/ejbca/core/ejb/EnterpriseEditionWSBridgeSessionLocal.java
+++ modules/edition-specific/src-interface/org/ejbca/core/ejb/EnterpriseEditionWSBridgeSessionLocal.java
@@ -43,18 +43,19 @@ public interface EnterpriseEditionWSBridgeSessionLocal {
     /**
      * Creates a new cryptotoken
      * 
-     * @param admin
+     * @param admin An authentication token
      * @param tokenName The name of the crypto token
      * @param tokenType The type of the crypto token. Available types: SoftCryptoToken, PKCS11CryptoToken
      * @param activationPin Pin code for the crypto token
      * @param autoActivate Set to true|false to allow|disallow whether crypto token should be autoactivated or not
      * @param cryptoTokenProperties The properties of the cryptotoken. See {@link org.ejbca.core.protocol.ws.objects.CryptoTokenConstantsWS}
-     * @throws UnsupportedMethodException When trying to access this method in the community version
-     * @throws AuthorizationDeniedException
-     * @throws CryptoTokenOfflineException
-     * @throws CryptoTokenAuthenticationFailedException
-     * @throws CryptoTokenNameInUseException
-     * @throws NoSuchSlotException
+     * 
+     * @throws UnsupportedMethodException if trying to access this method in the community version
+     * @throws AuthorizationDeniedException if admin lacks access to resource /cryptotoken/modify
+     * @throws CryptoTokenOfflineException if the crypto token was unavailable
+     * @throws CryptoTokenAuthenticationFailedException if the password specified in activationPin was incorrect
+     * @throws CryptoTokenNameInUseException if a crypto token with the given name already exists
+     * @throws NoSuchSlotException if the slot as defined in cryptoTokenProperties doesn't exist
      */
     void createCryptoToken(AuthenticationToken admin, String tokenName, String tokenType, String activationPin, boolean autoActivate, 
             List<KeyValuePair> cryptoTokenProperties) throws UnsupportedMethodException, AuthorizationDeniedException, 
@@ -63,15 +64,16 @@ public interface EnterpriseEditionWSBridgeSessionLocal {
     /**
      * Generates a keys pair in the specified cryptotoken
      * 
-     * @param admin
+     * @param admin An authentication token
      * @param cryptoTokenName The name of the cryptotoken
      * @param keyPairAlias Key pair alias
      * @param keySpecification Key specification, for example RSA2048, secp256r1, DSA1024, gost3410, dstu4145
+     * 
      * @throws UnsupportedMethodException When trying to access this method in the community version
-     * @throws InvalidKeyException
-     * @throws CryptoTokenOfflineException
-     * @throws InvalidAlgorithmParameterException
-     * @throws AuthorizationDeniedException
+     * @throws InvalidKeyException if key generation failed
+     * @throws CryptoTokenOfflineException if the crypto token was unavailable
+     * @throws InvalidAlgorithmParameterException if the keySpecification is not available for this CryptoToken.
+     * @throws AuthorizationDeniedException if admin lacks access to resource /cryptotoken/modify
      */
     void generateCryptoTokenKeys(AuthenticationToken admin, String cryptoTokenName, String keyPairAlias, String keySpecification) 
             throws UnsupportedMethodException, InvalidKeyException, CryptoTokenOfflineException, InvalidAlgorithmParameterException, 
@@ -80,28 +82,28 @@ public interface EnterpriseEditionWSBridgeSessionLocal {
     /**
      * Creates a new CA
      * 
-     * @param admin
+     * @param admin An authentication token
      * @param caname The CA name
      * @param cadn The CA subjectDN
      * @param catype The CA type. It could be either 'x509' or 'cvc'
      * @param validityInDays Validity of the CA in days.
      * @param certprofile Makes the CA use the certificate profile 'certprofile' instead of the default ROOTCA or SUBCA.
      * @param signAlg Signing Algorithm may be one of the following: SHA1WithRSA, SHA256WithRSA, SHA384WithRSA, SHA512WithRSA
-     * SHA256WithRSAAndMGF1, SHA1withECDSA, SHA224withECDSA, SHA256withECDSA, SHA384withECDSA, SHA512withECDSA, SHA1WithDSA, 
-     * GOST3411withECGOST3410, GOST3411withDSTU4145
-     * @param signedByCAId The ID of a CA that will sign this CA. Use '1' for self signed CA (i.e. a root CA).
-     * CAs created using the WS cannot be signed by external CAs.
+     *        SHA256WithRSAAndMGF1, SHA1withECDSA, SHA224withECDSA, SHA256withECDSA, SHA384withECDSA, SHA512withECDSA, SHA1WithDSA, 
+     *        GOST3411withECGOST3410, GOST3411withDSTU4145
+     * @param signedByCAId The ID of a CA that will sign this CA. Use '1' for self signed CA (i.e. a root CA). CAs created using the WS cannot be signed by external CAs.
      * @param cryptoTokenName The name of the cryptotoken associated with the CA
      * @param purposeKeyMapping The mapping the the cryptotoken keys and their purpose. See {@link org.ejbca.core.protocol.ws.objects.CAConstantsWS}
      * @param caProperties Optional CA properties. See {@link org.ejbca.core.protocol.ws.objects.CAConstantsWS}
+     * 
      * @throws UnsupportedMethodException When trying to access this method in the community version
      * @throws SignedByExternalCANotSupportedException
-     * @throws CAExistsException
-     * @throws AuthorizationDeniedException
-     * @throws CertificateProfileDoesNotExistException
-     * @throws CertificateProfileTypeNotAcceptedException
-     * @throws CryptoTokenOfflineException
-     * @throws InvalidAlgorithmException
+     * @throws CAExistsException if a CA with the given name already exists
+     * @throws AuthorizationDeniedException if admin is not authorized to create CAs
+     * @throws CertificateProfileDoesNotExistException if the certificate profile specified by certprofile doesn't exist
+     * @throws CertificateProfileTypeNotAcceptedException if the certificate profile was not of type ROOTCA or SUBCA
+     * @throws CryptoTokenOfflineException if the crypto token was unavailable
+     * @throws InvalidAlgorithmException if the CA signature algorithm was invalid
      */
     void createCA(AuthenticationToken admin, String caname, String cadn, String catype, long validityInDays, String certprofile, 
             String signAlg, int signedByCAId, String cryptoTokenName, List<KeyValuePair> purposeKeyMapping, List<KeyValuePair> caProperties) 
@@ -111,18 +113,19 @@ public interface EnterpriseEditionWSBridgeSessionLocal {
     /**
      * Adds an administrator to the specified role
      * 
-     * @param admin
+     * @param admin An authentication token
      * @param roleName The role to add the admin to
      * @param caName Name of the CA that issued the new administrator's certificate
      * @param matchWith Could be any of: NONE, WITH_COUNTRY, WITH_DOMAINCOMPONENT, WITH_STATEORPROVINCE, WITH_LOCALITY, WITH_ORGANIZATION,
               WITH_ORGANIZATIONALUNIT, WITH_TITLE, WITH_COMMONNAME, WITH_UID, WITH_DNSERIALNUMBER, WITH_SERIALNUMBER,
               WITH_DNEMAILADDRESS, WITH_RFC822NAME, WITH_UPN, WITH_FULLDN
      * @param matchType Could be one of: TYPE_EQUALCASE, TYPE_EQUALCASEINS, TYPE_NOT_EQUALCASE, TYPE_NOT_EQUALCASEINS, TYPE_NONE
-     * @param matchValue
-     * @throws UnsupportedMethodException
-     * @throws RoleNotFoundException
-     * @throws CADoesntExistsException
-     * @throws AuthorizationDeniedException
+     * @param matchValue the value to match against
+     * 
+     * @throws UnsupportedMethodException if trying to access this method in the community version
+     * @throws RoleNotFoundException if the role specified by rolename doesn't exist
+     * @throws CADoesntExistsException if the CA specified by caName doesn't exist
+     * @throws AuthorizationDeniedException if admin doesn't have access to CA or rights to manage roles.
      */
     void addSubjectToRole(AuthenticationToken admin, String roleName, String caName, String matchWith, String matchType, 
             String matchValue) throws UnsupportedMethodException, RoleNotFoundException, CADoesntExistsException, AuthorizationDeniedException;
@@ -130,18 +133,19 @@ public interface EnterpriseEditionWSBridgeSessionLocal {
     /**
      * Removes an administrator to the specified role
      * 
-     * @param admin
+     * @param admin An authentication token
      * @param roleName The role to add the admin to
      * @param caName Name of the CA that issued the new administrator's certificate
      * @param matchWith Could be any of: NONE, WITH_COUNTRY, WITH_DOMAINCOMPONENT, WITH_STATEORPROVINCE, WITH_LOCALITY, WITH_ORGANIZATION,
               WITH_ORGANIZATIONALUNIT, WITH_TITLE, WITH_COMMONNAME, WITH_UID, WITH_DNSERIALNUMBER, WITH_SERIALNUMBER,
               WITH_DNEMAILADDRESS, WITH_RFC822NAME, WITH_UPN, WITH_FULLDN
      * @param matchType Could be one of: TYPE_EQUALCASE, TYPE_EQUALCASEINS, TYPE_NOT_EQUALCASE, TYPE_NOT_EQUALCASEINS, TYPE_NONE
-     * @param matchValue
-     * @throws UnsupportedMethodException
-     * @throws RoleNotFoundException
-     * @throws CADoesntExistsException
-     * @throws AuthorizationDeniedException
+     * @param matchValue if admin doesn't have access to CA or rights to manage roles.
+     * 
+     * @throws UnsupportedMethodException if trying to access this method in the community version
+     * @throws RoleNotFoundException if the role specified by rolename doesn't exist
+     * @throws CADoesntExistsException if the CA specified by caName doesn't exist
+     * @throws AuthorizationDeniedException if admin doesn't have access to CA or rights to manage roles.
      */
     void removeSubjectFromRole(AuthenticationToken admin, String roleName, String caName, String matchWith, String matchType, 
             String matchValue) throws UnsupportedMethodException, RoleNotFoundException, CADoesntExistsException, AuthorizationDeniedException;
