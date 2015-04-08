a2dea6d51c9f2d9fca79a44b91c684e554ba91da
aveen4711 <USER@primekey.com>
2015-04-08 14:27:54 +0000

    ECA-4205: Added 'createcryptotoken', 'generatectkeys', 'createca', 'addadmintorole', 'removeadminfromrole', 'getexpiredcerts', 'getexpiredcertsbyissuer' and 'getexpiredcertsbytype' commands to the WSCLI
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@21072 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ejbca-ws-cli/src/org/ejbca/core/protocol/ws/client/GenerateCryptoTokenKeysCommand.java
@@ -0,0 +1,78 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
+package org.ejbca.core.protocol.ws.client;
+
+import org.ejbca.core.protocol.ws.client.gen.EjbcaException_Exception;
+import org.ejbca.ui.cli.ErrorAdminCommandException;
+import org.ejbca.ui.cli.IAdminCommand;
+import org.ejbca.ui.cli.IllegalAdminCommandException;
+
+
+/**
+ * Adds an administrator to an existing Administrator role
+ *
+ * @version $Id: CaCertRequestCommand.java 19902 2014-09-30 14:32:24Z anatom $
+ */
+public class GenerateCryptoTokenKeysCommand extends EJBCAWSRABaseCommand implements IAdminCommand{
+
+
+    private static final int ARG_CRYPTOTOKEN_NAME    = 1;
+    private static final int ARG_KEY_PAIR_ALIAS      = 2;
+    private static final int ARG_KEY_SPECIFICATION   = 3;
+
+    /**
+     * Creates a new instance of Command
+     *
+     * @param args command line arguments
+     */
+    public GenerateCryptoTokenKeysCommand(String[] args) {
+        super(args);
+    }
+
+    /**
+     * Runs the command
+     *
+     * @throws IllegalAdminCommandException Error in command args
+     * @throws ErrorAdminCommandException Error running command
+     */
+    public void execute() throws IllegalAdminCommandException, ErrorAdminCommandException {
+        try {   
+            if (args.length < 4 ) {
+                getPrintStream().println("Error. Too few arguments: "+args.length);
+                usage();
+                System.exit(-1); // NOPMD, this is not a JEE app
+            }
+
+            String cryptotokenName = args[ARG_CRYPTOTOKEN_NAME];
+            String keyAlias = args[ARG_KEY_PAIR_ALIAS];
+            String keySpec = args[ARG_KEY_SPECIFICATION];
+            
+            getEjbcaRAWS().generateCryptoTokenKeys(cryptotokenName, keyAlias, keySpec);
+            getPrintStream().println("Generate key with alias '" + keyAlias + "' in cryptotoken '" + cryptotokenName + "'");
+        } catch (Exception e) {
+            if (e instanceof EjbcaException_Exception) {
+                EjbcaException_Exception e1 = (EjbcaException_Exception)e;
+                getPrintStream().println("Error code is: " + e1.getFaultInfo().getErrorCode().getInternalErrorCode());
+            }
+            throw new ErrorAdminCommandException(e);
+        }
+    }
+
+    protected void usage() {
+        getPrintStream().println("Command used to generate a keypair for a specific cryptotoken");
+        getPrintStream().println("Usage : generatectkeys <cryptotokenName> <keypairAlias> <keySpecification>");
+        getPrintStream().println();
+        getPrintStream().println("Examples of key specifications: RSA2048, secp256r1, DSA1024, gost3410, dstu4145");
+    }
+}
