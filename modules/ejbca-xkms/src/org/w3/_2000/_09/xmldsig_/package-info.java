07492ee201bcc940803e33ced755368b762890c2
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2015-09-17 19:25:21 +0000

    ECA-4292: remove XKMS, all traces of it.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@21901 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-xkms/src/org/w3/_2000/_09/xmldsig_/package-info.java
+++
@@ -1,9 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(
-	namespace = "http://www.w3.org/2000/09/xmldsig#",
-	xmlns={
-		@javax.xml.bind.annotation.XmlNs(prefix="ds", namespaceURI="http://www.w3.org/2000/09/xmldsig#"),
-		@javax.xml.bind.annotation.XmlNs(prefix="xenc", namespaceURI="http://www.w3.org/2001/04/xmlenc#"),
-		@javax.xml.bind.annotation.XmlNs(prefix="", namespaceURI="http://www.w3.org/2002/03/xkms#")},
-	elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED
-)
-package org.w3._2000._09.xmldsig_;
