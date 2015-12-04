0749f892586608c7b21418618bfe294882926b70
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2015-12-04 12:52:33 +0000

    Noticket: debug info
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@22363 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-scep-war/src/org/ejbca/ui/web/protocol/ClientCertificateRenewalExtension.java
+++ modules/ejbca-scep-war/src/org/ejbca/ui/web/protocol/ClientCertificateRenewalExtension.java
@@ -97,6 +97,9 @@ public class ClientCertificateRenewalExtension implements ScepResponsePlugin {
             keyAlias = ca.getCAToken().getAliasFromPurpose(CATokenConstants.CAKEYPURPOSE_CERTSIGN_NEXT);
             cacert = rolloverCACert;
             useRolloverCert = true;
+            if (log.isDebugEnabled()) {
+                log.debug("SCEP renewal request is using CA Rollover certificate for alias '"+scepAlias+"'");
+            }            
         } else {
             keyAlias = ca.getCAToken().getAliasFromPurpose(CATokenConstants.CAKEYPURPOSE_CERTSIGN);
             cacert = ca.getCACertificate();
@@ -124,17 +127,23 @@ public class ClientCertificateRenewalExtension implements ScepResponsePlugin {
             }
             latestIssued = certificateStoreSession.findLatestX509CertificateBySubject(reqmsg.getRequestDN(), cacert, false);
             if (latestIssued == null) {
-                throw new IllegalStateException("End entity with username " + endEntityInformation.getUsername() + " has a rollover certiticate, but no valid certificate was found. RequestDN: '"+reqmsg.getRequestDN()+"', end entity DN: '"+endEntityInformation.getDN()+"'.");
+                throw new IllegalStateException("End entity with username " + endEntityInformation.getUsername() + " has no rollover certiticate, but no valid certificate was found. RequestDN: '"+reqmsg.getRequestDN()+"', end entity DN: '"+endEntityInformation.getDN()+"'.");
             }
-            log.debug("Found existing certificate, will use client certificate renewal for rollover certificate");
+            if (log.isDebugEnabled()) {
+                log.debug("Found existing certificate, will use client certificate renewal for rollover certificate");
+            }
         } else if (endEntityInformation.getStatus() == EndEntityConstants.STATUS_GENERATED) {
             latestIssued = certificateStoreSession.findLatestX509CertificateBySubject(reqmsg.getRequestDN());
             if (latestIssued == null) {
                 throw new IllegalStateException("End entity with username " + endEntityInformation.getUsername() + " has status generated, but no certificate was found. RequestDN: '"+reqmsg.getRequestDN()+"', end entity DN: '"+endEntityInformation.getDN()+"'.");
             }
-            log.debug("Found existing certificate, will use client certificate renewal");
+            if (log.isDebugEnabled()) {
+                log.debug("Found existing certificate, will use client certificate renewal");
+            }
         } else {
-            log.debug("No existing certificate exists. Performing normal enrolment with password authentication");
+            if (log.isDebugEnabled()) {
+                log.debug("No existing certificate exists. Performing normal enrolment with password authentication");
+            }
             return signSession.createCertificate(authenticationToken, reqmsg, ScepResponseMessage.class, null);
         }
         
