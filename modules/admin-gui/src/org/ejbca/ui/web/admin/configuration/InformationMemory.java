a6bda5e6bc86339bc90abeae74f068417dde5055
Mike Agrenius Kushner <mike.kushner@primekey.com>
2015-12-08 15:02:14 +0000

    ECA-1910: Put CA's in their proper order
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@22401 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/admin-gui/src/org/ejbca/ui/web/admin/configuration/InformationMemory.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/configuration/InformationMemory.java
@@ -16,6 +16,7 @@ package org.ejbca.ui.web.admin.configuration;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
