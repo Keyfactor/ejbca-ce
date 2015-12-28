138aef4cbb758a555948eaf7aaf73ac2e071efd3
Mike Agrenius Kushner <mike.kushner@primekey.com>
2015-12-28 21:31:21 +0000

    ECA-4463: Hopefully fixed the privilege escalation issue
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@22502 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-entity/src-test/org/cesecore/roles/RoleDataTest.java
+++ modules/cesecore-entity/src-test/org/cesecore/roles/RoleDataTest.java
@@ -35,21 +35,29 @@ public class RoleDataTest {
         final String roleName = "role";
         RoleData testRole = new RoleData(1, roleName);
         Map<Integer, AccessRuleData> accessRules = new HashMap<Integer, AccessRuleData>();
-        accessRules.put(1, new AccessRuleData(roleName, "/foo", AccessRuleState.RULE_ACCEPT, false));
+        accessRules.put(1, new AccessRuleData(roleName, "/fuu", AccessRuleState.RULE_ACCEPT, false));
         accessRules.put(2, new AccessRuleData(roleName, "/foo/bar", AccessRuleState.RULE_DECLINE, true));
         accessRules.put(3, new AccessRuleData(roleName, "/xyz", AccessRuleState.RULE_DECLINE, true));
         accessRules.put(4, new AccessRuleData(roleName, "/xyz_abc", AccessRuleState.RULE_ACCEPT, true));
         accessRules.put(5, new AccessRuleData(roleName, "/recursive", AccessRuleState.RULE_ACCEPT, true));
-        accessRules.put(6, new AccessRuleData(roleName, "/non_recursive", AccessRuleState.RULE_ACCEPT, false));
+        accessRules.put(6, new AccessRuleData(roleName, "/recursivewithdenied", AccessRuleState.RULE_ACCEPT, true));
+        accessRules.put(7, new AccessRuleData(roleName, "/recursivewithdenied/denied", AccessRuleState.RULE_DECLINE, false));
+        accessRules.put(8, new AccessRuleData(roleName, "/non_recursive", AccessRuleState.RULE_ACCEPT, false));
 
 
         testRole.setAccessRules(accessRules);
         assertFalse("Was incorrectly given access to a rule that should have state unknown", testRole.hasAccessToRule("/"));
-        assertTrue("Should have been given access to a rule which was explicitly accepted.", testRole.hasAccessToRule("/foo"));
+        assertTrue("Should have been given access to a rule which was explicitly accepted.", testRole.hasAccessToRule("/fuu"));
         assertFalse("Was incorrectly given access to a rule which was explicitly denied.",testRole.hasAccessToRule("/foo/bar"));
         assertFalse("Was incorrectly given access to a rule which was denied in a subrule.",testRole.hasAccessToRule("/foo/bar/xyz"));  
         assertTrue("Should have been given access to a rule with a similar name as one denied", testRole.hasAccessToRule("/xyz_abc"));
         assertTrue("Should have been given access to a rule with recursive required", testRole.hasAccessToRule("/recursive", true));
+        assertTrue("Should have been given access to a rule with recursive allowed by parent", testRole.hasAccessToRule("/recursive/subrule", false));
+        assertTrue("Should have been given access to a rule with recursive required", testRole.hasAccessToRule("/recursive/subrule", true));
+        assertFalse("[Privilege escalation] Was incorrectly given access to a rule which was has declined in a subrule", testRole.hasAccessToRule("/recursivewithdenied", true));
+        assertFalse("Was incorrectly given access to a rule which was explicitly denied.", testRole.hasAccessToRule("/recursivewithdenied/denied", false));
+        assertFalse("Was incorrectly given access to a rule which was explicitly denied.", testRole.hasAccessToRule("/recursivewithdenied/denied", true));
+        assertFalse("Should not have been given access to a rule ", testRole.hasAccessToRule("/non_recursive", true));
         assertFalse("Should not have been given access to a rule with recursive required", testRole.hasAccessToRule("/non_recursive", true));
     }
 
