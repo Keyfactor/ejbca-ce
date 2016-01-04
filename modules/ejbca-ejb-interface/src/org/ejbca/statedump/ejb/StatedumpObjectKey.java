2c97e0dc32ed005948f4f3e789017ffeeb4bbb16
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2016-01-04 11:49:52 +0000

    ECA-4641: Fix lock down checkbox and improve logging
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@22518 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb-interface/src/org/ejbca/statedump/ejb/StatedumpObjectKey.java
+++ modules/ejbca-ejb-interface/src/org/ejbca/statedump/ejb/StatedumpObjectKey.java
@@ -48,6 +48,11 @@ public final class StatedumpObjectKey implements Serializable {
     }
     
     @Override
+    public String toString() {
+        return type+" "+name+" ("+id+")";
+    }
+    
+    @Override
     public boolean equals(final Object o) {
         if (o instanceof StatedumpObjectKey) {
             final StatedumpObjectKey sc = (StatedumpObjectKey)o;
