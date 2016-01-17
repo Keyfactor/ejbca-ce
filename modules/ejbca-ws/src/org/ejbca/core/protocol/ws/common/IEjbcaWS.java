730b1e719578a6c1337a27b904ba947091861a20
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2016-01-17 09:32:05 +0000

    Noticket: another typo
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@22587 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ws/src/org/ejbca/core/protocol/ws/common/IEjbcaWS.java
+++ modules/ejbca-ws/src/org/ejbca/core/protocol/ws/common/IEjbcaWS.java
@@ -998,7 +998,7 @@ public interface IEjbcaWS {
 	 *
 	 * @param profileId ID of the profile we want to retrieve.
 	 * @param profileType The type of the profile we want to retrieve. 'eep' for End Entity Profiles and 'cp' for Certificate Profiles
-	 * @return a byte array contaning the specified profile in XML format
+	 * @return a byte array containing the specified profile in XML format
 	 * @throws EjbcaException if an error occured
 	 * @throws AuthorizationDeniedException
 	 * @throws UnknownProfileTypeException
