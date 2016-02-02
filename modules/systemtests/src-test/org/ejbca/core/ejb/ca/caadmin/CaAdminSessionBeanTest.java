7a3ff0b691e7519c24a6d0f159e3a8a63ef1d551
Johan Eklund <johan.eklund@primekey.com>
2016-02-02 12:52:13 +0000

    ECA-4310: System test of certificate profile key specification enforcement during CA creation.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@22692 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/systemtests/src-test/org/ejbca/core/ejb/ca/caadmin/CaAdminSessionBeanTest.java
+++ modules/systemtests/src-test/org/ejbca/core/ejb/ca/caadmin/CaAdminSessionBeanTest.java
@@ -15,8 +15,11 @@ package org.ejbca.core.ejb.ca.caadmin;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
 import java.security.KeyPair;
 import java.security.Principal;
 import java.security.cert.Certificate;
@@ -26,10 +29,13 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Properties;
 import java.util.Set;
 
+import javax.ejb.EJBException;
 import javax.security.auth.x500.X500Principal;
 
+import org.apache.log4j.Logger;
 import org.bouncycastle.operator.OperatorCreationException;
 import org.cesecore.CaTestUtils;
 import org.cesecore.authentication.tokens.AuthenticationSubject;
@@ -41,18 +47,30 @@ import org.cesecore.authorization.rules.AccessRuleState;
 import org.cesecore.authorization.user.AccessMatchType;
 import org.cesecore.authorization.user.AccessUserAspectData;
 import org.cesecore.authorization.user.matchvalues.X500PrincipalAccessMatchValue;
+import org.cesecore.certificates.ca.CAConstants;
 import org.cesecore.certificates.ca.CADoesntExistsException;
 import org.cesecore.certificates.ca.CAExistsException;
 import org.cesecore.certificates.ca.CAInfo;
 import org.cesecore.certificates.ca.CaSessionRemote;
+import org.cesecore.certificates.ca.InvalidAlgorithmException;
 import org.cesecore.certificates.ca.X509CA;
+import org.cesecore.certificates.ca.X509CAInfo;
+import org.cesecore.certificates.ca.catoken.CAToken;
+import org.cesecore.certificates.ca.catoken.CATokenConstants;
+import org.cesecore.certificates.certificate.IllegalKeyException;
 import org.cesecore.certificates.certificate.InternalCertificateStoreSessionRemote;
 import org.cesecore.certificates.certificateprofile.CertificateProfile;
 import org.cesecore.certificates.certificateprofile.CertificateProfileConstants;
 import org.cesecore.certificates.certificateprofile.CertificateProfileExistsException;
 import org.cesecore.certificates.certificateprofile.CertificateProfileSessionRemote;
 import org.cesecore.certificates.util.AlgorithmConstants;
+import org.cesecore.keys.token.CryptoToken;
+import org.cesecore.keys.token.CryptoTokenAuthenticationFailedException;
+import org.cesecore.keys.token.CryptoTokenManagementSessionRemote;
+import org.cesecore.keys.token.CryptoTokenNameInUseException;
 import org.cesecore.keys.token.CryptoTokenOfflineException;
+import org.cesecore.keys.token.SoftCryptoToken;
+import org.cesecore.keys.token.p11.exception.NoSuchSlotException;
 import org.cesecore.keys.util.KeyTools;
 import org.cesecore.mock.authentication.SimpleAuthenticationProviderSessionRemote;
 import org.cesecore.mock.authentication.tokens.TestAlwaysAllowLocalAuthenticationToken;
@@ -64,6 +82,7 @@ import org.cesecore.util.CertTools;
 import org.cesecore.util.CryptoProviderTools;
 import org.cesecore.util.EJBTools;
 import org.cesecore.util.EjbRemoteHelper;
+import org.cesecore.util.StringTools;
 import org.ejbca.core.ejb.ca.publisher.PublisherProxySessionRemote;
 import org.ejbca.core.model.ca.publisher.LdapPublisher;
 import org.ejbca.core.model.ca.publisher.PublisherExistsException;
@@ -77,6 +96,8 @@ import org.junit.Test;
  *
  */
 public class CaAdminSessionBeanTest {
+
+    private static final Logger log = Logger.getLogger(CaAdminSessionBeanTest.class);
 
     private CAAdminSessionRemote caAdminSession = EjbRemoteHelper.INSTANCE.getRemoteSession(CAAdminSessionRemote.class);
     private CaSessionRemote caSession = EjbRemoteHelper.INSTANCE.getRemoteSession(CaSessionRemote.class);
@@ -246,6 +267,108 @@ public class CaAdminSessionBeanTest {
                 // NOPMD ignore
             }
             internalCertStoreSession.removeCertificatesBySubject("CN=" + TEST_BC_CERT_CA);
+        }
+    }
+
+    @Test
+    public void testInvalidKeySpecs() throws InvalidAlgorithmParameterException, CertificateProfileExistsException, AuthorizationDeniedException, CryptoTokenOfflineException,
+    CryptoTokenAuthenticationFailedException, CryptoTokenNameInUseException, NoSuchSlotException, InvalidKeyException, CAExistsException, InvalidAlgorithmException,
+    CADoesntExistsException {
+        final String TEST_NAME = Thread.currentThread().getStackTrace()[1].getMethodName();
+        final CryptoTokenManagementSessionRemote cryptoTokenManagementSession = EjbRemoteHelper.INSTANCE.getRemoteSession(CryptoTokenManagementSessionRemote.class);
+        final Integer oldCryptoTokenId = cryptoTokenManagementSession.getIdFromName(TEST_NAME);
+        if (oldCryptoTokenId != null) {
+            cryptoTokenManagementSession.deleteCryptoToken(alwaysAllowToken, oldCryptoTokenId.intValue());
+        }
+        final String TOKEN_PIN = "foo1234";
+        final Properties cryptoTokenProperties = new Properties();
+        cryptoTokenProperties.setProperty(CryptoToken.ALLOW_EXTRACTABLE_PRIVATE_KEY, Boolean.TRUE.toString());
+        cryptoTokenProperties.setProperty(CryptoToken.AUTOACTIVATE_PIN_PROPERTY, TOKEN_PIN);
+        final String KEY_SPEC_RSA = "1024";
+        final String KEY_SPEC_EC = "prime256v1";
+        final String KEY_ALIAS_RSA = "signKeyRsa";
+        final String KEY_ALIAS_EC = "signKeyEc";
+        final Properties caTokenProperties = new Properties();
+        caTokenProperties.setProperty(CATokenConstants.CAKEYPURPOSE_CERTSIGN_STRING, KEY_ALIAS_RSA);
+        caTokenProperties.setProperty(CATokenConstants.CAKEYPURPOSE_CRLSIGN_STRING, KEY_ALIAS_RSA);
+        caTokenProperties.setProperty(CATokenConstants.CAKEYPURPOSE_DEFAULT_STRING, KEY_ALIAS_RSA);
+        try {
+            final int cryptoTokenId = cryptoTokenManagementSession.createCryptoToken(alwaysAllowToken, TEST_NAME, SoftCryptoToken.class.getName(), cryptoTokenProperties, null, TOKEN_PIN.toCharArray());
+            final CAToken caToken = new CAToken(cryptoTokenId, caTokenProperties);
+            caToken.setSignatureAlgorithm(AlgorithmConstants.SIGALG_SHA256_WITH_RSA);
+            caToken.setEncryptionAlgorithm(AlgorithmConstants.SIGALG_SHA256_WITH_RSA);
+            caToken.setKeySequence(CAToken.DEFAULT_KEYSEQUENCE);
+            caToken.setKeySequenceFormat(StringTools.KEY_SEQUENCE_FORMAT_NUMERIC);
+            cryptoTokenManagementSession.createKeyPair(alwaysAllowToken, cryptoTokenId, KEY_ALIAS_RSA, KEY_SPEC_RSA);
+            cryptoTokenManagementSession.createKeyPair(alwaysAllowToken, cryptoTokenId, KEY_ALIAS_EC, KEY_SPEC_EC);
+            final CertificateProfile certificateProfile = new CertificateProfile(CertificateProfileConstants.CERTPROFILE_FIXED_ROOTCA);
+            final int certificateProfileId = certificateProfileSession.addCertificateProfile(alwaysAllowToken, TEST_NAME, certificateProfile);
+            final X509CAInfo x509CaInfo = new X509CAInfo("CN="+TEST_NAME, TEST_NAME, CAConstants.CA_ACTIVE, certificateProfileId, 3650, CAInfo.SELFSIGNED, null, caToken);
+            // Test happy path. RSA 1024 bit key. RSA 1024 allowed by certificate profile.
+            testInvalidKeySpecsInternal(true, TEST_NAME, x509CaInfo, new String[] {AlgorithmConstants.KEYALGORITHM_RSA},
+                    new String[] {CertificateProfile.ANY_EC_CURVE}, new int[] {1024}, KEY_ALIAS_RSA, AlgorithmConstants.SIGALG_SHA256_WITH_RSA);
+            // Test failure. RSA 1024 bit key. 1024 bits not allowed by certificate profile.
+            testInvalidKeySpecsInternal(false, TEST_NAME, x509CaInfo, new String[] {AlgorithmConstants.KEYALGORITHM_RSA},
+                    new String[] {CertificateProfile.ANY_EC_CURVE}, new int[] {2048}, KEY_ALIAS_RSA, AlgorithmConstants.SIGALG_SHA256_WITH_RSA);
+            // Test failure. RSA 1024 bit key. RSA not allowed by certificate profile.
+            testInvalidKeySpecsInternal(false, TEST_NAME, x509CaInfo, new String[] {AlgorithmConstants.KEYALGORITHM_ECDSA},
+                    new String[] {CertificateProfile.ANY_EC_CURVE}, new int[] {1024}, KEY_ALIAS_RSA, AlgorithmConstants.SIGALG_SHA256_WITH_RSA);
+            // Test failure. EC "prime256v1" 256 bit key. EC not allowed by certificate profile.
+            testInvalidKeySpecsInternal(false, TEST_NAME, x509CaInfo, new String[] {AlgorithmConstants.KEYALGORITHM_RSA},
+                    new String[] {CertificateProfile.ANY_EC_CURVE}, new int[] {256}, KEY_ALIAS_EC, AlgorithmConstants.SIGALG_SHA256_WITH_ECDSA);
+            // Test failure. EC "prime256v1" 256 bit key. 256 bits not allowed by certificate profile.
+            testInvalidKeySpecsInternal(false, TEST_NAME, x509CaInfo, new String[] {AlgorithmConstants.KEYALGORITHM_ECDSA},
+                    new String[] {CertificateProfile.ANY_EC_CURVE}, new int[] {512}, KEY_ALIAS_EC, AlgorithmConstants.SIGALG_SHA256_WITH_ECDSA);
+            // Test failure. EC "prime256v1" (a.k.a "secp256r1") 256 bit key. "prime256v1" not allowed by certificate profile.
+            testInvalidKeySpecsInternal(false, TEST_NAME, x509CaInfo, new String[] {AlgorithmConstants.KEYALGORITHM_ECDSA},
+                    new String[] {"secp256k1"}, new int[] {256}, KEY_ALIAS_EC, AlgorithmConstants.SIGALG_SHA256_WITH_ECDSA);
+            // Test happy path. EC "prime256v1" 256 bit key. "prime256v1" allowed by certificate profile.
+            testInvalidKeySpecsInternal(true, TEST_NAME, x509CaInfo, new String[] {AlgorithmConstants.KEYALGORITHM_ECDSA},
+                    new String[] {"prime256v1"}, new int[] {512}, KEY_ALIAS_EC, AlgorithmConstants.SIGALG_SHA256_WITH_ECDSA);
+            // Test happy path. EC "prime256v1" 256 bit key. "prime256v1" and RSA 1024 allowed by certificate profile.
+            testInvalidKeySpecsInternal(true, TEST_NAME, x509CaInfo, new String[] {AlgorithmConstants.KEYALGORITHM_ECDSA, AlgorithmConstants.KEYALGORITHM_RSA},
+                    new String[] {"prime256v1"}, new int[] {1024}, KEY_ALIAS_EC, AlgorithmConstants.SIGALG_SHA256_WITH_ECDSA);
+            // Test happy path. RSA 1024 bit key. "prime256v1" and RSA 1024 allowed by certificate profile.
+            testInvalidKeySpecsInternal(true, TEST_NAME, x509CaInfo, new String[] {AlgorithmConstants.KEYALGORITHM_ECDSA, AlgorithmConstants.KEYALGORITHM_RSA},
+                    new String[] {"prime256v1"}, new int[] {1024}, KEY_ALIAS_EC, AlgorithmConstants.SIGALG_SHA256_WITH_ECDSA);
+            // Test happy path. EC "prime256v1" (a.k.a "secp256r1") 256 bit key. "prime256v1" alias "secp256r1" allowed by certificate profile.
+            testInvalidKeySpecsInternal(true, TEST_NAME, x509CaInfo, new String[] {AlgorithmConstants.KEYALGORITHM_ECDSA},
+                    new String[] {"secp256r1"}, new int[] {512}, KEY_ALIAS_EC, AlgorithmConstants.SIGALG_SHA256_WITH_ECDSA);
+        } finally {
+            try {
+                caSession.removeCA(alwaysAllowToken, caSession.getCAInfo(alwaysAllowToken, TEST_NAME).getCAId());
+            } catch (CADoesntExistsException e) {
+                log.debug(e.getMessage());
+            }
+            cryptoTokenManagementSession.deleteCryptoToken(alwaysAllowToken, cryptoTokenManagementSession.getIdFromName(TEST_NAME));
+            certificateProfileSession.removeCertificateProfile(alwaysAllowToken, TEST_NAME);
+        }
+    }
+
+    private void testInvalidKeySpecsInternal(final boolean expectNoIllegalKeyException, final String certificateProfileName, final CAInfo caInfo,
+            final String[] availableKeyAlgorithms, final String[] availableEcCurves, final int[] availableBitLengths, final String keyAlias,
+            final String signatureAlgoritm) throws AuthorizationDeniedException,
+            CAExistsException, CryptoTokenOfflineException, InvalidAlgorithmException, CADoesntExistsException {
+        final CertificateProfile certificateProfile = new CertificateProfile(CertificateProfileConstants.CERTPROFILE_FIXED_ROOTCA);
+        certificateProfile.setAvailableKeyAlgorithms(availableKeyAlgorithms);
+        certificateProfile.setAvailableEcCurves(availableEcCurves);
+        certificateProfile.setAvailableBitLengths(availableBitLengths);
+        certificateProfileSession.changeCertificateProfile(alwaysAllowToken, certificateProfileName, certificateProfile);
+        caInfo.getCAToken().setProperty(CATokenConstants.CAKEYPURPOSE_CERTSIGN_STRING, keyAlias);
+        caInfo.getCAToken().setProperty(CATokenConstants.CAKEYPURPOSE_CRLSIGN_STRING, keyAlias);
+        caInfo.getCAToken().setSignatureAlgorithm(signatureAlgoritm);
+        try {
+            caAdminSession.createCA(alwaysAllowToken, caInfo);
+            caSession.removeCA(alwaysAllowToken, caSession.getCAInfo(alwaysAllowToken, caInfo.getName()).getCAId());
+            if (!expectNoIllegalKeyException) {
+                fail("Validation should not work with invalid key size and/or algoritmh,");
+            }
+        } catch (EJBException e) {
+            if (e.getCause() instanceof IllegalKeyException) {
+                if (expectNoIllegalKeyException) {
+                    fail("Key algorithm and spec should have been allowed by certificate profile.");
+                }
+            }
         }
     }
 }
