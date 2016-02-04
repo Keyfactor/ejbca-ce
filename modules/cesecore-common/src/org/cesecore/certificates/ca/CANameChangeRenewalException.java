2a4ed8c290fc1c8127b09b078188a8df3b1e5472
marko <USER@primekey.com>
2016-02-04 13:24:54 +0000

    ECA-4543: CANameChangeRenewalException was introduced with two tests for it. Some refactorization and fixes.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@22711 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/cesecore-common/src/org/cesecore/certificates/ca/CANameChangeRenewalException.java
@@ -0,0 +1,55 @@
+/*************************************************************************
+ *                                                                       *
+ *  CESeCore: CE Security Core                                           *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/ 
+package org.cesecore.certificates.ca;
+
+import javax.ejb.ApplicationException;
+import javax.xml.ws.WebFault;
+
+import org.cesecore.CesecoreException;
+import org.cesecore.ErrorCode;
+
+
+/**
+ * An exception thrown when CA Name Change renewal could not be completed.
+ *
+ * @version $Id: CADoesntExistsException.java 17625 2013-09-20 07:12:06Z netmackan $
+ */
+@WebFault
+@ApplicationException(rollback=true)
+public class CANameChangeRenewalException extends CesecoreException {
+    
+    private static final long serialVersionUID = 1542504214401684378L;
+
+    /**
+     * Creates a new instance of <code>CANameChangeRenewalException</code> without detail message.
+     */
+    public CANameChangeRenewalException() {
+        super(ErrorCode.CA_NAME_CHANGE_RENEWAL_ERROR);
+    }
+        
+    /**
+     * Constructs an instance of <code>CANameChangeRenewalException</code> with the specified detail message.
+     * @param msg the detail message.
+     */
+    public CANameChangeRenewalException(String msg) {
+        super(ErrorCode.CA_NAME_CHANGE_RENEWAL_ERROR, msg);
+    }
+
+    /**
+     * Constructs an instance of <code>CANameChangeRenewalException</code> with the specified cause.
+     * @param msg the detail message.
+     */
+    public CANameChangeRenewalException(Exception e) {
+        super(e);
+    }
+}
