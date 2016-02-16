9494b7e533dde068f873106cfa3ee7567ebaa85d
aveen4711 <USER@primekey.com>
2016-02-16 11:25:56 +0000

    ECA-4569: Added license text and javadoc
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@22820 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/systemtests/src-test/org/cesecore/certificates/certificate/CertRevocationStatusCheckerTest.java
+++ modules/systemtests/src-test/org/cesecore/certificates/certificate/CertRevocationStatusCheckerTest.java
@@ -1,3 +1,15 @@
+/*************************************************************************
+ *                                                                       *
+ *  CESeCore: CE Security Core                                           *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.cesecore.certificates.certificate;
 
 import static org.junit.Assert.assertEquals;
@@ -63,6 +75,11 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * Tests PKIXCertRevocationStatusChecker
+ * 
+ * @version $Id$
+ */
 public class CertRevocationStatusCheckerTest extends CaTestCase {
 
     @Override
