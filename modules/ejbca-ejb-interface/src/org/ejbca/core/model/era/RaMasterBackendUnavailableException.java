b1b19b342d29f9a8cb9819c67d99dccdedc8711a
Johan Eklund <johan.eklund@primekey.com>
2016-03-09 15:37:04 +0000

    ECA-4867: PoC implementation of long hanging peer connections for reverse calls to a RaMasterApi.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@22953 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ejbca-ejb-interface/src/org/ejbca/core/model/era/RaMasterBackendUnavailableException.java
@@ -0,0 +1,21 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA - Proprietary Modules: Enterprise Certificate Authority        *
+ *                                                                       *
+ *  Copyright (c), PrimeKey Solutions AB. All rights reserved.           *
+ *  The use of the Proprietary Modules are subject to specific           * 
+ *  commercial license terms.                                            *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.core.model.era;
+
+/**
+ * Exception thrown when peer backend is not available.
+ * 
+ * @version $Id$
+ */
+public class RaMasterBackendUnavailableException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+}
