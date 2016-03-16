472246d686e460f47c7acc3663edb3052e2cd125
Johan Eklund <johan.eklund@primekey.com>
2016-03-16 14:08:21 +0000

    ECA-4870: Basic JSF 2.0 with ManagedBean injection examples, facelets templating, localization, script load, CSS import and a authentication checks. Simple test call over peers was GUI enabled. WAR was enabled under /ejbca/ra/ URL. Enabled JSF tool support for ra-gui and admin-gui in Eclipse properties.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@23013 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ra-gui/src/org/ejbca/ra/RaAuthenticationBean.java
@@ -0,0 +1,64 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.ra;
+
+import java.io.Serializable;
+import java.security.cert.X509Certificate;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.cesecore.authentication.tokens.AuthenticationSubject;
+import org.cesecore.authentication.tokens.AuthenticationToken;
+import org.cesecore.authentication.tokens.PublicAccessAuthenticationToken;
+import org.ejbca.core.ejb.authentication.web.WebAuthenticationProviderSessionLocal;
+
+/**
+ * JSF Managed Bean for handling authentication of clients.
+ * 
+ * @version $Id$
+ */
+@ManagedBean
+@SessionScoped
+public class RaAuthenticationBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @EJB
+    private WebAuthenticationProviderSessionLocal webAuthenticationProviderSession;
+
+    private AuthenticationToken authenticationToken = null;
+
+    /** @return the X509CertificateAuthenticationToken if the client has provided a certificate or a PublicAccessAuthenticationToken otherwise. */
+    public AuthenticationToken getAuthenticationToken() {
+        if (authenticationToken==null) {
+            final HttpServletRequest currentRequest = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+            final X509Certificate[] certificates = (X509Certificate[]) currentRequest.getAttribute("javax.servlet.request.X509Certificate");
+            if (certificates != null && certificates.length>0) {
+                final Set<X509Certificate> credentials = new HashSet<X509Certificate>();
+                credentials.add(certificates[0]);
+                final AuthenticationSubject subject = new AuthenticationSubject(null, credentials);
+                authenticationToken = webAuthenticationProviderSession.authenticate(subject);
+            }
+            if (authenticationToken == null) {
+                authenticationToken = new PublicAccessAuthenticationToken(currentRequest.getRemoteAddr());
+            }
+        }
+        return authenticationToken;
+    }
+}
---
+++ modules/ra-gui/src/org/ejbca/ra/RaExampleBean.java
@@ -0,0 +1,61 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.ra;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+
+import org.cesecore.authorization.AuthorizationDeniedException;
+import org.ejbca.core.EjbcaException;
+
+/**
+ * Example of JSF Managed Bean for backing a page. 
+ * 
+ * @version $Id$
+ */
+@ManagedBean
+@ViewScoped
+public class RaExampleBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    //private static final Logger log = Logger.getLogger(RaExampleBean.class);
+    
+    @ManagedProperty(value="#{raMasterApiBean}")
+    private RaMasterApiBean raMasterApiBean;
+    public void setRaMasterApiBean(final RaMasterApiBean raMasterApiBean) { this.raMasterApiBean = raMasterApiBean; }
+
+    @ManagedProperty(value="#{raAuthenticationBean}")
+    private RaAuthenticationBean raAuthenticationBean;
+    public void setRaAuthenticationBean(final RaAuthenticationBean raAuthenticationBean) { this.raAuthenticationBean = raAuthenticationBean; }
+
+    @ManagedProperty(value="#{raLocaleBean}")
+    private RaLocaleBean raLocaleBean;
+    public void setRaLocaleBean(final RaLocaleBean raLocaleBean) { this.raLocaleBean = raLocaleBean; }
+
+    private String value = null;
+
+    public String getValue() { return value; }
+    public void setValue(String value) { this.value = value.trim(); }
+
+    public void testAction() {
+        try {
+            final String result = raMasterApiBean.testCall(raAuthenticationBean.getAuthenticationToken(), value, 12345);
+            raLocaleBean.addMessageInfo("somefunction_testok", result);
+        } catch (AuthorizationDeniedException | EjbcaException e) {
+            raLocaleBean.addMessageError("somefunction_testfail", e.getMessage());
+        }
+    }
+}
