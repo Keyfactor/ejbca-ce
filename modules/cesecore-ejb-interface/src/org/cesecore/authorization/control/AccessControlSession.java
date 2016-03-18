e64ea17b09de28ab19c3b5ecc3f3a5746ad48250
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2016-03-18 17:37:59 +0000

    ECA-4918: Initial implementation of "AccessSets" that can be cached on the RA
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@23033 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-ejb-interface/src/org/cesecore/authorization/control/AccessControlSession.java
+++ modules/cesecore-ejb-interface/src/org/cesecore/authorization/control/AccessControlSession.java
@@ -12,7 +12,9 @@
  *************************************************************************/
 package org.cesecore.authorization.control;
 
+import org.cesecore.authentication.AuthenticationFailedException;
 import org.cesecore.authentication.tokens.AuthenticationToken;
+import org.cesecore.authorization.access.AccessSet;
 
 /**
  * Main interface for checking authorization. This interface makes use of roles and entity authentication to verify authorization.
@@ -74,6 +76,12 @@ public interface AccessControlSession {
      * due to circular dependencies.
      */
     void forceCacheExpire();
+
+    /**
+     * Returns all rules that the given authenticationToken is allowed to access. Includes *SOME wildcard rules
+     * @throws AuthenticationFailedException On authentication errors, such as an invalid password for a CLI token
+     */
+    AccessSet getAccessSetForAuthToken(AuthenticationToken authenticationToken) throws AuthenticationFailedException;
     
 
 
