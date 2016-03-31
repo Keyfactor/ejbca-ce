bdafc686e1f4982e71ccb36c27f81ef16cf9b0f4
Johan Eklund <johan.eklund@primekey.com>
2016-03-31 13:27:48 +0000

    ECA-4969: Fix HTTP session JavaScript HTTP session keep-alive in combination with secure flag cookie setting.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@23120 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ra-gui/resources/js/common.js
+++ modules/ra-gui/resources/js/common.js
@@ -18,14 +18,18 @@
 // Executed when the document had been loaded
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("Document loaded.");
-    new SessionKeepAlive();
+    new SessionKeepAlive("sessionKeepAliveLink");
 }, false);
 
 /** Keep JSF session alive by polling back-end before the session has expired */
-function SessionKeepAlive() {
+function SessionKeepAlive(linkElementId) {
     var instance = this;
     this.timeToNextCheckInMs = 100; // Make first check after 100 ms.
     this.xmlHttpReq = new XMLHttpRequest();
+    var linkComponent = document.getElementById(linkElementId);
+    if (linkComponent) {
+        this.link = linkComponent.getAttribute("href");
+    }
     this.xmlHttpReq.onreadystatechange = function() {
         if (instance.xmlHttpReq.readyState == 4) {
             if (instance.xmlHttpReq.status == 200) {
@@ -37,12 +41,16 @@ function SessionKeepAlive() {
         }
     };
     this.poll = function() {
-    	instance.xmlHttpReq.open("GET", "sessionKeepAlive", true);
+    	instance.xmlHttpReq.open("GET", instance.link, true);
         try {
         	instance.xmlHttpReq.send();
         } catch (exception) {
             console.log("SessionKeepAlive failed: " + exception);
         }
     };
-    setTimeout(this.poll, this.timeToNextCheckInMs);
+    if (this.link) {
+        setTimeout(this.poll, this.timeToNextCheckInMs);
+    } else {
+        console.log("Unable to find link element with id " + linkElementId + ". SessionKeepAlive will not be enabled.");
+    }
 };
