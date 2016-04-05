9610176ff55f4c24b833d06a6fe18ed4aa88f29d
Johan Eklund <johan.eklund@primekey.com>
2016-04-05 22:54:22 +0000

    ECA-4975: Added Post Redirect Get pattern example call using faces-redirect that keeps messages and a value in flash scope.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@23134 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ra-gui/src/org/ejbca/ra/RaExampleBean.java
+++ modules/ra-gui/src/org/ejbca/ra/RaExampleBean.java
@@ -15,9 +15,11 @@ package org.ejbca.ra;
 import java.io.Serializable;
 import java.util.Random;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import org.apache.log4j.Logger;
 import org.cesecore.authorization.AuthorizationDeniedException;
@@ -47,6 +49,15 @@ public class RaExampleBean implements Serializable {
     private RaLocaleBean raLocaleBean;
     public void setRaLocaleBean(final RaLocaleBean raLocaleBean) { this.raLocaleBean = raLocaleBean; }
 
+    @PostConstruct
+    private void postContruct() {
+        // Check if we have a flash scoped value from a redirect to this page
+        final String value = (String) FacesContext.getCurrentInstance().getExternalContext().getFlash().remove("value");
+        if (value!=null) {
+            this.value = value;
+        }
+    }
+    
     private String value = null;
 
     public String getValue() { return value; }
@@ -57,6 +68,7 @@ public class RaExampleBean implements Serializable {
         throw new Exception("RaErrorBean.throwException " + new Random().nextInt(100));
     }
 
+    // Simple call to stay on page and keep using this instance of the @ViewScoped bean
     public void testAction() {
         try {
             final long timeBefore = System.currentTimeMillis();
@@ -66,5 +78,21 @@ public class RaExampleBean implements Serializable {
         } catch (AuthorizationDeniedException | EjbcaException e) {
             raLocaleBean.addMessageError("somefunction_testfail", e.getMessage());
         }
+    }
+
+    // Call to stay on page by Post Redirect Get pattern and use a new instance of this bean, but save a "value" in the flash scope
+    // Suitable for actions that are not repeatable if client tries to reload page (e.g. "Cancel" or "Perform non-idempotent action"
+    public String testActionAndRedirect() {
+        try {
+            final long timeBefore = System.currentTimeMillis();
+            final String result = raMasterApiBean.testCall(raAuthenticationBean.getAuthenticationToken(), value, 12345);
+            final long timeAfter = System.currentTimeMillis();
+            raLocaleBean.addMessageInfo("somefunction_testok", result, timeAfter-timeBefore);
+        } catch (AuthorizationDeniedException | EjbcaException e) {
+            raLocaleBean.addMessageError("somefunction_testfail", e.getMessage());
+        }
+        FacesContext.getCurrentInstance().getExternalContext().getFlash().put("value", value);
+        FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
+        return "index.xhtml?faces-redirect=true";
     }
 }
