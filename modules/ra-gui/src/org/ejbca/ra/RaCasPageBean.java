982271e3d61440533c74f2a48212de2009b4d0fb
Johan Eklund <johan.eklund@primekey.com>
2016-04-12 16:06:55 +0000

    ECA-4984: Simplified backing bean code to reflect what we want to render now.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@23178 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ra-gui/src/org/ejbca/ra/RaCasPageBean.java
+++ modules/ra-gui/src/org/ejbca/ra/RaCasPageBean.java
@@ -19,15 +19,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 
-import org.apache.log4j.Logger;
 import org.bouncycastle.util.encoders.Base64;
 import org.cesecore.certificates.ca.CAInfo;
 import org.cesecore.certificates.crl.CRLInfo;
@@ -47,47 +48,37 @@ public class RaCasPageBean implements Serializable {
     public class CaAndCrl {
         private final String name;
         private final String subjectDn;
+        private final int caId;
         private String crlLink;
         private String deltaCrlLink;
         private final int position;
-        private int relativePosition;
+        private final List<String> chainNames;
         
-        CaAndCrl(final String name, final String subjectDn, final int position) {
-            this.name = name;
-            this.subjectDn = subjectDn;
-            this.position = position;
-        }
-        
-        public String getName() { return name; }
-        public String getSubjectDn() { return subjectDn; }
-        public String getCrlLink() { return crlLink; }
-        public String getDeltaCrlLink() { return deltaCrlLink; }
-        public int getPosition() { return position; }
-        public int getRelativePosition() { return relativePosition; }
-        private void setRelativePosition(final int relativePosition) { this.relativePosition = relativePosition; }
-    }
-
-    /** Representation of a CA's chain. */
-    public class CaChainAndCrls {
-        private final List<CaAndCrl> casAndCrls = new ArrayList<>();
-        private final String name;
-        private final String subjectDn;
-        private final int caId;
-        
-        public CaChainAndCrls(final String name, final String subjectDn, final int caId) {
+        CaAndCrl(final String name, final String subjectDn, final int caId, final int position, final List<String> chainNames) {
             this.name = name;
             this.subjectDn = subjectDn;
             this.caId = caId;
+            this.position = position;
+            this.chainNames = chainNames;
         }
+        
         public String getName() { return name; }
         public String getSubjectDn() { return subjectDn; }
-        public List<CaAndCrl> getCasAndCrls() { return casAndCrls; }
         public int getCaId() { return caId; }
+        public String getCrlLink() { return crlLink; }
+        public String getDeltaCrlLink() { return deltaCrlLink; }
+        public int getPosition() { return position; }
+        
+        @Override
+        public int hashCode() { return subjectDn.hashCode(); }
+        @Override
+        public boolean equals(final Object obj) { return obj instanceof CaAndCrl && subjectDn.equals(((CaAndCrl)obj).subjectDn); }
     }
-    
+
     private static final long serialVersionUID = 1L;
-    private static final Logger log = Logger.getLogger(RaCasPageBean.class);
+    //private static final Logger log = Logger.getLogger(RaCasPageBean.class);
     private static final String RFC4387_DEFAULT_EJBCA_URL = "/ejbca/publicweb/crls/search.cgi";
+    private static final int NO_CAID_AVAILABLE = 0;
 
     @ManagedProperty(value="#{raMasterApiBean}")
     private RaMasterApiBean raMasterApiBean;
@@ -104,140 +95,81 @@ public class RaCasPageBean implements Serializable {
     @EJB
     private CrlStoreSessionLocal crlSession;
 
-    private List<CaChainAndCrls> caChainsAndCrls = null;;
-    private Map<String,String> caSubjectToNameMap = new HashMap<>();
+    private List<CaAndCrl> casAndCrlItems = null;
+    private boolean atLeastOneCrlLinkPresent = false;
 
-    /** @return a list of all authorized CAs. */
-    public List<CaChainAndCrls> getCaChainsAndCrls() {
-        if (caChainsAndCrls==null) {
-            caChainsAndCrls = new ArrayList<>();
+    /** @return true if at least one of the CAs available via #getCasAndCrlItems() has CRLs present on this system. */
+    public boolean isAtLeastOneCrlLinkPresent() {
+        getCasAndCrlItems();
+        return atLeastOneCrlLinkPresent;
+    }
+
+    /** @return a list of all known authorized CAs with links to CRLs (if present) */
+    public List<CaAndCrl> getCasAndCrlItems() {
+        if (casAndCrlItems==null) {
             final List<CAInfo> caInfos = new ArrayList<>(raMasterApiBean.getAuthorizedCas(raAuthenticationBean.getAuthenticationToken()));
             // First build a mapping of all subjects and their short names
+            final Map<String,String> caSubjectToNameMap = new HashMap<>();
             for (final CAInfo caInfo : caInfos) {
                 caSubjectToNameMap.put(caInfo.getSubjectDN(), caInfo.getName());
             }
+            // Convert all CA's chains into CA objects (use a Set to avoid duplicates)
+            final Set<CaAndCrl> cas = new HashSet<>();
             for (final CAInfo caInfo : caInfos) {
-                if (caInfo.getCertificateChain()!=null && !caInfo.getCertificateChain().isEmpty()) {
-                    caChainsAndCrls.add(getCasAndCrls(caInfo));
+                final List<Certificate> chain = new ArrayList<>(caInfo.getCertificateChain());
+                Collections.reverse(chain);
+                final List<String> chainNames = new ArrayList<>();
+                final int caId = caInfo.getCAId();
+                for (final Certificate caCertificate : chain) {
+                    final String subjectDn = CertTools.getSubjectDN(caCertificate);
+                    String name = caSubjectToNameMap.get(subjectDn);
+                    if (name==null) {
+                        name = subjectDn;
+                    }
+                    chainNames.add(name);
+                    final CaAndCrl caAndCrl = new CaAndCrl(name, subjectDn, chainNames.size()==chain.size()?caId:NO_CAID_AVAILABLE, chainNames.size()-1, new ArrayList<>(chainNames));
+                    // Construct links to RFC4387 CRL Download Servlet
+                    if (caCertificate instanceof X509Certificate) {
+                        final CRLInfo crlInfoFull = crlSession.getLastCRLInfo(subjectDn, false);
+                        if (crlInfoFull!=null) {
+                            atLeastOneCrlLinkPresent = true;
+                            caAndCrl.crlLink = RFC4387_DEFAULT_EJBCA_URL + "?iHash=" + getSubjectPrincipalHashAsUnpaddedBase64((X509Certificate)caCertificate);
+                            final CRLInfo crlInfoDelta = crlSession.getLastCRLInfo(subjectDn, true);
+                            if (crlInfoDelta!=null) {
+                                caAndCrl.deltaCrlLink = RFC4387_DEFAULT_EJBCA_URL + "?iHash=" + getSubjectPrincipalHashAsUnpaddedBase64((X509Certificate)caCertificate) + "&delta=";
+                            }
+                        }
+                    }
+                    // Add missing items and replace items when we know the CAId
+                    if (caAndCrl.getCaId()!=NO_CAID_AVAILABLE) {
+                        cas.remove(caAndCrl);
+                    }
+                    if (!cas.contains(caAndCrl)) {
+                        cas.add(caAndCrl);
+                    }
                 }
             }
-            removeCaChainsCoveredByLargerChains(caChainsAndCrls);
-            sortByHigherLeverCas(caChainsAndCrls);
-            hideAlreadyRenderedHigherLevelCas(caChainsAndCrls);
-            setRelativePositionInFinalList(caChainsAndCrls);
-        }
-        return caChainsAndCrls;
-    }
-    
-    private void removeCaChainsCoveredByLargerChains(final List<CaChainAndCrls> caChainsAndCrls) {
-        // If two CAs share chain, remove the shorter chain 
-        for (final CaChainAndCrls caChainAndCrls1 : new ArrayList<>(caChainsAndCrls)) {
-            for (final CaChainAndCrls caChainAndCrls2 : new ArrayList<>(caChainsAndCrls)) {
-                // Avoid checking itself
-                if (caChainAndCrls1.getCaId()!=caChainAndCrls2.getCaId()) {
-                    final int size1 = caChainAndCrls1.getCasAndCrls().size();
-                    final int size2 = caChainAndCrls2.getCasAndCrls().size();
+            casAndCrlItems = new ArrayList<>(cas);
+            // Sort by higher level CAs
+            Collections.sort(casAndCrlItems, new Comparator<CaAndCrl>() {
+                @Override
+                public int compare(final CaAndCrl caAndCrl1, final CaAndCrl caAndCrl) {
+                    final int size1 = caAndCrl1.chainNames.size();
+                    final int size2 = caAndCrl.chainNames.size();
                     // Avoid checking if chain length is the same
-                    if (size1!=size2) {
-                        boolean shareChain = true;
-                        for (int i=0; i<Math.min(size1, size2); i++) {
-                            if (!caChainAndCrls1.getCasAndCrls().get(i).getSubjectDn().equals(caChainAndCrls2.getCasAndCrls().get(i).getSubjectDn())) {
-                                shareChain = false;
-                                break;
-                            }
-                        }
-                        // If they share the full chain of the shortest, remove the shortest from list of CAs
-                        if (shareChain) {
-                            if (size1<size2) {
-                                caChainsAndCrls.remove(caChainAndCrls1);
-                            } else {
-                                caChainsAndCrls.remove(caChainAndCrls2);
-                            }
+                    for (int i=0; i<Math.min(size1, size2); i++) {
+                        final String name1 = caAndCrl1.chainNames.get(i);
+                        final String name2 = caAndCrl.chainNames.get(i);
+                        final int compareTo = name1.compareTo(name2);
+                        if (compareTo!=0) {
+                            return compareTo;
                         }
                     }
+                    return size1-size2;
                 }
-            }
+            });
         }
-    }
-
-    private void sortByHigherLeverCas(final List<CaChainAndCrls> caChainsAndCrls) {
-        Collections.sort(caChainsAndCrls, new Comparator<CaChainAndCrls>() {
-            @Override
-            public int compare(final CaChainAndCrls caChainAndCrls1, final CaChainAndCrls caChainAndCrls2) {
-                final int size1 = caChainAndCrls1.getCasAndCrls().size();
-                final int size2 = caChainAndCrls2.getCasAndCrls().size();
-                // Avoid checking if chain length is the same
-                for (int i=0; i<Math.min(size1, size2); i++) {
-                    final String name1 = caChainAndCrls1.getCasAndCrls().get(i).getName();
-                    final String name2 = caChainAndCrls2.getCasAndCrls().get(i).getName();
-                    final int compareTo = name1.compareTo(name2);
-                    if (compareTo!=0) {
-                        return compareTo;
-                    }
-                }
-                return 0;   // Unreachable
-            }
-        });
-    }
-
-    /** Mark subsequent occurrences of higher level CAs as non-rendered. */
-    private void hideAlreadyRenderedHigherLevelCas(final List<CaChainAndCrls> sortedCaChainsAndCrls) {
-        List<String> lastNames = new ArrayList<>();
-        for (final CaChainAndCrls caChainAndCrls : sortedCaChainsAndCrls) {
-            final List<CaAndCrl> caAndCrls = caChainAndCrls.getCasAndCrls();
-            for (final CaAndCrl caAndCrl : new ArrayList<>(caAndCrls)) {
-                //log.debug("lastNames=" + Arrays.toString(lastNames.toArray()) + " name=" + caAndCrl.getName() + " pos=" + caAndCrl.getPosition());
-                if (lastNames.size()-1>=caAndCrl.getPosition()) {
-                    if (caAndCrl.getName().equals(lastNames.get(caAndCrl.getPosition()))) {
-                        caAndCrls.remove(caAndCrl);
-                    } else {
-                        lastNames.set(caAndCrl.getPosition(), caAndCrl.getName());
-                        lastNames = new ArrayList<>(lastNames.subList(0, caAndCrl.getPosition()+1));
-                        break;
-                    }
-                } else {
-                    lastNames.add(caAndCrl.getName());
-                }
-            }
-        }
-    }
-
-    private void setRelativePositionInFinalList(List<CaChainAndCrls> sortedCaChainsAndCrls) {
-        int i=0;
-        for (final CaChainAndCrls caChainAndCrls : sortedCaChainsAndCrls) {
-            for (final CaAndCrl caAndCrl : caChainAndCrls.getCasAndCrls()) {
-                caAndCrl.setRelativePosition(i++);
-            }
-        }
-    }
-
-    /** @return a representation of a CA's chain. */
-    private CaChainAndCrls getCasAndCrls(final CAInfo caInfo) {
-        final CaChainAndCrls ret = new CaChainAndCrls(caInfo.getName(), caInfo.getSubjectDN(), caInfo.getCAId());
-        int position = caInfo.getCertificateChain().size()-1;
-        for (final Certificate caCertificate : caInfo.getCertificateChain()) {
-            final String subjectDn = CertTools.getSubjectDN(caCertificate);
-            String name = caSubjectToNameMap.get(subjectDn);
-            if (name==null) {
-                name = subjectDn;
-            }
-            final CaAndCrl caAndCrl = new CaAndCrl(name, subjectDn, position);
-            // Construct links to RFC4387 CRL Download Servlet
-            if (caCertificate instanceof X509Certificate) {
-                final CRLInfo crlInfoFull = crlSession.getLastCRLInfo(subjectDn, false);
-                if (crlInfoFull!=null) {
-                    caAndCrl.crlLink = RFC4387_DEFAULT_EJBCA_URL + "?iHash=" + getSubjectPrincipalHashAsUnpaddedBase64((X509Certificate)caCertificate);
-                    final CRLInfo crlInfoDelta = crlSession.getLastCRLInfo(subjectDn, true);
-                    if (crlInfoDelta!=null) {
-                        caAndCrl.deltaCrlLink = RFC4387_DEFAULT_EJBCA_URL + "?iHash=" + getSubjectPrincipalHashAsUnpaddedBase64((X509Certificate)caCertificate) + "&delta=";
-                    }
-                }
-            }
-            ret.casAndCrls.add(caAndCrl);
-            position--;
-        }
-        Collections.reverse(ret.casAndCrls);
-        return ret;
+        return casAndCrlItems;
     }
 
     /** @return the issuer hash in base64 encoding without padding which is the way RFC4387 search function expects the iHash parameter. */
