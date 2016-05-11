9129c91f2787a0cdd9891416034a3555cfc7a00b
aveen4711 <USER@primekey.com>
2016-05-11 14:56:54 +0000

    ECA-5061: Added key word "Id" and documentations to new class
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@23398 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb/src/org/ejbca/core/model/approval/ApprovalUtils.java
+++ modules/ejbca-ejb/src/org/ejbca/core/model/approval/ApprovalUtils.java
@@ -1,9 +1,27 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.ejbca.core.model.approval;
 
 import org.cesecore.certificates.ca.CAInfo;
 import org.cesecore.certificates.certificateprofile.CertificateProfile;
 import org.ejbca.core.ejb.approval.ApprovalProfileSession;
 
+/**
+ * Tools to handle common approval-related operations.
+ * 
+ * @version $Id$
+ *
+ */
 public class ApprovalUtils {
     
     public static ApprovalProfile[] getApprovalProfiles(final int action, final CAInfo cainfo, final CertificateProfile certProfile, 
