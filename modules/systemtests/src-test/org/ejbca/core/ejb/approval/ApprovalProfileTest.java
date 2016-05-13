f92af9f9c698f017d0a075c34b0232d55703e013
aveen4711 <USER@primekey.com>
2016-05-13 11:21:19 +0000

    ECA-5049: Added system test for adding new approval profiles
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@23434 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/systemtests/src-test/org/ejbca/core/ejb/approval/ApprovalProfileTest.java
@@ -0,0 +1,33 @@
+package org.ejbca.core.ejb.approval;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.cesecore.authentication.tokens.AuthenticationToken;
+import org.cesecore.authentication.tokens.UsernamePrincipal;
+import org.cesecore.mock.authentication.tokens.TestAlwaysAllowLocalAuthenticationToken;
+import org.cesecore.util.EjbRemoteHelper;
+import org.ejbca.core.model.approval.ApprovalProfile;
+import org.ejbca.core.model.approval.ApprovalProfileNumberOfApprovals;
+import org.junit.Test;
+
+public class ApprovalProfileTest {
+
+    private final AuthenticationToken ADMIN = new TestAlwaysAllowLocalAuthenticationToken(new UsernamePrincipal("ApprovalProfileTest"));
+    
+    private ApprovalProfileSessionRemote approvalProfileSession = EjbRemoteHelper.INSTANCE.getRemoteSession(ApprovalProfileSessionRemote.class);
+
+    @Test
+    public void addNrOfApprovalProfile() throws Exception {
+        final String approvalProfileName = "nrOfApprovalProfile";
+        final ApprovalProfile approvalProfile = new ApprovalProfile(approvalProfileName);
+        approvalProfile.setNumberOfApprovals(2);
+        final int id = approvalProfileSession.addApprovalProfile(ADMIN, approvalProfileName, approvalProfile);
+        ApprovalProfile addedApprovalProfile = approvalProfileSession.getApprovalProfile(id);
+        assertNotNull(addedApprovalProfile);
+        assertTrue(addedApprovalProfile.getApprovalProfileType() instanceof ApprovalProfileNumberOfApprovals);
+        assertEquals(2, addedApprovalProfile.getNumberOfApprovals());
+    }
+    
+}
