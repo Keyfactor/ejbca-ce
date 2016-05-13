de0d3c9af58f7c0721ad4c5c7d1eaee206c6303e
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2016-05-13 14:08:10 +0000

    ECA-5006: Fix sorting bug etc. and started working on "view request" page
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@23443 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ra-gui/src/org/ejbca/ra/RaManageRequestBean.java
@@ -0,0 +1,109 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.ra;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+import org.cesecore.authorization.AuthorizationDeniedException;
+import org.cesecore.certificates.ca.CAInfo;
+import org.ejbca.core.model.approval.ApprovalDataVO;
+import org.ejbca.core.model.era.IdNameHashMap;
+import org.ejbca.core.model.era.RaApprovalRequestInfo;
+import org.ejbca.core.model.era.RaMasterApiProxyBeanLocal;
+
+/**
+ * Backing bean for Manage Request page (for individual requests).
+ *  
+ * @see RaManageRequestsBean
+ * @version $Id$
+ */
+@ManagedBean
+@ViewScoped
+public class RaManageRequestBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private static final Logger log = Logger.getLogger(RaManageRequestBean.class);
+    
+    public class ApprovalDataRow {
+        private final String nameText;
+        private final String valueText;
+        
+        public ApprovalDataRow(final String name, final String value) {
+            nameText = name; // TODO
+            valueText = value; // TODO some values might need translation
+        }
+        
+        public String getNameText() { return nameText; }
+        public String getValueText() { return valueText; }
+    }
+    
+    @EJB
+    private RaMasterApiProxyBeanLocal raMasterApiProxyBean;
+
+    @ManagedProperty(value="#{raAuthenticationBean}")
+    private RaAuthenticationBean raAuthenticationBean;
+    public void setRaAuthenticationBean(final RaAuthenticationBean raAuthenticationBean) { this.raAuthenticationBean = raAuthenticationBean; }
+
+    @ManagedProperty(value="#{raLocaleBean}")
+    private RaLocaleBean raLocaleBean;
+    public void setRaLocaleBean(final RaLocaleBean raLocaleBean) { this.raLocaleBean = raLocaleBean; }
+
+    
+    private ApprovalRequestGUIInfo requestInfo;
+    
+    private void initializeRequestInfo() {
+        if (requestInfo == null) {
+            final String idHttpParam = ((HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest())
+                    .getParameter("id");
+            final int id = Integer.parseInt(idHttpParam);
+            final RaApprovalRequestInfo request = raMasterApiProxyBean.getApprovalRequest(raAuthenticationBean.getAuthenticationToken(), id);
+            requestInfo = new ApprovalRequestGUIInfo(request, raLocaleBean);
+            if (requestInfo == null) {
+                throw new IllegalStateException("Request does not exist");
+            }
+        }
+    }
+    
+    public ApprovalRequestGUIInfo getRequest() {
+        initializeRequestInfo();
+        return requestInfo;
+    }
+    
+    public String getPageTitle() {
+        return raLocaleBean.getMessage("view_request_page_title", getRequest().getDisplayName());
+    }
+    
+    public List<ApprovalDataRow> getRequestData() {
+        // TODO
+        return new ArrayList<>();
+    }
+    
+    public void approve() {
+        // TODO
+    }
+    
+    public void reject() {
+        // TODO
+    }
+    
+}
