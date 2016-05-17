0bd624dbd3c7c116da79078b43375c6c3c70cc9b
aveen4711 <USER@primekey.com>
2016-05-17 11:22:05 +0000

    ECA-5049: Improved test for adding approval profiles
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@23469 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/systemtests/src-test/org/ejbca/core/ejb/approval/ApprovalProfileTest.java
+++ modules/systemtests/src-test/org/ejbca/core/ejb/approval/ApprovalProfileTest.java
@@ -21,13 +21,20 @@ public class ApprovalProfileTest {
     @Test
     public void addNrOfApprovalProfile() throws Exception {
         final String approvalProfileName = "nrOfApprovalProfile";
-        final ApprovalProfile approvalProfile = new ApprovalProfile(approvalProfileName);
-        approvalProfile.setNumberOfApprovals(2);
-        final int id = approvalProfileSession.addApprovalProfile(ADMIN, approvalProfileName, approvalProfile);
-        ApprovalProfile addedApprovalProfile = approvalProfileSession.getApprovalProfile(id);
-        assertNotNull(addedApprovalProfile);
-        assertTrue(addedApprovalProfile.getApprovalProfileType() instanceof ApprovalProfileNumberOfApprovals);
-        assertEquals(2, addedApprovalProfile.getNumberOfApprovals());
+        if(approvalProfileSession.getApprovalProfile(approvalProfileName)!=null) {
+            approvalProfileSession.removeApprovalProfile(ADMIN, approvalProfileName);
+        }
+        try {
+            final ApprovalProfile approvalProfile = new ApprovalProfile(approvalProfileName);
+            approvalProfile.setNumberOfApprovals(2);
+            final int id = approvalProfileSession.addApprovalProfile(ADMIN, approvalProfileName, approvalProfile);
+            ApprovalProfile addedApprovalProfile = approvalProfileSession.getApprovalProfile(id);
+            assertNotNull(addedApprovalProfile);
+            assertTrue(addedApprovalProfile.getApprovalProfileType() instanceof ApprovalProfileNumberOfApprovals);
+            assertEquals(2, addedApprovalProfile.getNumberOfApprovals());
+        } finally {
+            approvalProfileSession.removeApprovalProfile(ADMIN, approvalProfileName);
+        }
     }
     
 }
