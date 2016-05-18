9074d2509f9cd7b535292793fcf3eb16d22f1e98
Mike Agrenius Kushner <mike.kushner@primekey.com>
2016-05-18 12:21:06 +0000

    ECA-5082: Approval requests created prior to 6.6.0 can now be approved on a modern system
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@23486 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/upgrade/650_660/650_660-upgrade-db2.sql
+++ src/upgrade/650_660/650_660-upgrade-db2.sql
@@ -1,3 +1,5 @@
 -- These columns are added by the JPA provider if there are sufficient privileges
 -- ALTER TABLE CertificateData ADD endEntityProfileId INTEGER;
 -- CALL SYSPROC.ADMIN_CMD('REORG TABLE CertificateData');
+-- ALTER TABLE ApprovalData ADD approvalprofileid INTEGER DEFAULT 0;
+
