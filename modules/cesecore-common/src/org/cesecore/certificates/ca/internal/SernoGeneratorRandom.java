6d2b1241c6a5e4b6e4e969d71b5ee897eb654dba
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2016-05-25 12:22:49 +0000

    EJBCAINTER-115: use default (secure) seeding of SecureRandom for certificate serial numbers. Make it configurable to use the OS default random, which on Linux means /dev/(u)random whic is very secure.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@23545 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-common/src/org/cesecore/certificates/ca/internal/SernoGeneratorRandom.java
+++ modules/cesecore-common/src/org/cesecore/certificates/ca/internal/SernoGeneratorRandom.java
@@ -16,9 +16,9 @@ import java.math.BigInteger;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.Random;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.cesecore.config.CesecoreConfiguration;
 import org.cesecore.internal.InternalResources;
@@ -89,46 +89,25 @@ public class SernoGeneratorRandom implements SernoGenerator {
     }
 
     private void init() {
-        // Init random number generator for random serial numbers
+        // Init random number generator for random serial numbers. 
+        // SecureRandom provides a cryptographically strong random number generator (RNG).
         try {
-            random = SecureRandom.getInstance(algorithm);
+            // Use a specified algorithm if ca.rngalgorithm is provided and it's not set to default
+            if (!StringUtils.isEmpty(algorithm) && !StringUtils.equalsIgnoreCase(algorithm, "default")) {
+                random = SecureRandom.getInstance(algorithm);
+                log.info("Using "+algorithm+" serialNumber RNG algorithm.");
+            } else {
+                // If no algorithm is specified use the default, which may differ between different OS:es. 
+                // On Linux the default Java implementation uses the (secure) /dev/(u)random, but on windows something else
+                random = new SecureRandom();
+                log.info("Using default serialNumber RNG algorithm.");
+            }
         } catch (NoSuchAlgorithmException e) {
             //This state is unrecoverable, and since algorithm is set in configuration requires a redeploy to handle
             throw new IllegalStateException("Algorithm " + algorithm + " was not a valid algorithm.", e);
         }
-
-        // Using this seed we should get a different seed every time.
-        // We are not concerned about the security of the random bits, only that
-        // they are different every time.
-        // Extracting 64 bit random numbers out of this should give us 2^32 (4
-        // 294 967 296) serialnumbers before
-        // collisions (which are seriously BAD), well anyhow sufficient for
-        // pretty large scale installations.
-        // Design criteria: 1. No counter to keep track on. 2. Multiple threads
-        // can generate numbers at once, in
-        // a clustered environment etc.
-        long seed = Math.abs((new Date().getTime()) + this.hashCode());
-        random.setSeed(seed);
-
-        /*
-         * Another possibility is to use SecureRandom's default seeding which is
-         * designed to be secure: <p>The seed is produced by counting the
-         * number of times the VM manages to loop in a given period. This number
-         * roughly reflects the machine load at that point in time. The samples
-         * are translated using a permutation (s-box) and then XORed together.
-         * This process is non linear and should prevent the samples from
-         * "averaging out". The s-box was designed to have even statistical
-         * distribution; it's specific values are not crucial for the security
-         * of the seed. We also create a number of sleeper threads which add
-         * entropy to the system by keeping the scheduler busy. Twenty such
-         * samples should give us roughly 160 bits of randomness. <P> These
-         * values are gathered in the background by a daemon thread thus
-         * allowing the system to continue performing it's different activities,
-         * which in turn add entropy to the random seed. <p> The class also
-         * gathers miscellaneous system information, some machine dependent,
-         * some not. This information is then hashed together with the 20 seed
-         * bytes.
-         */
+        // Call nextBytes directly after in order to force seeding (secure) if not already done
+        random.nextBytes(new byte[20]);
     }
 
     /**
