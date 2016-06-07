5606a6b1217c58e14f26d2e2efa107ca71bcd2bf
Johan Eklund <johan.eklund@primekey.com>
2016-06-07 13:05:10 +0000

    ECA-5106: Set limit of new column CertificateData.subjectAltName to length 2000 (in order to keep theoretical MySQL NDB support alive).
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@23617 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/upgrade/650_660/650_660-upgrade-informix.sql
+++ src/upgrade/650_660/650_660-upgrade-informix.sql
@@ -1,5 +1,5 @@
 -- These columns are added by the JPA provider if there are sufficient privileges
 -- ALTER TABLE CertificateData ADD notBefore DECIMAL(18,0);
 -- ALTER TABLE CertificateData ADD endEntityProfileId INTEGER;
--- ALTER TABLE CertificateData ADD subjectAltName LVARCHAR(4000);
+-- ALTER TABLE CertificateData ADD subjectAltName LVARCHAR(2000);
 -- ALTER TABLE ApprovalData ADD approvalprofileid INTEGER DEFAULT 0;
