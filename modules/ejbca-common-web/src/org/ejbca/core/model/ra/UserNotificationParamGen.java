7dd5ceb9e0d9bc871e0166076b0f2e0c14a97edf
Johan Eklund <johan.eklund@primekey.com>
2016-06-17 13:22:03 +0000

    ECA-5159: Trigger end entity revocation notification also when a single EE certificate has been revoked without EE status change.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@23689 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-common-web/src/org/ejbca/core/model/ra/UserNotificationParamGen.java
+++ modules/ejbca-common-web/src/org/ejbca/core/model/ra/UserNotificationParamGen.java
@@ -14,7 +14,12 @@
 package org.ejbca.core.model.ra;
 
 import java.security.cert.Certificate;
+import java.util.Date;
 
+import org.cesecore.certificates.certificate.CertificateConstants;
+import org.cesecore.certificates.certificate.CertificateData;
+import org.cesecore.certificates.certificate.CertificateDataWrapper;
+import org.cesecore.certificates.crl.RevocationReasons;
 import org.cesecore.certificates.endentity.EndEntityInformation;
 import org.cesecore.certificates.util.DNFieldExtractor;
 import org.cesecore.util.CertTools;
@@ -54,13 +59,54 @@ public class UserNotificationParamGen extends ApprovalNotificationParamGen {
 		populateWithExpiringCert(expiringCert);
 	}
 
-	public UserNotificationParamGen(EndEntityInformation userData, String approvalAdminDN, EndEntityInformation admin) {
+	public UserNotificationParamGen(EndEntityInformation userData, String approvalAdminDN, EndEntityInformation admin, CertificateDataWrapper revokedCertificate) {
 		populateWithUserData(userData);
 		populateWithApprovalAdminDN(approvalAdminDN);
 		populateWithEmailAddresses(userData, admin);
+		populateWithRevokedCertificate(revokedCertificate);
 	}
 
-	protected void populateWithExpiringCert(Certificate expiringCert) {
+	protected void populateWithRevokedCertificate(final CertificateDataWrapper revokedCertificate) {
+	    if (revokedCertificate!=null) {
+	        final CertificateData certificateData = revokedCertificate.getCertificateData();
+	        if (certificateData != null) {
+	            paramPut("revokedCertificate.CERTSERIAL", certificateData.getSerialNumber());
+	            paramPut("revokedCertificate.EXPIREDATE", fastDateFormat(new Date(certificateData.getExpireDate())));
+	            paramPut("revokedCertificate.CERTSUBJECTDN", certificateData.getSubjectDN());
+	            paramPut("revokedCertificate.CERTISSUERDN", certificateData.getIssuerDN());
+	            String newStatus = "Unknown";
+	            switch (certificateData.getStatus()) {
+	            case CertificateConstants.CERT_ACTIVE:
+                case CertificateConstants.CERT_NOTIFIEDABOUTEXPIRATION:
+                    newStatus = "Active";
+                    break;
+                case CertificateConstants.CERT_REVOKED:
+                case CertificateConstants.CERT_ARCHIVED:
+                    newStatus = "Revoked";
+                    break;
+	            }
+                String newRevocationReason = "";
+                // Use plain RFC5280 CRLReason naming until we localize this
+                switch (RevocationReasons.getFromDatabaseValue(certificateData.getRevocationReason())) {
+                case UNSPECIFIED: newRevocationReason = "unspecified"; break;
+                case KEYCOMPROMISE: newRevocationReason = "keyCompromise"; break;
+                case CACOMPROMISE: newRevocationReason = "cACompromise"; break;
+                case AFFILIATIONCHANGED: newRevocationReason = "affiliationChanged"; break;
+                case SUPERSEDED: newRevocationReason = "superseded"; break;
+                case CESSATIONOFOPERATION: newRevocationReason = "cessationOfOperation"; break;
+                case CERTIFICATEHOLD: newRevocationReason = "certificateHold"; break;
+                case REMOVEFROMCRL: newRevocationReason = "removeFromCRL"; break;
+                case PRIVILEGESWITHDRAWN: newRevocationReason = "privilegeWithdrawn"; break;
+                case AACOMPROMISE: newRevocationReason = "aACompromise"; break;
+                default: break;
+                }
+                paramPut("revokedCertificate.REVOCATIONSTATUS", newStatus);
+                paramPut("revokedCertificate.REVOCATIONREASON", newRevocationReason);
+	        }
+	    }
+    }
+
+    protected void populateWithExpiringCert(Certificate expiringCert) {
 		if(expiringCert != null){
 			paramPut("expiringCert.CERTSERIAL",CertTools.getSerialNumberAsString(expiringCert));
 			paramPut("expiringCert.EXPIREDATE", fastDateFormat(CertTools.getNotAfter(expiringCert)));
