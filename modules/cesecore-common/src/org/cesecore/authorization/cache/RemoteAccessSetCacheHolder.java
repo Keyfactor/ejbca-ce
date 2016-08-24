aedcdc986ce122f0dc859159e0b6723586fcba19
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2016-08-24 08:57:45 +0000

    Noticket: improve debug logging
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@24226 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-common/src/org/cesecore/authorization/cache/RemoteAccessSetCacheHolder.java
+++ modules/cesecore-common/src/org/cesecore/authorization/cache/RemoteAccessSetCacheHolder.java
@@ -67,6 +67,7 @@ public final class RemoteAccessSetCacheHolder {
                 }
                 lastUpdate = updateNumber;
                 regularUpdateInProgress = true;
+                log.debug("Started cache reload");
             } else if (regularUpdateInProgress) {
                 log.trace("<startCacheReload (regular update was in progress)");
                 return null;
@@ -86,7 +87,7 @@ public final class RemoteAccessSetCacheHolder {
                 return;
             }
         } else if (regularUpdateInProgress) {
-            log.trace("<finishCacheReload");
+            log.trace("<finishCacheReload (not updating because regularUpdateInProgress)");
             return;
         }
         
@@ -106,8 +107,8 @@ public final class RemoteAccessSetCacheHolder {
                 log.trace("<finishCacheReload (regular update was in progress)");
                 return;
             }
-            log.debug("Replacing access set cache");
             cache = newCache;
+            log.debug("Replaced access set cache");
         }
         log.trace("<finishCacheReload");
     }
