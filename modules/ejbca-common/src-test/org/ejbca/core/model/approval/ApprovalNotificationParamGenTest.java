5029bb88535c5640190b4f67d0371b2c0c753285
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2016-09-28 12:10:34 +0000

    ECA-5410: add junit test and some javadoc for ApprovalNotificationParameterGenerator
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@24431 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ejbca-common/src-test/org/ejbca/core/model/approval/ApprovalNotificationParamGenTest.java
@@ -0,0 +1,27 @@
+package org.ejbca.core.model.approval;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+/** Tests some substitution variables for approval profile notifications
+ * @version $Id
+ */
+public class ApprovalNotificationParamGenTest {
+
+	@Test
+	public void testInterpolate(){
+        final ApprovalNotificationParameterGenerator paramGen = new ApprovalNotificationParameterGenerator(123, 3, 47,
+                "Approval Step 1", ApprovalDataVO.APPROVALTYPENAMES[0], "approved", "CN=requestor", "CN=Last approved by");
+        assertNotNull("paramGen is null", paramGen);
+        
+        String msg = paramGen.interpolate("${approvalRequest.ID} ${approvalRequest.STEP_ID} ${approvalRequest.PARTITION_ID} ${approvalRequest.PARTITION_NAME} ${approvalRequest.TYPE} ${approvalRequest.WORKFLOWSTATE} ${approvalRequest.REQUESTOR} ${approvalRequest.APPROVALADMIN}");
+        assertFalse("Interpolating message failed", (msg==null || msg.length()==0));
+        assertEquals("123 3 47 Approval Step 1 APDUMMY approved CN=requestor CN=Last approved by", msg);
+		
+	}
+	
+
+}
