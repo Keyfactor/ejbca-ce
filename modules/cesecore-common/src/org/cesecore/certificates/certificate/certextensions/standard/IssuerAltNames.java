fe1dc25f38e05bb5dce4be0ab0659e2cb8d57910
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2016-10-18 15:32:48 +0000

    ECA-5387: Add IAN when creating a new Root CAs also, not just during renewal
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@24542 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-common/src/org/cesecore/certificates/certificate/certextensions/standard/IssuerAltNames.java
+++ modules/cesecore-common/src/org/cesecore/certificates/certificate/certextensions/standard/IssuerAltNames.java
@@ -14,12 +14,15 @@ package org.cesecore.certificates.certificate.certextensions.standard;
 
 import java.security.PublicKey;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.bouncycastle.asn1.ASN1Encodable;
 import org.bouncycastle.asn1.x509.Extension;
 import org.bouncycastle.asn1.x509.GeneralNames;
 import org.cesecore.certificates.ca.CA;
+import org.cesecore.certificates.ca.X509CAInfo;
 import org.cesecore.certificates.ca.internal.CertificateValidity;
+import org.cesecore.certificates.certificate.CertificateConstants;
 import org.cesecore.certificates.certificateprofile.CertificateProfile;
 import org.cesecore.certificates.endentity.EndEntityInformation;
 import org.cesecore.util.CertTools;
@@ -44,8 +47,25 @@ public class IssuerAltNames extends StandardCertificateExtension {
     public ASN1Encodable getValue(final EndEntityInformation subject, final CA ca, final CertificateProfile certProfile,
             final PublicKey userPublicKey, final PublicKey caPublicKey, CertificateValidity val) {
         GeneralNames ret = null;
-        String altName = CertTools.getSubjectAlternativeName(ca.getCACertificate()); //subject.getSubjectAltName(); 
-        if ( (altName != null) && (altName.length() > 0) ) {
+        String altName = null;
+        if (ca.getCACertificate() != null) {
+            altName = CertTools.getSubjectAlternativeName(ca.getCACertificate());
+        } else {
+            // If we have a new Root CA (not renewing), we go here
+            if (certProfile.getType() == CertificateConstants.CERTTYPE_ROOTCA && ca.getCAInfo() instanceof X509CAInfo) {
+                altName = ((X509CAInfo)ca.getCAInfo()).getSubjectAltName();
+                if (StringUtils.isNotEmpty(altName) && certProfile.getUseSubjectAltNameSubSet()) {
+                    altName = certProfile.createSubjectAltNameSubSet(altName);
+                }
+                if (log.isDebugEnabled()) {
+                    log.debug("Using the SAN value as the IssuerAltName value for Root CA. SAN='" + altName + "'");
+                }
+            } else if (log.isDebugEnabled()) {
+                // This should never happen
+                log.debug("Missing CA certificate in CA " + ca.getCAId() + ", and subject is not an X509 Root CA. Will not add IssuerAltName extension");
+            }
+        }
+        if (StringUtils.isNotEmpty(altName)) {
             ret = CertTools.getGeneralNamesFromAltName(altName);
         }
         if (ret == null) {
