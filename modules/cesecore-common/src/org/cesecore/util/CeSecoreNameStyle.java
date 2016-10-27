64fc8d059b46b4fd5d5e411e0f65ba2a2f6dcd83
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2016-10-27 12:21:42 +0000

    Noticket: javadoc
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@24589 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-common/src/org/cesecore/util/CeSecoreNameStyle.java
+++ modules/cesecore-common/src/org/cesecore/util/CeSecoreNameStyle.java
@@ -25,7 +25,9 @@ import org.bouncycastle.asn1.x500.X500NameStyle;
 import org.bouncycastle.asn1.x500.style.BCStyle;
 import org.bouncycastle.asn1.x500.style.IETFUtils;
 
-/**
+/** Class that determines string representations of DNs. Overrides the default BCStyle in order to:
+ * - Be consistent for all future for backwards compatibility (serialnumber)
+ * - Be able to add fields that does not (yet) exist in BC (like CABForum Jurisdiction* in BC 1.54)
  *
  * @version $Id$
  *
