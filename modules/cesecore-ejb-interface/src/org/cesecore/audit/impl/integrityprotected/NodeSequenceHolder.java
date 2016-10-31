9d70b094d63b6fafd005970d0dd38298819cf277
Johan Eklund <johan.eklund@primekey.com>
2016-10-31 11:34:40 +0000

    ECA-5451: Cache node identifier that the returned log sequence numbers are for. Use initialization call-back to read node Id and database lookup to keep NodeSequenceHolder free of CRUD dependencies.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@24598 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-ejb-interface/src/org/cesecore/audit/impl/integrityprotected/NodeSequenceHolder.java
+++ modules/cesecore-ejb-interface/src/org/cesecore/audit/impl/integrityprotected/NodeSequenceHolder.java
@@ -15,13 +15,11 @@ package org.cesecore.audit.impl.integrityprotected;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.ReentrantLock;
 
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
-
-import org.cesecore.util.QueryResultWrapper;
-
 /**
  * Get this node's next log row sequence number.
+ * 
+ * The sequence number is guaranteed to be unique as long as the read "node identifier"
+ * read on first access is unique among the nodes sharing the database.
  * 
  * @version $Id$
  */
@@ -33,19 +31,26 @@ public enum NodeSequenceHolder {
 
     private final AtomicLong lastSequenceNumberAtomic = new AtomicLong(-1);
     private final ReentrantLock lockDataBaseUpdate = new ReentrantLock();
+    private String nodeId = null;
+    
+    /** Interface for callback of methods that is invoked once. */
+    public interface OnInitCallBack {
+        /** @return the current node identifier */
+        String getNodeId();
+        /** @return the highest known sequence number for the node identifier returned by {@link OnInitCallBack#getNodeId()}*/
+        long getMaxSequenceNumberForNode(String nodeId);
+    }
 
     /** @return the node's next log row sequence number. */
-    public long getNext(final EntityManager entityManager, final String nodeId) {
+    public long getNext(final OnInitCallBack callBack) {
         if (lastSequenceNumberAtomic.get()==-1L) {
             try {
-                // Lock threads during initializations of value from the database (only one thread needs to do the lookup)
+                // Lock threads during initializations of value from the database (only one thread needs to/should do the lookup)
                 lockDataBaseUpdate.lock();
                 if (lastSequenceNumberAtomic.get()==-1L) {
-                    // Get the latest sequenceNumber from last run from the database..
-                    final Query query = entityManager.createQuery("SELECT MAX(a.sequenceNumber) FROM AuditRecordData a WHERE a.nodeId=:nodeId");
-                    query.setParameter("nodeId", nodeId);
-                    final long value = QueryResultWrapper.getSingleResult(query, Long.valueOf(-1)).longValue();
-                    lastSequenceNumberAtomic.set(value);
+                    // Note that it very important that the nodeId is cached to avoid gaps in the sequence if the nodeId changes
+                    nodeId = callBack.getNodeId();
+                    lastSequenceNumberAtomic.set(callBack.getMaxSequenceNumberForNode(nodeId));
                 }
             } finally {
                 lockDataBaseUpdate.unlock();
@@ -54,6 +59,16 @@ public enum NodeSequenceHolder {
         return lastSequenceNumberAtomic.incrementAndGet();
     }
 
+    /** @return the Node Identifier that this sequence number applies to. */
+    public String getNodeId() {
+        return nodeId;
+    }
+
+    /**
+     * Trigger a re-read of the nodeId and last sequence number for the node on next call to {@link NodeSequenceHolder#getNext(OnInitCallBack)}.
+     * 
+     * WARNING: This class is NOT safe to reset in an concurrent environment and this method should only be used from unit tests.
+     */
     public void reset() {
         lastSequenceNumberAtomic.set(-1L);
     }
