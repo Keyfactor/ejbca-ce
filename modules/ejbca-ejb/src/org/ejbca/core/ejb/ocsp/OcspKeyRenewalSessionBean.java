c35b6095e6f5b891ec8ff7b28ce354c838347763
Johan Eklund <johan.eklund@primekey.com>
2016-11-10 09:48:54 +0000

    ECA-5502: Prevent legacy OCSP signer renewal from processing the same entry twice.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@24673 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb/src/org/ejbca/core/ejb/ocsp/OcspKeyRenewalSessionBean.java
+++ modules/ejbca-ejb/src/org/ejbca/core/ejb/ocsp/OcspKeyRenewalSessionBean.java
@@ -30,9 +30,11 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.Set;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
@@ -174,9 +176,19 @@ public class OcspKeyRenewalSessionBean implements OcspKeyRenewalSessionLocal, Oc
             }
             final StringBuffer matched = new StringBuffer();
             final StringBuffer unMatched = new StringBuffer();
+            final Set<Integer> processedOcspKeyBindingIds = new HashSet<Integer>();
             for (final OcspSigningCacheEntry ocspSigningCacheEntry : OcspSigningCache.INSTANCE.getEntries()) {
                 // Only perform renewal for non CA signing key OCSP signers
                 if (!ocspSigningCacheEntry.isUsingSeparateOcspSigningCertificate()) {
+                    continue;
+                }
+                // Only perform renewal once for each OcspKeyBinding.
+                // (the cache can map each OcspKeyBinding multiple times by SHA-1, SHA-256 and as default responder)
+                final int ocspKeyBindingId = ocspSigningCacheEntry.getOcspKeyBinding().getId();
+                if (!processedOcspKeyBindingIds.add(Integer.valueOf(ocspKeyBindingId))) {
+                    if (log.isDebugEnabled()) {
+                        log.debug("Skipping renewal processing of OcspKeyBinding with id " + ocspKeyBindingId + " that was already processed.");
+                    }
                     continue;
                 }
                 final X509Certificate ocspSigningCertificate = ocspSigningCacheEntry.getOcspSigningCertificate();
