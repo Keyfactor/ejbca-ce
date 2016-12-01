a41fbc773111c1a11a76c6ad16c18552781efce3
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2016-12-01 10:01:02 +0000

    Noticket: some more comments in the JUnit test with pointers to eIDAS standards
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@24813 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-common/src-test/org/cesecore/certificates/certificate/certextensions/QcStatementTest.java
+++ modules/cesecore-common/src-test/org/cesecore/certificates/certificate/certextensions/QcStatementTest.java
@@ -27,6 +27,7 @@ import org.bouncycastle.asn1.ASN1Sequence;
 import org.bouncycastle.asn1.util.ASN1Dump;
 import org.bouncycastle.asn1.x509.qualified.ETSIQCObjectIdentifiers;
 import org.bouncycastle.asn1.x509.qualified.QCStatement;
+import org.bouncycastle.asn1.x509.qualified.RFC3739QCObjectIdentifiers;
 import org.bouncycastle.util.encoders.Hex;
 import org.cesecore.certificates.certificate.certextensions.standard.QcStatement;
 import org.cesecore.certificates.certificateprofile.CertificateProfile;
@@ -47,17 +48,24 @@ public class QcStatementTest {
         prof.setUseQCStatement(true);
         prof.setUseQCEtsiQCCompliance(true);
         prof.setUseQCEtsiSignatureDevice(true);
+        // QC Etsi type, eSign, from eIDAS EN 319 412-5
         prof.setQCEtsiType("0.4.0.1862.1.6.1");
         prof.setQCEtsiPds(Arrays.asList(new PKIDisclosureStatement("http://qcs.localhost/QcPDS", "en")));
+        // id-qcs-pkixQCSyntax-v2 (OID = 1.3.6.1.55.7.11.2) with SematicsID = 0.4.0.194121.1.2 
+        // SemanticsID = "Legal Person", according to eIDAS EN 319 412-1
+        prof.setUsePkixQCSyntaxV2(true);
+        prof.setQCSemanticsId("0.4.0.194121.1.2");
         QcStatement statement = new QcStatement();
         byte[] value = statement.getValueEncoded(null, null, prof, null, null, null);
         @SuppressWarnings("resource")
         final String dump = ASN1Dump.dumpAsString(new ASN1InputStream(value).readObject(), true);
         log.info(dump);
         // Hex dump can be used in Custom Certificate Extensions
-        log.info(new String(Hex.encode(value)));
-        // Dump included IDs
+        final String hexEncodedQcStatements = new String(Hex.encode(value)); 
+        log.info(hexEncodedQcStatements);
+        // Parsing the Hex encoded QCStatements that we creted above, will it parse?
         final ASN1Sequence seq = (ASN1Sequence) ASN1Sequence.fromByteArray(value);
+        // Dump included IDs
         // This is just a loop to get all the statement IDs in the QcStatements extension, so we can view them and count them
         ArrayList<String> oids = new ArrayList<>();
         for (int i = 0; i < seq.size(); i++) {
@@ -71,9 +79,10 @@ public class QcStatementTest {
         }
         log.info(oids);
         // Check that all OIDs we set exist
-        assertEquals("Not all QC statement Ids were included", 4, oids.size());
+        assertEquals("Not all QC statement Ids were included", 5, oids.size());
         assertTrue(oids.contains(ETSIQCObjectIdentifiers.id_etsi_qcs_QcCompliance.getId()));
         assertTrue(oids.contains(ETSIQCObjectIdentifiers.id_etsi_qcs_QcSSCD.getId()));
+        assertTrue(oids.contains(RFC3739QCObjectIdentifiers.id_qcs_pkixQCSyntax_v2.getId()));
         assertTrue(oids.contains("0.4.0.1862.1.6")); // ETSIQCObjectIdentifiers.id_etsi_qcs_QcType
         assertTrue(oids.contains("0.4.0.1862.1.5")); // ETSIQCObjectIdentifiers.id_etsi_qcs_QcPds
         // Check the values we set
