22af3647789cd1da861e70ae4b53e57f88d8a9cd
Mike Agrenius Kushner <mike.kushner@primekey.com>
2016-12-06 19:26:22 +0000

    ECA-5374: Moved ApprovalSession.query* methods into local and created a test proxy
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@24848 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/systemtests/src/org/ejbca/core/ejb/approval/ApprovalSessionProxyBean.java
@@ -0,0 +1,53 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.core.ejb.approval;
+
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+
+import org.cesecore.jndi.JndiConstants;
+import org.ejbca.core.ejb.approval.ApprovalSessionLocal;
+import org.ejbca.core.ejb.approval.ApprovalSessionProxyRemote;
+import org.ejbca.core.model.approval.ApprovalDataVO;
+import org.ejbca.util.query.IllegalQueryException;
+import org.ejbca.util.query.Query;
+
+/**
+ * @version $Id$
+ *
+ */
+@Stateless(mappedName = JndiConstants.APP_JNDI_PREFIX + "ApprovalSessionProxyRemote")
+@TransactionAttribute(TransactionAttributeType.SUPPORTS)
+public class ApprovalSessionProxyBean implements ApprovalSessionProxyRemote {
+
+    @EJB
+    private ApprovalSessionLocal approvalSession;
+
+    @Override
+    public List<ApprovalDataVO> query(Query query, int index, int numberofrows, String caAuthorizationString,
+            String endEntityProfileAuthorizationString) throws IllegalQueryException {
+        return approvalSession.query(query, index, numberofrows, caAuthorizationString, endEntityProfileAuthorizationString);
+    }
+
+    @Override
+    public List<ApprovalDataVO> queryByStatus(boolean includeUnfinished, boolean includeProcessed, int index, int numberofrows,
+            String caAuthorizationString, String endEntityProfileAuthorizationString) {
+        return approvalSession.queryByStatus(includeUnfinished, includeProcessed, index, numberofrows, caAuthorizationString,
+                endEntityProfileAuthorizationString);
+    }
+
+}
