07aad9d924ff3cdc6a6e4d69d08f5b8a0a2f9aab
Johan Eklund <johan.eklund@primekey.com>
2016-12-12 11:39:11 +0000

    ECA-5568: Use VARCHAR2 instead of VARCHAR is upgrade script for Oracle.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@24878 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/upgrade/650_660/650_660-upgrade-oracle.sql
+++ src/upgrade/650_660/650_660-upgrade-oracle.sql
@@ -9,6 +9,6 @@
 --
 -- subjectDN and subjectAltName columns in UserData has been extended to accommodate longer names
 -- subjectDN from 250 to 400 characters and subjectAltName from 250 to 2000 characters
--- ALTER TABLE UserData MODIFY subjectAltName VARCHAR(2000 byte);
--- ALTER TABLE UserData MODIFY subjectDN VARCHAR(400 byte);
--- ALTER TABLE CertificateData MODIFY subjectDN VARCHAR(400 byte);
+-- ALTER TABLE UserData MODIFY subjectAltName VARCHAR2(2000 byte);
+-- ALTER TABLE UserData MODIFY subjectDN VARCHAR2(400 byte);
+-- ALTER TABLE CertificateData MODIFY subjectDN VARCHAR2(400 byte);
