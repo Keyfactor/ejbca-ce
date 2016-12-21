b98d6b29f58b22024ca7f7edc8825119dc41ce77
Mike Agrenius Kushner <mike.kushner@primekey.com>
2016-12-21 15:04:09 +0000

    ECA-5284: Disabled the save button in approvals for the requesting admin, who by definition can't approve their own request.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@24945 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/admin-gui/src/org/ejbca/ui/web/admin/approval/ApproveActionManagedBean.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/approval/ApproveActionManagedBean.java
@@ -465,7 +465,8 @@ public class ApproveActionManagedBean extends BaseManagedBean {
      * @return true if the current admin has access to approve any partitions at all
      */
     public boolean canApproveAnyPartitions() {
-        return !partitionsAuthorizedToApprove.isEmpty();
+        ApprovalDataVO approvalDataVO = approvalSession.findNonExpiredApprovalRequest(getAdmin(), approvalDataVOView.getApprovalId());
+        return !partitionsAuthorizedToApprove.isEmpty() && !approvalDataVO.getApprovalRequest().getRequestAdmin().equals(getAdmin());
     }
     
     /**
