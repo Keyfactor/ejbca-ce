e808b9398852988f49f9a3d0ac84084417571ae5
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2017-01-18 17:35:57 +0000

    EJBCAINTER-107: add filter to always set headers for no caching
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@25043 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/NoCacheFilter.java
@@ -0,0 +1,76 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.ui.web.admin;
+
+import java.io.IOException;
+
+import javax.faces.application.ResourceHandler;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Filter that prevents a browser to cache everything except resources like java script, css and images.
+ * It's part of JSF best practices.
+ * 
+ * @version $Id: NoCacheFilter.java 25035 2017-01-18 17:11:37Z anatom $
+ */
+@WebFilter(filterName = "NoCacheFilter", urlPatterns = {"/*"})
+public class NoCacheFilter implements Filter {
+
+    private static final Logger log = Logger.getLogger(NoCacheFilter.class);
+
+    @Override
+    public void destroy() {
+    }
+
+    /**
+     * Returns true for the request URIs that should be cached, false otherwise
+     * @param request
+     * @return
+     */
+    private boolean cacheURI(HttpServletRequest request) {
+        String requestUri = request.getRequestURI();
+        return requestUri.startsWith(request.getContextPath() + ResourceHandler.RESOURCE_IDENTIFIER)
+                || requestUri.startsWith(request.getContextPath() + "/themes") || requestUri.startsWith(request.getContextPath() + "/js")
+                || requestUri.startsWith(request.getContextPath() + "/images") || requestUri.endsWith(".js");
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        if (!cacheURI(request)) {
+            response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+            response.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+            response.setDateHeader("Expires", 0); // Proxies.
+        }
+
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void init(FilterConfig arg0) throws ServletException {
+        log.debug("NoCacheFilter: init");
+    }
+
+}
