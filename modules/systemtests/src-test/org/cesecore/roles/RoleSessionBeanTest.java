c763f6f1506e9db27f271c34252d1863bc64cf47
Johan Eklund <johan.eklund@primekey.com>
2017-01-18 12:26:02 +0000

    ECA-5629: Remove unused import.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@25030 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/systemtests/src-test/org/cesecore/roles/RoleSessionBeanTest.java
+++ modules/systemtests/src-test/org/cesecore/roles/RoleSessionBeanTest.java
@@ -15,7 +15,6 @@ package org.cesecore.roles;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import org.apache.log4j.Logger;
