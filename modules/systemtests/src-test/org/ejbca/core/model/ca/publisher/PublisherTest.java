5a40909c0832a1c1911a43189bacc8678dad036b
Johan Eklund <johan.eklund@primekey.com>
2017-03-13 22:08:11 +0000

    ECA-5746: Setup both old and new authorization for system tests.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@25459 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/systemtests/src-test/org/ejbca/core/model/ca/publisher/PublisherTest.java
+++ modules/systemtests/src-test/org/ejbca/core/model/ca/publisher/PublisherTest.java
@@ -16,39 +16,27 @@ package org.ejbca.core.model.ca.publisher;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.security.Principal;
 import java.security.cert.CRLException;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import javax.security.auth.x500.X500Principal;
-
 import org.apache.log4j.Logger;
-import org.cesecore.authentication.tokens.AuthenticationSubject;
+import org.cesecore.RoleUsingTestCase;
 import org.cesecore.authentication.tokens.AuthenticationToken;
-import org.cesecore.authentication.tokens.UsernamePrincipal;
 import org.cesecore.authorization.AuthorizationDeniedException;
 import org.cesecore.authorization.control.StandardRules;
-import org.cesecore.authorization.rules.AccessRuleData;
-import org.cesecore.authorization.rules.AccessRuleState;
-import org.cesecore.authorization.user.AccessMatchType;
-import org.cesecore.authorization.user.AccessUserAspectData;
-import org.cesecore.authorization.user.matchvalues.X500PrincipalAccessMatchValue;
 import org.cesecore.certificates.certificate.CertificateConstants;
 import org.cesecore.certificates.certificate.CertificateData;
 import org.cesecore.certificates.certificate.CertificateDataWrapper;
 import org.cesecore.certificates.certificateprofile.CertificateProfileConstants;
 import org.cesecore.certificates.crl.RevokedCertInfo;
 import org.cesecore.certificates.endentity.EndEntityInformation;
-import org.cesecore.mock.authentication.SimpleAuthenticationProviderSessionRemote;
 import org.cesecore.mock.authentication.tokens.TestAlwaysAllowLocalAuthenticationToken;
-import org.cesecore.roles.AdminGroupData;
-import org.cesecore.roles.management.RoleManagementSessionRemote;
 import org.cesecore.util.Base64;
 import org.cesecore.util.CertTools;
 import org.cesecore.util.CryptoProviderTools;
@@ -71,7 +59,7 @@ import org.junit.runners.MethodSorters;
  * @version $Id$
  */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class PublisherTest {
+public class PublisherTest extends RoleUsingTestCase {
 
 	private static final byte[] testcert = Base64.decode(("MIICWzCCAcSgAwIBAgIIJND6Haa3NoAwDQYJKoZIhvcNAQEFBQAwLzEPMA0GA1UE"
 			+ "AxMGVGVzdENBMQ8wDQYDVQQKEwZBbmFUb20xCzAJBgNVBAYTAlNFMB4XDTAyMDEw"
@@ -110,17 +98,12 @@ public class PublisherTest {
 	private final static String newName = "TESTNEWDUMMYCUSTOM";
 
 	private static final Logger log = Logger.getLogger(PublisherTest.class);
-
-	private static final AuthenticationToken internalAdmin = new TestAlwaysAllowLocalAuthenticationToken(new UsernamePrincipal("PublisherTest"));
-
-	private final static String commonname = PublisherTest.class.getCanonicalName();
-
-	private final static Set<String> publisherNames = new HashSet<String>();
+	private static final AuthenticationToken internalAdmin = new TestAlwaysAllowLocalAuthenticationToken("PublisherTest");
+	private static final String commonname = PublisherTest.class.getCanonicalName();
+	private static final Set<String> publisherNames = new HashSet<String>();
 
 	private final PublisherSessionRemote publisherSession = EjbRemoteHelper.INSTANCE.getRemoteSession(PublisherSessionRemote.class);
 	private final PublisherProxySessionRemote publisherProxySession = EjbRemoteHelper.INSTANCE.getRemoteSession(PublisherProxySessionRemote.class, EjbRemoteHelper.MODULE_TEST);
-	private final RoleManagementSessionRemote roleManagementSession = EjbRemoteHelper.INSTANCE.getRemoteSession(RoleManagementSessionRemote.class);
-	private final SimpleAuthenticationProviderSessionRemote simpleAuthenticationProvider = EjbRemoteHelper.INSTANCE.getRemoteSession(SimpleAuthenticationProviderSessionRemote.class, EjbRemoteHelper.MODULE_TEST);
 
 	private AuthenticationToken admin;
 
@@ -132,33 +115,30 @@ public class PublisherTest {
 
 	@Before
 	public void setUp() throws Exception {
-		Certificate cert = CertTools.getCertfromByteArray(testcert, Certificate.class);
-		int caid = CertTools.getIssuerDN(cert).hashCode();
-		String subjectDn = CertTools.getSubjectDN(cert);
-		String cN = CertTools.getPartsFromDN(subjectDn, "CN").get(0);
-
-		Set<Principal> principals = new HashSet<Principal>();
-		Set<Certificate> credentials = new HashSet<Certificate>();
-		credentials.add(cert);
-		X500Principal p = new X500Principal(subjectDn);
-		principals.add(p);
-		AuthenticationSubject subject = new AuthenticationSubject(principals, credentials);
-		this.admin = this.simpleAuthenticationProvider.authenticate(subject);
-
-		AdminGroupData role = this.roleManagementSession.create(internalAdmin, commonname);
-		Collection<AccessRuleData> rules = new ArrayList<AccessRuleData>();
-		rules.add(new AccessRuleData(commonname, StandardRules.CAACCESS.resource() + caid, AccessRuleState.RULE_ACCEPT, false));
-		role = this.roleManagementSession.addAccessRulesToRole(internalAdmin, role, rules);
-		Collection<AccessUserAspectData> users = new ArrayList<AccessUserAspectData>();
-		users.add(new AccessUserAspectData(commonname, caid, X500PrincipalAccessMatchValue.WITH_COMMONNAME, AccessMatchType.TYPE_EQUALCASE, cN));
-		role = this.roleManagementSession.addSubjectsToRole(internalAdmin, role, users);
+        Certificate cert = CertTools.getCertfromByteArray(testcert, Certificate.class);
+        int caid = CertTools.getIssuerDN(cert).hashCode();
+        super.setUpAuthTokenAndRole(null, commonname, Arrays.asList(StandardRules.CAACCESS.resource() + caid), null);
+        this.admin = roleMgmgToken;
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		this.roleManagementSession.remove(internalAdmin, commonname);
+	    super.tearDownRemoveRole();
 	}
 
+    /** removes all publishers */
+    @AfterClass
+    public static void removePublishers() {
+        final PublisherProxySessionRemote publisherProxySession = EjbRemoteHelper.INSTANCE.getRemoteSession(PublisherProxySessionRemote.class, EjbRemoteHelper.MODULE_TEST);
+        boolean ret = true;
+        for( final String publisherName : publisherNames ) {
+            try {
+                publisherProxySession.removePublisher(internalAdmin, publisherName);
+                log.debug("Publisher named '"+publisherName+"' removed.");
+            } catch (Exception pee) {ret = false;}
+        }
+        assertTrue("Removing Publisher failed", ret);
+    }
 
 	/**
 	 * adds ldap publisher
@@ -275,11 +255,9 @@ public class PublisherTest {
 	@Test
 	public void test06EditPublisher() throws AuthorizationDeniedException {
 		log.trace(">test06EditPublisher()");
-
 		final BasePublisher publisher = this.publisherSession.getPublisher(cloneName);
 		publisher.setDescription(publisher.getDescription().toUpperCase());
 		this.publisherSession.changePublisher(internalAdmin, cloneName, publisher);
-
 		log.trace("<test06EditPublisher()");
 	}
 
@@ -318,7 +296,6 @@ public class PublisherTest {
 		publishers.add(Integer.valueOf(this.publisherProxySession.getPublisherId(newName)));
 		final boolean ret = this.publisherSession.storeCRL(this.admin, publishers, testcrl, null, 1, issuerDn);
 		assertTrue("Storing CRL to dummy publisher failed", ret);
-
 		log.trace("<test08storeCRLToDummy()");
 	}
 
@@ -348,21 +325,4 @@ public class PublisherTest {
         final boolean ret = publisherSession.storeCertificate(internalAdmin, publishers, cdw, "foo123", CertTools.getSubjectDN(testCertificate), null);
         assertTrue("Unable to store certificate for " + publishers.size() + " publishers in one call.", ret);
     }
-
-	/**
-	 * removes all publishers
-	 */
-	@AfterClass
-	public static void removePublishers() {
-		final PublisherProxySessionRemote publisherProxySession = EjbRemoteHelper.INSTANCE.getRemoteSession(PublisherProxySessionRemote.class, EjbRemoteHelper.MODULE_TEST);
-		boolean ret = true;
-		for( final String publisherName : publisherNames ) {
-			try {
-				publisherProxySession.removePublisher(internalAdmin, publisherName);
-				log.debug("Publisher named '"+publisherName+"' removed.");
-			} catch (Exception pee) {ret = false;}
-		}
-		assertTrue("Removing Publisher failed", ret);
-	}
-
-} // TestPublisher
+}
