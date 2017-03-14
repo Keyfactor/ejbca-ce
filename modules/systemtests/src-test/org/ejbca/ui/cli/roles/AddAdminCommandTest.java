7718f20ed74760267c8dff97dc3006a4ffb0f060
Johan Eklund <johan.eklund@primekey.com>
2017-03-14 13:09:31 +0000

    ECA-5746: Setup both old and new authorization for system tests.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@25472 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/systemtests/src-test/org/ejbca/ui/cli/roles/AddAdminCommandTest.java
+++ modules/systemtests/src-test/org/ejbca/ui/cli/roles/AddAdminCommandTest.java
@@ -12,9 +12,13 @@
  *************************************************************************/
 package org.ejbca.ui.cli.roles;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.util.List;
+
 import org.cesecore.authentication.tokens.AuthenticationToken;
+import org.cesecore.authorization.AuthorizationDeniedException;
 import org.cesecore.authorization.user.AccessMatchType;
 import org.cesecore.authorization.user.AccessUserAspect;
 import org.cesecore.authorization.user.AccessUserAspectData;
@@ -26,8 +30,12 @@ import org.cesecore.keys.token.CryptoTokenManagementSessionRemote;
 import org.cesecore.keys.token.CryptoTokenTestUtils;
 import org.cesecore.mock.authentication.tokens.TestAlwaysAllowLocalAuthenticationToken;
 import org.cesecore.roles.AdminGroupData;
+import org.cesecore.roles.Role;
 import org.cesecore.roles.access.RoleAccessSessionRemote;
 import org.cesecore.roles.management.RoleManagementSessionRemote;
+import org.cesecore.roles.management.RoleSessionRemote;
+import org.cesecore.roles.member.RoleMember;
+import org.cesecore.roles.member.RoleMemberSessionRemote;
 import org.cesecore.util.CryptoProviderTools;
 import org.cesecore.util.EjbRemoteHelper;
 import org.junit.After;
@@ -49,12 +57,15 @@ public class AddAdminCommandTest {
     private static final CaSessionRemote caSession = EjbRemoteHelper.INSTANCE.getRemoteSession(CaSessionRemote.class);
     private static final CryptoTokenManagementSessionRemote cryptoTokenManagementSession = EjbRemoteHelper.INSTANCE
             .getRemoteSession(CryptoTokenManagementSessionRemote.class);
+    private final RoleSessionRemote roleSession = EjbRemoteHelper.INSTANCE.getRemoteSession(RoleSessionRemote.class);
+    private final RoleMemberSessionRemote roleMemberSession = EjbRemoteHelper.INSTANCE.getRemoteSession(RoleMemberSessionRemote.class);
     private final RoleAccessSessionRemote roleAccessSession = EjbRemoteHelper.INSTANCE.getRemoteSession(RoleAccessSessionRemote.class);
     private final RoleManagementSessionRemote roleManagementSession = EjbRemoteHelper.INSTANCE.getRemoteSession(RoleManagementSessionRemote.class);
 
     private static X509CA x509ca = null;
 
     private AddAdminCommand command = new AddAdminCommand();
+    private int roleId = Role.ROLE_ID_UNASSIGNED;
 
     private static final AuthenticationToken authenticationToken = new TestAlwaysAllowLocalAuthenticationToken(TESTCLASS_NAME);
 
@@ -76,6 +87,7 @@ public class AddAdminCommandTest {
     @Before
     public void setup() throws Exception {
         roleManagementSession.create(authenticationToken, TESTCLASS_NAME);
+        roleId = roleSession.persistRole(authenticationToken, new Role(null, TESTCLASS_NAME)).getRoleId();
     }
     
     @After
@@ -84,10 +96,11 @@ public class AddAdminCommandTest {
         if (role != null) {
             roleManagementSession.remove(authenticationToken, role);
         }
+        roleSession.deleteRoleIdempotent(authenticationToken, roleId);
     }
 
     @Test
-    public void testAddAdminCommandLegacy() {
+    public void testAddAdminCommandLegacy() throws AuthorizationDeniedException {
         final String matchValue = TESTCLASS_NAME + " Legacy";
         String[] args = new String[] { TESTCLASS_NAME, x509ca.getName(), X500PrincipalAccessMatchValue.WITH_COMMONNAME.toString(),
                 AccessMatchType.TYPE_EQUALCASEINS.toString(), matchValue };
@@ -95,11 +108,19 @@ public class AddAdminCommandTest {
         AccessUserAspect result = accessUserAspectManagerTestSession.find(AccessUserAspectData.generatePrimaryKey(TESTCLASS_NAME, x509ca.getCAId(),
                 X500PrincipalAccessMatchValue.WITH_COMMONNAME, AccessMatchType.TYPE_EQUALCASE, matchValue));
         assertNotNull("Admin was not added,", result);
-
+        final List<RoleMember> roleMembers = roleMemberSession.getRoleMembersByRoleId(authenticationToken, roleId);
+        /*
+        assertEquals(1, roleMembers.size());
+        final RoleMember roleMember = roleMembers.get(0);
+        assertEquals(x509ca.getCAId(), roleMember.getTokenIssuerId());
+        assertEquals(X500PrincipalAccessMatchValue.WITH_COMMONNAME.getNumericValue(), roleMember.getTokenMatchKey());
+        assertEquals(AccessMatchType.TYPE_EQUALCASE.getNumericValue(), roleMember.getTokenMatchOperator());
+        assertEquals(matchValue, roleMember.getTokenMatchValue());
+        */
     }
 
     @Test
-    public void testAddAdminCommand() {
+    public void testAddAdminCommand() throws AuthorizationDeniedException {
         final String matchValue = TESTCLASS_NAME;
         String[] args = new String[] { TESTCLASS_NAME, "--caname", x509ca.getName(), "--with", X500PrincipalAccessMatchValue.WITH_COMMONNAME.toString(),
                 "--value", matchValue };
@@ -107,6 +128,14 @@ public class AddAdminCommandTest {
         AccessUserAspect result = accessUserAspectManagerTestSession.find(AccessUserAspectData.generatePrimaryKey(TESTCLASS_NAME, x509ca.getCAId(),
                 X500PrincipalAccessMatchValue.WITH_COMMONNAME, AccessMatchType.TYPE_EQUALCASE, matchValue));
         assertNotNull("Admin was not added,", result);
-
+        final List<RoleMember> roleMembers = roleMemberSession.getRoleMembersByRoleId(authenticationToken, roleId);
+        /*
+        assertEquals(1, roleMembers.size());
+        final RoleMember roleMember = roleMembers.get(0);
+        assertEquals(x509ca.getCAId(), roleMember.getTokenIssuerId());
+        assertEquals(X500PrincipalAccessMatchValue.WITH_COMMONNAME.getNumericValue(), roleMember.getTokenMatchKey());
+        assertEquals(AccessMatchType.TYPE_EQUALCASE.getNumericValue(), roleMember.getTokenMatchOperator());
+        assertEquals(matchValue, roleMember.getTokenMatchValue());
+        */
     }
 }
