6738a5ece2feaaf4c9414b93895dc540fc333711
Johan Eklund <johan.eklund@primekey.com>
2017-03-20 15:07:32 +0000

    ECA-5781: Remove legacy authorization system code used from EJBCA EJB CLI.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@25543 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb-interface/src/org/ejbca/core/ejb/authorization/ComplexAccessControlSession.java
+++ modules/ejbca-ejb-interface/src/org/ejbca/core/ejb/authorization/ComplexAccessControlSession.java
@@ -12,25 +12,10 @@
  *************************************************************************/
 package org.ejbca.core.ejb.authorization;
 
-import org.cesecore.authentication.tokens.AuthenticationToken;
-import org.cesecore.authorization.AuthorizationDeniedException;
-import org.cesecore.roles.RoleExistsException;
 
 /**
  * @version $Id$
  */
 @Deprecated // Use AuthorizationSystemSession
 public interface ComplexAccessControlSession {
-	
-	/** Initializes the authorization module with a superadmin rule matching the given caid and superadminCN
-	 * 
-	 * @param admin AuthenticationToken of the admin adding the rule
-	 * @param caid the ca id of the CA issuing the SuperAdmin certificate
-	 * @param superAdminCN the CN of the superadmin to match in the rule
-	 * @throws AuthorizationDeniedException 
-	 * @throws RoleExistsException 
-     * @deprecated superseded by AuthorizationSystemSessionLocal.initializeAuthorizationModule() and use of RoleSession and RoleMemberSession
-	 */
-    @Deprecated
-    void initializeAuthorizationModule(AuthenticationToken admin, int caid, String superAdminCN) throws RoleExistsException, AuthorizationDeniedException;
 }
