73e06c3eb9877f50bd69336314be187a0ce152f6
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2017-03-22 14:34:02 +0000

    ECA-5761: Add namespace column (shown on demand), and hide the token type column if not needed
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@25581 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ra-gui/src/org/ejbca/ra/RaRoleMemberGUIInfo.java
+++ modules/ra-gui/src/org/ejbca/ra/RaRoleMemberGUIInfo.java
@@ -24,16 +24,16 @@ public final class RaRoleMemberGUIInfo {
     public final RoleMember roleMember;
     public final String caName;
     public final String roleName;
-    public final String title;
+    public final String roleNamespace;
     public final String tokenTypeText;
     public final boolean tokenMatchValueIsLink;
     
-    public RaRoleMemberGUIInfo(final RoleMember roleMember, final String caName, final String roleName, final String title, final String tokenTypeText) {
+    public RaRoleMemberGUIInfo(final RoleMember roleMember, final String caName, final String roleName, final String roleNamespace, final String tokenTypeText) {
         this.roleMember = roleMember;
         this.caName = caName;
         this.roleName = roleName;
+        this.roleNamespace = roleNamespace;
         this.tokenTypeText = tokenTypeText;
-        this.title = title;
         this.tokenMatchValueIsLink = X509CertificateAuthenticationTokenMetaData.TOKEN_TYPE.equals(roleMember.getTokenType()) &&
                 roleMember.getTokenMatchKey() == X500PrincipalAccessMatchValue.WITH_SERIALNUMBER.getNumericValue();
     }
@@ -50,8 +50,8 @@ public final class RaRoleMemberGUIInfo {
         return roleName;
     }
     
-    public String getTitle() {
-        return title;
+    public String getRoleNamespace() {
+        return roleNamespace;
     }
     
     public String getTokenTypeText() {
