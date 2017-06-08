5921d6672d1e3c6187321148659951c6fa4bfbb1
Henrik Sunmark <henrik.sunmark@primekey.com>
2017-06-08 08:02:03 +0000

    ECA-4550: added trim() for EE email field in JS to prevent misleading error message
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@25957 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/admin-gui/resources/ejbcajslib.js
+++ modules/admin-gui/resources/ejbcajslib.js
@@ -171,6 +171,7 @@ function checkfieldforlegalemailcharswithchangeable(thetextfield , alerttext){
 function checkfieldforlegalemailcharswithoutat(thetextfield , alerttext){
   field = eval(thetextfield);
   var text = new String(field.value);
+  text = text.trim();
   re = /[^\u0041-\u005a\u0061-\u007a\u00a1-\ud7ff\ue000-\uffff_0-9\.\-\']/g;
   if(re.exec(text)){
     alert(alerttext);
