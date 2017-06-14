f43425d258ee4943aa923d6f306964f2da6859be
Henrik Sunmark <henrik.sunmark@primekey.com>
2017-06-14 12:40:21 +0000

    ECA-5758: Prevented hidden names based on case for EEPs
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@26019 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-common-web/src/org/ejbca/core/model/ra/RAAuthorization.java
+++ modules/ejbca-common-web/src/org/ejbca/core/model/ra/RAAuthorization.java
@@ -160,7 +160,11 @@ public class RAAuthorization implements Serializable {
     		authprofilenames = new TreeMap<String, Integer>(new Comparator<String>() {
                 @Override
                 public int compare(String o1, String o2) {
-                    return o1.compareToIgnoreCase(o2);
+                    int result = o1.compareToIgnoreCase(o2);
+                    if (result == 0) {
+                        result = o1.compareTo(o2);
+                    }
+                    return result;
                 }
             });
     		final Map<Integer, String> idtonamemap = endEntityProfileSession.getEndEntityProfileIdToNameMap();
