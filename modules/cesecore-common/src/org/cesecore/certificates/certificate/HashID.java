922bc9d42f4e7385903c384185d096413bb0c89b
Mike Agrenius Kushner <mike.kushner@primekey.com>
2017-06-14 12:41:08 +0000

    NoTicket: Fixing test failures
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@26020 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-common/src/org/cesecore/certificates/certificate/HashID.java
+++ modules/cesecore-common/src/org/cesecore/certificates/certificate/HashID.java
@@ -20,6 +20,7 @@ import java.security.cert.X509Certificate;
 import javax.security.auth.x500.X500Principal;
 
 import org.apache.log4j.Logger;
+import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.cert.X509CertificateHolder;
 import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
 import org.bouncycastle.util.encoders.Base64;
@@ -107,7 +108,11 @@ public class HashID {
      */
     public static HashID getFromDNString(String sDN) {
 		// Note that the DN string has to be encoded to an ASN1 with the BC lib. BC endcoding is EJBCA standard.
-        return getFromDN(new X500Principal((CertTools.isDNReversed(sDN) ? CertTools.reverseDN(sDN) : sDN)));
+        try {
+            return getFromDN( new X500Principal(new X500Name(CertTools.isDNReversed(sDN) ? CertTools.reverseDN(sDN) : sDN).getEncoded()));
+        } catch (IOException e) {
+           throw new IllegalStateException(e);
+        }
 	}
 	/**
 	 * @param s The hash base64 encoded. See RFC4387
