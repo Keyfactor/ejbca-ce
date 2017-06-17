9eaeea8697c5e27327cd472df143a5f1b15d0722
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2017-06-17 18:46:00 +0000

    EJBCAINTER-144: change error message after updated validation routines
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@26032 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/systemtests/src-test/org/ejbca/core/protocol/cmp/CrmfKeyUpdateTest.java
+++ modules/systemtests/src-test/org/ejbca/core/protocol/cmp/CrmfKeyUpdateTest.java
@@ -887,7 +887,7 @@ public class CrmfKeyUpdateTest extends CmpTestCase {
             assertEquals(23, body.getType());
             ErrorMsgContent err = (ErrorMsgContent) body.getContent();
             final String errMsg = err.getPKIStatusInfo().getStatusString().getStringAt(0).getString();
-            final String expectedErrMsg = "The End Entity certificate attached to the PKIMessage is issued by the wrong CA";
+            final String expectedErrMsg = "The certificate attached to the PKIMessage in the extraCert field is not valid  - Trust anchor for certification path not found.";
             assertEquals(expectedErrMsg, errMsg);
         } finally {
             removeTestCA(differentX509ca.getCAId());
