8ec2c4d5b3e4044c15837982282e4a635585d2ae
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2017-07-03 09:48:28 +0000

    ECA-5286: Fix check whether key algorithm should be visible or not
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@26115 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ra-gui/src/org/ejbca/ra/EnrollWithRequestIdBean.java
+++ modules/ra-gui/src/org/ejbca/ra/EnrollWithRequestIdBean.java
@@ -438,8 +438,8 @@ public class EnrollWithRequestIdBean implements Serializable {
      * Checks if a non-modifiable text displaying the previously set key algorithm should be shown.
      */
     public boolean isPreSetKeyAlgorithmRendered() {
-        return endEntityInformation.getExtendedinformation().getKeyStoreAlgorithmType() != null &&
-                endEntityInformation.getTokenType() != EndEntityConstants.TOKEN_USERGEN &&
+        return (endEntityInformation.getExtendedinformation().getKeyStoreAlgorithmType() != null ||
+                endEntityInformation.getTokenType() == EndEntityConstants.TOKEN_USERGEN) &&
                 endEntityInformation.getStatus() != EndEntityConstants.STATUS_KEYRECOVERY;
     }
     
