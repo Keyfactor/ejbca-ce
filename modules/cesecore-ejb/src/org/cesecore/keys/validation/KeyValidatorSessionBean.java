1ef73171a6c0e10d1d8f48b7f663f2c1196cd0c1
Mike Agrenius Kushner <mike.kushner@primekey.com>
2017-08-18 15:14:32 +0000

    ECA-6062: Made some cosmetic improvements to how validators are presented
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@26367 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-ejb/src/org/cesecore/keys/validation/KeyValidatorSessionBean.java
+++ modules/cesecore-ejb/src/org/cesecore/keys/validation/KeyValidatorSessionBean.java
@@ -21,12 +21,14 @@ import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.TreeMap;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipException;
 import java.util.zip.ZipInputStream;
@@ -535,7 +537,12 @@ public class KeyValidatorSessionBean implements KeyValidatorSessionLocal, KeyVal
 
     @Override
     public Map<String, Integer> getKeyValidatorNameToIdMap() {
-        final HashMap<String, Integer> result = new HashMap<>();
+        final TreeMap<String, Integer> result = new TreeMap<>(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareToIgnoreCase(o2);
+            }
+        });
         for (ProfileData data : profileSession.findAllProfiles(Validator.TYPE_NAME)) {
             result.put(data.getProfileName(), data.getId());
         }
