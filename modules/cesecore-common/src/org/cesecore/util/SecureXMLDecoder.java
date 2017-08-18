cc1cbfd776719fb0624d3c843bd9cdb12e685456
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2017-08-18 09:41:23 +0000

    ECA-6061: Allow import of UserNotification objects
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@26362 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-common/src/org/cesecore/util/SecureXMLDecoder.java
+++ modules/cesecore-common/src/org/cesecore/util/SecureXMLDecoder.java
@@ -272,6 +272,15 @@ public class SecureXMLDecoder implements AutoCloseable {
             case "org.cesecore.certificates.certificateprofile.PKIDisclosureStatement":
                 value = parseObject(new PKIDisclosureStatement());
                 break;
+            case "org.ejbca.core.model.ra.raadmin.UserNotification":
+                try {
+                    // EJBCA class, so not available in CESeCore.
+                    // In the long run we should consider whitelisting parts of the org.ejbca and org.cesecore package namespaces (ECA-4916)
+                    value = parseObject(Class.forName(className).newInstance());
+                } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+                    throw new IOException(errorMessage("Deserialization of class '" + className + "' failed: " + e.getMessage()), e);
+                }
+                break;
             case "java.util.Collections":
                 value = parseSpecialCollection(method);
                 method = null; // value has been used, don't report error
