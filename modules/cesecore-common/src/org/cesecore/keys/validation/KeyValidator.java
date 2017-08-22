53e74393f562fc4f359f43003b754b39a6f423b3
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2017-08-22 15:20:58 +0000

    ECA-4219: fix date format to display yyyy-MM-dd HH mm ss
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@26390 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-common/src/org/cesecore/keys/validation/KeyValidator.java
+++ modules/cesecore-common/src/org/cesecore/keys/validation/KeyValidator.java
@@ -27,7 +27,7 @@ import org.cesecore.certificates.certificateprofile.CertificateProfile;
 public interface KeyValidator extends Validator {
     
     /** List of accepted date formats for notBefore and notAfter filter. */
-    static final String[] DATE_FORMAT = new String[] { "yyyy-MM-dd HH:m:sZZ", "yyyy-MM-dd H:m:s", "yyyy-MM-dd" };
+    static final String[] DATE_FORMAT = new String[] { "yyyy-MM-dd HH:mm:ssZZ", "yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd" };
     
     /**
      * Method that validates the public key.
