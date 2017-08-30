c49dd5fc109eb5788e94ecda884099fa2687c06b
Mike Agrenius Kushner <mike.kushner@primekey.com>
2017-08-30 14:37:39 +0000

    NoTicket: Added serialVersionUID to a bunch of auto generated exceptions
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@26472 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ws-cli/src-gen/org/ejbca/core/protocol/ws/client/gen/UserDataSourceException_Exception.java
+++ modules/ejbca-ws-cli/src-gen/org/ejbca/core/protocol/ws/client/gen/UserDataSourceException_Exception.java
@@ -15,6 +15,8 @@ public class UserDataSourceException_Exception
     extends Exception
 {
 
+    private static final long serialVersionUID = 1L;
+    
     /**
      * Java type that goes as soapenv:Fault detail element.
      * 
