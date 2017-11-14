20e1603a1c6e4d0c300826c4b0605faa1b5aec4a
Bastian Fredriksson <bastian.fredriksson@primekey.com>
2017-11-14 12:31:03 +0000

    NoTicket: Cleanup.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@27152 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-ejb/src/org/cesecore/certificates/certificate/CertificateStoreSessionBean.java
+++ modules/cesecore-ejb/src/org/cesecore/certificates/certificate/CertificateStoreSessionBean.java
@@ -44,8 +44,6 @@ import javax.ejb.TimerService;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
-import javax.persistence.NonUniqueResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
