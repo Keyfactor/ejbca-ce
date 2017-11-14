6ecd929d91590353262c4172be7c0315f24a641a
Bastian Fredriksson <bastian.fredriksson@primekey.com>
2017-11-14 12:31:23 +0000

    NoTicket: Cleanup.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@27153 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/systemtests/src-test/org/ejbca/core/protocol/ws/EjbcaWSTest.java
+++ modules/systemtests/src-test/org/ejbca/core/protocol/ws/EjbcaWSTest.java
@@ -15,7 +15,6 @@ package org.ejbca.core.protocol.ws;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.junit.Assume.assumeTrue;
