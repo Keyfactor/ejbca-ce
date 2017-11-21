6e02b446b3a3b0704ceda2442321aef024774e8c
Bastian Fredriksson <bastian.fredriksson@primekey.com>
2017-11-21 10:20:27 +0000

    ECA-6307 Fixed CSS and license headers.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@27248 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-common/src/org/cesecore/certificates/certificatetransparency/DuplicateCtLogException.java
+++ modules/cesecore-common/src/org/cesecore/certificates/certificatetransparency/DuplicateCtLogException.java
@@ -1,3 +1,13 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA - Proprietary Modules: Enterprise Certificate Authority        *
+ *                                                                       *
+ *  Copyright (c), PrimeKey Solutions AB. All rights reserved.           *
+ *  The use of the Proprietary Modules are subject to specific           *
+ *  commercial license terms.                                            *
+ *                                                                       *
+ *************************************************************************/
+
 package org.cesecore.certificates.certificatetransparency;
 
 /**
