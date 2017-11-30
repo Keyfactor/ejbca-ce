c7e9ea8421744928785e2fd31e5eb7129576eb3c
Ulf Undmark <ulf.undmark@primekey.com>
2017-11-30 16:13:25 +0000

    ECA-6223: Code fixes according to review
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@27362 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/admin-gui/src/org/ejbca/util/validator/NoNegativeNumbersValidator.java
+++ modules/admin-gui/src/org/ejbca/util/validator/NoNegativeNumbersValidator.java
@@ -8,38 +8,39 @@ import javax.faces.context.FacesContext;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 
+import org.apache.log4j.Logger;
 import org.cesecore.internal.InternalResources;
 
 
 /**
- * Validates that no negative numbers can be set for a Rsa Key Validator
+ * Validates that no negative numbers can be set for a Rsa Key Validator.
  * 
+ * @version 
  */
-
 
 public class NoNegativeNumbersValidator implements Validator {
     
-    private static final InternalResources intres = InternalResources.getInstance();    
+    /** Localization of log and error messages. */
+    private static final InternalResources intres = InternalResources.getInstance();
     
-    /**
-     * Validates RSA Key Validator fields, asserting that they are not negative
-     * @param context the faces context.
-     * @param component the events source component
-     * @param value the source components value attribute
-     * @throws ValidatorException if the validation fails.
-     */
+    /** Class logger. */
+    private static final Logger log = Logger.getLogger(MinSmallerThanMaxValidator.class);
+    
     @Override
     public void validate(FacesContext context, UIComponent component, Object object) throws ValidatorException {
         if (null != object){
             BigInteger intVal;
             if (object instanceof  Integer ){
-                intVal = new BigInteger(object.toString());
+                intVal = BigInteger.valueOf(((Integer) object).intValue());
             } else {
                 intVal = (BigInteger) object; 
             }
             if (intVal.compareTo(BigInteger.ZERO ) == -1) {
                 final String field = (String) component.getAttributes().get("fieldName");
                 final String message = intres.getLocalizedMessage("validator.error.set_key_validator_values", intVal, field);
+                if (log.isDebugEnabled()) {
+                    log.debug(message);
+                }
                 throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR, message, message));
             }
         }
