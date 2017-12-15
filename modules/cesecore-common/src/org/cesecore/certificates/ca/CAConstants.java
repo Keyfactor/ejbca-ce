34a73bbf9389cb8e1938d372e769997a2d5899aa
Bastian Fredriksson <bastian.fredriksson@primekey.com>
2017-12-15 10:09:20 +0000

    ECA-6423 Added javadoc.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@27553 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-common/src/org/cesecore/certificates/ca/CAConstants.java
+++ modules/cesecore-common/src/org/cesecore/certificates/ca/CAConstants.java
@@ -9,7 +9,7 @@
  *                                                                       *
  *  See terms of license at gnu.org.                                     *
  *                                                                       *
- *************************************************************************/  
+ *************************************************************************/
 package org.cesecore.certificates.ca;
 
 /**
@@ -18,14 +18,42 @@ package org.cesecore.certificates.ca;
  * @version $Id$
  */
 public final class CAConstants {
-    
-    /** Constants used to indicate status of a CA. */
+    /**
+     * The state of a node-local CA with a keypair which is neither expired nor revoked.
+     * An active CA should be able to create signatures unless the crypto token associated
+     * with the CA is offline, in which case healthcheck will fail. A CA stays in this
+     * state until the public key expires or is revoked.
+     */
     public static final int CA_ACTIVE = 1;
+    /**
+     * The state of an external CA where a CSR has been created but the signed
+     * certificate has not yet been imported into EJBCA.
+     */
     public static final int CA_WAITING_CERTIFICATE_RESPONSE = 2;
+    /**
+     * The state of a node-local or external CA with a public key which has expired. Once
+     * a CA is expired, it will stay in this state indefinitely.
+     */
     public static final int CA_EXPIRED = 3;
+    /**
+     * The state of a node-local CA with a public key which has been revoked.
+     */
     public static final int CA_REVOKED = 4;
+    /**
+     * The state of a node-local CA which has been purposely put offline by the user, i.e
+     * a CA whose "CA Service State" is "Offline". Healthcheck will be disabled for CAs
+     * in this state.
+     */
     public static final int CA_OFFLINE = 5;
+    /**
+     * An external CA with a signed public key available. A CA stays in this state until
+     * the public key expires.
+     */
     public static final int CA_EXTERNAL = 6;
+    /**
+     * The initial state of an external CA before a CSR has been created, in which case
+     * it moves to the CA_WAITING_CERTIFICATE_RESPONSE state.
+     */
     public static final int CA_UNINITIALIZED = 7;
 
     private static final String[] statustexts = {"", "ACTIVE", "WAITINGFORCERTRESPONSE", "EXPIRED", "REVOKED", "OFFLINE","EXTERNALCA", "UNINITIALIZED"};
@@ -41,7 +69,7 @@ public final class CAConstants {
      */
     public static final int CAID_USEUSERDEFINED = 0;
 
-    /** Used in profiles and service workers to make the catch all every CA instead of listing individual CAs when operating on them 
+    /** Used in profiles and service workers to make the catch all every CA instead of listing individual CAs when operating on them
      * This is duplicated in SecConst */
     public static final int ALLCAS = 1;
 
