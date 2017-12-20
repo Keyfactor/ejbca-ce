7e8bfa125c05645c01573830c09308f7395d2a2f
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2017-12-20 15:21:57 +0000

    ECA-4220: a stab at resolvingCommunity build by using the established enterprise edition module with different session bean implementation for community and enterprise
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@27605 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/edition-specific/src-ejb/org/ejbca/core/ejb/EstOperationsSessionBean.java
@@ -0,0 +1,40 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA - Proprietary Modules: Enterprise Certificate Authority        *
+ *                                                                       *
+ *  Copyright (c), PrimeKey Solutions AB. All rights reserved.           *
+ *  The use of the Proprietary Modules are subject to specific           *
+ *  commercial license terms.                                            *
+ *                                                                       *
+ *************************************************************************/
+
+package org.ejbca.core.ejb;
+
+import java.security.cert.X509Certificate;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+
+import org.cesecore.jndi.JndiConstants;
+import org.ejbca.core.protocol.est.EstOperationsSessionLocal;
+import org.ejbca.core.protocol.est.EstOperationsSessionRemote;
+
+/**
+ * Class that receives a EST message and passes it on to the correct message handler.
+ * Not available in Community Edition
+ *
+ * @version $Id$
+ */
+@Stateless(mappedName = JndiConstants.APP_JNDI_PREFIX + "EstOperationsSessionRemote")
+@TransactionAttribute(TransactionAttributeType.SUPPORTS)
+public class EstOperationsSessionBean implements EstOperationsSessionLocal, EstOperationsSessionRemote {
+
+	@Override
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	public byte[] dispatchRequest(String operation, String alias, X509Certificate cert, String username, String password, byte[] requestBody) 
+	        throws UnsupportedOperationException{
+        throw new UnsupportedOperationException("EST calls are only supported in EJBCA Enterprise");
+	}
+
+}
