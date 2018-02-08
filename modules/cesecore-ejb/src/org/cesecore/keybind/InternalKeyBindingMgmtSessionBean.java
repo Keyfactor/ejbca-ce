ce9be97e8396eba3576721643d100201f639f3b4
Henrik Sunmark <henrik.sunmark@primekey.com>
2018-02-08 12:19:01 +0000

    ECA-6525: Added configdump export for admin roles + test
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@28216 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-ejb/src/org/cesecore/keybind/InternalKeyBindingMgmtSessionBean.java
+++ modules/cesecore-ejb/src/org/cesecore/keybind/InternalKeyBindingMgmtSessionBean.java
@@ -192,6 +192,19 @@ public class InternalKeyBindingMgmtSessionBean implements InternalKeyBindingMgmt
 
     @TransactionAttribute(TransactionAttributeType.SUPPORTS)
     @Override
+    public Map<Integer, String> getAllInternalKeyBindingIdNameMap(String internalKeyBindingType) {
+        final Map<Integer, String> idNameMap = new HashMap<>();
+        final List<Integer> ids = internalKeyBindingDataSession.getIds(internalKeyBindingType);
+        for (final Integer id : ids) {
+            final InternalKeyBinding internalKeyBindingInstance = internalKeyBindingDataSession.getInternalKeyBinding(id.intValue());
+            idNameMap.put(id, internalKeyBindingInstance.getName());
+        }
+        return idNameMap;
+    }
+
+    
+    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
+    @Override
     public InternalKeyBinding getInternalKeyBindingReference(AuthenticationToken authenticationToken, int id) throws AuthorizationDeniedException {
         if (!authorizationSession.isAuthorized(authenticationToken, InternalKeyBindingRules.VIEW.resource() + "/" + id)) {
             final String msg = intres.getLocalizedMessage("authorization.notuathorizedtoresource", InternalKeyBindingRules.VIEW.resource(),
