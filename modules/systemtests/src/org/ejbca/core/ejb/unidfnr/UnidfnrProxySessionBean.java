94ed7d659307e7a3d421b039c7b40581fc0571db
Amin Khorsandi <amin.khorsandi@primekey.com>
2018-03-29 10:55:11 +0000

    ECA-6708: Fix build in wildfly.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@28597 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/systemtests/src/org/ejbca/core/ejb/unidfnr/UnidfnrProxySessionBean.java
+++ modules/systemtests/src/org/ejbca/core/ejb/unidfnr/UnidfnrProxySessionBean.java
@@ -3,16 +3,15 @@ package org.ejbca.core.ejb.unidfnr;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
-import javax.inject.Inject;
 
 import org.cesecore.jndi.JndiConstants;
+import org.ejbca.core.model.util.EjbLocalHelper;
 
 @Stateless(mappedName = JndiConstants.APP_JNDI_PREFIX + "UnidfnrSessionProxyRemote")
 @TransactionAttribute(TransactionAttributeType.SUPPORTS)
 public class UnidfnrProxySessionBean implements UnidfnrProxySessionRemote {
 
-    @Inject
-    private UnidfnrSessionLocal unidfnrSession;
+    private UnidfnrSessionLocal unidfnrSession = new EjbLocalHelper().getUnidfnrSession();
 
     public void removeUnidFnrDataIfPresent(final String unid) {
         unidfnrSession.removeUnidFnrDataIfPresent(unid);
