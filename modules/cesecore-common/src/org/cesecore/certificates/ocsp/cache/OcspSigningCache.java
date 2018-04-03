96f5c4d9e4eb0a20b8031f03d359ca0d85a40c33
Amin Khorsandi <amin.khorsandi@primekey.com>
2018-04-03 11:39:47 +0000

    ECA-6723: Modify code to get cache updates properly.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@28615 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-common/src/org/cesecore/certificates/ocsp/cache/OcspSigningCache.java
+++ modules/cesecore-common/src/org/cesecore/certificates/ocsp/cache/OcspSigningCache.java
@@ -62,7 +62,7 @@ public enum OcspSigningCache {
     public OcspSigningCacheEntry getDefaultEntry() {
         return defaultResponderCacheEntry;
     }
-
+    
     /** WARNING: This method potentially exports references to CAs private keys! */
     public Collection<OcspSigningCacheEntry> getEntries() {
         return cache.values();
@@ -75,8 +75,10 @@ public enum OcspSigningCache {
 
     public void stagingAdd(OcspSigningCacheEntry ocspSigningCacheEntry) {
         List<CertificateID> certIDs = ocspSigningCacheEntry.getCertificateID();
-        for (CertificateID certID : certIDs) {
-            staging.put(getCacheIdFromCertificateID(certID), ocspSigningCacheEntry);            
+        if (certIDs != null) {
+            for (CertificateID certID : certIDs) {
+                staging.put(getCacheIdFromCertificateID(certID), ocspSigningCacheEntry);
+            }
         }
     }
 
@@ -224,20 +226,25 @@ public enum OcspSigningCache {
 
     /** @return the CertificateID's based on the provided certificate */
     public static List<CertificateID> getCertificateIDFromCertificate(final X509Certificate certificate) {
-        try {
-            if (log.isTraceEnabled()) {
-                log.trace("Building CertificateId's from certificate with subjectDN '" + CertTools.getSubjectDN(certificate) + "'.");
+        if (certificate != null) {
+            try {
+                if (log.isTraceEnabled()) {
+                    log.trace("Building CertificateId's from certificate with subjectDN '" + CertTools.getSubjectDN(certificate) + "'.");
+                }
+                List<CertificateID> ret = new ArrayList<CertificateID>();
+                ret.add(new JcaCertificateID(new BcDigestCalculatorProvider().get(new AlgorithmIdentifier(OIWObjectIdentifiers.idSHA1)), certificate,
+                        certificate.getSerialNumber()));
+                ret.add(new JcaCertificateID(new BcDigestCalculatorProvider().get(new AlgorithmIdentifier(NISTObjectIdentifiers.id_sha256)),
+                        certificate, certificate.getSerialNumber()));
+                return ret;
+            } catch (OCSPException e) {
+                throw new OcspFailureException(e);
+            } catch (CertificateEncodingException e) {
+                throw new OcspFailureException(e);
+            } catch (OperatorCreationException e) {
+                throw new OcspFailureException(e);
             }
-            List<CertificateID> ret = new ArrayList<CertificateID>();
-            ret.add(new JcaCertificateID(new BcDigestCalculatorProvider().get(new AlgorithmIdentifier(OIWObjectIdentifiers.idSHA1)), certificate, certificate.getSerialNumber()));
-            ret.add(new JcaCertificateID(new BcDigestCalculatorProvider().get(new AlgorithmIdentifier(NISTObjectIdentifiers.id_sha256)), certificate, certificate.getSerialNumber()));
-            return ret;
-        } catch (OCSPException e) {
-            throw new OcspFailureException(e);
-        } catch (CertificateEncodingException e) {
-            throw new OcspFailureException(e);
-        } catch (OperatorCreationException e) {
-            throw new OcspFailureException(e);
         }
+        return null;
     }
 }
