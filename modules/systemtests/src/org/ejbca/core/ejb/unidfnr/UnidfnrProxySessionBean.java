595237a94e93ef283dae4694786e6b70c0d6415e
Amin Khorsandi <amin.khorsandi@primekey.com>
2018-04-10 09:48:34 +0000

    ECA-6764: Fix missing headers and svn id.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@28658 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/systemtests/src/org/ejbca/core/ejb/unidfnr/UnidfnrProxySessionBean.java
+++ modules/systemtests/src/org/ejbca/core/ejb/unidfnr/UnidfnrProxySessionBean.java
@@ -1,3 +1,15 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.ejbca.core.ejb.unidfnr;
 
 import javax.ejb.Stateless;
@@ -7,6 +19,11 @@ import javax.ejb.TransactionAttributeType;
 import org.cesecore.jndi.JndiConstants;
 import org.ejbca.core.model.util.EjbLocalHelper;
 
+/**
+ * 
+ * @version $Id$
+ *
+ */
 @Stateless(mappedName = JndiConstants.APP_JNDI_PREFIX + "UnidfnrSessionProxyRemote")
 @TransactionAttribute(TransactionAttributeType.SUPPORTS)
 public class UnidfnrProxySessionBean implements UnidfnrProxySessionRemote {
