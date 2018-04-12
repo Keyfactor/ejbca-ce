cb3cfd9782dbcddf912e426625d921aab3ec63ec
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2018-04-12 08:12:35 +0000

    ECA-6737: Use NoConflictCertificateData in SSB. Clean up after WS test
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@28676 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-ejb/src/org/cesecore/certificates/certificate/NoConflictCertificateStoreSessionBean.java
+++ modules/cesecore-ejb/src/org/cesecore/certificates/certificate/NoConflictCertificateStoreSessionBean.java
@@ -15,11 +15,14 @@ package org.cesecore.certificates.certificate;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
+import java.util.UUID;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.apache.log4j.Logger;
 import org.bouncycastle.util.encoders.Hex;
@@ -29,6 +32,7 @@ import org.cesecore.certificates.ca.CAInfo;
 import org.cesecore.certificates.ca.CaSessionLocal;
 import org.cesecore.certificates.certificateprofile.CertificateProfileConstants;
 import org.cesecore.certificates.crl.RevocationReasons;
+import org.cesecore.config.CesecoreConfiguration;
 import org.cesecore.jndi.JndiConstants;
 import org.cesecore.util.CertTools;
 
@@ -41,6 +45,9 @@ public class NoConflictCertificateStoreSessionBean implements NoConflictCertific
 
     private final static Logger log = Logger.getLogger(NoConflictCertificateStoreSessionBean.class);
 
+    @PersistenceContext(unitName = CesecoreConfiguration.PERSISTENCE_UNIT)
+    private EntityManager entityManager;
+    
     @EJB
     private CaSessionLocal caSession;
     @EJB
@@ -64,10 +71,40 @@ public class NoConflictCertificateStoreSessionBean implements NoConflictCertific
             }
             return null; // Certificate is non-existent
         }
-        // FIXME this should be stored in the new table (NoConflictCertificateData)
+        final NoConflictCertificateData certificateData = getLimitedNoConflictCertDataRow(cainfo, certserno);
+        return new CertificateDataWrapper(certificateData);
+    }
+    
+    @Override
+    public boolean setRevokeStatus(final AuthenticationToken admin, final CertificateDataWrapper cdw, final Date revokedDate, final int reason)
+            throws CertificateRevokeException, AuthorizationDeniedException {
+        final BaseCertificateData certificateData = cdw.getBaseCertificateData();
+        final String issuerdn = certificateData.getIssuerDN();
+        final int caid = issuerdn.hashCode();
+        final CAInfo cainfo = caSession.getCAInfoInternal(caid);
+        if (cainfo == null || !cainfo.getSubjectDN().equals(issuerdn) || cainfo.isUseCertificateStorage()) {
+            // Not a throw away certificate. Go ahead with standard CertificateData logic.
+            if (cainfo == null && log.isDebugEnabled()) {
+                log.debug("Certificate " + cdw.getCertificateData().getSubjectDN() +" references a CA that does not exist. CA Id: " + caid + ". Issuer DN: '" + issuerdn + "'");
+            }
+            return certificateStoreSession.setRevokeStatus(admin, cdw, revokedDate, reason);
+        }
+//        throw new UnsupportedOperationException("Throw away case is not implemented"); // TODO
+        return certificateStoreSession.setRevokeStatus(admin, cdw, revokedDate, reason);  // XXX Works with CertificateData, but remains to be tested with NoConflictCertificateData
+    }
+    
+    /**
+     * Returns a new row in the append-only NoConflictCertificateData table.
+     * The row is initialized with the data from the most recent entry in the table,
+     * or as a new unrevoked entry if non-existent.
+     * @param cainfo Issuer.
+     * @param certserno Certificate serial number.
+     * @return Always a new row.
+     */
+    private NoConflictCertificateData getLimitedNoConflictCertDataRow(final CAInfo cainfo, final BigInteger certserno) {
         // FIXME should try to look up in NoConflictCertificateData also, and take the latest result
-        //final NoConflictCertificateData certificateData = new NoConflictCertificateData();
-        final CertificateData certificateData = new CertificateData();
+        final NoConflictCertificateData certificateData = new NoConflictCertificateData();
+        certificateData.setId(UUID.randomUUID().toString());
         // See org.cesecore.certificates.certificate.CertificateStoreSessionBean.updateLimitedCertificateDataStatus
         certificateData.setSerialNumber(certserno.toString());
         // A fingerprint is needed by the publisher session, so we put a dummy fingerprint here
@@ -82,25 +119,7 @@ public class NoConflictCertificateStoreSessionBean implements NoConflictCertific
         certificateData.setUpdateTime(Long.valueOf(System.currentTimeMillis()));
         certificateData.setCaFingerprint(CertTools.getFingerprintAsString(cainfo.getCertificateChain().get(0)));
         certificateData.setEndEntityProfileId(-1);
-        return new CertificateDataWrapper(certificateData, null);
-    }
-    
-    @Override
-    public boolean setRevokeStatus(final AuthenticationToken admin, final CertificateDataWrapper cdw, final Date revokedDate, final int reason)
-            throws CertificateRevokeException, AuthorizationDeniedException {
-        final CertificateData certificateData = cdw.getCertificateData();
-        final String issuerdn = certificateData.getIssuerDN();
-        final int caid = issuerdn.hashCode();
-        final CAInfo cainfo = caSession.getCAInfoInternal(caid);
-        if (cainfo == null || !cainfo.getSubjectDN().equals(issuerdn) || cainfo.isUseCertificateStorage()) {
-            // Not a throw away certificate. Go ahead with standard CertificateData logic.
-            if (cainfo == null && log.isDebugEnabled()) {
-                log.debug("Certificate " + cdw.getCertificateData().getSubjectDN() +" references a CA that does not exist. CA Id: " + caid + ". Issuer DN: '" + issuerdn + "'");
-            }
-            return certificateStoreSession.setRevokeStatus(admin, cdw, revokedDate, reason);
-        }
-        throw new UnsupportedOperationException("Throw away case is not implemented"); // TODO
-//        return certificateStoreSession.setRevokeStatus(admin, cdw, revokedDate, reason);  // XXX Works with CertificateData, but remains to be tested with NoConflictCertificateData
+        return certificateData;
     }
     
     private static String generateDummyFingerprint(final String issuerdn, final BigInteger certserno) {
