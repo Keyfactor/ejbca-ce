226d3071560b4d38a53da3f3f82fd575c27b20f1
Amin Khorsandi <amin.khorsandi@primekey.com>
2018-04-13 11:52:40 +0000

    ECA-6680: Apply review comments (second try).
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@28705 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb-interface/src/org/ejbca/configdump/ConfigDumpSetting.java
+++ modules/ejbca-ejb-interface/src/org/ejbca/configdump/ConfigDumpSetting.java
@@ -42,7 +42,9 @@ public class ConfigDumpSetting implements Serializable {
     private List<ConfigdumpPattern> includedAnyType = new ArrayList<>();
     private List<ConfigdumpPattern> excludedAnyType = new ArrayList<>();
     private boolean ignoreErrors;
+    private boolean ignoreWarnings;
 
+    
     public List<ConfigdumpPattern> getIncludedAnyType() {
         return includedAnyType;
     }
@@ -86,15 +88,20 @@ public class ConfigDumpSetting implements Serializable {
     public boolean getIgnoreErrors() {
         return ignoreErrors;
     }
+
+    public boolean getIgnoreWarnings() {
+        return ignoreWarnings;
+    }
     
     public ConfigDumpSetting(final File location, final Map<ItemType, List<ConfigdumpPattern>> included, final Map<ItemType, List<ConfigdumpPattern>> excluded,
-            final List<ConfigdumpPattern> includedAnyType, final List<ConfigdumpPattern> excludedAnyType, final boolean ignoreErrors) {
+            final List<ConfigdumpPattern> includedAnyType, final List<ConfigdumpPattern> excludedAnyType, final boolean ignoreErrors, final boolean ignoreWarnings) {
         this.location = location;
         this.included = included;
         this.excluded = excluded;
         this.includedAnyType = includedAnyType;
         this.excludedAnyType = excludedAnyType;
         this.ignoreErrors = ignoreErrors;
+        this.ignoreWarnings = ignoreWarnings;
     }
 
     public boolean isIncluded(final ItemType type, final String nameStr) {
