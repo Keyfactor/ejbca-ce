ddbb0087bc7980ae3710a08106df2d4f76dcb069
Henrik Sunmark <henrik.sunmark@primekey.com>
2018-04-30 13:06:34 +0000

    ECA-6858: Added automated test for ECAQA-67
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@28805 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ejbca-webtest/src-test/org/ejbca/helper/AdminRolesHelper.java
@@ -0,0 +1,86 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
+package org.ejbca.helper;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.ejbca.utils.WebTestUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+/**
+ * Helper class for handling 'Administrator Roles' page in automated web tests
+ * @version $Id$
+ *
+ */
+public final class AdminRolesHelper {
+    
+    private AdminRolesHelper() {}
+    
+    /**
+     * Navigates to 'Administrator Roles' page and verifies outcome
+     * @param webDriver the webDriver to use in navigation
+     * @param adminWebUrl URL of EJBCA Admin Web
+     */
+    public static void goTo(WebDriver webDriver, String adminWebUrl) {
+        webDriver.get(adminWebUrl);
+        webDriver.findElement(By.xpath("//li/a[contains(@href,'roles.xhtml')]")).click();
+        assertEquals("Clicking 'Administrator Roles' link did not redirect to expected page",
+                WebTestUtils.getUrlIgnoreDomain(webDriver.getCurrentUrl()),
+                "/ejbca/adminweb/administratorprivileges/roles.xhtml");
+    }
+    
+    /**
+     * Adds a role
+     * @param webDriver the webDriver to use in navigation
+     * @param roleName to add
+     * @param assertSuccess verify that the role was added to the list of administrator roles
+     */
+    public static void addRole(WebDriver webDriver, String roleName, boolean assertSuccess) {
+        webDriver.findElement(By.xpath("//input[@value='Add' and @type='submit']")).click();
+        webDriver.findElement(By.xpath("//input[@title='Mandatory role name']")).sendKeys(roleName);
+        webDriver.findElement(By.id("modal:j_idt154")).click();
+        
+        if (assertSuccess) {
+            assertExists(webDriver, roleName);
+        }
+    }
+    
+    /**
+     * Edits access rules of the given role
+     * @param webDriver the webDriver to use in navigation
+     * @param roleName to edit
+     */
+    public static void editAccessRules(WebDriver webDriver, String roleName) {
+        WebElement row = webDriver.findElement(By.xpath("//tr/td[text()='" + roleName + "']"));
+        row.findElement(By.xpath("..//a[@title='Edit Access Rules']")).click();
+    }
+    
+    /**
+     * Checks that a given Administrator Role exists in list of Administrator Roles.
+     * 
+     * @param webDriver the WebDriver to use
+     * @param cpName the name of the Certificate Profile
+     */
+    public static void assertExists(WebDriver webDriver, String roleName) {
+        try {
+            webDriver.findElement(By.xpath("//tr/td[text()='" + roleName + "']"));
+        } catch (NoSuchElementException e) {
+            fail(roleName + " was not found in the list of Administrator Roles");
+        }
+    }
+}
---
+++ modules/ejbca-webtest/src-test/org/ejbca/webtest/EcaQa67_AccessRuleSorted.java
@@ -0,0 +1,101 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
+package org.ejbca.webtest;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.cesecore.authentication.tokens.AuthenticationToken;
+import org.cesecore.authentication.tokens.UsernamePrincipal;
+import org.cesecore.authorization.AuthorizationDeniedException;
+import org.cesecore.mock.authentication.tokens.TestAlwaysAllowLocalAuthenticationToken;
+import org.cesecore.roles.Role;
+import org.cesecore.roles.management.RoleSessionRemote;
+import org.cesecore.util.EjbRemoteHelper;
+import org.ejbca.WebTestBase;
+import org.ejbca.helper.AdminRolesHelper;
+import org.ejbca.utils.ConfigurationConstants;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class EcaQa67_AccessRuleSorted extends WebTestBase {
+    
+    private static final AuthenticationToken admin = new TestAlwaysAllowLocalAuthenticationToken(new UsernamePrincipal("EjbcaWebTest"));
+    private static final String roleName = "ECAQA67_TestRole";
+    
+    private static WebDriver webDriver;
+    private static RoleSessionRemote roleSession = EjbRemoteHelper.INSTANCE.getRemoteSession(RoleSessionRemote.class);
+ 
+    @BeforeClass
+    public static void init() {
+        setUp(true, ConfigurationConstants.PROFILE_FIREFOX_SUPERADMIN);
+        webDriver = getWebDriver();
+    }
+    
+    @AfterClass
+    public static void exit() throws AuthorizationDeniedException {
+        webDriver.quit();
+        Role role = roleSession.getRole(admin, null, roleName);
+        roleSession.deleteRoleIdempotent(admin, role.getRoleId());
+    }
+    
+    @Test
+    public void testA_addRoleAndEditRules() {
+        AdminRolesHelper.goTo(webDriver, getAdminWebUrl());
+        AdminRolesHelper.addRole(webDriver, roleName, true);
+        AdminRolesHelper.editAccessRules(webDriver, roleName);
+        webDriver.findElement(By.xpath("//tr/td/a[text()='Advanced Mode']")).click();
+    }
+    
+    @Test
+    public void testB_assertOrdering() {
+        List<String> displayedRulesOrder;
+        List<String> sortedRules;
+        List<WebElement> ruleGroups = webDriver.findElements(By.xpath("//td/table[@class='fullwidth']"));
+        for (WebElement ruleGroup : ruleGroups) {
+            displayedRulesOrder = new ArrayList<>();
+            String groupName = ruleGroup.findElement(By.xpath(".//thead/tr/th")).getText();
+            List<WebElement> rows = ruleGroup.findElements(By.xpath(".//td[@class='rulesColumn1 alignmiddle']"));
+            for (WebElement row : rows) {
+                displayedRulesOrder.add(row.getText());
+            }
+            sortedRules = new ArrayList<>();
+            sortedRules.addAll(displayedRulesOrder);
+            sortIgnoreCase(sortedRules);
+            assertEquals(groupName + " was not sorted alphabetically", sortedRules, displayedRulesOrder);
+        }
+    }
+    
+    private void sortIgnoreCase(List<String> rulesToSort) {
+        Collections.sort(rulesToSort, new Comparator<String>() {
+            @Override
+            public int compare(String first, String second) {
+                return first.compareToIgnoreCase(second);
+            }
+
+        });
+    }
+}
