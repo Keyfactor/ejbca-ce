ce5c2cb5e209aafd968ba72b36d7a61b9f10e710
Tarmo Raudsep <tarmo.raudsep.ext@primekey.com>
2018-05-03 10:36:33 +0000

    ECA-6744 Modify EjbcaWS.revokeCert call to accept more meta data fixes from code review vol4
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@28828 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ws-cli/src-test/org/ejbca/core/protocol/ws/client/RevokeCertWithMetadataCommandTest.java
+++ modules/ejbca-ws-cli/src-test/org/ejbca/core/protocol/ws/client/RevokeCertWithMetadataCommandTest.java
@@ -1,8 +1,20 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
 package org.ejbca.core.protocol.ws.client;
 
 import org.ejbca.ui.cli.ErrorAdminCommandException;
 import org.ejbca.ui.cli.IllegalAdminCommandException;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -19,7 +31,7 @@ public class RevokeCertWithMetadataCommandTest {
         String[] args = new String[3];
         args[0] = "ejbcawsracli";
         args[1] = "revokecertwithmetadata";
-        args[2] = "CN=CA1";
+        args[2] = "63706289784032807";
 
         RevokeCertWithMetadataCommand command = new RevokeCertWithMetadataCommand(args);
         command.execute();
