ebd4c21009a50d345688dc059452ef4265783864
Tarmo Raudsep <tarmo.raudsep.ext@primekey.com>
2018-05-03 07:47:42 +0000

    ECA-6744 Modify EjbcaWS.revokeCert call to accept more meta data fixes from code review vol2
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@28825 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb-interface/src/org/ejbca/core/ejb/dto/CertRevocationDto.java
+++ modules/ejbca-ejb-interface/src/org/ejbca/core/ejb/dto/CertRevocationDto.java
@@ -1,8 +1,24 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.ejbca.core.ejb.dto;
 
 import java.io.Serializable;
 import java.util.Date;
 
+/**
+ * Wrapper class for holding input parameters of revokeCertWithMetadata operation.
+ * @version $Id: CertRevocationDto.java 28674 2018-05-03 10:17:34Z tarmo_r_helmes $
+ */
 public class CertRevocationDto implements Serializable {
     
     private static final long serialVersionUID = 1L;
