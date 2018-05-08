d33dcd09f14bc4feb6e3d9f6c274be8041acfcbb
Amin Khorsandi <amin.khorsandi@primekey.com>
2018-05-08 09:07:40 +0000

    ECA-6878: CA endpoint for rest-api. Initial commit.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@28866 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ejbca-rest-api/src/org/ejbca/ui/web/rest/api/resources/CaEndPoint.java
@@ -0,0 +1,24 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA - Proprietary Modules: Enterprise Certificate Authority        *
+ *                                                                       *
+ *  Copyright (c), PrimeKey Solutions AB. All rights reserved.           *
+ *  The use of the Proprietary Modules are subject to specific           * 
+ *  commercial license terms.                                            *
+ *                                                                       *
+ *************************************************************************/
+
+package org.ejbca.ui.web.rest.api.resources;
+
+import javax.ws.rs.Path;
+
+/**
+ * Endpoint handling CA related requests.
+ * @version $Id$
+ *
+ */
+@Path("/v1/ca")
+public class CaEndPoint {
+    
+
+}
