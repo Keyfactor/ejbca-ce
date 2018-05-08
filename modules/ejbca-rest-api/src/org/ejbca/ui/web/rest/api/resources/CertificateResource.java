0b66caa8ae11034842173b5e8351c8d384f5aac1
Tarmo Raudsep <tarmo.raudsep.ext@primekey.com>
2018-05-08 13:37:02 +0000

    ECA-6882 Create JAXRS "certificate" endpoint in ejbca-rest-api module
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@28873 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ejbca-rest-api/src/org/ejbca/ui/web/rest/api/resources/CertificateResource.java
@@ -0,0 +1,38 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA - Proprietary Modules: Enterprise Certificate Authority        *
+ *                                                                       *
+ *  Copyright (c), PrimeKey Solutions AB. All rights reserved.           *
+ *  The use of the Proprietary Modules are subject to specific           * 
+ *  commercial license terms.                                            *
+ *                                                                       *
+ *************************************************************************/
+
+package org.ejbca.ui.web.rest.api.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import org.apache.log4j.Logger;
+
+/**
+ * JAX-RS resource handling certificate-related requests.
+ * @version $Id: CertificateResource.java 28867 2018-05-08 09:26:51Z tarmo_r_helmes $
+ *
+ */
+@Path("v1/certificate")
+public class CertificateResource {
+    
+    private static final Logger log = Logger.getLogger(CertificateResource.class);
+    
+    /* test it with endpoint:
+     * https://localhost:8443/ejbca/ejbca-rest-api/v1/certificate/hello
+     */
+    @GET
+    @Path("/hello")
+    @Produces("text/html")
+    public String hello() {
+        return "hello";
+    }
+}
