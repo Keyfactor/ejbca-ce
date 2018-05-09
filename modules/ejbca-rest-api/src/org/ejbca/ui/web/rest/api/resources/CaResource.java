3c6d5ea520172f5608e1a1a5b61a820eb41b359d
Amin Khorsandi <amin.khorsandi@primekey.com>
2018-05-09 08:12:01 +0000

    ECA-6878: Use ca type to return list of CAs.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@28893 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-rest-api/src/org/ejbca/ui/web/rest/api/resources/CaResource.java
+++ modules/ejbca-rest-api/src/org/ejbca/ui/web/rest/api/resources/CaResource.java
@@ -1,10 +1,13 @@
 /*************************************************************************
  *                                                                       *
- *  EJBCA - Proprietary Modules: Enterprise Certificate Authority        *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
  *                                                                       *
- *  Copyright (c), PrimeKey Solutions AB. All rights reserved.           *
- *  The use of the Proprietary Modules are subject to specific           * 
- *  commercial license terms.                                            *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
  *                                                                       *
  *************************************************************************/
 
@@ -12,15 +15,18 @@ package org.ejbca.ui.web.rest.api.resources;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map.Entry;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.apache.log4j.Logger;
 import org.cesecore.certificates.ca.CaSessionLocal;
 import org.ejbca.core.model.util.EjbLocalHelper;
+import org.ejbca.ui.web.rest.api.types.CaType;
 
 /**
  * JAX-RS resource handling CA related requests.
@@ -37,11 +43,17 @@ public class CaResource {
     
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<String> getCAs() {
+    public Response getCAs() {
         log.trace(">getCAs");
         caSession = new EjbLocalHelper().getCaSession();
+        
+        List<CaType> result = new ArrayList<CaType>();
+        
+        for (final Entry<Integer, String> caEntry : caSession.getCAIdToNameMap().entrySet()) {
+            result.add(new CaType(caEntry.getKey(), caEntry.getValue()));
+        }
+        
         log.trace("<getCAs");
-        return new ArrayList<String>(caSession.getCAIdToNameMap().values());
+        return Response.ok(result).build();
     }
-
 }
