497deb51ab9298ba45a854f75fa83c09aa9d226f
Henrik Sunmark <henrik.sunmark@primekey.com>
2018-05-15 12:18:32 +0000

    ECA-6876: implemented client cert authentication for the RESI API
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@28936 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ejbca-rest-api/src/org/ejbca/ui/web/rest/common/BaseRestResource.java
@@ -0,0 +1,46 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
+package org.ejbca.ui.web.rest.common;
+
+import java.security.cert.X509Certificate;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.cesecore.authentication.tokens.AuthenticationToken;
+import org.cesecore.authorization.AuthorizationDeniedException;
+import org.ejbca.core.model.util.EjbLocalHelper;
+
+/**
+ * Base class for common methods used across all REST end points.
+ * @version $Id$
+ *
+ */
+public abstract class BaseRestResource {
+    
+    
+    /**
+     * Returns an AuthenticationToken for the requesting administrator based on the SSL client certificate
+     * @param requestContext HTTP context
+     * @return AuthenticationToken for the requesting administrator
+     * @throws AuthorizationDeniedException
+     */
+    protected AuthenticationToken getAdmin(HttpServletRequest requestContext) throws AuthorizationDeniedException {
+        X509Certificate[] certs = (X509Certificate[]) requestContext.getAttribute("javax.servlet.request.X509Certificate");
+        if (certs == null || certs[0] == null) {
+            throw new AuthorizationDeniedException("Error no client certificate received for authentication.");
+        }
+        return new EjbLocalHelper().getEjbcaRestHelperSession().getAdmin(certs[0]);
+    }
+    
+}
