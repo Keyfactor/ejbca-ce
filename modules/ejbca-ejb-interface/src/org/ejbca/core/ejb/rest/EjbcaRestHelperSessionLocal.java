976393503513075982381692a227c0f0ea99fcd6
Henrik Sunmark <henrik.sunmark@primekey.com>
2018-05-15 12:18:32 +0000

    ECA-6876: implemented client cert authentication for the RESI API
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@28936 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ejbca-ejb-interface/src/org/ejbca/core/ejb/rest/EjbcaRestHelperSessionLocal.java
@@ -0,0 +1,38 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
+package org.ejbca.core.ejb.rest;
+
+import java.security.cert.X509Certificate;
+
+import javax.ejb.Local;
+
+import org.cesecore.authentication.tokens.AuthenticationToken;
+import org.cesecore.authorization.AuthorizationDeniedException;
+
+/**
+ * 
+ * @version $Id$
+ *
+ */
+@Local
+public interface EjbcaRestHelperSessionLocal extends EjbcaRestHelperSession {
+
+    /**
+     * @param cert X509 certificate
+     * @return AuthenticationToken object based on the SSL client certificate
+     * @throws AuthorizationDeniedException 
+     */
+    AuthenticationToken getAdmin(X509Certificate cert) throws AuthorizationDeniedException;
+    
+}
