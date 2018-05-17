af7490716f1308c9ea5c79cc4eda0967bcd2922e
Henrik Sunmark <henrik.sunmark@primekey.com>
2018-05-17 12:02:35 +0000

    ECA-6903: Added new classes. Forgot in previous commit
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@28952 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ejbca-rest-api/src/org/ejbca/ui/web/rest/api/types/RevocationResultType.java
@@ -0,0 +1,75 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
+package org.ejbca.ui.web.rest.api.types;
+
+import java.math.BigInteger;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Revocation result type holder. Used to produce JSON.
+ * @version $Id$
+ *
+ */
+public class RevocationResultType {
+
+    public static final String STATUS_ALREADYREVOKED       = "Already revoked";
+    public static final String STATUS_ERROR                = "Error";
+    public static final String STATUS_REVOKED              = "Revoked";
+    public static final String STATUS_WAITINGFORAPPROVAL   = "Waiting for approval"; 
+    
+    private BigInteger serialNumber;
+    private Date revocationDate;
+    private String status;
+    private String message;
+    
+    public RevocationResultType(BigInteger serialNumber, String status) {
+        this.serialNumber = serialNumber;
+        this.status = status;
+    }
+
+    public BigInteger getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(BigInteger serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getDate() {
+        // "2018-02-10T17:06:15+00:00"
+        final SimpleDateFormat dateFormater = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
+        return revocationDate != null ? dateFormater.format(revocationDate) : null;
+    }
+
+    public void setDate(Date date) {
+        this.revocationDate = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
