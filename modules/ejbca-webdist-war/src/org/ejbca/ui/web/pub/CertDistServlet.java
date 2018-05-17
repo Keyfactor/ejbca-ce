6c6b83923ba423a8842e25b278414a9bef5e890a
Jekaterina Bunina <jekaterina.Bunina.ext@primekey.com>
2018-05-17 08:51:45 +0000

    ECA-6902 refactor: remove BEGIN_CRL_WITH_NL and END_CRL_WITH_NL from RequestHelper. Use ones from CertTools
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@28947 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-webdist-war/src/org/ejbca/ui/web/pub/CertDistServlet.java
+++ modules/ejbca-webdist-war/src/org/ejbca/ui/web/pub/CertDistServlet.java
@@ -542,7 +542,7 @@ public class CertDistServlet extends HttpServlet {
             res.setContentType("application/octet-stream");
         }
         if (StringUtils.equals(format, "PEM")) {
-            RequestHelper.sendNewB64File(Base64.encode(cert, true), res, filename, RequestHelper.BEGIN_CERTIFICATE_WITH_NL, RequestHelper.END_CERTIFICATE_WITH_NL);
+            RequestHelper.sendNewB64File(Base64.encode(cert, true), res, filename, CertTools.BEGIN_CERTIFICATE_WITH_NL, CertTools.END_CERTIFICATE_WITH_NL);
         } else if (StringUtils.equals(format, "PKCS7")) {
             try {
                 final byte[] pkcs7 = CertTools.createCertsOnlyCMS(CertTools.convertCertificateChainToX509Chain(getFullChainOfCertificate(certcert)));
