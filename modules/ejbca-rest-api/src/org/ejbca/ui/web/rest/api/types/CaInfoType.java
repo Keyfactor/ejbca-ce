3ef4ad4081caadeea3ca6fdfbadccbc9a55128c3
Andrey Sergeev <andrey.sergeev.ext@primekey.com>
2018-05-18 06:54:45 +0000

    ECA-6878 REST API call: List of CAs - Minor refactoring - Test fixes - Javadoc
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@28962 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-rest-api/src/org/ejbca/ui/web/rest/api/types/CaInfoType.java
+++ modules/ejbca-rest-api/src/org/ejbca/ui/web/rest/api/types/CaInfoType.java
@@ -14,11 +14,10 @@ package org.ejbca.ui.web.rest.api.types;
 
 import java.util.Date;
 
-// TODO Javadoc
 /**
  * A class representing general information about CA certificate.
  *
- * @version $Id: CaInfoType.java 28909 2018-05-10 12:16:53Z aminkh $
+ * @version $Id: CaInfoType.java 28909 2018-05-10 12:16:53Z andrey_s_helmes $
  */
 public class CaInfoType {
 
@@ -28,10 +27,14 @@ public class CaInfoType {
     private String issuerDn;
     private Date expirationDate;
 
+    /**
+     * Simple constructor.
+     */
     public CaInfoType() {
     }
 
-    CaInfoType(final Integer id, final String name, final String subjectDn, final String issuerDn, final Date expirationDate) {
+    // Private
+    private CaInfoType(final Integer id, final String name, final String subjectDn, final String issuerDn, final Date expirationDate) {
         this.id = id;
         this.name = name;
         this.subjectDn = subjectDn;
@@ -39,50 +42,108 @@ public class CaInfoType {
         this.expirationDate = expirationDate;
     }
 
+    /**
+     * Return a builder instance for this class.
+     *
+     * @return builder instance for this class.
+     */
     public static CaInfoTypeBuilder builder() {
         return new CaInfoTypeBuilder();
     }
 
+    /**
+     * Return the identifier.
+     *
+     * @return identifier.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Return the name.
+     *
+     * @return name.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns the Subject DN.
+     *
+     * @return Subject DN.
+     */
     public String getSubjectDn() {
         return subjectDn;
     }
 
+    /**
+     * Returns the Issuer DN.
+     *
+     * @return Issuer DN.
+     */
     public String getIssuerDn() {
         return issuerDn;
     }
 
+    /**
+     * Returns the expiration date.
+     *
+     * @return expiration date.
+     */
     public Date getExpirationDate() {
         return expirationDate;
     }
 
+    /**
+     * Sets an identifier.
+     *
+     * @param id identifier.
+     */
     public void setId(Integer id) {
         this.id = id;
     }
 
+    /**
+     * Sets a name.
+     *
+     * @param name name.
+     */
     public void setName(final String name) {
         this.name = name;
     }
 
+    /**
+     * Sets a Subject DN.
+     *
+     * @param subjectDn Subject DN.
+     */
     public void setSubjectDn(String subjectDn) {
         this.subjectDn = subjectDn;
     }
 
+    /**
+     * Sets an Issuer DN.
+     *
+     * @param issuerDn Issuer DN.
+     */
     public void setIssuerDn(String issuerDn) {
         this.issuerDn = issuerDn;
     }
 
+    /**
+     * Sets an expiration date.
+     *
+     * @param expirationDate expiration date.
+     */
     public void setExpirationDate(Date expirationDate) {
         this.expirationDate = expirationDate;
     }
 
+    /**
+     * Builder of this class.
+     */
     public static class CaInfoTypeBuilder {
 
         private Integer id;
@@ -94,31 +155,71 @@ public class CaInfoType {
         CaInfoTypeBuilder() {
         }
 
+        /**
+         * Sets an identifier in this builder.
+         *
+         * @param id identifier.
+         *
+         * @return instance of this builder.
+         */
         public CaInfoTypeBuilder id(final Integer id) {
             this.id = id;
             return this;
         }
 
+        /**
+         * Sets a name in this builder.
+         *
+         * @param name name.
+         *
+         * @return instance of this builder.
+         */
         public CaInfoTypeBuilder name(final String name) {
             this.name = name;
             return this;
         }
 
+        /**
+         * Sets a Subject DN in this builder.
+         *
+         * @param subjectDn Subject DN.
+         *
+         * @return instance of this builder.
+         */
         public CaInfoTypeBuilder subjectDn(final String subjectDn) {
             this.subjectDn = subjectDn;
             return this;
         }
 
+        /**
+         * Sets an Issuer DN in this builder.
+         *
+         * @param issuerDn Issuer DN.
+         *
+         * @return instance of this builder.
+         */
         public CaInfoTypeBuilder issuerDn(final String issuerDn) {
             this.issuerDn = issuerDn;
             return this;
         }
 
+        /**
+         * Sets an expiration date in this builder.
+         *
+         * @param expirationDate expiration date.
+         *
+         * @return instance of this builder.
+         */
         public CaInfoTypeBuilder expirationDate(final Date expirationDate) {
             this.expirationDate = expirationDate;
             return this;
         }
 
+        /**
+         * Builds an instance of CaInfoType using this builder.
+         *
+         * @return instance of CaInfoType using this builder.
+         */
         public CaInfoType build() {
             return new CaInfoType(
                     id,
--- modules/ejbca-rest-api/src/org/ejbca/ui/web/rest/common/BaseRestResource.java
+++ modules/ejbca-rest-api/src/org/ejbca/ui/web/rest/common/BaseRestResource.java
@@ -16,19 +16,38 @@ package org.ejbca.ui.web.rest.common;
 import java.security.cert.X509Certificate;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.core.Response;
 
 import org.cesecore.authentication.tokens.AuthenticationToken;
 import org.cesecore.authorization.AuthorizationDeniedException;
 import org.ejbca.core.model.util.EjbLocalHelper;
+import org.ejbca.ui.web.rest.api.types.RestServiceStatusType;
 
 /**
- * Base class for common methods used across all REST end points.
- * @version $Id$
+ * Base class for common methods used across all REST resources.
  *
+ * @version $Id$
  */
 public abstract class BaseRestResource {
-    
-    
+
+    private static final String RESOURCE_STATUS = "OK";
+    private static final String RESOURCE_VERSION = "1.0";
+    private static final String RESOURCE_REVISION = "ALPHA";
+
+    /**
+     * Returns the status information of this resource as Json.
+     *
+     * @return response as Json.
+     */
+    public Response status() {
+        return Response.ok(RestServiceStatusType.builder()
+                .status(RESOURCE_STATUS)
+                .version(RESOURCE_VERSION)
+                .revision(RESOURCE_REVISION)
+                .build()
+        ).build();
+    }
+
     /**
      * Returns an AuthenticationToken for the requesting administrator based on the SSL client certificate
      * @param requestContext HTTP context
