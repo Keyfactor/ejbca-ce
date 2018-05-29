e74315f13e51da9904cf22747b47d2f6ea8916c6
Henrik Sunmark <henrik.sunmark@primekey.com>
2018-05-29 09:26:56 +0000

    ECA-6955: Created response message holder for managed exceptions
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@29053 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ejbca-rest-api/src/org/ejbca/ui/web/rest/api/io/response/ExceptionErrorRestResponse.java
@@ -0,0 +1,127 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.ui.web.rest.api.io.response;
+
+/**
+ * A class representing the information about an exceptional event triggered by REST resource failure.
+ *
+ * @version $Id$
+ */
+public class ExceptionErrorRestResponse {
+
+    // Have to match HTTP Status codes
+    private int errorCode;
+    private String errorMessage;
+
+    /**
+     * Simple constructor.
+     */
+    public ExceptionErrorRestResponse() {}
+
+    private ExceptionErrorRestResponse(final int errorCode, final String errorMessage) {
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+    }
+
+    /**
+     * Return the numeric value of error code.
+     *
+     * @return numeric value of error code.
+     */
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    /**
+     * Sets a numeric value of error code.
+     * <br/>
+     * <b>Note:</b> should match standard HTTP response codes.
+     *
+     * @param errorCode numeric value of error code.
+     */
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    /**
+     * Return the error message.
+     *
+     * @return error message.
+     */
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    /**
+     * Sets an error message.
+     *
+     * @param errorMessage error message.
+     */
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    /**
+     * Return a builder instance for this class.
+     *
+     * @return builder instance for this class.
+     */
+    public static ExceptionErrorTypeBuilder builder() {
+        return new ExceptionErrorTypeBuilder();
+    }
+
+    /**
+     * Builder of this class.
+     */
+    public static class ExceptionErrorTypeBuilder {
+
+        private int errorCode;
+        private String errorMessage;
+
+        ExceptionErrorTypeBuilder() {
+        }
+
+        /**
+         * Sets a numeric value of error code in this builder.
+         *
+         * @param errorCode numeric value of error code.
+         *
+         * @return instance of this builder.
+         */
+        public ExceptionErrorTypeBuilder errorCode(final int errorCode) {
+            this.errorCode = errorCode;
+            return this;
+        }
+
+        /**
+         * Sets an error message in this builder.
+         *
+         * @param errorMessage error message.
+         *
+         * @return instance of this builder.
+         */
+        public ExceptionErrorTypeBuilder errorMessage(final String errorMessage) {
+            this.errorMessage = errorMessage;
+            return this;
+        }
+
+        /**
+         * Builds an instance of ExceptionErrorTypeBuilder using this builder.
+         *
+         * @return instance of ExceptionErrorTypeBuilder using this builder.
+         */
+        public ExceptionErrorRestResponse build() {
+            return new ExceptionErrorRestResponse(errorCode, errorMessage);
+        }
+    }
+}
