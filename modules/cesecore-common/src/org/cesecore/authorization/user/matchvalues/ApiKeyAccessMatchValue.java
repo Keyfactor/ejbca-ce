257d754deaf6bb88055fb7a88a01fd7ff31088a9
Henrik Sunmark <henrik.sunmark@primekey.com>
2018-05-29 07:20:54 +0000

    ECA-6877: Leave out current code until final implementation is complete
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@29044 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-common/src/org/cesecore/authorization/user/matchvalues/ApiKeyAccessMatchValue.java
+++ modules/cesecore-common/src/org/cesecore/authorization/user/matchvalues/ApiKeyAccessMatchValue.java
@@ -11,57 +11,59 @@
  *                                                                       *
  *************************************************************************/
 
+// SUBJECT FOR ECA-6877. LEAVE OUT UNTIL FINAL IMPLEMENTATION IS COMPLETE
+
 package org.cesecore.authorization.user.matchvalues;
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.cesecore.authentication.tokens.ApiKeyAuthenticationTokenMetaData;
-import org.cesecore.authorization.user.AccessMatchType;
-
-/**
- * 
- * @version $Id$
- *
- */
-public enum ApiKeyAccessMatchValue implements AccessMatchValue {
-    API_KEY(0);
-
-    private final int numericValue;
-    
-    private ApiKeyAccessMatchValue(int numericValue) {
-        this.numericValue = numericValue;
-    }
-    
-    @Override
-    public int getNumericValue() {
-        return numericValue;
-    }
-
-    @Override
-    public boolean isDefaultValue() {
-        return numericValue == API_KEY.numericValue;
-    }
-
-    @Override
-    public String getTokenType() {
-        return ApiKeyAuthenticationTokenMetaData.TOKEN_TYPE;
-    }
-
-    @Override
-    public boolean isIssuedByCa() {
-        // Not required for API key
-        return false;
-    }
-
-    @Override
-    public List<AccessMatchType> getAvailableAccessMatchTypes() {
-        // Always use case sensitive match for API keys
-        return Arrays.asList(AccessMatchType.TYPE_EQUALCASE);
-    }
-
-    @Override
-    public String normalizeMatchValue(String value) {
-        return value != null ? value.trim() : null;
-    }
-}
+//import java.util.Arrays;
+//import java.util.List;
+//
+//import org.cesecore.authentication.tokens.ApiKeyAuthenticationTokenMetaData;
+//import org.cesecore.authorization.user.AccessMatchType;
+//
+///**
+// * 
+// * @version $Id$
+// *
+// */
+//public enum ApiKeyAccessMatchValue implements AccessMatchValue {
+//    API_KEY(0);
+//
+//    private final int numericValue;
+//    
+//    private ApiKeyAccessMatchValue(int numericValue) {
+//        this.numericValue = numericValue;
+//    }
+//    
+//    @Override
+//    public int getNumericValue() {
+//        return numericValue;
+//    }
+//
+//    @Override
+//    public boolean isDefaultValue() {
+//        return numericValue == API_KEY.numericValue;
+//    }
+//
+//    @Override
+//    public String getTokenType() {
+//        return ApiKeyAuthenticationTokenMetaData.TOKEN_TYPE;
+//    }
+//
+//    @Override
+//    public boolean isIssuedByCa() {
+//        // Not required for API key
+//        return false;
+//    }
+//
+//    @Override
+//    public List<AccessMatchType> getAvailableAccessMatchTypes() {
+//        // Always use case sensitive match for API keys
+//        return Arrays.asList(AccessMatchType.TYPE_EQUALCASE);
+//    }
+//
+//    @Override
+//    public String normalizeMatchValue(String value) {
+//        return value != null ? value.trim() : null;
+//    }
+//}
