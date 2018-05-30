04dbb694af8cebae279088a606ec0bfd7c6efb3a
Bastian Fredriksson <bastian.fredriksson@primekey.com>
2018-05-30 09:42:10 +0000

    ECA-6920 Added database storage for Acme nonce data.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@29072 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ejbca-entity/src/org/ejbca/acme/AcmeNonceData.java
@@ -0,0 +1,106 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA - Proprietary Modules: Enterprise Certificate Authority        *
+ *                                                                       *
+ *  Copyright (c), PrimeKey Solutions AB. All rights reserved.           *
+ *  The use of the Proprietary Modules are subject to specific           *
+ *  commercial license terms.                                            *
+ *                                                                       *
+ *************************************************************************/
+
+package org.ejbca.acme;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.PostLoad;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.persistence.Version;
+
+import org.cesecore.dbprotection.ProtectedData;
+import org.cesecore.dbprotection.ProtectionStringBuilder;
+
+/**
+ * Storage representation of a used up ACME protocol Replay-Nonce.
+ *
+ * @version $Id$
+ */
+@Entity
+@Table(name = "AcmeNonceData")
+public class AcmeNonceData extends ProtectedData implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String nonce;
+    private long timeExpires;
+    private int rowVersion = 0;
+    private String rowProtection;
+
+    public AcmeNonceData() {}
+
+    public AcmeNonceData(final String nonce, final long timeExpires) {
+        this.setNonce(nonce);
+        this.setTimeExpires(timeExpires);
+    }
+
+    // @Column
+    public String getNonce() { return nonce; }
+    public void setNonce(String nonce) { this.nonce = nonce; }
+
+    // @Column
+    public long getTimeExpires() { return timeExpires; }
+    public void setTimeExpires(long timeExpires) { this.timeExpires = timeExpires; }
+
+    // @Column
+    @Version
+    public int getRowVersion() { return rowVersion; }
+    public void setRowVersion(int rowVersion) { this.rowVersion = rowVersion; }
+
+    // @Column
+    @Override
+    public String getRowProtection() { return rowProtection; }
+    @Override
+    public void setRowProtection(String rowProtection) { this.rowProtection = rowProtection; }
+
+    //
+    // Start Database integrity protection methods
+    //
+
+    @Transient
+    @Override
+    protected String getProtectString(final int version) {
+        // rowVersion is automatically updated by JPA, so it's not important, it is only used for optimistic locking so we will not include that in the database protection
+        return new ProtectionStringBuilder().append(getNonce()).append(getTimeExpires()).toString();
+    }
+
+    @Transient
+    @Override
+    protected int getProtectVersion() {
+        return 1;
+    }
+
+    @PrePersist
+    @PreUpdate
+    @Override
+    protected void protectData() {
+        super.protectData();
+    }
+
+    @PostLoad
+    @Override
+    protected void verifyData() {
+        super.verifyData();
+    }
+
+    @Override
+    @Transient
+    protected String getRowId() {
+        return getNonce();
+    }
+
+    //
+    // End Database integrity protection methods
+    //
+}
