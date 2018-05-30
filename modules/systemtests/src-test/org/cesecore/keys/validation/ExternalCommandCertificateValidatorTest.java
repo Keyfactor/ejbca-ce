12483797650dd55067d4a90d505ae6d6418e9216
andresjakobs <USER@primekey.com>
2018-05-30 08:21:35 +0000

    ECA-7001: Unit test improved, test cases added.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@29066 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/systemtests/src-test/org/cesecore/keys/validation/ExternalCommandCertificateValidatorTest.java
+++ modules/systemtests/src-test/org/cesecore/keys/validation/ExternalCommandCertificateValidatorTest.java
@@ -13,10 +13,14 @@
 
 package org.cesecore.keys.validation;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.io.File;
 import java.security.KeyPair;
 import java.security.cert.X509Certificate;
 import java.util.LinkedHashMap;
+import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.SystemUtils;
@@ -34,7 +38,12 @@ import org.junit.Test;
  * an external command was executed as specified we need to place a file on the fileystem in the location
  * given by the value mapped to the key <code>EXTERNAL_COMMAND</code>, or if we only care about whether
  * the command was invoked, it suffices to ensure the executable does not exist and then check for an
- * <code>ExternalProcessException</code>.
+ * <code>ExternalProcessException</code>. In ECCV this exception is re-thrown as <code>ValidatorNotApplicableException</code>. 
+ * 
+ * Make scripts executable, if not done with SVN extension!
+ * 
+ * chmod u+x $EJBCA_HOME/modules/systemtests/resources/platform/unix/*.sh
+ * 
  * @version $Id$
  */
 public class ExternalCommandCertificateValidatorTest {
@@ -65,11 +74,55 @@ public class ExternalCommandCertificateValidatorTest {
     public void testAllowedCommand() throws Exception {
         final ExternalCommandCertificateValidator validator = new ExternalCommandCertificateValidator();
         final LinkedHashMap<Object, Object> data = new LinkedHashMap<>();
-        final String path = getFilePathFromClasspath("external_process_tools_with_write_to_disk_exit_code_0");
+        final X509Certificate certificate = createCert("C=Test,O=Test,OU=Test,CN=testAllowedCommand");
+        String path = getFilePathFromClasspath("external_process_tools_with_write_to_disk_exit_code_0");
+        
+        // A: Let validation succeed. 
+        // A.1 Sample script logs to error stream with failOnStandardError=false.
         data.put(ExternalCommandCertificateValidator.EXTERNAL_COMMAND, path);
+        data.put(ExternalCommandCertificateValidator.FAIL_ON_STANDARD_ERROR, false);
         data.put(UpgradeableDataHashMap.VERSION, 1f);
         validator.setDataMap(data);
-        validator.validate(null, createCert("C=Test,O=Test,OU=Test,CN=testAllowedCommand"), new ExternalScriptsWhitelist(path));
+        List<String> out = validator.validate(null, certificate, new ExternalScriptsWhitelist(path));
+        assertEquals("Validation with external script logged to ERROUT with failOnStandardError=false must succeed.", out.size(), 0);
+        
+        // B: Let validation fail (result list size > 0).
+        // B.1 Sample script logs to error stream with failOnStandardError=true.
+        data.put(ExternalCommandCertificateValidator.FAIL_ON_STANDARD_ERROR, true);
+        validator.setDataMap(data);
+        out = validator.validate(null, certificate, new ExternalScriptsWhitelist(path));
+        assertTrue("Validation with external script logged to ERROUT with failOnStandardError=true must have failed.", out.size() > 0);
+        log.info( "ECCV validation called with result: " + out);
+        
+//        Does not work as expected.
+//        // B.2 Sample script logs to error stream with failOnStandardError=true but logStandardOut=false and logErrorOut=false.
+//        data.put(ExternalCommandCertificateValidator.FAIL_ON_STANDARD_ERROR, true);
+//        data.put(ExternalCommandCertificateValidator.LOG_STANDARD_OUT, false);
+//        data.put(ExternalCommandCertificateValidator.LOG_ERROR_OUT, false);
+//        validator.setDataMap(data);
+//        out = validator.validate(null, certificate, new ExternalScriptsWhitelist(path));
+//        assertTrue("Validation with external script logged to ERROUT with failOnStandardError=true but logStandardOut=false and logErrorOut=false must have failed as well.", out.size() > 0);
+//        log.info( "ECCV validation called with result: " + out);
+        
+        // B.3 Sample scripts return code is larger than 0 with failOnStandardError=true.
+        path = getFilePathFromClasspath("external_process_tools_with_write_to_disk_exit_code_1");
+        data.put(ExternalCommandCertificateValidator.EXTERNAL_COMMAND, path);
+        data.put(ExternalCommandCertificateValidator.FAIL_ON_STANDARD_ERROR, false);
+        data.put(ExternalCommandCertificateValidator.FAIL_ON_ERROR_CODE, true);
+        data.put(ExternalCommandCertificateValidator.LOG_STANDARD_OUT, true);
+        data.put(ExternalCommandCertificateValidator.LOG_ERROR_OUT, true);
+        validator.setDataMap(data);
+        out = validator.validate(null, certificate, new ExternalScriptsWhitelist(path));
+        assertTrue("Validation with external script logged to ERROUT with failOnStandardError=true must have failed.", out.size() > 0);
+        log.info( "ECCV validation called with result: " + out);
+        
+        // B.4 Sample scripts return code is larger than 0 with failOnStandardError=true, logStandardOut=false and logErrorOut=false. 
+        data.put(ExternalCommandCertificateValidator.LOG_STANDARD_OUT, false);
+        data.put(ExternalCommandCertificateValidator.LOG_ERROR_OUT, false);
+        validator.setDataMap(data);
+        out = validator.validate(null, certificate, new ExternalScriptsWhitelist(path));
+        assertTrue("Validation with external script logged to ERROUT with failOnStandardError=true, logStandardOut=false and logErrorOut=false must have failed as well.", out.size() > 0);
+        log.info( "ECCV validation called with result: " + out);
     }
 
     @Test
@@ -80,7 +133,8 @@ public class ExternalCommandCertificateValidatorTest {
         data.put(ExternalCommandCertificateValidator.EXTERNAL_COMMAND, path + " param1 param2");
         data.put(UpgradeableDataHashMap.VERSION, 1f);
         validator.setDataMap(data);
-        validator.validate(null, createCert("C=Test,O=Test,OU=Test,CN=testAllowedCommandWithParameters"), new ExternalScriptsWhitelist(path));
+        final List<String> out = validator.validate(null, createCert("C=Test,O=Test,OU=Test,CN=testAllowedCommandWithParameters"), new ExternalScriptsWhitelist(path));
+        log.info( "External script called with result: " + out);
     }
 
     @Test(expected = ValidatorNotApplicableException.class)
@@ -88,10 +142,10 @@ public class ExternalCommandCertificateValidatorTest {
         final ExternalCommandCertificateValidator validator = new ExternalCommandCertificateValidator();
         final String path = getFilePathFromClasspath("external_process_tools_with_write_to_disk_exit_code_0");
         final LinkedHashMap<Object, Object> data = new LinkedHashMap<>();
-        data.put(ExternalCommandCertificateValidator.EXTERNAL_COMMAND, path + "/foo/forbidden");
+        data.put(ExternalCommandCertificateValidator.EXTERNAL_COMMAND, path);
         data.put(UpgradeableDataHashMap.VERSION, 1f);
         validator.setDataMap(data);
-        validator.validate(null, null, new ExternalScriptsWhitelist(path));
+        validator.validate(null, null, new ExternalScriptsWhitelist(path + "/foo/doesnotexist"));
     }
 
     @Test(expected = ValidatorNotApplicableException.class)
@@ -99,7 +153,18 @@ public class ExternalCommandCertificateValidatorTest {
         final ExternalCommandCertificateValidator validator = new ExternalCommandCertificateValidator();
         final String path = getFilePathFromClasspath("external_process_tools_with_write_to_disk_exit_code_0");
         final LinkedHashMap<Object, Object> data = new LinkedHashMap<>();
-        data.put(ExternalCommandCertificateValidator.EXTERNAL_COMMAND, path + "/foo/forbidden param1 param2");
+        data.put(ExternalCommandCertificateValidator.EXTERNAL_COMMAND, path + " param1 param2");
+        data.put(UpgradeableDataHashMap.VERSION, 1f);
+        validator.setDataMap(data);
+        validator.validate(null, null, new ExternalScriptsWhitelist(path + "/foo/doesnotexist"));
+    }
+    
+    @Test(expected = ValidatorNotApplicableException.class)
+    public void testCommandNotFound() throws Exception {
+        final ExternalCommandCertificateValidator validator = new ExternalCommandCertificateValidator();
+        final String path = getFilePathFromClasspath("external_process_tools_with_write_to_disk_exit_code_0") + "/foo/doesnotexist";
+        final LinkedHashMap<Object, Object> data = new LinkedHashMap<>();
+        data.put(ExternalCommandCertificateValidator.EXTERNAL_COMMAND, path);
         data.put(UpgradeableDataHashMap.VERSION, 1f);
         validator.setDataMap(data);
         validator.validate(null, null, new ExternalScriptsWhitelist(path));
