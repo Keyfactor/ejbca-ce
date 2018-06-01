4d7f1aee08b21a6c9a02a612238bc9d6b8b34b32
Andrey Sergeev <andrey.sergeev.ext@primekey.com>
2018-06-01 12:56:39 +0000

    ECA-6957 REST system tests - Initial implementation of System tests for REST API
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@29101 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ejbca-rest-api/src-test/org/ejbca/ui/web/rest/api/resource/CertificateRestResourceSystemTest.java
@@ -0,0 +1,46 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.ui.web.rest.api.resource;
+
+import org.jboss.resteasy.client.ClientResponse;
+import org.junit.Test;
+
+import javax.ws.rs.core.Response;
+
+import static org.ejbca.ui.web.rest.api.Assert.EjbcaAssert.assertJsonContentType;
+import static org.ejbca.ui.web.rest.api.Assert.EjbcaAssert.assertProperJsonStatusResponse;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * A unit test class for CertificateRestResource to test its content.
+ *
+ * @version $Id: CertificateRestResourceSystemTest.java 29080 2018-05-31 11:12:13Z andrey_s_helmes $
+ */
+public class CertificateRestResourceSystemTest extends RestResourceSystemTestBase {
+
+    @Test
+    public void shouldReturnStatusInformation() throws Exception {
+        // given
+        final String expectedStatus = "OK";
+        final String expectedVersion = "1.0";
+        final String expectedRevision = "ALPHA";
+        // when
+        final ClientResponse<?> actualResponse = newRequest("/v1/certificate/status").get();
+        final String actualJsonString = actualResponse.getEntity(String.class);
+        // then
+        assertEquals(Response.Status.OK.getStatusCode(), actualResponse.getStatus());
+        assertJsonContentType(actualResponse);
+        assertProperJsonStatusResponse(expectedStatus, expectedVersion, expectedRevision, actualJsonString);
+    }
+
+}
