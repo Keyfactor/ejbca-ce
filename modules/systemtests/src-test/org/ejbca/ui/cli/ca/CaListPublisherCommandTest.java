ee8eb1d1bb93e8b047f30f079bb25faa827fe0fe
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2018-06-07 08:04:54 +0000

    ECA-7035: add a new listpublishers command to the CA CLI.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@29129 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/systemtests/src-test/org/ejbca/ui/cli/ca/CaListPublisherCommandTest.java
@@ -0,0 +1,50 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.ui.cli.ca;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.cesecore.util.CryptoProviderTools;
+import org.ejbca.ui.cli.infrastructure.command.CommandResult;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @version $Id$
+ *
+ */
+public class CaListPublisherCommandTest {
+
+    private CaListPublishersCommand caListPublishersCommand;
+
+    @Before
+    public void setUp() throws Exception {
+        CryptoProviderTools.installBCProviderIfNotAvailable();
+        caListPublishersCommand = new CaListPublishersCommand();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testSanity() {
+        try {
+            assertEquals("CA listpublishers command is broken.", CommandResult.SUCCESS, caListPublishersCommand.execute());
+        } catch (Exception e) {
+            fail("CA listpublishers command is broken: " + e.getMessage());
+        }
+    }
+}
