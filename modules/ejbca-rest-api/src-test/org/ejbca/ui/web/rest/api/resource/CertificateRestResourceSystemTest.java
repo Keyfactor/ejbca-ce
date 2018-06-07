ff51e3a4ff9f208d23d29338cab105b6b8b2bf0b
Andrey Sergeev <andrey.sergeev.ext@primekey.com>
2018-06-07 12:28:58 +0000

    ECA-6957 REST system tests: - Imports fix
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@29140 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-rest-api/src-test/org/ejbca/ui/web/rest/api/resource/CertificateRestResourceSystemTest.java
+++ modules/ejbca-rest-api/src-test/org/ejbca/ui/web/rest/api/resource/CertificateRestResourceSystemTest.java
@@ -13,7 +13,11 @@
 package org.ejbca.ui.web.rest.api.resource;
 
 import org.jboss.resteasy.client.ClientResponse;
-import org.junit.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 import javax.ws.rs.core.Response;
 
