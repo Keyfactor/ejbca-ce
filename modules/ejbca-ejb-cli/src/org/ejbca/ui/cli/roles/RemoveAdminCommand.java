5b23df504e6b37a58fd6f1a30d7569270e06e08e
Johan Eklund <johan.eklund@primekey.com>
2018-06-11 11:13:01 +0000

    ECA-7038: Extend EJBCA EJB CLI to support adding RoleMembers of any registered UI-configurable tokenType.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@29199 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb-cli/src/org/ejbca/ui/cli/roles/RemoveAdminCommand.java
+++ modules/ejbca-ejb-cli/src/org/ejbca/ui/cli/roles/RemoveAdminCommand.java
@@ -13,7 +13,9 @@
 
 package org.ejbca.ui.cli.roles;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
+import org.cesecore.authentication.tokens.AuthenticationTokenMetaData;
 import org.cesecore.authentication.tokens.X509CertificateAuthenticationTokenMetaData;
 import org.cesecore.authorization.AuthorizationDeniedException;
 import org.cesecore.authorization.user.AccessMatchType;
@@ -83,23 +85,53 @@ public class RemoveAdminCommand extends BaseRolesCommand {
             getLogger().error("No such role " + super.getFullRoleName(namespace, roleName) + ".");
             return CommandResult.FUNCTIONAL_FAILURE;
         }
-        final String caName = parameters.get(CA_NAME_KEY);
-        final CAInfo caInfo;
-        try {
-            caInfo = EjbRemoteHelper.INSTANCE.getRemoteSession(CaSessionRemote.class).getCAInfo(getAuthenticationToken(), caName);
-        }  catch (AuthorizationDeniedException e) {
-            getLogger().error("CLI user not authorized to CA '" + caName + "'.");
-            return CommandResult.AUTHORIZATION_FAILURE;
-        }
-        if (caInfo == null) {
-            getLogger().error("No such CA '" + caName + "'.");
+        final String matchWithKeyParam = parameters.get(MATCH_WITH_KEY);
+        if (StringUtils.isEmpty(matchWithKeyParam)) {
+            getLogger().error("No such thing to match with as '" + parameters.get(MATCH_WITH_KEY) + "'.");
             return CommandResult.FUNCTIONAL_FAILURE;
         }
-        final String tokenType = X509CertificateAuthenticationTokenMetaData.TOKEN_TYPE;
-        final AccessMatchValue accessMatchValue = AccessMatchValueReverseLookupRegistry.INSTANCE.lookupMatchValueFromTokenTypeAndName(tokenType,
-                parameters.get(MATCH_WITH_KEY));
+        final String tokenType;
+        final String matchWithKey;
+        final String[] matchWithKeySplit = matchWithKeyParam.split(":");
+        if (matchWithKeySplit.length == 1) {
+            tokenType = X509CertificateAuthenticationTokenMetaData.TOKEN_TYPE;
+            matchWithKey = matchWithKeySplit[0];
+            getLogger().info("Match TokenType is assumed to be '" + tokenType + "'.");
+        } else {
+            tokenType = matchWithKeySplit[0];
+            matchWithKey = matchWithKeySplit[1];
+            final AuthenticationTokenMetaData authenticationTokenMetaData = AccessMatchValueReverseLookupRegistry.INSTANCE.getMetaData(tokenType);
+            if (authenticationTokenMetaData == null || !authenticationTokenMetaData.isUserConfigurable()) {
+                getLogger().error("TokenType '" + tokenType + "' is not configurable.");
+                return CommandResult.FUNCTIONAL_FAILURE;
+            }
+        }
+        final int tokenIssuerId;
+        final String caName;
+        if (X509CertificateAuthenticationTokenMetaData.TOKEN_TYPE.equals(tokenType)) {
+            final String caNameParam = parameters.get(CA_NAME_KEY);
+            final CAInfo caInfo;
+            try {
+                caInfo = EjbRemoteHelper.INSTANCE.getRemoteSession(CaSessionRemote.class).getCAInfo(getAuthenticationToken(), caNameParam);
+            }  catch (AuthorizationDeniedException e) {
+                getLogger().error("CLI user not authorized to CA");
+                return CommandResult.AUTHORIZATION_FAILURE;
+            }
+            if (caInfo == null) {
+                getLogger().error("No such CA '" + caNameParam + "'.");
+                return CommandResult.FUNCTIONAL_FAILURE;
+            }
+            tokenIssuerId = caInfo.getCAId();
+            caName = caInfo.getName();
+        } else {
+            // To be backwards compatible with existing scripts in the wild, we will still have to require parameter but we ignore the value if is unused
+            //getLogger().info("Ignoring mandatory '" + CA_NAME_KEY + "' parameter value for the selected Token Type '" + tokenType + "'.");
+            tokenIssuerId = RoleMember.NO_ISSUER;
+            caName = "-";
+        }
+        final AccessMatchValue accessMatchValue = AccessMatchValueReverseLookupRegistry.INSTANCE.lookupMatchValueFromTokenTypeAndName(tokenType, matchWithKey);
         if (accessMatchValue == null) {
-            getLogger().error("No such thing to match with as '" + parameters.get(MATCH_WITH_KEY) + "'.");
+            getLogger().error("No such thing to match with as '" + matchWithKey + "'.");
             return CommandResult.FUNCTIONAL_FAILURE;
         }
         final String accessMatchTypeParam = parameters.get(MATCH_TYPE_KEY);
@@ -119,13 +151,12 @@ public class RemoveAdminCommand extends BaseRolesCommand {
             }
         }
         final String tokenMatchValue = parameters.get(MATCH_VALUE_KEY);
-        final int caId = caInfo.getCAId();
         final RoleMemberSessionRemote roleMemberSession = EjbRemoteHelper.INSTANCE.getRemoteSession(RoleMemberSessionRemote.class);
         try {
             boolean foundMatch = false;
             for (final RoleMember roleMember : roleMemberSession.getRoleMembersByRoleId(getAuthenticationToken(), role.getRoleId())) {
                 if (tokenType.equals(roleMember.getTokenType()) &&
-                        caId == roleMember.getTokenIssuerId() &&
+                        tokenIssuerId == roleMember.getTokenIssuerId() &&
                         accessMatchValue.getNumericValue()==roleMember.getTokenMatchKey() &&
                         accessMatchType.getNumericValue()==roleMember.getTokenMatchOperator() &&
                         tokenMatchValue.equals(roleMember.getTokenMatchValue())) {
