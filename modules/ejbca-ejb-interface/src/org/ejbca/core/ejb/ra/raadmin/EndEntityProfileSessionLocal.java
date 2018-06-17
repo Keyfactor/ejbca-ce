0be12096fdc6cf37bf11dc99090152a694bbc4b6
andresjakobs <USER@primekey.com>
2018-06-17 06:14:19 +0000

    ECA-6971: Try getAvailableCAsInProfile for first instance, best practices for RaMasterApi applied.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@29245 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb-interface/src/org/ejbca/core/ejb/ra/raadmin/EndEntityProfileSessionLocal.java
+++ modules/ejbca-ejb-interface/src/org/ejbca/core/ejb/ra/raadmin/EndEntityProfileSessionLocal.java
@@ -55,10 +55,27 @@ public interface EndEntityProfileSessionLocal extends EndEntityProfileSession {
      * @param admin the authentication of the caller.
      * @param entityProfileId id of the end entity profile.
      * @return a map of available certificate profiles names and IDs or an empty map.
-     * @throws EjbcaException if an error occured.
      * @throws AuthorizationDeniedException if the authorization was denied.
+     * @throws EjbcaException if an error occured.
      */
     Map<String, Integer> getAvailableCertificateProfiles(AuthenticationToken admin, int entityProfileId) throws AuthorizationDeniedException, EjbcaException;
+    
+    /**
+     * Fetches the available CAs associated with an end entity profile.
+     *
+     * Authorization requirements:<pre>
+     * - /administrator
+     * - /endentityprofilesrules/&lt;end entity profile&gt;
+     * </pre>
+     *
+     * @param admin the authentication of the caller.
+     * @param entityProfileId id of the end entity profile.
+     * @return a map of available CA names and IDs or an empty map.
+     * @throws AuthorizationDeniedException if the authorization was denied.
+     * @throws EjbcaException any EjbcaException.
+     */
+    Map<String,Integer> getAvailableCAsInProfile(AuthenticationToken admin, final int entityProfileId) throws AuthorizationDeniedException, EjbcaException;
+    
     
     /** WARNING: This method must only be used when doing read_only operation on the profile. Otherwise
      * any changes to the profile will affect the profile in the cache and thus affect all other threads.
