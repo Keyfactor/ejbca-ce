f6d72bd42ece08c5bb6cb6b897f6738d22b198d5
Jekaterina Bunina <jekaterina.Bunina.ext@primekey.com>
2018-06-25 14:06:40 +0000

    ECA-7029 add rest api swagger documentation to docs.war
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@29333 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ lib/ext/swagger docs/readme.txt
@@ -0,0 +1,3 @@
+"Project";                      "Library Name";                                         "License type(s)";      "License text";             "Source";                                                                   "Comment";
+"Swagger Codegen (executable)"; "swagger-codegen-cli-2.3.1";                            "ALv2";                 "LICENSE-APACHE-2.0";       "http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.3.1/";    "Needed for Rest Api documentation generation";
+"Swaggerdocgen Maven Plug-in";  "swaggerdocgen-maven-plugin-1.2-jar-with-dependencies"; "ALv2";                 "LICENSE-APACHE-2.0";       "https://github.com/WASdev/tool.swagger.docgen";                            "Needed for Rest Api documentation generation";
--- modules/ejbca-rest-api/src/org/ejbca/ui/web/rest/api/RestApiApplication.java
+++ modules/ejbca-rest-api/src/org/ejbca/ui/web/rest/api/RestApiApplication.java
@@ -12,6 +12,9 @@ package org.ejbca.ui.web.rest.api;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.Info;
+import io.swagger.annotations.SwaggerDefinition;
 import org.ejbca.ui.web.rest.api.config.SnakeCaseConverter;
 
 import io.swagger.converter.ModelConverters;
@@ -24,6 +27,14 @@ import io.swagger.jaxrs.config.BeanConfig;
  *
  */
 @ApplicationPath("/")
+@Api(value = "Certificate Rest Management API")
+@SwaggerDefinition(info =
+@Info(
+        title = "Certificate Management REST Interface",
+        version = "1.0.0",
+        description = "API reference documentation."
+)
+)
 public class RestApiApplication extends Application {
     // Nothing here for now so RESTEasy takes care of registering end points automatically.
     // Later if manual control over some resources required those could be added here.
@@ -38,7 +49,7 @@ public class RestApiApplication extends Application {
         beanConfig.setVersion("1.0.0");
         beanConfig.setSchemes(new String[]{"https"});
         beanConfig.setBasePath("/ejbca/ejbca-rest-api");
-        beanConfig.setResourcePackage("org.ejbca.ui.web.rest.api.resource");
+        beanConfig.setResourcePackage("org.ejbca.ui.web.rest.api");
         beanConfig.setScan(true);
     }
 
