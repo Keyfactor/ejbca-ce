ab4dc9582eae35afa68a5d8e8eb68cda120173f1
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2018-06-27 13:08:41 +0000

    Noticket: Fix message for non-existent custom extensions
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@29380 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/admin-gui/src/org/ejbca/ui/web/admin/certprof/CertProfileBean.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/certprof/CertProfileBean.java
@@ -1175,7 +1175,7 @@ public class CertProfileBean extends BaseManagedBean implements Serializable {
             }
             for (int id : usedExtensions) {
                 if (!cceConfig.isCustomCertExtensionSupported(id)) {
-                    String note = id + " (No longer used. Please unselect this option)";
+                    String note = "ID #" + id + " (No longer used. Please unselect this option)";
                     ret.add(new SelectItem(id, note));
                 }
             }
