5b8f47681497e4e7cb51f46a0b2f800097dbeb89
Jekaterina Bunina <jekaterina.Bunina.ext@primekey.com>
2018-07-03 09:48:46 +0000

    ECA-6932 add URL validator
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@29438 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/UrlValidator.java
@@ -0,0 +1,49 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
+package org.ejbca.ui.web.admin;
+
+import org.apache.log4j.Logger;
+import org.ejbca.ui.web.admin.configuration.EjbcaJSFHelper;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/** JSF validator to check that input fields are valid urls
+ *
+ * @version $Id: UrlValidator.java 28844 2018-05-04 08:31:02Z samuellb $
+ */
+public class UrlValidator implements Validator {
+    private static final Logger log = Logger.getLogger(UrlValidator.class);
+
+    @Override
+    public void validate(FacesContext facesContext, UIComponent uiComponent, Object o) throws ValidatorException {
+        StringBuilder url = new StringBuilder();
+        String urlValue = o.toString();
+
+        log.debug("Validating component " + uiComponent.getClientId(facesContext) + " with value \"" + urlValue + "\"");
+
+        url.append(urlValue);
+        try {
+            new URI(url.toString());
+        } catch (URISyntaxException e) {
+            final String msg = EjbcaJSFHelper.getBean().getEjbcaWebBean().getText("INVALIDURL");
+            throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR, msg, null));
+        }
+    }
+}
