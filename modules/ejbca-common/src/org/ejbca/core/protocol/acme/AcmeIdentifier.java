1c515e1494113ea326c1daea67269805d63e8121
Mike Agrenius Kushner <mike.kushner@primekey.com>
2018-08-07 15:25:52 +0000

    ECA-6921: Added basic persistence for  AcmeAccountData.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@29587 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ejbca-common/src/org/ejbca/core/protocol/acme/AcmeIdentifier.java
@@ -0,0 +1,35 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.core.protocol.acme;
+
+/**
+ * An ACME identifier is what the client requests the CA to certify.
+ * Only the type DNS name exists so far (RFC draft 06).
+ * 
+ * @version $Id$
+ */
+public interface AcmeIdentifier {
+
+    String getType();
+
+    void setType(String type);
+
+    String getValue();
+
+    void setValue(String value);
+
+    int hashCode();
+
+    boolean equals(Object obj);
+
+}
