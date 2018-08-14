423ca85a350fb6e8c894b0bb89f76620d98c08fc
Mike Agrenius Kushner <mike.kushner@primekey.com>
2018-08-14 08:55:21 +0000

    ECA-7185: Moved some methods from Remote to Local interfaces
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@29630 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/systemtests/src/org/ejbca/core/protocool/acme/AcmeAccountDataSessionProxyBean.java
@@ -0,0 +1,46 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.core.protocool.acme;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+
+import org.cesecore.jndi.JndiConstants;
+import org.ejbca.core.protocol.acme.AcmeAccount;
+import org.ejbca.core.protocol.acme.AcmeAccountDataSessionLocal;
+import org.ejbca.core.protocol.acme.AcmeAccountDataSessionProxyRemote;
+
+/**
+ * @version $Id$
+ *
+ */
+@Stateless(mappedName = JndiConstants.APP_JNDI_PREFIX + "AcmeAccountDataSessionProxyRemote")
+@TransactionAttribute(TransactionAttributeType.SUPPORTS)
+public class AcmeAccountDataSessionProxyBean implements AcmeAccountDataSessionProxyRemote {
+
+    @EJB
+    private AcmeAccountDataSessionLocal acmeAccountDataSession;
+
+    @Override
+    public String createOrUpdate(AcmeAccount acmeAccount) {
+        return acmeAccountDataSession.createOrUpdate(acmeAccount);
+    }
+
+    @Override
+    public void remove(String accountId) {
+        acmeAccountDataSession.remove(accountId);
+    }
+
+}
