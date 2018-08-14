4fb265c5cb549606f660cc927072e68ebaa49ce9
Tarmo Raudsep <tarmo.raudsep.ext@primekey.com>
2018-08-14 13:50:21 +0000

    ECA-7198 ACME persistence: Create ORM scripts/entities/CRUD for AcmeOrderData
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@29640 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/systemtests/src/org/ejbca/core/protocool/acme/AcmeOrderDataSessionProxyBean.java
@@ -0,0 +1,45 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.core.protocool.acme;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+
+import org.cesecore.jndi.JndiConstants;
+import org.ejbca.core.protocol.acme.AcmeOrder;
+import org.ejbca.core.protocol.acme.AcmeOrderDataSessionLocal;
+import org.ejbca.core.protocol.acme.AcmeOrderDataSessionProxyRemote;
+
+/**
+ * @version $Id: AcmeOrderDataSessionProxyBean.java 29630 2018-08-14 08:55:21Z tarmor $
+ *
+ */
+@Stateless(mappedName = JndiConstants.APP_JNDI_PREFIX + "AcmeOrderDataSessionProxyRemote")
+@TransactionAttribute(TransactionAttributeType.SUPPORTS)
+public class AcmeOrderDataSessionProxyBean implements AcmeOrderDataSessionProxyRemote {
+
+    @EJB
+    private AcmeOrderDataSessionLocal acmeOrderDataSession;
+
+    @Override
+    public String createOrUpdate(AcmeOrder acmeOrder) {
+        return acmeOrderDataSession.createOrUpdate(acmeOrder);
+    }
+
+    @Override
+    public void remove(String orderId) {
+        acmeOrderDataSession.remove(orderId);
+    }
+}
