cee82216f9f704586276f1f469e9f7e44f1fc160
Jekaterina Bunina <jekaterina.Bunina.ext@primekey.com>
2018-08-22 07:32:34 +0000

    ECA-7199 add license header to AcmeAuthorization classes
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@29714 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-entity/src/org/ejbca/acme/AcmeAuthorizationData.java
+++ modules/ejbca-entity/src/org/ejbca/acme/AcmeAuthorizationData.java
@@ -1,3 +1,12 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA - Proprietary Modules: Enterprise Certificate Authority        *
+ *                                                                       *
+ *  Copyright (c), PrimeKey Solutions AB. All rights reserved.           *
+ *  The use of the Proprietary Modules are subject to specific           *
+ *  commercial license terms.                                            *
+ *                                                                       *
+ *************************************************************************/
 package org.ejbca.acme;
 
 import java.beans.XMLDecoder;
