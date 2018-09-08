133a020a70d7a4ed80960d7b70db5318db35c442
Amin Khorsandi <amin.khorsandi@primekey.com>
2018-09-08 10:08:29 +0000

    ECA-7269: Remove beans.xml and certificate rest service layer.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@29840 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-rest-api/src/org/ejbca/ui/web/rest/api/resource/CertificateRestResource.java
+++ modules/ejbca-rest-api/src/org/ejbca/ui/web/rest/api/resource/CertificateRestResource.java
@@ -26,12 +26,14 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
-import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -52,6 +54,7 @@ import org.cesecore.CesecoreException;
 import org.cesecore.authentication.tokens.AuthenticationToken;
 import org.cesecore.authorization.AuthorizationDeniedException;
 import org.cesecore.certificates.ca.CADoesntExistsException;
+import org.cesecore.certificates.ca.CAInfo;
 import org.cesecore.certificates.certificate.CertificateStatus;
 import org.cesecore.certificates.crl.RevocationReasons;
 import org.cesecore.certificates.endentity.EndEntityConstants;
@@ -72,6 +75,8 @@ import org.ejbca.core.model.approval.ApprovalRequest;
 import org.ejbca.core.model.approval.WaitingForApprovalException;
 import org.ejbca.core.model.approval.approvalrequests.KeyRecoveryApprovalRequest;
 import org.ejbca.core.model.era.RaApprovalRequestInfo;
+import org.ejbca.core.model.era.RaCertificateSearchRequest;
+import org.ejbca.core.model.era.RaCertificateSearchResponse;
 import org.ejbca.core.model.era.RaMasterApiProxyBeanLocal;
 import org.ejbca.core.model.ra.AlreadyRevokedException;
 import org.ejbca.core.model.ra.NotFoundException;
@@ -81,6 +86,7 @@ import org.ejbca.ui.web.rest.api.exception.RestException;
 import org.ejbca.ui.web.rest.api.io.request.EnrollCertificateRestRequest;
 import org.ejbca.ui.web.rest.api.io.request.FinalizeRestRequest;
 import org.ejbca.ui.web.rest.api.io.request.KeyStoreRestRequest;
+import org.ejbca.ui.web.rest.api.io.request.SearchCertificateCriteriaRestRequest;
 import org.ejbca.ui.web.rest.api.io.request.SearchCertificatesRestRequest;
 import org.ejbca.ui.web.rest.api.io.response.CertificateRestResponse;
 import org.ejbca.ui.web.rest.api.io.response.CertificatesRestResponse;
@@ -93,7 +99,6 @@ import org.ejbca.ui.web.rest.api.io.response.SearchCertificatesRestResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import org.ejbca.ui.web.rest.api.service.CertificateRestService;
 
 /**
  * JAX-RS resource handling certificate-related requests.
@@ -114,9 +119,6 @@ public class CertificateRestResource extends BaseRestResource {
 
     @EJB
     private RaMasterApiProxyBeanLocal raMasterApi;
-
-    @Inject
-    private CertificateRestService certificateRestService;
 
     @GET
     @Path("/status")
@@ -450,8 +452,126 @@ public class CertificateRestResource extends BaseRestResource {
     ) throws AuthorizationDeniedException, RestException, CertificateEncodingException {
         final AuthenticationToken authenticationToken = getAdmin(requestContext, true);
         validateObject(searchCertificatesRestRequest);
-        certificateRestService.authorizeSearchCertificatesRestRequestReferences(authenticationToken, searchCertificatesRestRequest);
-        final SearchCertificatesRestResponse searchCertificatesRestResponse = certificateRestService.searchCertificates(authenticationToken, searchCertificatesRestRequest);
+        authorizeSearchCertificatesRestRequestReferences(authenticationToken, searchCertificatesRestRequest);
+        final SearchCertificatesRestResponse searchCertificatesRestResponse = searchCertificates(authenticationToken, searchCertificatesRestRequest);
         return Response.ok(searchCertificatesRestResponse).build();
+    }
+    
+    /**
+     * Authorizes the input search request for proper access references (End entity profile ids, Certificate profile ids and CA ids) inside a request.
+     *
+     * @param authenticationToken authentication token to use.
+     * @param searchCertificatesRestRequest input search request.
+     * @throws RestException In case of inaccessible reference usage.
+     */
+    private void authorizeSearchCertificatesRestRequestReferences(
+            final AuthenticationToken authenticationToken,
+            final SearchCertificatesRestRequest searchCertificatesRestRequest
+    ) throws RestException {
+        Map<Integer, String> availableEndEntityProfiles = new HashMap<>();
+        Map<Integer, String> availableCertificateProfiles = new HashMap<>();
+        Map<Integer, String> availableCAs = new HashMap<>();
+        for(SearchCertificateCriteriaRestRequest searchCertificateCriteriaRestRequest : searchCertificatesRestRequest.getCriteria()) {
+            final SearchCertificateCriteriaRestRequest.CriteriaProperty criteriaProperty = SearchCertificateCriteriaRestRequest.CriteriaProperty.resolveCriteriaProperty(searchCertificateCriteriaRestRequest.getProperty());
+            if(criteriaProperty == null) {
+                throw new RestException(
+                        Response.Status.BAD_REQUEST.getStatusCode(),
+                        "Invalid search criteria content."
+                );
+            }
+            switch (criteriaProperty) {
+                case END_ENTITY_PROFILE:
+                    availableEndEntityProfiles = loadAuthorizedEndEntityProfiles(authenticationToken, availableEndEntityProfiles);
+                    final String criteriaEndEntityProfileName = searchCertificateCriteriaRestRequest.getValue();
+                    final Integer criteriaEndEntityProfileId = getKeyFromMapByValue(availableEndEntityProfiles, criteriaEndEntityProfileName);
+                    if(criteriaEndEntityProfileId == null) {
+                        throw new RestException(
+                                Response.Status.BAD_REQUEST.getStatusCode(),
+                                "Invalid search criteria content, unknown end entity profile."
+                        );
+                    }
+                    searchCertificateCriteriaRestRequest.setIdentifier(criteriaEndEntityProfileId);
+                    break;
+                case CERTIFICATE_PROFILE:
+                    availableCertificateProfiles = loadAuthorizedCertificateProfiles(authenticationToken, availableCertificateProfiles);
+                    final String criteriaCertificateProfileName = searchCertificateCriteriaRestRequest.getValue();
+                    final Integer criteriaCertificateProfileId = getKeyFromMapByValue(availableCertificateProfiles, criteriaCertificateProfileName);
+                    if(criteriaCertificateProfileId == null) {
+                        throw new RestException(
+                                Response.Status.BAD_REQUEST.getStatusCode(),
+                                "Invalid search criteria content, unknown certificate profile."
+                        );
+                    }
+                    searchCertificateCriteriaRestRequest.setIdentifier(criteriaCertificateProfileId);
+                    break;
+                case CA:
+                    availableCAs = loadAuthorizedCAs(authenticationToken, availableCAs);
+                    final String criteriaCAName = searchCertificateCriteriaRestRequest.getValue();
+                    final Integer criteriaCAId = getKeyFromMapByValue(availableCAs, criteriaCAName);
+                    if(criteriaCAId == null) {
+                        throw new RestException(
+                                Response.Status.BAD_REQUEST.getStatusCode(),
+                                "Invalid search criteria content, unknown CA."
+                        );
+                    }
+                    searchCertificateCriteriaRestRequest.setIdentifier(criteriaCAId);
+                    break;
+                default:
+                    // Do nothing
+            }
+        }
+    }
+    
+    /**
+     * Searches for certificates within given criteria.
+     *
+     * @param authenticationToken authentication token to use.
+     * @param searchCertificatesRestRequest search criteria.
+     * @return Search results.
+     * @throws RestException In case of malformed criteria.
+     * @throws CertificateEncodingException In case of failure in certificate reading.
+     */
+    private SearchCertificatesRestResponse searchCertificates(
+            final AuthenticationToken authenticationToken,
+            final SearchCertificatesRestRequest searchCertificatesRestRequest
+    ) throws RestException, CertificateEncodingException {
+        final RaCertificateSearchRequest raCertificateSearchRequest = SearchCertificatesRestRequest.converter().toEntity(searchCertificatesRestRequest);
+        final RaCertificateSearchResponse raCertificateSearchResponse = raMasterApi.searchForCertificates(authenticationToken, raCertificateSearchRequest);
+        return SearchCertificatesRestResponse.converter().toRestResponse(raCertificateSearchResponse);
+    }
+
+    private Map<Integer, String> loadAuthorizedEndEntityProfiles(final AuthenticationToken authenticationToken, final  Map<Integer, String> availableEndEntityProfiles) {
+        if(availableEndEntityProfiles.isEmpty()) {
+            return raMasterApi.getAuthorizedEndEntityProfileIdsToNameMap(authenticationToken);
+        }
+        return availableEndEntityProfiles;
+    }
+
+    private Map<Integer, String> loadAuthorizedCertificateProfiles(final AuthenticationToken authenticationToken, final  Map<Integer, String> availableCertificateProfiles) {
+        if(availableCertificateProfiles.isEmpty()) {
+            return raMasterApi.getAuthorizedCertificateProfileIdsToNameMap(authenticationToken);
+        }
+        return availableCertificateProfiles;
+    }
+
+    private Map<Integer, String> loadAuthorizedCAs(final AuthenticationToken authenticationToken, final Map<Integer, String> availableCAs) {
+        if(availableCAs.isEmpty()) {
+            final Map<Integer, String> authorizedCAIds = new HashMap<>();
+            final List<CAInfo> caInfosList = raMasterApi.getAuthorizedCas(authenticationToken);
+            for(final CAInfo caInfo : caInfosList) {
+                authorizedCAIds.put(caInfo.getCAId(), caInfo.getName());
+            }
+            return authorizedCAIds;
+        }
+        return availableCAs;
+    }
+
+    private Integer getKeyFromMapByValue(final Map<Integer, String> map, final String value) {
+        for(Integer key : map.keySet()) {
+            if(map.get(key).equals(value)) {
+                return key;
+            }
+        }
+        return null;
     }
 }
