6b0f6bd379107e4c47f68fe51379e6a975173835
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2018-09-25 08:09:26 +0000

    ECA-7304: Update and move default DNSSEC root anchors to one place. Fixes for multiple root anchors in GUI
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@29898 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ejbca-common/src/org/ejbca/core/protocol/dnssec/DnsSecDefaults.java
@@ -0,0 +1,24 @@
+package org.ejbca.core.protocol.dnssec;
+
+/**
+ * Defaults for DNSSEC.
+ * @version $Id$
+ */
+public final class DnsSecDefaults {
+
+    private DnsSecDefaults() { }
+
+    /**
+     * Default root anchors for DNSSEC from IANA. Defined in https://data.iana.org/root-anchors/root-anchors.xml
+     * <p>
+     * In the long run we should consider:
+     * <ol>
+     * <li>Making DNSSEC configurable in a central place
+     * <li>Automatic update of the DNSSEC key using
+     * </ol>
+     */
+    public static final String IANA_ROOT_ANCHORS_DEFAULT =
+            ". IN DS 19036 8 2 49AAC11D7B6F6446702E54A1607371607A1A41855200FD2CE1CDDE32F24E8FB5\n" + // old key, going to be phased out
+            ". IN DS 20326 8 2 E06D44B80B8F1D39A95C0B0D7C65D08458E880409BBC683457104237C7F8EC8D"; // new key, valid from 2018-10-11
+
+}
