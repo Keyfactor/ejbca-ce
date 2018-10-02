d5eba7c0b90f65c9009e4df8cf858ae76230648f
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2018-10-02 06:21:22 +0000

    ECA-7304: Add license header
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@29944 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-common/src/org/ejbca/core/protocol/dnssec/DnsSecDefaults.java
+++ modules/ejbca-common/src/org/ejbca/core/protocol/dnssec/DnsSecDefaults.java
@@ -1,3 +1,15 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.ejbca.core.protocol.dnssec;
 
 /**
