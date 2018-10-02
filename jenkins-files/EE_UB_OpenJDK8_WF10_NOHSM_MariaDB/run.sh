534072add09834b404a7de212771b93bd2336487
Tarmo Raudsep <tarmo.raudsep.ext@primekey.com>
2018-10-02 12:24:59 +0000

    ECA-6864 Set up a Jenkins instance to test JDK8/Wildfly12 using Docker WF10 deploy & systests
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@29970 37aeb1de-b14b-0410-9a0e-82b28170d228

--- jenkins-files/EE_UB_OpenJDK8_WF10_NOHSM_MariaDB/run.sh
+++ jenkins-files/EE_UB_OpenJDK8_WF10_NOHSM_MariaDB/run.sh
@@ -2,6 +2,9 @@
 
 cp /opt/standalone1.xml /opt/jboss/wildfly/standalone/configuration/standalone.xml
 
+ls -la /app/ejbca/conf/
+ls -la /opt/conf/
+
 cp /opt/conf/* /app/ejbca/conf/
 
 /opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 &
