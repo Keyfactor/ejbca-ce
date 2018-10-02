9a7b8f972d198be46d126161a25782477b9af749
Tarmo Raudsep <tarmo.raudsep.ext@primekey.com>
2018-10-02 08:21:31 +0000

    ECA-6864 Set up a Jenkins instance to test JDK8/Wildfly12 using Docker WF10 deploy & systests first commit
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@29953 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ jenkins-files/EE_UB_OpenJDK8_WF10_NOHSM_MariaDB/run.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+cp /opt/standalone1.xml /opt/jboss/wildfly/standalone/configuration/standalone.xml
+
+cp /opt/conf/* /app/ejbca/conf/
+
+/opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 &
+
+ant clean deployear
+
+echo '=================== Waiting for deploy ================================='
+
+
+wait_for_deployment() {
+	# Wait for up to 180 seconds for app to start up
+	for i in {1..90} ; do
+		if [ -e "/opt/jboss/wildfly/standalone/deployments/ejbca.ear.deployed" ] ; then
+			echo "EJBCA successfully started."
+			break
+		fi
+		echo 'waiting...'
+		sleep 2
+	done
+#TODO what about deploy failure case??
+}
+
+wait_for_deployment
+echo '=================== ant deployear done and successfully deployed! ================================='
+
+ant runinstall
+echo '=================== ant runinstall done! ================================='
+
+ant deploy-keystore
+echo '=================== ant deploy-keystore done! ================================='
+
+cp /opt/standalone2.xml /opt/jboss/wildfly/standalone/configuration/standalone.xml
+/opt/jboss/wildfly/bin/jboss-cli.sh -c --command=:reload
+
+# wait for reload to kick in and start undeploying and drop ejbca.ear.deployed file (otherwise we'd detect ejbca.ear.deployed file immediately again)
+sleep 10
+
+wait_for_deployment
+
+echo '=================== starting system tests ================================='
+#ant test:runsys <- COMMENT BACK IN!
