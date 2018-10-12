711f68de4507cfb881e285d8eaedeb3483391415
Henrik Sunmark <henrik.sunmark@primekey.com>
2018-10-12 13:29:28 +0000

    ECA-7256: Allowed creating of end entities in RA web
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@30096 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-common/src/org/ejbca/core/model/SecConst.java
+++ modules/ejbca-common/src/org/ejbca/core/model/SecConst.java
@@ -47,6 +47,8 @@ public final class SecConst {
     public static final int TOKEN_SOFT = 100;
 
     public static final String[] TOKENTEXTS = {"TOKENSOFTUSERGENERATED","TOKENSOFTP12","TOKENSOFTJKS","TOKENSOFTPEM"};
+    
+    public static final String[] TOKEN_DISPLAY_TEXTS = {"User Generated","P12 File","JKS File","PEM File"};
 
     public static final int[]    TOKENIDS   = {SecConst.TOKEN_SOFT_BROWSERGEN,SecConst.TOKEN_SOFT_P12,SecConst.TOKEN_SOFT_JKS,SecConst.TOKEN_SOFT_PEM};
 
