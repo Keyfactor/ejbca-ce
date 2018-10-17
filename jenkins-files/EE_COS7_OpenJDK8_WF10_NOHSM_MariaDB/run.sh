22f2f6b8c3b9167a144f14ead95118834ed1d17a
Tarmo Raudsep <tarmo.raudsep.ext@primekey.com>
2018-10-17 13:26:21 +0000

    ECA-6864 Set up a Jenkins instance to test JDK8/Wildfly12 using Docker testing failure/timeout script
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@30129 37aeb1de-b14b-0410-9a0e-82b28170d228

--- jenkins-files/EE_COS7_OpenJDK8_WF10_NOHSM_MariaDB/run.sh
+++ jenkins-files/EE_COS7_OpenJDK8_WF10_NOHSM_MariaDB/run.sh
@@ -12,16 +12,25 @@ echo '=================== Waiting for deploy ================================='
 
 
 wait_for_deployment() {
+    DEPLOY_SUCCESSFUL = 0;
 	# Wait for up to 180 seconds for app to start up
 	for i in {1..90} ; do
 		if [ -e "/opt/jboss/wildfly/standalone/deployments/ejbca.ear.deployed" ] ; then
 			echo "EJBCA successfully started."
+			DEPLOY_SUCCESSFUL = 1;
 			break
 		fi
+		if [ -e "/opt/jboss/wildfly/standalone/deployments/ejbca.ear.failed" ] ; then
+            echo "EJBCA deploy failed." 
+            exit 1;
+        fi
 		echo 'waiting...'
 		sleep 2
 	done
-#TODO what about deploy failure case??
+    if [ $DEPLOY_SUCCESSFUL -ne 1 ]; then
+        echo "EJBCA deploy timed out." 
+        exit 1;
+    fi
 }
 
 wait_for_deployment
