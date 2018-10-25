3c57524ecb7d0e42ad2fb5e3d11b6afddde40e5b
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2018-10-25 14:25:47 +0000

    ECA-7402: Fix compiler warnings in test
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@30193 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-common/src-test/org/cesecore/keybind/impl/OcspKeyBindingTest.java
+++ modules/cesecore-common/src-test/org/cesecore/keybind/impl/OcspKeyBindingTest.java
@@ -19,11 +19,7 @@ import static org.junit.Assert.fail;
 
 import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
 import java.security.KeyPair;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.SignatureException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.Arrays;
@@ -45,7 +41,6 @@ import org.cesecore.keybind.impl.OcspKeyBinding.ResponderIdType;
 import org.cesecore.keys.util.KeyTools;
 import org.cesecore.util.CertTools;
 import org.cesecore.util.CryptoProviderTools;
-import org.cesecore.util.ui.DynamicUiProperty;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
@@ -90,7 +85,7 @@ public class OcspKeyBindingTest {
     }
 
     @Test
-    public void testOcspSigningCertificateValidationPositives() throws IOException, InvalidAlgorithmParameterException, InvalidKeyException, NoSuchAlgorithmException, SignatureException, IllegalStateException, NoSuchProviderException, OperatorCreationException, CertificateException {
+    public void testOcspSigningCertificateValidationPositives() throws IOException, InvalidAlgorithmParameterException, OperatorCreationException, CertificateException {
         AvailableExtendedKeyUsagesConfiguration ekuConfig = new AvailableExtendedKeyUsagesConfiguration();
         assertTrue("KU=digitalSignature and EKU=id_kp_OCSPSigning should be treated as a valid OCSP singing certificate.",
                 OcspKeyBinding.isOcspSigningCertificate(getCertificate(X509KeyUsage.digitalSignature, ekuExtensionOnly), ekuConfig));
@@ -103,7 +98,7 @@ public class OcspKeyBindingTest {
     }
 
     @Test
-    public void testOcspSigningCertificateAssertionPositives() throws IOException, InvalidAlgorithmParameterException, InvalidKeyException, NoSuchAlgorithmException, SignatureException, IllegalStateException, NoSuchProviderException, OperatorCreationException, CertificateException {
+    public void testOcspSigningCertificateAssertionPositives() throws IOException, InvalidAlgorithmParameterException, OperatorCreationException, CertificateException {
         try {
             new OcspKeyBinding().assertCertificateCompatability(getCertificate(X509KeyUsage.digitalSignature, ekuExtensionOnly), new AvailableExtendedKeyUsagesConfiguration());
         } catch (CertificateImportException e) {
@@ -112,7 +107,7 @@ public class OcspKeyBindingTest {
     }
 
     @Test
-    public void testOcspSigningCertificateValidationNegatives() throws IOException, InvalidAlgorithmParameterException, InvalidKeyException, NoSuchAlgorithmException, SignatureException, IllegalStateException, NoSuchProviderException, OperatorCreationException, CertificateException {
+    public void testOcspSigningCertificateValidationNegatives() throws IOException, InvalidAlgorithmParameterException, OperatorCreationException, CertificateException {
         AvailableExtendedKeyUsagesConfiguration ekuConfig = new AvailableExtendedKeyUsagesConfiguration();
         assertFalse("KU!=digitalSignature|nonRepudiation and EKU=id_kp_OCSPSigning should be treated as an invalid OCSP singing certificate.",
                 OcspKeyBinding.isOcspSigningCertificate(getCertificate(X509KeyUsage.keyAgreement + X509KeyUsage.cRLSign, ekuExtensionOnly), ekuConfig));
@@ -123,7 +118,7 @@ public class OcspKeyBindingTest {
  }
 
     @Test
-    public void testOcspSigningCertificateAssertionNegatives() throws IOException, InvalidAlgorithmParameterException, InvalidKeyException, NoSuchAlgorithmException, SignatureException, IllegalStateException, NoSuchProviderException, OperatorCreationException, CertificateException {
+    public void testOcspSigningCertificateAssertionNegatives() throws IOException, InvalidAlgorithmParameterException, OperatorCreationException, CertificateException {
         try {
             new OcspKeyBinding().assertCertificateCompatability(getCertificate(X509KeyUsage.cRLSign, null), new AvailableExtendedKeyUsagesConfiguration() );
             fail("KU=cRLSign and EKU!=id_kp_OCSPSigning should be treated as an invalid OCSP singing certificate.");
@@ -133,8 +128,7 @@ public class OcspKeyBindingTest {
     }
 
     /** @return A self-signed certificate. */
-    private X509Certificate getCertificate(final int keyUsage, final List<Extension> extensions) throws InvalidAlgorithmParameterException, InvalidKeyException, NoSuchAlgorithmException,
-        SignatureException, IllegalStateException, NoSuchProviderException, OperatorCreationException, CertificateException, IOException {
+    private X509Certificate getCertificate(final int keyUsage, final List<Extension> extensions) throws InvalidAlgorithmParameterException, OperatorCreationException, CertificateException, IOException {
         final KeyPair keyPair = KeyTools.genKeys("512", AlgorithmConstants.KEYALGORITHM_RSA);
         return CertTools.genSelfCertForPurpose("CN=OcspSinger", 365, null, keyPair.getPrivate(), keyPair.getPublic(), "SHA256WithRSA", false,
                 keyUsage, null, null, BouncyCastleProvider.PROVIDER_NAME, true, extensions);
