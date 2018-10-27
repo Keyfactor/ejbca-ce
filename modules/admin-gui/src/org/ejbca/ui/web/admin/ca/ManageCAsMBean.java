b9018010f0aa9ae2dd0883aed676a96788789bf3
Amin Khorsandi <amin.khorsandi@primekey.com>
2018-10-27 22:27:43 +0000

    ECA-5851: File headers etc.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@30245 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/admin-gui/src/org/ejbca/ui/web/admin/ca/ManageCAsMBean.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/ca/ManageCAsMBean.java
@@ -1,3 +1,15 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.ejbca.ui.web.admin.ca;
 
 import java.beans.Beans;
@@ -24,6 +36,13 @@ import org.ejbca.ui.web.admin.BaseManagedBean;
 import org.ejbca.ui.web.admin.cainterface.CADataHandler;
 import org.ejbca.ui.web.admin.cainterface.CAInterfaceBean;
 
+/**
+ * 
+ * JSF MBean backing the edit ca page pages.
+ *
+ * @version $Id$
+ * 
+ */
 @ManagedBean
 @ViewScoped
 public class ManageCAsMBean extends BaseManagedBean implements Serializable {
