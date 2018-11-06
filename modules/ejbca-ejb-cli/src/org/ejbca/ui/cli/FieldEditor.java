0a64caf901f5cdb953f7bf2d4f398acf5de271ae
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2018-11-06 11:49:22 +0000

    ECA-7470: check for array values separated by; and add them as arrays
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@30409 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb-cli/src/org/ejbca/ui/cli/FieldEditor.java
+++ modules/ejbca-ejb-cli/src/org/ejbca/ui/cli/FieldEditor.java
@@ -14,11 +14,13 @@ package org.ejbca.ui.cli;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 import org.apache.commons.beanutils.ConvertingWrapDynaBean;
 import org.apache.commons.beanutils.DynaBean;
 import org.apache.commons.beanutils.DynaProperty;
 import org.apache.commons.beanutils.WrapDynaBean;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.math.NumberUtils;
 import org.apache.log4j.Logger;
 
@@ -115,14 +117,56 @@ public class FieldEditor {
             // If the value can be converted into an integer, we will use an ArrayList<Integer>
             // Our problem here is that the type of a collection (<Integer>, <String>) is only compile time, it can not be determined in runtime.
             List<Object> arr = new ArrayList<Object>();
-
-            if (NumberUtils.isNumber(value)) {
-                logger.info("using Integer value.");
-                arr.add(Integer.valueOf(value));
-            } else {
-                // Make it into an array of String
-                logger.info("using String value.");
-                arr.add(value);
+            // In order to set an array of in the Array types, we can use ; as delimiter, so we can for example set more than one validator
+            // bin/ejbca.sh ca editca --caname "MyCA" --field validators --value "404411607;3434;433453"
+            // This code is a bit too verbose, but I couldn't figure out an easy way to handle this, including a fall back if:
+            // 1. There was a ; delimiter, but not all values were of the same type (if not same type we can't add them to an array)
+            // -- In this case we fallback to using the stirng as a single value
+            // 2. There was no delimiter, so we should add a single value
+            StringTokenizer t = new StringTokenizer(value, ";");
+            boolean isSame = true;
+            if (t.countTokens() > 1) {
+                logger.info("Found an array, separated by ';', checking if it's all the same type...");
+                String s = t.nextToken();
+                if (NumberUtils.isNumber(s)) {
+                    logger.info("using Integer value: "+s);
+                    arr.add(Integer.valueOf(s));
+                } else {
+                    // Make it into an array of String
+                    logger.info("using String value: "+s);
+                    arr.add(s);
+                }
+                boolean isInt = NumberUtils.isNumber(s);
+                while (t.hasMoreTokens()) {
+                    s = t.nextToken();
+                    if (NumberUtils.isNumber(s) && !isInt) {
+                        logger.info("Values are not of the same type, can not split up as array...");
+                        isSame = false;
+                        break;
+                    }
+                    if (NumberUtils.isNumber(s)) {
+                        logger.info("using Integer value: "+s);
+                        arr.add(Integer.valueOf(s));
+                    } else {
+                        // Make it into an array of String
+                        logger.info("using String value: "+s);
+                        arr.add(s);
+                    }
+                }
+            }
+            if ((arr.size() == 0) || !isSame) {
+                arr = new ArrayList<Object>();
+                logger.info("Setting as single value...");
+                if (NumberUtils.isNumber(value)) {
+                    logger.info("using Integer value: "+value);
+                    arr.add(Integer.valueOf(value));
+                } else if (StringUtils.isNotEmpty(value)) {
+                    // Make it into an array of String
+                    logger.info("using String value: "+value);
+                    arr.add(value);
+                } else {                	
+                    logger.info("value was empty, setting as empty Array: "+value);
+                }
             }
             val = arr;
         }
