3144f1c2f90a40e2908a747ac6036c524e888c40
Amin Khorsandi <amin.khorsandi@primekey.com>
2018-11-08 13:35:31 +0000

    ECA-5851: Fix review comments and some refactoring.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@30439 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/ca/CRLFieldValidator.java
@@ -0,0 +1,44 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.ui.web.admin.ca;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+import org.apache.commons.lang.StringUtils;
+import org.cesecore.util.SimpleTime;
+
+/**
+ * Validator used in validating CRL and Validity fields.
+ * 
+ * @version $Id$
+ *
+ */
+@FacesValidator("org.ejbca.ui.web.admin.ca.CRLFieldValidator")
+public class CRLFieldValidator implements Validator {
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        SimpleTime simpleTime = SimpleTime.getInstance((String)value, StringUtils.EMPTY);
+
+        if (!value.toString().equals(StringUtils.EMPTY) && (simpleTime == null)) {
+            FacesMessage msg = new FacesMessage("Invalid CRL or Validity field input!");
+            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+            throw new ValidatorException(msg);
+        }
+    }
+}
