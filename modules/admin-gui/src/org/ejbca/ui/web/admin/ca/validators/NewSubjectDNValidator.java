527e547d96de82bd4711a8ac0216bac73c40faae
Amin Khorsandi <amin.khorsandi@primekey.com>
2018-11-09 11:04:32 +0000

    ECA-5851: Add module info and svn id.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@30449 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/admin-gui/src/org/ejbca/ui/web/admin/ca/validators/NewSubjectDNValidator.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/ca/validators/NewSubjectDNValidator.java
@@ -1,3 +1,15 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.ejbca.ui.web.admin.ca.validators;
 
 import java.util.regex.Matcher;
@@ -12,6 +24,12 @@ import javax.faces.validator.ValidatorException;
 
 import org.apache.commons.lang.StringUtils;
 
+/**
+ * Validator used in validating new subject dn field (edit ca page).
+ * 
+ * @version $Id$
+ *
+ */
 @FacesValidator("org.ejbca.ui.web.admin.ca.validators.NewSubjectDNValidator")
 public class NewSubjectDNValidator implements Validator {
 
