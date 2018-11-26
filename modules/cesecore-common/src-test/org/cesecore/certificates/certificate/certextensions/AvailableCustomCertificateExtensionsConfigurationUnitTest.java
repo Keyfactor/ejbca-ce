91c522d73817559b3dacc4b6f9d4fda1205f7a1b
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2018-11-26 09:42:20 +0000

    ECA-7535: Add license header
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@30620 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-common/src-test/org/cesecore/certificates/certificate/certextensions/AvailableCustomCertificateExtensionsConfigurationUnitTest.java
+++ modules/cesecore-common/src-test/org/cesecore/certificates/certificate/certextensions/AvailableCustomCertificateExtensionsConfigurationUnitTest.java
@@ -1,3 +1,15 @@
+/*************************************************************************
+ *                                                                       *
+ *  CESeCore: CE Security Core                                           *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/ 
 package org.cesecore.certificates.certificate.certextensions;
 
 import static org.junit.Assert.assertArrayEquals;
