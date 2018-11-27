b7f14a7cf95264bfd43bc36f412c051caab2132e
Tarmo Raudsep <tarmo.raudsep.ext@primekey.com>
2018-11-27 14:26:43 +0000

    ECA-7327 Convert viewcainfo.jsp and viewcertificate.jsp popUps to jsf first commit
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@30647 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/admin-gui/src/org/ejbca/ui/web/admin/cainterface/CAInfoView.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/cainterface/CAInfoView.java
@@ -72,35 +72,62 @@ public class CAInfoView implements Serializable, Cloneable {
     * It must also have CRLSPECIFICDATA in position n° 9 (CRL Specific Data) 
     * It must also have SERVICES in position n° 15 (Services), if exists 
     */
-   public static String[] X509CA_CAINFODATATEXTS = {"CANAME","CERT_SUBJECTDN","EXT_ABBR_SUBJECTALTNAME","CATYPE",
-       "CADATA",               /* CA data */
-       "EXPIRES","STATUS",/*"CATOKENSTATUS"*/ "","DESCRIPTION",
-       "CRLSPECIFICDATA",      /* CRL Specific Data */
-       "CRL_CA_CRLPERIOD","CRL_CA_ISSUEINTERVAL","CRL_CA_OVERLAPTIME","CRL_CA_DELTACRLPERIOD","PUBLISHERS","VALIDATORS",
-       "SERVICES",             /* Services */
-       "OCSPSERVICE"};
+   public static String[] X509CA_CAINFODATATEXTS = {
+           "CANAME",
+           "CERT_SUBJECTDN",
+           "EXT_ABBR_SUBJECTALTNAME",
+           "CATYPE",
+           "CADATA",               /* CA data */
+           "EXPIRES","STATUS",/*"CATOKENSTATUS"*/ 
+           "",
+           "DESCRIPTION",
+           "CRLSPECIFICDATA",      /* CRL Specific Data */
+           "CRL_CA_CRLPERIOD",
+           "CRL_CA_ISSUEINTERVAL",
+           "CRL_CA_OVERLAPTIME",
+           "CRL_CA_DELTACRLPERIOD",
+           "PUBLISHERS",
+           "VALIDATORS",
+           "SERVICES",             /* Services */
+           "OCSPSERVICE"
+   };
 
-public static String[] CVCCA_CAINFODATATEXTS = {"NAME","CERT_SUBJECTDN","","CATYPE",
-      "CADATA",                /* CA data */
-      "EXPIRES","STATUS",/*"CATOKENSTATUS"*/ "","DESCRIPTION",
-      "CRLSPECIFICDATA",       /* CRL Specific Data */
-      "CRL_CA_CRLPERIOD","CRL_CA_ISSUEINTERVAL","CRL_CA_OVERLAPTIME","CRL_CA_DELTACRLPERIOD"};
+   public static String[] CVCCA_CAINFODATATEXTS = {
+        "NAME",
+        "CERT_SUBJECTDN",
+        "",
+        "CATYPE",
+        "CADATA",           /* CA data */
+        "EXPIRES",
+        "STATUS",           /*"CATOKENSTATUS"*/ 
+        "",
+        "DESCRIPTION",
+        "CRLSPECIFICDATA",  /* CRL Specific Data */
+        "CRL_CA_CRLPERIOD",
+        "CRL_CA_ISSUEINTERVAL",
+        "CRL_CA_OVERLAPTIME",
+        "CRL_CA_DELTACRLPERIOD"
+   };
 
    private String[] cainfodata = null;
    private String[] cainfodatatexts = null;
    
-   private CAInfo          cainfo   = null;
+   private CAInfo cainfo = null;
    
-    public CAInfoView(CAInfo cainfo, EjbcaWebBean ejbcawebbean, Map<Integer, String> publishersidtonamemap, Map<Integer, String> keyValidatorsIdToNameMap) {
+   public CAInfoView(final CAInfo cainfo, final EjbcaWebBean ejbcawebbean, final Map<Integer, String> publishersidtonamemap, final Map<Integer, String> keyValidatorsIdToNameMap) {
       this.cainfo = cainfo;  
-        
+      
+      buildCaInformation(cainfo, ejbcawebbean);
+      
       if (cainfo instanceof X509CAInfo) {
+        addX509CAInformation(cainfo, ejbcawebbean, publishersidtonamemap, keyValidatorsIdToNameMap);
+        
         setupGeneralInfo(X509CA_CAINFODATATEXTS, cainfo, ejbcawebbean);
 
         cainfodata[SUBJECTALTNAME] = HTMLTools.htmlescape(((X509CAInfo) cainfo).getSubjectAltName());
 
 		cainfodata[CRLPUBLISHERS] = "";
-        Iterator<Integer> publisherIds = ((X509CAInfo) cainfo).getCRLPublishers().iterator();
+        final Iterator<Integer> publisherIds = ((X509CAInfo) cainfo).getCRLPublishers().iterator();
         if(publisherIds.hasNext()) {
         	cainfodata[CRLPUBLISHERS] = publishersidtonamemap.get(publisherIds.next()); 
         } else {
@@ -125,15 +152,15 @@ public static String[] CVCCA_CAINFODATATEXTS = {"NAME","CERT_SUBJECTDN","","CATY
 		cainfodata[SECTION_SERVICE]          = "&nbsp;"; // Section row
         
       } else if (cainfo instanceof CVCCAInfo) {
-          setupGeneralInfo(CVCCA_CAINFODATATEXTS, cainfo, ejbcawebbean);          
+          setupGeneralInfo(CVCCA_CAINFODATATEXTS, cainfo, ejbcawebbean);
       }
    }
-
-	private void setupGeneralInfo(String[] strings, CAInfo cainfo, EjbcaWebBean ejbcawebbean) {
+   
+	private void setupGeneralInfo(final String[] strings, final CAInfo cainfo, final EjbcaWebBean ejbcawebbean) {
 		cainfodatatexts = new String[strings.length];
-        cainfodata = new String[strings.length];  
+        cainfodata = new String[strings.length];
         
-        for(int i=0; i < strings.length; i++){
+		for(int i=0; i < strings.length; i++){
           if(strings[i].equals("")) {
               cainfodatatexts[i]="&nbsp;";
           } else {
@@ -143,7 +170,7 @@ public static String[] CVCCA_CAINFODATATEXTS = {"NAME","CERT_SUBJECTDN","","CATY
         
         cainfodata[SUBJECTDN]  = HTMLTools.htmlescape(cainfo.getSubjectDN());
         cainfodata[NAME]       = HTMLTools.htmlescape(cainfo.getName());
-        int catype = cainfo.getCAType();
+        final int catype = cainfo.getCAType();
         if (catype == CAInfo.CATYPE_CVC) {
             cainfodata[CATYPE]     = ejbcawebbean.getText("CVCCA");        	
         } else {
@@ -156,45 +183,143 @@ public static String[] CVCCA_CAINFODATATEXTS = {"NAME","CERT_SUBJECTDN","","CATY
           cainfodata[EXPIRETIME] = ejbcawebbean.formatAsISO8601(cainfo.getExpireTime());
         }
         
-        switch(cainfo.getStatus()){
-            case CAConstants.CA_ACTIVE :
-              cainfodata[STATUS]     = ejbcawebbean.getText("ACTIVE");     
-              break;
-            case CAConstants.CA_EXPIRED :
-              cainfodata[STATUS]     = ejbcawebbean.getText("EXPIRED");
-              break;
-            case CAConstants.CA_OFFLINE :
-              cainfodata[STATUS]     = ejbcawebbean.getText("OFFLINE");
-              break;
-            case CAConstants.CA_REVOKED :
-              cainfodata[STATUS]     = ejbcawebbean.getText("CAREVOKED") + "<br>&nbsp;&nbsp;" + 
-                                                    ejbcawebbean.getText("REASON") + " : <br>&nbsp;&nbsp;&nbsp;&nbsp;" + 
-                                                    ejbcawebbean.getText(SecConst.reasontexts[cainfo.getRevocationReason()]) + "<br>&nbsp;&nbsp;" +
-			                                        ejbcawebbean.getText("CRL_ENTRY_REVOCATIONDATE") + "<br>&nbsp;&nbsp;&nbsp;&nbsp;" + 
-			                                        ejbcawebbean.formatAsISO8601(cainfo.getRevocationDate());
-              break;
-            case CAConstants.CA_WAITING_CERTIFICATE_RESPONSE :
-              cainfodata[STATUS]     = ejbcawebbean.getText("WAITINGFORCERTRESPONSE");
-              break;              
-            case CAConstants.CA_EXTERNAL :
-                cainfodata[STATUS]     = ejbcawebbean.getText("EXTERNALCA");
-                break;              
-        } 
+        cainfodata[STATUS] = getStatus(cainfo, ejbcawebbean);
         
         cainfodata[DESCRIPTION] = HTMLTools.htmlescape(cainfo.getDescription());
         
-		cainfodata[SECTION_CRL]          = "&nbsp;"; // Section row
+        cainfodata[SECTION_CRL]          = "&nbsp;"; // Section row
 
         cainfodata[CRLPERIOD] = SimpleTime.getInstance(cainfo.getCRLPeriod()).toString(SimpleTime.TYPE_MINUTES);
         cainfodata[CRLISSUEINTERVAL] = SimpleTime.getInstance(cainfo.getCRLIssueInterval()).toString(SimpleTime.TYPE_MINUTES);
         cainfodata[CRLOVERLAPTIME] = SimpleTime.getInstance(cainfo.getCRLOverlapTime()).toString(SimpleTime.TYPE_MINUTES);
         cainfodata[DELTACRLPERIOD] = SimpleTime.getInstance(cainfo.getDeltaCRLPeriod()).toString(SimpleTime.TYPE_MINUTES);
 	}
+	
+    private void buildCaInformation(final CAInfo cainfo, final EjbcaWebBean ejbcawebbean) {
+        caInformation = new CaGuiInfo();
+        
+        final String subjectDnText = ejbcawebbean.getText("CERT_SUBJECTDN");
+        final String subjectDn = HTMLTools.htmlescape(cainfo.getSubjectDN());
+        caInformation.setSubjectDn(new CaInfoProperty(subjectDnText, subjectDn));
+        
+        final String caNameText = ejbcawebbean.getText("CANAME");
+        final String caName = HTMLTools.htmlescape(cainfo.getName());
+        caInformation.setCaName(new CaInfoProperty(caNameText, caName));
+        
+        final String caTypeText = ejbcawebbean.getText("CATYPE");
+        final int caType = cainfo.getCAType();
+        final String caTypeValue = (caType == CAInfo.CATYPE_CVC) ? ejbcawebbean.getText("CVCCA") : ejbcawebbean.getText("X509");
+        caInformation.setCaType(new CaInfoProperty(caTypeText, caTypeValue));
+        
+        final String expireTimeText = ejbcawebbean.getText("EXPIRES");
+        final String expireTime = (cainfo.getExpireTime() == null) ? "" : ejbcawebbean.formatAsISO8601(cainfo.getExpireTime());
+        caInformation.setExpireTime(new CaInfoProperty(expireTimeText, expireTime));
+        
+        final String statusText = ejbcawebbean.getText("STATUS");
+        final String caTokenStatus = getStatus(cainfo, ejbcawebbean);
+        caInformation.setStatus(new CaInfoProperty(statusText, caTokenStatus));
+        
+        final String descriptionText = ejbcawebbean.getText("DESCRIPTION");
+        final String description = HTMLTools.htmlescape(cainfo.getDescription());
+        caInformation.setDescription(new CaInfoProperty(descriptionText, description));
+        
+        final String crlPeriodText = ejbcawebbean.getText("CRL_CA_CRLPERIOD");
+        final String crlPeriod = SimpleTime.getInstance(cainfo.getCRLPeriod()).toString(SimpleTime.TYPE_MINUTES);
+        caInformation.setCrlPeriod(new CaInfoProperty(crlPeriodText, crlPeriod));
+        
+        final String issueIntervalText = ejbcawebbean.getText("CRL_CA_ISSUEINTERVAL");
+        final String issueInterval = SimpleTime.getInstance(cainfo.getCRLIssueInterval()).toString(SimpleTime.TYPE_MINUTES);
+        caInformation.setCrlIssueInterval(new CaInfoProperty(issueIntervalText, issueInterval));
+        
+        final String overlapTimeText = ejbcawebbean.getText("CRL_CA_OVERLAPTIME");
+        final String overlapTime = SimpleTime.getInstance(cainfo.getCRLOverlapTime()).toString(SimpleTime.TYPE_MINUTES);
+        caInformation.setCrlOverlapTime(new CaInfoProperty(overlapTimeText, overlapTime));
+        
+        final String deltaCrlPeriodText = ejbcawebbean.getText("CRL_CA_DELTACRLPERIOD");
+        final String deltaCrlPeriod = SimpleTime.getInstance(cainfo.getDeltaCRLPeriod()).toString(SimpleTime.TYPE_MINUTES);
+        caInformation.setDeltaCrlPeriod(new CaInfoProperty(deltaCrlPeriodText, deltaCrlPeriod));
+    }
+    
+    private void addX509CAInformation(final CAInfo cainfo, final EjbcaWebBean ejbcawebbean, 
+            final Map<Integer, String> publishersidtonamemap, final Map<Integer, String> keyValidatorsIdToNameMap) {
+        final String alternativeNameText = ejbcawebbean.getText("EXT_ABBR_SUBJECTALTNAME");
+        final String alternativeName = HTMLTools.htmlescape(((X509CAInfo) cainfo).getSubjectAltName());
+        caInformation.setAlternativeName(new CaInfoProperty(alternativeNameText, alternativeName));
+        
+        final String publishersText = ejbcawebbean.getText("PUBLISHERS");
+        final String publishers = getPublishers(cainfo, ejbcawebbean, publishersidtonamemap);
+        caInformation.setCrlPublishers(new CaInfoProperty(publishersText, publishers));
+                
+        final String validatorsText = ejbcawebbean.getText("VALIDATORS");
+        final String validators = getValidarors(cainfo, ejbcawebbean, keyValidatorsIdToNameMap);
+        caInformation.setValidators(new CaInfoProperty(validatorsText, validators));
+    }
 
-   public String[] getCAInfoData(){ return cainfodata;}
-   public String[] getCAInfoDataText(){ return cainfodatatexts;} 
+    private String getValidarors(final CAInfo cainfo, final EjbcaWebBean ejbcawebbean, final Map<Integer, String> keyValidatorsIdToNameMap) {
+        String result = StringUtils.EMPTY;
+        
+        final Iterator<Integer> keyValidatorIds = ((X509CAInfo) cainfo).getValidators().iterator();
+        if(keyValidatorIds.hasNext()) {
+            result = keyValidatorsIdToNameMap.get(keyValidatorIds.next()); 
+        } else {
+            result = ejbcawebbean.getText("NONE");
+        }
+        while(keyValidatorIds.hasNext()) {
+            result += ", " + keyValidatorsIdToNameMap.get(keyValidatorIds.next());
+        }
+        return result;
+    }
+    
+    private String getPublishers(final CAInfo cainfo, final EjbcaWebBean ejbcawebbean, final Map<Integer, String> publishersidtonamemap) {
+        String result = "";
+        final Iterator<Integer> publisherIds = ((X509CAInfo) cainfo).getCRLPublishers().iterator();
+        
+        if(publisherIds.hasNext()) {
+            result = publishersidtonamemap.get(publisherIds.next());
+        } else {
+            result = ejbcawebbean.getText("NONE");
+        }
+        while(publisherIds.hasNext()) {
+            result += ", " + publishersidtonamemap.get(publisherIds.next());
+        }
+        return result;
+    }
 
-   public CAInfo getCAInfo() { return cainfo;}
-   public CAToken getCAToken() { return cainfo.getCAToken(); }
-   public Collection<Certificate> getCertificateChain() { return cainfo.getCertificateChain(); }
+    private String getStatus(final CAInfo cainfo, final EjbcaWebBean ejbcawebbean) {
+        switch(cainfo.getStatus()) {
+            case CAConstants.CA_ACTIVE :
+              return ejbcawebbean.getText("ACTIVE");     
+            case CAConstants.CA_EXPIRED :
+              return ejbcawebbean.getText("EXPIRED");
+            case CAConstants.CA_OFFLINE :
+              return ejbcawebbean.getText("OFFLINE");
+            case CAConstants.CA_REVOKED :
+              return ejbcawebbean.getText("CAREVOKED") + "<br>&nbsp;&nbsp;" + 
+                    ejbcawebbean.getText("REASON") + " : <br>&nbsp;&nbsp;&nbsp;&nbsp;" + 
+                    ejbcawebbean.getText(SecConst.reasontexts[cainfo.getRevocationReason()]) + "<br>&nbsp;&nbsp;" +
+                    ejbcawebbean.getText("CRL_ENTRY_REVOCATIONDATE") + "<br>&nbsp;&nbsp;&nbsp;&nbsp;" + 
+                    ejbcawebbean.formatAsISO8601(cainfo.getRevocationDate());
+            case CAConstants.CA_WAITING_CERTIFICATE_RESPONSE :
+              return ejbcawebbean.getText("WAITINGFORCERTRESPONSE");
+            case CAConstants.CA_EXTERNAL :
+              return ejbcawebbean.getText("EXTERNALCA");
+        }
+        return "";
+    }
+	
+    private CaGuiInfo caInformation;
+    
+    public CaGuiInfo getCaInformation() {
+        return caInformation;
+    }
+    
+    // TODO cleanup those getters later
+    public String[] getCAInfoData(){ return cainfodata;}
+    public String[] getCAInfoDataText(){ return cainfodatatexts;} 
+    
+    
+
+    public CAInfo getCAInfo() { return cainfo;}
+    public CAToken getCAToken() { return cainfo.getCAToken(); }
+    public Collection<Certificate> getCertificateChain() { return cainfo.getCertificateChain(); }
 }
