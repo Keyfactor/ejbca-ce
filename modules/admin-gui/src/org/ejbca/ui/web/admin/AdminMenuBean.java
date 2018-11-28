4b8fe82e424acfac7eafb57c563e634d2d6deaaf
Amin Khorsandi <amin.khorsandi@primekey.com>
2018-11-28 08:46:12 +0000

    ECA-7367: ACME must be in status unavailable in EJBCA community.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@30653 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/admin-gui/src/org/ejbca/ui/web/admin/AdminMenuBean.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/AdminMenuBean.java
@@ -172,11 +172,11 @@ public class AdminMenuBean extends BaseManagedBean implements Serializable {
     }
     
     public boolean isAuthorizedToViewEstConfiguration() {
-        return getEjbcaWebBean().isEstConfigurationPresent() && isAuthorizedToViewSystemConfiguration();
+        return getEjbcaWebBean().isRunningEnterprise() && isAuthorizedToViewSystemConfiguration();
     }
 
     public boolean isAuthorizedToViewAcmeConfiguration() {
-        return getEjbcaWebBean().isAcmeConfigurationPresent() && isAuthorizedToViewSystemConfiguration();
+        return getEjbcaWebBean().isRunningEnterprise() && isAuthorizedToViewSystemConfiguration();
     }
     
     public boolean isAuthorizedToConfigureSystem() {
