620390a92fff634949059f1f278e5b90136db27d
Amin Khorsandi <amin.khorsandi@primekey.com>
2018-11-29 15:11:07 +0000

    ECA-7501: Validate ACME dns resolver.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@30681 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/admin-gui/src/org/ejbca/util/validator/IpAddressValidator.java
@@ -0,0 +1,43 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.util.validator;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+/**
+ * Validator used in validating IP addresses in jsf pages (Acme alias configuration for example).
+ *  
+ * @version $Id$
+ *
+ */
+@FacesValidator("org.ejbca.util.validator.ipAddressValidator")
+public class IpAddressValidator implements Validator {
+ 
+    private static final String IP_ADDRESS_PATTERN =
+            "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+ 
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) {
+        if (!((String) value).matches(IP_ADDRESS_PATTERN)) {
+            throw new ValidatorException(new FacesMessage("Incorrectly formatter address!"));
+        }
+    }
+}
