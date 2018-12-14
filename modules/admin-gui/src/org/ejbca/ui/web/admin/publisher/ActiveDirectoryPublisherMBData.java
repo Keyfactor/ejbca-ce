af9c47d825056de1730f83e1b4e2a2c04dc7999d
Amin Khorsandi <amin.khorsandi@primekey.com>
2018-12-14 14:33:04 +0000

    ECA-6807: Improvements and code cleanup.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@30866 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/admin-gui/src/org/ejbca/ui/web/admin/publisher/ActiveDirectoryPublisherMBData.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/publisher/ActiveDirectoryPublisherMBData.java
@@ -1,8 +1,30 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.ejbca.ui.web.admin.publisher;
+
+import java.io.Serializable;
 
 import org.ejbca.core.model.ca.publisher.ActiveDirectoryPublisher;
 
-public final class ActiveDirectoryPublisherMBData extends LdapPublisherMBData {
+/**
+ * Class holding data for active directory publisher used in edit publisher bean.
+ * 
+ * @version $Id$
+ *
+ */
+public final class ActiveDirectoryPublisherMBData implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private int samAccountName;
     private String userDescription;
--- modules/admin-gui/src/org/ejbca/ui/web/admin/publisher/LdapSearchPublisherMBData.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/publisher/LdapSearchPublisherMBData.java
@@ -1,9 +1,31 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
 package org.ejbca.ui.web.admin.publisher;
+
+import java.io.Serializable;
 
 import org.ejbca.core.model.ca.publisher.LdapSearchPublisher;
 
-public final class LdapSearchPublisherMBData {
+/**
+ * Class keeping data for ldap search publisher.
+ * 
+ * @version $Id$
+ *
+ */
+public final class LdapSearchPublisherMBData implements Serializable {
 
+    private static final long serialVersionUID = 1L;
     private String searchBaseDN;
     private String searchFilter;
     
