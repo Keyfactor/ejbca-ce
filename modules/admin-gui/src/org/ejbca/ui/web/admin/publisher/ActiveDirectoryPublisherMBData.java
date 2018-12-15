d932545915c0dd421a885dda84f98be929f01fcd
Amin Khorsandi <amin.khorsandi@primekey.com>
2018-12-15 13:11:46 +0000

    ECA-6807: Add custom publisher data class and some refactoring.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@30869 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/admin-gui/src/org/ejbca/ui/web/admin/publisher/ActiveDirectoryPublisherMBData.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/publisher/ActiveDirectoryPublisherMBData.java
@@ -13,8 +13,16 @@
 package org.ejbca.ui.web.admin.publisher;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
+import javax.faces.model.SelectItem;
+
+import org.cesecore.certificates.util.DNFieldExtractor;
 import org.ejbca.core.model.ca.publisher.ActiveDirectoryPublisher;
+import org.ejbca.ui.web.admin.configuration.EjbcaJSFHelper;
 
 /**
  * Class holding data for active directory publisher used in edit publisher bean.
@@ -25,6 +33,18 @@ import org.ejbca.core.model.ca.publisher.ActiveDirectoryPublisher;
 public final class ActiveDirectoryPublisherMBData implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    
+    private static final Map<Integer, String> AVAILABLE_SAM_ACCOUNTS;
+    
+    static {
+        AVAILABLE_SAM_ACCOUNTS = new LinkedHashMap<>();
+        AVAILABLE_SAM_ACCOUNTS.put(DNFieldExtractor.UPN, "MATCHUPN");
+        AVAILABLE_SAM_ACCOUNTS.put(DNFieldExtractor.CN, "MATCHCOMMONNAME");
+        AVAILABLE_SAM_ACCOUNTS.put(DNFieldExtractor.UID, "MATCHUID");
+        AVAILABLE_SAM_ACCOUNTS.put(DNFieldExtractor.SN, "MATCHDNSERIALNUMBER");
+        AVAILABLE_SAM_ACCOUNTS.put(DNFieldExtractor.GIVENNAME, "MATCHGIVENNAME");
+        AVAILABLE_SAM_ACCOUNTS.put(DNFieldExtractor.SURNAME, "MATCHSURNAME");
+    }
     
     public ActiveDirectoryPublisherMBData(final ActiveDirectoryPublisher activeDirectoryPublisher) {
         initializeData(activeDirectoryPublisher);
@@ -49,6 +69,14 @@ public final class ActiveDirectoryPublisherMBData implements Serializable {
     public void setSamAccountName(final int samAccountName) {
         this.samAccountName = samAccountName;
     }
+    
+    public List<SelectItem> getAvailableSamAccountNames() {
+        List<SelectItem> result = new ArrayList<>();
+        for(final int samAccount : AVAILABLE_SAM_ACCOUNTS.keySet()){ 
+            result.add(new SelectItem(samAccount, EjbcaJSFHelper.getBean().getEjbcaWebBean().getText(AVAILABLE_SAM_ACCOUNTS.get(samAccount))));
+        }
+        return result;
+    }    
 
     public void setActiveDirectoryPublisherParameters(final ActiveDirectoryPublisher activeDirectoryPublisher) {
         activeDirectoryPublisher.setSAMAccountName(samAccountName);
