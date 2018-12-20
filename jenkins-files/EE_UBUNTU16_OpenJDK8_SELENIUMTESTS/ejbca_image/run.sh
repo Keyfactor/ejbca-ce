9ff829c988fc9732534db8a055f5adb257d0cb76
Tarmo Raudsep <tarmo.raudsep.ext@primekey.com>
2018-12-20 12:55:27 +0000

    ECA-7365 Add a Jenkins job for EJBCA UI Tests
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@30929 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ jenkins-files/EE_UBUNTU16_OpenJDK8_SELENIUMTESTS/ejbca_image/run.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+cp /opt/conf/* /app/ejbca/conf/
+cp /opt/ManagementCA.pem /app/ejbca/ManagementCA.pem
+
+/opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 &
+
+
+wait_for_deployment() {
+    DEPLOY_SUCCESSFUL=0
+	# Wait for up to 180 seconds for app to start up
+	for i in {1..90} ; do
+		if [ -e "/opt/jboss/wildfly/standalone/deployments/ejbca.ear.deployed" ] ; then
+			echo "EJBCA successfully started."
+			DEPLOY_SUCCESSFUL=1
+			break
+		fi
+		if [ -e "/opt/jboss/wildfly/standalone/deployments/ejbca.ear.failed" ] ; then
+            echo "EJBCA deploy failed."
+            exit 1;
+        fi
+		echo 'waiting...'
+		sleep 2
+	done
+    if [ "$DEPLOY_SUCCESSFUL" -ne 1 ]; then
+        echo "EJBCA deploy timed out." 
+        exit 1;
+    fi
+}
+
+ant clean deployear
+wait_for_deployment
+echo '=================== should be started now ========================'
+
+ant deploy-keystore
+echo '=================== deploy-keystore done ========================'
+
+/opt/jboss/wildfly/bin/jboss-cli.sh -c --command=:reload
+echo '=================== waiting 30... ========================'
+sleep 30
+wait_for_deployment
+echo '=================== Wildfly restarted after deploy-keystore ========================'
+
+bin/ejbca.sh ca importcacert ManagementCA ManagementCA.pem
+bin/ejbca.sh roles addrolemember --role "Super Administrator Role" --caname ManagementCA --with WITH_COMMONNAME --value SuperAdmin
+echo '=================== import cert commands done ========================'
+
+# stay alive until UI tests finish. otherwise the container would just be closed and UI tests would not be able to use it anymore
+sleep 10000000
--- jenkins-files/EE_UBUNTU16_OpenJDK8_SELENIUMTESTS/selenium_image/run.sh
+++ jenkins-files/EE_UBUNTU16_OpenJDK8_SELENIUMTESTS/selenium_image/run.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-cp -R /app/svq3ko35.default /home/seluser/.mozilla/firefox/
-cp /app/profiles.ini /home/seluser/.mozilla/firefox/profiles.ini
+cp /opt/ejbca_conf/* /app/ejbca/conf/
+cp /opt/ejbca_webtest_conf/* /app/ejbca/modules/ejbca-webtest/conf/
 
 /opt/bin/entry_point.sh &
 
 sleep 10
 
+# this should run eventually!
+# ant test:webtest
+
+# this one hangs!
 # ant test:runone -Dtest.runone=EcaQa12_CPManagement
 
 # this test works in RA mode!
