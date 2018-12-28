1bf10ae8fe71811b0ae17898309258a8d98fbc11
Amin Khorsandi <amin.khorsandi@primekey.com>
2018-12-28 14:48:39 +0000

    ECA-7614: Add test class and some tests and fix required modifications in relevant code.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@30956 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ejbca-webtest/src-test/org/ejbca/webtest/scenario/EcaQa196_MultiGroupPublisher.java
@@ -0,0 +1,89 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.webtest.scenario;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.cesecore.authorization.AuthorizationDeniedException;
+import org.cesecore.common.exception.ReferencesToItemExistException;
+import org.ejbca.core.model.ca.publisher.PublisherConst;
+import org.ejbca.webtest.WebTestBase;
+import org.ejbca.webtest.helper.PublisherHelper;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * 
+ * @version $Id$
+ *
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class EcaQa196_MultiGroupPublisher extends WebTestBase {
+
+    private static WebDriver webDriver;
+
+    // Helpers
+    private static PublisherHelper publisherHelper; 
+    
+    // Test Data
+    private static class TestData {
+        static final Map<String, String> PUBLISHERS;
+        static {
+            PUBLISHERS = new HashMap<>();
+            PUBLISHERS.put("PUBLISHER_ONE", "pub1");
+            PUBLISHERS.put("PUBLISHER_TWO", "pub2");
+            PUBLISHERS.put("PUBLISHER_THREE", "pub3");
+            PUBLISHERS.put("PUBLISHER_FOUR", "pub4");
+        }
+        static final String EXPECTED_AVAILABLE_PUBLISHERS = "pub2\npub3\npub4";
+    }
+    
+    @BeforeClass
+    public static void init() {
+        beforeClass(true, null);
+        webDriver = getWebDriver();
+        publisherHelper = new PublisherHelper(webDriver);
+    }
+    
+    @AfterClass
+    public static void exit() throws ReferencesToItemExistException, AuthorizationDeniedException {
+        for (final String publisherName : TestData.PUBLISHERS.values()) {
+            removePublisherByName(publisherName);
+        }
+        afterClass();
+    }
+    
+    @Test
+    public void stepA_addPublishers() {
+        publisherHelper.openPage(getAdminWebUrl());
+        for (final String publisher : TestData.PUBLISHERS.values()) {
+            publisherHelper.addPublisher(publisher);
+        }
+        publisherHelper.assertPublishersExist(TestData.PUBLISHERS.values());
+    }
+    
+    @Test
+    public void stepB_setPublisherTypeAsMultiGroupPublisher() {
+        publisherHelper.openPage(getAdminWebUrl());
+        publisherHelper.selectPublisherFromList(TestData.PUBLISHERS.get("PUBLISHER_ONE"));
+        publisherHelper.editPublisher();
+        publisherHelper.setPublisherType(String.valueOf(PublisherConst.TYPE_MULTIGROUPPUBLISHER));
+        publisherHelper.assertMultiGroupPublisherPage(TestData.EXPECTED_AVAILABLE_PUBLISHERS);
+
+    }
+}
