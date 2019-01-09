05f1a08f875f50ed852ddbe58bf1b3461125f29c
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2019-01-09 15:28:58 +0000

    ECA-7642: Ignore test failure if EJBCA is running externally
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@31090 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/systemtests/src-test/org/ejbca/ui/web/pub/cluster/WebEjbcaClearCacheTest.java
+++ modules/systemtests/src-test/org/ejbca/ui/web/pub/cluster/WebEjbcaClearCacheTest.java
@@ -14,19 +14,24 @@
 package org.ejbca.ui.web.pub.cluster;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assume.assumeTrue;
 
+import java.io.IOException;
+import java.net.ConnectException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.apache.log4j.Logger;
+import org.cesecore.SystemTestsConfiguration;
 import org.cesecore.util.EjbRemoteHelper;
 import org.ejbca.config.WebConfiguration;
 import org.ejbca.core.ejb.config.ConfigurationSessionRemote;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 /**
+ * Tests the ClearCacheServlet 
  *
  * @version $Id$
  */
@@ -38,34 +43,29 @@ public class WebEjbcaClearCacheTest {
     protected String httpPort;
     protected String httpReqPath;
     protected String httpReqPathNoCommand;
+    protected String httpReqPathNoCommandExternal;
 
     @Before
     public void setUp() throws Exception {
         // Clear cache is only available when sent from localhost or a resolved IP, so there is no point in trying to go through a proxy
-        httpPort = configurationSession.getProperty(WebConfiguration.CONFIG_HTTPSERVERPUBHTTP);
+        httpPort = SystemTestsConfiguration.getRemotePortHttp(configurationSession.getProperty(WebConfiguration.CONFIG_HTTPSERVERPUBHTTP));
         httpReqPath = "http://localhost:" + httpPort + "/ejbca/clearcache/?command=clearcaches";
         httpReqPathNoCommand = "http://localhost:" + httpPort + "/ejbca/clearcache/";
-    }
-
-    @After
-    public void tearDown() throws Exception {
     }
 
     @Test
     public void testEjbcaClearCacheHttp() throws Exception {
         log.trace(">testEjbcaHealthHttp()");
 
-		URL url = new URL(httpReqPath);
-		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        HttpURLConnection con = openConnection(httpReqPath);
         final int responseCode = con.getResponseCode();
         if (responseCode != HttpURLConnection.HTTP_OK) {
             log.info("ResponseMessage: " + con.getResponseMessage());
             assertEquals("Response code", HttpURLConnection.HTTP_OK, responseCode);
         }
 
-		url = new URL(httpReqPathNoCommand);
-		con = (HttpURLConnection) url.openConnection();
-		// SC_BAD_REQUEST returned if we do not gove the command=clearcaches parameter in the request
+        con = openConnection(httpReqPathNoCommand);
+        // SC_BAD_REQUEST returned if we do not gove the command=clearcaches parameter in the request
         final int responseCodeBad = con.getResponseCode();
         if (responseCodeBad != HttpURLConnection.HTTP_BAD_REQUEST) {
             log.info("ResponseMessage: " + con.getResponseMessage());
@@ -75,4 +75,24 @@ public class WebEjbcaClearCacheTest {
         log.trace("<testEjbcaHealthHttp()");
     }
 
+    private HttpURLConnection openConnection(final String url) throws MalformedURLException, ConnectException, IOException {
+        try {
+            HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+            con.getResponseCode(); // trigger a connection
+            return con;
+        } catch (ConnectException e) {
+            log.debug("Failed to connect to localhost:" + httpPort);
+            if (isEjbcaRunningExternally()) {
+                assumeTrue("Ignoring test failure since EJBCA is running externally at " + SystemTestsConfiguration.getRemoteHost(null), false);
+            }
+            log.debug("Target hostname does NOT appear to be local, re-throwing exception.");
+            throw e;
+        }
+    }
+
+    private boolean isEjbcaRunningExternally() {
+        final String targetHostname = SystemTestsConfiguration.getRemoteHost("localhost");
+        log.debug("Checking if target hostname '" + targetHostname + "' is external (this check might not be 100% reliable)");
+        return !"localhost".equals(targetHostname) && !"127.0.0.1".equals(targetHostname) && !"::1".equals(targetHostname);
+    }
 }
