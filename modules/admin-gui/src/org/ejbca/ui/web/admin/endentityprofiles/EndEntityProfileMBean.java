4395237ddb70360caa615ac37b78bf046467eddf
Ulf Undmark <ulf.undmark@primekey.com>
2019-01-11 06:40:49 +0000

    ECA-4630: Changed scope for managed bean from requestscoped to viewscoped
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@31121 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/admin-gui/src/org/ejbca/ui/web/admin/endentityprofiles/EndEntityProfileMBean.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/endentityprofiles/EndEntityProfileMBean.java
@@ -27,7 +27,7 @@ import java.util.TreeMap;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
 import javax.faces.model.SelectItem;
@@ -66,7 +66,7 @@ import org.ejbca.ui.web.admin.rainterface.ViewEndEntityHelper;
  * @version $Id$
  */
 @ManagedBean
-@SessionScoped
+@ViewScoped
 public class EndEntityProfileMBean extends BaseManagedBean implements Serializable {
     private static final long serialVersionUID = 1L;
     private static final Logger log = Logger.getLogger(EndEntityProfileMBean.class);
