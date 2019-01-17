98178204386248d9c0dfb16eec57a20b13c57cef
Tarmo Raudsep <tarmo.raudsep.ext@primekey.com>
2019-01-17 12:07:40 +0000

    ECA-7365 Add a Jenkins job for EJBCA UI Tests trying the whole webtest suite
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@31232 37aeb1de-b14b-0410-9a0e-82b28170d228

--- jenkins-files/EE_UBUNTU16_OpenJDK8_SELENIUMTESTS/selenium_image/run.sh
+++ jenkins-files/EE_UBUNTU16_OpenJDK8_SELENIUMTESTS/selenium_image/run.sh
@@ -12,28 +12,11 @@ sudo -E env "PATH=$PATH" cp -rf /opt/jboss-ejb-client.properties /app/ejbca/src/
 
 sleep 10
 
-
 cd /app/ejbca
-
-
-echo '=================== Debug Info ========================'
-# ls -la /home/seluser/.mozilla/firefox/svq3ko35.default
-tail /app/ejbca/src/appserver/jboss/jboss7/jboss-ejb-client.properties
 
 # needs to be *clean* build, because otherwise the ejb remote configs won't be built into the package
 sudo -E env "PATH=$PATH" ant clean build
 
 echo '=================== build finished ========================'
 
-
-
-# this test works in RA mode!
-#chown -R root /home/seluser
-#ant test:runone -Dtest.runone=EcaQa28_ServiceManagement
-
-# this one hangs?
-# ant test:runone -Dtest.runone=EcaQa12_CPManagement
-
-# this should run eventually!
 ant test:webtest
-
