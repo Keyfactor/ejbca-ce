7d485efc354c96c74ebdc91411b9d96e6536b29d
Bastian Fredriksson <bastian.fredriksson@primekey.com>
2019-02-19 14:46:18 +0000

    ECA-7810 Added notification in the RA Web with validation failures, and a structure for sending this data from the CA.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@31535 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-common-web/src/org/ejbca/core/model/approval/approvalrequests/GenerateTokenApprovalRequest.java
+++ modules/ejbca-common-web/src/org/ejbca/core/model/approval/approvalrequests/GenerateTokenApprovalRequest.java
@@ -29,22 +29,22 @@ import org.ejbca.core.model.approval.profile.ApprovalProfile;
 /**
  * Special Approval Request created when an administrator wants
  * to generate a token through the Web Service interface.
- *  
+ *
  * It is a two step approval request were the first step is
  * a view hard token PUK data and the second is the actual
  * hard token generation
- * 
+ *
  * @version $Id$
  */
 public class GenerateTokenApprovalRequest extends ApprovalRequest {
 
 	private static final long serialVersionUID = -1L;
-	
+
 	public static final int STEP_0_VIEWHARDTOKENDATA = 0;
 	public static final int STEP_1_GENERATETOKEN     = 1;
-	
+
 	private static final int LATEST_VERSION = 1;
-		
+
 	private String dn;
 	private String username;
 	private String tokenTypeLabel;
@@ -52,15 +52,15 @@ public class GenerateTokenApprovalRequest extends ApprovalRequest {
 	/** Constructor used in externalization only */
 	public GenerateTokenApprovalRequest() {}
 
-	public GenerateTokenApprovalRequest(String username, String userDN, String tokenTypeLabel, AuthenticationToken requestAdmin, 
+	public GenerateTokenApprovalRequest(String username, String userDN, String tokenTypeLabel, AuthenticationToken requestAdmin,
 	        String requestSignature, int cAId, int endEntityProfileId, ApprovalProfile approvalProfile) {
         // This is a 2 step approval, whatever that means...
-        super(requestAdmin, requestSignature, REQUESTTYPE_SIMPLE, cAId, endEntityProfileId, 2, approvalProfile);
+        super(requestAdmin, requestSignature, REQUESTTYPE_SIMPLE, cAId, endEntityProfileId, 2, approvalProfile, /* validation results */ null);
 		this.username = username;
 		this.dn = userDN;
 		this.tokenTypeLabel = tokenTypeLabel;
 	}
-	
+
 	@Override
 	public void execute() throws ApprovalRequestExecutionException {
 		// This is a non-executable approval
@@ -69,34 +69,38 @@ public class GenerateTokenApprovalRequest extends ApprovalRequest {
     /**
      * Approval Id is generated of This approval type (i.e AddEndEntityApprovalRequest) and UserName
      */
-	public int generateApprovalId() {
-		return new String(getApprovalType() + ";" + username + ";" + CertTools.getFingerprintAsString(getRequestAdminCert()) + 
+	@Override
+    public int generateApprovalId() {
+		return new String(getApprovalType() + ";" + username + ";" + CertTools.getFingerprintAsString(getRequestAdminCert()) +
 		        ";" + getApprovalProfile().getProfileName()).hashCode();
 	}
 
-	public int getApprovalType() {		
+	@Override
+    public int getApprovalType() {
 		return ApprovalDataVO.APPROVALTYPE_GENERATETOKEN;
 	}
 
 	@Override
 	public List<ApprovalDataText> getNewRequestDataAsText(AuthenticationToken admin) {
 		ArrayList<ApprovalDataText> retval = new ArrayList<ApprovalDataText>();
-		retval.add(new ApprovalDataText("USERNAME",username,true,false));		
+		retval.add(new ApprovalDataText("USERNAME",username,true,false));
 		retval.add(new ApprovalDataText("SUBJECTDN",dn,true,false));
 		retval.add(new ApprovalDataText("LABEL",tokenTypeLabel,true,true));
 		return retval;
 	}
-	
+
 	@Override
 	public List<ApprovalDataText> getOldRequestDataAsText(AuthenticationToken admin) {
 		return null;
 	}
 
-	public boolean isExecutable() {		
+	@Override
+    public boolean isExecutable() {
 		return false;
 	}
-	
-	public void writeExternal(ObjectOutput out) throws IOException {
+
+	@Override
+    public void writeExternal(ObjectOutput out) throws IOException {
 		super.writeExternal(out);
 		out.writeInt(LATEST_VERSION);
 		out.writeObject(username);
@@ -104,13 +108,14 @@ public class GenerateTokenApprovalRequest extends ApprovalRequest {
 		out.writeObject(tokenTypeLabel);
 	}
 
-	public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {        
+	@Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
 		super.readExternal(in);
         int version = in.readInt();
         if(version == 1){
     		username = (String) in.readObject();
             dn = (String) in.readObject();
-            tokenTypeLabel = (String) in.readObject();            
+            tokenTypeLabel = (String) in.readObject();
         }
 	}
 
