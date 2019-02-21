736a75a81eaf32a4c123fc9cede80e6a8310d2ed
Lauri Kongas <Lauri.Kongas.ext@primekey.com>
2019-02-21 11:37:22 +0000

    ECA-7693 CSR clear button in Ra Web
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@31549 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ra-gui/src/org/ejbca/ra/RaEndEntityBean.java
+++ modules/ra-gui/src/org/ejbca/ra/RaEndEntityBean.java
@@ -27,6 +27,7 @@ import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang.StringUtils;
+import org.cesecore.authentication.tokens.AuthenticationToken;
 import org.cesecore.authorization.AuthorizationDeniedException;
 import org.cesecore.certificates.ca.CADoesntExistsException;
 import org.cesecore.certificates.ca.CAInfo;
@@ -83,6 +84,7 @@ public class RaEndEntityBean implements Serializable {
     private int selectedStatus = -1;
     private String enrollmentCode = "";
     private String enrollmentCodeConfirm = "";
+    private boolean clearCsrChecked = false;
 
     /**
      * The SelectStatus class holds a status string/constant pair
@@ -164,6 +166,7 @@ public class RaEndEntityBean implements Serializable {
         issuedCerts = null;
         selectableStatuses = null;
         selectedStatus = -1;
+        clearCsrChecked = false;
     }
 
     public String getUsername() { return username; }
@@ -216,6 +219,12 @@ public class RaEndEntityBean implements Serializable {
                 endEntityInformation.setPassword(enrollmentCode);
                 changed = true;
             }
+        } 
+        if (clearCsrChecked) {
+            if (endEntityInformation.getExtendedInformation() != null) {
+                endEntityInformation.getExtendedInformation().setCertificateRequest(null);
+            }            
+            changed = true;
         }
         if (changed) {
             // Edit the End Entity if changes were made
@@ -358,5 +367,21 @@ public class RaEndEntityBean implements Serializable {
      */
     public boolean isApiEditCompatible() {
         return raMasterApiProxyBean.getApiVersion() >= 2;
+    }
+    
+    /**
+     * @return whether the Clear CSR checkbox is checked
+     */
+    public boolean getClearCsrChecked() {
+        return clearCsrChecked;
+    }
+    
+    /**
+     * Sets the CSR to be cleared after the save button is pressed
+     * 
+     * @param checked Whether the checkbox is checked
+     */
+    public void setClearCsrChecked(boolean checked) {
+        this.clearCsrChecked = checked;
     }
 }
