fb61f28dd78406c5fa295d6430a74139cf493d2e
Bastian Fredriksson <bastian.fredriksson@primekey.com>
2019-02-24 21:21:46 +0000

    Bastian is testing commit from new machine.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@31562 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/admin-gui/src/org/ejbca/ui/web/admin/BaseManagedBean.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/BaseManagedBean.java
@@ -17,7 +17,6 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.lang.reflect.Field;
 import java.net.URLEncoder;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
