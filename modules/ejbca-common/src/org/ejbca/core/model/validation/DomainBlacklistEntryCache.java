152b8b5345a3a3f48d5aae50a4bac43af78c1539
Ulf Undmark <ulf.undmark@primekey.com>
2019-02-25 11:54:17 +0000

    ECA-7809: Added DomainBlacklistEntryCache and updated CesecoreConfiguration
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@31573 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ejbca-common/src/org/ejbca/core/model/validation/DomainBlacklistEntryCache.java
@@ -0,0 +1,90 @@
+package org.ejbca.core.model.validation;
+
+import java.util.List;
+import java.util.Map;
+
+import org.cesecore.config.CesecoreConfiguration;
+import org.cesecore.internal.CommonCache;
+import org.cesecore.internal.CommonCacheBase;
+
+/**
+ * @version $Id: $
+ */
+
+
+public enum DomainBlacklistEntryCache implements CommonCache<DomainBlacklistEntry> {
+    INSTANCE;
+            
+    private final CommonCache<DomainBlacklistEntry> cache = new CommonCacheBase<DomainBlacklistEntry>() {
+        @Override
+        protected long getCacheTime() {
+            return Math.max(CesecoreConfiguration.getCacheDomainBlacklistTime(), 0);
+        };
+
+        @Override
+        protected long getMaxCacheLifeTime() {
+            // We never purge DomainBlacklist unless a database select discovers a missing object.
+            return 0L;
+        }
+    };
+    
+    @Override
+    public DomainBlacklistEntry getEntry(final Integer id) {
+        if (id == null) {
+            return null;
+        }
+        return cache.getEntry(id);
+    }
+
+    @Override
+    public DomainBlacklistEntry getEntry(final int id) {
+        return cache.getEntry(id);
+    }
+
+    @Override
+    public boolean shouldCheckForUpdates(final int id) {
+        return cache.shouldCheckForUpdates(id);
+    }
+
+    /**
+     * @param id entry ID
+     * @param digest Data.getProtectString(0).hashCode()
+     * @param name the fingerprint of the entry object
+     * @param object black list entry
+     */
+
+    @Override
+    public void updateWith(int id, int digest, String name, DomainBlacklistEntry object) {
+        cache.updateWith(id, digest, name, object);
+    }
+
+    @Override
+    public void removeEntry(int id) {
+        cache.removeEntry(id);
+    }
+
+    @Override
+    public String getName(int id) {
+        return cache.getName(id);
+    }
+
+    @Override
+    public Map<String, Integer> getNameToIdMap() {
+        return cache.getNameToIdMap();
+    }
+
+    @Override
+    public void flush() {
+        cache.flush();
+    }
+
+    @Override
+    public void replaceCacheWith(List<Integer> keys) {
+        cache.replaceCacheWith(keys);
+    }
+    @Override
+    public boolean willUpdate(int id, int digest) {
+        return cache.willUpdate(id, digest);
+    }
+
+}
