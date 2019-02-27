4bc4aaa6aba390b3c386bef2c3765def815b3373
Jekaterina Bunina <jekaterina.Bunina.ext@primekey.com>
2019-02-27 08:23:16 +0000

    ECA-7861 Implement DomainBlacklistAsciiLookalieNormalizer
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@31593 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-common-web/src/org/ejbca/core/model/validation/domainblacklist/DomainBlacklistAsciiLookalikeNormalizer.java
+++ modules/ejbca-common-web/src/org/ejbca/core/model/validation/domainblacklist/DomainBlacklistAsciiLookalikeNormalizer.java
@@ -12,14 +12,45 @@
  *************************************************************************/
 package org.ejbca.core.model.validation.domainblacklist;
 
+import org.apache.commons.lang.StringUtils;
+
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
- * TODO ECA-6052
+ * Replaces look alike ascii characters.
  *
  * @version $Id$
  */
 public class DomainBlacklistAsciiLookalikeNormalizer implements DomainBlacklistNormalizer {
+    private static Pattern pattern = Pattern.compile("(xn--)(.*?)(-.*)");
+
+
+    private Map<String, Character> replacementMap;
+
+    private Map<String, Character> getReplacementMap() {
+        if (replacementMap == null) {
+            replacementMap = new LinkedHashMap<>();
+            replacementMap.put("ci", 'a');
+            replacementMap.put("fi", 'a'); //fi->A->a
+            replacementMap.put("cl", 'd');
+            replacementMap.put("rn", 'm');
+            replacementMap.put("vv", 'w');
+            replacementMap.put("0", 'o');
+            replacementMap.put("6", 'b');
+            replacementMap.put("q", 'g');
+            replacementMap.put("9", 'g');
+            replacementMap.put("|", 'l');
+            replacementMap.put("1", 'l');
+            replacementMap.put("i", 'l');
+            replacementMap.put("5", 's');
+            replacementMap.put("2", 'z');
+            replacementMap.put("u", 'v');
+        }
+        return replacementMap;
+    }
 
     @Override
     public String getNameKey() {
@@ -34,8 +65,30 @@ public class DomainBlacklistAsciiLookalikeNormalizer implements DomainBlacklistN
 
     @Override
     public String normalize(final String domain) {
-        // TODO ECA-6052
-        return null;
+        if (StringUtils.isNotEmpty(domain)) {
+            String normalizeString = domain.toLowerCase();
+            StringBuilder sb = new StringBuilder(domain.length());
+            Matcher matcher = pattern.matcher(normalizeString);
+            boolean isPuny = false;
+            if (matcher.matches()) {
+                sb.append(matcher.group(1));
+                isPuny = true;
+                normalizeString = matcher.group(2);
+            }
+
+            for (Map.Entry<String, Character> entry : getReplacementMap().entrySet()) {
+                if (!isPuny || entry.getKey().length() == 1) {
+                    normalizeString = normalizeString.replace(entry.getKey(), String.valueOf(entry.getValue()));
+                }
+            }
+
+            sb.append(normalizeString);
+            if (isPuny) {
+                sb.append(matcher.group(3));
+            }
+            return sb.toString();
+        }
+        return domain;
     }
 
 }
