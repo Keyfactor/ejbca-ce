9bfd832a37df7cbcfe4b792ed30cc3939227fde5
Jekaterina Bunina <jekaterina.Bunina.ext@primekey.com>
2019-02-28 06:37:36 +0000

    ECA-7861 fix review comments
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@31607 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-common-web/src-test/org/ejbca/core/model/validation/domainblacklist/DomainBlacklistAsciiLookalikeNormalizerTest.java
+++ modules/ejbca-common-web/src-test/org/ejbca/core/model/validation/domainblacklist/DomainBlacklistAsciiLookalikeNormalizerTest.java
@@ -1,3 +1,16 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
 package org.ejbca.core.model.validation.domainblacklist;
 
 
@@ -46,5 +59,24 @@ public class DomainBlacklistAsciiLookalikeNormalizerTest  {
         assertEquals("Expexted normalized unicode", "котикgooglevv", normilizedUnicode);
     }
 
+    @Test
+    public void testNormalizePunicodeUnicodeCharsInMiddle() throws ParseException {
+        String punycode = IDN.toASCII("ama20nкотикg009levv");
+        String punycodeNormalized = normalizer.normalize(punycode);
+
+        String normilizedUnicode = IDN.toUnicode(punycodeNormalized);
+        assertEquals("Expexted normalized punicode", "xn--amazongooglevv-gxl5ab4ezd", punycodeNormalized);
+        assertEquals("Expexted normalized unicode", "amazonкотикgooglevv", normilizedUnicode);
+    }
+
+    @Test
+    public void testNormalizePunicodeAsciCharsInMiddle() throws ParseException {
+        String punycode = IDN.toASCII("котикg009levvчеширик");
+        String punycodeNormalized = normalizer.normalize(punycode);
+
+        String normilizedUnicode = IDN.toUnicode(punycodeNormalized);
+        assertEquals("Expexted normalized punicode", "xn--googlevv-j8gvjaobl4c6bxa9hva", punycodeNormalized);
+        assertEquals("Expexted normalized unicode", "котикgooglevvчеширик", normilizedUnicode);
+    }
 
 }
