a6b70b99f0c90c9d07aebb2b9cd81970ab4fff2e
Margaret Thomas <margaret.thomas@primekey.com>
2019-03-01 10:44:39 +0000

    EcaQa198 Refactor to support executing correctly on os.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@31648 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-webtest/src/org/ejbca/webtest/utils/CommandLineHelper.java
+++ modules/ejbca-webtest/src/org/ejbca/webtest/utils/CommandLineHelper.java
@@ -1,5 +1,11 @@
 package org.ejbca.webtest.utils;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
+
 public class CommandLineHelper {
 
 
@@ -11,18 +17,54 @@ public class CommandLineHelper {
      */
     public Boolean runCommand(String cmd) {
         Boolean isSuccessful = false;
+        String whichOS = System.getProperty("os.name");
+        Boolean isWindows = true;
 
-        try {
-            Runtime rt = Runtime.getRuntime();
-            Process pr = rt.exec(cmd);
-            isSuccessful = true;
-            return isSuccessful;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return isSuccessful;
+
+        if (!whichOS.toLowerCase().startsWith("windows")) {
+            isWindows = false;
         }
 
+        Process process = null;
+        try {
+            if (isWindows) {
+                process = Runtime.getRuntime()
+                        .exec("cmd.exe " + cmd);
+
+            } else {
+                System.out.println("I am running: sh " + cmd);
+                process = Runtime.getRuntime()
+                        .exec("sh " + cmd);
+            }
+
+            StreamGobbler streamGobbler =
+                    new StreamGobbler(process.getInputStream(), System.out::println);
+            Executors.newSingleThreadExecutor().submit(streamGobbler);
+            int exitCode = process.waitFor();
+            System.out.println("Exit Code:  " + exitCode);
+            assert exitCode == 0;
+            isSuccessful = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return isSuccessful;
     }
 
+    private static class StreamGobbler implements Runnable {
+        private InputStream inputStream;
+        private Consumer<String> consumer;
+
+        public StreamGobbler(InputStream inputStream, Consumer<String> consumer) {
+            this.inputStream = inputStream;
+            this.consumer = consumer;
+        }
+
+        @Override
+        public void run() {
+            new BufferedReader(new InputStreamReader(inputStream)).lines()
+                    .forEach(consumer);
+        }
+    }
 
 }
