cce16908f38b87e10ec7eee7d26b4b31f079f0a6
Bastian Fredriksson <bastian.fredriksson@primekey.com>
2019-03-05 11:04:17 +0000

    NoTicket: Testing commit after Eclipse broke down.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@31698 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/systemtests/src-test/org/ejbca/core/model/approval/approvalrequests/ApprovalPhaseValidationSystemTest.java
+++ modules/systemtests/src-test/org/ejbca/core/model/approval/approvalrequests/ApprovalPhaseValidationSystemTest.java
@@ -15,7 +15,6 @@ package org.ejbca.core.model.approval.approvalrequests;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
