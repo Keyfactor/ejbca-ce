69cec5b6ba6d2ef01a30ee05165d8d270595551f
Jekaterina Bunina <jekaterina.Bunina.ext@primekey.com>
2019-03-26 08:11:42 +0000

    ECA-8011 fix ingres scripts. Improve find query: if crlPartitionIndex is 0, it may be null in DB.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@31982 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/upgrade/700_710/700_710-upgrade-ingres.sql
+++ src/upgrade/700_710/700_710-upgrade-ingres.sql
@@ -1,5 +1,5 @@
 -- New columns in CertificateData are added by the JPA provider if there are sufficient privileges
 -- if not added automatically the following SQL statements can be run to add the new columns 
--- ALTER TABLE CertificateData ADD crlPartitionIndex INT4;
--- ALTER TABLE NoConflictCertificateData ADD crlPartitionIndex;
--- ALTER TABLE CRLData ADD crlPartitionIndex;
+-- ALTER TABLE CertificateData ADD crlPartitionIndex INT4 with null;
+-- ALTER TABLE NoConflictCertificateData ADD crlPartitionIndex with null;
+-- ALTER TABLE CRLData ADD crlPartitionIndex with null;
