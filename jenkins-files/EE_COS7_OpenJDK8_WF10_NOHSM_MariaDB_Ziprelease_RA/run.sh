bc68e91f5dfcdf7af4eb58c4970ad20ae8dd9a20
Tarmo Raudsep <tarmo.raudsep.ext@primekey.com>
2019-03-28 09:22:56 +0000

    ECA-8026 Create Jenkins jobs for limited RA / VA builds first commit
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@32005 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ jenkins-files/EE_COS7_OpenJDK8_WF10_NOHSM_MariaDB_Ziprelease_RA/run.sh
@@ -0,0 +1,103 @@
+#!/bin/sh
+
+# Options for JUnit JVM
+export TEST_OPTS="-XX:+UseG1GC -XX:+UseCompressedOops -XX:OnOutOfMemoryError='kill -9 %p' -Xms64m -Xmx512m"
+# Options for ant itself. The report building can be memory heavy, otherwise it shouldn't need much memory
+export ANT_OPTS="-XX:+UseG1GC -XX:+UseCompressedOops -XX:OnOutOfMemoryError='kill -9 %p' -Xms64m -Xmx512m"
+# Options for the CLI tools that require little memory, like the JBoss CLI
+export CLI_OPTS="-XX:+UseG1GC -XX:+UseCompressedOops -XX:OnOutOfMemoryError='kill -9 %p' -Xms64m -Xmx128m"
+
+echo '=================== CHECKING JAVA VERSION: ================================='
+java -version
+
+cp /opt/standalone1.xml /opt/jboss/wildfly/standalone/configuration/standalone.xml
+
+
+ant ziprelease -Dedition=ee -Dvariant=ra
+
+find . -name "ejbca*.zip" | xargs unzip
+
+echo '=================== fixing permissions in the original source folder ================================='
+chown -R 1001:1001 .
+
+cd ../ejbca_ee*
+
+
+# TODO try to exclude the documentation building part ?
+
+
+cp /opt/conf/* ./conf/
+
+
+/opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 &
+
+ant clean deployear
+
+
+wait_for_deployment() {
+    DEPLOY_SUCCESSFUL=0
+	# Wait for up to 180 seconds for app to start up
+	for i in {1..90} ; do
+		if [ -e "/opt/jboss/wildfly/standalone/deployments/ejbca.ear.deployed" ] ; then
+			echo "EJBCA successfully started."
+			DEPLOY_SUCCESSFUL=1
+			break
+		fi
+		if [ -e "/opt/jboss/wildfly/standalone/deployments/ejbca.ear.failed" ] ; then
+            echo "EJBCA deploy failed."
+            exit 1;
+        fi
+		echo 'waiting...'
+		sleep 2
+	done
+    if [ "$DEPLOY_SUCCESSFUL" -ne 1 ]; then
+        echo "EJBCA deploy timed out." 
+        exit 1;
+    fi
+}
+
+echo '=================== Waiting for deploy ================================='
+wait_for_deployment
+
+echo '=================== ant deployear done and successfully deployed! ================================='
+
+ant runinstall
+echo '=================== ant runinstall done! ================================='
+
+ant deploy-keystore
+echo '=================== ant deploy-keystore done! ================================='
+
+# load the final version of Wildfly conf and restart wildfly
+cp /opt/standalone2.xml /opt/jboss/wildfly/standalone/configuration/standalone.xml
+JAVA_OPTS="$CLI_OPTS" /opt/jboss/wildfly/bin/jboss-cli.sh -c --command=:reload
+
+# wait for reload to kick in and start undeploying and drop ejbca.ear.deployed file (otherwise we'd detect ejbca.ear.deployed file immediately again)
+sleep 10
+
+wait_for_deployment
+
+echo '=================== verify that VA and the X509CA and CVCCA implementation classes are missing ================================='
+
+if [-f ./modules/va/src-war/org/ejbca/ui/web/protocol/OCSPServlet.java ]; then
+    echo "RA-only build should not contain OCSPServlet.java"
+    exit 1;
+fi
+
+if [-f ./modules/va/resources/WEB-INF/web-status-ejbca.xml ]; then
+    echo "RA-only build should not contain web-status-ejbca.xml"
+    exit 1;
+fi
+
+if [-d ./modules/cesecore-cvcca ]; then
+    echo "RA-only build should not contain cesecore-cvcca module"
+    exit 1;
+fi
+
+if [-d ./modules/cesecore-x509ca ]; then
+    echo "RA-only build should not contain cesecore-x509ca module"
+    exit 1;
+fi
+
+
+echo '=================== fixing permissions in unzipped ziprlease folder ================================='
+chown -R 1001:1001 .
