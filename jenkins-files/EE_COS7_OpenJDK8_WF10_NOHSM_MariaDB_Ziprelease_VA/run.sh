b7186b7b2e09c3f934943eb9977e9d2163f52c75
Amin Khorsandi <amin.khorsandi@primekey.com>
2019-04-09 06:55:51 +0000

    ECA-8026: test successful so revert back the change.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@32083 37aeb1de-b14b-0410-9a0e-82b28170d228

--- jenkins-files/EE_COS7_OpenJDK8_WF10_NOHSM_MariaDB_Ziprelease_VA/run.sh
+++ jenkins-files/EE_COS7_OpenJDK8_WF10_NOHSM_MariaDB_Ziprelease_VA/run.sh
@@ -116,7 +116,6 @@ if [ -d ./modules/cesecore-x509ca ]; then
     exit 1;
 fi
 
-exit 1;
 
 echo "=================== All the files, that should be missing, are indeed properly missing ================================="
 
