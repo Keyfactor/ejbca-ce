fe806beb5556b92c31f09942e0fe884bdeecaf6a
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2019-04-09 16:05:47 +0000

    ECA-7984: Run Selenium containers as jenkins user
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@32097 37aeb1de-b14b-0410-9a0e-82b28170d228

--- jenkins-files/EE_UBUNTU16_OpenJDK8_WF12_SELENIUMTESTS/selenium_image/run.sh
+++ jenkins-files/EE_UBUNTU16_OpenJDK8_WF12_SELENIUMTESTS/selenium_image/run.sh
@@ -5,20 +5,10 @@ export TEST_OPTS="-XX:+UseG1GC -XX:+UseCompressedOops -XX:OnOutOfMemoryError='ki
 # Options for ant itself. The report building can be memory heavy, otherwise it shouldn't need much memory
 export ANT_OPTS="-XX:+UseG1GC -XX:+UseCompressedOops -XX:OnOutOfMemoryError='kill -9 %p' -Xms64m -Xmx1536m"
 
-# Function that is always run at exit
-workspacesubdir=$(pwd)
-cleanup() {
-        echo '=================== cleanup. fixing permissions ================================='
-        chown -R 1001:1001 "$workspacesubdir"
-}
-trap cleanup EXIT
-
-# try those with sudo
-# add the seluser guy to root group
-sudo -E env "PATH=$PATH" cp /opt/ejbca_conf/* /app/ejbca/conf/
-sudo -E env "PATH=$PATH" cp /opt/ejbca_webtest_conf/* /app/ejbca/modules/ejbca-webtest/conf/
-sudo -E env "PATH=$PATH" cp /opt/propertyDefaults.xml /app/ejbca/propertyDefaults.xml
-sudo -E env "PATH=$PATH" cp -rf /opt/jboss-ejb-client.properties /app/ejbca/src/appserver/jboss/jboss7/jboss-ejb-client.properties
+cp /opt/ejbca_conf/* /app/ejbca/conf/
+cp /opt/ejbca_webtest_conf/* /app/ejbca/modules/ejbca-webtest/conf/
+cp /opt/propertyDefaults.xml /app/ejbca/propertyDefaults.xml
+cp -rf /opt/jboss-ejb-client.properties /app/ejbca/src/appserver/jboss/jboss7/jboss-ejb-client.properties
 
 /opt/bin/entry_point.sh &
 
@@ -27,7 +17,7 @@ sleep 10
 cd /app/ejbca
 
 # needs to be *clean* build, because otherwise the ejb remote configs won't be built into the package
-sudo -E env "PATH=$PATH" "ANT_OPTS=$ANT_OPTS" "JAVA_OPTS=$JAVA_OPTS" ant clean build
+ant clean build
 
 echo '=================== build finished ========================'
 
