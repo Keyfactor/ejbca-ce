c15660c0d0f36f310b296ee00d715bef113f5f7a
Jekaterina Bunina <jekaterina.Bunina.ext@primekey.com>
2019-04-09 07:13:18 +0000

    ECA-7966 RA-API, WS and REST support for Partitioned CRLs
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@32085 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ejbca-ws-cli/src-gen/org/ejbca/core/protocol/ws/client/gen/GetLatestCRLPartitionResponse.java
@@ -0,0 +1,63 @@
+
+package org.ejbca.core.protocol.ws.client.gen;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElementRef;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for getLatestCRLPartitionResponse complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="getLatestCRLPartitionResponse">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="return" type="{http://www.w3.org/2001/XMLSchema}base64Binary" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "getLatestCRLPartitionResponse", propOrder = {
+    "_return"
+})
+public class GetLatestCRLPartitionResponse {
+
+    @XmlElementRef(name = "return", type = JAXBElement.class, required = false)
+    protected JAXBElement<byte[]> _return;
+
+    /**
+     * Gets the value of the return property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link JAXBElement }{@code <}{@link byte[]}{@code >}
+     *     
+     */
+    public JAXBElement<byte[]> getReturn() {
+        return _return;
+    }
+
+    /**
+     * Sets the value of the return property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link JAXBElement }{@code <}{@link byte[]}{@code >}
+     *     
+     */
+    public void setReturn(JAXBElement<byte[]> value) {
+        this._return = value;
+    }
+
+}
