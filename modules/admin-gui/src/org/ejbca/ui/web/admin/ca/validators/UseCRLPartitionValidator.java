7120a1a85270ba257ddc906f7ec663b48401aeb5
Jekaterina Bunina <jekaterina.Bunina.ext@primekey.com>
2019-04-12 12:29:08 +0000

    ECA-8052 Partitioned CRLs should not be allowed without "Issuing Distribution Point" CRL extension
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@32133 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/ca/validators/UseCRLPartitionValidator.java
@@ -0,0 +1,51 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.ui.web.admin.ca.validators;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+/**
+ * Validator used in validating useCrlPartition conditions (edit ca page).
+ *
+ * @version $Id$
+ */
+@FacesValidator("org.ejbca.ui.web.admin.ca.validators.UseCRLPartitionValidator")
+public class UseCRLPartitionValidator implements Validator {
+    private static final Logger log = Logger.getLogger(UseCRLPartitionValidator.class);
+
+    @Override
+    public void validate(FacesContext facesContext, UIComponent component, Object o) throws ValidatorException {
+        Boolean usePartitionedCrl = (Boolean) o;
+        if (usePartitionedCrl) {
+            UIInput defaultCRLDistPointInput = (UIInput) component.getAttributes().get("defaultCRLDistPointInput");
+            String defaultCRLDistPoint = (String) defaultCRLDistPointInput.getValue();
+            UIInput useCrlDistributiOnPointOnCrlInput = (UIInput) component.getAttributes().get("useCrlDistributiOnPointOnCrl");
+            Boolean useCrlDistributiOnPointOnCrl = (Boolean) useCrlDistributiOnPointOnCrlInput.getValue();
+
+            if (!useCrlDistributiOnPointOnCrl || StringUtils.isEmpty(defaultCRLDistPoint)) {
+                FacesMessage msg = new FacesMessage("Partitioned CRLs are not allowed without 'Issuing Distribution Point' and 'Default CRL Distribution Point'.", "Use CRL partition validation failed!");
+                msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+                throw new ValidatorException(msg);
+            }
+        }
+    }
+}
