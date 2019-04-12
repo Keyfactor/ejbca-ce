79b77d0a4c6215f1a2b5d9cc7b05e798b9f4c509
Lauri Kongas <Lauri.Kongas.ext@primekey.com>
2019-04-12 10:41:12 +0000

    ECA-8059 Make Jenkins Selenium UI tests run on a full CA installation
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@32126 37aeb1de-b14b-0410-9a0e-82b28170d228

--- jenkins-files/EE_UBUNTU16_OpenJDK8_WF12_SELENIUMTESTS/ejbca_image/run.sh
+++ jenkins-files/EE_UBUNTU16_OpenJDK8_WF12_SELENIUMTESTS/ejbca_image/run.sh
@@ -57,12 +57,13 @@ sleep 30
 wait_for_deployment
 echo '=================== Wildfly restarted after deploy-keystore ========================'
 
-JAVA_OPTS="$EJBCACLI_OPTS" bin/ejbca.sh ca importcacert ManagementCA ManagementCA.pem
-JAVA_OPTS="$EJBCACLI_OPTS" bin/ejbca.sh roles addrolemember --role "Super Administrator Role" --caname ManagementCA --with WITH_COMMONNAME --value SuperAdmin
+#JAVA_OPTS="$EJBCACLI_OPTS" bin/ejbca.sh ca importcacert ManagementCA ManagementCA.pem
+#JAVA_OPTS="$EJBCACLI_OPTS" bin/ejbca.sh roles addrolemember --role "Super Administrator Role" --caname ManagementCA --with WITH_COMMONNAME --value SuperAdmin
 
 # manually change the "status" of CA from external -> active
 mysql -u ejbca -pejbca -hmariadb_selenium -e 'use ejbca; update CAData set status = 1 where status = 6;'
-ls -l /home/jenkins/.mozilla
+find
+#ls -l /home/jenkins/.mozilla
 ant ejbca:setup:selenium -Dbrowser.firefox.binary=/usr/bin/firefox -Denv.HOME=/home/jenkins
 echo '=================== import cert commands done ========================'
 
