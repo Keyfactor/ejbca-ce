e6a9083f349264632d336629db756a51f326341b
Tarmo Raudsep <tarmo.raudsep.ext@primekey.com>
2019-04-16 06:02:40 +0000

    ECA-8029 Remove Hard Tokens, Hard Token Profiles and Hard Token Issuers from EJBCA cleanup
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@32161 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-common/src/org/ejbca/core/model/hardtoken/HardTokenConstants.java
+++ modules/ejbca-common/src/org/ejbca/core/model/hardtoken/HardTokenConstants.java
@@ -17,16 +17,6 @@ package org.ejbca.core.model.hardtoken;
  * @version $Id$
  */
 public interface HardTokenConstants {
-//    public static final int TOKENTYPE_TURKISHEID = 3;   //TurkishEIDProfile.TYPE_TURKISHEID;
-//    public static final int TOKENTYPE_SWEDISHEID = 1;   //SwedishEIDProfile.TYPE_SWEDISHEID;
-//    public static final int TOKENTYPE_ENHANCEDEID = 2;  //EnhancedEIDProfile.TYPE_ENHANCEDEID;
-//    
-//    public static final String LABEL_REGULARCARD   = "LABEL_REGULARCARD";   //HardToken.LABEL_REGULARCARD;
-//    public static final String LABEL_TEMPORARYCARD = "LABEL_TEMPORARYCARD"; //HardToken.LABEL_TEMPORARYCARD;
-//    public static final String LABEL_PROJECTCARD   = "LABEL_PROJECTCARD";   //HardToken.LABEL_PROJECTCARD;
-//    
-//    public static final int PINTYPE_BASIC = 1;
-//    public static final int PINTYPE_SIGNATURE = 2;
     
     public static final int REQUESTTYPE_PKCS10_REQUEST   = 1;
     public static final int REQUESTTYPE_KEYSTORE_REQUEST = 2;
