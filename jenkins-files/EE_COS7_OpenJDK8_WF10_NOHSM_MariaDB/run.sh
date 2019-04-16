8cbd280d5bd318429fe76fb2ec84cb3fb29200ee
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2019-04-16 09:35:02 +0000

    ECA-8010: Remove unused environment variables
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@32173 37aeb1de-b14b-0410-9a0e-82b28170d228

--- jenkins-files/EE_COS7_OpenJDK8_WF10_NOHSM_MariaDB/run.sh
+++ jenkins-files/EE_COS7_OpenJDK8_WF10_NOHSM_MariaDB/run.sh
@@ -7,7 +7,6 @@ export ANT_OPTS="-XX:+UseG1GC -XX:+UseCompressedOops -XX:OnOutOfMemoryError='kil
 # Options for the CLI tools. These require very little memory.
 # Note that the Wildfly CLI does not do escaping properly, so we can't use option values with spaces.
 export JBOSSCLI_OPTS="-XX:+UseG1GC -XX:+UseCompressedOops -Xms32m -Xmx128m"
-export EJBCACLI_OPTS="-XX:+UseG1GC -XX:+UseCompressedOops -XX:OnOutOfMemoryError='kill -9 %p' -Xms32m -Xmx128m"
 
 echo '=================== CHECKING JAVA VERSION: ================================='
 java -version
