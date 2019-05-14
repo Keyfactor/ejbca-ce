33838180e5480708ac9f532870e424248a3ac812
Tarmo Raudsep <tarmo.raudsep.ext@primekey.com>
2019-05-14 09:01:57 +0000

    ECA-8126 Automate ECAQA-5 more fields filled
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@32340 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-webtest/src-test/org/ejbca/webtest/scenario/EcaQa5_AddEndUserEndEntity.java
+++ modules/ejbca-webtest/src-test/org/ejbca/webtest/scenario/EcaQa5_AddEndUserEndEntity.java
@@ -62,9 +62,29 @@ public class EcaQa5_AddEndUserEndEntity extends WebTestBase {
         fields.put("Password (or Enrollment Code)", "foo123");
         fields.put("Confirm Password", "foo123");
         
+        
+        fields.put("ST, State or Province", "Germany");
+        fields.put("OU, Organizational Unit", "QA");
+        fields.put("L, Locality", "Europe");
+        fields.put("Jurisdiction Locality [EV Certificate]", "aJurisdictionLocality");
+        fields.put("unstructuredName, Domain name (FQDN)", "pkiemail.qa.primekey.se");
+        fields.put("postalAddress", "#301d0c0f536f6d6520737472656574203132330c0a534f4d4520504c41434");
+        fields.put("name", "Tester");
+        fields.put("Jurisdiction State or Province [EV Certificate]", "aJurisdictionState");
+        fields.put("UID, Unique Identifier", "ECAQA5");
+        fields.put("NIF, Tax ID number, for individuals (Spain)", "1234");
+        fields.put("CIF, Tax ID code, for companies (Spain)", "5678");
+        fields.put("unstructuredAddress, IP address", "127.0.0.1");
+        fields.put("businessCategory, Organization type",  "QA");
+        fields.put("CN, Common name", "TestEndEnityEMPTY");
+        fields.put("postalCode", "12345");
+        fields.put("O, Organization", "QA");
+        fields.put("pseudonym", "tester");
+                
         addEndEntityHelper.fillFields(fields);
         addEndEntityHelper.triggerBatchGeneration();
         
         addEndEntityHelper.fillFieldEmail("you_mail_box", "primekey.se");
+        
     }
 }
