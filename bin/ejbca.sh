32a6da210f85493204c2c209b8c3c133c368c91a
Bastian Fredriksson <bastian.fredriksson@primekey.com>
2019-05-15 07:49:09 +0000

    ECA-8160 Using -z instead of ! -n and executing the jar with exec.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@32342 37aeb1de-b14b-0410-9a0e-82b28170d228

--- bin/ejbca.sh
+++ bin/ejbca.sh
@@ -2,17 +2,17 @@
 
 JAVACMD=$(which java)
 
-if [ ! -n "$JAVACMD" ]; then
-    if [ ! -n "$JAVA_HOME" ]; then
+if [ -z "$JAVACMD" ]; then
+    if [ -z "$JAVA_HOME" ]; then
         echo 'You must set JAVA_HOME before running the EJBCA CLI.' 1>&2
         exit 1
     else
-	JAVACMD="$JAVA_HOME/bin/java"
+	    JAVACMD="$JAVA_HOME/bin/java"
     fi
 fi
 
 
-if [ ! -n "$EJBCA_HOME" ]; then
+if [ -z "$EJBCA_HOME" ]; then
     CLI_JAR=$(dirname "$0")/../dist/ejbca-ejb-cli/ejbca-ejb-cli.jar
 else
     CLI_JAR="$EJBCA_HOME/dist/ejbca-ejb-cli/ejbca-ejb-cli.jar"
@@ -23,4 +23,4 @@ if [ ! -f "$CLI_JAR" ]; then
     exit 2
 fi
 
-"$JAVACMD" -jar "$CLI_JAR" "$@"
+exec "$JAVACMD" -jar "$CLI_JAR" "$@"
