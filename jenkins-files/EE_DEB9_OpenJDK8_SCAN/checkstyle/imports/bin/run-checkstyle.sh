43b7ea4ad3fa4348573d199bd9454e3f4585841b
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2019-05-25 05:47:32 +0000

    ECA-8086: Change CheckStyle warnings
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@32411 37aeb1de-b14b-0410-9a0e-82b28170d228

--- jenkins-files/EE_DEB9_OpenJDK8_SCAN/checkstyle/imports/bin/run-checkstyle.sh
+++ jenkins-files/EE_DEB9_OpenJDK8_SCAN/checkstyle/imports/bin/run-checkstyle.sh
@@ -40,9 +40,6 @@ fi
 
 styleCheckRules="code-analyzer-tools/checkstyle/checks/sun_checks.xml"
 
-# TODO: Instead of pulling the Sun recommendations and doing hard-coded redactions the EJBCA team
-#       should agree on a set of rules and configure such XML configartion to be used right away.
-#
 # Redact rules that we definitely don't care about and makes the report to large to handle
 # - LineLength: "Line is longer than 80 characters (found ...)."
 # - RegexpSinglelineCheck: "Line has trailing spaces." -> There are too many such "errors" to cope with. Allow 99..
@@ -51,14 +48,25 @@ styleCheckRules="code-analyzer-tools/checkstyle/checks/sun_checks.xml"
 # - JavadocPackage: Defacto not followed in EJBCA
 # - JavadocVariable: Defacto not followed in EJBCA unless motivated.
 # - DesignForExtension: Usually implicit when such extension is intended. Not everything needs to be designed for extension.
+# - AvoidInlineConditionalsCheck: Not anything bad with this
+# - AvoidNestedBlocksCheck: Not anything bad with this
+# - ConstantNameCheck: Matches non-compile time constant fields also, such as "log", so too much spam
+# - EmptyBlockCheck: Also matches empty blocks with a comment explaining why it is empty.
+# - InnerAssignmentCheck: Useful code pattern sometimes.
+# - InterfaceIsTypeCheck: Matches interfaces that lack methods, but that gives false positives with interfaces that describe a behavior only.
+# - LeftCurlyCheck: Lots of false positives for getters and setters.
+# - MethodNameCheck: Conflicts with EcaQa test case naming.
+# - MissingSwitchDefaultCheck: This is actually a good coding practice, because then the compiler can check for missing enum cases.
+# - NoWhitespaceAfterCheck: Contrary to our usual style for array initializers.
+# - OperatorWrapCheck: Controversial topic, some find this less error prone other find it more error prone. Also contrary to most of the EJBCA code.
+# - RegexpSinglelineCheck: False positives on indented blank lines, which in turn is controversial.
+# - TodoCommentCheck: False positives with TODO comments that have an issue number.
+# - TypeNameCheck: False positives with WS method names.
 cat "${styleCheckRules}" \
-    | grep -v LineLength \
     | sed 's/maximum" value="0/maximum" value="99/' \
-    | grep -v WhitespaceAround \
-    | grep -v FinalParameters \
-    | grep -v JavadocPackage \
-    | grep -v JavadocVariable \
-    | grep -v DesignForExtension \
+    | grep -vE '(LineLength|WhitespaceAround|FinalParameters|JavadocPackage|JavadocVariable|DesignForExtension|AvoidInlineConditionalsCheck)' \
+    | grep -vE '(AvoidNestedBlocksCheck|ConstantNameCheck|EmptyBlockCheck|InnerAssignmentCheck|InterfaceIsTypeCheck|LeftCurlyCheck)̈́' \
+    | grep -vE '(MethodNameCheck|MissingSwitchDefaultCheck|NoWhitespaceAfterCheck|OperatorWrapCheck|RegexpSinglelineCheck|TodoCommentCheck|TypeNameCheck)' \
     > /tmp/checks.xml
 
 # --checker-threads-number=$coreLimit -> "IllegalArgumentException: Multi thread mode for Checker module is not implemented"
