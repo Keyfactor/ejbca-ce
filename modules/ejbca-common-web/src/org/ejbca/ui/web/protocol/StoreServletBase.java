bee101dcadbbde28420b7753c7548b50c6928226
Amin Khorsandi <amin.khorsandi@primekey.com>
2019-05-28 12:48:37 +0000

    ECA-8188: Update the code comment.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@32449 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-common-web/src/org/ejbca/ui/web/protocol/StoreServletBase.java
+++ modules/ejbca-common-web/src/org/ejbca/ui/web/protocol/StoreServletBase.java
@@ -58,8 +58,8 @@ public abstract class StoreServletBase extends HttpServlet {
     public void init(ServletConfig config) throws ServletException {
 		super.init(config);
 		this.certCache = CaCertificateCache.INSTANCE;
-        // Since this servlet is referenced in cmpHttpProxy module (via ejbca-common-web dependency), 
-        // to make that module deploy-able in JEE servers we initialize 
+        // Since this servlet is referenced in ejbca-common-web module and that module is referenced 
+        // by cmpHttpProxy module, to make cmpHttpProxy module deploy-able in JEE servers we initialize 
         // the certificateStoreSession bean here instead of using the EJB annotation.
         certificateStoreSession = new EjbLocalHelper().getCertificateStoreSession();
 	}
