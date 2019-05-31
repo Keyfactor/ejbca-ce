ddb26200a9ebd6683f4f5065a4cb1a4ec4a84580
Tarmo Raudsep <tarmo.raudsep.ext@primekey.com>
2019-05-31 09:02:04 +0000

    ECA-8192 Move REST resources into separate modules
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@32475 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-rest-common/src-test/org/ejbca/ui/web/rest/api/builder/SearchCertificatesRestRequestTestBuilder.java
+++ modules/ejbca-rest-common/src-test/org/ejbca/ui/web/rest/api/builder/SearchCertificatesRestRequestTestBuilder.java
@@ -8,23 +8,26 @@
  *                                                                       *
  *************************************************************************/
 
-package org.ejbca.ui.web.rest.api.io.request;
+package org.ejbca.ui.web.rest.api.builder;
 
 import java.util.Collections;
+
+import org.ejbca.ui.web.rest.api.io.request.SearchCertificateCriteriaRestRequest;
+import org.ejbca.ui.web.rest.api.io.request.SearchCertificatesRestRequest;
 
 /**
  *
  * @version $Id: SearchCertificatesRestRequestBuilder.java 29544 2018-08-01 12:07:08Z mikekushner $
  */
-public class SearchCertificatesRestRequestBuilder extends SearchCertificatesRestRequest {
+public class SearchCertificatesRestRequestTestBuilder extends SearchCertificatesRestRequest {
 
-    public static org.ejbca.ui.web.rest.api.io.request.SearchCertificatesRestRequestBuilder.SearchCertificatesRestRequestBuilder withDefaults() {
+    public static SearchCertificatesRestRequestBuilder withDefaults() {
         final SearchCertificateCriteriaRestRequest searchCertificateCriteriaRestRequest = SearchCertificateCriteriaRestRequest.builder()
                 .property(SearchCertificateCriteriaRestRequest.CriteriaProperty.QUERY.name())
                 .operation(SearchCertificateCriteriaRestRequest.CriteriaOperation.EQUAL.name())
                 .value("TEST")
                 .build();
-        return org.ejbca.ui.web.rest.api.io.request.SearchCertificatesRestRequestBuilder
+        return SearchCertificatesRestRequestTestBuilder
                 .builder()
                 .maxNumberOfResults(1)
                 .criteria(Collections.singletonList(searchCertificateCriteriaRestRequest));
