aa7da0e33b2d1f94c6e6f260ea8bbbf0e05b6efe
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2019-06-27 09:05:29 +0000

    ECA-7278: initial support for Azure Key Vault (REST API)
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@32626 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/cesecore-common/src/org/cesecore/keys/token/KeyAliasesCache.java
@@ -0,0 +1,44 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.cesecore.keys.token;
+
+import java.security.PublicKey;
+
+import org.cesecore.internal.CommonCacheBase;
+
+/**
+ * Cache for Key aliases (used in Azure Crypto Token, but is generic). Caches a public key, with alias as key
+ * 
+ * @version $Id: InternalKeyBindingCache.java 28332 2018-02-20 14:40:52Z anatom $
+ */
+public class KeyAliasesCache extends CommonCacheBase<PublicKey> {
+
+    @Override
+    public PublicKey getEntry(final Integer id) {
+        if (id == null) {
+            return null;
+        }
+        return super.getEntry(id);
+    }
+
+    @Override
+    protected long getCacheTime() {
+        return 30000; // Cache key aliases for 30 seconds
+    }
+
+    @Override
+    protected long getMaxCacheLifeTime() {
+        return 30000; // Cache key aliases for 30 seconds
+    }
+
+}
