e93c8deac0be8562dbae05af266739feb2fb6b0c
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2019-06-27 12:31:30 +0000

    Noticket: update to version 7.3
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@32640 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/systemtests/src-test/org/ejbca/core/protocol/ws/EjbcaWSTest.java
+++ modules/systemtests/src-test/org/ejbca/core/protocol/ws/EjbcaWSTest.java
@@ -1304,7 +1304,7 @@ public class EjbcaWSTest extends CommonEjbcaWs {
     public void testEjbcaVersion() {
         final String version = ejbcaraws.getEjbcaVersion();
         // We don't know which specific version we are testing
-        final String expectedSubString = "EJBCA 7.2";
+        final String expectedSubString = "EJBCA 7.3";
         assertTrue("Wrong version: "+version + " (expected to contain " + expectedSubString + ")", version.contains(expectedSubString));    }
 
     @Test
