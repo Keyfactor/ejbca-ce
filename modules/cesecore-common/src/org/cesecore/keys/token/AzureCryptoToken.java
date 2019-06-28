4879daf17997eac0a4a34c4f792a6fcbe5fa9356
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2019-06-28 10:07:23 +0000

    ECA-7278: don't parse the dummy key every time we want it
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@32657 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-common/src/org/cesecore/keys/token/AzureCryptoToken.java
+++ modules/cesecore-common/src/org/cesecore/keys/token/AzureCryptoToken.java
@@ -123,12 +123,20 @@ public class AzureCryptoToken extends BaseCryptoToken {
      */
     private KeyAliasesCache aliasCache = new KeyAliasesCache();
     
+    private static PublicKey ecPublicDummyKey = null;
+    private static final byte[] ecPublicKeyBytes = Base64.decode(("MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEAnXBeTH4xcl2c8VBZqtfgCTa+5sc" + 
+            "wV+deHQeaRJQuM5DBYfee9TQn+mvBfYPCTbKEnMGeoYq+BpLCBYgaqV6hw==").getBytes());
     /** @return a dummy key (hardcoded prime256v1) to add in initial stage of caching, because we can not put anything empty in the cache, because putting null means remove...
      */
     private static final PublicKey getDummyCacheKey() {
-        final byte[] ecPublicKey = Base64.decode(("MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEAnXBeTH4xcl2c8VBZqtfgCTa+5sc" + 
-                "wV+deHQeaRJQuM5DBYfee9TQn+mvBfYPCTbKEnMGeoYq+BpLCBYgaqV6hw==").getBytes());
-        return KeyTools.getPublicKeyFromBytes(ecPublicKey);
+        if (ecPublicDummyKey == null) {
+            synchronized (ecPublicDummyKey) {
+                if (ecPublicDummyKey == null) {
+                    ecPublicDummyKey = KeyTools.getPublicKeyFromBytes(ecPublicKeyBytes);
+                }
+            }
+        }
+        return ecPublicDummyKey;
     }
 
     /** get the keyVaultName, set during init of crypto token */
