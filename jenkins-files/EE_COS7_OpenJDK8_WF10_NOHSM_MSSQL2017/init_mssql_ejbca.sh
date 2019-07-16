a3ad32edeb933233478f7567bdbcf20603f43206
Andrey Sergeev <andrey.sergeev.ext@primekey.com>
2019-07-16 10:31:31 +0000

    [ECA-8121] Add Jenkins job with MS SQL
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@32738 37aeb1de-b14b-0410-9a0e-82b28170d228

--- jenkins-files/EE_COS7_OpenJDK8_WF10_NOHSM_MSSQL2017/init_mssql_ejbca.sh
+++ jenkins-files/EE_COS7_OpenJDK8_WF10_NOHSM_MSSQL2017/init_mssql_ejbca.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-docker exec -it $DOCKER_NAME_DB /opt/mssql-tools/bin/sqlcmd -S $DOCKER_NAME_NET -U sa -P $DB_PASSWORD<<EOF
-sqlcmd -Q "create database ejbca" -o ejbca.log
-cat ejbca.log
-EOF
+docker exec -i $DOCKER_NAME_DB /opt/mssql-tools/bin/sqlcmd -S $DOCKER_NAME_DB -U sa -P 'DB_PASSWORD' -Q 'CREATE DATABASE ejbca'
+
