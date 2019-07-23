b37648d56b8f72a9380ad80ffc9b727a1b7f4bb6
Henrik Sunmark <henrik.sunmark@primekey.com>
2019-07-23 11:25:03 +0000

    ECA-8355: EST Name Generation Enhancements
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@32833 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/admin-gui/src/org/ejbca/ui/web/admin/configuration/EditEstConfigMBean.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/configuration/EditEstConfigMBean.java
@@ -13,6 +13,7 @@
 package org.ejbca.ui.web.admin.configuration;
 
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -22,6 +23,7 @@ import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 import javax.faces.model.SelectItem;
 
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.math.NumberUtils;
 import org.cesecore.authorization.AuthorizationDeniedException;
@@ -29,6 +31,7 @@ import org.ejbca.config.EstConfiguration;
 import org.ejbca.core.model.authorization.AccessRulesConstants;
 import org.ejbca.ui.web.admin.BaseManagedBean;
 import org.ejbca.ui.web.jsf.configuration.EjbcaJSFHelper;
+import org.ejbca.core.model.ra.UsernameGeneratorParams;
 
 /**
  * Backing bean for edit EST alias view.
@@ -39,6 +42,12 @@ import org.ejbca.ui.web.jsf.configuration.EjbcaJSFHelper;
 @ViewScoped
 public class EditEstConfigMBean extends BaseManagedBean implements Serializable {
     private static final long serialVersionUID = 1L;
+
+    private static final List<String> dnfields = Arrays.asList("CN", "UID", "OU", "O", "L", "ST", "DC", "C", "emailAddress", "SN", "givenName", "initials", "surname", "title", 
+            "unstructuredAddress", "unstructuredName", "postalCode", "businessCategory", "dnQualifier", "postalAddress", 
+            "telephoneNumber", "pseudonym", "streetAddress", "name", "CIF", "NIF");
+
+    private String selectedRaNameSchemeDnPart;
 
     @ManagedProperty(value = "#{estConfigMBean}")
     private EstConfigMBean estConfigMBean;
@@ -211,6 +220,176 @@ public class EditEstConfigMBean extends BaseManagedBean implements Serializable 
         getEjbcaWebBean().updateEstConfigFromClone(alias);
         reset();
         return "done";
+    }
+
+    /**
+     * Add DN field to name generation parameter
+     *
+     */
+    public void actionAddRaNameSchemeDnPart() {
+    	String alias = estAliasGui.getName();
+    	EstConfiguration estConfiguration = getEjbcaWebBean().getEstConfigForEdit(alias);
+        String currentNameGenParam = estConfiguration.getRANameGenParams(getSelectedEstAlias());
+        String[] params = currentNameGenParam == null ? new String[0] : currentNameGenParam.split(";");
+        // Verify that current param is instance of DN fields
+        if((params.length > 0) && ( dnfields.contains(params[0]) )) {
+            if(!ArrayUtils.contains(params, getSelectedRaNameSchemeDnPart())) {
+                currentNameGenParam += ";" + getSelectedRaNameSchemeDnPart();
+            }
+        } else {
+                currentNameGenParam = getSelectedRaNameSchemeDnPart();
+        }
+        estConfiguration.setRANameGenParams(getSelectedEstAlias(), currentNameGenParam);
+    }
+
+    /**
+     * Remove DN field from name generation parameter
+     *
+     */    
+    public void actionRemoveRaNameSchemeDnPart() {
+    	String alias = estAliasGui.getName();
+    	EstConfiguration estConfiguration = getEjbcaWebBean().getEstConfigForEdit(alias);
+        String currentNameGenParam = estConfiguration.getRANameGenParams(getSelectedEstAlias());
+        if(StringUtils.contains(currentNameGenParam, getSelectedRaNameSchemeDnPart())) {
+            String[] params = currentNameGenParam.split(";");
+            if(params.length == 1) {
+                currentNameGenParam = "";
+            } else {
+                if(StringUtils.equals(params[0], getSelectedRaNameSchemeDnPart())) {
+                    currentNameGenParam = StringUtils.remove(currentNameGenParam, getSelectedRaNameSchemeDnPart() + ";");
+                } else {
+                    currentNameGenParam = StringUtils.remove(currentNameGenParam, ";" + getSelectedRaNameSchemeDnPart());
+                }
+            }
+            estConfiguration.setRANameGenParams(getSelectedEstAlias(), currentNameGenParam);
+        }
+    }
+
+    /**
+     * Set the name generation scheme: DN/RANDOM/FIXED/USERNAME
+     *
+     */ 
+    public void setRaNameGenScheme(final String scheme) {
+    	String alias = estAliasGui.getName();
+    	EstConfiguration estConfiguration = getEjbcaWebBean().getEstConfigForEdit(alias);
+        estConfiguration.setRANameGenScheme(getSelectedEstAlias(), scheme);
+    }
+    
+    /**
+     * Get the name generation scheme: DN/RANDOM/FIXED/USERNAME
+     *
+     */     
+    public String getRaNameGenScheme() {
+    	String alias = estAliasGui.getName();
+    	EstConfiguration estConfiguration = getEjbcaWebBean().getEstConfigForEdit(alias);
+        return estConfiguration.getRANameGenScheme(getSelectedEstAlias());
+    }    
+
+    public String getSelectedEstAlias() {
+        return estConfigMBean.getSelectedAlias();
+    }
+
+    /**
+     * Get the available RA name generation schemes for radio buttons
+     *
+     */ 
+    public List<SelectItem> getAvailableRaNameGenSchemes() {
+        List<SelectItem> selectItems = new ArrayList<>();
+        selectItems.add(new SelectItem(UsernameGeneratorParams.DN));
+        selectItems.add(new SelectItem(UsernameGeneratorParams.RANDOM));
+        selectItems.add(new SelectItem(UsernameGeneratorParams.FIXED));
+        selectItems.add(new SelectItem(UsernameGeneratorParams.USERNAME));
+        return selectItems;
+    }
+
+    /**
+     * Set the name generation parameters
+     * Semicolon delimited DN Fields
+     *
+     */ 
+    public void setRaNameGenParams(final String params) {
+    	String alias = estAliasGui.getName();
+    	EstConfiguration estConfiguration = getEjbcaWebBean().getEstConfigForEdit(alias);
+        estConfiguration.setRANameGenParams(getSelectedEstAlias(), params);
+    }
+
+    /**
+     * Get the name generation parameters
+     * Semicolon delimited DN Fields
+     *
+     */     
+    public String getRaNameGenParams() {
+    	String alias = estAliasGui.getName();
+    	EstConfiguration estConfiguration = getEjbcaWebBean().getEstConfigForEdit(alias);
+        return estConfiguration.getRANameGenParams(getSelectedEstAlias());
+    }
+
+    /**
+     * Get the selected name generation DN part for addition or removal
+     *
+     */ 
+    public String getSelectedRaNameSchemeDnPart() {
+        return selectedRaNameSchemeDnPart == null ? dnfields.get(0) : selectedRaNameSchemeDnPart;
+    }
+
+    /**
+     * Set the selected name generation DN part for addition or removal
+     *
+     */ 
+    public void setSelectedRaNameSchemeDnPart(final String selectedRaNameSchemeDnPart) {
+        this.selectedRaNameSchemeDnPart = selectedRaNameSchemeDnPart;
+    }
+
+    /**
+     * Get the DN field select items. Full list of available DN fields.
+     *
+     */ 
+    public List<SelectItem> getDnFieldSelectItems() {
+        final List<SelectItem> selectItems = new ArrayList<>();
+        for (String  dnField : dnfields) {
+            selectItems.add(new SelectItem(dnField));
+        }
+        return selectItems;
+    }
+
+    /**
+     * Set the RA name generation prefix
+     *
+     */ 
+    public void setRaNameGenPrefix(final String prefix) {
+        String alias = estAliasGui.getName();
+        EstConfiguration estConfiguration = getEjbcaWebBean().getEstConfigForEdit(alias);
+        estConfiguration.setRANameGenPrefix(getSelectedEstAlias(), prefix);
+    }
+
+    /**
+     * Get the RA name generation prefix
+     *
+     */     
+    public String getRaNameGenPrefix() {
+        String alias = estAliasGui.getName();
+        EstConfiguration estConfiguration = getEjbcaWebBean().getEstConfigForEdit(alias);
+        return estConfiguration.getRANameGenPrefix(getSelectedEstAlias());
+    }
+
+    /**
+     * Set the RA name generation postfix
+     *
+     */ 
+    public void setRaNameGenPostfix(final String prefix) {
+        String alias = estAliasGui.getName();
+        EstConfiguration estConfiguration = getEjbcaWebBean().getEstConfigForEdit(alias);
+        estConfiguration.setRANameGenPostfix(getSelectedEstAlias(), prefix);
+    }
+
+    /**
+     * Get the RA name generation postfix
+     *
+     */     
+    public String getRaNameGenPostfix() {
+        String alias = estAliasGui.getName();
+        EstConfiguration estConfiguration = getEjbcaWebBean().getEstConfigForEdit(alias);
+        return estConfiguration.getRANameGenPostfix(getSelectedEstAlias());
     }
 
     private void reset() {
