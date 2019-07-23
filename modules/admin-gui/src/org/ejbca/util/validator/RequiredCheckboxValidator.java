673d8bc71b17e0ad5fe5258fe8bec3d0f13f3575
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2019-07-23 08:17:30 +0000

    ECA-8360: Fix Generate buttons on Edit CA page
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@32821 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/admin-gui/src/org/ejbca/util/validator/RequiredCheckboxValidator.java
@@ -0,0 +1,38 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.util.validator;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+/**
+ * JSF validator that shows an error if a checkbox is not checked.
+ *
+ * @version $Id$
+ */
+@FacesValidator("org.ejbca.util.validator.RequiredCheckboxValidator")
+public class RequiredCheckboxValidator implements Validator {
+
+    @Override
+    public void validate(final FacesContext facesContext, final UIComponent uiComponent, final Object o) throws ValidatorException {
+        if (o == null || (boolean) o != true) {
+            final String message = (String) uiComponent.getAttributes().get("errorMessage");
+            throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR, message, null));
+        }
+    }
+
+}
