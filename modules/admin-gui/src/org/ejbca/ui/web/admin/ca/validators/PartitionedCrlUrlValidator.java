fb5569744a973619011fb95e3676362f70257cd5
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2019-07-23 08:17:30 +0000

    ECA-8360: Fix Generate buttons on Edit CA page
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@32821 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/ca/validators/PartitionedCrlUrlValidator.java
@@ -0,0 +1,42 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.ui.web.admin.ca.validators;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+import org.ejbca.ui.web.jsf.configuration.EjbcaJSFHelper;
+
+/**
+ * Validator used for validating CRL DP URLs with partition number placeholders (edit ca page).
+ *
+ * @version $Id$
+ */
+@FacesValidator("org.ejbca.ui.web.admin.ca.validators.PartitionedCrlUrlValidator")
+public class PartitionedCrlUrlValidator implements Validator {
+
+    @Override
+    public void validate(FacesContext facesContext, UIComponent component, Object o) throws ValidatorException {
+        final String crlDistributionPointUrl = (String) o;
+        if (crlDistributionPointUrl == null || crlDistributionPointUrl.indexOf('*') == -1) {
+            final String message = EjbcaJSFHelper.getBean().getEjbcaWebBean().getText("PARTITIONEDCRLS_WITHOUT_ASTERISK");
+            FacesMessage msg = new FacesMessage(message, null/*"Use CRL partition validation failed!"*/);
+            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+            throw new ValidatorException(msg);
+        }
+    }
+}
