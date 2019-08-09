cd0d0bf95b8c699cc78b2e93469921dce2aeb694
Bastian Fredriksson <bastian.fredriksson@primekey.com>
2019-08-09 14:03:20 +0000

    ECA-8039 Added archive cutoff settings to OcspKeyBindings, implemented the new OCSP extension + GUI changes.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@32983 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/cesecore-common/src/org/cesecore/certificates/ocsp/extension/OcspArchiveCutoffExtension.java
@@ -0,0 +1,111 @@
+/*************************************************************************
+ *                                                                       *
+ *  CESeCore: CE Security Core                                           *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
+package org.cesecore.certificates.ocsp.extension;
+
+import java.security.cert.X509Certificate;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+import org.bouncycastle.asn1.ASN1ObjectIdentifier;
+import org.bouncycastle.asn1.x509.Extension;
+import org.bouncycastle.cert.ocsp.CertificateStatus;
+import org.cesecore.keybind.InternalKeyBinding;
+
+/**
+ * Represents the OCSP Archive Cutoff extension described in RFC6960, section 4.4.4.
+ * 
+ * <blockquote>
+ *  An OCSP responder MAY choose to retain revocation information beyond
+ *  a certificate's expiration.  The date obtained by subtracting this
+ *  retention interval value from the producedAt time in a response is
+ *  defined as the certificate's "archive cutoff" date.
+ *  <p>
+ *  OCSP-enabled applications would use an OCSP archive cutoff date to
+ *  contribute to a proof that a digital signature was (or was not)
+ *  reliable on the date it was produced even if the certificate needed
+ *  to validate the signature has long since expired.
+ *  <p>
+ *  OCSP servers that provide support for such a historical reference
+ *  SHOULD include an archive cutoff date extension in responses.  If
+ *  included, this value SHALL be provided as an OCSP singleExtensions
+ *  extension identified by id-pkix-ocsp-archive-cutoff and of syntax
+ *  GeneralizedTime.
+ *  <p>
+ *    <code>id-pkix-ocsp-archive-cutoff OBJECT IDENTIFIER ::= {id-pkix-ocsp 6}</code>
+ *    <br/>
+ *    <code>ArchiveCutoff ::= GeneralizedTime</code>
+ *  <p>
+ *  To illustrate, if a server is operated with a 7-year retention
+ *  interval policy and status was produced at time t1, then the value
+ *  for ArchiveCutoff in the response would be (t1 - 7 years).
+ * </blockquote>
+ * 
+ * It is also used in ETSI EN 319 411-2 as follows:
+ * <blockquote>
+ *    CSS-6.3.10-08 [CONDITIONAL]: If OCSP is provided, the OCSP responder should use
+ *    the ArchiveCutOff extension as specified in IETF RFC 6960 [i.9], with the archiveCutOff 
+ *    date set to the CA's certificate "valid from" date.
+ * </blockquote>
+ * 
+ * This extension was enabled in ocsp.properties by setting the property <code>ocsp.expiredcert.retentionperiod</code>.
+ * As of EJBCA 7.3, it is instead enabled per OCSP key binding, and can be configured to use either a specified retention
+ * period or the <code>notBefore</code> date of the issuer.
+ * 
+ * @version $Id$
+ */
+public class OcspArchiveCutoffExtension implements OCSPExtension {
+    private final Logger log = Logger.getLogger(OcspArchiveCutoffExtension.class);
+    public static final String EXTENSION_OID = "1.3.6.1.5.5.7.48.6";
+    public static final String EXTENSION_NAME = "Archive Cutoff";
+
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public Map<ASN1ObjectIdentifier, Extension> process(final X509Certificate[] requestCertificates, final String remoteAddress, final String remoteHost,
+            final X509Certificate cert, final CertificateStatus status, final InternalKeyBinding internalKeyBinding) {
+        if (log.isDebugEnabled()) {
+            log.debug(remoteAddress + " sent an OCSP request containing an OCSP archive cutoff extension, asking for the status of the certificate"
+                    + " with serial number " + cert.getSerialNumber() + " issued by '" + cert.getIssuerDN()  + "'. The OCSP archive cutoff extension"
+                    + " should only be present in OCSP responses. This message is probably an indication of a misconfigured OCSP client.");
+        }
+        return new HashMap<ASN1ObjectIdentifier, Extension>();
+    }
+
+    @Override
+    public int getLastErrorCode() {
+        return 0;
+    }
+
+    @Override
+    public Set<OCSPExtensionType> getExtensionType() {
+        final HashSet<OCSPExtensionType> applicableExtensionTypes = new HashSet<>();
+        applicableExtensionTypes.add(OCSPExtensionType.RESPONSE);
+        return applicableExtensionTypes;
+    }
+
+    @Override
+    public String getOid() {
+        return EXTENSION_OID;
+    }
+
+    @Override
+    public String getName() {
+        return EXTENSION_NAME;
+    }
+}
