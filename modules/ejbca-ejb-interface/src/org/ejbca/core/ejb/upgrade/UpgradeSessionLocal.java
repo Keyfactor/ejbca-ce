08c58ab76a0abcf86d9b7d1a1eb6cf5913c1eccb
Bastian Fredriksson <bastian.fredriksson@primekey.com>
2019-08-10 08:49:18 +0000

    ECA-8039 Adding upgrade code for OCSP archive cutoff migration.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@32986 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb-interface/src/org/ejbca/core/ejb/upgrade/UpgradeSessionLocal.java
+++ modules/ejbca-ejb-interface/src/org/ejbca/core/ejb/upgrade/UpgradeSessionLocal.java
@@ -73,6 +73,8 @@ public interface UpgradeSessionLocal  extends UpgradeSession{
     void migrateDatabase6150() throws UpgradeFailedException;
     /** For internal user from UpgradeSessionBean only! */
     void upgradeCrlStoreAndCertStoreConfiguration720();
+    /** For internal user from UpgradeSessionBean only */
+    void migrateDatabase730();
 
     
     /** Persist the time when the post-upgrade starts or 0L when it is no longer running. */
