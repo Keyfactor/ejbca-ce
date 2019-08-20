9a385c1a37eec21815f45b11e46ba89f9102b439
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2019-08-20 07:11:05 +0000

    ECA-7278: Make lazy initialized variable volatile. Fixes thread safety warning
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@33075 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-common/src/org/cesecore/keys/token/AzureCryptoToken.java
+++ modules/cesecore-common/src/org/cesecore/keys/token/AzureCryptoToken.java
@@ -123,7 +123,7 @@ public class AzureCryptoToken extends BaseCryptoToken {
      */
     private KeyAliasesCache aliasCache = new KeyAliasesCache();
     
-    private static PublicKey ecPublicDummyKey = null;
+    private static volatile PublicKey ecPublicDummyKey = null;
     private static final byte[] ecPublicKeyBytes = Base64.decode(("MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEAnXBeTH4xcl2c8VBZqtfgCTa+5sc" + 
             "wV+deHQeaRJQuM5DBYfee9TQn+mvBfYPCTbKEnMGeoYq+BpLCBYgaqV6hw==").getBytes());
     /** @return a dummy key (hardcoded prime256v1) to add in initial stage of caching, because we can not put anything empty in the cache, because putting null means remove...
@@ -260,9 +260,9 @@ public class AzureCryptoToken extends BaseCryptoToken {
                 aliasCache.flush(); // make sure the crypto token is off-line
                 throw new CryptoTokenOfflineException(e);
             }
-            return new ArrayList<String>(aliasCache.getAllNames());
+            return new ArrayList<>(aliasCache.getAllNames());
         } else {
-            return new ArrayList<String>(aliasCache.getAllNames());
+            return new ArrayList<>(aliasCache.getAllNames());
         }
         
     }
