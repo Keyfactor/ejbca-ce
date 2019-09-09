fa2d9a93ca4e616200ecfc554f64a7893f675f70
Bastian Fredriksson <bastian.fredriksson@primekey.com>
2019-09-09 14:56:54 +0000

    ECA-8483 Converted KeyGenParams to an immutable object with a builder.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@33268 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-common/src/org/cesecore/keys/token/KeyGenParams.java
+++ modules/cesecore-common/src/org/cesecore/keys/token/KeyGenParams.java
@@ -16,64 +16,140 @@ package org.cesecore.keys.token;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.pkcs11.jacknji11.CKA;
+
 /**
- * Object used for passing key parameters and attributes for key generation.
+ * Immutable object used for passing key parameters and attributes for key generation.
+ * 
  * @version $Id$
- *
  */
 public class KeyGenParams {
-    
-    private String keySpecification;
-    private Map<Long, Object> publicAttributesMap = new HashMap<>();
-    private Map<Long, Object> privateAttributesMap = new HashMap<>();
-    
-    public KeyGenParams() {}
+    private final String keySpecification;
+    private final Map<Long, Object> publicAttributesMap;
+    private final Map<Long, Object> privateAttributesMap;
 
     /**
-     * Should be in the form "RSAnnnn", "DSAnnnn" or a known EC curve name.
-     * @param keySpecification
+     * Describes a set of PKCS #11 attribute templates.
      */
-    public KeyGenParams(final String keySpecification) {
-        this.keySpecification = keySpecification;
+    public enum KeyPairTemplate {
+        /**
+         * Template for a keypair only allowed to be used for signing and verifying.
+         */
+        SIGN,
+        /**
+         * Template for a keypair only allowed to be used for decrypting and encrypting.
+         */
+        ENCRYPT
+    }
+
+    public static class KeyGenParamsBuilder {
+        private String keySpecification;
+        private Map<Long, Object> publicAttributesMap;
+        private Map<Long, Object> privateAttributesMap;
+
+        protected KeyGenParamsBuilder(final String keySpecification) {
+            this.keySpecification = keySpecification;
+        }
+
+        protected KeyGenParamsBuilder(final KeyGenParams keyGenParams) {
+            this.keySpecification = keyGenParams.getKeySpecification();
+            this.publicAttributesMap = keyGenParams.getPublicAttributesMap();
+            this.privateAttributesMap = keyGenParams.getPrivateAttributesMap();
+        }
+
+        /**
+         * Specify the PKCS #11 attribute template to use.
+         * 
+         * @param keyPairTemplate the key pair template to use.
+         * @return the builder.
+         */
+        public KeyGenParamsBuilder withKeyPairTemplate(final KeyPairTemplate keyPairTemplate) {
+            if (keyPairTemplate == KeyPairTemplate.ENCRYPT) {
+                privateAttributesMap.put(CKA.DECRYPT, true);
+                privateAttributesMap.put(CKA.SIGN, false);
+                publicAttributesMap.put(CKA.ENCRYPT, true);
+                publicAttributesMap.put(CKA.VERIFY, false);
+            } else if (keyPairTemplate == KeyPairTemplate.SIGN) {
+                privateAttributesMap.put(CKA.DECRYPT, false);
+                privateAttributesMap.put(CKA.SIGN, true);
+                publicAttributesMap.put(CKA.ENCRYPT, false);
+                publicAttributesMap.put(CKA.VERIFY, true);
+            }
+            return this;
+        }
+
+        /**
+         * Set the type of key to use, e.g. 'RSA2048' or 'secp256r1'.
+         * 
+         * @param keySpecification the type of key to use.
+         * @return the builder.
+         */
+        public KeyGenParamsBuilder setKeySpecification(final String keySpecification) {
+            this.keySpecification = keySpecification;
+            return this;
+        }
+
+        /**
+         * Build an instance of the {@link KeyGenParams} class.
+         * 
+         * @return an instance of the {@link KeyGenParams} class.
+         */
+        public KeyGenParams build() {
+            return new KeyGenParams(this);
+        }
     }
     
     /**
-     * @return algorithm and key spec. E.g. 'RSA2048'.
+     * Get a builder for constructing {@link KeyGenParams} instances.
+     * 
+     * @param keySpecification the type of key to use, e.g. 'RSA2048' or 'secp256r1'.
+     * @return a builder for constructing {@link KeyGenParams} instances.
+     */
+    public static KeyGenParamsBuilder builder(final String keySpecification) {
+        return new KeyGenParamsBuilder(keySpecification);
+    }
+
+    /**
+     * Get a builder for constructing {@link KeyGenParams} instances, based on an existing
+     * instance of {@link KeyGenParams}.
+     * 
+     * @param keyGenParams an existing instance of {@link KeyGenParams}.
+     * @return a builder for constructing {@link KeyGenParams} instances.
+     */
+    public static KeyGenParamsBuilder builder(final KeyGenParams keyGenParams) {
+        return new KeyGenParamsBuilder(keyGenParams);
+    }
+
+    private KeyGenParams(final KeyGenParamsBuilder builder) {
+        this.keySpecification = builder.keySpecification;
+        this.publicAttributesMap = builder.publicAttributesMap;
+        this.privateAttributesMap = builder.privateAttributesMap;
+    }
+    
+    /**
+     * Get the type of key, e.g. 'RSA2048' or 'secp256r1'.
+     * 
+     * @return the key specification as a string.
      */
     public String getKeySpecification() {
         return keySpecification;
     }
 
     /**
-     * @param keySpecification algorithm and key spec. E.g. 'RSA2048'.
+     * Get a map with PKCS #11 attributes for the public key.
+     * 
+     * @return a map with PKCS #11 attributes.
      */
-    public void setKeySpecification(String keySpecification) {
-        this.keySpecification = keySpecification;
-    }
-
     public Map<Long, Object> getPublicAttributesMap() {
-        return publicAttributesMap;
+        return new HashMap<Long, Object>(publicAttributesMap);
     }
 
+    /**
+     * Get a map with PKCS #11 attributes for the private key.
+     * 
+     * @return a map with PKCS #11 attributes.
+     */
     public Map<Long, Object> getPrivateAttributesMap() {
-        return privateAttributesMap;
-    }
-    
-    /**
-     * Override default PKCS#11 public key attributes used for key generation.
-     * @param key attribute key. E.g. CKA.ENCRYPT
-     * @param value attribute value. E.g. true / false
-     */
-    public void addPublicKeyAttribute(Long key, Object value) {
-        publicAttributesMap.put(key, value);
-    }
-    
-    /**
-     * Override default PKCS#11 private key attributes used for key generation.
-     * @param key attribute key. E.g. CKA.DECRYPT
-     * @param value attribute value. E.g. true / false
-     */
-    public void addPrivateKeyAttribute(Long key, Object value) {
-        privateAttributesMap.put(key, value);
+        return new HashMap<Long, Object>(privateAttributesMap);
     }
 }
