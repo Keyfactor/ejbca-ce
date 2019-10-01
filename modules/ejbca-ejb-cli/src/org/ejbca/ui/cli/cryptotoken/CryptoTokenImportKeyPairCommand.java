ac69efbc8fb87a5563c3f851ddd47994ef4c89d7
Amin Khorsandi <amin.khorsandi@primekey.com>
2019-10-01 09:54:36 +0000

    ECA-8581: Some more code.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@33461 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb-cli/src/org/ejbca/ui/cli/cryptotoken/CryptoTokenImportKeyPairCommand.java
+++ modules/ejbca-ejb-cli/src/org/ejbca/ui/cli/cryptotoken/CryptoTokenImportKeyPairCommand.java
@@ -2,7 +2,11 @@ package org.ejbca.ui.cli.cryptotoken;
 
 import org.apache.log4j.Logger;
 import org.cesecore.authorization.AuthorizationDeniedException;
+import org.cesecore.keys.token.CryptoTokenManagementSessionRemote;
 import org.cesecore.keys.token.CryptoTokenOfflineException;
+import org.cesecore.keys.token.KeyGenParams;
+import org.cesecore.keys.token.KeyGenParams.KeyGenParamsBuilder;
+import org.cesecore.util.EjbRemoteHelper;
 import org.ejbca.ui.cli.infrastructure.command.CommandResult;
 import org.ejbca.ui.cli.infrastructure.parameter.Parameter;
 import org.ejbca.ui.cli.infrastructure.parameter.ParameterContainer;
@@ -15,10 +19,19 @@ public class CryptoTokenImportKeyPairCommand extends BaseCryptoTokenCommand {
     private static final Logger log = Logger.getLogger(CryptoTokenImportKeyPairCommand.class);
 
     private static final String KEYFILEPATH = "--key-file-path";
+    private static final String KEYALGORITHM = "--key-algorithm";
+    private static final String ALIAS = "--alias";
+    private static final String KEYSPEC = "--key-spec";
 
     {
         registerParameter(
                 new Parameter(KEYFILEPATH, "Key file path", MandatoryMode.MANDATORY, StandaloneMode.ALLOW, ParameterMode.ARGUMENT, "Path to the directory containing key files."));
+        registerParameter(
+                new Parameter(ALIAS, "Alias", MandatoryMode.MANDATORY, StandaloneMode.ALLOW, ParameterMode.ARGUMENT, "Alias for the key pair which will be created."));
+        registerParameter(
+                new Parameter(KEYALGORITHM, "Key algorithm", MandatoryMode.OPTIONAL, StandaloneMode.ALLOW, ParameterMode.ARGUMENT, "Algorithm the key is generated with, if not provided RSA will be assumed."));
+        registerParameter(
+                new Parameter(KEYSPEC, "Key spec", MandatoryMode.OPTIONAL, StandaloneMode.ALLOW, ParameterMode.ARGUMENT, "Key specification for the key pair, if not provided RSA2048 will be assumed."));
     }
     
     @Override
@@ -34,7 +47,23 @@ public class CryptoTokenImportKeyPairCommand extends BaseCryptoTokenCommand {
     @Override
     public CommandResult executeCommand(Integer cryptoTokenId, ParameterContainer parameters)
             throws AuthorizationDeniedException, CryptoTokenOfflineException {
-        return null;
+        final String alias = parameters.get(ALIAS);
+        String keySpec = parameters.get(KEYSPEC);
+        if (keySpec == null) {
+            keySpec = "RSA2048";
+        }
+        final KeyGenParamsBuilder keyGenParamsBuilder = KeyGenParams.builder(keySpec);
+        
+        try {
+            final CryptoTokenManagementSessionRemote cryptoTokenManagementSession = EjbRemoteHelper.INSTANCE
+                    .getRemoteSession(CryptoTokenManagementSessionRemote.class);
+            cryptoTokenManagementSession.createKeyPair(getAdmin(), cryptoTokenId, alias, keyGenParamsBuilder.build());
+            return CommandResult.SUCCESS;
+        } catch (Exception e) {
+            e.printStackTrace();
+            getLogger().error("Changing authentication data for the key  "  + alias + " failed : " + e);
+            return CommandResult.FUNCTIONAL_FAILURE;
+        }
     }
 
     @Override
