b1d86727ef10f974e70ae488c1b9f32fe4b907a3
Amin Khorsandi <amin.khorsandi@primekey.com>
2019-10-07 14:17:15 +0000

    ECA-8396: Add header and svn Id.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@33525 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-common/src-test/org/cesecore/keys/token/p11ng/Pkcs11NgCryptoTokenTest.java
+++ modules/cesecore-common/src-test/org/cesecore/keys/token/p11ng/Pkcs11NgCryptoTokenTest.java
@@ -1,8 +1,23 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA - Proprietary Modules: Enterprise Certificate Authority        *
+ *                                                                       *
+ *  Copyright (c), PrimeKey Solutions AB. All rights reserved.           *
+ *  The use of the Proprietary Modules are subject to specific           * 
+ *  commercial license terms.                                            *
+ *                                                                       *
+ *************************************************************************/
 package org.cesecore.keys.token.p11ng;
 
 import org.cesecore.keys.token.CryptoTokenTestBase;
 import org.cesecore.keys.token.PKCS11TestUtils;
 
+/**
+ * Test class for Pkcs11Ng functions.
+ * 
+ * @version $Id$
+ *
+ */
 public class Pkcs11NgCryptoTokenTest extends CryptoTokenTestBase {
 
     @Override
