75d2bc1ef2a6d31fae9ffd8e783454c792a11987
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2019-11-14 08:47:33 +0000

    ECA-8634: Use correct directory for indexes in DB2 jenkins scripts
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@33841 37aeb1de-b14b-0410-9a0e-82b28170d228

--- jenkins-files/EE_COS7_OpenJDK8_WF10_NOHSM_DB2/db2/db2_init_ejbca.sh
+++ jenkins-files/EE_COS7_OpenJDK8_WF10_NOHSM_DB2/db2/db2_init_ejbca.sh
@@ -21,8 +21,8 @@ db2 "CREATE REGULAR TABLESPACE EJBCADB_DATA_01 PAGESIZE 16K EXTENTSIZE 32 PREFET
 # To output db2 pools, uncomment next line
 # db2 "select TBSP_NAME,TBSP_PAGE_SIZE,TBSP_STATE from TABLE(MON_GET_TABLESPACE('',-2))"
 # Add EJBCA indexes
-db2 -f /app/create-tables-ejbca-db2.sql
-db2 -f /app/create-index-ejbca.sql
+db2 -f /var/custom/create-tables-ejbca-db2.sql
+db2 -f /var/custom/create-index-ejbca.sql
 db2 'CREATE INDEX userdata_idx_test1 ON UserData (cAId)'
 # Close connection
 db2 connect reset
