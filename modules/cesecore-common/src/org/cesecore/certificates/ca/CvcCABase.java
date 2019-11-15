6b1476af529973115db65a4e19841370279625ca
Jekaterina Bunina <jekaterina.Bunina.ext@primekey.com>
2019-11-15 08:20:59 +0000

    ECA-6790 Add "Enforce Key Renewal" Option to CA manage page
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@33858 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-common/src/org/cesecore/certificates/ca/CvcCABase.java
+++ modules/cesecore-common/src/org/cesecore/certificates/ca/CvcCABase.java
@@ -115,7 +115,7 @@ public abstract class CvcCABase extends CABase implements Serializable, CvcCA {
 				getCAToken(), getDescription(), getRevocationReason(), getRevocationDate(), getCRLPeriod(), getCRLIssueInterval(), getCRLOverlapTime(), getDeltaCRLPeriod(), 
 				getCRLPublishers(), getValidators(), getFinishUser(), externalcaserviceinfos, 
 				getApprovals(),
-				getIncludeInHealthCheck(), isDoEnforceUniquePublicKeys(), isDoEnforceUniqueDistinguishedName(), isDoEnforceUniqueSubjectDNSerialnumber(),
+				getIncludeInHealthCheck(), isDoEnforceUniquePublicKeys(),isDoEnforceKeyRenewal(), isDoEnforceUniqueDistinguishedName(), isDoEnforceUniqueSubjectDNSerialnumber(),
 				isUseCertReqHistory(), isUseUserStorage(), isUseCertificateStorage(), isAcceptRevocationNonExistingEntry());
         //These to settings were deprecated in 6.8.0, but are still set for upgrade reasons
         info.setApprovalProfile(getApprovalProfile());
