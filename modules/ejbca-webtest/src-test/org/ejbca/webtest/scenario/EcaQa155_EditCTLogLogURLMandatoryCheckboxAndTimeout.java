fe0355c27676fc0418cb82d5236d2cba303f0078
Amin Khorsandi <amin.khorsandi@primekey.com>
2019-11-18 14:32:53 +0000

    ECA-8694: Add some tests.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@33870 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-webtest/src-test/org/ejbca/webtest/scenario/EcaQa155_EditCTLogLogURLMandatoryCheckboxAndTimeout.java
+++ modules/ejbca-webtest/src-test/org/ejbca/webtest/scenario/EcaQa155_EditCTLogLogURLMandatoryCheckboxAndTimeout.java
@@ -12,13 +12,20 @@
  *************************************************************************/
 package org.ejbca.webtest.scenario;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.cesecore.authorization.AuthorizationDeniedException;
 import org.ejbca.webtest.WebTestBase;
+import org.ejbca.webtest.helper.CTLogHelper;
 import org.ejbca.webtest.helper.SystemConfigurationHelper;
-import org.ejbca.webtest.helper.SystemConfigurationHelper.SysConfigTabs;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.openqa.selenium.WebDriver;
 
 /**
@@ -28,8 +35,18 @@ import org.openqa.selenium.WebDriver;
  */
 public class EcaQa155_EditCTLogLogURLMandatoryCheckboxAndTimeout extends WebTestBase {
     
+    private static final String TEST_LABEL = "Test";
     private static SystemConfigurationHelper systemConfigurationHelper;
-    private static String LOG_URL = "https://localhost:8443/ejbca/adminweb/";
+    private static CTLogHelper ctLogHelper;
+    
+    private static final String PUBLIC_KEY = "-----BEGIN PUBLIC KEY-----\n" +
+            "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEFJY5TplekPjaNgCckezeyhkccA8O\n" +
+            "63Sj84rZ1RCRoJ7vHa8FF2IIbF/S1iEb/gbkmqNJ4K3m+oNzcr76yoH3Dg==\n" +
+            "-----END PUBLIC KEY-----";
+    
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
     
     @BeforeClass
     public static void init() {
@@ -39,6 +56,7 @@ public class EcaQa155_EditCTLogLogURLMandatoryCheckboxAndTimeout extends WebTest
 
         // Init helpers
         systemConfigurationHelper = new SystemConfigurationHelper(webDriver);
+        ctLogHelper = new CTLogHelper(webDriver);
     }
 
     @AfterClass
@@ -49,13 +67,18 @@ public class EcaQa155_EditCTLogLogURLMandatoryCheckboxAndTimeout extends WebTest
     
     @Test
     public void stepOne_CtLogPageOpen(){
-        systemConfigurationHelper.openPage(getAdminWebUrl());
-        systemConfigurationHelper.openTab(SysConfigTabs.CTLOGS);
+        goToSystemConfigurationPage();
     }
     
     @Test
-    public void stepTwo_CtLogAddFirstLog() {
-        
+    public void stepTwo_CtLogAddFirstLog() throws IOException {
+        final String logUrl = "https://localhost:8443/ejbca/adminweb/";
+        ctLogHelper.fillLogUrlField(logUrl);
+        ctLogHelper.fillPublicKeyField(createPublicKeyFile());
+        ctLogHelper.fillTimeoutField(60000);
+        ctLogHelper.fillLabelField(TEST_LABEL);
+        ctLogHelper.addCertificateTransparencyLog();
+        ctLogHelper.assertIsTableAndRowExists(TEST_LABEL, logUrl);
     }
     
     @Test
@@ -68,5 +91,17 @@ public class EcaQa155_EditCTLogLogURLMandatoryCheckboxAndTimeout extends WebTest
         
     }
     
+    private void goToSystemConfigurationPage(){
+        systemConfigurationHelper.openPage(getAdminWebUrl());
+        systemConfigurationHelper.openTab(SystemConfigurationHelper.SysConfigTabs.CTLOGS);
+    }
     
+    private File createPublicKeyFile() throws IOException {
+        File publicKeyFile = folder.newFile("test_pub.pem");
+        FileWriter fileWriter = new FileWriter(publicKeyFile);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        bufferedWriter.write(PUBLIC_KEY);
+        bufferedWriter.close();
+        return publicKeyFile;
+    }
 }
