f7710f39ab6ba3223ee1d1c837f5097bc214fe32
Amin Khorsandi <amin.khorsandi@primekey.com>
2019-11-27 08:12:31 +0000

    ECA-8694: Update helper and test class.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@33933 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-webtest/src-test/org/ejbca/webtest/helper/CTLogHelper.java
+++ modules/ejbca-webtest/src-test/org/ejbca/webtest/helper/CTLogHelper.java
@@ -1,3 +1,15 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.ejbca.webtest.helper;
 
 import org.openqa.selenium.By;
@@ -10,6 +22,11 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
+/**
+ * 
+ * @version $Id$
+ *
+ */
 public class CTLogHelper extends BaseHelper {
     public CTLogHelper(final WebDriver webDriver) {
         super(webDriver);
@@ -22,6 +39,10 @@ public class CTLogHelper extends BaseHelper {
         static final By LABEL_INPUT_FIELD = By.id("ctlogsform:ctLogLabelInput");
         static final By BUTTON_ADD = By.id("ctlogsform:addCtLog");
         static final By TABLE_ROW_DATA = By.xpath("//table[@class='grid ctlogTable']/tbody/tr/td[1]/span");
+        static final By EDIT_LOG_URL_INPUT_FIELD = By.id("editCtLogForm:logUrl");
+        static final By EDIT_TIMEOUT_INPUT_FIELD = By.id("editCtLogForm:editedCTLogTimeout");
+        static final By EDIT_LABEL_INPUT_FIELD = By.id("editCtLogForm:editedCtLogLabel");
+        static final By BUTTON_EDIT_CTLOG = By.id("editCtLogForm:saveCtLogEdit");
 
         static By getLabelTextFromTable(final String text) {
             return By.xpath("//tr/td/h3[contains(text(), '" + text + "')]");
@@ -44,6 +65,13 @@ public class CTLogHelper extends BaseHelper {
                     "tbody/tr/td[span[contains(text(), '" + text + "')]]/" +
                     "following-sibling::td/input[contains(@id, 'moveUpCtLogButton')]");
         }
+        
+        static By getEditCtLogButton(final String label, final String text) {
+            return By.xpath("//table/tbody/tr/td/h3[contains(text(), '" + label + "')]" +
+                    "/following-sibling::table[@class='grid ctlogTable']/" +
+                    "tbody/tr/td[span[contains(text(), '" + text + "')]]/" +
+                    "following-sibling::td/input[contains(@id, 'editCtLogButton')]");
+        }
     }
 
     public void fillLogUrlField(final String inputText) {
@@ -60,6 +88,18 @@ public class CTLogHelper extends BaseHelper {
 
     public void fillLabelField(final String inputText) {
         fillInput(Page.LABEL_INPUT_FIELD, inputText);
+    }
+    
+    public void fillEditLogUrlField(final String inputText) {
+        fillInput(Page.EDIT_LOG_URL_INPUT_FIELD, inputText);
+    }
+    
+    public void fillEditTimeoutField(final String inputNumber) {
+        fillInput(Page.EDIT_TIMEOUT_INPUT_FIELD, inputNumber);
+    }
+    
+    public void fillEditLabelField(final String inputText) {
+        fillInput(Page.EDIT_LABEL_INPUT_FIELD, inputText);
     }
 
     public void addCertificateTransparencyLog() {
@@ -83,7 +123,15 @@ public class CTLogHelper extends BaseHelper {
     public void pressArrowDownButton(final String label, final String text) {
         clickLink(Page.getNavigateDownButton(label, text));
     }
-
+    
+    public void pressEditCtLogButton(final String label, final String text) {
+        clickLink(Page.getEditCtLogButton(label, text));
+    }
+    
+    public void pressSaveEditCtLogButton() {
+        clickLink(Page.BUTTON_EDIT_CTLOG);
+    }
+    
     public void isArrowUpButtonDisabled(final String label, final String text) {
         assertFalse(findElement(Page.getNavigateUpButton(label, text)).isEnabled());
     }
