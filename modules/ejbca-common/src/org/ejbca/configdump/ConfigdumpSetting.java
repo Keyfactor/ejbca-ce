4d2b868685feae686143baf25b8d20bd3b66cb2f
Amin Khorsandi <amin.khorsandi@primekey.com>
2019-12-03 14:54:35 +0000

    ECA-8555: Add crud class and update files.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@33997 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-common/src/org/ejbca/configdump/ConfigdumpSetting.java
+++ modules/ejbca-common/src/org/ejbca/configdump/ConfigdumpSetting.java
@@ -18,12 +18,10 @@ import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 
 import org.apache.commons.lang.StringUtils;
 
