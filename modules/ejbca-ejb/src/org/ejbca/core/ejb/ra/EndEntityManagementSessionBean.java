182f57ba9255760f44f0044dfe630673a73d4207
Lauri Kongas <Lauri.Kongas.ext@primekey.com>
2019-12-27 12:57:58 +0000

    ECA-8146 OCSP signer renewal via peers not working for throw-away CA
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@34157 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb/src/org/ejbca/core/ejb/ra/EndEntityManagementSessionBean.java
+++ modules/ejbca-ejb/src/org/ejbca/core/ejb/ra/EndEntityManagementSessionBean.java
@@ -47,6 +47,7 @@ import org.apache.log4j.Logger;
 import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.asn1.x500.X500NameStyle;
 import org.bouncycastle.asn1.x509.GeneralNames;
+import org.bouncycastle.asn1.x509.KeyPurposeId;
 import org.bouncycastle.util.encoders.Hex;
 import org.cesecore.ErrorCode;
 import org.cesecore.audit.enums.EventStatus;
@@ -442,9 +443,10 @@ public class EndEntityManagementSessionBean implements EndEntityManagementSessio
                 throw e;
             }
         }
+        
+        final CertificateProfile certProfile = certificateProfileSession.getCertificateProfile(endEntity.getCertificateProfileId());
         // Check if approvals is required. (Only do this if store users, otherwise this approval is disabled.)
         if (caInfo.isUseUserStorage()) {
-            final CertificateProfile certProfile = certificateProfileSession.getCertificateProfile(endEntity.getCertificateProfileId());
             final ApprovalProfile approvalProfile = approvalProfileSession.getApprovalProfileForAction(ApprovalRequestType.ADDEDITENDENTITY, caInfo,
                     certProfile);
             if (approvalProfile != null) {
@@ -472,11 +474,10 @@ public class EndEntityManagementSessionBean implements EndEntityManagementSessio
             }
         }
         
-        String certProfileName = certificateProfileSession.getCertificateProfileName(endEntity.getCertificateProfileId());
-        
         // Store a new UserData in the database, if this CA is configured to do so.
         // Store it in case of the OCSPSIGNER certificate profile anyway, since otherwise OCSP signer renewal via peers won't work for throwaway CAs
-        if (caInfo.isUseUserStorage() || CertificateProfile.OCSPSIGNERPROFILENAME.equals(certProfileName)) {
+        if (caInfo.isUseUserStorage() || (certProfile.getUseExtendedKeyUsage() && !CollectionUtils.isEmpty(certProfile.getExtendedKeyUsageOids())
+                        && certProfile.getExtendedKeyUsageOids().contains(KeyPurposeId.id_kp_OCSPSigning.getId()))) {
             try {
                 final ExtendedInformation extendedInformation = endEntity.getExtendedInformation();
                 ensureOldClusterNodeCompatibility(extendedInformation);
