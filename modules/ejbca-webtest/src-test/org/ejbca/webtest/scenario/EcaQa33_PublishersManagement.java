77111317a5b56b4f770de9310f340814a9f2ad1e
Amin Khorsandi <amin.khorsandi@primekey.com>
2020-01-29 08:00:45 +0000

    ECA-8783: Add more tests to publisher test suite (Selenium).
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@34398 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-webtest/src-test/org/ejbca/webtest/scenario/EcaQa33_PublishersManagement.java
+++ modules/ejbca-webtest/src-test/org/ejbca/webtest/scenario/EcaQa33_PublishersManagement.java
@@ -17,7 +17,6 @@ import java.util.Map;
 
 import org.cesecore.authorization.AuthorizationDeniedException;
 import org.cesecore.common.exception.ReferencesToItemExistException;
-import org.ejbca.core.model.ca.publisher.PublisherConst;
 import org.ejbca.webtest.WebTestBase;
 import org.ejbca.webtest.helper.PublisherHelper;
 import org.junit.AfterClass;
@@ -50,9 +49,11 @@ public class EcaQa33_PublishersManagement extends WebTestBase {
             PUBLISHERS.put("RENAME_PUBLISHER_NAME", "NewPublisher");
         }
         static final String PUBLISHER_DELETE_MESSAGE = "Are you sure you want to delete this?";
-        static final String BAD_SERVER = "ldap://0.0.0.0:4001";
-        
         static final String BAD_DATA_SOURCE_ERROR = "Invalid data source!";
+        static final String BAD_SERVER = "ldap://0.0.0.0:4001";
+        static final String GOOD_SERVER = "java://0.0.0.0:4001";
+        static final String MALLFORMED_SERVER = "/java://0.0.0.0:8080";
+        static final String ENTERPRISE_VA_TYPE = "1-org.ejbca.va.publisher.EnterpriseValidationAuthorityPublisher";
     }
     
     @BeforeClass
@@ -113,15 +114,36 @@ public class EcaQa33_PublishersManagement extends WebTestBase {
     }
     
     @Test
-    public void stepF_validateVaPublisherDataSource() {
+    public void stepF_invalidVaPublisherDataSource() {
         publisherHelper.openPage(getAdminWebUrl());
         publisherHelper.selectPublisherFromList(TestData.PUBLISHERS.get("PUBLISHER_NAME"));
         publisherHelper.editPublisher();
-        publisherHelper.setPublisherType("1-org.ejbca.va.publisher.EnterpriseValidationAuthorityPublisher");
+        publisherHelper.setPublisherType(TestData.ENTERPRISE_VA_TYPE);
+        publisherHelper.setDataSource(TestData.MALLFORMED_SERVER);
+        publisherHelper.save();
+        publisherHelper.assertHasErrorMessage(TestData.BAD_DATA_SOURCE_ERROR);
+    }
+    
+    @Test
+    public void stepF_mallformedVaPublisherDataSource() {
+        publisherHelper.openPage(getAdminWebUrl());
+        publisherHelper.selectPublisherFromList(TestData.PUBLISHERS.get("PUBLISHER_NAME"));
+        publisherHelper.editPublisher();
+        publisherHelper.setPublisherType(TestData.ENTERPRISE_VA_TYPE);
         publisherHelper.setDataSource(TestData.BAD_SERVER);
         publisherHelper.save();
         publisherHelper.assertHasErrorMessage(TestData.BAD_DATA_SOURCE_ERROR);
-
+    }
+    
+    @Test
+    public void stepF_validVaPublisherDataSource() {
+        publisherHelper.openPage(getAdminWebUrl());
+        publisherHelper.selectPublisherFromList(TestData.PUBLISHERS.get("PUBLISHER_NAME"));
+        publisherHelper.editPublisher();
+        publisherHelper.setPublisherType(TestData.ENTERPRISE_VA_TYPE);
+        publisherHelper.setDataSource(TestData.GOOD_SERVER);
+        publisherHelper.save();
+        publisherHelper.assertBackToListPublisherPage();
     }
     
 }
