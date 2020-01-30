fe5a033835ddc984ae3abdc5024e743750b03e7e
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2020-01-30 10:44:05 +0000

    ECA-8819: Fix failing deserialization on RA connected to 6.15.x (or older) CA
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@34413 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ejbca-common/src/org/ejbca/core/model/ca/caadmin/extendedcaservices/HardTokenEncryptCAServiceInfo.java
@@ -0,0 +1,45 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.core.model.ca.caadmin.extendedcaservices;
+
+import java.io.Serializable;
+
+import org.cesecore.certificates.ca.extendedservices.ExtendedCAServiceInfo;
+
+/**
+ * Deprecated class. Kept for backwards compatibility with 6.15 or older RA peer systems, which send this class in serialized data.
+ * @version $Id$
+ * @deprecated Since 7.0 (was removed in 7.0 until 7.4.0)
+ */
+@Deprecated
+public class HardTokenEncryptCAServiceInfo extends ExtendedCAServiceInfo implements Serializable {
+
+    private static final long serialVersionUID = -6186500870565287684L;
+
+    /** Dummy constructor. Not used. Deserialization uses the default constructor */
+    public HardTokenEncryptCAServiceInfo(int status) {
+        super(status);
+        throw new UnsupportedOperationException("Internal error. A part of EJBCA attempted to use legacy hard token functionality");
+    }
+
+    @Override
+    public String getImplClass() {
+        throw new UnsupportedOperationException("Internal error. A part of EJBCA attempted to use legacy hard token functionality");
+    }
+
+    @Override
+    public int getType() {
+        throw new UnsupportedOperationException("Internal error. A part of EJBCA attempted to use legacy hard token functionality");
+    }
+
+}
