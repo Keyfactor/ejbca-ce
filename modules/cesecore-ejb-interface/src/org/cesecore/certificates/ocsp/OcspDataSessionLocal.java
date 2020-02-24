a14e5b018e2fa7c56f1f2e00bbb895f709e6f84d
Amin Khorsandi <amin.khorsandi@primekey.com>
2020-02-24 14:23:23 +0000

    ECA-8878: Session bean for ocsp data.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@34518 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/cesecore-ejb-interface/src/org/cesecore/certificates/ocsp/OcspDataSessionLocal.java
@@ -0,0 +1,33 @@
+/*************************************************************************
+ *                                                                       *
+ *  CESeCore: CE Security Core                                           *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.cesecore.certificates.ocsp;
+
+import javax.ejb.Local;
+
+import org.cesecore.oscp.OcspResponseData;
+import org.cesecore.oscp.ResponsePK;
+
+/**
+ * Local interface for OcspDataSession.
+ * 
+ * @version $Id$
+ *
+ */
+@Local
+public interface OcspDataSessionLocal extends OcspDataSession {
+    
+    void storeOcspData(final OcspResponseData ocspResponseData);
+    
+    byte[] fetchOcspData(final ResponsePK key);
+
+}
