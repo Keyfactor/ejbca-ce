c2dc33085d53e63a6517e37f76798790ee0aa780
Amin Khorsandi <amin.khorsandi@primekey.com>
2020-02-24 14:23:23 +0000

    ECA-8878: Session bean for ocsp data.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@34518 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/cesecore-ejb/src/org/cesecore/certificates/ocsp/OcspDataSessionBean.java
@@ -0,0 +1,57 @@
+/*************************************************************************
+ *                                                                       *
+ *  CESeCore: CE Security Core                                           *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.cesecore.certificates.ocsp;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.apache.log4j.Logger;
+import org.cesecore.config.CesecoreConfiguration;
+import org.cesecore.jndi.JndiConstants;
+import org.cesecore.oscp.OcspResponseData;
+import org.cesecore.oscp.ResponsePK;
+
+/**
+ * 
+ * @version $Id$
+ *
+ */
+@Stateless(mappedName = JndiConstants.APP_JNDI_PREFIX + "OcspDataSessionRemote")
+@TransactionAttribute(TransactionAttributeType.SUPPORTS)
+public class OcspDataSessionBean implements OcspDataSessionLocal, OcspDataSessionRemote {
+
+    private static final Logger log = Logger.getLogger(OcspDataSessionBean.class);
+
+    @PersistenceContext(unitName = CesecoreConfiguration.PERSISTENCE_UNIT)
+    private EntityManager entityManager;
+
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    @Override
+    public void storeOcspData(final OcspResponseData responseData) {
+        log.trace(">persistOcspData");
+        this.entityManager.persist(responseData);
+        log.trace("<persistOcspData");
+    }
+
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    @Override
+    public byte[] fetchOcspData(final ResponsePK key) {
+        log.trace(">fetchOcspData");
+        final OcspResponseData ocspResponseData = this.entityManager.find(OcspResponseData.class, key);
+        log.trace("<fetchOcspData");
+        return ocspResponseData != null ? ocspResponseData.getOcspResponse() : null;
+    }
+}
