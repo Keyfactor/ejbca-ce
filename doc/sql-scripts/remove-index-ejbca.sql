e1d7e4524b1d70112b3d83b9562e9f90679aa28e
Amin Khorsandi <amin.khorsandi@primekey.com>
2020-03-11 07:40:19 +0000

    ECA-8895: Fix index for OcspResponseData table
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@34597 37aeb1de-b14b-0410-9a0e-82b28170d228

--- doc/sql-scripts/remove-index-ejbca.sql
+++ doc/sql-scripts/remove-index-ejbca.sql
@@ -45,4 +45,3 @@ DROP INDEX acmeauthorizationdata_idx2 ON AcmeAuthorizationData;
 DROP INDEX acmechallengedata_idx1 ON AcmeChallengeData;
 
 DROP INDEX ocspresponsedata_idx1 ON OcspResponseData;
-DROP INDEX ocspresponsedata_idx2 ON OcspResponseData;
