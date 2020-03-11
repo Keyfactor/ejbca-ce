4d80525c8e862ab6f64dd215448cf0ac1ff64192
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2020-03-11 16:25:47 +0000

    ECA-8915: add the invalid alias name to the error so the user knows
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@34609 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-common/src/org/cesecore/keys/token/AzureCryptoToken.java
+++ modules/cesecore-common/src/org/cesecore/keys/token/AzureCryptoToken.java
@@ -172,7 +172,7 @@ public class AzureCryptoToken extends BaseCryptoToken {
      */
     private static void checkAliasName(final String alias) throws IllegalArgumentException {
         if (!aliasPattern.matcher(alias).matches()) {
-            throw new IllegalArgumentException("Key Vault only supports numbers, letters and hyphen in alias names");
+            throw new IllegalArgumentException("Key Vault only supports numbers, letters and hyphen in alias names. Invalid name: " + alias);
         }
     }
     
