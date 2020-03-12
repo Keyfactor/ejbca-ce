8b1bd8f070af17abc310641b939890ff7ab8712c
Amin Khorsandi <amin.khorsandi@primekey.com>
2020-03-12 13:06:55 +0000

    ECA-8924: Automate CA creation validity check test.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@34612 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ejbca-webtest/src-test/org/ejbca/webtest/scenario/EcaQa116_CACreationValidityCheck.java
@@ -0,0 +1,91 @@
+package org.ejbca.webtest.scenario;
+
+import org.apache.commons.lang.StringUtils;
+import org.ejbca.webtest.WebTestBase;
+import org.ejbca.webtest.helper.CaHelper;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * 
+ * 
+ * @version $Id$
+ *
+ */
+public class EcaQa116_CACreationValidityCheck extends WebTestBase {
+    
+    // Helpers
+    private static CaHelper caHelper;
+    
+    // Test Data
+    private static class TestData {
+        static final String CA_NAME = "ECAQA-116-TestCA";
+        static final String CA_VALIDITY_EMPTY = StringUtils.EMPTY;
+        static final String CA_VALIDITY_BAD_FORMAT = "blabla";
+        static final String INVALID_SAN_ERROR = "Error: Invalid validity or certificate end time: Illegal characters.";
+        static final String EMPTY_SAN_ERROR = "Ca Validity: Validation Error: Value is required.";
+        static final String GOOD_VALIDITY_SHORT_FORMAT = "2y";
+        static final String GOOD_VALIDITY_LONG_FORMAT = "2024-10-01";
+    }
+    
+    @BeforeClass
+    public static void init() {
+        // super
+        beforeClass(true, null);
+        WebDriver webDriver = getWebDriver();
+        // Init helpers
+        caHelper = new CaHelper(webDriver);
+    }
+    
+    @AfterClass
+    public static void exit(){
+        removeCaAndCryptoToken(TestData.CA_NAME);
+        // super
+        afterClass();
+    }
+
+    @After
+    public void clean(){
+        removeCaAndCryptoToken(TestData.CA_NAME);
+    }
+    
+    @Test
+    public void addCaWithInvalidValidity() {
+        caHelper.openPage(getAdminWebUrl());
+        caHelper.addCa(TestData.CA_NAME);
+        caHelper.setValidity(TestData.CA_VALIDITY_BAD_FORMAT);
+        caHelper.createCa();
+        caHelper.assertHasErrorMessage(TestData.INVALID_SAN_ERROR);
+    }
+    
+    @Test
+    public void addCaWithEmptyValidity() {
+        caHelper.openPage(getAdminWebUrl());
+        caHelper.addCa(TestData.CA_NAME);
+        caHelper.setValidity(TestData.CA_VALIDITY_EMPTY);
+        caHelper.createCa();
+        caHelper.assertHasErrorMessage(TestData.EMPTY_SAN_ERROR);
+    }
+    
+    @Test
+    public void addCaWithGoodValidityShortFormat() {
+        caHelper.openPage(getAdminWebUrl());
+        caHelper.addCa(TestData.CA_NAME);
+        caHelper.setValidity(TestData.GOOD_VALIDITY_SHORT_FORMAT);
+        caHelper.createCa();
+        caHelper.assertExists(TestData.CA_NAME);
+    }
+    
+    @Test
+    public void addCaWithGoodValidityLongFormat() {
+        caHelper.openPage(getAdminWebUrl());
+        caHelper.addCa(TestData.CA_NAME);
+        caHelper.setValidity(TestData.GOOD_VALIDITY_LONG_FORMAT);
+        caHelper.createCa();
+        caHelper.assertExists(TestData.CA_NAME);
+    }    
+    
+}
