e36267da7ba0c30eaa23f3e671eed238b820d1d6
Lauri Kongas <Lauri.Kongas.ext@primekey.com>
2020-03-20 09:32:37 +0000

    ECA-8892 Handling conflict between CA setting to pre-produce OCSP responses and OCSP Key binding nonce setting Add missing unversioned file
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@34676 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/cesecore-common/src/org/cesecore/keybind/InternalKeyBindingNonceConflictException.java
@@ -0,0 +1,59 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.cesecore.keybind;
+
+import org.cesecore.CesecoreException;
+import org.cesecore.ErrorCode;
+
+/**
+ * An exception thrown when someone tries to create an InternalKeyBinding with a nonce setting that conflicts with the CA's OCSP response pre-production setting.
+ *
+ * @version $Id$
+ */
+public class InternalKeyBindingNonceConflictException extends CesecoreException {
+
+    private static final long serialVersionUID = 1L;
+    public static final ErrorCode INTERNAL_KEY_BINDING_NONCE_CONFLICT = ErrorCode.INTERNAL_KEY_BINDING_NONCE_CONFLICT;
+
+    /**
+     * Creates a new instance of <code>InternalKeyBindingNonceConflictException</code> without detail message.
+     */
+    public InternalKeyBindingNonceConflictException() {
+        super(INTERNAL_KEY_BINDING_NONCE_CONFLICT);
+    }
+
+    /**
+     * Constructs an instance of <code>InternalKeyBindingNonceConflictException</code> with the specified detail message.
+     * @param msg the detail message.
+     */
+    public InternalKeyBindingNonceConflictException(String msg) {
+        super(INTERNAL_KEY_BINDING_NONCE_CONFLICT, msg);
+    }
+
+    /**
+     * Constructs an instance of <code>InternalKeyBindingNonceConflictException</code> with the specified detail message.
+     * @param exception the exception that caused this
+     */
+    public InternalKeyBindingNonceConflictException(Exception exception) {
+        super(INTERNAL_KEY_BINDING_NONCE_CONFLICT, exception);
+    }
+
+    /**
+     * Constructs an instance of <code>InternalKeyBindingNonceConflictException</code> with the specified detail message.
+     * @param msg the detail message.
+     * @param exception the exception that caused this
+     */
+    public InternalKeyBindingNonceConflictException(String msg, Exception e) {
+        super(INTERNAL_KEY_BINDING_NONCE_CONFLICT, msg, e);
+    }
+}
