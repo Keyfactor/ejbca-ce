1ddbeece02aaee714268003115fae6c5e03e7dab
Andrey Sergeev <andrey.sergeev.ext@primekey.com>
2020-03-20 16:30:48 +0000

    [ECA-4363] Use different return codes for importprofiles CLI command
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@34683 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/ejbca-ejb-cli/src-test/org/ejbca/ui/cli/TestFileResource.java
@@ -0,0 +1,46 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.ui.cli;
+
+import java.io.File;
+import java.net.URL;
+
+// TODO ECA-8963: Extract into a separate module ejbca-unittest, as it is common utility class that can be reused.
+/**
+ * This is a help class to locate an input file for a test from classpath.
+ *
+ * @version $Id$
+ */
+public class TestFileResource {
+
+    private String fileName;
+    private File file;
+
+    public TestFileResource(final String fileName) {
+        this.fileName = fileName;
+        final URL fileUrl = getClass().getClassLoader().getResource(fileName);
+        if(fileUrl == null) {
+            throw new RuntimeException("Cannot locate file [" + fileName + "] in classpath.");
+        }
+        this.file = new File(fileUrl.getFile());
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+}
