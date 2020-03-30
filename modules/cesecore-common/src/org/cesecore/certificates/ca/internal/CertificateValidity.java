47a862019a533677a319e050a93a790d61bd8578
Mike Agrenius Kushner <mike.kushner@primekey.com>
2020-03-30 09:16:31 +0000

    ECA-1691: Fixed reviewer's comments and added provision (and unit test) for creating backdated link certificates
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@34745 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-common/src/org/cesecore/certificates/ca/internal/CertificateValidity.java
+++ modules/cesecore-common/src/org/cesecore/certificates/ca/internal/CertificateValidity.java
@@ -187,10 +187,10 @@ public class CertificateValidity {
         if (lastDate == null) {
         	lastDate = certProfileLastDate;
         }
-        // Limit validity: No not allow lastDate to be set in the past, unless certificate is being created as a backdated revocation
-        if (lastDate.before(now) && subject.getStatus() != EndEntityConstants.STATUS_REVOKED) {
-            String msg = "notAfter in request for user " + subject.getUsername() + " set before the current date (" + lastDate.toString()
-                    + "), which is only allowed for backdated revocations.";
+        // Limit validity: No not allow lastDate to be set in the past, unless certificate is being created as a backdated revocation or as a link certificate
+        if (lastDate.before(now) && subject.getStatus() != EndEntityConstants.STATUS_REVOKED && !isLinkCertificate) {
+            String msg = "notAfter (" + lastDate.toString() +") in request for user '" + subject.getUsername() + "' set before the current date (" + now
+                    + "), which is only allowed for backdated revocations or link certificates.";
             log.info(msg);
             throw new IllegalValidityException(msg);
         }
