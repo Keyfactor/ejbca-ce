5cf4276a520a9d03c620c46a59732dd3cd0ade42
Jekaterina Bunina <jekaterina.Bunina.ext@primekey.com>
2020-03-31 07:33:40 +0000

    ECA-8911 ClientToolBox version
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@34756 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ws-cli/src/org/ejbca/core/protocol/ws/client/ejbcawsracli.java
+++ modules/ejbca-ws-cli/src/org/ejbca/core/protocol/ws/client/ejbcawsracli.java
@@ -42,7 +42,8 @@ public class ejbcawsracli  {
                     "getpublisherqueuelength | revoketoken | revokeuser | checkrevocationstatus | generatenewuser | " +
                     "createcrl | cacertrequest | cacertresponse | customlog | getprofile | createcryptotoken | " + 
                     "generatectkeys | createca | importcacert | updatecacert | addadmintorole | removeadminfromrole | getexpiredcerts | " +
-                    "getexpiredcertsbyissuer | getexpiredcertsbytype | stress | keyrecover | keyrecoverenroll | getremainingnumberofapprovals");
+                    "getexpiredcertsbyissuer | getexpiredcertsbytype | stress | keyrecover | keyrecoverenroll | getremainingnumberofapprovals |" +
+                    "getClientToolBoxVersion");
             }
         } catch (ErrorAdminCommandException e) {
             final Throwable cause = e.getCause();
