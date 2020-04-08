2b91ac28d3d1dbd9938ff41a8601dd1032174410
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2020-04-08 12:06:11 +0000

    ECA-9035: upgrade to BC 1.65, fix code that is caught in more firm tests in BC, and legacy SERIALNUMBER/SN handling, add test for encrypting/decrypting KeyPair
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@34835 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-common/src-test/org/ejbca/util/crypto/CryptoToolsTest.java
+++ modules/ejbca-common/src-test/org/ejbca/util/crypto/CryptoToolsTest.java
@@ -12,9 +12,31 @@
  *************************************************************************/
 package org.ejbca.util.crypto;
 
-import static org.junit.Assert.assertEquals;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.util.Properties;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.cesecore.keys.token.CryptoToken;
+import org.cesecore.keys.token.CryptoTokenFactory;
+import org.cesecore.keys.token.CryptoTokenOfflineException;
+import org.cesecore.keys.token.KeyGenParams;
+import org.cesecore.keys.token.SoftCryptoToken;
+import org.cesecore.keys.token.p11.exception.NoSuchSlotException;
+import org.cesecore.keys.util.KeyTools;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * This class contains unit tests for the CryptoTools class.
@@ -23,7 +45,7 @@ import org.junit.Test;
  *
  */
 public class CryptoToolsTest {
-    
+
     @Test
     public void testExtractSaltFromPasswordHash() {
         //Firstly, generate a hash.
@@ -33,6 +55,65 @@ public class CryptoToolsTest {
         
         String extractedSalt = CryptoTools.extractSaltFromPasswordHash(passwordHash);
         assertEquals(salt, extractedSalt);
+    }
+    
+    /** Test that we can generate and serialize/deserialize a key pair */
+    @Test
+    public void testKeyPair() throws NoSuchAlgorithmException, NoSuchProviderException, IOException, ClassNotFoundException {
+        KeyPairGenerator keygen = KeyPairGenerator.getInstance("RSA", new BouncyCastleProvider());
+        keygen.initialize(1024);
+        KeyPair keys = keygen.generateKeyPair();
+        byte[] encodedPublicKey = keys.getPublic().getEncoded();
+        assertEquals("org.bouncycastle.jcajce.provider.asymmetric.rsa.BCRSAPublicKey", keys.getPublic().getClass().getName());
+        assertEquals("X.509", keys.getPublic().getFormat());
+        assertEquals("RSA", keys.getPublic().getAlgorithm());
+        assertNotNull("Encoded public key should not be null", encodedPublicKey);
+        byte[] encodedPrivateKey = keys.getPrivate().getEncoded();
+        assertEquals("org.bouncycastle.jcajce.provider.asymmetric.rsa.BCRSAPrivateCrtKey", keys.getPrivate().getClass().getName());
+        assertEquals("PKCS#8", keys.getPrivate().getFormat());
+        assertNotNull("Encoded private key should not be null", encodedPrivateKey);
+        
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream os = new ObjectOutputStream(baos);
+        os.writeObject(keys);
+        byte[] output = baos.toByteArray();
+        ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(output));
+        Object o = ois.readObject();
+        KeyPair keysSerialized = (KeyPair)o;
+        encodedPublicKey = keysSerialized.getPublic().getEncoded();
+        assertEquals("org.bouncycastle.jcajce.provider.asymmetric.rsa.BCRSAPublicKey", keys.getPublic().getClass().getName());
+        assertEquals("X.509", keys.getPublic().getFormat());
+        assertEquals("RSA", keys.getPublic().getAlgorithm());
+        assertNotNull("Encoded public key should not be null", encodedPublicKey);
+        encodedPrivateKey = keysSerialized.getPrivate().getEncoded();
+        assertEquals("org.bouncycastle.jcajce.provider.asymmetric.rsa.BCRSAPrivateCrtKey", keys.getPrivate().getClass().getName());
+        assertEquals("PKCS#8", keys.getPrivate().getFormat());
+        assertNotNull("Encoded private key should not be null", encodedPrivateKey);
+    }
+    
+    /** Test that we can encrypt and decrypt a KeyPair */
+    @Test
+    public void testEncryptDecrypt() throws NoSuchSlotException, InvalidAlgorithmParameterException, CryptoTokenOfflineException, IOException, InvalidKeyException {
+        CryptoToken cryptoToken = CryptoTokenFactory.createCryptoToken(SoftCryptoToken.class.getName(), new Properties(), null, 111, "Soft CryptoToken");
+        final String alias = "alias";
+        cryptoToken.generateKeyPair(KeyGenParams.builder("1024").build(), alias);
+        final KeyPair keypair = KeyTools.genKeys("1024",  "RSA");
+        byte[] encryptedBytes = CryptoTools.encryptKeys(cryptoToken, alias, keypair);
+        assertNotNull("Encrypted key pair should not be null", encryptedBytes);
+        final KeyPair keys = CryptoTools.decryptKeys(cryptoToken, alias, encryptedBytes);
+        assertNotNull("Decrypted key pair should not be null", keys);
+        // Throws exception is testing does not work
+        KeyTools.testKey(keys.getPrivate(), keys.getPublic(), BouncyCastleProvider.PROVIDER_NAME);
+        
+        byte[] encodedPublicKey = keys.getPublic().getEncoded();
+        assertEquals("org.bouncycastle.jcajce.provider.asymmetric.rsa.BCRSAPublicKey", keys.getPublic().getClass().getName());
+        assertEquals("X.509", keys.getPublic().getFormat());
+        assertEquals("RSA", keys.getPublic().getAlgorithm());
+        assertNotNull("Encoded public key should not be null", encodedPublicKey);
+        byte[] encodedPrivateKey = keys.getPrivate().getEncoded();
+        assertEquals("org.bouncycastle.jcajce.provider.asymmetric.rsa.BCRSAPrivateCrtKey", keys.getPrivate().getClass().getName());
+        assertEquals("PKCS#8", keys.getPrivate().getFormat());
+        assertNotNull("Encoded private key should not be null", encodedPrivateKey);
     }
 
 }
