250cc168810a7b8d2f74240184c4f60e6643eaaf
Henrik Sunmark <henrik.sunmark@primekey.com>
2020-04-22 13:07:56 +0000

    ECA-9059: Applied fixes for review comments
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@34891 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-webtest/src-test/org/ejbca/webtest/scenario/EcaQa257_CreateCertificateAuthorityInCAWebAdmin.java
+++ modules/ejbca-webtest/src-test/org/ejbca/webtest/scenario/EcaQa257_CreateCertificateAuthorityInCAWebAdmin.java
@@ -19,32 +19,39 @@ import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
-import org.openqa.selenium.WebDriver;
+
+/**
+ * This test verifies creation of Subordinate certificate authorities in CA Web admin.
+ * <br>
+ * Reference: <a href="https://jira.primekey.se/browse/ECAQA-257">ECAQA-257</a>
+ * 
+ * @version $Id$ EcaQa257_CreateCertificateAuthorityInCAWebAdmin.java 2020-04-21 15:00 tobiasM$
+ *
+ */
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class EcaQa257_CreateCertificateAuthorityInCAWebAdmin extends WebTestBase {
     //Helpers
     private static CaHelper caHelper;
-    private static WebDriver webDriver;
 
     //TestData
-    private static final String CA_NAME = "CHASubCA";
-    private static final String SUBJECT_DN = "CN=CHASubCA,O=Primekey,C=SE";
-    private static final String SIGN_CA = "ManagementCA";
-    private static final String CERTIFICATE_PROFILE = "SUBCA";
-    private static final String VALIDITY = "10y3mo";
-    private static final String POLICY_ID = "2.5.29.32.0";
-    private static final String CRL_EXPIRE_PERIOD = "1d";
-    private static final String CRL_ISSUE_INTERVAL = "1d";
-    private static final String CRL_OVERLAP_TIME = "0m";
-
-    private static final String DELETE_MESSAGE = "Are you sure you want to delete the CA CHASubCA? You should revoke the CA instead if you already have used it to issue certificates.";
+    public static class TestData {
+        private static final String CA_NAME = "EcaQa257";
+        private static final String SUBJECT_DN = "CN="+CA_NAME+",O=Primekey,C=SE";
+        private static final String SIGN_CA = "ManagementCA";
+        private static final String CERTIFICATE_PROFILE = "SUBCA";
+        private static final String VALIDITY = "10y3mo";
+        private static final String POLICY_ID = "2.5.29.32.0";
+        private static final String CRL_EXPIRE_PERIOD = "1d";
+        private static final String CRL_ISSUE_INTERVAL = "1d";
+        private static final String CRL_OVERLAP_TIME = "0m";
+        private static final String DELETE_MESSAGE = "Are you sure you want to delete the CA "+CA_NAME+"? You should revoke the CA instead if you already have used it to issue certificates.";
+    }
 
     @BeforeClass
     public static void init() {
         beforeClass(true, null);
-        webDriver = getWebDriver();
-        caHelper = new CaHelper(webDriver);
+        caHelper = new CaHelper(getWebDriver());
     }
 
     @AfterClass
@@ -53,31 +60,31 @@ public class EcaQa257_CreateCertificateAuthorityInCAWebAdmin extends WebTestBase
     }
 
     @Test
-    public void testA_AddCA() {
+    public void stepA_AddCA() {
         caHelper.openPage(getAdminWebUrl());
-        caHelper.addCa(CA_NAME);
+        caHelper.addCa(TestData.CA_NAME);
     }
 
     @Test
-    public void testB_InsertTestData() {
-        caHelper.setSubjectDn(SUBJECT_DN);
-        caHelper.setSignedBy(SIGN_CA);
-        caHelper.setCertificateProfile(CERTIFICATE_PROFILE);
-        caHelper.setValidity(VALIDITY);
-        caHelper.setPolicyID(POLICY_ID);
-        caHelper.setCrlPeriod(CRL_EXPIRE_PERIOD);
-        caHelper.setCrlIssueInterval(CRL_ISSUE_INTERVAL);
-        caHelper.setCrlOverlapTime(CRL_OVERLAP_TIME);
+    public void stepB_InsertTestData() {
+        caHelper.setSubjectDn(TestData.SUBJECT_DN);
+        caHelper.setSignedBy(TestData.SIGN_CA);
+        caHelper.setCertificateProfile(TestData.CERTIFICATE_PROFILE);
+        caHelper.setValidity(TestData.VALIDITY);
+        caHelper.setPolicyID(TestData.POLICY_ID);
+        caHelper.setCrlPeriod(TestData.CRL_EXPIRE_PERIOD);
+        caHelper.setCrlIssueInterval(TestData.CRL_ISSUE_INTERVAL);
+        caHelper.setCrlOverlapTime(TestData.CRL_OVERLAP_TIME);
     }
 
     @Test
-    public void testC_CreateCa() {
+    public void stepC_CreateCa() {
         caHelper.createCa();
-        caHelper.assertExists(CA_NAME);
+        caHelper.assertExists(TestData.CA_NAME);
     }
 
     @Test
-    public void testD_DeleteCa() {
-        caHelper.deleteCaAndAssert(DELETE_MESSAGE, true, false, "CHASubCA", CA_NAME);
+    public void stepD_DeleteCa() {
+        caHelper.deleteCaAndAssert(TestData.DELETE_MESSAGE, true, false, "EcaQa257", TestData.CA_NAME);
     }
 }
