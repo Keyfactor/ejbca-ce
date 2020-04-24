5b1e251beddf1dceee8dfb32f78ba219bea9382a
Andrey Sergeev <andrey.sergeev.ext@primekey.com>
2020-04-24 13:00:03 +0000

    [ECA-9047] Create automated test for ECAQA-263 (minor fixes)
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@34910 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-webtest/src-test/org/ejbca/webtest/scenario/EcaQa263_CreationOfEndEntityProfileInCAWebAdmin.java
+++ modules/ejbca-webtest/src-test/org/ejbca/webtest/scenario/EcaQa263_CreationOfEndEntityProfileInCAWebAdmin.java
@@ -13,47 +13,65 @@
 package org.ejbca.webtest.scenario;
 
 import org.ejbca.webtest.WebTestBase;
+import org.ejbca.webtest.helper.CaHelper;
 import org.ejbca.webtest.helper.EndEntityProfileHelper;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
+import org.openqa.selenium.WebDriver;
 
 /**
  * This test describes a creation of End Entity Profile in CA Web admin.
- *  <br/>
+ * <br/>
  * Reference: <a href="https://jira.primekey.se/browse/ECAQA-263">ECAQA-263</a>
- * 
- * @version $Id$ EcaQa263_CreationOfEndEntityProfileInCAWebAdmin.java 2020-04-21 15:00 $tobiasM
  *
+ * @version $Id$ EcaQa263_CreationOfEndEntityProfileInCAWebAdmin.java 2020-04-21 15:00 $tobiasM
  */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class EcaQa263_CreationOfEndEntityProfileInCAWebAdmin extends WebTestBase {
-    //Helpers
+
+    // Helpers
+    private static CaHelper caHelper;
     private static EndEntityProfileHelper endEntityProfileHelper;
 
-    //TestData
+    // TestData
     public static class TestData {
-        private static final String ENTITY_PROFILE_NAME = "EcaQa263";
-        private static final String DEFAULT_CA_NAME = "Example Person CA";
-        private static final String DN_ATTRIBUTE_ORGANIZATION = "O, Organization";
-        private static final String DN_ATTRIBUTE_COUNTRY = "C, Country (ISO 3166)";
+        static final String CA_NAME = "EcaQa263_CA";
+        static final String CA_VALIDITY = "2y";
+        static final String ENTITY_PROFILE_NAME = "EcaQa263_EE";
+        static final String DEFAULT_CA_NAME = CA_NAME;
+        static final String DN_ATTRIBUTE_ORGANIZATION = "O, Organization";
+        static final String DN_ATTRIBUTE_COUNTRY = "C, Country (ISO 3166)";
     }
 
     @BeforeClass
     public static void init() {
         beforeClass(true, null);
-        endEntityProfileHelper = new EndEntityProfileHelper(getWebDriver());
+        final WebDriver webDriver = getWebDriver();
+        caHelper = new CaHelper(webDriver);
+        endEntityProfileHelper = new EndEntityProfileHelper(webDriver);
     }
 
     @AfterClass
     public static void exit() {
+        removeCaAndCryptoToken(TestData.CA_NAME);
+        // super
         afterClass();
     }
 
     @Test
-    public void stepA_AddEndEntityProfile() {
+    public void stepA_AddCA() {
+        caHelper.openPage(getAdminWebUrl());
+        caHelper.addCa(TestData.CA_NAME);
+        caHelper.setValidity(TestData.CA_VALIDITY);
+        caHelper.createCa();
+        caHelper.assertExists(TestData.CA_NAME);
+    }
+
+    @Test
+    public void stepB_AddEndEntityProfile() {
         endEntityProfileHelper.openPage(getAdminWebUrl());
         endEntityProfileHelper.addEndEntityProfile(TestData.ENTITY_PROFILE_NAME);
         endEntityProfileHelper.openEditEndEntityProfilePage(TestData.ENTITY_PROFILE_NAME);
@@ -61,7 +79,7 @@ public class EcaQa263_CreationOfEndEntityProfileInCAWebAdmin extends WebTestBase
     }
 
     @Test
-    public void stepB_AddAttributes() {
+    public void stepC_AddAttributes() {
         endEntityProfileHelper.addSubjectDnAttribute(TestData.DN_ATTRIBUTE_ORGANIZATION);
         endEntityProfileHelper.subjectDnAttributeRequiredBoxTrigger(TestData.DN_ATTRIBUTE_ORGANIZATION);
         endEntityProfileHelper.addSubjectDnAttribute(TestData.DN_ATTRIBUTE_COUNTRY);
@@ -69,7 +87,7 @@ public class EcaQa263_CreationOfEndEntityProfileInCAWebAdmin extends WebTestBase
     }
 
     @Test
-    public void stepC_DefaultCaAndSave() {
+    public void stepD_DefaultCaAndSave() {
         endEntityProfileHelper.selectDefaultCa(TestData.DEFAULT_CA_NAME);
         endEntityProfileHelper.saveEndEntityProfile();
         endEntityProfileHelper.deleteEndEntityProfile(TestData.ENTITY_PROFILE_NAME);
