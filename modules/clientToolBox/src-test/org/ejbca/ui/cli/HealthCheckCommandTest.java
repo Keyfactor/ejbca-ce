580180f01a3e9719fb42dd734eb02a5804da7ce7
Jekaterina Bunina <jekaterina.Bunina.ext@primekey.com>
2020-05-05 07:54:17 +0000

    ECA-9089
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@34967 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/clientToolBox/src-test/org/ejbca/ui/cli/HealthCheckCommandTest.java
@@ -0,0 +1,57 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.ui.cli;
+
+import org.cesecore.SystemTestsConfiguration;
+import org.cesecore.util.EjbRemoteHelper;
+import org.ejbca.config.WebConfiguration;
+import org.ejbca.core.ejb.config.ConfigurationSessionRemote;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+
+/**
+ * @version $Id$
+ */
+public class HealthCheckCommandTest {
+
+    private HealthCheck command = new HealthCheck();
+    private String httpReqPath;
+
+    @Rule
+    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
+
+    @Before
+    public void setUp() throws Exception {
+        ConfigurationSessionRemote configurationSessionRemote = EjbRemoteHelper.INSTANCE.getRemoteSession(ConfigurationSessionRemote.class, EjbRemoteHelper.MODULE_TEST);
+        final String httpHost = SystemTestsConfiguration.getRemoteHost(configurationSessionRemote.getProperty(WebConfiguration.CONFIG_HTTPSSERVERHOSTNAME));
+        final String httpPort = SystemTestsConfiguration.getRemotePortHttp(configurationSessionRemote.getProperty(WebConfiguration.CONFIG_HTTPSERVERPUBHTTP));
+        httpReqPath = "http://"+httpHost+":" + httpPort + "/ejbca";
+    }
+
+    @Test
+    public void testCommandSucceeds() {
+        String[] args = new String[]{"healthCheck", httpReqPath + "/publicweb/healthcheck/ejbcahealth"};
+        command.execute(args);
+    }
+
+    @Test
+
+    public void testCommandWronUri() {
+        exit.expectSystemExitWithStatus(-1);
+
+        String[] args = new String[]{"healthCheck", "bad.url"};
+        command.execute(args);
+    }
+}
---
+++ modules/clientToolBox/src-test/org/ejbca/ui/cli/HealthCheckTestCommandTest.java
@@ -0,0 +1,53 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.ui.cli;
+
+import org.cesecore.SystemTestsConfiguration;
+import org.cesecore.util.EjbRemoteHelper;
+import org.ejbca.config.WebConfiguration;
+import org.ejbca.core.ejb.config.ConfigurationSessionRemote;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+
+/**
+ * @version $Id$
+ */
+public class HealthCheckTestCommandTest {
+
+    private HealthCheckTest command = new HealthCheckTest();
+    private String httpReqPath;
+
+    @Rule
+    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
+
+    @Before
+    public void setUp() throws Exception {
+        ConfigurationSessionRemote configurationSessionRemote = EjbRemoteHelper.INSTANCE.getRemoteSession(ConfigurationSessionRemote.class, EjbRemoteHelper.MODULE_TEST);
+        final String httpHost = SystemTestsConfiguration.getRemoteHost(configurationSessionRemote.getProperty(WebConfiguration.CONFIG_HTTPSSERVERHOSTNAME));
+        final String httpPort = SystemTestsConfiguration.getRemotePortHttp(configurationSessionRemote.getProperty(WebConfiguration.CONFIG_HTTPSERVERPUBHTTP));
+        httpReqPath = "http://"+httpHost+":" + httpPort + "/ejbca";
+    }
+
+    @Test
+    public void testCommand() {
+        exit.expectSystemExitWithStatus(0);
+        int numberOfThreads = 1000;
+        int numberOfTests = 100_000;
+        String waitTime ="2000";
+        String[] args = new String[]{"healthCheckTest", httpReqPath + "/publicweb/healthcheck/ejbcahealth",
+                numberOfThreads + ":" + numberOfTests, waitTime};
+        command.execute(args);
+    }
+}
