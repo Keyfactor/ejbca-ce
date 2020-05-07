b6ca5f81e902e029fe1cb39266e3559466d72313
Serkan Ongan <serkan.ongan.ext@primekey.com>
2020-05-07 11:23:39 +0000

    ECA-9126: Add a small fix for deleting ocsp responses and related tests.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@34996 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/systemtests/src-test/org/cesecore/certificates/ocsp/OcspDataSessionBeanTest.java
+++ modules/systemtests/src-test/org/cesecore/certificates/ocsp/OcspDataSessionBeanTest.java
@@ -15,11 +15,11 @@ package org.cesecore.certificates.ocsp;
 import java.util.List;
 import org.apache.log4j.Logger;
 import org.junit.*;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import org.cesecore.oscp.OcspResponseData;
 import org.cesecore.util.EjbRemoteHelper;
+
+import static org.junit.Assert.*;
 
 /**
  * @version $Id
@@ -34,31 +34,168 @@ public class OcspDataSessionBeanTest {
     private final static Integer certificateAuthOne = 123456789;
     private final static Integer certificateAuthTwo = 234567890;
 
-    @BeforeClass
-    public static void setupTestClass() {
-        log.trace(">OcspDataSessionBeanTest");
-
-        // Required by testFindExpiringOcpsDataReturnsCorrectResponses
-        persistMixofExpiredResponses();
-    }
-
-    @AfterClass
-    public static void teardownTestClass() {
+    @After
+    public void removeData() {
+        log.trace(">removeData");
         removeAllTestOcspResponses();
-
-        log.trace("<OcspDataSessionBeanTest");
+        log.trace("<removeData");
     }
 
+    // Remote Session methods
 
     @Test
-    public void testFindExpiringOcpsDataReturnsCorrectResponses() {
+    public void testFindExpiringOcpsDataReturnsCorrectResponses() throws InterruptedException {
         log.trace(">testFindExpiringOcpsDataReturnsCorrectResponses");
+
+        persistMixofExpiredResponses();
+        Thread.sleep(2000);
 
         List<String> expiredResponses = ocspDataSessionRemote.findExpiringOcpsData(certificateAuthOne, System.currentTimeMillis(), 500, 0);
         assertEquals(1, expiredResponses.size());
         assertTrue(expiredResponses.contains("test-sn-2"));
 
         log.trace("<testFindExpiringOcpsDataReturnsCorrectResponses");
+    }
+
+    @Test
+    public void testFindOcspDataByCaId() throws InterruptedException {
+        log.trace(">testfindOcspDataByCaId");
+
+        Long time = System.currentTimeMillis();
+
+        OcspResponseData responseA = new OcspResponseData("id-find-by-ca-1", certificateAuthOne, "sn-find-by-ca-1", time, time, new byte[0]);
+        OcspResponseData responseB = new OcspResponseData("id-find-by-ca-2", certificateAuthOne, "sn-find-by-ca-2", time, time, new byte[0]);
+        ocspDataProxySessionRemote.storeOcspData(responseA);
+        ocspDataProxySessionRemote.storeOcspData(responseB);
+        Thread.sleep(1000);
+
+        assertEquals(2, ocspDataSessionRemote.findOcspDataByCaId(certificateAuthOne).size());
+
+        log.trace("<testfindOcspDataByCaId");
+    }
+
+    @Test
+    public void testFindOcspDataByCaIdSerialNumber() throws InterruptedException {
+        log.trace(">testFindOcspDataByCaIdSerialNumber");
+
+        Long now = System.currentTimeMillis();
+        Long future = now + 360000;
+
+        OcspResponseData response = new OcspResponseData("id-find-by-ca-serial-1", certificateAuthOne, "sn-find-by-ca-serial-1", now, future, new byte[0]);
+        ocspDataProxySessionRemote.storeOcspData(response);
+        Thread.sleep(2000);
+
+        OcspResponseData found = ocspDataSessionRemote.findOcspDataByCaIdSerialNumber(response.getCaId(), response.getSerialNumber());
+        OcspResponseData notFound = ocspDataSessionRemote.findOcspDataByCaIdSerialNumber(certificateAuthOne, "sn-find-by-ca-serial-2");
+
+        assertNotNull(found);
+        assertEquals("sn-find-by-ca-serial-1", found.getSerialNumber());
+
+        assertNull(notFound);
+
+        log.trace("<testFindOcspDataByCaIdSerialNumber");
+    }
+
+    @Test
+    public void testFindOcspDataById() throws InterruptedException {
+        log.trace(">testFindOcspDataById");
+
+        Long now = System.currentTimeMillis();
+
+        OcspResponseData response = new OcspResponseData("id-find-by-id-1", certificateAuthOne, "sn-find-by-id-1", now, now, new byte[0]);
+        ocspDataProxySessionRemote.storeOcspData(response);
+        Thread.sleep(1000);
+
+        OcspResponseData found = ocspDataSessionRemote.findOcspDataById(response.getId());
+
+        assertNotNull(found);
+        assertEquals(response.getSerialNumber(), found.getSerialNumber());
+
+        log.trace("<testFindOcspDataById");
+    }
+
+    @Test
+    public void testFindOcspDataBySerialNumber() throws InterruptedException {
+        log.trace(">testFindOcspDataBySerialNumber");
+
+        Long now = System.currentTimeMillis();
+
+        OcspResponseData responseA = new OcspResponseData("id-find-by-id-1", certificateAuthOne, "sn-find-by-id-1", now, now, new byte[0]);
+        OcspResponseData responseB = new OcspResponseData("id-find-by-id-2", certificateAuthOne, "sn-find-by-id-1", now, now, new byte[0]);
+        ocspDataProxySessionRemote.storeOcspData(responseA);
+        ocspDataProxySessionRemote.storeOcspData(responseB);
+        Thread.sleep(1000);
+
+        List<OcspResponseData> foundResponses = ocspDataSessionRemote.findOcspDataBySerialNumber(responseA.getSerialNumber());
+
+        assertEquals(2, foundResponses.size());
+        assertEquals(responseA.getSerialNumber(), foundResponses.get(0).getSerialNumber());
+        assertEquals(responseA.getSerialNumber(), foundResponses.get(1).getSerialNumber());
+
+        log.trace("<testFindOcspDataBySerialNumber");
+    }
+
+
+    // Local interface methods tested via proxy.
+
+    @Test
+    public void testStoreOcspData() throws InterruptedException {
+        Long now = System.currentTimeMillis();
+
+        // For either certificateAuthOne or certificateAuthTwo so it can be cleaned at @After
+        OcspResponseData response = new OcspResponseData("id-store-ocsp-1", certificateAuthOne, "sn-store-ocsp-1", now, now, new byte[0]);
+        ocspDataProxySessionRemote.storeOcspData(response);
+        Thread.sleep(1000);
+
+        OcspResponseData found = ocspDataSessionRemote.findOcspDataById(response.getId());
+
+        assertNotNull(found);
+        assertEquals(response.getSerialNumber(), found.getSerialNumber());
+    }
+
+    @Test
+    public void testDeleteOcspDataByCaId() throws InterruptedException {
+        Long now = System.currentTimeMillis();
+
+        OcspResponseData response = new OcspResponseData("id-delete-by-ca", certificateAuthOne, "sn-delete-by-ca", now, now, new byte[0]);
+        ocspDataProxySessionRemote.storeOcspData(response);
+        Thread.sleep(1000);
+
+        OcspResponseData found = ocspDataSessionRemote.findOcspDataById(response.getId());
+        assertNotNull(found);
+
+        ocspDataProxySessionRemote.deleteOcspDataByCaId(response.getCaId());
+        assertNull(ocspDataSessionRemote.findOcspDataById(response.getId()));
+    }
+
+    @Test
+    public void testDeleteOcspDataBySerialNumber() throws InterruptedException {
+        Long now = System.currentTimeMillis();
+
+        OcspResponseData response = new OcspResponseData("id-delete-by-serial", certificateAuthOne, "sn-delete-by-serial", now, now, new byte[0]);
+        ocspDataProxySessionRemote.storeOcspData(response);
+        Thread.sleep(1000);
+
+        OcspResponseData found = ocspDataSessionRemote.findOcspDataById(response.getId());
+        assertNotNull(found);
+
+        ocspDataProxySessionRemote.deleteOcspDataBySerialNumber(response.getSerialNumber());
+        assertNull(ocspDataSessionRemote.findOcspDataById(response.getId()));
+    }
+
+    @Test
+    public void testDeleteOcspDataByCaIdSerialNumber() throws InterruptedException {
+        Long now = System.currentTimeMillis();
+
+        OcspResponseData response = new OcspResponseData("id-delete-by-ca-serial", certificateAuthOne, "sn-delete-by-ca-serial", now, now, new byte[0]);
+        ocspDataProxySessionRemote.storeOcspData(response);
+        Thread.sleep(1000);
+
+        OcspResponseData found = ocspDataSessionRemote.findOcspDataById(response.getId());
+        assertNotNull(found);
+
+        ocspDataProxySessionRemote.deleteOcspDataByCaIdSerialNumber(response.getCaId(), response.getSerialNumber());
+        assertNull(ocspDataSessionRemote.findOcspDataById(response.getId()));
     }
 
     /**
