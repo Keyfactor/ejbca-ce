454275e60bddbd34b6814fbe4c4c5600338fe8c5
Andrey Sergeev <andrey.sergeev.ext@primekey.com>
2020-05-11 07:37:17 +0000

    [ECA-8892] Handling conflict between CA setting to pre-produce OCSP responses and OCSP Key binding nonce setting (review clean-ups)
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@35016 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb-interface/src/org/ejbca/core/ejb/ocsp/OcspKeyRenewalSessionLocal.java
+++ modules/ejbca-ejb-interface/src/org/ejbca/core/ejb/ocsp/OcspKeyRenewalSessionLocal.java
@@ -35,13 +35,13 @@ public interface OcspKeyRenewalSessionLocal {
      * an exception if passwords can't be set in memory.
      * 
      * @param signerSubjectDN subject DN of the signing key to be renewed. The string OcspKeyRenewalSessionLocal.RENEW_ALL_KEYS will result 
-     * @throws KeyStoreException if p11 key store hasn't been activated
+
      * @throws CryptoTokenOfflineException if Crypto Token is not available or connected, or key with alias does not exist.
      * @throws InvalidKeyException if the public key can not be used to verify a string signed by the private key, because the key is wrong or the 
      * signature operation fails for other reasons such as a NoSuchAlgorithmException or SignatureException.
      * 
      */
-    void renewKeyStores(String signerSubjectDN) throws KeyStoreException, CryptoTokenOfflineException, InvalidKeyException; 
+    void renewKeyStores(String signerSubjectDN) throws CryptoTokenOfflineException, InvalidKeyException;
     
     /**
      * Cancels all running timers and starts the initial timer. This method should be called from a servlet and be used to start the rekeying 
