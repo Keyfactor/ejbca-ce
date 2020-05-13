0760e5b7309ace17e83f9efca49e854d23c18d85
Mike Agrenius Kushner <mike.kushner@primekey.com>
2020-05-13 15:37:39 +0000

    ECA-9142: Fixing WS test failures - had to regenerate some WS classes
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@35048 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ws-cli/src-gen/org/ejbca/core/protocol/ws/client/gen/CAOfflineException.java
+++ modules/ejbca-ws-cli/src-gen/org/ejbca/core/protocol/ws/client/gen/CAOfflineException.java
@@ -12,16 +12,16 @@ import javax.xml.bind.annotation.XmlType;
  * <p>The following schema fragment specifies the expected content contained within this class.
  * 
  * <pre>
- * &lt;complexType name="CAOfflineException"&gt;
- *   &lt;complexContent&gt;
- *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
- *       &lt;sequence&gt;
- *         &lt;element name="errorCode" type="{http://ws.protocol.core.ejbca.org/}errorCode" minOccurs="0"/&gt;
- *         &lt;element name="message" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
- *       &lt;/sequence&gt;
- *     &lt;/restriction&gt;
- *   &lt;/complexContent&gt;
- * &lt;/complexType&gt;
+ * &lt;complexType name="CAOfflineException">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="errorCode" type="{http://ws.protocol.core.ejbca.org/}errorCode" minOccurs="0"/>
+ *         &lt;element name="message" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
  * </pre>
  * 
  * 
