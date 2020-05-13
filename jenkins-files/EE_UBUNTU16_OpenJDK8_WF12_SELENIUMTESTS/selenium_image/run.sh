40fde7990f1605a85c4ce2c60fa30ab9f5b9f15c
Andrey Sergeev <andrey.sergeev.ext@primekey.com>
2020-05-13 13:56:44 +0000

    [ECA-9112] Selenium Tests in Jenkins
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@35045 37aeb1de-b14b-0410-9a0e-82b28170d228

--- jenkins-files/EE_UBUNTU16_OpenJDK8_WF12_SELENIUMTESTS/selenium_image/run.sh
+++ jenkins-files/EE_UBUNTU16_OpenJDK8_WF12_SELENIUMTESTS/selenium_image/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Options for test JVM. The browser runs in a separate process, so it shouldn't need much memory
-export TEST_OPTS="-XX:+UseG1GC -XX:+UseCompressedOops -XX:OnOutOfMemoryError='kill -9 %p' -Xms64m -Xmx768m"
+export TEST_OPTS="-XX:+UseG1GC -XX:+UseCompressedOops -XX:OnOutOfMemoryError='kill -9 %p' -Xms64m -Xmx1536m"
 # Options for ant itself. The report building can be memory heavy, otherwise it shouldn't need much memory
-export ANT_OPTS="-XX:+UseG1GC -XX:+UseCompressedOops -XX:OnOutOfMemoryError='kill -9 %p' -Xms64m -Xmx1536m"
+export ANT_OPTS="-XX:+UseG1GC -XX:+UseCompressedOops -XX:OnOutOfMemoryError='kill -9 %p' -Xms64m -Xmx256m"
 
 cp /opt/ejbca_conf/* /app/ejbca/conf/
 cp /opt/ejbca_webtest_conf/* /app/ejbca/modules/ejbca-webtest/conf/
@@ -24,5 +24,5 @@ echo '=================== build finished ========================'
 ls -la /app/ejbca/modules/ejbca-webtest/resources
 
 ant ejbca:setup:selenium -Dbrowser.firefox.binary=/usr/bin/firefox -Denv.HOME=/home/jenkins
-ant test:webtest -Dtests.jvmargs="$TEST_OPTS"
+ant test:webtest:jenkins -Dtests.jvmargs="$TEST_OPTS"
 
