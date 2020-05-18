cbf9f8de3eee405880a322c2fe542aa431f1716d
Andrey Sergeev <andrey.sergeev.ext@primekey.com>
2020-05-18 20:32:59 +0000

    [ECA-9150] Audit Log page error
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@35086 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-ejb-interface/src/org/cesecore/certificates/certificateprofile/CertificateProfileSession.java
+++ modules/cesecore-ejb-interface/src/org/cesecore/certificates/certificateprofile/CertificateProfileSession.java
@@ -18,188 +18,213 @@ import java.util.Map;
 import org.cesecore.authentication.tokens.AuthenticationToken;
 import org.cesecore.authorization.AuthorizationDeniedException;
 
-/** Session bean to manage certificate profiles, i.e. add, remove, find
- * 
+/**
+ * Session bean to manage certificate profiles, i.e. add, remove, find
+ * <br/>
  * In order to add or edit CertificateProfiles the admin needs access to:
  * - /ca_functionality/edit_certificate_profiles (StandardRules.CERTIFICATEPROFILEEDIT)
- * - the CAs set as "available CAs" with CA rules as /ca/<caid> (StandardRules.CAACCESS)
+ * - the CAs set as "available CAs" with CA rules as /ca/[caid] (StandardRules.CAACCESS)
  * - if "available CAs" contains ANYCA (-1) the -1 caid privilege is needed.
- *
  * @version $Id$
  */
 public interface CertificateProfileSession {
 
     /**
      * Adds a certificate profile to the database.
-     * 
-     * @param admin administrator performing the task
-     * @param name readable name of new certificate profile
+     * @param authenticationToken administrator performing the task
+     * @param certificateProfileName readable name of new certificate profile
      * @param profile the profile to be added
      * @return the generated certificate profile id
-     * 
      * @throws CertificateProfileExistsException if the profile already exists
 	 * @throws AuthorizationDeniedException if admin is not authorized to edit certificate profile 
      */
-    public int addCertificateProfile(AuthenticationToken admin, String name, CertificateProfile profile)
-            throws CertificateProfileExistsException, AuthorizationDeniedException;
+    int addCertificateProfile(
+            final AuthenticationToken authenticationToken,
+            final String certificateProfileName,
+            final CertificateProfile profile
+    ) throws CertificateProfileExistsException, AuthorizationDeniedException;
 
     /**
      * Adds a certificate profile to the database.
-     * 
-     * @param admin administrator performing the task
-     * @param id internal ID of new certificate profile, use only if you know it's right.
-     * @param name readable name of new certificate profile
-     * @param profile the profile to be added
+     * @param authenticationToken administrator performing the task
+     * @param certificateProfileId internal ID of new certificate profile, use only if you know it's right.
+     * @param certificateProfileName readable name of new certificate profile
+     * @param certificateProfile the profile to be added
      * @return the generated certificate profile id
-     * 
      * @throws CertificateProfileExistsException if the profile already exists
 	 * @throws AuthorizationDeniedException if admin is not authorized to edit certificate profile 
      */
-    public int addCertificateProfile(AuthenticationToken admin, int id, String name, CertificateProfile profile)
-            throws CertificateProfileExistsException, AuthorizationDeniedException;
+    int addCertificateProfile(
+            final AuthenticationToken authenticationToken,
+            final int certificateProfileId,
+            final String certificateProfileName,
+            final CertificateProfile certificateProfile
+    ) throws CertificateProfileExistsException, AuthorizationDeniedException;
 
     /**
      * Updates certificate profile data
-     * 
-     * @param admin Administrator performing the operation
-     * @param name readable name of new certificate profile
-     * @param profile the profile to be added
+     * @param authenticationToken Administrator performing the operation
+     * @param certificateProfileName readable name of new certificate profile
+     * @param certificateProfile the profile to be added
+     * @throws AuthorizationDeniedException if admin is not authorized to change certificate profile
      */
-    void changeCertificateProfile(AuthenticationToken admin, String name, CertificateProfile profile) throws AuthorizationDeniedException;
+    void changeCertificateProfile(
+            final AuthenticationToken authenticationToken,
+            final String certificateProfileName,
+            final CertificateProfile certificateProfile
+    ) throws AuthorizationDeniedException;
 
     /**
 	 * Do not use, use changeCertificateProfile instead. Used internally for
 	 * testing only. Updates a profile without flushing caches.
+     * @param authenticationToken Administrator performing the operation
+     * @param certificateProfileName readable name of certificate profile
+     * @param certificateProfile the profile
+     * @throws AuthorizationDeniedException if admin is not authorized
 	 */
-	void internalChangeCertificateProfileNoFlushCache(AuthenticationToken admin, String name, CertificateProfile profile) throws AuthorizationDeniedException;
+	void internalChangeCertificateProfileNoFlushCache(
+	        final AuthenticationToken authenticationToken,
+            final String certificateProfileName,
+            final CertificateProfile certificateProfile
+    ) throws AuthorizationDeniedException;
 
-	/** Clear and reload certificate profile caches. */
+	/**
+     * Clear and reload certificate profile caches.
+     */
 	void flushProfileCache();
 
 	/**
      * Adds a certificate profile with the same content as the original certificate profile.
-     * 
-     * @param admin Administrator performing the operation
-     * @param orgname name of old certificate profile
-     * @param newname name of new certificate profile
-	 * @param availableCaIds list of CAid to replace the original list with, or null.
+     * @param authenticationToken Administrator performing the operation
+     * @param oldCertificateProfileName name of old certificate profile
+     * @param newCertificateProfileName name of new certificate profile
+	 * @param authorizedCaIds list of CAid to replace the original list with, or null.
+     * @throws CertificateProfileExistsException if the profile already exists
+     * @throws CertificateProfileDoesNotExistException if the profile does not exist
+     * @throws AuthorizationDeniedException if admin is not authorized
      */
-    void cloneCertificateProfile(AuthenticationToken admin, String orgname, String newname,
-            List<Integer> authorizedCaIds) throws CertificateProfileExistsException, CertificateProfileDoesNotExistException, AuthorizationDeniedException;
+    void cloneCertificateProfile(
+            final AuthenticationToken authenticationToken,
+            final String oldCertificateProfileName,
+            final String newCertificateProfileName,
+            final List<Integer> authorizedCaIds
+    ) throws CertificateProfileExistsException, CertificateProfileDoesNotExistException, AuthorizationDeniedException;
 
     /**
-     * Retrieves a Collection of id:s (Integer) to authorized profiles. Only profiles that refer to CA's that the authentication token is 
-     * authorized to will be returned. 
-     * 
-     * @param admin Administrator performing the operation
-     * @param certprofiletype
+     * Retrieves a Collection of id:s (Integer) to authorized profiles. Only profiles that refer to CA's that
+     * the authentication token is authorized to will be returned.
+     * @param authenticationToken Administrator performing the operation
+     * @param certificateProfileType
      *            should be either CertificateConstants.CERTTYPE_ENDENTITY,
      *            CertificateConstants.CERTTYPE_SUBCA,
      *            CertificateConstants.CERTTYPE_ROOTCA,
      *            or CertificateConstants.CERTTYPE_UNKNOWN for all.
      *            Retrieves certificate profile names sorted.
-     * @return Collection of id:s (Integer)
+     * @return List of id's (Integer)
      */
-    List<Integer> getAuthorizedCertificateProfileIds(AuthenticationToken admin, int certprofiletype);
+    List<Integer> getAuthorizedCertificateProfileIds(
+            final AuthenticationToken authenticationToken,
+            final int certificateProfileType
+    );
 
     /**
      * Retrieves a Collection of id:s (Integer) of all certificate profiles which have non-existent CA Ids.
      * This requires access to the root resource (i.e. superadmin access). If access is denied then an empty
      * list is returned.
+     * @param authenticationToken Administrator performing the operation
      */
-    List<Integer> getAuthorizedCertificateProfileWithMissingCAs(AuthenticationToken admin);
+    List<Integer> getAuthorizedCertificateProfileWithMissingCAs(final AuthenticationToken authenticationToken);
 
     /**
      * Finds a certificate profile by id.
-     * 
-     * @param id certificate profile id
+     * @param certificateProfileId certificate profile id
      * @return Certificate profile (cloned) or null if it can not be found.
      */
-    CertificateProfile getCertificateProfile(int id);
+    CertificateProfile getCertificateProfile(final int certificateProfileId);
 
     /**
      * Retrieves a named certificate profile or null if none was found.
-     * 
-     * @param name certificate profile name
+     * @param certificateProfileName certificate profile name
      * @return Certificate profile (cloned) or null if it can not be found.
      */
-    CertificateProfile getCertificateProfile(String name);
+    CertificateProfile getCertificateProfile(final String certificateProfileName);
 
     /**
      * Returns a certificate profile id, given it's certificate profile name.
-     * 
-     * @param name certificate profile name
+     * @param certificateProfileName certificate profile name
      * @return the id or 0 if certificate profile cannot be found.
      */
-    int getCertificateProfileId(String name);
+    int getCertificateProfileId(final String certificateProfileName);
 
     /**
      * Returns a certificate profiles name given it's id.
-     * 
-     * @param id certificate profile id
+     * @param certificateProfileId certificate profile id
      * @return certificate profile name or null if certificate profile id does not exist.
      */
-    String getCertificateProfileName(int id);
+    String getCertificateProfileName(final int certificateProfileId);
 
     /**
-     * Method creating a Map mapping profile id (Integer) to profile name
-     * (String).
+     * Method creating a Map mapping profile id (Integer) to profile name (String).
+     * @return Map of ids and names.
      */
     Map<Integer, String> getCertificateProfileIdToNameMap();
 
     /**
-     * A method designed to be called at startuptime to (possibly) upgrade
-     * certificate profiles. This method will read all Certificate Profiles and
-     * as a side-effect upgrade them if the version changed.
+     * A method designed to be called at startup time to (possibly) upgrade certificate profiles. This method will
+     * read all Certificate Profiles and as a side-effect upgrade them if the version changed.
      */
-    public void initializeAndUpgradeProfiles();
+    void initializeAndUpgradeProfiles();
 
     /**
 	 * Renames a certificate profile
-	 * @param oldname the name of the certificate profile to rename
-	 * @param newname the new name of the certificate profile
+	 * @param oldCertificateProfileName the name of the certificate profile to rename
+	 * @param newCertificateProfileName the new name of the certificate profile
+     * @throws CertificateProfileExistsException if the profile already exists
+     * @throws AuthorizationDeniedException if admin is not authorized
 	 */
-	void renameCertificateProfile(AuthenticationToken admin, String oldname, String newname)
-	        throws CertificateProfileExistsException, AuthorizationDeniedException;
+	void renameCertificateProfile(
+	        final AuthenticationToken authenticationToken,
+            final String oldCertificateProfileName,
+            final String newCertificateProfileName
+    ) throws CertificateProfileExistsException, AuthorizationDeniedException;
 
 	/**
      * Removes a certificate profile from the database, does not throw any errors if the profile does not exist.
-     *
-     * @param admin Administrator performing the operation
-     * @param name the name of the certificate profile to remove
+     * @param authenticationToken Administrator performing the operation
+     * @param certificateProfileName the name of the certificate profile to remove
+     * @throws AuthorizationDeniedException if admin is not authorized
      */
-    public void removeCertificateProfile(AuthenticationToken admin, String name) throws AuthorizationDeniedException;
+    void removeCertificateProfile(
+            final AuthenticationToken authenticationToken,
+            final String certificateProfileName
+    ) throws AuthorizationDeniedException;
 
     /**
-     * Method to check if a CA id exists in any of the certificate profiles. Used
-     * to avoid desyncronization of CA data.
-     * 
-     * @param admin Administrator performing the operation
-     * @param caid the caid to search for.
+     * Method to check if a CA id exists in any of the certificate profiles. Used to avoid desynchronization of CA data.
+     * @param caId the caId to search for.
      * @return true if ca exists in any of the certificate profiles.
      */
-    public boolean existsCAIdInCertificateProfiles(int caid);
+    boolean existsCAIdInCertificateProfiles(int caId);
     
     /**
      * Method to check if a Publisher id exists in any of the certificate profiles.
      * Used to avoid desynchronization of publisher data.
-     * 
-     * @param admin Administrator performing the operation
-     * @param publisherid the publisherid to search for.
+     * @param publisherId the publisherid to search for.
      * @return true if publisher exists in any of the certificate profiles.
      */
-    public boolean existsPublisherIdInCertificateProfiles(int publisherid);
+    boolean existsPublisherIdInCertificateProfiles(int publisherId);
 
     /**
      * Returns the given certificate profile in XML format
-     * 
      * @param authenticationToken the administrator requesting the action
      * @param profileId the id of the certificate profile
-     * @return the certificate profile encoded as XML
      * @throws CertificateProfileDoesNotExistException if the profile with the given ID didn't exist. 
-     * @throws AuthorizationDeniedException if the profile contained CAs that the admin wasn't authorized to, or the admin wasn't authorized to 
-     * view profiles
+     * @throws AuthorizationDeniedException if the profile contained CAs that the admin wasn't authorized to, or
+     * the admin wasn't authorized to view profiles
      */
-    byte[] getProfileAsXml(final AuthenticationToken authenticationToken, final int profileId) throws CertificateProfileDoesNotExistException, AuthorizationDeniedException;
+    byte[] getProfileAsXml(
+            final AuthenticationToken authenticationToken,
+            final int profileId
+    ) throws CertificateProfileDoesNotExistException, AuthorizationDeniedException;
 
 }
