94b47ffe13228f75efbd2d213589f0ea578334d6
Margaret Thomas <margaret.thomas@primekey.com>
2020-05-19 13:40:39 +0000

    ECAQA-43 Manual test automated
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@35097 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-webtest/src-test/org/ejbca/webtest/helper/CmpConfigurationHelper.java
+++ modules/ejbca-webtest/src-test/org/ejbca/webtest/helper/CmpConfigurationHelper.java
@@ -1,11 +1,11 @@
 package org.ejbca.webtest.helper;
 
-import static org.junit.Assert.assertTrue;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 import java.util.List;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import static org.junit.Assert.assertTrue;
 
 /**
  * CMP Configuration helper class for EJBCA Web Tests.
@@ -25,12 +25,14 @@ public class CmpConfigurationHelper extends BaseHelper {
         
         static final By BUTTON_ADD = By.id("cmpaliasesform:buttonaddcmpalias");
         static final By BUTTON_RENAME = By.id("cmpaliasesform:buttonrenamecmpalias");
+        static final By BUTTON_CLONE = By.id("cmpaliasesform:buttonclonecmpalias");
 
         /** Input field for alias name */
         static final By INPUT_NAME = By.id("cmpaliasesform:textfielcmpaliasname");
 
         /** List of available CMP aliases */
         static final By SELECT_ALIAS = By.id("cmpaliasesform:selectaliaslist");
+
     }
     
     public CmpConfigurationHelper(final WebDriver webDriver) {
@@ -68,6 +70,20 @@ public class CmpConfigurationHelper extends BaseHelper {
         fillInput(Page.INPUT_NAME, newAlias);
         clickLink(Page.BUTTON_RENAME);
     }
+
+    /**
+     * Clones a CMP alias by selecting an alias from
+     * the list of available aliases, filling the alias name
+     * text field and clicking on the 'Clone' button
+     *
+     * @param oldAlias name of the alias to rename
+     * @param cloneAlias name of the cloned alias
+     */
+    public void cloneCmpAlias(final String oldAlias ,final String cloneAlias) {
+        selectOptionByName(Page.SELECT_ALIAS, oldAlias);
+        fillInput(Page.INPUT_NAME, cloneAlias);
+        clickLink(Page.BUTTON_CLONE);
+    }
     
     /**
      * Asserts the list of available CMP aliases in the Admin GUI contains the specified alias.
@@ -76,6 +92,6 @@ public class CmpConfigurationHelper extends BaseHelper {
      */
     public void assertCmpAliasExists(final String alias) {
         final List<String> selectNames = getSelectNames(Page.SELECT_ALIAS);
-        assertTrue("CMP alias '" + alias + "' was not found in the list of CMP aliases", selectNames.contains(alias));
+        assertTrue("CMP alias '" +  alias  + "' was not found in the list of CMP aliases", selectNames.contains(alias));
     }
 }
