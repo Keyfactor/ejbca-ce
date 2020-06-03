408566a096b3cec7a89e6e269863c3f6cf0f4c52
Mike Agrenius Kushner <mike.kushner@primekey.com>
2020-06-03 11:35:49 +0000

    ECA-9142: Added Tomas' patch and fixed reviewer's comments
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@35185 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ws-cli/src-gen/org/ejbca/core/protocol/ws/client/gen/WaitingForApprovalException.java
+++ modules/ejbca-ws-cli/src-gen/org/ejbca/core/protocol/ws/client/gen/WaitingForApprovalException.java
@@ -12,16 +12,16 @@ import javax.xml.bind.annotation.XmlType;
  * <p>The following schema fragment specifies the expected content contained within this class.
  * 
  * <pre>
- * &lt;complexType name="WaitingForApprovalException">
- *   &lt;complexContent>
- *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
- *       &lt;sequence>
- *         &lt;element name="message" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
- *         &lt;element name="requestId" type="{http://www.w3.org/2001/XMLSchema}int"/>
- *       &lt;/sequence>
- *     &lt;/restriction>
- *   &lt;/complexContent>
- * &lt;/complexType>
+ * &lt;complexType name="WaitingForApprovalException&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="message" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *         &lt;element name="requestId" type="{http://www.w3.org/2001/XMLSchema}int"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
  * </pre>
  * 
  * 
