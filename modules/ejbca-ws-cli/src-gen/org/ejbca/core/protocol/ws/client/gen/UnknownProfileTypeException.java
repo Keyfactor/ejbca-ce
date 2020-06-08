3c69b5abee3ae6c2252f91fc0f02a5e4de663c9b
Mike Agrenius Kushner <mike.kushner@primekey.com>
2020-06-08 08:18:15 +0000

    ECA-9142: Added a missing quotation mark to the WS gen javadoc
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@35199 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ws-cli/src-gen/org/ejbca/core/protocol/ws/client/gen/UnknownProfileTypeException.java
+++ modules/ejbca-ws-cli/src-gen/org/ejbca/core/protocol/ws/client/gen/UnknownProfileTypeException.java
@@ -14,7 +14,7 @@ import javax.xml.bind.annotation.XmlType;
  * <pre>
  * &lt;complexType name="UnknownProfileTypeException"&gt;
  *   &lt;complexContent&gt;
- *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
  *       &lt;sequence&gt;
  *         &lt;element name="errorCode" type="{http://ws.protocol.core.ejbca.org/}errorCode" minOccurs="0"/&gt;
  *         &lt;element name="message" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
