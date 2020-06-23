d4eb2a7813f517cf1d13b492774f2997ca02b39b
Mike Agrenius Kushner <mike.kushner@primekey.com>
2020-06-23 20:39:29 +0000

    ECA-9121: Initial commit of SSH Certificates
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@35280 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-common/src/org/ejbca/util/JAXWSDocAndConvTools.java
+++ modules/ejbca-common/src/org/ejbca/util/JAXWSDocAndConvTools.java
@@ -548,9 +548,6 @@ public class JAXWSDocAndConvTools {
 					}
 				}
 			}
-			if(server.methods.get(s) == null) {
-			    System.err.println("foo: " + s);
-			}
 			String jdoc = server.methods.get(s).java_doc;
 			if (jdoc == null) { 
 				bad ("missing javadoc for " + s);
