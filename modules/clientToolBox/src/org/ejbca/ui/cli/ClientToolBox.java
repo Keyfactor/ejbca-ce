83697638bdca7f5100f096f6fc4659416b1b0eb0
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2020-06-24 12:29:55 +0000

    ECA-9265: add RESTTest stress test command to clientToolBox
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@35283 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/clientToolBox/src/org/ejbca/ui/cli/ClientToolBox.java
+++ modules/clientToolBox/src/org/ejbca/ui/cli/ClientToolBox.java
@@ -64,6 +64,7 @@ public abstract class ClientToolBox {
                 new CaIdGenerator(),
                 new JdbcTool(),
                 new UrlGenerator(),
+                new RESTTest(),
         };
         for ( int i=0; args.length>0 && i<toolBox.length; i++) {
             if ( toolBox[i].executeIfSelected(args) ) {
