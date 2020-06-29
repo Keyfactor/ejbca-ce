2ea90cf04172f0a46e75db5f9d63ff5345ba1080
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2020-06-29 07:45:56 +0000

    ECA-9023: Fix review comments
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@35299 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/upgrade/740_750/740_750-upgrade-derby.sql
+++ src/upgrade/740_750/740_750-upgrade-derby.sql
@@ -1,4 +1,4 @@
--- New columns in CertificateData are added by the JPA provider if there are sufficient privileges
+-- New columns in ApprovalData are added by the JPA provider if there are sufficient privileges
 -- if not added automatically the following SQL statements can be run to add the new columns 
 -- If you are using an older version of Derby, such as 10.2, you need to drop and re-create this table. See doc/howto/create-tables-ejbca-derby.sql for drop/create statements.
 -- ALTER TABLE ApprovalData ADD subjectDn VARCHAR(256);
--- src/upgrade/740_750/740_750-upgrade-sybase.sql
+++ src/upgrade/740_750/740_750-upgrade-sybase.sql
@@ -1,4 +1,4 @@
--- New columns in CertificateData are added by the JPA provider if there are sufficient privileges
+-- New columns in ApprovalData are added by the JPA provider if there are sufficient privileges
 -- if not added automatically the following SQL statements can be run to add the new columns 
 -- ALTER TABLE ApprovalData ADD subjectDn VARCHAR(255);
 -- ALTER TABLE ApprovalData ADD email VARCHAR(255);
