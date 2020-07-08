6d22117e17f0a29048370a7c72ea3fb16547460b
Mike Agrenius Kushner <mike.kushner@primekey.com>
2020-07-08 06:58:30 +0000

    ECA-9295: Changed the license header on all files under the ssh module
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/trunk/ejbca@35332 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ssh/src/org/ejbca/ssh/keys/rsa/SshRsaKeyPair.java
+++ modules/ssh/src/org/ejbca/ssh/keys/rsa/SshRsaKeyPair.java
@@ -1,13 +1,10 @@
 /*************************************************************************
  *                                                                       *
- *  EJBCA: The OpenSource Certificate Authority                          *
+ *  EJBCA - Proprietary Modules: Enterprise Certificate Authority        *
  *                                                                       *
- *  This software is free software; you can redistribute it and/or       *
- *  modify it under the terms of the GNU Lesser General Public           *
- *  License as published by the Free Software Foundation; either         *
- *  version 2.1 of the License, or any later version.                    *
- *                                                                       *
- *  See terms of license at gnu.org.                                     *
+ *  Copyright (c), PrimeKey Solutions AB. All rights reserved.           *
+ *  The use of the Proprietary Modules are subject to specific           * 
+ *  commercial license terms.                                            *
  *                                                                       *
  *************************************************************************/
 package org.ejbca.ssh.keys.rsa;
