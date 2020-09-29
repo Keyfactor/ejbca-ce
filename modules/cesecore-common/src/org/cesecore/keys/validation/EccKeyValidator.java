f66d28c48e7925cb7f7e2c2788e0b4bd5bc6b090
Henrik Sunmark <henrik.sunmark@primekey.com>
2020-09-29 16:18:52 +0200

    ECA-9031: Cleanup unused imports, throws declarations and parameters

--- modules/cesecore-common/src/org/cesecore/keys/validation/EccKeyValidator.java
+++ modules/cesecore-common/src/org/cesecore/keys/validation/EccKeyValidator.java
@@ -29,7 +29,6 @@ import org.cesecore.certificates.util.AlgorithmTools;
 import org.cesecore.profiles.Profile;
 import org.cesecore.util.ui.DynamicUiActionCallback;
 import org.cesecore.util.ui.DynamicUiCallbackException;
-import org.cesecore.util.ui.DynamicUiModel;
 import org.cesecore.util.ui.DynamicUiProperty;
 
 /**
