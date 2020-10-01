7b37c5ac48f4c479e9c0bc256394f826f63750fa
Bastian Fredriksson <bastian@primekey.com>
2020-10-01 16:16:05 +0200

    ECA-9280: Support for nested classes.
    Signed-off-by: Bastian Fredriksson <bastian@primekey.com>

--- modules/ejbca-common/src-test/org/ejbca/core/model/raadmin/userdatasource/UserDataVOTest.java
+++ modules/ejbca-common/src-test/org/ejbca/core/model/raadmin/userdatasource/UserDataVOTest.java
@@ -13,27 +13,53 @@
 
 package org.ejbca.core.model.raadmin.userdatasource;
 
+import org.apache.log4j.Logger;
+import org.cesecore.certificates.endentity.EndEntityType;
+import org.cesecore.certificates.endentity.EndEntityTypes;
+import org.cesecore.util.Base64;
 import org.cesecore.util.SecureXMLDecoder;
+import org.ejbca.core.model.ra.ExtendedInformation;
 import org.ejbca.core.model.ra.UserDataVO;
 import org.junit.Test;
 
 import java.beans.XMLEncoder;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
-import static org.junit.Assert.assertEquals;
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
 
 public class UserDataVOTest {
+    private final static Logger log = Logger.getLogger(UserDataVOTest.class);
+
     @Test
     public void testEncodeDecodeXml() throws Exception {
+        final Date date = new Date();
+        final EndEntityType endEntityType = new EndEntityType();
+        endEntityType.addType(EndEntityTypes.ENDUSER);
+        endEntityType.addType(EndEntityTypes.KEYRECOVERABLE);
         final ByteArrayOutputStream baos = new ByteArrayOutputStream();
         try (final XMLEncoder encoder = new XMLEncoder(baos)) {
             final UserDataVO userDataVO = new UserDataVO();
-            userDataVO.setDN("CN=test");
+            userDataVO.setType(endEntityType);
+            final ExtendedInformation extendedInformation = new ExtendedInformation();
+            extendedInformation.setCertificateSerialNumber(new BigInteger("123"));
+            userDataVO.setExtendedinformation(extendedInformation);
+            userDataVO.setTimeCreated(date);
+            userDataVO.setKeyRecoverable(true);
             encoder.writeObject(userDataVO);
         }
+        log.info(new String(baos.toByteArray()));
         final SecureXMLDecoder decoder = new SecureXMLDecoder(new ByteArrayInputStream(baos.toByteArray()));
         final UserDataVO userDataVO = (UserDataVO) decoder.readObject();
-        assertEquals("CN=test", userDataVO.getDN());
+        assertTrue(userDataVO.getType().contains(EndEntityTypes.ENDUSER));
+        assertTrue(userDataVO.getType().contains(EndEntityTypes.KEYRECOVERABLE));
+        assertEquals("123", new String(Base64.decode(userDataVO.getExtendedinformation().
+                getMapData("CERTIFICATESERIALNUMBER").getBytes(StandardCharsets.US_ASCII))));
+        assertEquals(date, userDataVO.getTimeCreated());
+        assertTrue(userDataVO.getKeyRecoverable());
     }
 }
