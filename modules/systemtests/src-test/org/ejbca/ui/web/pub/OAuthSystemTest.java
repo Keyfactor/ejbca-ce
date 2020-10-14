07c73503b58d12ecd24bf655ce4d3c1937b7db8d
Jekaterina Bunina <jekaterina.bunina.ext@primekey.com>
2020-10-14 14:55:04 +0300

    ECA-9431 enable requiere protocols

--- modules/systemtests/src-test/org/ejbca/ui/web/pub/OAuthSystemTest.java
+++ modules/systemtests/src-test/org/ejbca/ui/web/pub/OAuthSystemTest.java
@@ -41,6 +41,7 @@ import org.cesecore.roles.member.RoleMemberSessionRemote;
 import org.cesecore.util.CertTools;
 import org.cesecore.util.CryptoProviderTools;
 import org.cesecore.util.EjbRemoteHelper;
+import org.ejbca.config.AvailableProtocolsConfiguration;
 import org.ejbca.config.GlobalConfiguration;
 import org.ejbca.config.WebConfiguration;
 import org.ejbca.core.ejb.config.ConfigurationSessionRemote;
@@ -161,11 +162,14 @@ public class OAuthSystemTest {
     private static String token;
     private static String expiredToken;
     private static SSLSocketFactory defaultSocketFactory;
+    private static boolean isRestEnabled;
+    private static boolean isRaWebEnabled;
+    private static boolean isWsEnabled;
 
     @Rule
     public ExpectedException exceptionRule = ExpectedException.none();
     @ClassRule
-    public static TemporaryFolder folder = new TemporaryFolder();
+    public static final TemporaryFolder folder = new TemporaryFolder();
 
     @BeforeClass
     public static void beforeClass() throws NoSuchAlgorithmException, InvalidKeySpecException, AuthorizationDeniedException, RoleExistsException, CertificateException, OperatorCreationException, CryptoTokenOfflineException, KeyManagementException, KeyStoreException, IOException {
@@ -184,6 +188,16 @@ public class OAuthSystemTest {
         oAuthKeyInfoInternalId = oAuthKeyInfo.getInternalId();
         globalConfiguration.addOauthKey(oAuthKeyInfo);
         globalConfigSession.saveConfiguration(authenticationToken, globalConfiguration);
+
+        AvailableProtocolsConfiguration availableProtocolsConfiguration = (AvailableProtocolsConfiguration)
+                globalConfigSession.getCachedConfiguration(AvailableProtocolsConfiguration.CONFIGURATION_ID);
+        isRestEnabled = availableProtocolsConfiguration.getProtocolStatus(AvailableProtocolsConfiguration.AvailableProtocols.REST_CERTIFICATE_MANAGEMENT.getName());
+        isRaWebEnabled = availableProtocolsConfiguration.getProtocolStatus(AvailableProtocolsConfiguration.AvailableProtocols.RA_WEB.getName());
+        isWsEnabled = availableProtocolsConfiguration.getProtocolStatus(AvailableProtocolsConfiguration.AvailableProtocols.WS.getName());
+        availableProtocolsConfiguration.setProtocolStatus(AvailableProtocolsConfiguration.AvailableProtocols.REST_CERTIFICATE_MANAGEMENT.getName(), true);
+        availableProtocolsConfiguration.setProtocolStatus(AvailableProtocolsConfiguration.AvailableProtocols.RA_WEB.getName(), true);
+        availableProtocolsConfiguration.setProtocolStatus(AvailableProtocolsConfiguration.AvailableProtocols.WS.getName(), true);
+        globalConfigSession.saveConfiguration(authenticationToken, availableProtocolsConfiguration);
 
         final int keyusage = X509KeyUsage.digitalSignature + X509KeyUsage.keyCertSign + X509KeyUsage.cRLSign;
         adminca = CaTestUtils.createTestX509CA("CN=" + CA, "foo123".toCharArray(), false, keyusage);
@@ -226,6 +240,12 @@ public class OAuthSystemTest {
             roleSession.deleteRoleIdempotent(authenticationToken, roleMember.getRoleId());
         }
         HttpsURLConnection.setDefaultSSLSocketFactory(defaultSocketFactory);
+        AvailableProtocolsConfiguration availableProtocolsConfiguration = (AvailableProtocolsConfiguration)
+                globalConfigSession.getCachedConfiguration(AvailableProtocolsConfiguration.CONFIGURATION_ID);
+        availableProtocolsConfiguration.setProtocolStatus(AvailableProtocolsConfiguration.AvailableProtocols.REST_CERTIFICATE_MANAGEMENT.getName(), isRestEnabled);
+        availableProtocolsConfiguration.setProtocolStatus(AvailableProtocolsConfiguration.AvailableProtocols.RA_WEB.getName(), isRaWebEnabled);
+        availableProtocolsConfiguration.setProtocolStatus(AvailableProtocolsConfiguration.AvailableProtocols.WS.getName(), isWsEnabled);
+        globalConfigSession.saveConfiguration(authenticationToken, availableProtocolsConfiguration);
     }
 
     private static String encodeToken(final String headerJson, final String payloadJson, final PrivateKey key) {
