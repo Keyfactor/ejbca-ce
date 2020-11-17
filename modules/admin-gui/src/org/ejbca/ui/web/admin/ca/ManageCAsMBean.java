11e87789b0113d5eacdcf17bab1f6ea46daaa3db
Henrik Sunmark <henrik.sunmark@primekey.com>
2020-11-17 10:47:51 +0100

    ECA-8705: Added missing imports

--- modules/admin-gui/src/org/ejbca/ui/web/admin/ca/ManageCAsMBean.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/ca/ManageCAsMBean.java
@@ -13,7 +13,10 @@
 package org.ejbca.ui.web.admin.ca;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
