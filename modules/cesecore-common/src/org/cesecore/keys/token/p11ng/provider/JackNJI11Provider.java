ced82a25877111df57ecb0f6b384ef1088095a5b
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2020-12-28 17:15:06 +0100

    ECA-9489: implement unwrap of AES keys using RSA unwrapping keys

--- modules/cesecore-common/src/org/cesecore/keys/token/p11ng/provider/JackNJI11Provider.java
+++ modules/cesecore-common/src/org/cesecore/keys/token/p11ng/provider/JackNJI11Provider.java
@@ -13,9 +13,11 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.math.BigInteger;
+import java.security.AlgorithmParameters;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.InvalidParameterException;
+import java.security.Key;
 import java.security.MessageDigestSpi;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
@@ -23,10 +25,20 @@ import java.security.PrivateKey;
 import java.security.Provider;
 import java.security.ProviderException;
 import java.security.PublicKey;
+import java.security.SecureRandom;
 import java.security.SignatureException;
 import java.security.SignatureSpi;
 import java.security.spec.AlgorithmParameterSpec;
 import java.util.Arrays;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.CipherSpi;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.ShortBufferException;
+import javax.crypto.spec.SecretKeySpec;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
@@ -35,9 +47,14 @@ import org.bouncycastle.asn1.DERSequenceGenerator;
 import org.bouncycastle.jcajce.provider.asymmetric.rsa.AlgorithmParametersSpi.PSS;
 import org.cesecore.certificates.util.AlgorithmConstants;
 import org.cesecore.certificates.util.AlgorithmTools;
+import org.cesecore.keys.token.AzureProvider.AzureCipher;
 import org.cesecore.keys.token.p11ng.MechanismNames;
 import org.cesecore.util.StringTools;
+import org.pkcs11.jacknji11.CKA;
+import org.pkcs11.jacknji11.CKK;
 import org.pkcs11.jacknji11.CKM;
+import org.pkcs11.jacknji11.CKO;
+import org.pkcs11.jacknji11.CKRException;
 
 /**
  * Provider using JackNJI11.
@@ -79,6 +96,9 @@ public class JackNJI11Provider extends Provider {
         putService(new MySigningService(this, "MessageDigest", "SHA384", MyMessageDigiest.class.getName()));
         putService(new MySigningService(this, "MessageDigest", "SHA512", MyMessageDigiest.class.getName()));
         putService(new MySigningService(this, "AlgorithmParameters", "PSS", MyAlgorithmParameters.class.getName()));
+        putService(new MySigningService(this, "Cipher", "RSAEncryption", MyCipher.class.getName()));
+        putService(new MySigningService(this, "Cipher", "1.2.840.113549.1.1.1", MyCipher.class.getName()));
+        // 1.2.840.113549.1.1.1
     }
 
     private static class MyService extends Service {
@@ -192,7 +212,7 @@ public class JackNJI11Provider extends Provider {
         @Override
         protected void engineInitSign(PrivateKey pk) throws InvalidKeyException {
             if (!(pk instanceof NJI11Object)) {
-                throw new InvalidKeyException("Not an NJI11Object: " + pk);
+                throw new InvalidKeyException("Not a NJI11Object: " + pk.getClass().getName());
             }
             myKey = (NJI11Object) pk;
             
@@ -409,4 +429,241 @@ public class JackNJI11Provider extends Provider {
             throw new UnsupportedOperationException("Not supported yet.");
         }
     }
+    
+    /**
+     * A Java Cipher provider for decrypting small values (key unwrapping) with JackNJI11 PKCS#11. Only does two types of "engineDoInit and engineDoFinal"
+     */
+    public static class MyCipher extends CipherSpi {
+
+        private static final Logger log = Logger.getLogger(AzureCipher.class);
+
+        private int opmode;
+        private String algorithm;
+        private NJI11Object myKey;
+        private long session;
+        private ByteArrayOutputStream buffer;
+        private boolean hasActiveSession;
+        private Exception debugStacktrace;
+
+        public MyCipher(Provider provider, String algorithm) {
+            super();
+            this.algorithm = algorithm;
+            if (log.isTraceEnabled()) {
+                log.info("Creating Cipher provider for algorithm: " + algorithm + ", and provider: " + provider);
+            }
+        }
+
+        @Override
+        protected byte[] engineUpdate(byte[] b, int off, int len) {
+            if (log.isDebugEnabled()) {
+                log.debug("engineUpdate1: " + this.getClass().getName());
+            }
+            return null;
+        }
+        
+        @Override
+        protected Key engineUnwrap(byte[] wrappedKey, String wrappedKeyAlgorithm, int wrappedKeyType)
+                throws InvalidKeyException, NoSuchAlgorithmException {
+            if (log.isDebugEnabled()) {
+                log.debug("engineUnwrap: " + this.getClass().getName() + ", " + this.opmode + ", " + myKey.getClass().getName()
+                        + ", " + wrappedKeyAlgorithm + ", " + wrappedKeyType);
+            }
+            if (!StringUtils.equalsIgnoreCase("AES", wrappedKeyAlgorithm)) {
+                throw new UnsupportedOperationException("Only AES keys can be unwrapped at this moment");
+            }
+            final long keyType = CKK.AES;
+            long seckey = 0; // handle to remove if we managed to create a key
+            try {
+                // We need to provide a full set of attributes for the secret key in order to unwrap it inside the HSM
+                // Unwrapping is done with the RSA private key, i.e. the secret key is never exposed unencrypted outside
+                // of the HSM (if we had generated the secret key with CKA.EXTRACTABLE=false that is)
+                CKA[] secTemplUnwrap = new CKA[] {
+                        new CKA(CKA.CLASS, CKO.SECRET_KEY),
+                        new CKA(CKA.KEY_TYPE, keyType),
+                        // We don't need a label and id for this key, we wil destroy it just efter we read it
+                        //new CKA(CKA.LABEL, "labelunwrap"), 
+                        //new CKA(CKA.ID, "labelunwrap"), 
+                        new CKA(CKA.TOKEN, false),
+                        new CKA(CKA.SENSITIVE, false),
+                        new CKA(CKA.EXTRACTABLE, true),
+                        new CKA(CKA.ENCRYPT, true),
+                        new CKA(CKA.DECRYPT, true),
+                        new CKA(CKA.DERIVE, true),
+                };
+                
+                long mechanism = MechanismNames.longFromEncAlgoName(this.algorithm).get();
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("engineUnwrap: session: " + session + ", object: " +
+                            myKey.getObject() + ", algoValue: 0x" + Long.toHexString(mechanism) + ", param: null");
+                    debugStacktrace = new Exception();
+                }
+
+                seckey = myKey.getSlot().getCryptoki().UnwrapKey(session, new CKM(mechanism), myKey.getObject(), wrappedKey, secTemplUnwrap);
+                byte[] seckeybuf = myKey.getSlot().getCryptoki().GetAttributeValue(session, seckey, CKA.VALUE).getValue();
+                // Get AES key from byte array
+                SecretKey key = new SecretKeySpec(seckeybuf, wrappedKeyAlgorithm);
+                return key;
+            } finally {
+                // Destroy the secret key object so it doesn't linger around in the PKCS#11 session
+                if (seckey != 0) {
+                    try {
+                        myKey.getSlot().getCryptoki().DestroyObject(session, seckey);
+                    } catch (CKRException e) {
+                        log.error("Can not destroy temporary secret key object: " + e.getCKR() + ": " + e.getMessage());
+                    }
+                }
+                // Decryption is done, either successful or failed
+                if (myKey instanceof NJI11ReleasebleSessionPrivateKey) {
+                    myKey.getSlot().releaseSession(session);
+                    hasActiveSession = false;
+                }
+            }
+        }
+        
+        @Override
+        protected int engineUpdate(byte[] arg0, int arg1, int arg2, byte[] arg3, int arg4) throws ShortBufferException {
+            if (log.isDebugEnabled()) {
+                log.debug("engineUpdate2: " + this.getClass().getName());
+            }
+            return 0;
+        }
+
+        @Override
+        protected byte[] engineDoFinal(byte[] arg0, int arg1, int arg2) throws IllegalBlockSizeException, BadPaddingException {
+            if (log.isDebugEnabled()) {
+                log.debug("engineDoFinal1: " + this.getClass().getName() + ", opmode=" + this.opmode);
+            }
+            return null;
+        }
+
+        @Override
+        protected int engineDoFinal(byte[] arg0, int arg1, int arg2, byte[] arg3, int arg4)
+                throws ShortBufferException, IllegalBlockSizeException, BadPaddingException {
+            if (log.isDebugEnabled()) {
+                log.debug("engineDoFinal2: " + this.getClass().getName());
+            }
+            return 0;
+        }
+
+        @Override
+        protected int engineGetBlockSize() {
+            if (log.isDebugEnabled()) {
+                log.debug("engineGetBlockSize: " + this.getClass().getName());
+            }
+            return 0;
+        }
+
+        @Override
+        protected byte[] engineGetIV() {
+            if (log.isDebugEnabled()) {
+                log.debug("engineGetIV: " + this.getClass().getName());
+            }
+            return null;
+        }
+
+        @Override
+        protected int engineGetOutputSize(int arg0) {
+            if (log.isDebugEnabled()) {
+                log.debug("engineGetOutputSize: " + this.getClass().getName());
+            }
+            return 0;
+        }
+
+        @Override
+        protected AlgorithmParameters engineGetParameters() {
+            if (log.isDebugEnabled()) {
+                log.debug("engineGetParameters: " + this.getClass().getName());
+            }
+            return null;
+        }
+
+        @Override
+        protected void engineInit(int opmode, Key key, SecureRandom random) throws InvalidKeyException {
+            if (log.isDebugEnabled()) {
+                log.debug("engineInit1: " + this.getClass().getName() + ", " + opmode);
+            }
+            this.opmode = opmode;
+            if (this.opmode != Cipher.UNWRAP_MODE) {
+                throw new UnsupportedOperationException("Only UNWRAP_MODE (4) can be used: " + opmode);
+            }
+            
+            if (!(key instanceof NJI11Object)) {
+                throw new InvalidKeyException("Not a NJI11Object: " + key.getClass().getName());
+            }
+            myKey = (NJI11Object) key;
+            
+            if (key instanceof NJI11StaticSessionPrivateKey) {
+                session = ((NJI11StaticSessionPrivateKey) key).getSession();
+            } else {
+                session = myKey.getSlot().aquireSession();
+                hasActiveSession = true;
+            }
+            try {
+                if (!MechanismNames.longFromEncAlgoName(this.algorithm).isPresent()) {
+                    final String message = "The cipher algorithm " + algorithm + " is not supported by P11NG.";
+                    log.error(message);
+                    throw new InvalidKeyException("The cipher algorithm " + algorithm + " is not supported by P11NG.");
+                }
+                log.debug("C_EncryptInit with algorithm " + this.algorithm + " successful.");
+            } catch (Exception e) {
+                log.error("An exception occurred when calling C_EncryptInit: " + e.getMessage());
+                if (myKey instanceof NJI11ReleasebleSessionPrivateKey) {
+                    myKey.getSlot().releaseSession(session);
+                    hasActiveSession = false;
+                }
+                throw e;
+            }
+        }
+
+        @Override
+        protected void engineInit(int opmode, Key arg1, AlgorithmParameterSpec arg2, SecureRandom arg3)
+                throws InvalidKeyException, InvalidAlgorithmParameterException {
+            if (log.isDebugEnabled()) {
+                log.debug("engineInit2: " + this.getClass().getName());
+            }
+        }
+
+        @Override
+        protected void engineInit(int opmode, Key arg1, AlgorithmParameters arg2, SecureRandom arg3)
+                throws InvalidKeyException, InvalidAlgorithmParameterException {
+            if (log.isDebugEnabled()) {
+                log.debug("engineInit3: " + this.getClass().getName());
+            }
+        }
+
+        @Override
+        protected void engineSetMode(String mode) throws NoSuchAlgorithmException {
+            if (log.isDebugEnabled()) {
+                log.debug("engineSetMode: " + this.getClass().getName() + ", " + mode);
+            }
+        }
+
+        @Override
+        protected void engineSetPadding(String arg0) throws NoSuchPaddingException {
+            if (log.isDebugEnabled()) {
+                log.debug("engineSetPadding: " + this.getClass().getName());
+            }
+        }
+
+        @Override
+        protected void finalize() throws Throwable {
+            try {
+                if (hasActiveSession) {
+                    log.warn("Decryption object was not de-initialized. Enable debug logging to see init stack trace", debugStacktrace);
+                    try {
+                        final CryptokiDevice.Slot slot = myKey.getSlot();
+                        if (slot != null) {
+                            slot.releaseSession(session);
+                        }
+                    } catch (RuntimeException e) {
+                        // Can't do anything
+                        log.warn("Failed to release PKCS#11 session in finalizer");
+                    }
+                }
+            } finally {
+                super.finalize();
+            }
+        }
+    }
+    
 }
