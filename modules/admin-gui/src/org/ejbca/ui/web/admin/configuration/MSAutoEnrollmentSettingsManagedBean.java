59e88b3adb8ffddd7db2f2e631d36d11f2780305
Serkan Ongan <serkan.ongan.ext@primekey.com>
2021-01-19 15:56:20 +0200

    ECA-9732: Add OID to UI

--- modules/admin-gui/src/org/ejbca/ui/web/admin/configuration/MSAutoEnrollmentSettingsManagedBean.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/configuration/MSAutoEnrollmentSettingsManagedBean.java
@@ -2,14 +2,25 @@ package org.ejbca.ui.web.admin.configuration;
 
 import org.apache.log4j.Logger;
 import org.cesecore.authorization.AuthorizationDeniedException;
+import org.cesecore.certificates.certificateprofile.CertificateProfile;
+import org.cesecore.certificates.certificateprofile.CertificateProfileSessionLocal;
+import org.cesecore.config.MSAutoEnrollmentOIDInfo;
 import org.cesecore.configuration.GlobalConfigurationSessionLocal;
 import org.ejbca.config.MSAutoEnrollmentConfiguration;
+import org.ejbca.core.ejb.ra.raadmin.EndEntityProfileSessionLocal;
 import org.ejbca.ui.web.admin.BaseManagedBean;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.model.ListDataModel;
+import javax.faces.model.SelectItem;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 /**
  * Backing bean for MSAutoEnrollmentConfiguration in System Settings.
@@ -19,6 +30,10 @@ import javax.faces.bean.ViewScoped;
 public class MSAutoEnrollmentSettingsManagedBean extends BaseManagedBean {
     private static final Logger log = Logger.getLogger(MSAutoEnrollmentSettingsManagedBean.class);
     private static final long serialVersionUID = 1L;
+
+    private static String SELECT_CEP = "Select a Certificate Profile";
+    private static String SELECT_EEP = "Select an End Entity Profile";
+    private static String SELECT_MST = "Select a Template";
 
     // MSAE Kerberos Settings
     private String msaeDomain;
@@ -35,6 +50,22 @@ public class MSAutoEnrollmentSettingsManagedBean extends BaseManagedBean {
     private String trustedKeyStorePath;
     private String trustedKeyStorePassword;
     private String caName;
+
+    // MS Template Settings
+    private List<MSAutoEnrollmentOIDInfo> msTemplateSettings;
+    private ListDataModel<MSAutoEnrollmentOIDInfo> msTemplateSettingsModel;
+
+    private String selectedTemplateSettingOID;
+    private String selectedCertificationProfile;
+    private Integer selectedCertificationProfileId;
+    private String selectedEndEntityProfile;
+
+    private List<SelectItem> availableTemplateSettingOIDs;
+    private List<SelectItem> availableCertificationProfiles;
+    private List<SelectItem> availableEndEntityProfiles;
+
+    private final CertificateProfileSessionLocal certificateProfileSession = getEjbcaWebBean().getEjb().getCertificateProfileSession();
+    private final EndEntityProfileSessionLocal endEntityProfileSession = getEjbcaWebBean().getEjb().getEndEntityProfileSession();
 
     @EJB
     private GlobalConfigurationSessionLocal globalConfigurationSession;
@@ -59,7 +90,19 @@ public class MSAutoEnrollmentSettingsManagedBean extends BaseManagedBean {
             trustedKeyStorePath = autoEnrollmentConfiguration.getTrustedKeyStorePath();
             trustedKeyStorePassword = autoEnrollmentConfiguration.getTrustedKeyStorePassword();
             caName = autoEnrollmentConfiguration.getCaName();
+
+            msTemplateSettings = autoEnrollmentConfiguration.getMsTemplateSettings();
         }
+    }
+
+    //TODO: Remove
+    private void addRandomOID(){
+        MSAutoEnrollmentOIDInfo oid = new MSAutoEnrollmentOIDInfo();
+        oid.setOid("OID" + System.nanoTime());
+        oid.setCertificationProfile("CEP " + System.nanoTime());
+        oid.setEndEntityProfile("EEP " + System.nanoTime());
+
+        msTemplateSettings.add(oid);
     }
 
     // MSAE Kerberos Settings
@@ -145,6 +188,195 @@ public class MSAutoEnrollmentSettingsManagedBean extends BaseManagedBean {
         this.caName = caName;
     }
 
+    // MS Template Settings
+    public List<MSAutoEnrollmentOIDInfo> getMsTemplateSettings() {
+        return msTemplateSettings;
+    }
+
+    public ListDataModel<MSAutoEnrollmentOIDInfo> getMsTemplateSettingsModel() {
+        if (msTemplateSettingsModel == null) {
+            msTemplateSettingsModel = new ListDataModel<>(getMsTemplateSettings());
+        }
+
+        return msTemplateSettingsModel;
+    }
+
+    public void setMsTemplateSettings(List<MSAutoEnrollmentOIDInfo> msTemplateSettings) {
+        this.msTemplateSettings = msTemplateSettings;
+    }
+
+    public void removeMsTemplateSettingFromModel(){
+        MSAutoEnrollmentOIDInfo templateToRemove = msTemplateSettingsModel.getRowData();
+        removeMsTemplateSettings(templateToRemove);
+    }
+
+    public void removeMsTemplateSettings(MSAutoEnrollmentOIDInfo oidInfo) {
+        msTemplateSettings.remove(oidInfo);
+        msTemplateSettingsModel = new ListDataModel<>(getMsTemplateSettings());
+    }
+
+    public void addMsTemplateSettingToModel() {
+        // If a template is already mapped, it should be removed first.
+        if (msTemplateSettingExists(msTemplateSettings, selectedTemplateSettingOID) != null) {
+            addErrorMessage("MSAE_ERROR_TEMPLATE_ALREADY_ADDED");
+            return;
+        }
+
+        if (selectedTemplateSettingOID.equals(SELECT_MST)) {
+            addErrorMessage("MSAE_ERROR_TEMPLATE");
+            return;
+        }
+
+        if (selectedCertificationProfileId == null || selectedCertificationProfileId == -1) {
+            addErrorMessage("MSAE_ERROR_CEP");
+            return;
+        }
+
+        if (selectedEndEntityProfile == null || selectedEndEntityProfile.equals(SELECT_EEP)) {
+            addErrorMessage("MSAE_ERROR_EEP");
+            return;
+        }
+
+        selectedCertificationProfile = certificateProfileSession.getCertificateProfileName(selectedCertificationProfileId);
+        if (selectedCertificationProfile == null) {
+            addErrorMessage("MSAE_ERROR_CEP_NAME");
+            return;
+        }
+
+        addMsTemplateSetting(selectedTemplateSettingOID, selectedEndEntityProfile, selectedCertificationProfile);
+    }
+
+    public void addMsTemplateSetting(final String templateOid, final String certProfile, final String eep) {
+        List<MSAutoEnrollmentOIDInfo> adTemplates = getAvailableTemplateSettingsFromAD();
+        MSAutoEnrollmentOIDInfo template = msTemplateSettingExists(adTemplates, templateOid);
+
+        if (template != null) {
+            template.setCertificationProfile(certProfile);
+            template.setEndEntityProfile(eep);
+            msTemplateSettings.add(template);
+            msTemplateSettingsModel = new ListDataModel<>(getMsTemplateSettings());
+        } else {
+            addErrorMessage("MSAE_TEMPLATE_NOT_FOUND");
+        }
+    }
+
+    private MSAutoEnrollmentOIDInfo msTemplateSettingExists(List<MSAutoEnrollmentOIDInfo> templates, final String templatedOid) {
+        for (MSAutoEnrollmentOIDInfo template: templates) {
+            if (template.getOid().equals(templatedOid)) {
+                return template;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Return available MS Template Settings from Active Directory.
+     *
+     * @return
+     */
+    public List<MSAutoEnrollmentOIDInfo> getAvailableTemplateSettingsFromAD() {
+        // TODO: Implement
+        List<MSAutoEnrollmentOIDInfo> templates = new ArrayList<>();
+
+        MSAutoEnrollmentOIDInfo oid1 = new MSAutoEnrollmentOIDInfo();
+        oid1.setOid("Template 1");
+        templates.add(oid1);
+
+        MSAutoEnrollmentOIDInfo oid2 = new MSAutoEnrollmentOIDInfo();
+        oid2.setOid("Template 2");
+        templates.add(oid2);
+
+        MSAutoEnrollmentOIDInfo oid3 = new MSAutoEnrollmentOIDInfo();
+        oid3.setOid("Template 3");
+        templates.add(oid3);
+
+        return templates;
+    }
+
+    public List<SelectItem> getAvailableTemplateSettingOIDs() {
+        availableTemplateSettingOIDs = new ArrayList<>();
+        availableTemplateSettingOIDs.add(new SelectItem(SELECT_MST));
+
+        for (MSAutoEnrollmentOIDInfo template: getAvailableTemplateSettingsFromAD()) {
+            availableTemplateSettingOIDs.add(new SelectItem(template.getOid()));
+        }
+
+        return availableTemplateSettingOIDs;
+    }
+
+    private TreeMap<String, Integer> getAllCertificateProfiles() {
+        final TreeMap<String, Integer> eecertificateprofilenames = getEjbcaWebBean().getAuthorizedEndEntityCertificateProfileNames();
+        final TreeMap<String, Integer> subcacertificateprofilenames = getEjbcaWebBean().getAuthorizedSubCACertificateProfileNames();
+        final TreeMap<String, Integer> sshcertificateprofilenames = getEjbcaWebBean().getAuthorizedSshCertificateProfileNames();
+        final TreeMap<String, Integer> mergedMap = new TreeMap<>();
+
+        mergedMap.putAll(eecertificateprofilenames);
+        mergedMap.putAll(subcacertificateprofilenames);
+        mergedMap.putAll(sshcertificateprofilenames);
+
+        return mergedMap;
+    }
+
+    public List<SelectItem> getAvailableCertificationProfiles() {
+        availableCertificationProfiles = new ArrayList<>();
+        availableCertificationProfiles.add(new SelectItem(-1, SELECT_CEP));
+
+        for (final Map.Entry<String,Integer> entry : getAllCertificateProfiles().entrySet()) {
+            final int certProfileId = entry.getValue(); // map is inverted
+            final String certProfileName = entry.getKey();
+            availableCertificationProfiles.add(new SelectItem(certProfileId, certProfileName));
+        }
+
+        return availableCertificationProfiles;
+    }
+
+    public List<SelectItem> getAvailableEndEntityProfiles() {
+        availableEndEntityProfiles = new ArrayList<>();
+        availableEndEntityProfiles.add(new SelectItem(SELECT_EEP));
+
+        if (selectedCertificationProfileId != null){
+            List<String> eepNames = endEntityProfileSession.getEndEntityProfilesUsingCertificateProfile(selectedCertificationProfileId);
+            for (String eepName: eepNames) {
+                availableEndEntityProfiles.add(new SelectItem(eepName));
+            }
+        }
+
+        return availableEndEntityProfiles;
+    }
+
+    public String getSelectedTemplateSettingOID() {
+        return selectedTemplateSettingOID;
+    }
+
+    public void setSelectedTemplateSettingOID(String selectedTemplateSettingOID) {
+        this.selectedTemplateSettingOID = selectedTemplateSettingOID;
+    }
+
+    public String getSelectedCertificationProfile() {
+        return selectedCertificationProfile;
+    }
+
+    public void setSelectedCertificationProfile(String selectedCertificationProfile) {
+        this.selectedCertificationProfile = selectedCertificationProfile;
+    }
+
+    public Integer getSelectedCertificationProfileId() {
+        return selectedCertificationProfileId;
+    }
+
+    public void setSelectedCertificationProfileId(Integer selectedCertificationProfileId) {
+        this.selectedCertificationProfileId = selectedCertificationProfileId;
+    }
+
+    public String getSelectedEndEntityProfile() {
+        return selectedEndEntityProfile;
+    }
+
+    public void setSelectedEndEntityProfile(String selectedEndEntityProfile) {
+        this.selectedEndEntityProfile = selectedEndEntityProfile;
+    }
+
     public void save() {
         try {
             final MSAutoEnrollmentConfiguration autoEnrollmentConfiguration = (MSAutoEnrollmentConfiguration)
@@ -165,6 +397,9 @@ public class MSAutoEnrollmentSettingsManagedBean extends BaseManagedBean {
             autoEnrollmentConfiguration.setTrustedKeyStorePath(trustedKeyStorePath);
             autoEnrollmentConfiguration.setTrustedKeyStorePassword(trustedKeyStorePassword);
             autoEnrollmentConfiguration.setCaName(caName);
+
+            // MS Template Settings
+            autoEnrollmentConfiguration.setMsTemplateSettings(msTemplateSettings);
 
             globalConfigurationSession.saveConfiguration(getAdmin(), autoEnrollmentConfiguration);
             addInfoMessage("CONFIGURATION_AUTOENROLLMENT_SAVE_OK");
---
+++ modules/cesecore-common/src/org/cesecore/config/MSAutoEnrollmentOIDInfo.java
@@ -0,0 +1,188 @@
+package org.cesecore.config;
+
+import java.io.Serializable;
+
+public class MSAutoEnrollmentOIDInfo implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private boolean isUsed;
+    private String oid;
+    private String certificationProfile;
+    private String endEntityProfile;
+    private String subjectNameFormat; // TODO: Enum?
+    private boolean includeEmailInSubjectDN;
+    private boolean includeEmailInSubjectSAN;
+    private boolean includeUPNInSubjectSAN;
+    private boolean includeSPNInSubjectSAN;
+    private boolean includeNetBiosInSubjectSAN;
+    private boolean includeDomainInSubjectSAN;
+    private boolean includeObjectGuidInSubjectSAN;
+    private String additionalSubjectDNAttributes;
+    private boolean publishToActiveDirectory;
+
+    public MSAutoEnrollmentOIDInfo() {
+        init();
+    }
+
+    private void init() {
+        setUsed(false);
+        setOid("");
+        setCertificationProfile("");
+        setEndEntityProfile("");
+        setSubjectNameFormat("");
+        setIncludeEmailInSubjectDN(false);
+        setIncludeEmailInSubjectSAN(false);
+        setIncludeUPNInSubjectSAN(false);
+        setIncludeSPNInSubjectSAN(false);
+        setIncludeNetBiosInSubjectSAN(false);
+        setIncludeDomainInSubjectSAN(false);
+        setIncludeObjectGuidInSubjectSAN(false);
+        setAdditionalSubjectDNAttributes("");
+        setPublishToActiveDirectory(false);
+    }
+
+    // Getters and Setters
+    public boolean isUsed() {
+        return isUsed;
+    }
+
+    public void setUsed(boolean used) {
+        isUsed = used;
+    }
+
+    public String getOid() {
+        return oid;
+    }
+
+    public void setOid(String oid) {
+        this.oid = oid;
+    }
+
+    public String getCertificationProfile() {
+        return certificationProfile;
+    }
+
+    public void setCertificationProfile(String certificationProfile) {
+        this.certificationProfile = certificationProfile;
+    }
+
+    public String getEndEntityProfile() {
+        return endEntityProfile;
+    }
+
+    public void setEndEntityProfile(String endEntityProfile) {
+        this.endEntityProfile = endEntityProfile;
+    }
+
+    public String getSubjectNameFormat() {
+        return subjectNameFormat;
+    }
+
+    public void setSubjectNameFormat(String subjectNameFormat) {
+        this.subjectNameFormat = subjectNameFormat;
+    }
+
+    public boolean isIncludeEmailInSubjectDN() {
+        return includeEmailInSubjectDN;
+    }
+
+    public void setIncludeEmailInSubjectDN(boolean includeEmailInSubjectDN) {
+        this.includeEmailInSubjectDN = includeEmailInSubjectDN;
+    }
+
+    public boolean isIncludeEmailInSubjectSAN() {
+        return includeEmailInSubjectSAN;
+    }
+
+    public void setIncludeEmailInSubjectSAN(boolean includeEmailInSubjectSAN) {
+        this.includeEmailInSubjectSAN = includeEmailInSubjectSAN;
+    }
+
+    public boolean isIncludeUPNInSubjectSAN() {
+        return includeUPNInSubjectSAN;
+    }
+
+    public void setIncludeUPNInSubjectSAN(boolean includeUPNInSubjectSAN) {
+        this.includeUPNInSubjectSAN = includeUPNInSubjectSAN;
+    }
+
+    public boolean isIncludeSPNInSubjectSAN() {
+        return includeSPNInSubjectSAN;
+    }
+
+    public void setIncludeSPNInSubjectSAN(boolean includeSPNInSubjectSAN) {
+        this.includeSPNInSubjectSAN = includeSPNInSubjectSAN;
+    }
+
+    public boolean isIncludeNetBiosInSubjectSAN() {
+        return includeNetBiosInSubjectSAN;
+    }
+
+    public void setIncludeNetBiosInSubjectSAN(boolean includeNetBiosInSubjectSAN) {
+        this.includeNetBiosInSubjectSAN = includeNetBiosInSubjectSAN;
+    }
+
+    public boolean isIncludeDomainInSubjectSAN() {
+        return includeDomainInSubjectSAN;
+    }
+
+    public void setIncludeDomainInSubjectSAN(boolean includeDomainInSubjectSAN) {
+        this.includeDomainInSubjectSAN = includeDomainInSubjectSAN;
+    }
+
+    public boolean isIncludeObjectGuidInSubjectSAN() {
+        return includeObjectGuidInSubjectSAN;
+    }
+
+    public void setIncludeObjectGuidInSubjectSAN(boolean includeObjectGuidInSubjectSAN) {
+        this.includeObjectGuidInSubjectSAN = includeObjectGuidInSubjectSAN;
+    }
+
+    public String isAdditionalSubjectDNAttributes() {
+        return additionalSubjectDNAttributes;
+    }
+
+    public void setAdditionalSubjectDNAttributes(String additionalSubjectDNAttributes) {
+        this.additionalSubjectDNAttributes = additionalSubjectDNAttributes;
+    }
+
+    public boolean isPublishToActiveDirectory() {
+        return publishToActiveDirectory;
+    }
+
+    public void setPublishToActiveDirectory(boolean publishToActiveDirectory) {
+        this.publishToActiveDirectory = publishToActiveDirectory;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((oid == null) ? 0 : oid.hashCode());
+        result = prime * result + ((oid == null) ? 0 : oid.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        MSAutoEnrollmentOIDInfo other = (MSAutoEnrollmentOIDInfo) obj;
+        if (!oid.equals(other.oid)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return getOid();
+    }
+}
