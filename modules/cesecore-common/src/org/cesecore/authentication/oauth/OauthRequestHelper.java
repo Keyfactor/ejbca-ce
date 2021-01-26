2dd466163cff29cd0fd3228d407a67bbb1b0e75a
Jekaterina Bunina <jekaterina.bunina.ext@primekey.com>
2021-01-26 14:17:07 +0200

    ECA-9747 refresh token in Admin web

---
+++ modules/cesecore-common/src/org/cesecore/authentication/oauth/OauthRequestHelper.java
@@ -0,0 +1,41 @@
+package org.cesecore.authentication.oauth;
+
+import java.io.IOException;
+
+/**
+ * Helper, sends requests to oauth token providers to exchange code to token or to refresh token
+ */
+public class OauthRequestHelper {
+
+    /**
+     * Exchanges code to token
+     * @param oAuthKeyInfo provider configurations
+     * @param code code received from oauth provider after clients login
+     * @param redirectUri ejbca url, used by provider to redirect response
+     * @return OAuthGrantResponseInfo oauth provider response
+     * @throws IOException
+     */
+    public static OAuthGrantResponseInfo sendTokenRequest( OAuthKeyInfo oAuthKeyInfo, String code, String redirectUri ) throws IOException {
+        return sendRequest(code, false, oAuthKeyInfo, redirectUri);
+    }
+
+    /**
+     * Exchanges refresh to authentication token
+     * @param oAuthKeyInfo provider configurations
+     * @param refreshToken refresh token received from oauth provider with previous token, what got expired
+     * @param redirectUri ejbca url, used by provider to redirect response
+     * @return OAuthGrantResponseInfo oauth provider response
+     * @throws IOException
+     */
+    public static OAuthGrantResponseInfo sendRefreshTokenRequest(String refreshToken, OAuthKeyInfo oAuthKeyInfo, String redirectUri) throws IOException {
+        return sendRequest(refreshToken, true, oAuthKeyInfo, redirectUri);
+    }
+
+    private static OAuthGrantResponseInfo sendRequest(String codeOrToken, boolean isRefresh, OAuthKeyInfo oAuthKeyInfo, String redirectUri) throws IOException {
+        final OAuthTokenRequest request = new OAuthTokenRequest();
+        request.setUri(oAuthKeyInfo.getUrl() + "/realms/" + oAuthKeyInfo.getRealm() + "/protocol/openid-connect/token");
+        request.setClientId(oAuthKeyInfo.getClient());
+        request.setRedirectUri(redirectUri);
+        return request.execute(codeOrToken, isRefresh);
+    }
+}
