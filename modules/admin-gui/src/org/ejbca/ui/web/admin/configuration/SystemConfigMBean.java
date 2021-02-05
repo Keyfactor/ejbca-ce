4c6b0bb532a476c41cdeeffb3155ba1000455602
Henrik Sunmark <henrik.sunmark@primekey.com>
2021-02-05 10:29:15 +0100

    ECA-9766: Renamed tab

--- modules/admin-gui/src/org/ejbca/ui/web/admin/configuration/SystemConfigMBean.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/configuration/SystemConfigMBean.java
@@ -1965,7 +1965,7 @@ public class SystemConfigMBean extends BaseManagedBean implements Serializable {
             availableTabs.add("Configuration Checker");
         }
         if (authorizationSession.isAuthorizedNoLogging(getAdmin(), StandardRules.SYSTEMCONFIGURATION_VIEW.resource())) {
-            availableTabs.add("AutoEnrollment");
+            availableTabs.add("Auto Enrollment");
         }
         return availableTabs;
     }
