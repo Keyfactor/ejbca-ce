b6516af0306da5b5d441054a0a0bb654dcec6f6e
Jekaterina Bunina <jekaterina.bunina.ext@primekey.com>
2021-03-12 12:51:59 +0200

    ECA-9786 add keys as cert or text or from url in CLI

--- modules/ejbca-ejb-cli/src/org/ejbca/ui/cli/config/oauth/ManageOauthPublicKeyCommand.java
+++ modules/ejbca-ejb-cli/src/org/ejbca/ui/cli/config/oauth/ManageOauthPublicKeyCommand.java
@@ -12,16 +12,31 @@
  *************************************************************************/
 package org.ejbca.ui.cli.config.oauth;
 
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
 import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
+import org.bouncycastle.util.encoders.DecoderException;
 import org.cesecore.authentication.oauth.OAuthKeyInfo;
+import org.cesecore.authentication.oauth.OAuthPublicKey;
+import org.cesecore.util.CertTools;
 import org.ejbca.ui.cli.infrastructure.command.CommandResult;
 import org.ejbca.ui.cli.infrastructure.parameter.Parameter;
 import org.ejbca.ui.cli.infrastructure.parameter.ParameterContainer;
 import org.ejbca.ui.cli.infrastructure.parameter.enums.MandatoryMode;
 import org.ejbca.ui.cli.infrastructure.parameter.enums.ParameterMode;
 import org.ejbca.ui.cli.infrastructure.parameter.enums.StandaloneMode;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.security.PublicKey;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateParsingException;
+import java.text.ParseException;
 
 /**
  * Adds or removes public keys to/from already existing Trusted OAuth Provider
@@ -33,17 +48,26 @@ public class ManageOauthPublicKeyCommand extends BaseOAuthConfigCommand{
     private static final String LABEL = "--label";
     private static final String ACTION = "--action";
     private static final String KEY_IDENTIFIER = "--keyidentifier";
-    private static final String PUBLIC_KEY = "--publickey";
+    private static final String KEY_FILE = "--keyfile";
+    private static final String KEY_WAY = "--keyway";
+    private static final String KEY_URL = "--keyurl";
+    private static final String KEY_VALUE = "--key";
 
     {
         registerParameter(new Parameter(LABEL, "Provider name", MandatoryMode.MANDATORY, StandaloneMode.ALLOW, ParameterMode.ARGUMENT,
                 "Trusted OAuth Provider name to be shown"));
         registerParameter(new Parameter(ACTION, "Action: add or remove", MandatoryMode.MANDATORY, StandaloneMode.ALLOW, ParameterMode.ARGUMENT,
                 "Action: add or remove"));
-        registerParameter(new Parameter(KEY_IDENTIFIER, "Key identifier", MandatoryMode.MANDATORY, StandaloneMode.ALLOW, ParameterMode.ARGUMENT,
+        registerParameter(new Parameter(KEY_IDENTIFIER, "Key identifier", MandatoryMode.OPTIONAL, StandaloneMode.ALLOW, ParameterMode.ARGUMENT,
                 "Key identifier of the key which is going to be added/removed."));
-        registerParameter(new Parameter(PUBLIC_KEY, "Public key", MandatoryMode.OPTIONAL, StandaloneMode.ALLOW, ParameterMode.ARGUMENT,
+        registerParameter(new Parameter(KEY_FILE, "Public key file", MandatoryMode.OPTIONAL, StandaloneMode.ALLOW, ParameterMode.ARGUMENT,
                 "Path to publickey file of public key."));
+        registerParameter(new Parameter(KEY_WAY, "Public key", MandatoryMode.MANDATORY, StandaloneMode.ALLOW, ParameterMode.ARGUMENT,
+                "Way to provide public key(--keyway): within file (FILE), configuration url (URL) or as text value (TEXT)"));
+        registerParameter(new Parameter(KEY_URL, "Config url", MandatoryMode.OPTIONAL, StandaloneMode.ALLOW, ParameterMode.ARGUMENT,
+                "Public key configuration URL"));
+        registerParameter(new Parameter(KEY_VALUE, "Public key", MandatoryMode.OPTIONAL, StandaloneMode.ALLOW, ParameterMode.ARGUMENT,
+                "Public key text value"));
     }
 
     @Override
@@ -62,11 +86,10 @@ public class ManageOauthPublicKeyCommand extends BaseOAuthConfigCommand{
         String action = parameters.get(ACTION);
         String label = parameters.get(LABEL);
         String kid = parameters.get(KEY_IDENTIFIER);
-        String publicKey = parameters.get(PUBLIC_KEY);
 
         switch (action.toLowerCase()) {
             case "add": {
-                result = addKey(label, kid, publicKey);
+                result = addKey(label, kid, parameters);
                 break;
             }
             case "remove": {
@@ -81,36 +104,135 @@ public class ManageOauthPublicKeyCommand extends BaseOAuthConfigCommand{
         return result;
     }
 
-    private CommandResult addKey(String label, String kid, String publicKey){
-        final OAuthKeyInfo oAuthKeyInfo = getOAuthConfiguration().getOauthKeyByLabel(label);
+    private CommandResult addKey(String label, String kid, ParameterContainer parameters){
+ final OAuthKeyInfo oAuthKeyInfo = getOAuthConfiguration().getOauthKeyByLabel(label);
 
         if (oAuthKeyInfo == null) {
             log.info("Trusted OAuth Provider with label: " + label + " not found!");
             return CommandResult.FUNCTIONAL_FAILURE;
         }
-        if (StringUtils.isEmpty(publicKey)) {
-            log.info("Public key file is not defined!");
+        String keyWay = parameters.get(KEY_WAY);
+        if (StringUtils.isEmpty(keyWay)) {
+            log.info("Please specify how you want to provide public key(--keyway): within file (FILE), configuration url (URL) or as text value (TEXT)");
             return CommandResult.FUNCTIONAL_FAILURE;
         }
-        byte[] publicKeyByteArray = getOauthKeyPublicKey(publicKey);
-        if (ArrayUtils.isEmpty(publicKeyByteArray)) {
-            return CommandResult.FUNCTIONAL_FAILURE;
+        switch (keyWay.toLowerCase()) {
+            case "file": {
+                String publicKey = parameters.get(KEY_FILE);
+                if (!parsePublicKeyFromFile(kid, oAuthKeyInfo, publicKey)) {
+                    return CommandResult.FUNCTIONAL_FAILURE;
+                }
+                break;
+            }
+            case "url": {
+                if (!addOauthPublicKeyFromUrl(oAuthKeyInfo, parameters.get(KEY_URL))) {
+                    return CommandResult.FUNCTIONAL_FAILURE;
+                }
+                break;
+            }
+            case "text": {
+                if (!parsePublicKeyFromText(oAuthKeyInfo, kid, parameters.get(KEY_VALUE))) {
+                    return CommandResult.FUNCTIONAL_FAILURE;
+                }
+                break;
+            }
         }
 
-        if (oAuthKeyInfo.getAllKeyIdentifiers()!= null && oAuthKeyInfo.getAllKeyIdentifiers().contains(kid)) {
-            log.info("Key with identifier " + kid + " already exists.");
-            return CommandResult.FUNCTIONAL_FAILURE;
-        }
-
-        oAuthKeyInfo.addPublicKey(kid, publicKeyByteArray);
         if (saveGlobalConfig()) {
-            log.info("Public key with kid " +kid+ " successfuly added to Trusted OAuth Provider with label: " + label + "!");
+            log.info("Public key successfuly added to Trusted OAuth Provider with label: " + label + "!");
             return CommandResult.SUCCESS;
         } else {
             log.info("Failed to update configuration due to authorization issue!");
             return CommandResult.AUTHORIZATION_FAILURE;
         }
     }
+
+    private boolean parsePublicKeyFromFile(String kid, OAuthKeyInfo oAuthKeyInfo, String publicKey) {
+        if (StringUtils.isEmpty(kid)) {
+            log.info("Public key identifier is not defined!");
+            return false;
+        }
+        if (StringUtils.isEmpty(publicKey)) {
+            log.info("Public key file is not defined!");
+            return false;
+        }
+        byte[] publicKeyByteArray = getOauthKeyPublicKey(publicKey);
+        if (ArrayUtils.isEmpty(publicKeyByteArray)) {
+            return false;
+        }
+        if (oAuthKeyInfo.getAllKeyIdentifiers()!= null && oAuthKeyInfo.getAllKeyIdentifiers().contains(kid)) {
+            log.info("Key with identifier " + kid + " already exists.");
+            return false;
+        }
+        oAuthKeyInfo.addPublicKey(kid, publicKeyByteArray);
+        return true;
+    }
+
+    private boolean addOauthPublicKeyFromUrl(OAuthKeyInfo oAuthKeyInfo, String url) {
+        if (StringUtils.isEmpty(url)) {
+            log.info("Public key config url is not defined!");
+            return false;
+        }
+        try {
+            JWKSet jwkSet = JWKSet.load(new URL(url));
+            for (JWK jwk : jwkSet.getKeys()) {
+                if (oAuthKeyInfo.getAllKeyIdentifiers() != null && oAuthKeyInfo.getAllKeyIdentifiers().contains(jwk.getKeyID())) {
+                    log.info("Key with identifier " + jwk.getKeyID() + " already exists.");
+                    continue;
+                }
+                final PublicKey publicKey = jwk.toRSAKey().toPublicKey();
+                final byte[] encoded = publicKey.getEncoded();
+                oAuthKeyInfo.addPublicKey(jwk.getKeyID(), encoded);
+            }
+        } catch (MalformedURLException e) {
+            log.info("Could not parse public key config url " + url);
+            return false;
+        } catch (ParseException | IOException | JOSEException e) {
+            log.info("Could not load keys using config url " + url);
+            return false;
+        }
+        return true;
+    }
+
+    private boolean parsePublicKeyFromText(OAuthKeyInfo oAuthKeyInfo, String kid, String value){
+        if (StringUtils.isEmpty(kid)) {
+            log.info("Public key identifier is not defined!");
+            return false;
+        }
+        if (oAuthKeyInfo.getAllKeyIdentifiers() != null && oAuthKeyInfo.getAllKeyIdentifiers().contains(kid)) {
+            log.info("Key with identifier " + kid + " already exists.");
+            return false;
+        }
+        if (StringUtils.isEmpty(value)) {
+            log.info("Public key value is not defined!");
+            return false;
+        }
+        byte[] newOauthKeyPublicKey ;
+        OAuthPublicKey oAuthPublicKey;
+        try {
+            newOauthKeyPublicKey = org.cesecore.util.Base64.decode(
+                    value.getBytes(StandardCharsets.US_ASCII));
+            oAuthPublicKey = new OAuthPublicKey(newOauthKeyPublicKey, kid);
+            oAuthPublicKey.getOauthPublicKey();
+        } catch (IllegalStateException e){
+            try {
+                final Certificate certificate = CertTools.getCertfromByteArray(
+                        org.cesecore.util.Base64.decode(value.getBytes()), Certificate.class);
+                if (certificate == null || certificate.getPublicKey() == null) {
+                    log.info("Could not parse the public key from  certificate.");
+                    return false;
+                }
+                final PublicKey publicKey = certificate.getPublicKey();
+                newOauthKeyPublicKey = publicKey.getEncoded();
+                oAuthKeyInfo.addPublicKey(kid, newOauthKeyPublicKey);
+            } catch (CertificateParsingException | DecoderException exception) {
+                log.info("Could not parse public key from certificate string ");
+                return false;
+            }
+        }
+        return true;
+    }
+
 
     private CommandResult removeKey(String label, String kid){
         final OAuthKeyInfo oAuthKeyInfo = getOAuthConfiguration().getOauthKeyByLabel(label);
