b7e03e369d42adeede33003410bea8e7dc967c6e
Lauri Kongas <lauri.kongas.ext@primekey.com>
2021-03-25 17:42:09 +0200

    ECA-9905 Validate the provider before changing the provider in memory

--- modules/admin-gui/src/org/ejbca/util/OAuthProviderUIHelper.java
+++ modules/admin-gui/src/org/ejbca/util/OAuthProviderUIHelper.java
@@ -3,6 +3,7 @@ package org.ejbca.util;
 import org.apache.commons.lang.StringUtils;
 import org.cesecore.authentication.oauth.MissingOAuthKeyAttributeException;
 import org.cesecore.authentication.oauth.OAuthKeyInfo;
+import org.ejbca.ui.web.admin.configuration.SystemConfigurationOAuthKeyManager.OAuthKeyEditor;
 import org.ejbca.ui.web.jsf.configuration.EjbcaJSFHelper;
 
 /**
@@ -12,20 +13,19 @@ import org.ejbca.ui.web.jsf.configuration.EjbcaJSFHelper;
 public class OAuthProviderUIHelper {
     
     private static final String OAUTHKEYCONFIGURATION_FIELD_MANDATORY = "OAUTHKEYCONFIGURATION_FIELD_MANDATORY";
-    private static final String OAUTHKEYCONFIGURATION_PUBLIC_KEY_MANDATORY = "OAUTHKEYCONFIGURATION_PUBLIC_KEY_MANDATORY";
     
-    public static void validateProvider(final OAuthKeyInfo provider) {
+    public static void validateProvider(final OAuthKeyEditor provider) {
         validateCommonType(provider);
-        if (OAuthKeyInfo.OAuthProviderType.TYPE_AZURE.getIndex() == provider.getTypeInt()) {
+        if (OAuthKeyInfo.OAuthProviderType.TYPE_AZURE.getIndex() == provider.getType().getIndex()) {
             validateAzureType(provider);
-        } else if (OAuthKeyInfo.OAuthProviderType.TYPE_KEYCLOAK.getIndex() == provider.getTypeInt()) {
+        } else if (OAuthKeyInfo.OAuthProviderType.TYPE_KEYCLOAK.getIndex() == provider.getType().getIndex()) {
             validateKeycloakType(provider);
-        } else if (OAuthKeyInfo.OAuthProviderType.TYPE_NONE.getIndex() != provider.getTypeInt()) {
+        } else if (OAuthKeyInfo.OAuthProviderType.TYPE_NONE.getIndex() != provider.getType().getIndex()) {
             throw new MissingOAuthKeyAttributeException(EjbcaJSFHelper.getBean().getEjbcaWebBean().getText(OAUTHKEYCONFIGURATION_FIELD_MANDATORY, false, "Provider Type"));
         }
     }
     
-    public static void validateAzureType(final OAuthKeyInfo provider) {
+    public static void validateAzureType(final OAuthKeyEditor provider) {
         if (StringUtils.isEmpty(provider.getUrl())) {
             throw new MissingOAuthKeyAttributeException(EjbcaJSFHelper.getBean().getEjbcaWebBean().getText(OAUTHKEYCONFIGURATION_FIELD_MANDATORY, false, "URL"));
         }
@@ -38,12 +38,12 @@ public class OAuthProviderUIHelper {
         if (StringUtils.isEmpty(provider.getClient())) {
             throw new MissingOAuthKeyAttributeException(EjbcaJSFHelper.getBean().getEjbcaWebBean().getText(OAUTHKEYCONFIGURATION_FIELD_MANDATORY, false, "Client"));
         }
-        if (StringUtils.isEmpty(provider.getClientSecretAndDecrypt())) {
+        if (StringUtils.isEmpty(provider.getClientSecret())) {
             throw new MissingOAuthKeyAttributeException(EjbcaJSFHelper.getBean().getEjbcaWebBean().getText(OAUTHKEYCONFIGURATION_FIELD_MANDATORY, false, "Client Secret"));
         }
     }
     
-    public static void validateKeycloakType(final OAuthKeyInfo provider) {
+    public static void validateKeycloakType(final OAuthKeyEditor provider) {
         if (StringUtils.isEmpty(provider.getUrl())) {
             throw new MissingOAuthKeyAttributeException(EjbcaJSFHelper.getBean().getEjbcaWebBean().getText(OAUTHKEYCONFIGURATION_FIELD_MANDATORY, false, "URL"));
         }
@@ -55,12 +55,9 @@ public class OAuthProviderUIHelper {
         }
     }
     
-    private static void validateCommonType(final OAuthKeyInfo provider) {
+    private static void validateCommonType(final OAuthKeyEditor provider) {
         if (StringUtils.isEmpty(provider.getLabel())) {
             throw new MissingOAuthKeyAttributeException(EjbcaJSFHelper.getBean().getEjbcaWebBean().getText(OAUTHKEYCONFIGURATION_FIELD_MANDATORY, false, "Label"));
-        }
-        if (provider.getKeys() == null || provider.getKeys().isEmpty()) {
-            throw new MissingOAuthKeyAttributeException(EjbcaJSFHelper.getBean().getEjbcaWebBean().getText(OAUTHKEYCONFIGURATION_PUBLIC_KEY_MANDATORY, false));
         }
     }
 }
