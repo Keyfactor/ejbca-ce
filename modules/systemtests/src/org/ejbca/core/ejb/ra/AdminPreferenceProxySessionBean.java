84ed3ef7c3b22c115f2952fcb4daf7da888e152c
Jekaterina Bunina <jekaterina.bunina.ext@primekey.com>
2021-03-25 11:14:01 +0200

    ECA-9916 oid claim

---
+++ modules/systemtests/src/org/ejbca/core/ejb/ra/AdminPreferenceProxySessionBean.java
@@ -0,0 +1,31 @@
+package org.ejbca.core.ejb.ra;
+
+import org.cesecore.authentication.tokens.AuthenticationToken;
+import org.cesecore.config.CesecoreConfiguration;
+import org.cesecore.jndi.JndiConstants;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+@Stateless(mappedName = JndiConstants.APP_JNDI_PREFIX + "AdminPreferenceProxySessionRemote")
+@TransactionAttribute(TransactionAttributeType.REQUIRED)
+public class AdminPreferenceProxySessionBean implements AdminPreferenceProxySessionRemote {
+    @EJB
+    private EndEntityManagementSessionLocal endEntityManagementSession;
+
+    @PersistenceContext(unitName = CesecoreConfiguration.PERSISTENCE_UNIT)
+    private EntityManager entityManager;
+    @Override
+    public void deleteAdminPreferences(AuthenticationToken token) {
+        final String id = makeAdminPreferenceId(token);
+        Query query = entityManager.createQuery("DELETE FROM AdminPreferencesData ap WHERE ap.id=:id ");
+        query.setParameter("id", id);
+        query.executeUpdate();
+    }
+
+}
