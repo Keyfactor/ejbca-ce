971f153f2669ced31498e7abdbbbf6a5167ce0e8
Jekaterina Bunina <jekaterina.bunina.ext@primekey.com>
2021-03-29 10:10:23 +0300

    ECA-9845 fix review comments

--- modules/ra-gui/src/org/ejbca/ra/RaAuthenticationHelper.java
+++ modules/ra-gui/src/org/ejbca/ra/RaAuthenticationHelper.java
@@ -26,14 +26,14 @@ import org.bouncycastle.util.encoders.Hex;
 import org.cesecore.authentication.oauth.OAuthGrantResponseInfo;
 import org.cesecore.authentication.oauth.TokenExpiredException;
 import org.cesecore.authentication.tokens.AuthenticationToken;
+import org.cesecore.roles.management.RoleSessionLocal;
 import org.cesecore.util.CertTools;
 import org.ejbca.core.ejb.authentication.web.WebAuthenticationProviderSessionLocal;
 import org.ejbca.util.HttpTools;
 
 /**
  * Web session authentication helper.
- * 
- * @version $Id$
+ *
  */
 public class RaAuthenticationHelper implements Serializable {
 
@@ -44,12 +44,14 @@ public class RaAuthenticationHelper implements Serializable {
     private static final String HTTP_HEADER_X_POWERED_BY = "X-Powered-By";
 
     private final WebAuthenticationProviderSessionLocal webAuthenticationProviderSession;
+    private final RoleSessionLocal roleSession;
     private AuthenticationToken authenticationToken = null;
     private String authenticationTokenTlsSessionId = null;
     private String x509AuthenticationTokenFingerprint = null;
 
-    public RaAuthenticationHelper(final WebAuthenticationProviderSessionLocal webAuthenticationProviderSession) {
+    public RaAuthenticationHelper(final WebAuthenticationProviderSessionLocal webAuthenticationProviderSession, RoleSessionLocal roleSession) {
         this.webAuthenticationProviderSession = webAuthenticationProviderSession;
+        this.roleSession = roleSession;
     }
 
     public void resetAuthenticationToken(){
@@ -91,6 +93,10 @@ public class RaAuthenticationHelper implements Serializable {
                 // No need to perform re-authentication if the client certificate was the same
                 if (authenticationToken == null) {
                     authenticationToken = webAuthenticationProviderSession.authenticateUsingClientCertificate(x509Certificate);
+                }
+                if (authenticationToken != null && roleSession.getRolesAuthenticationTokenIsMemberOf(authenticationToken).isEmpty()) {
+                    authenticationToken = null;
+                    log.info("Authentication failed using certificate with fingerprint " + fingerprint + " reason: user has no access");
                 }
                 x509AuthenticationTokenFingerprint = authenticationToken == null ? null : fingerprint;
             }
