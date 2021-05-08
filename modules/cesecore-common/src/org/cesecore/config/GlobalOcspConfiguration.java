d78176d67e66aeb4d6579a9ea49f2e4b9f16a863
Bastian Fredriksson <bastian@primekey.com>
2021-05-08 12:54:55 +0200

    ECA-9996 Migrate configuration from ocsp.properties and use configuration database configuration during runtime.
    Signed-off-by: Bastian Fredriksson <bastian@primekey.com>

--- modules/cesecore-common/src/org/cesecore/config/GlobalOcspConfiguration.java
+++ modules/cesecore-common/src/org/cesecore/config/GlobalOcspConfiguration.java
@@ -114,7 +114,7 @@ public class GlobalOcspConfiguration extends ConfigurationBase implements Serial
 
     public String getOcspTransactionLogPattern() {
         if (data.get(PROPERTY_OCSP_TRANSACTION_LOG_PATTERN) == null) {
-            return "\\\\$\\\\{(.+?)\\\\}";
+            return "\\$\\{(.+?)\\}";
         }
         return (String) data.get(PROPERTY_OCSP_TRANSACTION_LOG_PATTERN);
     }
@@ -125,10 +125,9 @@ public class GlobalOcspConfiguration extends ConfigurationBase implements Serial
 
     public String getOcspTransactionLogValues() {
         if (data.get(PROPERTY_OCSP_TRANSACTION_LOG_VALUES) == null) {
-            return "${SESSION_ID};${LOG_ID};${STATUS};${REQ_NAME}\"${CLIENT_IP}\";" +
-                    "\"${SIGN_ISSUER_NAME_DN}\";\"${SIGN_SUBJECT_NAME}\";${SIGN_SERIAL_NO};" +
-                    "\"${LOG_TIME}\";${REPLY_TIME};${NUM_CERT_ID};0;0;0;0;0;0;0;" +
-                    "\"${ISSUER_NAME_DN}\";${ISSUER_NAME_HASH};${ISSUER_KEY};${DIGEST_ALGOR};" +
+            return "${SESSION_ID};${LOG_ID};${STATUS};${REQ_NAME}\"${CLIENT_IP}\";\"${SIGN_ISSUER_NAME_DN}\";\"" +
+                    "${SIGN_SUBJECT_NAME}\";${SIGN_SERIAL_NO};\"${LOG_TIME}\";${REPLY_TIME};${NUM_CERT_ID};0;" +
+                    "0;0;0;0;0;0;\"${ISSUER_NAME_DN}\";${ISSUER_NAME_HASH};${ISSUER_KEY};${DIGEST_ALGOR};" +
                     "${SERIAL_NOHEX};${CERT_STATUS};${CERT_PROFILE_ID};${FORWARDED_FOR}";
         }
         return (String) data.get(PROPERTY_OCSP_TRANSACTION_LOG_VALUES);
@@ -151,7 +150,7 @@ public class GlobalOcspConfiguration extends ConfigurationBase implements Serial
 
     public String getOcspAuditLogPattern() {
         if (data.get(PROPERTY_OCSP_AUDIT_LOG_PATTERN) == null) {
-            return "\\\\$\\\\{(.+?)\\\\}";
+            return "\\$\\{(.+?)\\}";
         }
         return (String) data.get(PROPERTY_OCSP_AUDIT_LOG_PATTERN);
     }
