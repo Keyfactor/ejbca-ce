2ad3cdaf2a168fc767c8512207a3089b59155936
Mark Wright <mark.wright@primekey.com>
2021-05-13 06:43:07 -0400

    ECA-8473 Only show key binding dropdown if binding selected.
    Previous code didn't have required form id in ajac's render list.
    Also removed some dead and test code.

--- modules/cesecore-common/src/org/cesecore/keys/token/AzureCryptoToken.java
+++ modules/cesecore-common/src/org/cesecore/keys/token/AzureCryptoToken.java
@@ -38,7 +38,6 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Properties;
 import java.util.Set;
 import java.util.UUID;
