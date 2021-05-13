acd0a9dfc2792c8463b2786418361a03dd8d4985
Mark Wright <mark.wright@primekey.com>
2021-05-12 23:23:29 -0400

    ECA-8473 Select Key Binding in UI.

--- modules/cesecore-common/src/org/cesecore/keys/token/CryptoTokenInfo.java
+++ modules/cesecore-common/src/org/cesecore/keys/token/CryptoTokenInfo.java
@@ -113,6 +113,9 @@ public class CryptoTokenInfo implements Named, Serializable {
     public boolean isKeyVaultUseKeyBinding() {
         return Boolean.parseBoolean(cryptoTokenProperties.getProperty(AzureCryptoToken.KEY_VAULT_USE_KEY_BINDING, "false"));        
     }
+    public String getKeyVaultKeyBinding() {
+        return cryptoTokenProperties.getProperty(AzureCryptoToken.KEY_VAULT_KEY_BINDING);        
+    }
     public String getAWSKMSRegion() {
         return cryptoTokenProperties.getProperty(CryptoTokenConstants.AWSKMS_REGION);        
     }
