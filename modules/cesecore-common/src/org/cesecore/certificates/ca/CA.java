ad38a35aecb44eb0b7e8e68cb91ea5edb521f41b
Henrik Sunmark <henrik.sunmark@primekey.com>
2021-06-09 11:45:43 +0200

    ECA-10085: Signing crl paritions with corresponding key in MS comp mode

--- modules/cesecore-common/src/org/cesecore/certificates/ca/CA.java
+++ modules/cesecore-common/src/org/cesecore/certificates/ca/CA.java
@@ -216,7 +216,7 @@ public interface CA extends CACommon {
            throws CryptoTokenOfflineException, CAOfflineException, InvalidAlgorithmException, IllegalValidityException, IllegalNameException,
            OperatorCreationException, CertificateCreateException, CertificateExtensionException, SignatureException, IllegalKeyException;
 
-   X509CRLHolder generateCRL(CryptoToken cryptoToken, int crlPartitionIndex, Collection<RevokedCertInfo> certs, int crlnumber) throws Exception;
+   X509CRLHolder generateCRL(CryptoToken cryptoToken, int crlPartitionIndex, Collection<RevokedCertInfo> certs, int crlnumber, byte[] crlSignSubjectKeyIdentifier) throws Exception;
 
    X509CRLHolder generateDeltaCRL(CryptoToken cryptoToken, int crlPartitionIndex, Collection<RevokedCertInfo> certs, int crlnumber, int basecrlnumber) throws Exception;
 
