95eac2c8484a8bb6cdc3626ac360b2bce66643af
andresj <andresj@primekey.com>
2021-08-17 10:11:08 +0200

    ECA-10183 Remove duplicates and sorted set.

--- modules/ejbca-common/src/org/ejbca/config/AcmeConfiguration.java
+++ modules/ejbca-common/src/org/ejbca/config/AcmeConfiguration.java
@@ -437,14 +437,19 @@ public class AcmeConfiguration extends UpgradeableDataHashMap implements Seriali
     public Set<Integer> getAuthorizedRedirectPortsList() {
         final String ports = getAuthorizedRedirectPorts();
         if (ports != null) {
-            final Set<Integer> result = Stream.of(ports.split(",")).map(Integer::parseInt).collect(Collectors.toSet());
-            return result;
+            return Stream.of(ports.split(",")).map(Integer::parseInt).sorted().collect(Collectors.toSet());
         }
         return Collections.emptySet();
     }
 
     public void setAuthorizedRedirectPorts(String ports) {
-        super.data.put(KEY_AUTHORIZED_REDIRECT_PORTS, ports);
+        if (ports != null && ports.length() > 0) {
+            ports = Stream.of(ports.split(",")).map(Integer::parseInt).collect(Collectors.toUnmodifiableSet())
+                    .stream().sorted().map(i -> Integer.toString(i)).collect(Collectors.joining(","));
+            super.data.put(KEY_AUTHORIZED_REDIRECT_PORTS, ports);
+        } else {
+            super.data.put(KEY_AUTHORIZED_REDIRECT_PORTS, "");
+        }
     }
     
     public boolean isTermsOfServiceRequireNewApproval() {
