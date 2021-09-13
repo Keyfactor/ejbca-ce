5a26e2f8d1654a373b8a72f8d214e4d38ec777c0
Mark Wright <mark.wright@primekey.com>
2021-09-13 17:07:55 -0400

    ECA-10092 Add key binding selection to configuration GUI

--- modules/admin-gui/src/org/ejbca/ui/web/admin/configuration/SystemConfigurationOAuthKeyManager.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/configuration/SystemConfigurationOAuthKeyManager.java
@@ -25,6 +25,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.log4j.Logger;
 import org.apache.myfaces.custom.fileupload.UploadedFile;
 import org.bouncycastle.util.encoders.Base64;
@@ -36,6 +37,7 @@ import org.cesecore.authentication.tokens.AuthenticationToken;
 import org.cesecore.authentication.tokens.OAuth2AuthenticationToken;
 import org.cesecore.config.OAuthConfiguration;
 import org.cesecore.configuration.GlobalConfigurationSessionLocal;
+import org.cesecore.keybind.impl.AuthenticationKeyBinding;
 import org.cesecore.keys.util.KeyTools;
 import org.cesecore.util.CertTools;
 import org.ejbca.core.model.util.EjbLocalHelper;
@@ -61,6 +63,7 @@ public class SystemConfigurationOAuthKeyManager extends OAuthKeyManager {
     private final OAuthKeyEditor oauthKeyEditor;
     private AuthenticationToken adminToken;
     private OAuthConfiguration oAuthConfiguration;
+    private List<Pair<String, Integer>> keyBindings = new ArrayList<>();
     
     private final EjbLocalHelper ejbLocalHelper = new EjbLocalHelper();
     private final GlobalConfigurationSessionLocal globalConfigurationSession = ejbLocalHelper.getGlobalConfigurationSession();
@@ -97,6 +100,9 @@ public class SystemConfigurationOAuthKeyManager extends OAuthKeyManager {
         private String audience;
         private String scope;
         private UploadedFile publicKeyFile;
+        
+        // if null, use client secret
+        private Integer keyBinding = null;
         private List<OAuthPublicKey> publicKeys;
         private int skewLimit = 60000;
         private OAuthKeyInfo oauthKeyBeingEdited;
@@ -372,6 +378,14 @@ public class SystemConfigurationOAuthKeyManager extends OAuthKeyManager {
         public final void setAudience(String audience) {
             this.audience = audience;
         }
+
+        public Integer getKeyBinding() {
+            return keyBinding;
+        }
+
+        public void setKeyBinding(Integer keyBinding) {
+            this.keyBinding = keyBinding;
+        }
     }
 
     public interface SystemConfigurationHelper {
@@ -411,6 +425,14 @@ public class SystemConfigurationOAuthKeyManager extends OAuthKeyManager {
         super(oAuthKeys);
         this.systemConfigurationHelper = systemConfigurationHelper;
         this.oauthKeyEditor = new OAuthKeyEditor();
+
+        // get the names of all the key bindings
+        log.trace("Loading key bindings");
+        ejbLocalHelper.getInternalKeyBindingMgmtSession().getAllInternalKeyBindingInfos(AuthenticationKeyBinding.IMPLEMENTATION_ALIAS).stream()
+                .sorted((b1, b2) -> b1.getName().compareTo(b2.getName())).forEach(b -> {
+                    log.info("Adding key binding:" + b.getName() + ":" + b.getId());
+                    keyBindings.add(Pair.of(b.getName(), b.getId()));
+                });
     }
     
     public AuthenticationToken getAdminToken() {
@@ -798,5 +820,9 @@ public class SystemConfigurationOAuthKeyManager extends OAuthKeyManager {
         systemConfigurationHelper.saveOauthKeys(super.getAllOauthKeys());
         oauthKeyEditor.stopEditing();
         return OAUTH_KEY_SAVED;
+    }
+
+    public List<Pair<String, Integer>> getKeyBindings() {
+        return keyBindings;
     }
 }
