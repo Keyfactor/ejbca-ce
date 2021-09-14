5678b541267a58885cefb00d87b4e4dafc5c8e54
Mark Wright <mark.wright@primekey.com>
2021-09-14 14:15:22 -0400

    ECA-10092 Add cert auth login for oauth.

---
+++ modules/cesecore-common/src/org/cesecore/keybind/KeyBindingNotFoundException.java
@@ -0,0 +1,9 @@
+package org.cesecore.keybind;
+
+public class KeyBindingNotFoundException extends Exception {
+
+    public KeyBindingNotFoundException(String msg) {
+        super(msg);
+    }
+
+}
