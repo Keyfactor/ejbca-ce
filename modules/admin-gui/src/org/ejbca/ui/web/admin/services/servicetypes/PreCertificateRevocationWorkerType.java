51d006515cbf29257b75caf7ecbae4ce2e27930c
Samuel Lidén Borell <samuel.liden.borell@primekey.com>
2021-09-16 15:15:34 +0200

    ECA-10215: Rename worker type to match UI. Exclude from Community Edition

--- modules/admin-gui/src/org/ejbca/ui/web/admin/services/servicetypes/PreCertificateRevocationWorkerType.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/services/servicetypes/PreCertificateRevocationWorkerType.java
@@ -17,22 +17,22 @@ import java.util.ArrayList;
 import java.util.Properties;
 
 import org.ejbca.core.model.services.IWorker;
-import org.ejbca.core.model.services.workers.IncompleteIssuanceRevocationWorker;
+import org.ejbca.core.model.services.workers.PreCertificateRevocationWorkerConstants;
 
 /**
- * @see IncompleteIssuanceRevocationWorker
+ * See PreCertificateRevocationWorker (available in Enterprise Edition only)
  */
-public class IncompleteIssuanceRevocationWorkerType extends BaseWorkerType {
+public class PreCertificateRevocationWorkerType extends BaseWorkerType {
 
-    public static final String NAME = "INCOMPLETEISSUANCEREVOCATIONWORKER";
+    public static final String NAME = "PRECERTIFICATEREVOCATIONWORKER";
 
     private static final long serialVersionUID = 1;
 
     private int maxCertAge = 60;
     private String maxIssuanceTimeUnit = IWorker.UNIT_MINUTES;
 
-    public IncompleteIssuanceRevocationWorkerType() {
-        super(ServiceTypeUtil.INCOMPLETEISSUANCEREVOCATIONWORKER_SUB_PAGE, NAME, true, IncompleteIssuanceRevocationWorker.class.getName());
+    public PreCertificateRevocationWorkerType() {
+        super(ServiceTypeUtil.PRECERTIFICATEREVOCATIONWORKER_SUB_PAGE, NAME, true, PreCertificateRevocationWorkerConstants.WORKER_CLASS);
         // No action available for this worker
         deleteAllCompatibleActionTypes();
         addCompatibleActionTypeName(NoActionType.NAME);
@@ -60,16 +60,16 @@ public class IncompleteIssuanceRevocationWorkerType extends BaseWorkerType {
     @Override
     public Properties getProperties(final ArrayList<String> errorMessages) throws IOException {
         Properties ret = super.getProperties(errorMessages);
-        ret.setProperty(IncompleteIssuanceRevocationWorker.PROP_MAX_ISSUANCE_TIME, String.valueOf(maxCertAge));
-        ret.setProperty(IncompleteIssuanceRevocationWorker.PROP_MAX_ISSUANCE_TIMEUNIT, maxIssuanceTimeUnit);
+        ret.setProperty(PreCertificateRevocationWorkerConstants.PROP_MAX_ISSUANCE_TIME, String.valueOf(maxCertAge));
+        ret.setProperty(PreCertificateRevocationWorkerConstants.PROP_MAX_ISSUANCE_TIMEUNIT, maxIssuanceTimeUnit);
         return ret;
     }
 
     @Override
     public void setProperties(Properties properties) throws IOException {
         super.setProperties(properties);
-        maxCertAge = Integer.valueOf(properties.getProperty(IncompleteIssuanceRevocationWorker.PROP_MAX_ISSUANCE_TIME, String.valueOf(maxCertAge)));
-        maxIssuanceTimeUnit = properties.getProperty(IncompleteIssuanceRevocationWorker.PROP_MAX_ISSUANCE_TIMEUNIT, maxIssuanceTimeUnit);
+        maxCertAge = Integer.valueOf(properties.getProperty(PreCertificateRevocationWorkerConstants.PROP_MAX_ISSUANCE_TIME, String.valueOf(maxCertAge)));
+        maxIssuanceTimeUnit = properties.getProperty(PreCertificateRevocationWorkerConstants.PROP_MAX_ISSUANCE_TIMEUNIT, maxIssuanceTimeUnit);
     }
 
 }
