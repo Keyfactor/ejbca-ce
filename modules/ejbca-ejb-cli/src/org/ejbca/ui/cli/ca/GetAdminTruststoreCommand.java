ed21f13cf0c42de1bc1a84fa5a031d41e020ec8c
Mark Wright <mark.wright@primekey.com>
2021-09-17 10:22:13 -0400

    ECA-10305 Make ca command not roles.  Add password option.
    Make changes per review (CR-EJBCA-4564).

--- modules/ejbca-ejb-cli/src/org/ejbca/ui/cli/ca/GetAdminTruststoreCommand.java
+++ modules/ejbca-ejb-cli/src/org/ejbca/ui/cli/ca/GetAdminTruststoreCommand.java
@@ -1,4 +1,16 @@
-package org.ejbca.ui.cli.roles;
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.ui.cli.ca;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -30,15 +42,18 @@ import org.ejbca.ui.cli.infrastructure.parameter.enums.MandatoryMode;
 import org.ejbca.ui.cli.infrastructure.parameter.enums.ParameterMode;
 import org.ejbca.ui.cli.infrastructure.parameter.enums.StandaloneMode;
 
-public class GetAdminTruststoreCommand extends BaseRolesCommand {
+public class GetAdminTruststoreCommand extends BaseCaAdminCommand {
     private static final Logger log = Logger.getLogger(GetAdminTruststoreCommand.class);
 
     private static final String FORMAT_KEY = "--format";
     private static final String TRUSTSTORE_KEY = "--truststore";
+    private static final String PASSWORD_KEY = "--password";
     
     {
         registerParameter(new Parameter(FORMAT_KEY, "Format", MandatoryMode.OPTIONAL, StandaloneMode.ALLOW, ParameterMode.ARGUMENT,
                 "Truststore format [PEM (default), JKS, PKCS12]."));
+        registerParameter(new Parameter(PASSWORD_KEY, "Password", MandatoryMode.OPTIONAL, StandaloneMode.ALLOW, ParameterMode.ARGUMENT,
+                "Truststore password.  Defaults to 'changeit'"));
         registerParameter(new Parameter(TRUSTSTORE_KEY, "Truststore", MandatoryMode.MANDATORY, StandaloneMode.ALLOW, ParameterMode.ARGUMENT,
                 "Path to where the truststore file is written."));
     }
@@ -59,21 +74,26 @@ public class GetAdminTruststoreCommand extends BaseRolesCommand {
     }
 
     @Override
-    protected CommandResult execute(ParameterContainer parameters) {
+    protected CommandResult execute(final ParameterContainer parameters) {
         String format = parameters.get(FORMAT_KEY);
-        if (format == null)
+        if (format == null) {
             format = "PEM";
-        String truststore = parameters.get(TRUSTSTORE_KEY);
+        }
+        String password = parameters.get(PASSWORD_KEY);
+        if (password == null) {
+            password = "changeit";
+        }
+        final String truststore = parameters.get(TRUSTSTORE_KEY);
         
-        Set<X509Certificate> certs = new LinkedHashSet<>();
+        final Set<X509Certificate> certs = new LinkedHashSet<>();
         
         final List<Role> roles = EjbRemoteHelper.INSTANCE.getRemoteSession(RoleSessionRemote.class).getAuthorizedRoles(getAuthenticationToken());
         final RoleMemberSessionRemote roleMemberSession = EjbRemoteHelper.INSTANCE.getRemoteSession(RoleMemberSessionRemote.class);
         for (final Role role : roles) {
             try {
-                List<RoleMember> roleMembers = roleMemberSession.getRoleMembersByRoleId(getAuthenticationToken(), role.getRoleId());
-                for (RoleMember member : roleMembers) {
-                    int tokenIssuerId = member.getTokenIssuerId();
+                final List<RoleMember> roleMembers = roleMemberSession.getRoleMembersByRoleId(getAuthenticationToken(), role.getRoleId());
+                for (final RoleMember member : roleMembers) {
+                    final int tokenIssuerId = member.getTokenIssuerId();
                     if (tokenIssuerId != RoleMember.NO_ISSUER) {
                         final CAInfo info = EjbRemoteHelper.INSTANCE.getRemoteSession(CaSessionRemote.class).getCAInfo(getAuthenticationToken(),
                                 member.getTokenIssuerId());
@@ -84,14 +104,14 @@ public class GetAdminTruststoreCommand extends BaseRolesCommand {
                         });
                     }
                 }
-            } catch (AuthorizationDeniedException e) {
+            } catch (final AuthorizationDeniedException e) {
                 getLogger().info("Unable to access certificate for role:" + role.getRoleName());
             }
         }
         
         if (format.equals("PEM")) {
             try (FileOutputStream truststoreStream = new FileOutputStream(truststore)) {
-                for (X509Certificate certificate : certs) {
+                for (final X509Certificate certificate : certs) {
                     truststoreStream.write(CertTools.getPemFromCertificateChain(Collections.singleton(certificate)));
                 }
                 getLogger().info(truststore + " created.");
@@ -102,12 +122,12 @@ public class GetAdminTruststoreCommand extends BaseRolesCommand {
         } else {
             try {
                 final KeyStore keystore = KeyStore.getInstance(format);
-                keystore.load(null, "changeit".toCharArray());
-                for (X509Certificate certificate : certs) {
+                keystore.load(null, password.toCharArray());
+                for (final X509Certificate certificate : certs) {
                     keystore.setCertificateEntry(CertTools.getSHA256FingerprintAsString(certificate.getEncoded()), certificate);
                 }
                 try (FileOutputStream truststoreStream = new FileOutputStream(truststore)) {
-                    keystore.store(truststoreStream, "changeit".toCharArray());
+                    keystore.store(truststoreStream, password.toCharArray());
                 }
                 getLogger().info(truststore + " created.");
             } catch (NoSuchAlgorithmException | CertificateException | IOException | KeyStoreException e) {
@@ -122,8 +142,7 @@ public class GetAdminTruststoreCommand extends BaseRolesCommand {
     @Override
     public String getFullHelpText() {
         return "Create a truststore, in PKCS12, JKS or concatentated PEM format, containing \n"
-                + "all the certificates used to match roles.\n\n"
-                + "For PKCS12 and JKS, the truststore password is 'changeit'.";
+                + "all the certificates used to match roles.\n";
     }
 
 }
