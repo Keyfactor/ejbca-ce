15a4d6fb8aa3a7f41685128b66636541b3669fc4
Mark Wright <mark.wright@primekey.com>
2021-10-01 12:13:11 -0400

    ECA-10121 Remove check for call to getServerPort
    EjbcaWebBeanImpl made no use of that value, so the call was removed.

--- modules/admin-gui/src-test/org/ejbca/ui/web/admin/configuration/EjbcaWebBeanImplUnitTest.java
+++ modules/admin-gui/src-test/org/ejbca/ui/web/admin/configuration/EjbcaWebBeanImplUnitTest.java
@@ -176,7 +176,6 @@ public final class EjbcaWebBeanImplUnitTest {
     private void expectRequestGetters() {
         expect(mockedRequest.getScheme()).andReturn(HTTPS);
         expect(mockedRequest.getServerName()).andReturn(MOCKED_SERVER_NAME);
-        expect(mockedRequest.getServerPort()).andReturn(MOCKED_SERVER_PORT);
         expect(mockedRequest.getRemoteAddr()).andReturn(MOCKED_REMOTE_ADDR);
     }
     
