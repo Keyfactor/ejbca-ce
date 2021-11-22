1a1caa76dd766565e3e3751b18ebce8ecaacc321
Mark Wright <mark.wright@primekey.com>
2021-11-22 10:51:35 -0500

    ECA-10412 Fix warnings.
    1. Replace json-simple with compatible json-smart in HealthCheckServlet.
       json-smart is a drop-in replacement with better performance and
       support for post-java-1.4 language features.
    2. Remove unused imports
    3. Remove unused variables
    4. Wrap FileInputStream in try-with-resources
    5. Various "unchecked" suppressions
    6. Various template additions

--- modules/ejbca-webtest/src-test/org/ejbca/webtest/helper/RaWebHelper.java
+++ modules/ejbca-webtest/src-test/org/ejbca/webtest/helper/RaWebHelper.java
@@ -20,7 +20,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
