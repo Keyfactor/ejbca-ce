d252178f79a6901aadaa629f873103e1303db37f
Tanmoy Kundu <tanmoy.kundu@primekey.com>
2022-03-02 16:36:34 +0100

    ECA-10489: code cleanup

--- modules/ejbca-rest-api/src-test/org/ejbca/ui/web/rest/api/resource/CaRestResourceSystemTest.java
+++ modules/ejbca-rest-api/src-test/org/ejbca/ui/web/rest/api/resource/CaRestResourceSystemTest.java
@@ -14,17 +14,10 @@ import static org.ejbca.ui.web.rest.api.Assert.EjbcaAssert.assertJsonContentType
 import static org.ejbca.ui.web.rest.api.Assert.EjbcaAssert.assertProperJsonStatusResponse;
 import static org.junit.Assert.assertEquals;
 
-import java.io.InputStream;
-
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-import org.apache.log4j.Logger;
-import org.bouncycastle.util.encoders.Hex;
 import org.ejbca.config.GlobalConfiguration;
-import org.ejbca.its.ca.model.enroll.EcaEnrollResponseCode;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -36,8 +29,6 @@ import org.junit.Test;
  */
 public class CaRestResourceSystemTest extends RestResourceSystemTestBase {
     
-    private static final Logger log = Logger.getLogger(CaRestResourceSystemTest.class);
-
     @BeforeClass
     public static void beforeClass() throws Exception {
         RestResourceSystemTestBase.beforeClass();
@@ -56,7 +47,7 @@ public class CaRestResourceSystemTest extends RestResourceSystemTestBase {
     public void tearDown() {
     }
 
-    //@Test
+    @Test
     public void shouldReturnStatusInformation() throws Exception {
         // given
         final String expectedStatus = "OK";
@@ -69,37 +60,14 @@ public class CaRestResourceSystemTest extends RestResourceSystemTestBase {
         assertEquals(Status.OK.getStatusCode(), actualResponse.getStatus());
         assertJsonContentType(actualResponse);
         assertProperJsonStatusResponse(expectedStatus, expectedVersion, expectedRevision, actualJsonString);
-    }
-    
-    @Test
-    public void shouldReturnStatusInformation2() throws Exception {
-        // given
-        final String expectedStatus = "OK";
-        final String expectedVersion = "1.0";
-        final String expectedRevision = GlobalConfiguration.EJBCA_VERSION;
-        
-        byte[] bytebody = Hex.decode("ABCDEF123456ABCD04");
-        Entity<byte[]> requestEntity = Entity.entity(bytebody, "application/x-its-request");
-        // when
-        final Response actualResponse = newRequest("/its/etsi/enroll-certificate",
-                getBaseUrl().replace("ejbca-rest-api", "")).request().post(requestEntity);
-        InputStream is = actualResponse.readEntity(InputStream.class);
-        log.info(Hex.toHexString(is.readAllBytes()));
-        log.info(actualResponse.getHeaders());
-        log.info(EcaEnrollResponseCode.DECRYPTION_FAILED.toString());
-        
-        // then
-        assertEquals(Status.OK.getStatusCode(), actualResponse.getStatus());
-        assertJsonContentType(actualResponse);
-        //assertProperJsonStatusResponse(expectedStatus, expectedVersion, expectedRevision, actualJsonString);
-    }
+    }    
 
     /**
      * Disables REST and then runs a simple REST access test which will expect status 403 when
      * service is disabled by configuration.
      * @throws Exception 
      */
-    //@Test
+    @Test
     public void shouldRestrictAccessToRestResourceIfProtocolDisabled() throws Exception {
         // given
         disableRestProtocolConfiguration();
