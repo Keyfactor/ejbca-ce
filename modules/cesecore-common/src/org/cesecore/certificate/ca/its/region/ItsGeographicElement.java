d8881d6620ac8d3e67070dc3016cfd81ac5daa26
Tanmoy Kundu <tanmoy.kundu@primekey.com>
2022-03-18 13:46:26 +0000

    Resolve ECA-10599 "Fb  bc upgrade 171b15"

--- modules/cesecore-common/src/org/cesecore/certificate/ca/its/region/ItsGeographicElement.java
+++ modules/cesecore-common/src/org/cesecore/certificate/ca/its/region/ItsGeographicElement.java
@@ -1,3 +1,15 @@
+/*************************************************************************
+ *                                                                       *
+ *  CESeCore: CE Security Core                                           *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.cesecore.certificate.ca.its.region;
 
 import java.util.List;
@@ -29,6 +41,6 @@ public interface ItsGeographicElement {
      * may use any(any/all) logic while validating against sequence. 
      * @return
      */
-    //public boolean isSubregion(ItsGeographicElement requestedRegion);
+    public boolean isSubregion(ItsGeographicElement requestedRegion);
     
 }
