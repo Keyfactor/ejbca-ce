abc8d87215a47515744899223ecc1fbd7bc09f17
Andris Dobičinaitis <andris.dobicinaitis@keyfactor.com>
2022-06-16 12:24:14 +0300

    ECA-10792: Moved CA REST resource definitions to ejbca-rest-api module.

--- modules/ejbca-rest-ca/src/org/ejbca/ui/web/rest/api/resource/CaRestResource.java
+++ modules/ejbca-rest-ca/src/org/ejbca/ui/web/rest/api/resource/CaRestResource.java
@@ -25,22 +25,9 @@ import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
@@ -73,24 +60,18 @@ import org.ejbca.ui.web.rest.api.io.response.CaInfoRestResponse;
 import org.ejbca.ui.web.rest.api.io.response.CaInfosRestResponse;
 import org.ejbca.ui.web.rest.api.io.response.CreateCrlRestResponse;
 import org.ejbca.ui.web.rest.api.io.response.CrlRestResponse;
-import org.ejbca.ui.web.rest.api.io.response.RestResourceStatusRestResponse;
-
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 
 /**
  * JAX-RS resource handling CA related requests.
  *
  * @version $Id$
  */
-@Path("/v1/ca")
-@Produces(MediaType.APPLICATION_JSON)
 @Stateless
 @TransactionAttribute(TransactionAttributeType.SUPPORTS)
 public class CaRestResource extends BaseRestResource {
 
     private static final Logger log = Logger.getLogger(CaRestResource.class);
-    
+
     @EJB
     private RaMasterApiProxyBeanLocal raMasterApiProxy;
     @EJB
@@ -102,11 +83,6 @@ public class CaRestResource extends BaseRestResource {
     @EJB
     private ImportCrlSessionLocal importCrlSession;
 
-    @GET
-    @Path("/status")
-    @ApiOperation(value = "Get the status of this REST Resource",
-                  notes = "Returns status, API version and EJBCA version.",
-                  response = RestResourceStatusRestResponse.class)
     @Override
     public Response status() {
         return super.status();
@@ -116,12 +92,7 @@ public class CaRestResource extends BaseRestResource {
      * @param subjectDn CA subjectDn
      * @return PEM file with CA certificates
      */
-    @GET
-    @Path("/{subject_dn}/certificate/download")
-    @Produces(MediaType.WILDCARD)
-    @ApiOperation(value = "Get PEM file with the active CA certificate chain")
-    public Response getCertificateAsPem(@Context HttpServletRequest requestContext,
-                                        @ApiParam(value = "CAs subject DN", required = true) @PathParam("subject_dn") String subjectDn)
+    public Response getCertificateAsPem(final HttpServletRequest requestContext, String subjectDn)
             throws AuthorizationDeniedException, CertificateEncodingException, CADoesntExistsException, RestException {
         final AuthenticationToken admin = getAdmin(requestContext, false);
         subjectDn = CertTools.stringToBCDNString(subjectDn);
@@ -138,14 +109,9 @@ public class CaRestResource extends BaseRestResource {
      * Returns the Response containing the list of CAs with general information per CA as Json.
      *
      * @param httpServletRequest HttpServletRequest of a request.
-     *
      * @return The response containing the list of CAs and its general information.
      */
-    @GET
-    @ApiOperation(value = "Returns the Response containing the list of CAs with general information per CA as Json",
-        notes = "Returns the Response containing the list of CAs with general information per CA as Json",
-        response = CaInfosRestResponse.class)
-    public Response listCas(@Context final HttpServletRequest httpServletRequest) throws AuthorizationDeniedException, CADoesntExistsException, RestException {
+    public Response listCas(final HttpServletRequest httpServletRequest) throws AuthorizationDeniedException, CADoesntExistsException, RestException {
         final AuthenticationToken adminToken = getAdmin(httpServletRequest, false);
         List<CaInfoRestResponse> caInfoRestResponseList = CaInfosRestResponse.converter().toRestResponses(raMasterApiProxy.getAuthorizedCAInfos(adminToken));
         final CaInfosRestResponse caInfosRestResponse = CaInfosRestResponse.builder()
@@ -154,16 +120,10 @@ public class CaRestResource extends BaseRestResource {
         return Response.ok(caInfosRestResponse).build();
     }
 
-    @GET
-    @Path("/{issuer_dn}/getLatestCrl")
-    @ApiOperation(value = "Returns the latest CRL issued by this CA",
-            response = CrlRestResponse.class)
-    public Response getLatestCrl(@Context HttpServletRequest httpServletRequest,
-                                 @ApiParam(value = "the CRL issuers DN (CAs subject DN)", required = true) @PathParam("issuer_dn") String issuerDn,
-                                 @ApiParam(value = "true to get the latest deltaCRL, false to get the latest complete CRL", required = false, defaultValue = "false")
-                                 @QueryParam("deltaCrl") boolean deltaCrl,
-                                 @ApiParam(value = "the CRL partition index", required = false, defaultValue = "0")
-                                 @QueryParam("crlPartitionIndex") int crlPartitionIndex
+    public Response getLatestCrl(final HttpServletRequest httpServletRequest,
+                                 final String issuerDn,
+                                 final boolean deltaCrl,
+                                 final int crlPartitionIndex
     ) throws AuthorizationDeniedException, RestException, EjbcaException, CADoesntExistsException {
         final AuthenticationToken adminToken = getAdmin(httpServletRequest, true);
         RaCrlSearchRequest request = new RaCrlSearchRequest();
@@ -174,81 +134,62 @@ public class CaRestResource extends BaseRestResource {
         CrlRestResponse restResponse = CrlRestResponse.builder().setCrl(latestCrl).setResponseFormat("DER").build();
         return Response.ok(restResponse).build();
     }
-    
-    @POST
-    @Path("/{issuer_dn}/createcrl")
-    @ApiOperation(value = "Create CRL(main, partition and delta) issued by this CA", response=CreateCrlRestResponse.class)
-    public Response createCrl(@Context HttpServletRequest httpServletRequest,
-                                 @ApiParam(value = "the CRL issuers DN (CAs subject DN)", required = true) @PathParam("issuer_dn") String issuerDn,
-                                 @ApiParam(value = "true to also create the deltaCRL, false to only create the base CRL", required = false, defaultValue = "false")
-                                 @QueryParam("deltacrl") boolean deltacrl
+
+    public Response createCrl(final HttpServletRequest httpServletRequest, String issuerDn, final boolean deltacrl
     ) throws AuthorizationDeniedException, RestException, EjbcaException, CADoesntExistsException {
         final AuthenticationToken admin = getAdmin(httpServletRequest, false);
         issuerDn = issuerDn.trim();
         int caId = issuerDn.hashCode();
-        
+
         CAInfo cainfo = caSession.getCAInfo(admin, caId);
         if (cainfo == null) {
-            throw new RestException(Response.Status.BAD_REQUEST.getStatusCode(), 
+            throw new RestException(Response.Status.BAD_REQUEST.getStatusCode(),
                     "CA with DN: " + issuerDn + " does not exist.");
         }
-        
+
         CreateCrlRestResponse response = new CreateCrlRestResponse();
         response.setIssuerDn(issuerDn);
-        
+
         boolean result = true;
         try {
             result &= publishingCrlSession.forceCRL(admin, caId); // always generated
-            if(deltacrl) { // generated on top of base CRL
+            if (deltacrl) { // generated on top of base CRL
                 result &= publishingCrlSession.forceDeltaCRL(admin, caId);
             }
         } catch (CADoesntExistsException | CryptoTokenOfflineException | CAOfflineException e) {
-            throw new RestException(Response.Status.BAD_REQUEST.getStatusCode(), 
+            throw new RestException(Response.Status.BAD_REQUEST.getStatusCode(),
                     e.getMessage());
         }
         response.setAllSuccess(result);
-        response.setLatestCrlVersion(crlStoreSession.getLastCRLNumber(issuerDn, 
+        response.setLatestCrlVersion(crlStoreSession.getLastCRLNumber(issuerDn,
                 CertificateConstants.NO_CRL_PARTITION, false));
-        response.setLatestDeltaCrlVersion(crlStoreSession.getLastCRLNumber(issuerDn, 
+        response.setLatestDeltaCrlVersion(crlStoreSession.getLastCRLNumber(issuerDn,
                 CertificateConstants.NO_CRL_PARTITION, true));
-        
+
         final CAInfo caInfo = caSession.getCAInfo(admin, caId);
         IntRange crlPartitions = caInfo != null ? caInfo.getAllCrlPartitionIndexes() : null;
         if (crlPartitions != null) {
             Map<String, Integer> latestPartitionCrlVersions = new HashMap<>();
             Map<String, Integer> latestPartitionDeltaCrlVersions = new HashMap<>();
-            
-            for (int crlPartitionIndex = crlPartitions.getMinimumInteger(); 
-                    crlPartitionIndex <= crlPartitions.getMaximumInteger(); crlPartitionIndex++) {
-                latestPartitionCrlVersions.put("partition_" + crlPartitionIndex, 
+
+            for (int crlPartitionIndex = crlPartitions.getMinimumInteger();
+                 crlPartitionIndex <= crlPartitions.getMaximumInteger(); crlPartitionIndex++) {
+                latestPartitionCrlVersions.put("partition_" + crlPartitionIndex,
                         crlStoreSession.getLastCRLNumber(issuerDn, crlPartitionIndex, false));
                 // always included, CRL for deltaCrl or otherwise
-                latestPartitionDeltaCrlVersions.put("partition_" + crlPartitionIndex, 
+                latestPartitionDeltaCrlVersions.put("partition_" + crlPartitionIndex,
                         crlStoreSession.getLastCRLNumber(issuerDn, crlPartitionIndex, true));
             }
-            
+
             response.setLatestPartitionCrlVersions(latestPartitionCrlVersions);
             response.setLatestPartitionDeltaCrlVersions(latestPartitionDeltaCrlVersions);
         }
-        
+
         return Response.ok(response).build();
     }
 
-    @POST
-    @Path("/{issuer_dn}/importcrl")
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
-    @Produces(MediaType.APPLICATION_JSON)
-    @ApiOperation(value = "Import a certificate revocation list (CRL) for a CA")
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "CRL file was imported successfully"),
-            @ApiResponse(code = 400, message = "Error while importing CRL file"),
-    }
-    )
-    public Response importCrl(@Context final HttpServletRequest httpServletRequest,
-                              @ApiParam(value = "the CRL issuers DN (CAs subject DN)", required = true) @PathParam("issuer_dn") String issuerDn,
-                              @ApiParam("CRL file in DER format") @FormParam("crlFile") final File crlFile,
-                              @ApiParam("CRL partition index") @DefaultValue("0") @FormParam("crlPartitionIndex") int crlPartitionIndex
-    ) throws AuthorizationDeniedException, RestException {
+    public Response importCrl(final HttpServletRequest httpServletRequest, String issuerDn, final File crlFile, int crlPartitionIndex)
+            throws AuthorizationDeniedException, RestException {
         final AuthenticationToken admin = getAdmin(httpServletRequest, false);
         issuerDn = issuerDn.trim();
         final CAInfo cainfo = caSession.getCAInfo(admin, issuerDn.hashCode());
