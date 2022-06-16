f4055094543396328ed402eb8a1836eea318ba72
Andris Dobičinaitis <andris.dobicinaitis@keyfactor.com>
2022-06-16 10:48:56 +0300

    ECA-10798: Fixed the ability to enable/disable REST Certificate Management protocals in CE

--- modules/ejbca-common/src/org/ejbca/config/AvailableProtocolsConfiguration.java
+++ modules/ejbca-common/src/org/ejbca/config/AvailableProtocolsConfiguration.java
@@ -29,6 +29,7 @@ public class AvailableProtocolsConfiguration extends ConfigurationBase implement
     private static final float LATEST_VERSION = 2f;
 
     public static final String CONFIGURATION_ID = "AVAILABLE_PROTOCOLS";
+    private static final String REST_CERTIFICATE_MANAGEMENT_PROTOCOL_EE_ONLY_PATH = "/ejbca/ejbca-rest-api/v1/ca";
 
     /**
      * Protocols currently supporting enable/disable configuration by EJBCA
@@ -46,7 +47,7 @@ public class AvailableProtocolsConfiguration extends ConfigurationBase implement
         SCEP("SCEP", "/ejbca/publicweb/apply/scep"),
         RA_WEB("RA Web", "/ejbca/ra"),
         REST_CA_MANAGEMENT("REST CA Management", "/ejbca/ejbca-rest-api/v1/ca_management"),
-        REST_CERTIFICATE_MANAGEMENT("REST Certificate Management", "/ejbca/ejbca-rest-api/v1/ca<br/>/ejbca/ejbca-rest-api/v1/certificate"),
+        REST_CERTIFICATE_MANAGEMENT("REST Certificate Management", "/ejbca/ejbca-rest-api/v1/certificate"),
         REST_CRYPTOTOKEN_MANAGEMENT("REST Crypto Token Management", "/ejbca/ejbca-rest-api/v1/cryptotoken"),
         REST_ENDENTITY_MANAGEMENT("REST End Entity Management", "/ejbca/ejbca-rest-api/v1/endentity"),
         REST_ENDENTITY_MANAGEMENT_V2("REST End Entity Management V2", "/ejbca/ejbca-rest-api/v2/endentity"),
@@ -86,6 +87,21 @@ public class AvailableProtocolsConfiguration extends ConfigurationBase implement
         }
         
         public static String getContextPathByName(String name) {
+            return reverseLookupMap.get(name);
+        }
+
+        /**
+         * Returns protocol URLs that should be shown on configuration page.
+         * Method is used to hide the /ca REST endpoint from configuration page while
+         * only a subset of Certificate Management APIs are rolled out to Community edition.
+         * @param name Protocol name
+         * @param isEnterprise true/false depending on whether EE version is running
+         * @return Protocol paths
+         */
+        public static String getContextPathByName(String name, boolean isEnterprise) {
+            if (isEnterprise && REST_CERTIFICATE_MANAGEMENT.name.equals(name)) {
+                return REST_CERTIFICATE_MANAGEMENT_PROTOCOL_EE_ONLY_PATH + "<br/>" + reverseLookupMap.get(name);
+            }
             return reverseLookupMap.get(name);
         }
     }
