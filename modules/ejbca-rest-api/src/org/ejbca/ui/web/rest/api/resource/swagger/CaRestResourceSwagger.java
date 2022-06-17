5c96d4aafaebdfaca85d986cfb20feb8c8794d04
Andris Dobičinaitis <andris.dobicinaitis@keyfactor.com>
2022-06-17 10:28:49 +0300

    ECA-10792: Moved CA REST resource /createcrl and /importcrl API definitions to ejbca-rest-api module.

--- modules/ejbca-rest-api/src/org/ejbca/ui/web/rest/api/resource/swagger/CaRestResourceSwagger.java
+++ modules/ejbca-rest-api/src/org/ejbca/ui/web/rest/api/resource/swagger/CaRestResourceSwagger.java
@@ -13,6 +13,8 @@ package org.ejbca.ui.web.rest.api.resource.swagger;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import io.swagger.annotations.SwaggerDefinition;
 import io.swagger.annotations.SwaggerDefinition.Scheme;
 import org.cesecore.authorization.AuthorizationDeniedException;
@@ -20,13 +22,18 @@ import org.cesecore.certificates.ca.CADoesntExistsException;
 import org.ejbca.core.EjbcaException;
 import org.ejbca.ui.web.rest.api.exception.RestException;
 import org.ejbca.ui.web.rest.api.io.response.CaInfosRestResponse;
+import org.ejbca.ui.web.rest.api.io.response.CreateCrlRestResponse;
 import org.ejbca.ui.web.rest.api.io.response.CrlRestResponse;
 import org.ejbca.ui.web.rest.api.io.response.RestResourceStatusRestResponse;
 import org.ejbca.ui.web.rest.api.resource.CaRestResource;
 
 import javax.ejb.Stateless;
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -34,6 +41,7 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.File;
 import java.security.cert.CertificateEncodingException;
 
 /**
@@ -87,5 +95,34 @@ public class CaRestResourceSwagger extends CaRestResource {
                                  @QueryParam("crlPartitionIndex") int crlPartitionIndex
     ) throws AuthorizationDeniedException, RestException, EjbcaException, CADoesntExistsException {
         return super.getLatestCrl(httpServletRequest, issuerDn, deltaCrl, crlPartitionIndex);
+    }
+
+    @POST
+    @Path("/{issuer_dn}/createcrl")
+    @ApiOperation(value = "Create CRL(main, partition and delta) issued by this CA", response = CreateCrlRestResponse.class)
+    public Response createCrl(@Context HttpServletRequest httpServletRequest,
+                              @ApiParam(value = "the CRL issuers DN (CAs subject DN)", required = true) @PathParam("issuer_dn") String issuerDn,
+                              @ApiParam(value = "true to also create the deltaCRL, false to only create the base CRL", required = false, defaultValue = "false")
+                              @QueryParam("deltacrl") boolean deltacrl
+    ) throws AuthorizationDeniedException, RestException, EjbcaException, CADoesntExistsException {
+        return super.createCrl(httpServletRequest, issuerDn, deltacrl);
+    }
+
+    @POST
+    @Path("/{issuer_dn}/importcrl")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Import a certificate revocation list (CRL) for a CA")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "CRL file was imported successfully"),
+            @ApiResponse(code = 400, message = "Error while importing CRL file"),
+    }
+    )
+    public Response importCrl(@Context final HttpServletRequest httpServletRequest,
+                              @ApiParam(value = "the CRL issuers DN (CAs subject DN)", required = true) @PathParam("issuer_dn") String issuerDn,
+                              @ApiParam("CRL file in DER format") @FormParam("crlFile") final File crlFile,
+                              @ApiParam("CRL partition index") @DefaultValue("0") @FormParam("crlPartitionIndex") int crlPartitionIndex
+    ) throws AuthorizationDeniedException, RestException {
+        return super.importCrl(httpServletRequest, issuerDn, crlFile, crlPartitionIndex);
     }
 }
