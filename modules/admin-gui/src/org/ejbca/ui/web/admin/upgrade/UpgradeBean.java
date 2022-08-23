9e691652d69e9e28c361c947835262aa2f985e14
Samuel Lidén Borell <samuel.liden.borell@keyfactor.com>
2022-08-23 16:05:42 +0200

    ECA-10908: Fix detection of upgrade failures. Fix log level

--- modules/admin-gui/src/org/ejbca/ui/web/admin/upgrade/UpgradeBean.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/upgrade/UpgradeBean.java
@@ -105,7 +105,7 @@ public class UpgradeBean extends BaseManagedBean implements Serializable {
 
     /** @return true if an upgrade is currently in progress on this node */
     public boolean isPostUpgradeFailed() {
-        return isActionStartUpgradeAllowed() && !getLogged().isEmpty();
+        return isActionStartUpgradeAllowed() && getLogged().stream().anyMatch(logEvent -> logEvent.isLevelError());
     }
 
     /** @return true if an upgrade is currently in progress on any node */
