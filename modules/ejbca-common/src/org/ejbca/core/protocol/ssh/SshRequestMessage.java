ef96abb8c36e070482968f5ca50f7b28ed07e054
Tanmoy Kundu <tanmoy.kundu@primekey.com>
2022-09-20 09:38:43 +0000

    Resolve ECA-9261 "Fb  enroll ssh ra web"

--- modules/ejbca-common/src/org/ejbca/core/protocol/ssh/SshRequestMessage.java
+++ modules/ejbca-common/src/org/ejbca/core/protocol/ssh/SshRequestMessage.java
@@ -24,18 +24,23 @@ import java.security.PublicKey;
 import java.security.cert.Certificate;
 import java.security.spec.InvalidKeySpecException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
 import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.asn1.x509.Extensions;
-import org.bouncycastle.util.encoders.Base64;
 import org.cesecore.certificates.certificate.request.RequestMessage;
+import org.cesecore.certificates.certificate.ssh.SshEndEntityProfileFields;
 import org.cesecore.certificates.certificate.ssh.SshKeyException;
 import org.cesecore.certificates.certificate.ssh.SshKeyFactory;
 import org.cesecore.certificates.certificate.ssh.SshPublicKey;
+import org.cesecore.certificates.certificateprofile.CertificateProfile;
+import org.cesecore.certificates.endentity.EndEntityInformation;
+import org.cesecore.certificates.endentity.ExtendedInformation;
 import org.cesecore.keys.util.KeyTools;
 
 /**
@@ -47,13 +52,13 @@ public class SshRequestMessage implements RequestMessage {
     
     private final String keyId;
     private final String comment;
-    private final byte[] publicKey;
+    private byte[] publicKey;
     private final List<String> principals;
     private final Map<String, String> criticalOptions;
     private final Map<String, byte[]> additionalExtensions;
     private String username;
     private transient String serialNumber;
-    private transient String password;
+    private String password;
 
 
     public SshRequestMessage(final PublicKey publicKey, final String keyId, List<String> principals, final Map<String, byte[]> additionalExtensions,
@@ -76,6 +81,59 @@ public class SshRequestMessage implements RequestMessage {
         this.additionalExtensions = (additionalExtensions != null ? additionalExtensions : new HashMap<>());
     }
     
+    protected SshRequestMessage(String subjectDn, String subjectAlternateName, ExtendedInformation ei) {
+        if(ei==null) {
+            throw new IllegalStateException("SSH request message is absent as extended information is null.");
+        }
+        if(StringUtils.isNotBlank(subjectDn)) {
+            this.keyId = subjectDn.substring("CN=".length());
+        } else {
+            this.keyId = "";
+        }
+        List<String> principals = new ArrayList<>();
+        
+        if(StringUtils.isNotBlank(subjectAlternateName)) {
+            subjectAlternateName = subjectAlternateName.substring("dnsName=".length());
+            int commentIndex = subjectAlternateName.indexOf(SshEndEntityProfileFields.SSH_CERTIFICATE_COMMENT);
+            if(commentIndex!=0) { // no principal
+                if(commentIndex==-1) {
+                    commentIndex = subjectAlternateName.length(); // principal is whole content
+                    this.comment = "";
+                } else {
+                    this.comment = subjectAlternateName.substring(commentIndex + 
+                            SshEndEntityProfileFields.SSH_CERTIFICATE_COMMENT.length() + 1);
+                    commentIndex--;
+                }
+                String allPrincipals = subjectAlternateName.substring(SshEndEntityProfileFields.SSH_PRINCIPAL.length()+1, commentIndex);
+                principals = Arrays.asList(allPrincipals.split(":"));
+            } else {
+                this.comment = subjectAlternateName.substring(SshEndEntityProfileFields.SSH_CERTIFICATE_COMMENT.length() + 1);
+            }
+            
+        } else {
+            this.comment = "";
+        }
+        
+        this.principals = principals;
+        this.criticalOptions = ei.getSshCriticalOptions();
+        this.additionalExtensions = ei.getSshExtensions();
+    }
+    
+    public SshRequestMessage(byte[] sshPublicKey, String subjectDn, String subjectAlternateName, ExtendedInformation ei) {
+        
+        this(subjectDn, subjectAlternateName, ei);
+        
+        try {
+            SshPublicKey pubKey = SshKeyFactory.INSTANCE.extractSshPublicKeyFromFile(sshPublicKey);
+            sshPublicKey = pubKey.encode();
+        } catch (InvalidKeySpecException | SshKeyException | IOException e) {
+            throw new IllegalStateException("SSH public key parsing failed.", e);
+        } 
+        
+        this.publicKey = sshPublicKey;
+                
+    }
+
     public byte[] getEncoded() throws IOException {
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         ObjectOutput out;
@@ -171,11 +229,15 @@ public class SshRequestMessage implements RequestMessage {
             return result;
         } else {
             try {
-                byte[] keyBody = new String(publicKey).split(" ")[1].getBytes();     
-                SshPublicKey sshPublicKey = SshKeyFactory.INSTANCE.getSshPublicKey(Base64.decode(keyBody));
+                SshPublicKey sshPublicKey = SshKeyFactory.INSTANCE.extractSshPublicKeyFromFile(publicKey);
                 return sshPublicKey.getPublicKey();
-            } catch (InvalidKeySpecException | SshKeyException | ArrayIndexOutOfBoundsException e) {
-               throw new InvalidKeyException(e);
+            } catch (Exception e) {
+                try {
+                    SshPublicKey sshPublicKey = SshKeyFactory.INSTANCE.getSshPublicKey(publicKey);
+                    return sshPublicKey.getPublicKey();
+                } catch (Exception e2) {
+                   throw new InvalidKeyException(e2);
+                }
             }
         }
     }
@@ -302,5 +364,50 @@ public class SshRequestMessage implements RequestMessage {
     @Override
     public void setRequestValidityNotAfter(Date notAfter) {
     }
-
+    
+    public void populateEndEntityData(EndEntityInformation userdata, CertificateProfile cerificateProfile) {
+                
+        if(userdata.getExtendedInformation()==null) {
+            userdata.setExtendedInformation(new ExtendedInformation());
+        }
+        // these serve as an indicator to SSH end entity
+        userdata.setSshEndEntity(true);
+        userdata.getExtendedInformation().setSshCustomData(
+                SshEndEntityProfileFields.SSH_CERTIFICATE_TYPE, cerificateProfile.getSshCertificateType().getLabel());
+        
+        if(StringUtils.isNotBlank(this.keyId)) {
+            userdata.setDN("CN=" + this.keyId);
+        } else {
+            userdata.setDN("CN="); // will set to blank DN
+        }
+        StringBuilder placeHolderSan = new StringBuilder();
+        if(getPrincipals()!=null && !getPrincipals().isEmpty()) {
+            placeHolderSan.append(SshEndEntityProfileFields.SSH_PRINCIPAL + ":");
+            for(String principal: getPrincipals()) {
+                if(StringUtils.isNotBlank(principal)) {
+                    placeHolderSan.append(principal);
+                    placeHolderSan.append(":");
+                }
+            }
+        }
+        
+        if(StringUtils.isNotBlank(getComment())) {
+            placeHolderSan.append(SshEndEntityProfileFields.SSH_CERTIFICATE_COMMENT + ":");
+            placeHolderSan.append(getComment());
+        }
+        
+        String placeHolderSanString = placeHolderSan.toString();
+        if(StringUtils.isNotBlank(placeHolderSanString)) {
+            userdata.setSubjectAltName("dnsName=" + placeHolderSanString);
+        }
+        
+        if(getCriticalOptions()!=null) {
+            userdata.getExtendedInformation().setSshCriticalOptions(getCriticalOptions());
+        }
+        
+        if(getAdditionalExtensions()!=null) {
+            userdata.getExtendedInformation().setSshExtensions(getAdditionalExtensions());
+        }
+    }
+    
 }
