cbe62c7726ee76427e5cc4ba2d44f21373c54385
Lauri Kongas <lauri.kongas.ext@primekey.com>
2022-09-20 16:23:37 +0300

    ECA-10944 Fix failing upgrade tests on MSSQL job

--- modules/ejbca-ejb-interface/src/org/ejbca/core/ejb/upgrade/UpgradeSessionLocal.java
+++ modules/ejbca-ejb-interface/src/org/ejbca/core/ejb/upgrade/UpgradeSessionLocal.java
@@ -77,7 +77,7 @@ public interface UpgradeSessionLocal  extends UpgradeSession{
     /** For internal user from UpgradeSessionBean only */
     void migrateDatabase7100() throws UpgradeFailedException;
     /** For internal user from UpgradeSessionBean only */
-    void fixPartitionedCrls(final int limit) throws UpgradeFailedException;
+    void fixPartitionedCrls(final int limit, final boolean isMSSQL) throws UpgradeFailedException;
     /** For internal user from UpgradeSessionBean only */
     IndexUpgradeResult upgradeIndex(final String oldIndexName, final String tableName, final String createIndexQuery);
     
