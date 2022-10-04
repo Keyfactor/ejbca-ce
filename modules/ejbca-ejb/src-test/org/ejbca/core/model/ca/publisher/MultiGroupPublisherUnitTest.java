7e9ea2a0783ec97858ec4d25e8bf19eeb00fef27
Andris Dobičinaitis <andris.dobicinaitis@keyfactor.com>
2022-10-04 10:00:21 +0300

    ECA-6776: Fixed publishing of reactivated certificates when setting "Publish only revoked certificates" is used.

--- modules/ejbca-ejb/src-test/org/ejbca/core/model/ca/publisher/MultiGroupPublisherUnitTest.java
+++ modules/ejbca-ejb/src-test/org/ejbca/core/model/ca/publisher/MultiGroupPublisherUnitTest.java
@@ -83,8 +83,8 @@ public class MultiGroupPublisherUnitTest {
     private static final class MockPublishRevokedOnlyPublisher extends BasePublisher {
         private static final long serialVersionUID = 1L;
         @Override
-        public boolean willPublishCertificate(int status, int revocationReason) {
-            return (status == CertificateConstants.CERT_REVOKED || revocationReason == RevokedCertInfo.REVOCATION_REASON_REMOVEFROMCRL);
+        public boolean willPublishCertificate(int status, long revocationDate) {
+            return (status == CertificateConstants.CERT_REVOKED || (status == CertificateConstants.CERT_ACTIVE && revocationDate != -1L));
         }
         @Override
         public boolean storeCertificate(AuthenticationToken admin, Certificate incert, String username, String password, String userDN, String cafp,
@@ -184,7 +184,7 @@ public class MultiGroupPublisherUnitTest {
         }
         replay(publisherSession);
         // Run and verify
-        assertEquals("Wrong result from willPublishCertificate", expectedWillPublishResult, publisher.willPublishCertificate(certData.getStatus(), certData.getRevocationReason()));
+        assertEquals("Wrong result from willPublishCertificate", expectedWillPublishResult, publisher.willPublishCertificate(certData.getStatus(), certData.getRevocationDate()));
         assertEquals("Wrong result from storeCertificate", true, publisher.storeCertificate(admin, certData, b64CertData, EE_PASSWORD, EE_DN, extinfo));
         verify(publisherSession);
     }
