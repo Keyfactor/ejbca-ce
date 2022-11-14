ae048faa366c82de6016bbf43bd31797235f5600
andresj <andresj@primekey.com>
2022-11-14 17:14:40 +0100

    ECA-11095 Client certificate for ACME over peers.

--- modules/ejbca-ejb/src/org/ejbca/core/model/era/RaMasterApiSessionBean.java
+++ modules/ejbca-ejb/src/org/ejbca/core/model/era/RaMasterApiSessionBean.java
@@ -162,6 +162,7 @@ import org.ejbca.core.ejb.approval.ApprovalExecutionSessionLocal;
 import org.ejbca.core.ejb.approval.ApprovalProfileSessionLocal;
 import org.ejbca.core.ejb.approval.ApprovalSessionLocal;
 import org.ejbca.core.ejb.authentication.cli.CliAuthenticationTokenMetaData;
+import org.ejbca.core.ejb.authentication.web.WebAuthenticationProviderSessionLocal;
 import org.ejbca.core.ejb.authorization.AuthorizationSystemSessionLocal;
 import org.ejbca.core.ejb.ca.auth.EndEntityAuthenticationSessionLocal;
 import org.ejbca.core.ejb.ca.caadmin.CAAdminSessionLocal;
@@ -331,6 +332,8 @@ public class RaMasterApiSessionBean implements RaMasterApiSessionLocal {
     private AcmeChallengeDataSessionLocal acmeChallengeDataSession;
     @EJB
     private EtsiEcaOperationsSessionLocal ecaOperationsSession;
+    @EJB
+    private WebAuthenticationProviderSessionLocal webAuthenticationProviderSession;
 
     @PersistenceContext(unitName = CesecoreConfiguration.PERSISTENCE_UNIT)
     private EntityManager entityManager;
@@ -395,6 +398,11 @@ public class RaMasterApiSessionBean implements RaMasterApiSessionLocal {
     @Override
     public boolean isAuthorizedNoLogging(AuthenticationToken authenticationToken, String... resources) {
         return authorizationSession.isAuthorizedNoLogging(authenticationToken, resources);
+    }
+    
+    @Override
+    public AuthenticationToken authenticateUsingClientCertificate(final X509Certificate certificate) {
+        return webAuthenticationProviderSession.authenticateUsingClientCertificate(certificate);
     }
 
     @Override
