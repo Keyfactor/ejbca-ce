6d8573c06a689b576ce2b396ba39c8309bcde620
Tanmoy Kundu <tanmoy.kundu@primekey.com>
2022-11-24 14:00:26 +0100

    ECA-11081: addressed review comments

--- modules/plugins/src-test/org/ejbca/core/services/worker/CertificateCrlReaderUnitTest.java
+++ modules/plugins/src-test/org/ejbca/core/services/worker/CertificateCrlReaderUnitTest.java
@@ -10,7 +10,7 @@
  *  See terms of license at gnu.org.                                     *
  *                                                                       *
  *************************************************************************/
-package org.ejbca.core.model.services.worker;
+package org.ejbca.core.services.worker;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
