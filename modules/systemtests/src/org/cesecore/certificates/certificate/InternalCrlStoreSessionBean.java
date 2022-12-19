01302b1c0056ca0884733b3f6b177c20fec3c091
Amin Khorsandi <akhorsandi@keyfactor.com>
2022-12-19 16:15:48 +0100

    ECA-11196: CRL data refactoring.

--- modules/systemtests/src/org/cesecore/certificates/certificate/InternalCrlStoreSessionBean.java
+++ modules/systemtests/src/org/cesecore/certificates/certificate/InternalCrlStoreSessionBean.java
@@ -12,41 +12,25 @@
  *************************************************************************/
 package org.cesecore.certificates.certificate;
 
-import java.util.List;
-
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-import org.cesecore.certificates.crl.CRLData;
 import org.cesecore.certificates.crl.CrlStoreSessionLocal;
-import org.cesecore.config.CesecoreConfiguration;
 import org.cesecore.jndi.JndiConstants;
 
 /**
- * @version $Id$
+ * 
  */
 @Stateless(mappedName = JndiConstants.APP_JNDI_PREFIX + "InternalCrlStoreSessionRemote")
 @TransactionAttribute(TransactionAttributeType.REQUIRED)
 public class InternalCrlStoreSessionBean implements InternalCrlStoreSessionRemote {
-
-
-    @PersistenceContext(unitName = CesecoreConfiguration.PERSISTENCE_UNIT)
-    private EntityManager entityManager;
 
     @EJB
     private CrlStoreSessionLocal crlStoreSession;
 
     @Override
     public void removeCrl(final String issuerDN) {
-        List<CRLData> crls =  CRLData.findByIssuerDN(entityManager, issuerDN);
-        for(CRLData crlData : crls) {
-            this.entityManager.remove(crlData);
-        }
+        crlStoreSession.removeByIssuerDN(issuerDN);
     }
-
-
 }
