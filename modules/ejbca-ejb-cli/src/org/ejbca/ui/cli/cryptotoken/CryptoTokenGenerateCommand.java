56765708a2a17fe392a7529455a283d6d4e527cf
Mike Kushner <mikek.kushner@keyfactor.com>
2023-01-25 10:41:58 +0100

    ECA-11265: Cleaned out a deprecate method from CryptoTokenManagementSession

--- modules/ejbca-ejb-cli/src/org/ejbca/ui/cli/cryptotoken/CryptoTokenGenerateCommand.java
+++ modules/ejbca-ejb-cli/src/org/ejbca/ui/cli/cryptotoken/CryptoTokenGenerateCommand.java
@@ -16,6 +16,7 @@ import org.apache.log4j.Logger;
 import org.cesecore.authorization.AuthorizationDeniedException;
 import org.cesecore.keys.token.CryptoTokenManagementSessionRemote;
 import org.cesecore.keys.token.CryptoTokenOfflineException;
+import org.cesecore.keys.token.KeyGenParams;
 import org.cesecore.util.EjbRemoteHelper;
 import org.ejbca.ui.cli.infrastructure.command.CommandResult;
 import org.ejbca.ui.cli.infrastructure.parameter.Parameter;
@@ -54,7 +55,7 @@ public class CryptoTokenGenerateCommand extends BaseCryptoTokenCommand {
         final String keyPairSpecification = parameters.get(KEY_SPECIFICATION_KEY);
         try {
             EjbRemoteHelper.INSTANCE.getRemoteSession(CryptoTokenManagementSessionRemote.class).createKeyPair(getAdmin(), cryptoTokenId,
-                    keyPairAlias, keyPairSpecification);
+                    keyPairAlias, KeyGenParams.builder(keyPairSpecification).build());
             getLogger().info("Key pair generated successfully.");
             return CommandResult.SUCCESS;
         } catch (AuthorizationDeniedException e) {
