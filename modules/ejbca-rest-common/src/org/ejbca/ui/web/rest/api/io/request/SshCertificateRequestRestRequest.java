6c8e748ee25c6eeead0c90f3a083be40df6a86b8
Taher <taher.uddin.ext@primekey.com>
2023-02-01 22:20:54 +0200

    ECA-9260 added file header

--- modules/ejbca-rest-common/src/org/ejbca/ui/web/rest/api/io/request/SshCertificateRequestRestRequest.java
+++ modules/ejbca-rest-common/src/org/ejbca/ui/web/rest/api/io/request/SshCertificateRequestRestRequest.java
@@ -1,3 +1,15 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.ejbca.ui.web.rest.api.io.request;
 
 import io.swagger.annotations.ApiModelProperty;
@@ -19,7 +31,6 @@ import java.util.Map;
  */
 @ValidSshCertificateRestRequest
 public class SshCertificateRequestRestRequest {
-    private static final long serialVersionUID = 1L;
 
     @ApiModelProperty(value = "End Entity profile name", example = "ExampleEEP")
     private String endEntityProfile;
