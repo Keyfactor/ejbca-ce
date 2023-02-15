3e393241d2e5a07ba2595d85aa93c365d07e1246
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2023-02-15 08:15:11 +0100

    ECA-11300: allow key identifiers to be used as custom extensions

--- modules/cesecore-common/src/org/cesecore/certificates/certificate/certextensions/standard/SubjectKeyIdentifier.java
+++ modules/cesecore-common/src/org/cesecore/certificates/certificate/certextensions/standard/SubjectKeyIdentifier.java
@@ -13,6 +13,8 @@
 package org.cesecore.certificates.certificate.certextensions.standard;
 
 import java.security.PublicKey;
+import java.util.Collections;
+import java.util.Map;
 
 import org.bouncycastle.asn1.ASN1Encodable;
 import org.bouncycastle.asn1.x509.Extension;
@@ -22,31 +24,44 @@ import org.bouncycastle.cert.bc.BcX509ExtensionUtils;
 import org.cesecore.certificates.ca.CA;
 import org.cesecore.certificates.ca.internal.CertificateValidity;
 import org.cesecore.certificates.certificate.certextensions.CertificateExtensionException;
+import org.cesecore.certificates.certificate.certextensions.CustomCertificateExtension;
 import org.cesecore.certificates.certificateprofile.CertificateProfile;
 import org.cesecore.certificates.endentity.EndEntityInformation;
 
 /** 
- * 
  * Class for standard X509 certificate extension. 
- * See rfc3280 or later for spec of this extension.      
- * 
- * @version $Id$
+ * See rfc5280 or later for spec of this extension.      
  */
-public class SubjectKeyIdentifier extends StandardCertificateExtension {
+public class SubjectKeyIdentifier extends StandardCertificateExtension implements CustomCertificateExtension {
 
     private static final long serialVersionUID = 1L;
 
-    @Override
-	public void init(final CertificateProfile certProf) {
-		super.setOID(Extension.subjectKeyIdentifier.getId());
-		super.setCriticalFlag(certProf.getSubjectKeyIdentifierCritical());
-	}
+    public SubjectKeyIdentifier() {
+        super.setDisplayName("SubjectKeyIdentifier");
+        
+    }
     
     @Override
+    public void init(final CertificateProfile certProf) {
+        super.setOID(Extension.subjectKeyIdentifier.getId());
+        super.setCriticalFlag(certProf.getSubjectKeyIdentifierCritical());
+    }
+    
     public ASN1Encodable getValue(final EndEntityInformation subject, final CA ca, final CertificateProfile certProfile,
             final PublicKey userPublicKey, final PublicKey caPublicKey, CertificateValidity val) throws CertificateExtensionException {
         SubjectPublicKeyInfo spki = SubjectPublicKeyInfo.getInstance(userPublicKey.getEncoded());
         X509ExtensionUtils x509ExtensionUtils = new BcX509ExtensionUtils();
         return x509ExtensionUtils.createSubjectKeyIdentifier(spki);
+    }
+
+    @Override
+    public Map<String, String[]> getAvailableProperties() {
+        return Collections.emptyMap();
+    }
+
+    @Override
+    public byte[] getValueEncoded(EndEntityInformation userData, CA ca, CertificateProfile certProfile, PublicKey userPublicKey,
+            PublicKey caPublicKey, CertificateValidity val, String oid) throws CertificateExtensionException {
+        return super.getValueEncoded(userData, ca, certProfile, userPublicKey, caPublicKey, val);
     }
 }
