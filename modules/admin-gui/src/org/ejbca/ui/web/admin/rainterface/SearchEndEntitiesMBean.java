fdc0d3de0ffd6c0c956d648971425f42eeebde6f
Mike Kushner <mikek.kushner@keyfactor.com>
2023-04-11 12:43:31 +0200

    ECA-11442: Interim commit

---
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/rainterface/SearchEndEntitiesMBean.java
@@ -0,0 +1,79 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.ui.web.admin.rainterface;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+
+import org.ejbca.core.model.authorization.AccessRulesConstants;
+import org.ejbca.ui.web.admin.BaseManagedBean;
+
+
+/**
+ * Backing bean for the Search End Entities page in the CA UI. 
+ */
+@Named("searchEndEntitiesMBean")
+@RequestScoped
+public class SearchEndEntitiesMBean extends BaseManagedBean implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String selectedTab = null; 
+    private String searchByName = null;
+
+    public SearchEndEntitiesMBean() {
+        super(AccessRulesConstants.ROLE_ADMINISTRATOR, AccessRulesConstants.REGULAR_EDITUSERDATASOURCES);
+    }
+    
+    public List<String> getAvailableTabs() {
+        return Arrays.asList("Basic", "Advanced");
+    }
+    
+    public String getSelectedTab() {
+        final String tabHttpParam = ((HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest()).getParameter("tab");
+        // First, check if the user has requested a valid tab
+        List<String> availableTabs = getAvailableTabs();
+        if (tabHttpParam != null && availableTabs.contains(tabHttpParam)) {
+            // The requested tab is an existing tab. Flush caches so we reload the page content
+            flushCache();
+            selectedTab = tabHttpParam;
+        }
+        if (selectedTab == null) {
+            // If no tab was requested, we use the first available tab as default
+            selectedTab = availableTabs.get(0);
+        }
+        return selectedTab;
+    }
+    
+    public void flushCache() {
+        
+    }
+
+    public String getSearchByName() {
+        return searchByName;
+    }
+
+    public void setSearchByName(String searchByName) {
+        this.searchByName = searchByName;
+    }
+    
+    public void performSearch() {
+        
+    }
+
+}
