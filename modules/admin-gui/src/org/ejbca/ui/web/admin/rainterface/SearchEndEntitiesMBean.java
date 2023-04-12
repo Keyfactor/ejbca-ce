4d8af1899c5b550c9e29f5d4590430f9144ce55b
Mike Kushner <mikek.kushner@keyfactor.com>
2023-04-12 10:44:26 +0200

    ECA-11442: Interim commit

--- modules/admin-gui/src/org/ejbca/ui/web/admin/rainterface/SearchEndEntitiesMBean.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/rainterface/SearchEndEntitiesMBean.java
@@ -13,17 +13,32 @@
 package org.ejbca.ui.web.admin.rainterface;
 
 import java.io.Serializable;
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
+import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.model.ListDataModel;
 import javax.inject.Named;
 import javax.servlet.http.HttpServletRequest;
 
+import org.cesecore.authorization.AuthorizationDeniedException;
+import org.cesecore.certificates.ca.CaSessionLocal;
+import org.cesecore.certificates.certificate.CertificateData;
+import org.cesecore.certificates.certificate.CertificateDataWrapper;
+import org.cesecore.certificates.certificate.CertificateStoreSessionLocal;
+import org.cesecore.certificates.endentity.EndEntityConstants;
+import org.cesecore.certificates.endentity.EndEntityInformation;
+import org.ejbca.core.ejb.ra.EndEntityAccessSessionLocal;
 import org.ejbca.core.model.authorization.AccessRulesConstants;
 import org.ejbca.ui.web.admin.BaseManagedBean;
 
+import com.keyfactor.util.CertTools;
+import com.keyfactor.util.StringTools;
 
 /**
  * Backing bean for the Search End Entities page in the CA UI. 
@@ -33,17 +48,27 @@ import org.ejbca.ui.web.admin.BaseManagedBean;
 public class SearchEndEntitiesMBean extends BaseManagedBean implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    private String selectedTab = null; 
+    @EJB
+    private CaSessionLocal caSession;
+    @EJB
+    private CertificateStoreSessionLocal certificateStoreSession;
+    @EJB
+    private EndEntityAccessSessionLocal endEntityAccessSession;
+    
+    private String selectedTab = null;
     private String searchByName = null;
+    private String searchBySerialNumber = null;
+
+    private ListDataModel<EndEntititySearchResult> searchResults = new ListDataModel<>();
 
     public SearchEndEntitiesMBean() {
         super(AccessRulesConstants.ROLE_ADMINISTRATOR, AccessRulesConstants.REGULAR_EDITUSERDATASOURCES);
     }
-    
+
     public List<String> getAvailableTabs() {
         return Arrays.asList("Basic", "Advanced");
     }
-    
+
     public String getSelectedTab() {
         final String tabHttpParam = ((HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest()).getParameter("tab");
         // First, check if the user has requested a valid tab
@@ -59,9 +84,11 @@ public class SearchEndEntitiesMBean extends BaseManagedBean implements Serializa
         }
         return selectedTab;
     }
-    
+
     public void flushCache() {
-        
+        searchResults = new ListDataModel<>();
+        searchByName = null;
+        searchBySerialNumber = null;
     }
 
     public String getSearchByName() {
@@ -71,9 +98,75 @@ public class SearchEndEntitiesMBean extends BaseManagedBean implements Serializa
     public void setSearchByName(String searchByName) {
         this.searchByName = searchByName;
     }
+
+    public void performSearchByName() {
+        Map<Integer, String> caIdToNameMap = caSession.getCAIdToNameMap();
+        EndEntityInformation endEntityInformation;
+        try {
+            endEntityInformation = endEntityAccessSession.findUser(getAdmin(), searchByName);
+            if(endEntityInformation == null) {
+                addNonTranslatedErrorMessage("No end entity with name " + searchByName + " found.");
+            } else {
+            String caName = caIdToNameMap.get(endEntityInformation.getCAId());
+            EndEntititySearchResult endEntititySearchResult = new EndEntititySearchResult(endEntityInformation, caName);
+            
+            this.searchResults = new ListDataModel<>(Arrays.asList(endEntititySearchResult));
+            }
+        } catch (AuthorizationDeniedException e) {
+            addNonTranslatedErrorMessage(e.getMessage());
+        }     
+    }
     
-    public void performSearch() {
+    public void performSearchBySerialNumber() {
+        final BigInteger serno = new BigInteger(StringTools.stripWhitespace(searchBySerialNumber), 16);
+        final List<CertificateDataWrapper> certificateDataWrappers = certificateStoreSession.getCertificateDataBySerno(serno);             
+        List<EndEntititySearchResult> results = new ArrayList<>();
+        for (final CertificateDataWrapper next : certificateDataWrappers) {
+            final CertificateData certdata = next.getCertificateData();
+            try {
+                final String username = certdata.getUsername();
+                if (username != null) {
+                    final EndEntityInformation endEntityInformation = endEntityAccessSession.findUser(getAdmin(), username);
+                    if (endEntityInformation != null) {
+                        results.add(new EndEntititySearchResult(endEntityInformation, username));
+                    }
+                }
+            } catch (AuthorizationDeniedException e) {
+                addNonTranslatedErrorMessage(e.getMessage());
+            }
+        }
+        this.searchResults = new ListDataModel<>(results);
+    }
+
+    public ListDataModel<EndEntititySearchResult> getSearchResults() {
+        return searchResults;
+    }
+
+    public class EndEntititySearchResult {
+        private final EndEntityInformation endEntityInformation;
+        private final String caName;
+
+        public EndEntititySearchResult(final EndEntityInformation endEntityInformation, final String caName) {
+            this.endEntityInformation = endEntityInformation;
+            this.caName = caName;
+        }
         
+        public String getUsername() {
+            return endEntityInformation.getUsername();
+        }
+        
+        public String getCaName() {
+            return caName;
+        }
+        
+        public String getCommonName() {
+           return CertTools.getCommonNameFromSubjectDn(endEntityInformation.getCertificateDN()); 
+        }
+        
+        public String getStatus() {
+            return EndEntityConstants.getStatusText(endEntityInformation.getStatus());
+        }
+
     }
 
 }
