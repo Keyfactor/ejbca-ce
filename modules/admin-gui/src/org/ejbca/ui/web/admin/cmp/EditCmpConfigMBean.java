debadb2390bd0bfa73c2c674927f59b3c9251d20
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2023-05-02 09:29:40 +0200

    ECA-11457: comment that we don't want people to use uniqueIdentifier

--- modules/admin-gui/src/org/ejbca/ui/web/admin/cmp/EditCmpConfigMBean.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/cmp/EditCmpConfigMBean.java
@@ -51,6 +51,7 @@ public class EditCmpConfigMBean extends BaseManagedBean implements Serializable 
 
     private static final String HIDDEN_PWD = "**********";
 
+    // UniqueIdentifier is left out, because we don't want people to use that
     private static final List<String> dnfields = Arrays.asList("CN", "UID", "OU", "O", "L", "ST", "DC", "C", "emailAddress", "SN", "givenName", "initials", "surname", "title", 
             "unstructuredAddress", "unstructuredName", "postalCode", "businessCategory", "dnQualifier", "postalAddress", 
             "telephoneNumber", "pseudonym", "streetAddress", "name", "role", "CIF", "NIF", "VID", "PID", "CertificationID");
