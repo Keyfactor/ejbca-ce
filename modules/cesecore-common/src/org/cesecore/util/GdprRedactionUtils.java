2a63c2c94807123afdd780883478651be8dc4683
andresj <andresj@primekey.com>
2023-07-21 16:51:55 +0200

    ECA-11672 PII log redaction for ACME.

--- modules/cesecore-common/src/org/cesecore/util/GdprRedactionUtils.java
+++ modules/cesecore-common/src/org/cesecore/util/GdprRedactionUtils.java
@@ -12,11 +12,17 @@
  *************************************************************************/
 package org.cesecore.util;
 
+import java.util.Collections;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.http.protocol.HttpContext;
 import org.cesecore.configuration.GdprConfigurationCache;
 
 public class GdprRedactionUtils {
     
-    public static final String REDACTED_CONTENT = "<redact>";
+    public static final String REDACTED_CONTENT = "<redacted>";
     
     public static String getSubjectDnLogSafe(String subjectDn, int endEntityProfileId) {
         if(GdprConfigurationCache.INSTANCE.getGdprConfiguration(endEntityProfileId).isRedactPii()) {
@@ -48,6 +54,71 @@ public class GdprRedactionUtils {
         } else {
             return san;
         }
+    }
+    
+    public static String getLogSafe(final String string, final String identifier, final int endEntityProfileId) {
+        return GdprConfigurationCache.INSTANCE.getGdprConfiguration(endEntityProfileId).isRedactPii() ? 
+                string.replace(identifier, GdprRedactionUtils.REDACTED_CONTENT) : string;
+    }
+    
+    public static String getLogSafe(String string, final List<String> identifiers, final int endEntityProfileId) {
+        if (GdprConfigurationCache.INSTANCE.getGdprConfiguration(endEntityProfileId).isRedactPii()) {
+            for (String identifier : identifiers) {
+                string = string.replace(identifier, GdprRedactionUtils.REDACTED_CONTENT);
+            }
+        }
+        return string;
+    }
+    
+    public static Integer getEndEntityProfileId(final HttpServletRequest request) {
+        if (request.getAttribute("redact-eepid") instanceof Integer) {
+            return (Integer) request.getAttribute("redact-eepid");
+        } else {
+            return null;
+        }
+    }
+    
+    public static void setEndEntityProfileId(final HttpServletRequest request, final int id) {
+        request.setAttribute("redact-eepid", id);
+    }
+    
+    public static Integer getEndEntityProfileId(final HttpContext context) {
+        if (context.getAttribute("redact-eepid") instanceof Integer) {
+            return (Integer) context.getAttribute("redact-eepid");
+        } else {
+            return null;
+        }
+    }
+    
+    public static void setEndEntityProfileId(final HttpContext context, final int id) {
+        context.setAttribute("redact-eepid", id);
+    }
+    
+    
+    @SuppressWarnings("unchecked")
+    public static List<String> getToBeRedacted(final HttpServletRequest request) {
+        if (request.getAttribute("redact") instanceof List) {
+            return (List<String>) request.getAttribute("redact");
+        } else {
+            return Collections.EMPTY_LIST;
+        }
+    }
+    
+    public static void setToBeRedacted(final HttpServletRequest request, final List<String> toBeRedacted) {
+        request.setAttribute("redact", toBeRedacted);
+    }
+    
+    @SuppressWarnings("unchecked")
+    public static List<String> getToBeRedacted(final HttpContext context) {
+        if (context.getAttribute("redact") instanceof List) {
+            return (List<String>) context.getAttribute("redact");
+        } else {
+            return Collections.EMPTY_LIST;
+        }
+    }
+    
+    public static void setToBeRedacted(final HttpContext context, final List<String> toBeRedacted) {
+        context.setAttribute("redact", toBeRedacted);
     }
 
 }
