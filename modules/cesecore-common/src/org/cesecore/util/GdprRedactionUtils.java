ee77b364d514b8eab7b4af9e897ede1783ef023c
Tanmoy Kundu <tanmoy.kundu@primekey.com>
2023-08-10 14:47:37 +0200

    EJBCAINTER-547: audit log redact test and other fixes

--- modules/cesecore-common/src/org/cesecore/util/GdprRedactionUtils.java
+++ modules/cesecore-common/src/org/cesecore/util/GdprRedactionUtils.java
@@ -69,6 +69,9 @@ public class GdprRedactionUtils {
         StringBuilder regex = new StringBuilder(); 
         regex.append("(");
         for(String dnPart: dnParts) {
+            if (dnPart.equals("e") || dnPart.equals("t")) {
+                continue;
+            }
             regex.append("(" + dnPart + "=)|");
         }
         regex.deleteCharAt(regex.length()-1);
