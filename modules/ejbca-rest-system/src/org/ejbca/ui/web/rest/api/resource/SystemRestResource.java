c21207f7ed30ab171c7918438023a68b76bd4e75
Ulf Undmark <ulf.undmark@primekey.com>
2023-09-13 15:06:15 +0200

    ECA-11662: remove lock, add timestamp, fix configdump

--- modules/ejbca-rest-system/src/org/ejbca/ui/web/rest/api/resource/SystemRestResource.java
+++ modules/ejbca-rest-system/src/org/ejbca/ui/web/rest/api/resource/SystemRestResource.java
@@ -12,6 +12,8 @@ import org.apache.log4j.Logger;
 import org.cesecore.authorization.AuthorizationDeniedException;
 import org.cesecore.util.KeyedLock;
 import org.ejbca.core.ejb.rest.EjbcaRestHelperSessionLocal;
+import org.ejbca.core.ejb.services.ServiceData;
+import org.ejbca.core.ejb.services.ServiceDataSessionLocal;
 import org.ejbca.core.ejb.services.ServiceSessionLocal;
 import org.ejbca.core.model.era.RaMasterApiProxyBeanLocal;
 import org.ejbca.ui.web.rest.api.exception.RestException;
@@ -30,26 +32,24 @@ public class SystemRestResource extends BaseRestResource {
     private RaMasterApiProxyBeanLocal raMasterApiProxy;
     @EJB
     private ServiceSessionLocal serviceSession;
+    @EJB
+    private ServiceDataSessionLocal serviceDataSession;
     private static final Logger log = Logger.getLogger(SystemRestResource.class);
-    private static final KeyedLock<String> lock = new KeyedLock<>();
 
     public Response runServiceWorker(
             final HttpServletRequest requestContext,
             final String serviceName) throws AuthorizationDeniedException, RestException{
-        final Integer id = serviceSession.getServiceId(serviceName);
-        if (id == 0) {
+        final Integer serviceId = serviceSession.getServiceId(serviceName);
+        if (serviceId == 0) {
             String exception = "The following service could not be found: " + serviceName;
             log.info(exception);
             throw new RestException(Status.NOT_FOUND.getStatusCode(),  exception);
         }
-        if (lock.tryLock(serviceName)) {
-            serviceSession.runService(id);
-        } else {
-            throw new RestException(Status.PRECONDITION_FAILED.getStatusCode(),  serviceName + "already running");
-        }
-        lock.release(serviceName);
+        final ServiceData serviceData = serviceDataSession.findById(serviceId);
+        serviceSession.runService(serviceId);
+        final long lastRun = serviceData.getRunTimeStamp();
         return Response.ok(RunServiceRestResponse.builder()
-                .message("Running service: " + serviceName)
+                .message("Running service: " + serviceName + ". Last run timestamp:  " + lastRun)
                 .build()
                 ).build();
     }
