9cb5b47296eccd375cad257456e0a9bcdc8bc4ec
Mike Kushner <mikek.kushner@keyfactor.com>
2023-09-19 10:42:32 +0200

    ECA-11823: Interim commit

--- modules/cesecore-common/src/org/cesecore/certificates/ca/catoken/CAToken.java
+++ modules/cesecore-common/src/org/cesecore/certificates/ca/catoken/CAToken.java
@@ -57,6 +57,8 @@ public class CAToken extends UpgradeableDataHashMap {
 
     /** Latest version of the UpgradeableHashMap, this determines if we need to auto-upgrade any data. */
     public static final float LATEST_VERSION = 8;
+    
+    public static final int NO_ALTERNATIVE_CRYPTOTOKEN_CONFIGURED = -1;
 
     @Deprecated // Used by upgrade code
     public static final String CLASSPATH = "classpath";
@@ -81,6 +83,12 @@ public class CAToken extends UpgradeableDataHashMap {
     public static final String SIGNATUREALGORITHM = "signaturealgorithm";
     public static final String ENCRYPTIONALGORITHM = "encryptionalgorithm";
     public static final String CRYPTOTOKENID = "cryptotokenid";
+    
+    //The crypto token containing the alternative key for a hybrid certificate. May be the same as the above, but may be different as well. 
+    private static final String ALTERNATIVE_CRYPTOTOKEN_ID = "alternativeCryptoTokenId";
+    //For quantum safe keys the signature alg is derived from the key type, but ISO 15118 allows non-quantum safe keys to be used as the alternative key
+    private static final String ALTERNATIVE_SIGNATURE_ALGORITHMS = "alternativeSignatureAlgorithm";
+    
 
     private int cryptoTokenId;
     private transient PurposeMapping keyStrings = null;
@@ -243,6 +251,21 @@ public class CAToken extends UpgradeableDataHashMap {
         this.cryptoTokenId = cryptoTokenId;
         data.put(CAToken.CRYPTOTOKENID, String.valueOf(cryptoTokenId));
     }
+    
+    /** @return the crypto token ID of the alternative crypto token */
+    public int getAlternativeCryptoTokenId() throws CryptoTokenOfflineException {
+        if(data.containsKey(ALTERNATIVE_CRYPTOTOKEN_ID)) {
+            return Integer.valueOf((String) data.get(ALTERNATIVE_CRYPTOTOKEN_ID));
+        } else {
+            return NO_ALTERNATIVE_CRYPTOTOKEN_CONFIGURED;
+        }
+    }
+    
+    
+    public void setAlternativeCryptoTokenId(final int alternativeCryptoTokenId) {
+        data.put(ALTERNATIVE_CRYPTOTOKEN_ID, String.valueOf(alternativeCryptoTokenId)); 
+    }
+    
 
     /** Set a property and update underlying Map */
     public void setProperty(String key, String value) {
@@ -343,8 +366,18 @@ public class CAToken extends UpgradeableDataHashMap {
         return (String) data.get(CAToken.SIGNATUREALGORITHM);
     }
 
-    /** Sets the SignatureAlgoritm */
+    /** Sets the alternative SignatureAlgoritm */
     public void setSignatureAlgorithm(String signaturealgoritm) {
+        data.put(CAToken.ALTERNATIVE_SIGNATURE_ALGORITHMS, signaturealgoritm);
+    }
+    
+    /** @return the alternative SignatureAlgoritm, or null if none is set */
+    public String getAlternativeSignatureAlgorithm() {
+        return (String) data.get(CAToken.ALTERNATIVE_SIGNATURE_ALGORITHMS);
+    }
+
+    /** Sets the SignatureAlgoritm */
+    public void setAlternativeSignatureAlgorithm(String signaturealgoritm) {
         data.put(CAToken.SIGNATUREALGORITHM, signaturealgoritm);
     }
 
