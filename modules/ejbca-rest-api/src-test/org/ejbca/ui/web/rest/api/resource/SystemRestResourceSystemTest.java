3704b1ad20940253bf6807c6159fa6e160350959
Ulf Undmark <ulf.undmark@primekey.com>
2023-10-04 14:04:52 +0200

    ECA-11662: improve exception message and add system test

--- modules/ejbca-rest-api/src-test/org/ejbca/ui/web/rest/api/resource/SystemRestResourceSystemTest.java
+++ modules/ejbca-rest-api/src-test/org/ejbca/ui/web/rest/api/resource/SystemRestResourceSystemTest.java
@@ -17,7 +17,7 @@ import org.ejbca.core.model.services.BaseWorker;
 import org.ejbca.core.model.services.ServiceConfiguration;
 import org.ejbca.core.model.services.actions.NoAction;
 import org.ejbca.core.model.services.intervals.PeriodicalInterval;
-import org.ejbca.core.model.services.workers.EmailSendingWorkerConstants;
+import org.ejbca.core.model.services.workers.CRLUpdateWorker;
 import org.ejbca.core.model.services.workers.UserPasswordExpireWorker;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -30,6 +30,7 @@ import org.junit.Test;
 public class SystemRestResourceSystemTest extends RestResourceSystemTestBase {
         
     private static final String SERVICE_NAME = "TestService";
+    private static final String SERVICE_NAME_NO_ACTION = "TestServiceNoAction";
     private ServiceSessionRemote serviceSession; 
     private ServiceConfiguration serviceConfig = new ServiceConfiguration();
     private final String caName = "SystemResourceTestCA";
@@ -52,10 +53,12 @@ public class SystemRestResourceSystemTest extends RestResourceSystemTestBase {
     public void tearDown() throws AuthorizationDeniedException {
         // remove CA
         CaTestUtils.removeCa(INTERNAL_ADMIN_TOKEN, cryptoTokenName, caName);
+        // remove CRL
+        CaTestUtils.removeCrlByIssuerDn(caDN);
         // remove cryptotoken
         CryptoTokenTestUtils.removeCryptoToken(INTERNAL_ADMIN_TOKEN, cryptoTokenName);
     }
- 
+    
     @Test
     public void shouldReturn200WhenServiceTriggered() throws Exception {
         if (serviceSession == null) {
@@ -72,6 +75,28 @@ public class SystemRestResourceSystemTest extends RestResourceSystemTestBase {
             // Verify result of rest request
             //then
             assertEquals(HTTP_STATUS_CODE_OK, status);
+        } finally {
+            actualResponse.close();
+        }
+    }
+
+    @Test
+    public void shouldReturn409WhenServiceTriggeredAndResultIsNoAction() throws Exception {
+        if (serviceSession == null) {
+          serviceSession = EjbRemoteHelper.INSTANCE.getRemoteSession(ServiceSessionRemote.class);
+        }
+        //given
+        addAndActivateServiceNoAction(SERVICE_NAME_NO_ACTION, createServiceConfiguration(), caName);
+        //verify the service ws created
+        assertNotNull("Service of name " + SERVICE_NAME_NO_ACTION + " does not exist", serviceSession.getService(SERVICE_NAME_NO_ACTION));
+        //when
+        final Response actualResponse = newRequest("/v1/system/service/" + SERVICE_NAME_NO_ACTION + "/run").request().put(null);
+        serviceSession.removeService(INTERNAL_ADMIN_TOKEN, SERVICE_NAME_NO_ACTION);
+        try {
+            final int status = actualResponse.getStatus();
+            // Verify result of rest request
+            //then
+            assertEquals(HTTP_STATUS_CODE_CONFLICT, status);
         } finally {
             actualResponse.close();
         }
@@ -96,9 +121,28 @@ public class SystemRestResourceSystemTest extends RestResourceSystemTestBase {
             actualResponse.close();
         }
     }
-    
+        
     private int addAndActivateService(final String name, final ServiceConfiguration config, final String caName) throws Exception {
-        CaTestUtils.createX509Ca(INTERNAL_ADMIN_TOKEN, cryptoTokenName, caName, caDN, CAConstants.CA_OFFLINE);
+        CaTestUtils.createX509Ca(INTERNAL_ADMIN_TOKEN, cryptoTokenName, caName, caDN, CAConstants.CA_ACTIVE);
+        if ((config.getIntervalProperties() != null) && (config.getIntervalProperties().getProperty(PeriodicalInterval.PROP_VALUE) == null)) {
+            Properties intervalprop = new Properties();
+            intervalprop.setProperty(PeriodicalInterval.PROP_VALUE, "30");
+            intervalprop.setProperty(PeriodicalInterval.PROP_UNIT, PeriodicalInterval.UNIT_SECONDS);
+            config.setIntervalProperties(intervalprop);            
+        }
+        config.setWorkerClassPath(CRLUpdateWorker.class.getName());
+        Properties workerprop = new Properties();
+        workerprop.setProperty(BaseWorker.PROP_CAIDSTOCHECK, String.valueOf(CaTestUtils.getCaIdByName(INTERNAL_ADMIN_TOKEN, caName)));
+        workerprop.setProperty(BaseWorker.PROP_TIMEBEFOREEXPIRING, "5");
+        workerprop.setProperty(BaseWorker.PROP_TIMEUNIT, BaseWorker.UNIT_SECONDS);
+        config.setWorkerProperties(workerprop);
+        serviceSession.addService(INTERNAL_ADMIN_TOKEN, name, config);
+        serviceSession.activateServiceTimer(INTERNAL_ADMIN_TOKEN, name);        
+        return serviceSession.getServiceId(name);
+    }
+
+    private int addAndActivateServiceNoAction(final String name, final ServiceConfiguration config, final String caName) throws Exception {
+        CaTestUtils.createX509Ca(INTERNAL_ADMIN_TOKEN, cryptoTokenName, caName, caDN, CAConstants.CA_ACTIVE);
         if ((config.getIntervalProperties() != null) && (config.getIntervalProperties().getProperty(PeriodicalInterval.PROP_VALUE) == null)) {
             Properties intervalprop = new Properties();
             intervalprop.setProperty(PeriodicalInterval.PROP_VALUE, "30");
