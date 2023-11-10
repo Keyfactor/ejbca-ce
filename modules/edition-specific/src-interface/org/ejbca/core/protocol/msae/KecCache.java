d394a307d3b92fd037e02ed1e2d966248164d67e
Amin Khorsandi <akhorsandi@keyfactor.com>
2023-11-10 18:57:26 +0100

    ECA-11950: add defaults etc.

--- modules/edition-specific/src-interface/org/ejbca/core/protocol/msae/KecCache.java
+++ modules/edition-specific/src-interface/org/ejbca/core/protocol/msae/KecCache.java
@@ -1,8 +1,22 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.ejbca.core.protocol.msae;
 
 import java.security.SignatureException;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateEncodingException;
+
+import javax.ejb.Local;
 
 import org.bouncycastle.operator.OperatorCreationException;
 import org.cesecore.authentication.tokens.AuthenticationToken;
@@ -17,13 +31,17 @@ import org.cesecore.certificates.certificate.certextensions.CertificateExtension
 
 import com.keyfactor.util.keys.token.CryptoTokenOfflineException;
 
+@Local
 public interface KecCache {
-    
-    public Certificate getCachedKEC(final AuthenticationToken admin, final int cAId, final int cPId)
+
+    default Certificate getCachedKEC(final AuthenticationToken admin, final int cAId, final int cPId)
             throws CertificateEncodingException, InvalidAlgorithmException, CryptoTokenOfflineException, CertificateCreateException,
             CAOfflineException, IllegalValidityException, SignatureException, IllegalKeyException, OperatorCreationException, IllegalNameException,
-            AuthorizationDeniedException, CertificateExtensionException, KeyArchivalException;
+            AuthorizationDeniedException, CertificateExtensionException, KeyArchivalException {
+        throw new UnsupportedOperationException("KEC cache methods are only supported in EJBCA Enterprise");
+    }
 
-    
-    public void flushKecCache();
+    default void flushKecCache() {
+        throw new UnsupportedOperationException("KEC cache methods are only supported in EJBCA Enterprise");
+    }
 }
