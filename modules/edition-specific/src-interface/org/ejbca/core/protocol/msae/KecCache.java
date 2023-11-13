70984207067d4d94572c38d367004e10168ae2e7
Amin Khorsandi <akhorsandi@keyfactor.com>
2023-11-13 18:08:32 +0100

    ECA-11950: Improve logs.

--- modules/edition-specific/src-interface/org/ejbca/core/protocol/msae/KecCache.java
+++ modules/edition-specific/src-interface/org/ejbca/core/protocol/msae/KecCache.java
@@ -16,8 +16,6 @@ import java.security.SignatureException;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateEncodingException;
 
-import javax.ejb.Local;
-
 import org.bouncycastle.operator.OperatorCreationException;
 import org.cesecore.authentication.tokens.AuthenticationToken;
 import org.cesecore.authorization.AuthorizationDeniedException;
@@ -28,12 +26,32 @@ import org.cesecore.certificates.ca.InvalidAlgorithmException;
 import org.cesecore.certificates.certificate.CertificateCreateException;
 import org.cesecore.certificates.certificate.IllegalKeyException;
 import org.cesecore.certificates.certificate.certextensions.CertificateExtensionException;
+import org.ejbca.core.ejb.config.ClearCacheSessionBean;
 
 import com.keyfactor.util.keys.token.CryptoTokenOfflineException;
 
-@Local
 public interface KecCache {
 
+    /**
+     * Gets a valid KEC certificate from the cache, or calls for a valid one to be generated and cached.
+     * @param admin
+     * @param cAId
+     * @param cPId
+     * @return
+     * @throws CertificateEncodingException
+     * @throws InvalidAlgorithmException
+     * @throws CryptoTokenOfflineException
+     * @throws CertificateCreateException
+     * @throws CAOfflineException
+     * @throws IllegalValidityException
+     * @throws SignatureException
+     * @throws IllegalKeyException
+     * @throws OperatorCreationException
+     * @throws IllegalNameException
+     * @throws AuthorizationDeniedException
+     * @throws CertificateExtensionException
+     * @throws KeyArchivalException
+     */
     default Certificate getCachedKEC(final AuthenticationToken admin, final int cAId, final int cPId)
             throws CertificateEncodingException, InvalidAlgorithmException, CryptoTokenOfflineException, CertificateCreateException,
             CAOfflineException, IllegalValidityException, SignatureException, IllegalKeyException, OperatorCreationException, IllegalNameException,
@@ -41,6 +59,9 @@ public interface KecCache {
         throw new UnsupportedOperationException("KEC cache methods are only supported in EJBCA Enterprise");
     }
 
+    /**
+     * Clears the cache, called from {@link ClearCacheSessionBean}
+     */
     default void flushKecCache() {
         throw new UnsupportedOperationException("KEC cache methods are only supported in EJBCA Enterprise");
     }
