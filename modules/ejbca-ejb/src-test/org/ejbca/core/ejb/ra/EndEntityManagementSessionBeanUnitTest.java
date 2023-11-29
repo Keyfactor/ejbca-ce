0bf47606af97e9e726b9965a0c617e1cdae7f1ca
Jekaterina Bunina <jekaterina.bunina@keyfactor.com>
2023-11-29 10:20:29 +0200

    ECA-11999 username should allow only characters, numbers

---
+++ modules/ejbca-ejb/src-test/org/ejbca/core/ejb/ra/EndEntityManagementSessionBeanUnitTest.java
@@ -0,0 +1,56 @@
+package org.ejbca.core.ejb.ra;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EndEntityManagementSessionBeanUnitTest {
+
+    @Test
+    public void isUsernameValidOnlyChars() {
+        assertTrue("Username with characters is a valid one", EndEntityManagementSessionBean.isUsernameValid("Someusername"));
+    }
+
+    @Test
+    public void isUsernameValidOnlyCharsAndNumbers() {
+        assertTrue("Username with numbers is a valid one", EndEntityManagementSessionBean.isUsernameValid("Someusername874"));
+    }
+    @Test
+    public void isUsernameValidCyrillicChar() {
+        assertTrue("Username with characters is a valid one", EndEntityManagementSessionBean.isUsernameValid("Шишка"));
+    }
+
+    @Test
+    public void isUsernameValidMinus() {
+        assertTrue("Username with minus is a valid one", EndEntityManagementSessionBean.isUsernameValid("Some-username"));
+    }
+
+    @Test
+    public void isUsernameValidPlus() {
+        assertFalse("Username with plus is a invalid", EndEntityManagementSessionBean.isUsernameValid("Some+username"));
+    }
+
+    @Test
+    public void isUsernameValidHash() {
+        assertFalse("Username with Hash is a invalid", EndEntityManagementSessionBean.isUsernameValid("Some#username"));
+    }
+
+    @Test
+    public void isUsernameValidAmpersand() {
+        assertFalse("Username with Ampersand is a invalid", EndEntityManagementSessionBean.isUsernameValid("Some&username"));
+    }
+
+    @Test
+    public void isUsernameValidAsterisk() {
+        assertTrue("Username with Asterisk is valid", EndEntityManagementSessionBean.isUsernameValid("Some*username"));
+    }
+
+    @Test
+    public void isUsernameValidSlash() {
+        assertTrue("Username with Slash is valid", EndEntityManagementSessionBean.isUsernameValid("Some/username"));
+    }
+    @Test
+    public void isUsernameValidUnderscore() {
+        assertTrue("Username with Underscore is valid", EndEntityManagementSessionBean.isUsernameValid("Some_username"));
+    }
+}
