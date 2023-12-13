c4908ac529fcee3c5ebffa999214730de869e42c
Mike Kushner <mikek.kushner@keyfactor.com>
2023-12-13 11:53:07 +0100

    ECA-10671: Shift ocsp.untilNextUpdate to config
    Moved ocsp.untilNextUpdate, other values to follow.

--- modules/ejbca-ejb/src-test/org/ejbca/core/certificates/ocsp/OcspResponseGeneratorSessionUnitTest.java
+++ modules/ejbca-ejb/src-test/org/ejbca/core/certificates/ocsp/OcspResponseGeneratorSessionUnitTest.java
@@ -12,6 +12,16 @@
  *************************************************************************/
 package org.ejbca.core.certificates.ocsp;
 
+import static org.easymock.EasyMock.anyLong;
+import static org.easymock.EasyMock.anyObject;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
@@ -28,11 +38,6 @@ import java.util.Properties;
 
 import javax.ejb.Timer;
 import javax.ejb.TimerService;
-
-import com.keyfactor.util.CertTools;
-import com.keyfactor.util.crypto.algorithm.AlgorithmConstants;
-import com.keyfactor.util.keys.KeyTools;
-import com.keyfactor.util.keys.token.CryptoToken;
 
 import org.apache.log4j.Logger;
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
@@ -70,9 +75,7 @@ import org.cesecore.certificates.ocsp.exception.MalformedRequestException;
 import org.cesecore.certificates.ocsp.logging.AuditLogger;
 import org.cesecore.certificates.ocsp.logging.TransactionCounter;
 import org.cesecore.certificates.ocsp.logging.TransactionLogger;
-import org.cesecore.config.ConfigurationHolder;
 import org.cesecore.config.GlobalOcspConfiguration;
-import org.cesecore.config.OcspConfiguration;
 import org.cesecore.configuration.GlobalConfigurationSessionLocal;
 import org.cesecore.keybind.InternalKeyBindingDataSessionLocal;
 import org.cesecore.keybind.impl.OcspKeyBinding;
@@ -88,15 +91,10 @@ import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
-import static org.easymock.EasyMock.anyLong;
-import static org.easymock.EasyMock.anyObject;
-import static org.easymock.EasyMock.expect;
-import static org.easymock.EasyMock.replay;
-import static org.easymock.EasyMock.verify;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import com.keyfactor.util.CertTools;
+import com.keyfactor.util.crypto.algorithm.AlgorithmConstants;
+import com.keyfactor.util.keys.KeyTools;
+import com.keyfactor.util.keys.token.CryptoToken;
 
 /**
  * Tests for the OcspResponseGenerator that don't involve creating a CA.
@@ -158,6 +156,7 @@ public class OcspResponseGeneratorSessionUnitTest {
             "U2RWBSRSaV1R5jlDvr4lB9O+CAmtUs2AsgAWomgPnvVUScDZt5nK7x5OFLeE85o=\n" +
             "-----END CERTIFICATE-----\n";
     // Complex chain with same Subject DN, but with correct SKID/AKID - Root CA
+    @SuppressWarnings("unused")
     private static final String SAMEISSUER_ROOTCA_CERT =
             "-----BEGIN CERTIFICATE-----\n" +
             "MIIFQzCCAyugAwIBAgIUQgkTyYXUYxl8Yoak81Y6KZA/hUAwDQYJKoZIhvcNAQEL\n" +
@@ -213,6 +212,7 @@ public class OcspResponseGeneratorSessionUnitTest {
             "Lv24f5LbofR5TiHPXmTX0hg6beohc+n2g6jBzpjprymNuuVvCwlpyhVTFGJLxas=\n" +
             "-----END CERTIFICATE-----\n";
     // Complex chain with same Subject DN, but with correct SKID/AKID - OCSP Signer cert
+    @SuppressWarnings("unused")
     private static final String SAMEISSUER_OCSPSIGNER_CERT =
             "-----BEGIN CERTIFICATE-----\n" +
             "MIIDZDCCAkygAwIBAgIQA2wYNyF6qEB/wqGo2eBk7jANBgkqhkiG9w0BAQsFADAp\n" +
@@ -235,6 +235,7 @@ public class OcspResponseGeneratorSessionUnitTest {
             "rndfDnOsGqNDkbx9JFKaDqpfv2K6HgplaZR5JEuUqhV76zp4cfya8hf/KidDB8sf\n" +
             "kAar4YVdjmA=\n" +
             "-----END CERTIFICATE-----\n";
+    @SuppressWarnings("unused")
     private static final String SAMEISSUER_OCSPSIGNER_PRIVKEY =
             "-----BEGIN PRIVATE KEY-----\n" +
             "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC0OURvapf5QXTc\n" +
@@ -268,7 +269,6 @@ public class OcspResponseGeneratorSessionUnitTest {
     private static final int ISSUER_CAID = ISSUER_CERT_DN.hashCode();
     private static final int CRYPTOTOKEN_ID = 1111;
     private static final String OCSP_SIGN_KEY_ALIAS = "ocspSignKey";
-    private static final int KEYBINDING_ID = 2222;
 
     private static final Logger log = Logger.getLogger(OcspResponseGeneratorSessionUnitTest.class);
 
@@ -283,6 +283,7 @@ public class OcspResponseGeneratorSessionUnitTest {
     private TimerService timerServiceMock = EasyMock.createStrictMock(TimerService.class);
     private OcspDataSessionLocal ocspDataSessionMock = EasyMock.createStrictMock(OcspDataSessionLocal.class);
     private CertificateStatus status;
+    
     
     @Before
     public void beforeTest() {
@@ -409,7 +410,10 @@ public class OcspResponseGeneratorSessionUnitTest {
     }
 
     private void setupOcspResponseCache() {
-        ConfigurationHolder.updateConfiguration(OcspConfiguration.UNTIL_NEXT_UPDATE, "60000");
+        GlobalOcspConfiguration ocspConfiguration = new GlobalOcspConfiguration();
+        ocspConfiguration.setDefaultValidityTime(60000L);
+
+        expect(globalConfigurationSessionMock.getCachedConfiguration(GlobalOcspConfiguration.OCSP_CONFIGURATION_ID)).andReturn(ocspConfiguration).once();
         OcspDataConfigCacheEntry entry = new OcspDataConfigCacheEntry(getIssuerCert(), ISSUER_CAID, true, false, false);
         OcspDataConfigCache.INSTANCE.stagingAdd(entry);
         OcspDataConfigCache.INSTANCE.stagingCommit();
@@ -672,17 +676,10 @@ public class OcspResponseGeneratorSessionUnitTest {
         return getCert(ISSUER_CERT);
     }
 
-    private X509Certificate getSameDnRootCert() {
-        return getCert(SAMEISSUER_ROOTCA_CERT);
-    }
-
     private X509Certificate getSameDnSubCert() {
         return getCert(SAMEISSUER_SUBCA_CERT);
     }
 
-    private X509Certificate getSameDnOcspSignerCert() {
-        return getCert(SAMEISSUER_OCSPSIGNER_CERT);
-    }
 
     private PrivateKey getPrivKey(final String pemData) {
         try {
@@ -696,10 +693,6 @@ public class OcspResponseGeneratorSessionUnitTest {
 
     private PrivateKey getIssuerPrivKey() {
         return getPrivKey(ISSUER_PRIVKEY);
-    }
-
-    private PrivateKey getSameDnOcspSignerPrivKey() {
-        return getPrivKey(SAMEISSUER_OCSPSIGNER_PRIVKEY);
     }
 
     private class TestOcspResponseGeneratorSessionBean extends OcspResponseGeneratorSessionBean {
