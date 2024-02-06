bd1924133cc24570de4e630846f963afca7ebf24
Amin Khorsandi <amin.khorsandi@keyfactor.com>
2024-02-06 23:12:09 +0100

    ECA-10949: Add ra side config cache.

---
+++ modules/ejbca-ejb-interface/src/org/ejbca/core/protocol/msae/MSAEConfigRaCacheLocal.java
@@ -0,0 +1,37 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.core.protocol.msae;
+
+import javax.ejb.Local;
+
+import org.ejbca.config.MSAutoEnrollmentConfiguration;
+
+@Local
+public interface MSAEConfigRaCacheLocal {
+
+    /**
+     * 
+     * @param alias msae alias which we are looking for its configuration in the cache
+     * @return associated msae configuration given the alias
+     */
+    default MSAutoEnrollmentConfiguration getCachedMSAERaConfig(final String alias) {
+        throw new UnsupportedOperationException("MSAE is an EJBCA Enterprise only feature");
+    }
+
+    /**
+     * Clears the cache, called from {@link #ClearCacheSessionBean}
+     */
+    default void flushKecCache() {
+        throw new UnsupportedOperationException("MSAE is an EJBCA Enterprise only feature");
+    }
+}
