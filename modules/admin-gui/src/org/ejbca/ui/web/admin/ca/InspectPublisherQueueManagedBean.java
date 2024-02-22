27da8e1cb16f865a2cdb6454b62bbc21f802fc02
Rein Remsu <rein.remsu@primekey.com>
2024-02-22 10:02:21 +0000

    ECA-12017: Add republish button per item in Publisher Queue

--- modules/admin-gui/src/org/ejbca/ui/web/admin/ca/InspectPublisherQueueManagedBean.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/ca/InspectPublisherQueueManagedBean.java
@@ -28,13 +28,14 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import javax.ejb.EJB;
-import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
 import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.log4j.Logger;
+import org.cesecore.authentication.tokens.AuthenticationToken;
 import org.cesecore.certificates.ca.CaSessionLocal;
 import org.cesecore.certificates.certificate.CertificateDataSessionLocal;
 import org.cesecore.certificates.certificate.CertificateInfo;
@@ -42,10 +43,13 @@ import org.cesecore.certificates.crl.CRLInfo;
 import org.cesecore.certificates.crl.CrlStoreSessionLocal;
 import org.cesecore.certificates.endentity.EndEntityConstants;
 import org.ejbca.core.ejb.ca.publisher.PublisherQueueSessionLocal;
+import org.ejbca.core.ejb.ca.publisher.PublisherSessionLocal;
+import org.ejbca.core.ejb.ca.publisher.PublishingResult;
 import org.ejbca.core.ejb.ra.raadmin.EndEntityProfileSessionLocal;
 import org.ejbca.core.ejb.services.ServiceDataSessionLocal;
 import org.ejbca.core.ejb.services.ServiceSessionLocal;
 import org.ejbca.core.model.authorization.AccessRulesConstants;
+import org.ejbca.core.model.ca.publisher.BasePublisher;
 import org.ejbca.core.model.ca.publisher.PublisherConst;
 import org.ejbca.core.model.ca.publisher.PublisherQueueData;
 import org.ejbca.core.model.services.ServiceConfiguration;
@@ -64,7 +68,6 @@ public class InspectPublisherQueueManagedBean extends BaseManagedBean {
     private static final long serialVersionUID = 1L;
     private static final int MAX_RESULTS = 20;
     private static final int DESCRIPTION_MAX_LENGTH = 80;
-    private static final String FLUSH_ITEM_PARAMETER = "fingerprintOfItemToFlush";
     @EJB
     private PublisherQueueSessionLocal publisherQueueSession;
     @EJB
@@ -80,9 +83,19 @@ public class InspectPublisherQueueManagedBean extends BaseManagedBean {
     @EJB
     private ServiceDataSessionLocal serviceDataSession;
 
+    @EJB
+    private PublisherSessionLocal publisherSession;
+
     private int pageNumber;
     private String publisherId;
     private boolean isLastPage;
+
+    public List<PublisherQueueItem> getItems() {
+        return items;
+    }
+
+    private List<PublisherQueueItem> items = new ArrayList<>();
+    private int successfullyPublishedItems;
 
     public InspectPublisherQueueManagedBean() {
         super(AccessRulesConstants.REGULAR_VIEWPUBLISHER);
@@ -93,6 +106,8 @@ public class InspectPublisherQueueManagedBean extends BaseManagedBean {
      */
     public final class PublisherQueueItem {
         private final PublisherQueueData publisherQueueData;
+
+        private boolean selected;
 
         public PublisherQueueItem(PublisherQueueData data) {
             this.publisherQueueData = data;
@@ -194,6 +209,14 @@ public class InspectPublisherQueueManagedBean extends BaseManagedBean {
             }
             return true;
         }
+
+        public boolean isSelected() {
+            return selected;
+        }
+
+        public void setSelected(boolean selected) {
+            this.selected = selected;
+        }
     }
 
     private boolean isAuthorizedToViewCertificate(final CertificateInfo certificateInfo) {
@@ -255,38 +278,111 @@ public class InspectPublisherQueueManagedBean extends BaseManagedBean {
         return "";
     }
 
-    public String flushItem() {
-        final String fingerprint = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get(FLUSH_ITEM_PARAMETER);
-        if (StringUtils.isNotBlank(fingerprint)) {
-            log.info("Attempting to flush item with fingerprint " + fingerprint + " from queue with publisher ID " + getPublisherId() + ".");
-            publisherQueueSession.getEntriesByFingerprint(fingerprint).stream()
-                    .filter(item -> item.getPublisherId() == Integer.parseInt(getPublisherId()))
-                    .forEach(item -> publisherQueueSession.removeQueueData(item.getPk()));
+    public boolean isFirstPage() {
+        return pageNumber == 0;
+    }
+
+    public boolean isLastPage() {
+        return this.isLastPage;
+    }
+
+    public String republishAll() {
+        final Integer publisherId = getPublisherIdInteger();
+        if (publisherId == null) {
+            addNonTranslatedErrorMessage("The publisher ID must be an integer.");
+            return "";
         }
-        return "";
-    }
-
-    public String flush() {
-        log.info("Attempting to flush items on page " + pageNumber  + " in the queue with publisher ID " + getPublisherId() + ".");
-        getItemsForCurrentPage(Integer.parseInt(getPublisherId()))
-                .stream()
-                .forEach(item -> publisherQueueSession.removeQueueData(item.getPrimaryKey()));
-        return "";
-    }
-
-    public String republish() {
-        log.info("Attempting to republish items in the queue with publisher ID " + getPublisherId() + ".");
-        final Optional<Integer> idOfPublisherQueueProcessService = getStreamOfPublishers()
-                .filter(entry -> entry.getValue().isActive())
-                .map(entry -> entry.getKey())
-                .findFirst();
+        log.info("Attempting to republish items in the queue with publisher ID " + publisherId + ".");
+        final Optional<Integer> idOfPublisherQueueProcessService = getStreamOfPublishers().filter(
+                entry -> entry.getValue().isActive()).map(entry -> entry.getKey()).findFirst();
         if (!idOfPublisherQueueProcessService.isPresent()) {
             log.error(getReasonWhyPublisherQueueProcessQueueCannotRun());
             return "";
         }
-        log.info("Scheduling timer for PublishQueueProcessWorker with ID " + idOfPublisherQueueProcessService.get() + ".");
+        log.info("Scheduling timer for PublishQueueProcessWorker with ID " + idOfPublisherQueueProcessService.get()
+                + ".");
         serviceSession.runService(idOfPublisherQueueProcessService.get());
-        addInfoMessage("INSPECT_PUBLISHER_QUEUE_STARTED_SERVICE", serviceSession.getServiceName(idOfPublisherQueueProcessService.get()));
+        addInfoMessage("INSPECT_PUBLISHER_QUEUE_STARTED_SERVICE",
+                serviceSession.getServiceName(idOfPublisherQueueProcessService.get()));
+        return "";
+    }
+
+    public String republishSelected() {
+        final Integer publisherId = getPublisherIdInteger();
+        if (publisherId == null) {
+            addNonTranslatedErrorMessage("The publisher ID must be an integer.");
+            return "";
+        }
+        final List<PublisherQueueItem> selectedItems = items.stream().filter(PublisherQueueItem::isSelected).collect(Collectors.toList());
+        if (selectedItems.size() > MAX_RESULTS) {
+            addNonTranslatedErrorMessage("Too many items selected.");
+            return "";
+        }
+        log.info("Attempting to republish items with fingerprints " + selectedItems.stream()
+                .map(PublisherQueueItem::getFingerprint).collect(Collectors.joining(", "))
+                + " in the queue with publisher ID " + publisherId + ".");
+        final AuthenticationToken admin = getAdmin();
+        final BasePublisher publisher = publisherSession.getAllPublishers().get(publisherId);
+        successfullyPublishedItems = 0;
+        selectedItems.forEach(item -> republishItem(admin, publisher, item));
+        if (successfullyPublishedItems != 0) {
+            addInfoMessage("INSPECT_PUBLISHER_QUEUE_REPUBLISHED_ITEMS", successfullyPublishedItems);
+        }
+        return "";
+    }
+
+    private void addRepublishErrorMessage(final PublisherQueueItem item, final String message, final Throwable e) {
+        final String logMessage = "Re-publishing of item with fingerprint " + item.getFingerprint() + " (" + item.getDescription() + ") failed";
+        if (e == null) {
+            log.error(logMessage);
+        } else {
+            log.error(logMessage + ": " + e.getMessage(), e);
+        }
+        addErrorMessage("INSPECT_PUBLISHER_QUEUE_REPUBLISH_ERROR", item.getFingerprint(), item.getDescription(),
+                message);
+    }
+
+    private void republishItem(final AuthenticationToken admin, final BasePublisher publisher, final PublisherQueueItem item) {
+        try {
+            final PublishingResult result = publisherQueueSession.publishQueueData(admin, item.getPrimaryKey(), publisher);
+            if (result == null) {
+                addRepublishErrorMessage(item, "Queued item could not be found in database", null);
+            } else if (result.getSuccesses() == 0) {
+                addRepublishErrorMessage(item, result.getMessage(item.getFingerprint()), null);
+            } else {
+                successfullyPublishedItems++;
+            }
+        } catch (RuntimeException e) {
+            addRepublishErrorMessage(item, ExceptionUtils.getRootCause(e).getMessage(), e);
+        }
+    }
+
+    public String flushAll() {
+        final Integer publisherId = getPublisherIdInteger();
+        if (publisherId == null) {
+            addNonTranslatedErrorMessage("The publisher ID must be an integer.");
+            return "";
+        }
+        log.info("Attempting to flush items in the queue with publisher ID " + publisherId + ".");
+        publisherQueueSession.removeQueueDataByPublisherId(publisherId);
+        return "";
+    }
+
+    public String flushSelected() {
+        final Integer publisherId = getPublisherIdInteger();
+        if (publisherId == null) {
+            addNonTranslatedErrorMessage("The publisher ID must be an integer.");
+            return "";
+        }
+        final List<PublisherQueueItem> selectedItems = items.stream().filter(PublisherQueueItem::isSelected).collect(Collectors.toList());
+        if (selectedItems.size() > MAX_RESULTS) {
+            addNonTranslatedErrorMessage("Too many items selected.");
+            return "";
+        }
+        log.info("Attempting to flush items with fingerprints " + selectedItems.stream()
+                .map(PublisherQueueItem::getFingerprint).collect(Collectors.joining(", "))
+                + " in the queue with publisher ID " + publisherId + ".");
+        selectedItems.forEach(item -> publisherQueueSession.removeQueueData(item.getPrimaryKey()));
         return "";
     }
 
@@ -305,37 +401,30 @@ public class InspectPublisherQueueManagedBean extends BaseManagedBean {
         return Arrays.stream(selectedPublishersString.split(";")).collect(toSet());
     }
 
-    public boolean isFirstPage() {
-        return pageNumber == 0;
-    }
-
-    public boolean isLastPage() {
-        return this.isLastPage;
-    }
-
-    public List<PublisherQueueItem> getItems() {
-        try {
-            final int publisherId = Integer.parseInt(getPublisherId());
-            final List<PublisherQueueItem> items = getItemsForCurrentPage(publisherId);
-            if (items.size() != MAX_RESULTS + 1) {
-                isLastPage = true;
-                return items;
-            } else {
-                isLastPage = false;
-                items.remove(items.size() - 1);
-                return items;
-            }
-        } catch (NumberFormatException e) {
+	public List<PublisherQueueItem> getItemsForCurrentPage() {
+        final Integer publisherId = getPublisherIdInteger();
+        if (publisherId == null) {
             addNonTranslatedErrorMessage("The publisher ID must be an integer.");
             return new ArrayList<>();
         }
-    }
-
-    private List<PublisherQueueItem> getItemsForCurrentPage(final int publisherId) {
-        return publisherQueueSession
-                .getPendingEntriesForPublisherWithLimitAndOffset(publisherId, MAX_RESULTS + 1, MAX_RESULTS * pageNumber)
-                .stream()
-                .map(PublisherQueueItem::new)
+        List<PublisherQueueItem> retValues = publisherQueueSession.getPendingEntriesForPublisherWithLimitAndOffset(
+                        publisherId, MAX_RESULTS + 1, MAX_RESULTS * pageNumber).stream().map(PublisherQueueItem::new)
                 .collect(Collectors.toList());
+        if (retValues.size() != MAX_RESULTS + 1) {
+            isLastPage = true;
+        } else {
+            isLastPage = false;
+            retValues.remove(retValues.size() - 1);
+        }
+        items = retValues;
+        return retValues;
+	}
+
+    private Integer getPublisherIdInteger() {
+        try {
+            return Integer.parseInt(getPublisherId());
+        } catch (NumberFormatException e) {
+            return null;
+        }
     }
 }
