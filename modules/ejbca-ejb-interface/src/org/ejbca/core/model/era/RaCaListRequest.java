fd28a9782f40f3684a649b4c56a5d029a36234eb
andresj <andresj@primekey.com>
2024-02-26 11:35:44 +0100

    ECA-10214 Fix CaInfoBuilder and unit test.

--- modules/ejbca-ejb-interface/src/org/ejbca/core/model/era/RaCaListRequest.java
+++ modules/ejbca-ejb-interface/src/org/ejbca/core/model/era/RaCaListRequest.java
@@ -16,9 +16,25 @@ public class RaCaListRequest implements Serializable {
 	public void setIncludeExternal(boolean includeExternal) {
 		this.includeExternal = includeExternal;
 	}
+	
+	/* Required for CaRestResourceUnitTest. Otherwise EasyMock does not recognize the method call with this object as parameter. */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        RaCaListRequest other = (RaCaListRequest) obj;
+        if (includeExternal != other.includeExternal)
+            return false;
+        return true;
+    }
 
 	@Override
 	public int hashCode() {
 		return HashCodeBuilder.reflectionHashCode(this);
 	}
+	
 }
