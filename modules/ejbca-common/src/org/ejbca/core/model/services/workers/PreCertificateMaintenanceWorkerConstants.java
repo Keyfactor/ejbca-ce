c4580f1916f669053f82194c0e2b440a505c9779
Amin Khorsandi <amin.khorsandi@keyfactor.com>
2024-03-19 19:33:55 +0100

    ECA-12094: Add parts of review comments.

--- modules/ejbca-common/src/org/ejbca/core/model/services/workers/PreCertificateMaintenanceWorkerConstants.java
+++ modules/ejbca-common/src/org/ejbca/core/model/services/workers/PreCertificateMaintenanceWorkerConstants.java
@@ -15,12 +15,12 @@ package org.ejbca.core.model.services.workers;
 import org.ejbca.core.model.services.IWorker;
 
 /**
- * Constants for PreCertificateRevocationWorker. This is a separate class, to make the constants
+ * Constants for PreCertificateMaintenanceWorker. This is a separate class, to make the constants
  * available in code in community edition.
  */
-public class PreCertificateRevocationWorkerConstants {
+public class PreCertificateMaintenanceWorkerConstants {
 
-    public static final String WORKER_CLASS = "org.ejbca.core.model.services.workers.PreCertificateRevocationWorker";
+    public static final String WORKER_CLASS = "org.ejbca.core.model.services.workers.PreCertificateMaintenanceWorker";
 
     public static final String PROP_MAX_ISSUANCE_TIME = "worker.maxIssuanceTime";
     public static final String PROP_MAX_ISSUANCE_TIMEUNIT = "worker.maxIssuancceTimeUnit";
@@ -28,6 +28,6 @@ public class PreCertificateRevocationWorkerConstants {
 
     public static final String DEFAULT_MAX_ISSUANCE_TIME = "30";
     public static final String DEFAULT_MAX_ISSUANCE_TIMEUNIT = IWorker.UNIT_MINUTES;
-    public static final String DEFAULT_IS_REVOKE_PRE_CERTS = "true";
+    public static final String DEFAULT_IS_REVOKE_PRE_CERTS = "false";
 
 }
