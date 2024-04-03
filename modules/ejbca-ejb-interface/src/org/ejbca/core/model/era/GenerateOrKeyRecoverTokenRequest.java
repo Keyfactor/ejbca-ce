322925d1f9ce806ad3a5c6c8536a8341079da5e6
Parumita Saha <parumita.saha@primekey.com>
2024-04-03 14:18:02 +0200

    ECA-12299 : Replace method parameters with parameter object GenerateOrKeyRecoverTokenRequest

---
+++ modules/ejbca-ejb-interface/src/org/ejbca/core/model/era/GenerateOrKeyRecoverTokenRequest.java
@@ -0,0 +1,74 @@
+package org.ejbca.core.model.era;
+
+import java.io.Serializable;
+
+public class GenerateOrKeyRecoverTokenRequest implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String username;
+    private String password;
+    private String hardTokenSN;
+    private String keySpecification;
+    private String keyAlgorithm;
+    private String altKeyAlgorithm;
+
+    public GenerateOrKeyRecoverTokenRequest(String username, String password, String hardTokenSN, String keySpecification, String keyAlgorithm,
+            String altKeyAlgorithm) {
+        this.username = username;
+        this.password = password;
+        this.hardTokenSN = hardTokenSN;
+        this.keySpecification = keySpecification;
+        this.keyAlgorithm = keyAlgorithm;
+        this.altKeyAlgorithm = altKeyAlgorithm;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setHardTokenSN(String hardTokenSN) {
+        this.hardTokenSN = hardTokenSN;
+    }
+
+    public void setKeySpecification(String keySpecification) {
+        this.keySpecification = keySpecification;
+    }
+
+    public void setKeyAlgorithm(String keyAlgorithm) {
+        this.keyAlgorithm = keyAlgorithm;
+    }
+
+    public void setAltKeyAlgorithm(String altKeyAlgorithm) {
+        this.altKeyAlgorithm = altKeyAlgorithm;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getHardTokenSN() {
+        return hardTokenSN;
+    }
+
+    public String getKeySpecification() {
+        return keySpecification;
+    }
+
+    public String getKeyAlgorithm() {
+        return keyAlgorithm;
+    }
+
+    public String getAltKeyAlgorithm() {
+        return altKeyAlgorithm;
+    }
+
+}
