6ce6e626c2a69598f89d91a292d58040b38f6105
Måns Andersson <mans.andersson@primekey.com>
2024-04-09 07:34:12 +0000

    EJBCAINTER-673: Gradle build system

---
+++ modules/ejbca-rest-ca/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    java
+}
+
+dependencies {
+    compileOnly(project(":modules:cesecore-common"))
+    compileOnly(project(":modules:cesecore-ejb-interface"))
+    compileOnly(project(":modules:ejbca-common"))
+    compileOnly(project(":modules:ejbca-ejb-interface"))
+    compileOnly(project(":modules:ejbca-rest-common"))
+    compileOnly(libs.bcpkix)
+    compileOnly(libs.bcprov)
+    compileOnly(libs.bctls)
+    compileOnly(libs.bcutil)
+    compileOnly(libs.commons.lang)
+    compileOnly(libs.commons.fileupload)
+    compileOnly(libs.log4j.v12.api)
+    compileOnly(libs.java.ee.api)
+    compileOnly(libs.x509.common.util)
+}
+
+sourceSets {
+    val main by getting {
+        java {
+            setSrcDirs(listOf("src"))
+        }
+    }
+}
+
+tasks.jar {
+    from(sourceSets["main"].output)
+}
