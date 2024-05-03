e18a7e2cb09676bf77de743ff75c28f5db140cf5
Tanmoy Kundu <tanmoy.kundu@primekey.com>
2024-05-03 11:46:07 +0200

    EJBCAINTER-842: check for missing licence headers, added missing licence headers

--- modules/edition-specific/src-interface/org/ejbca/core/ejb/its/EtsiEcaOperationsSessionRemote.java
+++ modules/edition-specific/src-interface/org/ejbca/core/ejb/its/EtsiEcaOperationsSessionRemote.java
@@ -1,3 +1,13 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA - Proprietary Modules: Enterprise Certificate Authority        *
+ *                                                                       *
+ *  Copyright (c), PrimeKey Solutions AB. All rights reserved.           *
+ *  The use of the Proprietary Modules are subject to specific           *
+ *  commercial license terms.                                            *
+ *                                                                       *
+ *************************************************************************/
+
 package org.ejbca.core.ejb.its;
 
 public interface EtsiEcaOperationsSessionRemote extends EtsiEcaOperationsSession {
