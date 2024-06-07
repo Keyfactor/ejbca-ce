d7bb043bffa2fbefe8d79c3b1174a77cb220145e
Andris Dobičinaitis <andris.dobicinaitis@keyfactor.com>
2024-06-07 17:17:25 +0300

    ECA-12371: Configured Gradle to recognize CLI sources in 'ejbca-entity' module.

---
+++ modules/ejbca-entity/cli/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    java
+}
+
+dependencies {
+    compileOnly(project(":modules:ejbca-common"))
+    compileOnly(project(":modules:ejbca-entity"))
+    compileOnly(project(":modules:cesecore-common"))
+    compileOnly(project(":modules:cesecore-entity"))
+    compileOnly(libs.java.ee.api)
+    compileOnly(libs.log4j.v12.api)
+    compileOnly(libs.xstream)
+    compileOnly(libs.hibernate.core)
+    compileOnly(libs.bundles.bouncy.castle)
+    compileOnly(libs.commons.configuration2)
+}
+
+sourceSets {
+    val main by getting {
+        java {
+            setSrcDirs(listOf("../src-cli"))
+        }
+    }
+}
