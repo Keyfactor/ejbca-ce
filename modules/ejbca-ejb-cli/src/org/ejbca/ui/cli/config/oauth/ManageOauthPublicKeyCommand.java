02551514c9f61660a81bdd632f34d6c38f4a6954
Lauri Kongas <lauri.kongas.ext@primekey.com>
2024-06-25 14:40:05 +0300

    ECA-12492: Only load RSA keys since EC keys are not supported

--- modules/ejbca-ejb-cli/src/org/ejbca/ui/cli/config/oauth/ManageOauthPublicKeyCommand.java
+++ modules/ejbca-ejb-cli/src/org/ejbca/ui/cli/config/oauth/ManageOauthPublicKeyCommand.java
@@ -36,6 +36,7 @@ import org.ejbca.util.oauth.OAuthTools;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.KeyType;
 
 /**
  * Adds or removes public keys to/from already existing Trusted OAuth Provider
@@ -219,6 +220,10 @@ public class ManageOauthPublicKeyCommand extends BaseOAuthConfigCommand{
                 if (jwk.getKeyID() != null && isDuplicateKeyId(jwk.getKeyID())) {
                     continue;
                 };
+                // We only support RSA keys
+                if (!jwk.getKeyType().equals(KeyType.RSA)) {
+                    continue;
+                }
                 final PublicKey publicKey = jwk.toRSAKey().toPublicKey();
                 final byte[] encoded = publicKey.getEncoded();
                 oAuthKeyInfo.addPublicKey(jwk.getKeyID(), encoded);
