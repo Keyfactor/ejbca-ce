12a33c7afe74254eceed05de44f6647d2c8bc278
Jekaterina Bunina <jekaterina.bunina@keyfactor.com>
2024-07-19 14:27:56 +0300

    fix merge

--- modules/admin-gui/src/org/ejbca/ui/web/admin/endentity/ViewEndEntityMBean.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/endentity/ViewEndEntityMBean.java
@@ -21,13 +21,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.annotation.PostConstruct;
-import javax.faces.context.FacesContext;
-import javax.faces.model.SelectItem;
-import javax.faces.view.ViewScoped;
-import javax.inject.Named;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.model.SelectItem;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang3.tuple.ImmutablePair;
