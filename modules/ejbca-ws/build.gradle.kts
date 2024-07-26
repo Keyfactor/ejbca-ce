5cb7f247946b21092d29dc27bc0ce301030b677e
Andris Dobičinaitis <andris.dobicinaitis@keyfactor.com>
2024-07-26 10:36:29 +0300

    ECA-12371: Added cryptotoken libraries to a Gradle lib bundle.

--- modules/ejbca-ws/build.gradle.kts
+++ modules/ejbca-ws/build.gradle.kts
@@ -22,8 +22,7 @@ dependencies {
     compileOnly(libs.cert.cvc)
     compileOnly(libs.log4j.v12.api)
     compileOnly(libs.x509.common.util)
-    compileOnly(libs.cryptotokens.api)
-    compileOnly(libs.cryptotokens.impl)
+    compileOnly(libs.bundles.cryptotokens)
 }
 
 sourceSets {
