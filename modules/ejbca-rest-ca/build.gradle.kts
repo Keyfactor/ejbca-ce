267c199adfc486563d7525ce1fb607b691a05aed
Serkan Ongan <serkan.ongan.ext@primekey.com>
2024-08-06 13:04:41 +0300

    EJBCAINTER-951: Fix gradle files

--- modules/ejbca-rest-ca/build.gradle.kts
+++ modules/ejbca-rest-ca/build.gradle.kts
@@ -17,7 +17,7 @@ dependencies {
     compileOnly(libs.commons.fileupload2.core)
     compileOnly(libs.commons.io)
     compileOnly(libs.log4j.v12.api)
-    compileOnly(libs.java.ee.api)
+    compileOnly(libs.jakartaee.api)
     compileOnly(libs.x509.common.util)
 }
 
