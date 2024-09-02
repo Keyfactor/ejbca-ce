99bb84f9bec6350a6424bfdb98ef45e5cbf03204
Lauri Kongas <lauri.kongas.ext@primekey.com>
2024-09-02 18:35:12 +0300

    ECA-12504: Don't set dialect explicitly

--- modules/ejbca-entity/build.gradle.kts
+++ modules/ejbca-entity/build.gradle.kts
@@ -8,27 +8,6 @@ val props: Properties = Properties().apply {
         load(file(propertiesFilePath + ".sample").inputStream())
     }
 }
-val dialectMap = mapOf(
-    "db2" to "org.hibernate.dialect.DB2Dialect",
-    "derby" to "org.hibernate.dialect.DerbyDialect",
-    "hsqldb" to "org.hibernate.dialect.HSQLDialect",
-    "h2" to "org.hibernate.dialect.H2Dialect",
-    "informix" to "org.hibernate.community.dialect.InformixDialect",
-    "ingres" to "org.hibernate.community.dialect.IngresDialect",
-    "mssql" to "org.hibernate.dialect.SQLServerDialect",
-    "oracle" to "org.hibernate.dialect.OracleDialect",
-    "postgres" to "org.hibernate.dialect.PostgreSQLDialect",
-    "sybase" to "org.hibernate.dialect.SybaseDialect"
-)
-
-val databaseName = props.getProperty("database.name", "mysql")
-val dialect = dialectMap[databaseName]
-
-if (dialect != null) {
-    props.setProperty("hibernate.dialect", dialect)
-} else if (databaseName != "mysql") {
-    throw IllegalArgumentException("Unsupported database type $databaseName")
-}
 
 plugins {
     java
@@ -63,11 +42,9 @@ tasks.jar {
         rename("persistence-ds-template.xml", "persistence.xml")
         into("META-INF")
         filter { line: String ->
-            val hibernateDialect = props.getProperty("hibernate.dialect", "")
             line.replace("\${datasource.jndi-name-prefix}", "java:/")
                 .replace("\${datasource.jndi-name}", props.getProperty("datasource.jndi-name", "EjbcaDS"))
                 .replace("\${database.name}", props.getProperty("database.name", "mysql"))
-                .replace("\${hibernate.dialect}", hibernateDialect)
         }
     }
 }
