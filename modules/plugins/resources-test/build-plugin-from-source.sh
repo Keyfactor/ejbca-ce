c597f4c27b7eb43df6b21c5e78cdc96968c1aebd
Gustav Hallén <gustav.hallen@keyfactor.com>
2024-09-05 13:30:43 +0200

    EJBCAINTER-1004: Added extra validation

--- modules/plugins/resources-test/build-plugin-from-source.sh
+++ modules/plugins/resources-test/build-plugin-from-source.sh
@@ -8,5 +8,23 @@ then
 fi
 
 pushd $EJBCA_HOME
-ant clean build -Dejbca.plugin.conf.path=$EJBCA_HOME/src/samples/plugins
+ant clean build -Dejbca.plugin.conf.path=$EJBCA_HOME/src/samples/plugins | tee output.log
+grep 'Plugin Builder Tester Executing!' output.log
+export result=$?
+rm -rf output.log
 popd
+
+echo ""
+echo ""
+echo ""
+
+if [ $result -eq 0 ];
+then
+  echo "Plugin built successfully"
+  echo ""
+else
+  echo "The line: 'Plugin Builder Tester Executing!' from the build script is missing."
+  echo ""
+  exit 1
+fi
+
