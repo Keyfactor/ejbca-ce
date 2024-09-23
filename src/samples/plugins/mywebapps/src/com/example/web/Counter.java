a27c735cedef8a2e8b709164960f39c086f06796
Gustav Hallén <gustav.hallen@keyfactor.com>
2024-09-23 11:55:28 +0200

    EJBCAINTER-1004: Fixed plugin mywebapps

--- src/samples/plugins/mywebapps/src/com/example/web/Counter.java
+++ src/samples/plugins/mywebapps/src/com/example/web/Counter.java
@@ -16,10 +16,10 @@ import javax.ejb.EJB;
 
 import java.io.IOException;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 
