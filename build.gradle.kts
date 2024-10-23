fd5ecbd5407257e9033187aa05afc4d3176eebca
Andris Dobičinaitis <andris.dobicinaitis@keyfactor.com>
2024-10-23 16:38:56 +0300

    ECA-12372: Removed duplicate streambuffer lib.

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,7 +62,6 @@ dependencies {
     deploy(project(path = ":modules:crlstore", configuration = "archives"))
     deploy(project(path = ":modules:ra-gui", configuration = "archives"))
     deploy(project(path = ":modules:ejbca-rest-api", configuration = "archives"))
-    deploy("com.sun.xml.stream.buffer:streambuffer:2.1.0")
     deploy("org.codehaus.woodstox:stax2-api:4.2.1")
 
 
