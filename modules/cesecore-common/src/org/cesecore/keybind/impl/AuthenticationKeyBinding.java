92e40340d535663bf227d6d89cfc4c8de14db6a9
Mike Agrenius Kushner <mike.kushner@keyfactor.com>
2024-11-19 10:47:38 +0100

    fix: restored the missing ciphers from defaultvalues.properties
    refs eca-12782

--- modules/cesecore-common/src/org/cesecore/keybind/impl/AuthenticationKeyBinding.java
+++ modules/cesecore-common/src/org/cesecore/keybind/impl/AuthenticationKeyBinding.java
@@ -45,20 +45,32 @@ public class AuthenticationKeyBinding extends InternalKeyBindingBase {
     public static final String IMPLEMENTATION_ALIAS = "AuthenticationKeyBinding"; // This should not change, even if we rename the class in EJBCA 5.3+..
     public static final String PROPERTY_PROTOCOL_AND_CIPHER_SUITE = "protocolAndCipherSuite";
     
-    public static final String[] CIPHER_SUITES_SUBSET = {"TLSv1.2;TLS_DHE_RSA_WITH_AES_128_GCM_SHA256",
+    public static final String[] CIPHER_SUITES_SUBSET = {
+            "TLSv1.2;TLS_DHE_RSA_WITH_AES_128_GCM_SHA256",
             "TLSv1.2;TLS_DHE_RSA_WITH_AES_256_GCM_SHA384",
+            "TLSv1.2;TLS_DHE_RSA_WITH_AES_256_CBC_SHA256",
             "TLSv1.2;TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
             "TLSv1.2;TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
-            "TLSv1.2;TLS_DHE_RSA_WITH_AES_256_CBC_SHA256",
             "TLSv1.2;TLS_RSA_WITH_AES_256_CBC_SHA256",
             "TLSv1.2;TLS_RSA_WITH_AES_128_CBC_SHA",
+            "TLSv1.2;TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
+            "TLSv1.2;TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256",
+            "TLSv1.2;TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
+            "TLSv1.2;TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384",
+            "TLSv1.2;TLS_ECDH_ECDSA_WITH_AES_128_CBC_SHA256",
+            "TLSv1.2;TLS_ECDH_ECDSA_WITH_AES_256_CBC_SHA384",
             "TLSv1;TLS_DHE_RSA_WITH_AES_256_CBC_SHA",
             "TLSv1;TLS_RSA_WITH_AES_256_CBC_SHA",
             "TLSv1;TLS_RSA_WITH_AES_128_CBC_SHA",
             "TLSv1.3;TLS_AES_128_GCM_SHA256",
             "TLSv1.3;TLS_AES_256_GCM_SHA384",
-            "TLSv1.3;TLS_CHACHA20_POLY1305_SHA256"};
+            "TLSv1.3;TLS_CHACHA20_POLY1305_SHA256"
+    };
 
+            
+
+    
+    
     { 
         addProperty(new DynamicUiProperty<>(PROPERTY_PROTOCOL_AND_CIPHER_SUITE, CIPHER_SUITES_SUBSET[0], Arrays.asList(CIPHER_SUITES_SUBSET)));
     }
