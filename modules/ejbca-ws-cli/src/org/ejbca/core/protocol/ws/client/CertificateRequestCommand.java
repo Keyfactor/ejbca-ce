e7116aea59e35a90994a8bf1ffabce3ef239b123
Henrik Sunmark <henrik.sunmark@keyfactor.com>
2024-11-22 09:58:16 +0100

    ECA-12816: Updated help text

--- modules/ejbca-ws-cli/src/org/ejbca/core/protocol/ws/client/CertificateRequestCommand.java
+++ modules/ejbca-ws-cli/src/org/ejbca/core/protocol/ws/client/CertificateRequestCommand.java
@@ -216,7 +216,7 @@ public class CertificateRequestCommand extends EJBCAWSRABaseCommand implements I
 	@Override
     protected void usage() {
 		getPrintStream().println("Command used to generate a users certificate. If the user does not exist it will be created, and if it exists it will be edited. This command uses a single WS call, certificateRequest.");
-        getPrintStream().println("Usage : certreq <username> <subjectdn> <subjectaltname or NULL> <caname> <endentityprofilename> <certificateprofilename> <reqpath> <reqtype (PKCS10|SPKAC|CRMF)> <encoding (DER|PEM)> <outputpath (optional)> \n\n");       
+        getPrintStream().println("Usage : certreq <username> <subjectdn> <subjectaltname or NULL> <caname> <endentityprofilename> <certificateprofilename> <reqpath> <reqtype (PKCS10|SPKAC|CRMF|PUBKEY)> <encoding (DER|PEM)> <outputpath (optional)> \n\n"); 
         getPrintStream().println("outputpath : directory where certificate is written in form outputpath/username+.cer|.pem ");
    }
 
