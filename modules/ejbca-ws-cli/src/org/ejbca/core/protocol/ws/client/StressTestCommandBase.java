b7e35fac2f4fa00f4163232fbe5ff2389cd2a05c
TaherUddin <taher.uddin@keyfactor.com>
2024-11-29 19:45:54 +0200

    EJBCAINTER-1173: refactored to use a base class for both cvc stress test and ejbca stress test

---
+++ modules/ejbca-ws-cli/src/org/ejbca/core/protocol/ws/client/StressTestCommandBase.java
@@ -0,0 +1,128 @@
+package org.ejbca.core.protocol.ws.client;
+
+import org.ejbca.ui.cli.ErrorAdminCommandException;
+import org.ejbca.ui.cli.IAdminCommand;
+import org.ejbca.ui.cli.IllegalAdminCommandException;
+import org.ejbca.util.PerformanceTest;
+
+import java.util.Random;
+
+public abstract class StressTestCommandBase extends EJBCAWSRABaseCommand implements IAdminCommand {
+    final static public String USER_NAME_TAG = "<userName>";
+    private static PerformanceTest PERFORMANCE_TEST = new PerformanceTest();
+
+    enum TestType {
+        BASIC,
+        BASICSINGLETRANS,
+        BASICSINGLETRANS_SAMEUSER,
+        REVOKE,
+        REVOKE_BACKDATED,
+        REVOKEALOT
+    }
+
+    StressTestCommandBase(String[] args) {
+        super(args);
+    }
+
+    @Override
+    protected abstract void usage();
+
+    protected abstract PerformanceTest.CommandFactory getCommandFactory(
+            String caName,
+            String endEntityProfileName,
+            String certificateProfileName,
+            StressTestCommand.TestType testType, // TODO: fix after StressTestCommand is refactored
+            int maxCertificateSN,
+            String subjectDN,
+            String keyAlgorithm,
+            int keySize,
+            String curve
+    );
+
+    @Override
+    public void execute() throws IllegalAdminCommandException, ErrorAdminCommandException {
+
+        try {
+            if(this.args.length < 2){
+                usage();
+                System.exit(-1); // NOPMD, this is not a JEE app
+            }
+            PerformanceTest.NrOfThreadsAndNrOfTests notanot = new PerformanceTest.NrOfThreadsAndNrOfTests(this.args.length>2 ? this.args[2] : null);
+            final int waitTime = this.args.length>3 ? Integer.parseInt(this.args[3]) : -1;
+            final String caName = this.args[1];
+            final String endEntityProfileName = this.args.length>4 ? this.args[4] : "EMPTY";
+            final String certificateProfileName = this.args.length>5 ? this.args[5] : "ENDUSER";
+            final StressTestCommand.TestType testType = this.args.length>6 ? StressTestCommand.TestType.valueOf(this.args[6]) : StressTestCommand.TestType.BASIC;
+            if (notanot.getTests() == -1) {
+                notanot.setTests(this.args.length>7 ? Integer.parseInt(this.args[7]) : -1);
+            }
+            final int maxCertificateSN;
+            final String subjectDN = System.getProperty("subjectDN", "CN="+USER_NAME_TAG);
+            {
+                final String sTmp = System.getProperty("maxCertSN");
+                int iTmp;
+                try {
+                    iTmp = sTmp!=null && sTmp.length()>0 ? Integer.parseInt(sTmp) : -1;
+                } catch ( NumberFormatException e ) {
+                    iTmp = -1;
+                }
+                maxCertificateSN = iTmp;
+            }
+            final String keyAlgorithm = System.getProperty("keyAlgorithm", "RSA");
+            final String curve = System.getProperty("curve", "secp192r1");
+            final int keySize = Integer.parseInt(System.getProperty("keySize",
+                    keyAlgorithm.equals("RSA") ? "1024" : "571"));
+
+            getPrintStream().println(
+                    "---- StressTestCommandBase ---- :: caName=" + caName + " :: "
+                            + " :: endEntityProfileName=" + endEntityProfileName + " :: "
+                            + " certificateProfileName=" + certificateProfileName + " :: "
+                            + " :: testType=" + testType + " :: "
+                            + " maxCertificateSN=" + maxCertificateSN + " :: "
+                            + " subjectDN=" + subjectDN + " :: "
+                            + " keyAlgorithm=" + keyAlgorithm + " :: "
+                            + " keySize=" + keySize + " :: "
+                            + " curve=" + curve + " :: "
+            );
+            this.PERFORMANCE_TEST.execute(
+                    getCommandFactory(
+                            caName,
+                            endEntityProfileName,
+                            certificateProfileName,
+                            testType,
+                            maxCertificateSN,
+                            subjectDN,
+                            keyAlgorithm,
+                            keySize,
+                            curve
+                    ),
+                    notanot.getThreads(),
+                    notanot.getTests(),
+                    waitTime,
+                    getPrintStream()
+            );
+            getPrintStream().println("A test key for each thread is generated. This could take some time if you have specified many threads and long keys.");
+            synchronized(this) {
+                wait();
+            }
+        } catch( InterruptedException e) {
+            // do nothing since user wants to exit.
+        } catch( Exception e) {
+            throw new ErrorAdminCommandException(e);
+        }finally{
+            this.PERFORMANCE_TEST.getLog().close();
+        }
+    }
+
+    public static long nextLong() {
+        return PERFORMANCE_TEST.nextLong();
+    }
+
+    public static PerformanceTest.Log getLog() {
+        return PERFORMANCE_TEST.getLog();
+    }
+
+    public static Random getRandom() {
+        return PERFORMANCE_TEST.getRandom();
+    }
+}
---
+++ modules/ejbca-ws-cli/src/org/ejbca/core/protocol/ws/client/stress/EditUserCommand.java
@@ -0,0 +1,55 @@
+package org.ejbca.core.protocol.ws.client.stress;
+
+import org.cesecore.certificates.endentity.EndEntityConstants;
+import org.ejbca.core.protocol.ws.client.StressTestCommandBase;
+import org.ejbca.core.protocol.ws.client.gen.EjbcaWS;
+import org.ejbca.core.protocol.ws.client.gen.UserDataVOWS;
+import org.ejbca.util.PerformanceTest;
+
+import java.math.BigInteger;
+
+public class EditUserCommand extends BaseCommand implements PerformanceTest.Command {
+    final private EjbcaWS ejbcaWS;
+    final private UserDataVOWS user;
+    final private boolean doCreateNewUser;
+    final private int bitsInCertificateSN;
+
+    public EditUserCommand(EjbcaWS _ejbcaWS, String caName, String endEntityProfileName, String certificateProfileName,
+                    JobData _jobData, boolean _doCreateNewUser, int _bitsInCertificateSN) {
+        super(_jobData);
+        this.doCreateNewUser = _doCreateNewUser;
+        this.ejbcaWS = _ejbcaWS;
+        this.user = new UserDataVOWS();
+        this.user.setClearPwd(true);
+        this.user.setCaName(caName);
+        this.user.setEmail(null);
+        this.user.setSubjectAltName(null);
+        this.user.setStatus(EndEntityConstants.STATUS_NEW);
+        this.user.setTokenType(org.ejbca.core.protocol.ws.objects.UserDataVOWS.TOKEN_TYPE_USERGENERATED);
+        this.user.setEndEntityProfileName(endEntityProfileName);
+        this.user.setCertificateProfileName(certificateProfileName);
+        this.bitsInCertificateSN = _bitsInCertificateSN;
+    }
+    @Override
+    public boolean doIt() throws Exception {
+        if ( this.doCreateNewUser ) {
+            this.jobData.passWord = "foo123";
+            this.jobData.userName = "WSTESTUSER"+ StressTestCommandBase.nextLong();
+        }
+        if ( this.bitsInCertificateSN>0 && this.doCreateNewUser ) {
+            this.user.setCertificateSerialNumber(new BigInteger(this.bitsInCertificateSN, StressTestCommandBase.getRandom()));
+        }
+        this.user.setSubjectDN(this.jobData.getDN());
+        this.user.setUsername(this.jobData.userName);
+        this.user.setPassword(this.jobData.passWord);
+        this.ejbcaWS.editUser(this.user);
+        return true;
+    }
+    @Override
+    public String getJobTimeDescription() {
+        if ( this.doCreateNewUser ) {
+            return "Relative time spent registering new users";
+        }
+        return "Relative time spent setting status of user to NEW";
+    }
+}
