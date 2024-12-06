a012900ea081880fa3c45d271817ffb714007d91
TaherUddin <taher.uddin@keyfactor.com>
2024-12-06 14:26:26 +0200

    EJBCAINTER-1173: added file heading

--- modules/ejbca-ws-cli/src/org/ejbca/core/protocol/ws/client/stress/EditUserCommand.java
+++ modules/ejbca-ws-cli/src/org/ejbca/core/protocol/ws/client/stress/EditUserCommand.java
@@ -3,6 +3,19 @@ package org.ejbca.core.protocol.ws.client.stress;
 import org.cesecore.certificates.endentity.EndEntityConstants;
 import org.ejbca.core.protocol.ws.client.StressTestCommandBase;
 import org.ejbca.core.protocol.ws.client.gen.EjbcaWS;
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
 import org.ejbca.core.protocol.ws.client.gen.UserDataVOWS;
 import org.ejbca.util.PerformanceTest;
 
