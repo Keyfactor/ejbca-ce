2d4bd04a605f1cbd2de275ed000af9da8771d30f
TaherUddin <taher.uddin@keyfactor.com>
2024-12-06 14:26:26 +0200

    EJBCAINTER-1173: added file heading

--- modules/ejbca-ws-cli/src/org/ejbca/core/protocol/ws/client/stress/RevokeCertCommand.java
+++ modules/ejbca-ws-cli/src/org/ejbca/core/protocol/ws/client/stress/RevokeCertCommand.java
@@ -1,3 +1,16 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA Community: The OpenSource Certificate Authority                *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
 package org.ejbca.core.protocol.ws.client.stress;
 
 import com.keyfactor.util.CertTools;
@@ -29,6 +42,6 @@ public class RevokeCertCommand extends BaseCommand implements PerformanceTest.Co
 
     @Override
     public String getJobTimeDescription() {
-        return "Relative time spent revoking certificates.";
+        return "Relative time spent revoking certificates";
     }
 }
