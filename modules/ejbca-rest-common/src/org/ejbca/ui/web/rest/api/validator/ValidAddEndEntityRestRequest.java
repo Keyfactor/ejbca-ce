17bdba4f07f5b2ccbd6507651710e3ea1f305f9c
Jekaterina Bunina <jekaterina.bunina@keyfactor.com>
2025-01-13 12:31:26 +0200

    feat: ECA-10221 edit end entity system test ref: ECA-10221

--- modules/ejbca-rest-common/src/org/ejbca/ui/web/rest/api/validator/ValidAddEndEntityRestRequest.java
+++ modules/ejbca-rest-common/src/org/ejbca/ui/web/rest/api/validator/ValidAddEndEntityRestRequest.java
@@ -91,7 +91,7 @@ public @interface ValidAddEndEntityRestRequest {
 
     class Validator implements ConstraintValidator<ValidAddEndEntityRestRequest, AddEndEntityRestRequest> {
 
-        private final Pattern PATTERN_DATE_ISO8601 = Pattern.compile("^(?:[1-9]\\d{3}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1\\d|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[1-9]\\d(?:0[48]|[2468][048]|[13579][26])|(?:[2468][048]|[13579][26])00)-02-29)T(?:[01]\\d|2[0-3]):[0-5]\\d:[0-5]\\d(?:Z)$");
+        private final Pattern PATTERN_DATE_ISO8601 = Pattern.compile("^(?:[1-9]\\d{3}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1\\d|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[1-9]\\d(?:0[48]|[2468][048]|[13579][26])|(?:[2468][048]|[13579][26])00)-02-29) (?:[01]\\d|2[0-3]):[0-5]\\d:[0-5]\\d$");
 
         @Override
         public void initialize(final ValidAddEndEntityRestRequest validAddEndEntityRestRequest) {
