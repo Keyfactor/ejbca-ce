dcffc401165c517df306870691952e32e2a7e468
Mark Wright <mark.wright@primekey.com>
2025-02-03 17:13:44 -0500

    Re-add incorrectly removed throws.  Remove unnecessary super().

--- modules/cesecore-common/src/org/cesecore/util/ui/DynamicUiModel.java
+++ modules/cesecore-common/src/org/cesecore/util/ui/DynamicUiModel.java
@@ -23,8 +23,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import com.keyfactor.CesecoreException;
+
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
+
+import com.keyfactor.CesecoreException;
 
 /**
  * Base implementation for domain objects (or other objects) with dynamic UI properties.
@@ -99,7 +103,6 @@ public class DynamicUiModel implements Serializable {
      * @param filteredDataToLog data to debug log, with large values removed.
      */
     public DynamicUiModel(final LinkedHashMap<Object, Object> data, final LinkedHashMap<Object, Object> filteredDataToLog) {
-        super();
         propertyChangeSupport = new PropertyChangeSupport(this);
         viewComponents = new HashMap<>();
         this.data = data;
@@ -156,7 +159,7 @@ public class DynamicUiModel implements Serializable {
      * Gets the raw data map for the dynamic properties.
      * @return the raw data map.
      */
-    public Map<String,Object> getRawData() {
+    public Map<String,Object> getRawData() throws CesecoreException {
         final LinkedHashMap<String,Object> result = new LinkedHashMap<>();
         for (Entry<String,DynamicUiProperty<?>> entry : properties.entrySet()) {
             if (entry.getValue().isTransientValue()) {
@@ -189,7 +192,7 @@ public class DynamicUiModel implements Serializable {
      * @param dataMap the data map of the entity.
      * @throws CesecoreException 
      */
-    public void writeProperties(final Map<Object, Object> dataMap)  {
+    public void writeProperties(final Map<Object, Object> dataMap) throws CesecoreException {
         dataMap.putAll(getRawData());
         if (log.isDebugEnabled()) {
             log.debug("Dynamic UI properties was written into data map: " + dataMap);
