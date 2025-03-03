f495e38cb4a16f7b1ac455561c117ba4bb69e0ea
Andris Dobičinaitis <andris.dobicinaitis@keyfactor.com>
2025-03-03 17:00:40 +0200

    ECA-12937: Removed redundant import.

--- modules/ejbca-common/src-test/org/ejbca/util/RequestIdUnitTest.java
+++ modules/ejbca-common/src-test/org/ejbca/util/RequestIdUnitTest.java
@@ -23,7 +23,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
 public class RequestIdUnitTest {
