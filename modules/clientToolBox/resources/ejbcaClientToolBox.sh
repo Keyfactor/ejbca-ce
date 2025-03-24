08460ae74ff969c26f405547b8e10c6689008d63
Gustav Hallén <gustav.hallen@keyfactor.com>
2025-03-24 11:26:05 +0100

    ECA-13216: Removed unwanted logs for Windows-users as well

--- modules/clientToolBox/resources/ejbcaClientToolBox.sh
+++ modules/clientToolBox/resources/ejbcaClientToolBox.sh
@@ -14,7 +14,11 @@ else
 	javaCmd=${JAVA_HOME}/bin/java
 fi
 
+# Temporary file to disable logging to  java.util.logging
+echo "handlers=" > java-util-logging.properties
 
 # Finally run java
 #set -x
-${javaCmd} ${JAVA_OPT} --add-exports=jdk.crypto.cryptoki/sun.security.pkcs11.wrapper=ALL-UNNAMED -Dlog4j1.compatibility=true -Djava.util.logging.config.file=java_util_logging.properties -cp "$TOOLBOX_HOME/clientToolBox.jar:${TOOLBOX_HOME}/ext/*" org.ejbca.ui.cli.ClientToolBox "${@}"
+${javaCmd} ${JAVA_OPT} --add-exports=jdk.crypto.cryptoki/sun.security.pkcs11.wrapper=ALL-UNNAMED -Dlog4j1.compatibility=true -Djava.util.logging.config.file=java-util-logging.properties -cp "$TOOLBOX_HOME/clientToolBox.jar:${TOOLBOX_HOME}/ext/*" org.ejbca.ui.cli.ClientToolBox "${@}"
+
+rm -rf java-util-logging.properties
