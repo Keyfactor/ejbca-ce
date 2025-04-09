e5e995b9080cf65d1858103e5663e1da54b1c47d
Tanmoy Kundu <tanmoy.kundu@primekey.com>
2025-04-09 09:08:45 +0200

    ECA-13294: revert changes due to ambiguity

--- modules/ejbca-ejb/src/org/ejbca/core/ejb/ra/EndEntityManagementSessionBean.java
+++ modules/ejbca-ejb/src/org/ejbca/core/ejb/ra/EndEntityManagementSessionBean.java
@@ -421,19 +421,6 @@ public class EndEntityManagementSessionBean implements EndEntityManagementSessio
                 endEntity.setUsername(autousername);
             }
         }
-        
-        if (profile.isIssuanceRevocationReasonUsed() && profile.isIssuanceRevocationReasonModifiable() &&
-                profile.getIssuanceRevocationReason().getDatabaseValue()!=RevokedCertInfo.NOT_REVOKED &&
-                (endEntity.getExtendedInformation()==null 
-                    || endEntity.getExtendedInformation().getCustomData(
-                            ExtendedInformation.CUSTOM_REVOCATIONREASON)==null)) {
-            if (endEntity.getExtendedInformation()==null) {
-                endEntity.setExtendedInformation(new ExtendedInformation());
-            }
-            endEntity.getExtendedInformation().setIssuanceRevocationReason(
-                                    profile.getIssuanceRevocationReason().getDatabaseValue());
-        }
-        
         // Trim
         endEntity.setUsername(StringTools.trim(endEntity.getUsername()));
         final String username = endEntity.getUsername();
@@ -972,17 +959,6 @@ public class EndEntityManagementSessionBean implements EndEntityManagementSessio
 
         if (!isUsernameValid(trimmedNewUsername)) {
             throw new IllegalNameException(INVALID_SYMBOLS_INUSERNAME);
-        }
-        if (profile.isIssuanceRevocationReasonUsed() && profile.isIssuanceRevocationReasonModifiable() &&
-                profile.getIssuanceRevocationReason().getDatabaseValue()!=RevokedCertInfo.NOT_REVOKED &&
-                (endEntityInformation.getExtendedInformation()==null 
-                    || endEntityInformation.getExtendedInformation().getCustomData(
-                            ExtendedInformation.CUSTOM_REVOCATIONREASON)==null)) {
-            if (endEntityInformation.getExtendedInformation()==null) {
-                endEntityInformation.setExtendedInformation(new ExtendedInformation());
-            }
-            endEntityInformation.getExtendedInformation().setIssuanceRevocationReason(
-                                    profile.getIssuanceRevocationReason().getDatabaseValue());
         }
         // Check if user fulfills it's profile.
         if (globalConfiguration.getEnableEndEntityProfileLimitations()) {
