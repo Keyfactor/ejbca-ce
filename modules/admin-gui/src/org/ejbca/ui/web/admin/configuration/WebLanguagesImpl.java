1e1c2d68469b918c23791d316c68a9215392388c
Mike Agrenius Kushner <mike.kushner@keyfactor.com>
2025-05-26 16:25:42 +0200

    fix: reviewer caught a reference I missed
    Refs: ECA-13475

--- modules/admin-gui/src/org/ejbca/ui/web/admin/configuration/WebLanguagesImpl.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/configuration/WebLanguagesImpl.java
@@ -59,9 +59,6 @@ public class WebLanguagesImpl implements Serializable, WebLanguages {
         if(languages == null){
             for(int i=0; i < AVAILABLE_LANGUAGES.length; i++){
                 AVAILABLE_LANGUAGES[i] = AVAILABLE_LANGUAGES[i].trim().toLowerCase();
-                if (AVAILABLE_LANGUAGES[i].equalsIgnoreCase("se")) {  /* For compatibility with EJBCA 6.2.x and before */
-                    AVAILABLE_LANGUAGES[i] = "sv";
-                }
             }
             // Load available languages
             languages = new LanguageProperties[AVAILABLE_LANGUAGES.length];
