c11217ab2afccd1acd44601bd27b9b0a7e08116d
Samuel Lidén Borell <samuel.liden.borell@keyfactor.com>
2025-06-05 16:53:49 +0200

    ECA-13540: Cover missing auth cache invalidation for RA web

--- modules/ra-gui/src/org/ejbca/ra/RaAuthenticationHelper.java
+++ modules/ra-gui/src/org/ejbca/ra/RaAuthenticationHelper.java
@@ -70,8 +70,12 @@ public class RaAuthenticationHelper implements Serializable {
     public AuthenticationToken getAuthenticationToken(final HttpServletRequest httpServletRequest, final HttpServletResponse httpServletResponse) {
         final String currentTlsSessionId = getTlsSessionId(httpServletRequest);
         final int lastUpdateNumber = AuthorizationCache.INSTANCE.getLastUpdateNumber();
-        if (authenticationToken==null || !StringUtils.equals(authenticationTokenTlsSessionId, currentTlsSessionId) ||
-                authenticatedAtUpdateNumber != lastUpdateNumber) {
+        if (authenticatedAtUpdateNumber != lastUpdateNumber) {
+            log.info("Roles have been updated. Forcing re-authentication");
+            authenticationToken = null;
+            x509AuthenticationTokenFingerprint = null;
+        }
+        if (authenticationToken==null || !StringUtils.equals(authenticationTokenTlsSessionId, currentTlsSessionId)) {
             if (log.isTraceEnabled()) {
                 log.trace("New TLS session IDs or authenticationToken: currentClientTlsSessionID: "+currentTlsSessionId+", authenticationTokenTlsSessionId: "+authenticationTokenTlsSessionId);
             }
