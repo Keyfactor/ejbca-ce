06d2af95a1c6ae4bcef1f4504e738e8613441466
Mike Agrenius Kushner <mike.kushner@keyfactor.com>
2025-06-16 16:14:00 -0400

    fix: removed missed references to certextensions.properties
    Refs: ECA-13520

--- modules/cesecore-common/src/org/cesecore/certificates/certificate/certextensions/AvailableCustomCertificateExtensionsConfiguration.java
+++ modules/cesecore-common/src/org/cesecore/certificates/certificate/certextensions/AvailableCustomCertificateExtensionsConfiguration.java
@@ -14,24 +14,19 @@ package org.cesecore.certificates.certificate.certextensions;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.Properties;
 
-import org.apache.commons.lang.StringUtils;
-import org.apache.log4j.Logger;
 import org.cesecore.configuration.ConfigurationBase;
 
 /**
  * This file handles configuration of available Custom Certificate Extensions
  * 
- * @version $Id$
  */
 public class AvailableCustomCertificateExtensionsConfiguration extends ConfigurationBase implements Serializable {
     private static final long serialVersionUID = 7798273820046510706L;
-    private static final Logger log = Logger.getLogger(AvailableCustomCertificateExtensionsConfiguration.class);
     
     public static final String CONFIGURATION_ID = "AVAILABLE_CUSTOM_CERT_EXTENSIONS";
     
@@ -111,67 +106,7 @@ public class AvailableCustomCertificateExtensionsConfiguration extends Configura
         }
         return properties;
     }
-    
-    /**
-     * Used for upgrading from old certextensions.properties files.
-     * Package-internal to allow for testing.
-     */
-    static CertificateExtension getCertificateExtensionFromFile(int id, Properties propertiesInFile) throws CertificateExtentionConfigurationException {
-        String PROPERTY_ID           = "id";
-        String PROPERTY_OID          = ".oid";
-        String PROPERTY_CLASSPATH    = ".classpath";
-        String PROPERTY_DISPLAYNAME  = ".displayname";
-        String PROPERTY_USED         = ".used";
-        String PROPERTY_TRANSLATABLE = ".translatable";
-        String PROPERTY_CRITICAL     = ".critical";
         
-        try{
-            String oid = StringUtils.trim(propertiesInFile.getProperty(PROPERTY_ID + id + PROPERTY_OID));
-            String classPath = StringUtils.trim(propertiesInFile.getProperty(PROPERTY_ID + id + PROPERTY_CLASSPATH));
-            String displayName = StringUtils.trim(propertiesInFile.getProperty(PROPERTY_ID + id + PROPERTY_DISPLAYNAME));
-            log.debug(PROPERTY_ID + id + PROPERTY_USED + ":" + propertiesInFile.getProperty(PROPERTY_ID + id + PROPERTY_USED));
-            boolean used = propertiesInFile.getProperty(PROPERTY_ID + id + PROPERTY_USED).trim().equalsIgnoreCase("TRUE");
-            boolean translatable = propertiesInFile.getProperty(PROPERTY_ID + id + PROPERTY_TRANSLATABLE).trim().equalsIgnoreCase("TRUE");
-            boolean critical = propertiesInFile.getProperty(PROPERTY_ID + id + PROPERTY_CRITICAL).trim().equalsIgnoreCase("TRUE");
-            log.debug(id + ", " + used + ", " +oid + ", " +critical+ ", " +translatable +  ", " + displayName);   
-            if(used){
-                if(oid != null && classPath != null && displayName != null){
-                    Class<?> implClass = Class.forName(classPath);
-                    CertificateExtension certificateExtension = (CertificateExtension) implClass.getDeclaredConstructor().newInstance();
-                    Properties extensionProperties = getExtensionProperties(id, propertiesInFile);
-                    if(translatable) {
-                        extensionProperties.put("translatable", true);
-                    }
-                    certificateExtension.init(id, oid.trim(), displayName, critical, /*required=*/true, extensionProperties);
-                    return certificateExtension;
-
-                }else{
-                    throw new CertificateExtentionConfigurationException("Certificate Extension " + id + " seems to be misconfigured in the certextensions.properties");
-                }
-            }
-            
-        }catch(Exception e){
-            throw new CertificateExtentionConfigurationException("Certificate Extension " + id + " seems to be misconfigured in the certextensions.properties", e);
-        }
-        return null;
-    }
-    
-    private static Properties getExtensionProperties(int id, Properties propertiesInFile) {
-        Properties extProps = new Properties();
-        Iterator<Object> keyIter = propertiesInFile.keySet().iterator();
-        String matchString = "id" + id + ".property."; 
-        while(keyIter.hasNext()){
-            String nextKey = (String) keyIter.next();
-            if(nextKey.startsWith(matchString)){
-                if(nextKey.length() > matchString.length()){
-                  extProps.put(nextKey.substring(matchString.length()), propertiesInFile.get(nextKey));               
-                }
-            }           
-        }
-        return extProps;
-    }
- 
-    
     @Override
     public void upgrade() { }
 
