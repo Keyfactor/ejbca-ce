2d42a5bb2986aea8017bd9dc9996e858e44cdd10
Lauri Kongas <lauri.kongas@keyfactor.com>
2025-06-18 17:08:54 +0300

    ECA-13442: Elevate logging level

--- modules/ejbca-common-web/src/org/ejbca/core/protocol/cmp/CrmfRequestMessage.java
+++ modules/ejbca-common-web/src/org/ejbca/core/protocol/cmp/CrmfRequestMessage.java
@@ -553,9 +553,7 @@ public class CrmfRequestMessage extends BaseCmpMessage implements ICrmfRequestMe
                                 log.debug("POP verify returns: " + ret);
                             }
                         } catch (OperatorCreationException e) {
-                            if (log.isDebugEnabled()) {
-                                log.debug("ContentVerifier verification failed: " + e.getMessage());
-                            }
+                            log.error("POP verification failed: " + e.getMessage());
                         }
                     } else {
                         log.info("Can not verify POP, protObject exists but there is nothing in it.");
