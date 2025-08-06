a419c312c648e53dcf6d71158f15211bc7e1ccd2
Andris Dobičinaitis <andris.dobicinaitis@keyfactor.com>
2025-08-06 09:14:23 +0300

    ECA-13691: Removed redundant manifest attribute that is already set globally.

--- modules/clientToolBox/build.gradle.kts
+++ modules/clientToolBox/build.gradle.kts
@@ -58,9 +58,7 @@ sourceSets {
 
 tasks.jar {
     manifest {
-        val versionString = project.extra["ejbcaVersionString"] as String
         attributes(
-            "Implementation-Version" to versionString,
             "Main-Class" to "org.ejbca.ui.cli.ClientToolBox",
             "Class-Path" to configurations.runtimeClasspath.get().joinToString(" ") { "lib/${it.name}" }
         )
