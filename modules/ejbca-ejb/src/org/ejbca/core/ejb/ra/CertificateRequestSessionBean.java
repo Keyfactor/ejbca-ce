b4c4cb8991168f64958b028a4ffe046d14d30d08
Amin Khorsandi <amin.khorsandi@keyfactor.com>
2025-08-12 21:47:31 +0200

    ECA-13689: integrate with the regression fix.

--- modules/ejbca-ejb/src/org/ejbca/core/ejb/ra/CertificateRequestSessionBean.java
+++ modules/ejbca-ejb/src/org/ejbca/core/ejb/ra/CertificateRequestSessionBean.java
@@ -355,7 +355,7 @@ public class CertificateRequestSessionBean implements CertificateRequestSessionR
         X500NameBuilder builder = new X500NameBuilder(BCStyle.INSTANCE);
 
         // Add RDNs from request
-        if (requestX500Name != null && requestX500Name.getRDNs().length == 0) {
+        if (requestX500Name != null && requestX500Name.getRDNs().length != 0) {
             for (RDN rdn : requestX500Name.getRDNs()) {
                 AttributeTypeAndValue atv = rdn.getFirst();
                 if (atv != null) {
