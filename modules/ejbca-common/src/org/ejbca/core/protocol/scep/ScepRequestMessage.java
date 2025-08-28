d48782c18f5afddcbb02e4796ca73fa8210798e2
Ulf Undmark <ulf.undmark@keyfactor.com>
2025-08-28 11:08:36 +0200

    ECA-13806: fix comment

--- modules/ejbca-common/src/org/ejbca/core/protocol/scep/ScepRequestMessage.java
+++ modules/ejbca-common/src/org/ejbca/core/protocol/scep/ScepRequestMessage.java
@@ -191,7 +191,7 @@ public class ScepRequestMessage extends PKCS10RequestMessage implements RequestM
     private transient String originalDigestAlgorithm = CMSSignedGenerator.DIGEST_SHA256; 
     
     /** preferred content encryption algorithm to use in replies, if applicable.
-     *  Defaults to SMIMECapability.aES256_CBC for SCEP messages. If SCEP request is 
+     *  Defaults to SMIMECapability.aES128_CBC for SCEP messages. If SCEP request is 
      * encrypted with dES_EDE3_CBC it is set to this though. This is only for backwards compatibility issues, as specified in a SCEP draft.
      */
     private transient ASN1ObjectIdentifier contentEncAlg = SMIMECapability.aES128_CBC;
