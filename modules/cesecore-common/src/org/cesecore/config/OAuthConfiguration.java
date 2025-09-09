9a2162747a58cd8d57861725994fffdbfef27e67
Amin Khorsandi <amin.khorsandi@keyfactor.com>
2025-09-09 20:13:43 +0200

    ECA-13844: Fix the code to accept None as default OAuth provider.

--- modules/cesecore-common/src/org/cesecore/config/OAuthConfiguration.java
+++ modules/cesecore-common/src/org/cesecore/config/OAuthConfiguration.java
@@ -72,7 +72,7 @@ public class OAuthConfiguration extends ConfigurationBase implements Serializabl
     }
 
     public void setDefaultOauthKeyLabel(final String label) {
-        if (!label.isBlank()) {
+        if (!label.isBlank() && !label.equalsIgnoreCase("None")) {
             final Map<String, OAuthKeyInfo> oAuthKeyInfoMap = getOauthKeys();
 
             if (oAuthKeyInfoMap != null && !oAuthKeyInfoMap.isEmpty()) {
@@ -86,6 +86,8 @@ public class OAuthConfiguration extends ConfigurationBase implements Serializabl
                 throw new IllegalArgumentException("No OAuth keys found.");
             }
 
+        } else {
+            setDefaultOauthKey(null);
         }
     }
 
