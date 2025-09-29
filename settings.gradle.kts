c7eb314d6341a51bbb581c5e8e31c421917461ca
Tomas Gustavsson <tomas.gustavsson@keyfactor.com>
2025-09-29 09:31:50 +0200

    ECA-13975: upgrade nimbus-jose to 9.37.4

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -48,7 +48,7 @@ dependencyResolutionManagement {
             library("commons-lang3-old", ":commons-lang3:3.14.0")
             library("commons-configuration2", ":commons-configuration2:2.11.0")
             library("commons-collections4", ":commons-collections4:4.4")
-            library("nimbus-jose-jwt", ":nimbus-jose-jwt:9.37.3")
+            library("nimbus-jose-jwt", ":nimbus-jose-jwt:9.37.4")
             library("x509-common-util", ":x509-common-util:5.3.5")
             library("cryptotokens-api", ":cryptotokens-api:3.0.0")
             library("cryptotokens-impl", ":cryptotokens-impl:3.0.0")
