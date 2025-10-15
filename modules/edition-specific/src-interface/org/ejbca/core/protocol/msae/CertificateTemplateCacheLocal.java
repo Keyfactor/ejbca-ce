2b18de5a05a7db000fbd276e9e753c8af39eca13
Lauri Kongas <lauri.kongas@keyfactor.com>
2025-10-15 15:47:07 +0300

    Prevent concurrency issues with the template cache

--- modules/edition-specific/src-interface/org/ejbca/core/protocol/msae/CertificateTemplateCacheLocal.java
+++ modules/edition-specific/src-interface/org/ejbca/core/protocol/msae/CertificateTemplateCacheLocal.java
@@ -23,6 +23,14 @@ import jakarta.ejb.Local;
 public interface CertificateTemplateCacheLocal {
 
     /**
+     * Loader used to fetch template records when the cache is empty or expired.
+     */
+    @FunctionalInterface
+    interface TemplateLoader {
+        List<TemplateRecord> load();
+    }
+
+    /**
      * Gets template records from cache if available and not expired, otherwise returns null
      * 
      * @param alias the alias to get templates for
@@ -33,13 +41,15 @@ public interface CertificateTemplateCacheLocal {
     }
 
     /**
-     * Stores template records in cache with the specified TTL
-     * 
-     * @param alias the alias to store templates for
-     * @param templates the templates to store
-     * @param timeToLive the time to live in milliseconds
+     * Gets template records from cache or loads them using the provided loader if absent/expired.
+     * Implementations must ensure that only one loader runs per alias at a time.
+     *
+     * @param alias the alias to get templates for
+     * @param timeToLive the time to live in seconds for the loaded entry
+     * @param loader a function that returns the list of TemplateRecord when a load is needed
+     * @return the list of TemplateRecord entries for the alias
      */
-    default void putTemplateRecords(String alias, List<TemplateRecord> templates, long timeToLive) {
+    default List<TemplateRecord> getOrLoadTemplateRecords(String alias, int timeToLive, TemplateLoader loader) {
         throw new UnsupportedOperationException("Certificate template cache methods are only supported in EJBCA Enterprise");
     }
 
