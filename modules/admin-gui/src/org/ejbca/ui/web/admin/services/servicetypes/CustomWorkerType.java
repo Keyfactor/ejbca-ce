42d3ceff9494b41ecb1f7468d638bfd228a0da04
andresj <andresj@primekey.com>
2025-11-10 09:32:50 -0600

    ECA-12377 Restore null check from commit d2376462.

--- modules/admin-gui/src/org/ejbca/ui/web/admin/services/servicetypes/CustomWorkerType.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/services/servicetypes/CustomWorkerType.java
@@ -128,9 +128,11 @@ public class CustomWorkerType extends WorkerType {
             retval.load(new ByteArrayInputStream(getPropertyText().getBytes()));        
 		} else {
 		    for (final CustomServiceWorkerProperty customUiProperty : (List<CustomServiceWorkerProperty>)customUiPropertyListDataModel.getWrappedData()) {
-		        // The value can be null, which renders the NPE title on a blank page.
-		        // Do input validation at another place.
-		        retval.setProperty(customUiProperty.getName(), customUiProperty.getValue());
+		        if (customUiProperty.getValue() != null) {
+                    retval.setProperty(customUiProperty.getName(), customUiProperty.getValue());
+                } else if (log.isTraceEnabled()) {
+                    log.trace("Null value property '" + customUiProperty.getName() + "' not stored.");
+                }
 		    }
 		}
 		return retval;
