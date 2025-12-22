aa06c65b991576d2442b046b3086a35b2b0ee9d8
Lauri Kongas <lauri.kongas@keyfactor.com>
2025-12-22 15:00:01 +0200

    ECA-13198: Add kf.roles as a new claim

--- modules/cesecore-common/src/org/cesecore/authorization/user/matchvalues/OAuth2AccessMatchValue.java
+++ modules/cesecore-common/src/org/cesecore/authorization/user/matchvalues/OAuth2AccessMatchValue.java
@@ -30,8 +30,10 @@ public enum OAuth2AccessMatchValue implements AccessMatchValue {
     CLAIM_AUDIENCE(3),
     /** Matches the "oid" (object identifier) claim */
     CLAIM_OBJECTID(4),
-    /** Match one of the entries in the "roles" claim */
+    /** Matches one of the entries in the "roles" claim */
     CLAIM_ROLE(5),
+    /** Matches one of the entries in the "kf.roles" claim */
+    CLAIM_KF_ROLES(6),
     // NOTE: preferred_username, email, etc. should not be considered to be stable, according to the OpenID connect specification.
     // And according to Azure documentation, they MUST NOT be used for authorization decisions. See:
     // https://openid.net/specs/openid-connect-core-1_0.html
--- modules/ejbca-ejb/src/org/ejbca/core/ejb/authentication/web/WebAuthenticationProviderSessionBean.java
+++ modules/ejbca-ejb/src/org/ejbca/core/ejb/authentication/web/WebAuthenticationProviderSessionBean.java
@@ -435,8 +435,8 @@ public class WebAuthenticationProviderSessionBean implements WebAuthenticationPr
         return true;
     }
 
-    private OAuth2Principal createOauthPrincipal(final JWTClaimsSet claims, OAuthKeyInfo keyInfo) {
-        final Builder oauthBuilder = OAuth2Principal.builder()
+    private OAuth2Principal createOauthPrincipal(final JWTClaimsSet claims, final OAuthKeyInfo keyInfo) {
+        return OAuth2Principal.builder()
                 .setOauthProviderId(keyInfo.getInternalId())
                 .setIssuer(claims.getIssuer())
                 .setSubject(claims.getSubject())
@@ -445,27 +445,10 @@ public class WebAuthenticationProviderSessionBean implements WebAuthenticationPr
                 .setPreferredUsername(safeGetClaim(claims, "preferred_username"))
                 .setName(safeGetClaim(claims, "name"))
                 .setEmail(safeGetClaim(claims, "email"))
-                .setEmailVerified(safeGetBooleanClaim(claims, "email_verified"));
-
-        // add Roles if they exist in the JWT and are of the expected type.  All this type checking may be overly paranoid,
-        // but this is an external value used in authentication, and there's no schema for JSON
-        if (claims.getClaims().containsKey("roles")) {
-            final Object rolesClaimObject = claims.getClaim("roles");
-            if (rolesClaimObject instanceof Collection<?>) {
-                ((Collection<?>) rolesClaimObject).forEach(r -> {
-                    if (r instanceof String) {
-                        oauthBuilder.addRole((String) r);
-                    }
-                });
-            }
-            else {
-                if (LOG.isDebugEnabled()) {
-                    LOG.debug("unexpected type of 'roles' claim: " + rolesClaimObject.getClass());
-                }
-            }
-        }
-
-        return oauthBuilder.build();
+                .setEmailVerified(safeGetBooleanClaim(claims, "email_verified"))
+                .addRoles(claims)
+                .addKfRoles(claims)
+                .build();
     }
 
     private String safeGetClaim(final JWTClaimsSet claims, final String claimName) {
