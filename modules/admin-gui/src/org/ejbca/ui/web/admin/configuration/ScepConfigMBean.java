2c5bd8fd2c0be203330005fdc54c2391f3e51110
Anderson Mathias <amathias@keyfactor.com>
2026-01-19 12:35:50 +0200

    fix: Apply same logic from ConfigDump to not show values for scep proxyCA templates when defaultCA is not proxyCA, remove unused EJBs
    ECA-14170

--- modules/admin-gui/src/org/ejbca/ui/web/admin/configuration/ScepConfigMBean.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/configuration/ScepConfigMBean.java
@@ -45,16 +45,15 @@ import org.cesecore.authorization.control.CryptoTokenRules;
 import org.cesecore.authorization.control.StandardRules;
 import org.cesecore.certificates.ca.CAConstants;
 import org.cesecore.certificates.ca.CAFactory;
+import org.cesecore.certificates.ca.CAInfo;
 import org.cesecore.certificates.ca.CaSessionLocal;
 import org.cesecore.certificates.ca.kfenroll.ProxyCa;
-import org.cesecore.certificates.certificate.CertificateCreateSessionLocal;
 import org.cesecore.certificates.certificateprofile.CertificateProfileSessionLocal;
 import org.cesecore.configuration.GlobalConfigurationSessionLocal;
 import org.cesecore.keys.token.CryptoTokenInfo;
 import org.cesecore.keys.token.CryptoTokenManagementSessionLocal;
 import org.ejbca.config.ScepConfiguration;
 import org.ejbca.core.ejb.EnterpriseEditionEjbBridgeSessionLocal;
-import org.ejbca.core.ejb.ra.EndEntityManagementSessionLocal;
 import org.ejbca.core.ejb.ra.raadmin.EndEntityProfileSessionLocal;
 import org.ejbca.core.model.authorization.AccessRulesConstants;
 import org.ejbca.core.model.ra.raadmin.EndEntityProfile;
@@ -192,11 +191,12 @@ public class ScepConfigMBean extends BaseManagedBean implements Serializable {
             this.signingCryptoTokenId = scepConfig.getSigningCryptoTokenId(alias);
             this.encryptionKeyAlias = scepConfig.getEncryptionKeyAlias(alias);
             this.encryptionCryptoTokenId = scepConfig.getEncryptionCryptoTokenId(alias);
-
             /// Proxy CA
-            this.proxyCaEncryptionCertTemplate = scepConfig.getProxyCaEncryptionCertTemplate(alias);
-            this.proxyCaSigningCertTemplate = scepConfig.getProxyCaSigningCertTemplate(alias);
-            this.proxyCaCaEnrollmentTemplate = scepConfig.getProxyCaEnrollmentTemplate(alias);
+            if (isProxyCaAvailable() && isExternalCA(this.raDefaultCA)) {
+                this.proxyCaEncryptionCertTemplate = scepConfig.getProxyCaEncryptionCertTemplate(alias);
+                this.proxyCaSigningCertTemplate = scepConfig.getProxyCaSigningCertTemplate(alias);
+                this.proxyCaCaEnrollmentTemplate = scepConfig.getProxyCaEnrollmentTemplate(alias);
+            }
 
             // ra mode RA keys settings
             String pemEncryptionCertificate = scepConfig.getEncryptionCertificate(alias);
@@ -811,15 +811,11 @@ public class ScepConfigMBean extends BaseManagedBean implements Serializable {
     @EJB
     private CaSessionLocal caSession;
     @EJB
-    private CertificateCreateSessionLocal certificateCreateSession;
-    @EJB
     private CertificateProfileSessionLocal certProfileSession;
     @EJB
     private CryptoTokenManagementSessionLocal cryptoTokenManagementSession;
     @EJB
     private EnterpriseEditionEjbBridgeSessionLocal editionEjbBridgeSession;
-    @EJB
-    private EndEntityManagementSessionLocal endEntityManagementSession;
     @EJB
     private EndEntityProfileSessionLocal endentityProfileSession;
     @EJB
@@ -1432,5 +1428,10 @@ public class ScepConfigMBean extends BaseManagedBean implements Serializable {
         }
         sortedCas.sort((o1, o2) -> o1.getLabel().compareToIgnoreCase(o2.getLabel()));
         return sortedCas;
+    }
+
+    private boolean isExternalCA(final String caName) {
+        final CAInfo defaultCA = caSession.getCAInfoInternal(-1, caName, true);
+        return defaultCA.getStatus() == CAConstants.CA_EXTERNAL;
     }
 }
