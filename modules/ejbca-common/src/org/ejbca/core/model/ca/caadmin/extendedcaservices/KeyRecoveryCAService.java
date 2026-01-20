9c3865a633ba1152e6a46d147dc42f42f4aa2769
Tanmoy Kundu <tanmoy.kundu@primekey.com>
2026-01-20 17:08:42 +0100

    ECA-14451: nullable keyEncryptKey from UI and Configdump

--- modules/ejbca-common/src/org/ejbca/core/model/ca/caadmin/extendedcaservices/KeyRecoveryCAService.java
+++ modules/ejbca-common/src/org/ejbca/core/model/ca/caadmin/extendedcaservices/KeyRecoveryCAService.java
@@ -120,6 +120,10 @@ public class KeyRecoveryCAService extends ExtendedCAService implements Serializa
                 if (log.isDebugEnabled()) {
                     log.debug("Encrypting using alias '"+keyAlias+"' from crypto token "+cryptoToken.getId());
                 }
+                if (keyAlias==null) {
+                    throw new IllegalExtendedCAServiceRequestException("No keyEncryptionKey is selected for CA."
+                            + "Either use a new key or use CA's default key.");
+                }
 	            // Creating the KeyId may just throw an exception, we will log this but store the cert and ignore the error
 	            String keyId = null;
 	            try {
