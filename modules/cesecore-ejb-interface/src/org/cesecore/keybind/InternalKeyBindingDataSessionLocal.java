5775ab3a49f632520fb4029fc4b6482aefe55066
Mike Agrenius Kushner <mike.kushner@keyfactor.com>
2026-01-23 16:35:33 +0100

    fix: reverted changes from ECA-14423
    Refs: ECA-14476

--- modules/cesecore-ejb-interface/src/org/cesecore/keybind/InternalKeyBindingDataSessionLocal.java
+++ modules/cesecore-ejb-interface/src/org/cesecore/keybind/InternalKeyBindingDataSessionLocal.java
@@ -46,11 +46,19 @@ public interface InternalKeyBindingDataSessionLocal {
     /** Clears the InternalKeyBinding cache. */
     void flushCache();
 
-    /** @return true if the specified name is already in use by another InternalKeyBinding of the same type (checks the database, not the cache) */
-    boolean isNameUsed(final String name, final String type);
+    /**
+     * Note that multiple keybindings can't share names, in spite of being of different types.  
+     * 
+     * @return true if the specified name is already in use by another InternalKeyBinding (checks the database, not the cache) 
+     */
+    boolean isNameUsed(final String name);
 
-    /** @return true if the specified name is used by exactly one InternalKeyBinding and that object has the same id (checks the database, not the cache) */
-    boolean isNameUsedByIdOnly(final String name, final int id, final String type);
+    /** 
+     * Note that multiple keybindings can't share names, in spite of being of different types. 
+     * 
+     * @return true if the specified name is used by exactly one InternalKeyBinding and that object has the same id (checks the database, not the cache) 
+     */
+    boolean isNameUsedByIdOnly(final String name, final int id);
 
     /** Should only be used internally by other methods. This is an Interface method so that we can specify @TransactionAttribute(TransactionAttributeType.REQUIRED). 
      * @return the a cached reference to the specified InternalKeyBinding that MAY NOT be edited. Null if not found. */
