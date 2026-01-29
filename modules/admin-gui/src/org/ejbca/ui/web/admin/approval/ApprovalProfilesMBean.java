1cc92dc2151cb80b13da82e302368124e0331595
Jekaterina Bunina <jekaterina.bunina@keyfactor.com>
2026-01-29 15:04:24 +0200

    ECA-14463 clean up

--- modules/admin-gui/src/org/ejbca/ui/web/admin/approval/ApprovalProfilesMBean.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/approval/ApprovalProfilesMBean.java
@@ -14,8 +14,6 @@ package org.ejbca.ui.web.admin.approval;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
