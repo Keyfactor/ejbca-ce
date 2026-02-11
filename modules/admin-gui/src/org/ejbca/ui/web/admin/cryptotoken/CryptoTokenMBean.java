b6534dba73dd27ad1c685b0f13df5f9b47762a3a
Samuel Lidén Borell <samuel.liden.borell@keyfactor.com>
2026-02-11 13:03:51 +0100

    ECA-14484: remove unused import

--- modules/admin-gui/src/org/ejbca/ui/web/admin/cryptotoken/CryptoTokenMBean.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/cryptotoken/CryptoTokenMBean.java
@@ -90,7 +90,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Properties;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
