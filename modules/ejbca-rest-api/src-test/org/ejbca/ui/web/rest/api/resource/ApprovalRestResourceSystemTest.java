afee2a631483ccea6bc4c8daf859a32fdc875e26
Jekaterina Bunina <jekaterina.bunina@keyfactor.com>
2026-02-26 15:45:35 +0200

    ECA-14549 add and fix tests

--- modules/ejbca-rest-api/src-test/org/ejbca/ui/web/rest/api/resource/ApprovalRestResourceSystemTest.java
+++ modules/ejbca-rest-api/src-test/org/ejbca/ui/web/rest/api/resource/ApprovalRestResourceSystemTest.java
@@ -12,6 +12,8 @@ package org.ejbca.ui.web.rest.api.resource;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.Arrays;
 import org.apache.log4j.Logger;
 import org.cesecore.CaTestUtils;
 import org.cesecore.authentication.tokens.AuthenticationToken;
@@ -26,6 +28,8 @@ import org.cesecore.junit.util.TraceLogMethodsTestWatcher;
 import org.cesecore.mock.authentication.tokens.TestAlwaysAllowLocalAuthenticationToken;
 import org.cesecore.mock.authentication.tokens.UsernameBasedAuthenticationToken;
 import org.cesecore.util.EjbRemoteHelper;
+import org.cesecore.util.ui.DynamicUiProperty;
+import org.cesecore.util.ui.RadioButton;
 import org.easymock.EasyMock;
 import org.easymock.EasyMockRunner;
 import org.easymock.Mock;
@@ -38,6 +42,9 @@ import org.ejbca.core.model.approval.ApprovalDataVO;
 import org.ejbca.core.model.approval.ApprovalRequestStatus;
 import org.ejbca.core.model.approval.WaitingForApprovalException;
 import org.ejbca.core.model.approval.profile.AccumulativeApprovalProfile;
+import org.ejbca.core.model.approval.profile.ApprovalPartition;
+import org.ejbca.core.model.approval.profile.ApprovalStep;
+import org.ejbca.core.model.approval.profile.PartitionedApprovalProfile;
 import org.ejbca.core.model.era.*;
 import org.ejbca.ui.web.rest.api.InMemoryRestServer;
 import org.ejbca.ui.web.rest.api.resource.swagger.ApprovalRestResourceSwagger;
@@ -72,12 +79,18 @@ public class ApprovalRestResourceSystemTest extends RestResourceSystemTestBase {
     private static final AuthenticationToken alwaysAllowToken = new TestAlwaysAllowLocalAuthenticationToken(new UsernamePrincipal("ApprovalRestResourceSystemTest"));
 
     private String eeName;
+    private String partitionedEeName;
     private Integer addEndEntityApprovalRequestId;
+    private Integer addEndEntityPartitionedApprovalRequestId;
     private static Integer caId;
+    private static Integer partitionedCaId;
     private static Integer accumulativeApprovalProfileId;
+    private static Integer partitionedApprovalProfileId;
 
     private static final String CA_NAME = "ApprovalRestResourceSystemTest_CA";
+    private static final String CA_NAME_PARTITIONED = "ApprovalRestResourceSystemTest_Partitioned_CA";
     private static final String APPROVAL_PROFILE_NAME = "ApprovalRestResourceSystemTest_APPROVAL_PROFILE";
+    private static final String PARTITIONED_APPROVAL_PROFILE_NAME = "PartitionedApprovalRestResourceSystemTest_APPROVAL_PROFILE";
 
     @Mock
     private RaMasterApiProxyBeanLocal raMasterApiSessionMock;
@@ -107,22 +120,44 @@ public class ApprovalRestResourceSystemTest extends RestResourceSystemTestBase {
         caSession.addCA(alwaysAllowToken, testCa);
         CAInfo testCaInfo = testCa.getCAInfo();
         caId = testCaInfo.getCAId();
+        // Create test CA for Partitioned Approval profile
+        X509CA testPartitionedCa = CaTestUtils.createTestX509CA("CN=" + CA_NAME_PARTITIONED, "foo123".toCharArray(), false);
+        caSession.addCA(alwaysAllowToken, testPartitionedCa);
+        CAInfo testPartitionedCaInfo = testPartitionedCa.getCAInfo();
+        partitionedCaId = testPartitionedCaInfo.getCAId();
         // Create Approval Profile
         final AccumulativeApprovalProfile approvalProfile = new AccumulativeApprovalProfile(APPROVAL_PROFILE_NAME);
         approvalProfile.setNumberOfApprovalsRequired(1);
         accumulativeApprovalProfileId = approvalProfileSession.addApprovalProfile(alwaysAllowToken, approvalProfile);
+        // Create Partitioned Approval Profile
+        final PartitionedApprovalProfile partitionedApprovalProfile = new PartitionedApprovalProfile(PARTITIONED_APPROVAL_PROFILE_NAME);
+        partitionedApprovalProfile.addStepFirst();
+        ApprovalStep firstStep = partitionedApprovalProfile.getFirstStep();
+        ApprovalPartition firstStepPartition = firstStep.getPartitions().values().iterator().next();
+        final DynamicUiProperty<RadioButton>  property = new DynamicUiProperty<>("Radio test", new RadioButton("Blue"),
+                new ArrayList<RadioButton>( Arrays.asList(new RadioButton("Green"), new RadioButton("Blue"), new RadioButton("Red"))));
+        property.setType(RadioButton.class);
+        partitionedApprovalProfile.addPropertyToPartition(firstStep.getStepIdentifier(), firstStepPartition.getPartitionIdentifier(), property);
+
+        partitionedApprovalProfileId = approvalProfileSession.addApprovalProfile(alwaysAllowToken, partitionedApprovalProfile);
         // Apply Approval Profile to CA
         Map<ApprovalRequestType, Integer> approvalSettings = testCaInfo.getApprovals();
         approvalSettings.put(ApprovalRequestType.ADDEDITENDENTITY, accumulativeApprovalProfileId);
         testCaInfo.setApprovals(approvalSettings);
         caSession.editCA(alwaysAllowToken, testCaInfo);
+        approvalSettings = testPartitionedCaInfo.getApprovals();
+        approvalSettings.put(ApprovalRequestType.ADDEDITENDENTITY, partitionedApprovalProfileId);
+        testPartitionedCaInfo.setApprovals(approvalSettings);
+        caSession.editCA(alwaysAllowToken, testPartitionedCaInfo);
     }
 
     @AfterClass
     public static void afterClass() throws Exception {
         RestResourceSystemTestBase.afterClass();
         CaTestUtils.removeCa(alwaysAllowToken, "CN=" + CA_NAME, CA_NAME);
+        CaTestUtils.removeCa(alwaysAllowToken, "CN=" + CA_NAME_PARTITIONED, CA_NAME_PARTITIONED);
         approvalProfileSession.removeApprovalProfile(alwaysAllowToken, accumulativeApprovalProfileId);
+        approvalProfileSession.removeApprovalProfile(alwaysAllowToken, partitionedApprovalProfileId);
     }
 
     @Before
@@ -143,12 +178,27 @@ public class ApprovalRestResourceSystemTest extends RestResourceSystemTestBase {
         } catch (Exception e) {
             throw new RuntimeException("Test setup failed. Could not add end entity", e);
         }
+        // Trigger an add end entity for partitioned approval
+        partitionedEeName = this.getClass().getName() + "_ee_" + System.currentTimeMillis();
+        userdata = new EndEntityInformation(partitionedEeName, "CN=" + partitionedEeName, partitionedCaId, null, null, new EndEntityType(
+                EndEntityTypes.ENDUSER), EndEntityConstants.EMPTY_END_ENTITY_PROFILE, CertificateProfileConstants.CERTPROFILE_FIXED_ENDUSER,
+                EndEntityConstants.TOKEN_SOFT_P12, null);
+        userdata.setPassword("foo123");
+        try {
+            endEntityManagementSession.addUser(alwaysAllowToken, userdata, true);
+        } catch (WaitingForApprovalException e) {
+            // Capture the requestId
+            addEndEntityPartitionedApprovalRequestId = e.getRequestId();
+        } catch (Exception e) {
+            throw new RuntimeException("Test setup failed. Could not add end entity", e);
+        }
     }
 
     @After
     public void after() throws Exception {
         // Remove approval requests
         approvalSession.removeApprovalRequest(alwaysAllowToken, addEndEntityApprovalRequestId);
+        approvalSession.removeApprovalRequest(alwaysAllowToken, addEndEntityPartitionedApprovalRequestId);
 	    // Kill REST Server
         if (mockRestServer != null) {
             mockRestServer.close();
@@ -356,6 +406,7 @@ public class ApprovalRestResourceSystemTest extends RestResourceSystemTestBase {
         // Then
         final JSONObject actualJsonObject = (JSONObject) jsonParser.parse(actualJsonString);
         final JSONArray steps = (JSONArray) actualJsonObject.get("steps");
+        final JSONObject nextStep = (JSONObject) actualJsonObject.get("next_step");
         assertEquals(Response.Status.OK.getStatusCode(), actualResponse.getStatus());
         assertJsonContentType(actualResponse);
         assertEquals(String.valueOf(addEndEntityApprovalRequestId), actualJsonObject.get("request_id"));
@@ -364,14 +415,65 @@ public class ApprovalRestResourceSystemTest extends RestResourceSystemTestBase {
         assertEquals("PENDING", actualJsonObject.get("status"));
         // Verify approval step
         assertNotNull("Steps should not be null", steps);
-        assertEquals("Should have one approval step", 1, steps.size());
-        final JSONObject step = (JSONObject) steps.get(0);
-        assertEquals("Step number should be 1", 1L, step.get("step"));
-        assertEquals("Approval action should be COMPLETED", "PENDING", step.get("approval_action"));
+        assertNotNull("Next step should not be null", nextStep);
+        assertEquals("Should have none approved step", 0, steps.size());
+        assertEquals("Step number should be 1", 1L, nextStep.get("step"));
+        assertEquals("Approval action should be COMPLETED", "PENDING", nextStep.get("approval_action"));
         assertEquals(EndEntityTypes.ENDUSER.toString(), actualJsonObject.get("certificate_profile_name"));
         assertEquals("Approval EEP should be EMPTY", "EMPTY", actualJsonObject.get("end_entity_profile_name"));
         assertEquals("Approval EEP should be subject dn is incorrect", "CN="+eeName, actualJsonObject.get("subject_dn"));
         assertEquals("Approval CA name is incorrect",  CA_NAME, actualJsonObject.get("ca_name"));
+        assertEquals("Approval email is incorrect", "NOVALUE", actualJsonObject.get("email"));
+        assertEquals("Approval key_recoverable is incorrect","NO", actualJsonObject.get("key_recoverable"));
+        assertFalse("Approval subject_name_log_redaction is incorrect", Boolean.getBoolean(actualJsonObject.get("subject_name_log_redaction").toString()));
+        assertEquals("Approval send_notification is incorrect","NO", actualJsonObject.get("send_notification"));
+        assertEquals("Approval subject_directory_attributes is incorrect", "NOVALUE", actualJsonObject.get("subject_directory_attributes"));
+        assertEquals("Approval subject_alternative_name is incorrect", "NOVALUE", actualJsonObject.get("subject_alternative_name"));
+
+
+        // Verify approval was actually processed internally
+        final ApprovalDataVO approvalData = approvalSession.findApprovalDataByRequestId(addEndEntityApprovalRequestId);
+        assertEquals("Approval status is incorrect", ApprovalDataVO.STATUS_WAITINGFORAPPROVAL, approvalData.getStatus());
+    }
+
+    @Test
+    public void testPartitionedApprovalDataRequest() throws Exception {
+        // Given
+
+        // When
+        final Response actualResponse = newRequest("/v1/approval/" + addEndEntityPartitionedApprovalRequestId)
+                .request().get();
+        final String actualJsonString = actualResponse.readEntity(String.class);
+
+        // Then
+        final JSONObject actualJsonObject = (JSONObject) jsonParser.parse(actualJsonString);
+        final JSONArray steps = (JSONArray) actualJsonObject.get("steps");
+        final JSONObject nextStep = (JSONObject) actualJsonObject.get("next_step");
+        assertEquals(Response.Status.OK.getStatusCode(), actualResponse.getStatus());
+        assertJsonContentType(actualResponse);
+        assertEquals("Request id should match", String.valueOf(addEndEntityPartitionedApprovalRequestId), actualJsonObject.get("request_id"));
+        assertEquals("Request type should be Add End Entity","Add End Entity", actualJsonObject.get("request_type"));
+        assertEquals("End Entity name should be correct", partitionedEeName, actualJsonObject.get("end_entity_name"));
+        assertEquals("PENDING", actualJsonObject.get("status"));
+        // Verify approval step
+        assertNotNull("Steps should not be null", steps);
+        assertNotNull("Next step should not be null", nextStep);
+        assertEquals("Should have none approved step", 0, steps.size());
+        assertEquals("Step number should be 1", 1L, nextStep.get("step"));
+        assertEquals("Approval action should be COMPLETED", "PENDING", nextStep.get("approval_action"));
+        final JSONArray properties = (JSONArray) nextStep.get("property_list");
+        assertEquals("Should have one property", 1, properties.size());
+        final JSONObject property = (JSONObject) properties.get(0);
+        assertEquals("Property label ", "Radio test", property.get("label"));
+        assertEquals("Property type ", "RadioButton", property.get("type"));
+        assertEquals("Property value ", "Blue", property.get("value"));
+        final JSONArray possibleValues = (JSONArray) property.get("possible_values");
+        assertEquals("Should have 3 possible values", 3, possibleValues.size());
+
+        assertEquals("Approval certificate profile be ENDUSER", EndEntityTypes.ENDUSER.toString(), actualJsonObject.get("certificate_profile_name"));
+        assertEquals("Approval EEP should be EMPTY", "EMPTY", actualJsonObject.get("end_entity_profile_name"));
+        assertEquals("Approval EEP should be subject dn is incorrect", "CN="+partitionedEeName, actualJsonObject.get("subject_dn"));
+        assertEquals("Approval CA name is incorrect",  CA_NAME_PARTITIONED, actualJsonObject.get("ca_name"));
         assertEquals("Approval email is incorrect", "NOVALUE", actualJsonObject.get("email"));
         assertEquals("Approval key_recoverable is incorrect","NO", actualJsonObject.get("key_recoverable"));
         assertFalse("Approval subject_name_log_redaction is incorrect", Boolean.getBoolean(actualJsonObject.get("subject_name_log_redaction").toString()));
