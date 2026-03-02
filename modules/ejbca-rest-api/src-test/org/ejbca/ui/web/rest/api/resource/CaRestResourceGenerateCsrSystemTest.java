18fed768247801d28228e4a3e7b78711bf41b09d
Tanmoy Kundu <tanmoy.kundu@primekey.com>
2026-03-02 08:51:39 +0100

    ECA-13869: tests for ca csr generation with rest api

--- modules/ejbca-rest-api/src-test/org/ejbca/ui/web/rest/api/resource/CaRestResourceGenerateCsrSystemTest.java
+++ modules/ejbca-rest-api/src-test/org/ejbca/ui/web/rest/api/resource/CaRestResourceGenerateCsrSystemTest.java
@@ -24,15 +24,16 @@ import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.log4j.Logger;
 import org.bouncycastle.operator.ContentVerifierProvider;
 import org.bouncycastle.operator.jcajce.JcaContentVerifierProviderBuilder;
 import org.bouncycastle.pkcs.PKCS10CertificationRequest;
 import org.cesecore.CaTestUtils;
 import org.cesecore.authorization.AuthorizationDeniedException;
+import org.cesecore.certificates.ca.CACommon;
 import org.cesecore.certificates.ca.CAConstants;
 import org.cesecore.certificates.ca.CAFactory;
 import org.cesecore.certificates.ca.CAInfo;
+import org.cesecore.certificates.ca.CaTestSessionRemote;
 import org.cesecore.certificates.ca.InvalidAlgorithmException;
 import org.cesecore.certificates.ca.X509CA;
 import org.cesecore.certificates.ca.X509CAInfo;
@@ -60,9 +61,7 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 public class CaRestResourceGenerateCsrSystemTest extends RestResourceSystemTestBase {
-    
-    private static final Logger log = Logger.getLogger(CaRestResourceGenerateCsrSystemTest.class);
-    
+        
     private static final String ROOT_CA_DN = "CN=RootCaRestResourceGenerateCsrSystemTest,OU=test";
     private static final String SUB_CA_DN = "CN=SubCaRestResourceGenerateCsrSystemTest,OU=test";
     private static final String CA_GENERATE_CSR_URL = "/v1/ca/issuer_dn/generatecsr";
@@ -74,12 +73,14 @@ public class CaRestResourceGenerateCsrSystemTest extends RestResourceSystemTestB
     private CAToken subCaToken;
     
     private X509Certificate rootCaCert;
-    private X509Certificate subCaCert;
     
     private PublicKey rootCaPublicKey;
+    private PublicKey subCaPublicKey;
     
     CryptoTokenManagementProxySessionRemote cryptoTokenManagementProxySession = EjbRemoteHelper.INSTANCE.getRemoteSession(
             CryptoTokenManagementProxySessionRemote.class, EjbRemoteHelper.MODULE_TEST);
+    CaTestSessionRemote caTestSessionRemote = EjbRemoteHelper.INSTANCE.getRemoteSession(
+            CaTestSessionRemote.class, EjbRemoteHelper.MODULE_TEST);
     
     @BeforeClass
     public static void beforeClass() throws Exception {
@@ -126,6 +127,8 @@ public class CaRestResourceGenerateCsrSystemTest extends RestResourceSystemTestB
         subCaToken = 
                 CaTestUtils.createCaToken(subCaCryptoTokenId, AlgorithmConstants.SIGALG_SHA256_WITH_RSA, 
                         AlgorithmConstants.SIGALG_SHA256_WITH_RSA, signingKeyNameSubCa, encryptionKeyNameSubCa);
+        subCaPublicKey = cryptoTokenManagementProxySession.getPublicKey(subCaCryptoTokenId,
+                signingKeyNameSubCa).getPublicKey();
         
     }
     
@@ -149,6 +152,7 @@ public class CaRestResourceGenerateCsrSystemTest extends RestResourceSystemTestB
     
     
     // x509 root ca
+    // after uploading the signed certificate, these root CAs become signed by external CA
     private void generateCsrOfRootCa(String keyPairName, String responseFormat) throws Exception {
         
         String caname = DnComponents.getPartFromDN(ROOT_CA_DN, "CN");
@@ -250,6 +254,124 @@ public class CaRestResourceGenerateCsrSystemTest extends RestResourceSystemTestB
     }
     
     // x509 sub ca + signed by external root
-    // x509 sub ca + signed by external root + upload chain
+    // CA_UNINITIALIZED can be created by Configdump but not AdminWeb
+    private void generateCsrOfSubCa(List<String> certificateChain) throws Exception {
+        
+        String keyPairName = SUB_CA_DN + "_" + CAToken.SOFTPRIVATESIGNKEYALIAS;
+        String responseFormat = "PEM";
+        
+        String caname = DnComponents.getPartFromDN(SUB_CA_DN, "CN");
+        int certificateProfile = CertificateProfileConstants.CERTPROFILE_FIXED_SUBCA;
+        X509CAInfo cainfo = X509CAInfo.getDefaultX509CAInfo(
+                SUB_CA_DN, caname, CAConstants.CA_UNINITIALIZED, certificateProfile, "1730d",
+                CAInfo.SIGNEDBYEXTERNALCA, null, subCaToken);
+        X509CA x509ca = (X509CA) CAFactory.INSTANCE.getX509CAImpl(cainfo);
+        try {
+            x509ca.setCAToken(subCaToken);
+        } catch (InvalidAlgorithmException e) {
+            throw new IllegalStateException(e);
+        }
+        
+        caSession.addCA(INTERNAL_ADMIN_TOKEN, x509ca);
+        
+        GenerateCsrCaRequest requestObject = new GenerateCsrCaRequest();
+        requestObject.setKeyPair(keyPairName);
+        requestObject.setResponseFormat(responseFormat);
+        requestObject.setCertificateChain(certificateChain);
+        
+        final ObjectMapper objectMapper = objectMapperContextResolver.getContext(null);
+        final String requestBody = objectMapper.writeValueAsString(requestObject);
+        final Entity<String> requestEntity = Entity.entity(requestBody, MediaType.APPLICATION_JSON);
+        Response response = newRequest(CA_GENERATE_CSR_URL.replace("issuer_dn", SUB_CA_DN)).request().put(requestEntity);
+        
+        assertEquals(response.getStatus(), 200);
+        
+        // read file content
+        InputStream inputStream = response.readEntity(InputStream.class);
+        byte[] bytes = inputStream.readAllBytes();
+
+        // read and assert file name
+        String contentDisposition =
+                response.getHeaderString("Content-Disposition");
+        if ("PEM".equals(responseFormat)) {
+            assertTrue(extractFileNameSuffix(contentDisposition).equals("pem"));
+        } else {
+            assertTrue(extractFileNameSuffix(contentDisposition).equals("der"));
+        }
+        
+        response.close();
+        
+        // verify signature existing key  
+        PKCS10CertificationRequest csr = "PEM".equals(responseFormat) ? 
+                CertTools.getCertificateRequestFromPem(new String(bytes)):
+                    new PKCS10CertificationRequest(bytes);
+        ContentVerifierProvider verifierProvider =
+                            new JcaContentVerifierProviderBuilder()
+                                    .setProvider("BC")
+                                    .build(subCaPublicKey);
+        boolean isValid = csr.isSignatureValid(verifierProvider);          
+        assertTrue(isValid);
+
+        if (certificateChain!=null) {
+          CACommon subCa = caTestSessionRemote.getCA(INTERNAL_ADMIN_TOKEN, SUB_CA_DN.hashCode());
+          assertEquals(subCa.getRequestCertificateChain().toArray()[0], rootCaCert);
+        }
+    }
+    
+    @Test
+    public void generateCsrOfSubCa() throws Exception {
+        generateCsrOfSubCa(null);
+    }
+    
+    @Test
+    public void generateCsrOfSubCaWithSingleCertificateInUploadedChain() throws Exception {
+        generateCsrOfSubCa(List.of(CertTools.getPemFromCertificate(rootCaCert)));
+    }
+
+    @Test
+    public void errorNonExistentKey() throws Exception {
+        
+        String caname = DnComponents.getPartFromDN(ROOT_CA_DN, "CN");
+        int certificateProfile = CertificateProfileConstants.CERTPROFILE_FIXED_ROOTCA;
+        X509CAInfo cainfo = X509CAInfo.getDefaultX509CAInfo(ROOT_CA_DN, caname, CAConstants.CA_ACTIVE, certificateProfile, "3650d",
+                CAInfo.SELFSIGNED, null, rootCaToken);
+        X509CA x509ca = (X509CA) CAFactory.INSTANCE.getX509CAImpl(cainfo);
+        try {
+            x509ca.setCAToken(rootCaToken);
+        } catch (InvalidAlgorithmException e) {
+            throw new IllegalStateException(e);
+        }
+        // A CA certificate
+        List<Certificate> cachain = new ArrayList<>();
+        cachain.add(rootCaCert);
+        x509ca.setCertificateChain(cachain);
+        
+        caSession.addCA(INTERNAL_ADMIN_TOKEN, x509ca);
+        
+        GenerateCsrCaRequest requestObject = new GenerateCsrCaRequest();
+        requestObject.setKeyPair("unknown");
+        
+        final ObjectMapper objectMapper = objectMapperContextResolver.getContext(null);
+        final String requestBody = objectMapper.writeValueAsString(requestObject);
+        final Entity<String> requestEntity = Entity.entity(requestBody, MediaType.APPLICATION_JSON);
+        Response response = newRequest(CA_GENERATE_CSR_URL.replace("issuer_dn", ROOT_CA_DN)).request().put(requestEntity);
+        
+        assertEquals(response.getStatus(), 400);
+        assertTrue(response.readEntity(String.class).contains("CA cryptotoken is offline or the key pair not found."));
+    }
+    
+    @Test
+    public void errorNonExistentCa() throws Exception {
+        GenerateCsrCaRequest requestObject = new GenerateCsrCaRequest();
+        requestObject.setKeyPair("signKey");
+        
+        final ObjectMapper objectMapper = objectMapperContextResolver.getContext(null);
+        final String requestBody = objectMapper.writeValueAsString(requestObject);
+        final Entity<String> requestEntity = Entity.entity(requestBody, MediaType.APPLICATION_JSON);
+        Response response = newRequest(CA_GENERATE_CSR_URL.replace("issuer_dn", "CN=Unknown")).request().put(requestEntity);
+        
+        assertEquals(response.getStatus(), 400);
+        assertTrue(response.readEntity(String.class).contains("CA with DN: CN=Unknown does not exist."));
+    }
 
 }
